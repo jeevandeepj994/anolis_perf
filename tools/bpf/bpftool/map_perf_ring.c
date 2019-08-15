a6171c80792e8635299c8e29bc5f1192be45855b
Quentin Monnet <quentin.monnet@netronome.com>
2019-08-15 15:32:15 +0100

    tools: bpftool: fix arguments for p_err() in do_event_pipe()
    ANBZ: #5530
    
    commit 9def249dc8409ffc1f5a1d7195f1c462f2b49c07 upstream.
    
    The last argument passed to some calls to the p_err() functions is not
    correct, it should be "*argv" instead of "**argv". This may lead to a
    segmentation fault error if CPU IDs or indices from the command line
    cannot be parsed correctly. Let's fix this.
    
    Fixes: f412eed9dfde ("tools: bpftool: add simple perf event output reader")
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>

--- tools/bpf/bpftool/map_perf_ring.c
+++ tools/bpf/bpftool/map_perf_ring.c
@@ -156,7 +156,7 @@ int do_event_pipe(int argc, char **argv)
 			NEXT_ARG();
 			ctx.cpu = strtoul(*argv, &endptr, 0);
 			if (*endptr) {
-				p_err("can't parse %s as CPU ID", **argv);
+				p_err("can't parse %s as CPU ID", *argv);
 				goto err_close_map;
 			}
 
@@ -167,7 +167,7 @@ int do_event_pipe(int argc, char **argv)
 			NEXT_ARG();
 			ctx.idx = strtoul(*argv, &endptr, 0);
 			if (*endptr) {
-				p_err("can't parse %s as index", **argv);
+				p_err("can't parse %s as index", *argv);
 				goto err_close_map;
 			}
 
