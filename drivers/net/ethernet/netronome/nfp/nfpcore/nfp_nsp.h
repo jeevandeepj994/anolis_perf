6716700a738eb203595ee71827e6c1fbbbf437d5
Dirk van der Merwe <dirk.vandermerwe@netronome.com>
2019-09-09 00:54:19 +0100

    nfp: nsp: add support for fw_loaded command
    ANBZ: #7904
    
    commit 1da16f0c84c39e138bf9245bf32b5cd102c8f142 upstream.
    
    Add support for the simple command that indicates whether application
    firmware is loaded.
    
    Signed-off-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [Yinjun: resolve conflicts]
    Signed-off-by: Yinjun Zhang <yinjun.zhang@corigine.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2618

--- drivers/net/ethernet/netronome/nfp/nfpcore/nfp_nsp.h
+++ drivers/net/ethernet/netronome/nfp/nfpcore/nfp_nsp.h
@@ -50,10 +50,16 @@ int nfp_nsp_device_soft_reset(struct nfp_nsp *state);
 int nfp_nsp_load_fw(struct nfp_nsp *state, const struct firmware *fw);
 int nfp_nsp_write_flash(struct nfp_nsp *state, const struct firmware *fw);
 int nfp_nsp_mac_reinit(struct nfp_nsp *state);
+int nfp_nsp_fw_loaded(struct nfp_nsp *state);
 
 static inline bool nfp_nsp_has_mac_reinit(struct nfp_nsp *state)
 {
 	return nfp_nsp_get_abi_ver_minor(state) > 20;
+}
+
+static inline bool nfp_nsp_has_fw_loaded(struct nfp_nsp *state)
+{
+	return nfp_nsp_get_abi_ver_minor(state) > 25;
 }
 
 enum nfp_eth_interface {
