55798e34643945003e2bd9f752da20fa2d3e0925
Jacob Pan <jacob.jun.pan@linux.intel.com>
2019-10-04 10:44:28 -0700

    iommu/vt-d: iommu/vt-d: report non-recoverable faults to device
    ANBZ: #1435
    
    commit c59707c3b1ceb99560a7ff57aed93f1383abf8c6 intel-github.
    
    Currently, dmar fault IRQ handler does nothing more than rate
    limited printk, no critical hardware handling need to be done
    in IRQ context.
    For some use case such as vIOMMU, it might be useful to report
    non-recoverable faults outside host IOMMU subsystem. DMAR fault
    can come from both DMA and interrupt remapping which has to be
    set up early before threaded IRQ is available.
    This patch adds an option and a workqueue such that when faults
    are requested, DMAR fault IRQ handler can use the IOMMU fault
    reporting API to report.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/482

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -3460,7 +3460,7 @@ static int __init init_dmars(void)
 				goto free_iommu;
 		}
 #endif
-		ret = dmar_set_interrupt(iommu);
+		ret = dmar_set_interrupt(iommu, true);
 		if (ret)
 			goto free_iommu;
 	}
@@ -3979,7 +3979,7 @@ static int intel_iommu_add(struct dmar_drhd_unit *dmaru)
 			goto disable_iommu;
 	}
 #endif
-	ret = dmar_set_interrupt(iommu);
+	ret = dmar_set_interrupt(iommu, true);
 	if (ret)
 		goto disable_iommu;
 
