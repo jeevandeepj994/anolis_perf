0b4f87be4e3d8606637684c9a9db6bd964445576
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2019-11-22 17:42:50 +0300

    x86/traps: Add #VE support for TDX guest
    ANBZ: #523
    
    commit 18a2711a275be0d5a254104762c6d32081952de2 intel-github.
    
    Virtualization Exceptions (#VE) are delivered to TDX guests due to
    specific guest actions which may happen in either user space or the
    kernel:
    
     * Specific instructions (WBINVD, for example)
     * Specific MSR accesses
     * Specific CPUID leaf accesses
     * Access to unmapped pages (EPT violation)
    
    In the settings that Linux will run in, virtualization exceptions are
    never generated on accesses to normal, TD-private memory that has been
    accepted.
    
    Syscall entry code has a critical window where the kernel stack is not
    yet set up. Any exception in this window leads to hard to debug issues
    and can be exploited for privilege escalation. Exceptions in the NMI
    entry code also cause issues. Returning from the exception handler with
    IRET will re-enable NMIs and nested NMI will corrupt the NMI stack.
    
    For these reasons, the kernel avoids #VEs during the syscall gap and
    the NMI entry code. Entry code paths do not access TD-shared memory,
    MMIO regions, use #VE triggering MSRs, instructions, or CPUID leaves
    that might generate #VE. VMM can remove memory from TD at any point,
    but access to unaccepted (or missing) private memory leads to VM
    termination, not to #VE.
    
    Similarly to page faults and breakpoints, #VEs are allowed in NMI
    handlers once the kernel is ready to deal with nested NMIs.
    
    During #VE delivery, all interrupts, including NMIs, are blocked until
    TDGETVEINFO is called. It prevents #VE nesting until the kernel reads
    the VE info.
    
    If a guest kernel action which would normally cause a #VE occurs in
    the interrupt-disabled region before TDGETVEINFO, a #DF (fault
    exception) is delivered to the guest which will result in an oops.
    
    Add basic infrastructure to handle any #VE which occurs in the kernel
    or userspace. Later patches will add handling for specific #VE
    scenarios.
    
    For now, convert unhandled #VE's (everything, until later in this
    series) so that they appear just like a #GP by calling the
    ve_raise_fault() directly. The ve_raise_fault() function is similar
    to #GP handler and is responsible for sending SIGSEGV to userspace
    and CPU die and notifying debuggers and other die chain users.
    
    Intel-SIG: commit 18a2711a275b x86/traps: Add #VE support for TDX guest.
    Add TDX guest kernel support.
    
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    [ Fan Du: amend commit log ]
    Signed-off-by: Fan Du <fan.du@intel.com>
    Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -10,6 +10,7 @@
 
 /* TDX module Call Leaf IDs */
 #define TDX_GET_INFO			1
+#define TDX_GET_VEINFO			3
 
 static bool tdx_guest_detected __ro_after_init;
 
@@ -72,6 +73,66 @@ static u64 get_cc_mask(void)
 	 * Set it for shared pages and clear it for private pages.
 	 */
 	return BIT_ULL(gpa_width - 1);
+}
+
+bool tdx_get_ve_info(struct ve_info *ve)
+{
+	struct tdx_module_output out;
+
+	/*
+	 * NMIs and machine checks are suppressed. Before this point any
+	 * #VE is fatal. After this point (TDGETVEINFO call), NMIs and
+	 * additional #VEs are permitted (but it is expected not to
+	 * happen unless kernel panics).
+	 */
+	if (__tdx_module_call(TDX_GET_VEINFO, 0, 0, 0, 0, &out))
+		return false;
+
+	ve->exit_reason = out.rcx;
+	ve->exit_qual   = out.rdx;
+	ve->gla         = out.r8;
+	ve->gpa         = out.r9;
+	ve->instr_len   = lower_32_bits(out.r10);
+	ve->instr_info  = upper_32_bits(out.r10);
+
+	return true;
+}
+
+/*
+ * Handle the user initiated #VE.
+ *
+ * For example, executing the CPUID instruction from user space
+ * is a valid case and hence the resulting #VE has to be handled.
+ *
+ * For dis-allowed or invalid #VE just return failure.
+ */
+static bool tdx_virt_exception_user(struct pt_regs *regs, struct ve_info *ve)
+{
+	pr_warn("Unexpected #VE: %lld\n", ve->exit_reason);
+	return false;
+}
+
+/* Handle the kernel #VE */
+static bool tdx_virt_exception_kernel(struct pt_regs *regs, struct ve_info *ve)
+{
+	pr_warn("Unexpected #VE: %lld\n", ve->exit_reason);
+	return false;
+}
+
+bool tdx_handle_virt_exception(struct pt_regs *regs, struct ve_info *ve)
+{
+	bool ret;
+
+	if (user_mode(regs))
+		ret = tdx_virt_exception_user(regs, ve);
+	else
+		ret = tdx_virt_exception_kernel(regs, ve);
+
+	/* After successful #VE handling, move the IP */
+	if (ret)
+		regs->ip += ve->instr_len;
+
+	return ret;
 }
 
 bool is_tdx_guest(void)
