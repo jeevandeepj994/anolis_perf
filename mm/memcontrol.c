dd44cff3e9a081eedb33b8dde3904f673715ec6a
zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
2020-04-22 20:54:20 +0800

    anolis: mm: kidled: provide a unified interface in preparation for scanning slab
    ANBZ: #875
    
    Currently, kidled_mem_cgroup_account use 'nr_pages' to store the lru
    pages, but the slab size will less than an page when scanning cold slab.
    In order to unify the two cases, we use the size instead of the number
    of the pages.
    
    Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4153,8 +4153,7 @@ output:
 		seq_printf(m, "  %-8s", kidled_type_str);
 
 		for (i = 0; i < j; i++) {
-			seq_printf(m, " %14lu",
-				   stats->count[t][i] << PAGE_SHIFT);
+			seq_printf(m, " %14lu", stats->count[t][i]);
 		}
 
 		seq_puts(m, "\n");
@@ -7020,7 +7019,7 @@ static int mem_cgroup_move_account(struct page *page,
 
 	ret = 0;
 
-	kidled_mem_cgroup_move_stats(from, to, page, nr_pages);
+	kidled_mem_cgroup_move_stats(from, to, page, nr_pages << PAGE_SHIFT);
 
 	local_irq_disable();
 	mem_cgroup_charge_statistics(to, page, nr_pages);
