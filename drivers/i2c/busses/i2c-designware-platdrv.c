0d404a0319574730e814b2f4ee439592cf7a578b
Pu Wen <puwen@hygon.cn>
2020-08-07 17:35:29 +0800

    i2c: designware: Add device HID for Hygon I2C controller
    ANBZ: #5455
    
    commit 384b02d6b83bd4c5df96de00658623b4e04e2472 upstream.
    
    Add device HID HYGO0010 to match the Hygon ACPI Vendor ID (HYGO) that
    was registered in http://www.uefi.org/acpi_id_list, and the I2C
    controller on Hygon paltform will use the HID.
    
    Signed-off-by: Pu Wen <puwen@hygon.cn>
    Acked-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Wolfram Sang <wsa@kernel.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [fix conflict during backport]
    Signed-off-by: Pu Wen <puwen@hygon.cn>

--- drivers/i2c/busses/i2c-designware-platdrv.c
+++ drivers/i2c/busses/i2c-designware-platdrv.c
@@ -151,6 +151,7 @@ static const struct acpi_device_id dw_i2c_acpi_match[] = {
 	{ "APMC0D0F", 0 },
 	{ "HISI02A1", 0 },
 	{ "HISI02A2", 0 },
+	{ "HYGO0010", ACCESS_INTR_MASK },
 	{ "PHYT0003", 0 },
 	{ }
 };
