39cd808163d553b1d8c1d040b3b9eda01d7f69fe
Wu Hao <hao.wu@intel.com>
2020-09-19 22:57:25 +0800

    kvm: x86: rename KVM_REQ_MCLOCK_INPROGRESS to BLOCK_VMENTRY
    ANBZ: #1660
    
    commit f609f8d7db3a1202eab18c5511dfadd313e76790 intel-github.
    
    This patch renames KVM_REQ_MCLOCK_INPROGREE to a common request
    KVM_REQ_BLOCK_VMENTRY to block vm entry. The purpose of this change
    is adding a common VCPU rendezvous mechanism (no VCPU can enter
    non-root mode before the desired operation is completed).
    
    v3: add rendezvous description.
        fixed building errors on non-x86 platforms.
    
    Intel-SIG: commit f609f8d7db3a kvm: x86: rename
    KVM_REQ_MCLOCK_INPROGRESS to BLOCK_VMENTRY. Necessary to support
    enqcmd/s.
    
    Signed-off-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
    [ Yi Sun: amend commit log ]
    Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/510
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -2579,10 +2579,21 @@ static void pvclock_update_vm_gtod_copy(struct kvm *kvm)
 #endif
 }
 
-void kvm_make_mclock_inprogress_request(struct kvm *kvm)
+void kvm_make_block_vmentry_request(struct kvm *kvm)
 {
-	kvm_make_all_cpus_request(kvm, KVM_REQ_MCLOCK_INPROGRESS);
+	kvm_make_all_cpus_request(kvm, KVM_REQ_BLOCK_VMENTRY);
 }
+EXPORT_SYMBOL_GPL(kvm_make_block_vmentry_request);
+
+void kvm_clear_block_vmentry_request(struct kvm *kvm)
+{
+	int i;
+	struct kvm_vcpu *vcpu;
+
+	kvm_for_each_vcpu(i, vcpu, kvm)
+		kvm_clear_request(KVM_REQ_BLOCK_VMENTRY, vcpu);
+}
+EXPORT_SYMBOL_GPL(kvm_clear_block_vmentry_request);
 
 static void kvm_gen_update_masterclock(struct kvm *kvm)
 {
@@ -2592,7 +2603,8 @@ static void kvm_gen_update_masterclock(struct kvm *kvm)
 	struct kvm_arch *ka = &kvm->arch;
 
 	spin_lock(&ka->pvclock_gtod_sync_lock);
-	kvm_make_mclock_inprogress_request(kvm);
+	kvm_make_block_vmentry_request(kvm);
+
 	/* no guest entries from this point */
 	pvclock_update_vm_gtod_copy(kvm);
 
@@ -2600,8 +2612,7 @@ static void kvm_gen_update_masterclock(struct kvm *kvm)
 		kvm_make_request(KVM_REQ_CLOCK_UPDATE, vcpu);
 
 	/* guest entries allowed */
-	kvm_for_each_vcpu(i, vcpu, kvm)
-		kvm_clear_request(KVM_REQ_MCLOCK_INPROGRESS, vcpu);
+	kvm_clear_block_vmentry_request(kvm);
 
 	spin_unlock(&ka->pvclock_gtod_sync_lock);
 #endif
@@ -7845,7 +7856,7 @@ static void kvm_hyperv_tsc_notifier(void)
 
 	mutex_lock(&kvm_lock);
 	list_for_each_entry(kvm, &vm_list, vm_list)
-		kvm_make_mclock_inprogress_request(kvm);
+		kvm_make_block_vmentry_request(kvm);
 
 	hyperv_stop_tsc_emulation();
 
@@ -7864,9 +7875,7 @@ static void kvm_hyperv_tsc_notifier(void)
 		kvm_for_each_vcpu(cpu, vcpu, kvm)
 			kvm_make_request(KVM_REQ_CLOCK_UPDATE, vcpu);
 
-		kvm_for_each_vcpu(cpu, vcpu, kvm)
-			kvm_clear_request(KVM_REQ_MCLOCK_INPROGRESS, vcpu);
-
+		kvm_clear_block_vmentry_request(kvm);
 		spin_unlock(&ka->pvclock_gtod_sync_lock);
 	}
 	mutex_unlock(&kvm_lock);
