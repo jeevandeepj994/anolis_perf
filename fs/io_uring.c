9ad886798e28462ddb13dbe4a537c865098cb96d
Jens Axboe <axboe@kernel.dk>
2020-09-25 15:23:43 -0600

    io_uring: ensure async buffered read-retry is setup properly
    ANBZ: #190
    
    commit eda593a9e3a8bcfe7f3406a795bf6afd95018126 upstream
    
    A previous commit for fixing up short reads botched the async retry
    path, so we ended up going to worker threads more often than we should.
    Fix this up, so retries work the way they originally were intended to.
    
    Fixes: 227c0c9673d8 ("io_uring: internally retry short reads")
    Reported-by: Hao_Xu <haoxu@linux.alibaba.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    [remove iopoll branch when EAGAIN in io_read]
    Signed-off-by: Hao Xu <haoxu@linux.alibaba.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -3097,13 +3097,11 @@ static int io_read(struct io_kiocb *req, bool force_nonblock)
 	} else if (ret == -EAGAIN) {
 		/* IOPOLL retry should happen for io-wq threads */
 		if (!force_nonblock && (req->ctx->flags & IORING_SETUP_IOPOLL))
-			goto copy_iov;
+			goto done;
 		/* some cases will consume bytes even on error returns */
 		iov_iter_revert(iter, iov_count - iov_iter_count(iter));
-		ret = io_setup_async_rw(req, iovec, inline_vecs, iter, false);
-		if (ret)
-			goto out_free;
-		return -EAGAIN;
+		ret = 0;
+		goto copy_iov;
 	} else if (ret < 0) {
 		goto out_free;
 	}
