faefcb28bd674ce53118059bb93f4d44a6e8bf67
hanliyang <hanliyang@hygon.cn>
2021-04-07 02:46:11 -0400

    anolis: KVM: x86: Restore control registers in __set_sregs() to support CSV2 guest live migration
    ANBZ: #8572
    
    When migrate CSV2 guest to the recipient, the KVM which on
    recipient's side needs to update the guest context so that the guest
    can continues to run. The control register state is necessary for
    updating the guest context.
    
    Allows the control registers to be updated in __set_sregs() so that
    the CSV2 guest could continue running correctly after migrated to
    the recipient.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -11517,21 +11517,24 @@ static int __set_sregs_common(struct kvm_vcpu *vcpu, struct kvm_sregs *sregs,
 	if (kvm_set_apic_base(vcpu, &apic_base_msr))
 		return -EINVAL;
 
-	if (vcpu->arch.guest_state_protected)
+	if (vcpu->arch.guest_state_protected &&
+	    boot_cpu_data.x86_vendor != X86_VENDOR_HYGON)
 		return 0;
 
-	dt.size = sregs->idt.limit;
-	dt.address = sregs->idt.base;
-	static_call(kvm_x86_set_idt)(vcpu, &dt);
-	dt.size = sregs->gdt.limit;
-	dt.address = sregs->gdt.base;
-	static_call(kvm_x86_set_gdt)(vcpu, &dt);
+	if (!vcpu->arch.guest_state_protected) {
+		dt.size = sregs->idt.limit;
+		dt.address = sregs->idt.base;
+		static_call(kvm_x86_set_idt)(vcpu, &dt);
+		dt.size = sregs->gdt.limit;
+		dt.address = sregs->gdt.base;
+		static_call(kvm_x86_set_gdt)(vcpu, &dt);
 
-	vcpu->arch.cr2 = sregs->cr2;
-	*mmu_reset_needed |= kvm_read_cr3(vcpu) != sregs->cr3;
-	vcpu->arch.cr3 = sregs->cr3;
-	kvm_register_mark_dirty(vcpu, VCPU_EXREG_CR3);
-	static_call_cond(kvm_x86_post_set_cr3)(vcpu, sregs->cr3);
+		vcpu->arch.cr2 = sregs->cr2;
+		*mmu_reset_needed |= kvm_read_cr3(vcpu) != sregs->cr3;
+		vcpu->arch.cr3 = sregs->cr3;
+		kvm_register_mark_dirty(vcpu, VCPU_EXREG_CR3);
+		static_call_cond(kvm_x86_post_set_cr3)(vcpu, sregs->cr3);
+	}
 
 	kvm_set_cr8(vcpu, sregs->cr8);
 
@@ -11544,6 +11547,9 @@ static int __set_sregs_common(struct kvm_vcpu *vcpu, struct kvm_sregs *sregs,
 
 	*mmu_reset_needed |= kvm_read_cr4(vcpu) != sregs->cr4;
 	static_call(kvm_x86_set_cr4)(vcpu, sregs->cr4);
+
+	if (vcpu->arch.guest_state_protected)
+		return 0;
 
 	if (update_pdptrs) {
 		idx = srcu_read_lock(&vcpu->kvm->srcu);
