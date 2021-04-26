2f7458f5c047510c6cb3800c6015fcb1f6eb3e43
hanliyang <hanliyang@hygon.cn>
2021-04-26 10:47:46 +0800

    anolis: KVM: x86: Support VM_ATTESTATION hypercall
    ANBZ: #8564
    
    When sev guest wants to collect the attestation report, it cannot directly
    communicate with psp. Add VM_ATTESTATION hypercall to allow sev guest to
    tell host to help get the attestation report. Since sev guest memory is
    encrypted, host cannot tamper with the report data.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2902

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1750,6 +1750,8 @@ struct kvm_x86_ops {
 	 * Returns vCPU specific APICv inhibit reasons
 	 */
 	unsigned long (*vcpu_get_apicv_inhibit_reasons)(struct kvm_vcpu *vcpu);
+
+	int (*vm_attestation)(struct kvm *kvm, unsigned long gpa, unsigned long len);
 };
 
 struct kvm_x86_nested_ops {
