22390eb3649f012f0309b6599a9ce8b14223e814
Yury Norov <yury.norov@gmail.com>
2021-05-06 18:03:03 -0700

    lib: inline _find_next_bit() wrappers
    ANBZ: #9566
    
    commit 5c88af59f9abc202648a431428ad9d32e5d2a201 upstream.
    
    lib/find_bit.c declares five single-line wrappers for _find_next_bit().
    We may turn those wrappers to inline functions.  It eliminates unneeded
    function calls and opens room for compile-time optimizations.
    
    Link: https://lkml.kernel.org/r/20210401003153.97325-8-yury.norov@gmail.com
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Acked-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Acked-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Cc: Alexey Klimov <aklimov@redhat.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: David Sterba <dsterba@suse.com>
    Cc: Dennis Zhou <dennis@kernel.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Jianpeng Ma <jianpeng.ma@intel.com>
    Cc: Joe Perches <joe@perches.com>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Rich Felker <dalias@libc.org>
    Cc: Stefano Brivio <sbrivio@redhat.com>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Cc: Yoshinori Sato <ysato@users.osdn.me>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

--- drivers/firmware/efi/libstub/find.c
+++ drivers/firmware/efi/libstub/find.c
@@ -3,7 +3,7 @@
 #include <linux/minmax.h>
 
 /*
- * Common helper for find_next_bit() function family
+ * Common helper for find_next_bit_stub() function family
  * @FETCH: The expression that fetches and pre-processes each word of bitmap(s)
  * @MUNGE: The expression that post-processes a word containing found bit (may be empty)
  * @size: The bitmap size in bits
@@ -30,32 +30,41 @@ out:										\
 	sz;									\
 })
 
-unsigned long _find_next_bit(const unsigned long *addr, unsigned long nbits, unsigned long start)
+unsigned long _find_next_bit_stub(const unsigned long *addr, unsigned long nbits,
+				  unsigned long start)
 {
 	return FIND_NEXT_BIT(addr[idx], /* nop */, nbits, start);
 }
 
-unsigned long _find_next_zero_bit(const unsigned long *addr, unsigned long nbits,
+unsigned long _find_next_zero_bit_stub(const unsigned long *addr, unsigned long nbits,
 					 unsigned long start)
 {
 	return FIND_NEXT_BIT(~addr[idx], /* nop */, nbits, start);
 }
 
-#ifndef find_next_bit
 /*
  * Find the next set bit in a memory region.
  */
-unsigned long find_next_bit(const unsigned long *addr, unsigned long size,
-			    unsigned long offset)
-{
-	return _find_next_bit(addr, size, offset);
-}
-#endif
-
-#ifndef find_next_zero_bit
-unsigned long find_next_zero_bit(const unsigned long *addr, unsigned long size,
+unsigned long find_next_bit_stub(const unsigned long *addr, unsigned long size,
 				 unsigned long offset)
 {
-	return _find_next_bit(addr, size, offset);
+	return _find_next_bit_stub(addr, size, offset);
 }
-#endif
+
+unsigned long find_next_zero_bit_stub(const unsigned long *addr, unsigned long size,
+				      unsigned long offset)
+{
+	return _find_next_zero_bit_stub(addr, size, offset);
+}
+
+/*
+ * As find.h has include extern _find_next_bit declaration, while there's not
+ * implement of _find_next_bit in compressed vmlinux, we implment a dummy one which
+ * won't be used anywhere to avoid build issue
+ */
+unsigned long _find_next_bit(const unsigned long *addr1,
+			     const unsigned long *addr2, unsigned long nbits,
+			     unsigned long start, unsigned long invert, unsigned long le)
+{
+	return 0;
+}
