ceb1f6fd4dd3c5a562be90d8cfd47e2770dbc2c7
Wen Gu <guwen@linux.alibaba.com>
2021-05-18 17:23:59 +0800

    ck: net/smc: Introduce sysctl tcp2smc
    to #34356110
    
    This patch adds sysctl 'tcp2smc' to provide a switch for replacing
    TCP to SMC-R when new sockets are created in a specific net namespace.
    
    Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -2538,6 +2538,7 @@ static __net_init int smc_net_init(struct net *net)
 			init_net.smc.sysctl_rmem_default;
 		net->smc.sysctl_rmem_default =
 			init_net.smc.sysctl_rmem_default;
+		net->smc.sysctl_tcp2smc = 0;
 	}
 
 	return smc_pnet_net_init(net);
@@ -2545,6 +2546,7 @@ static __net_init int smc_net_init(struct net *net)
 
 static void __net_exit smc_net_exit(struct net *net)
 {
+	net->smc.sysctl_tcp2smc = 0;
 	smc_pnet_net_exit(net);
 }
 
@@ -2654,6 +2656,7 @@ static int __init smc_init(void)
 
 	init_net.smc.sysctl_wmem_default = 256 * 1024;
 	init_net.smc.sysctl_rmem_default = 384 * 1024;
+	init_net.smc.sysctl_tcp2smc = 0;
 
 #ifdef CONFIG_SYSCTL
 	smc_sysctl_init();
