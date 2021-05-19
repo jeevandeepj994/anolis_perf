1c698a2facf671e15183cca3a557874a87f18ffd
Wang Yinfeng <wangyinfeng@phytium.com.cn>
2021-05-19 16:54:18 +0800

    anolis: PHYTIUM: Add cputypes and machine support
    ANBZ: #1982
    
    Add cputypes and machine support for FT_2000PLUS, FT_2004 and S2500.
    
    Backport notes:
    - Backported from PHYTIUM company.
    - Add machine_type.h to ARM's include path to avoid compilation errors.
    
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/666

---
+++ arch/arm/include/asm/machine_types.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *  Authors: Wang Yinfeng <wangyinfenng@phytium.com.cn>
+ *
+ *  Copyright (C) 2021, PHYTIUM Information Technology Co., Ltd.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+#ifndef _MACHINE_TYPE_H_
+#define _MACHINE_TYPE_H_
+
+#define typeof_ft1500a()	0
+#define typeof_ft2000ahk()	0
+#define typeof_ft2000plus()	0
+#define typeof_ft2004()		0
+#define typeof_s2500()		0
+
+#endif
