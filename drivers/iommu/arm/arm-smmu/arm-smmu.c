ff8d998e7cf6b2331b035a41c70d4528eeb0e974
Wang Yinfeng <wangyinfeng@phytium.com.cn>
2021-05-20 10:52:08 +0800

    anolis: PHYTIUM: smmu: one device per iommu group for VF/PF passthrough
    ANBZ: #1982
    
    One device per iommu group during VF/PF passthrough.
    
    Backport notes:
    - Backported from PHYTIUM company.
    - As arm-smmu.c uses asm/machine_types.h in ARM/ARM64's include path, add
      CONFIG_ARCH_PHYTIUM dependency for CONFIG_ARM_SMMU to avoid compiling
      arm-smmu.c for other architectures.
    
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/666

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -37,6 +37,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/ratelimit.h>
 #include <linux/slab.h>
+#include <asm/machine_types.h>
 
 #include <linux/amba/bus.h>
 #include <linux/fsl/mc.h>
@@ -1539,19 +1540,22 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 	struct iommu_group *group = NULL;
 	int i, idx;
 
-	for_each_cfg_sme(cfg, fwspec, i, idx) {
-		if (group && smmu->s2crs[idx].group &&
-		    group != smmu->s2crs[idx].group) {
-			dev_warn(smmu->dev, "unable handle %s stream conflict\n",
-				 dev_name(dev));
-			return ERR_PTR(-EINVAL);
-		}
+	if (!(typeof_ft2000plus() || typeof_s2500())) {
+		for_each_cfg_sme(cfg, fwspec, i, idx) {
+			if (group && smmu->s2crs[idx].group &&
+			    group != smmu->s2crs[idx].group) {
+				dev_warn(smmu->dev,
+					 "unable handle %s stream conflict\n",
+					 dev_name(dev));
+				return ERR_PTR(-EINVAL);
+			}
 
-		if (ft2000_iommu_hwfix) {
-			if (smmu->s2crs[idx].group)
+			if (ft2000_iommu_hwfix) {
+				if (smmu->s2crs[idx].group)
+					group = smmu->s2crs[idx].group;
+			} else {
 				group = smmu->s2crs[idx].group;
-		} else {
-			group = smmu->s2crs[idx].group;
+			}
 		}
 	}
 
