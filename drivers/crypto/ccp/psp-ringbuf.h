8e9c0e34360f9942c8fb5aced84d0eddee9a4d8c
fangbaoshun <fangbaoshun@hygon.cn>
2021-07-30 15:35:47 +0800

    anolis: crypto: ccp: Add support for dequeue status in CSV RING_BUFFER mode
    ANBZ: #8572
    
    In CSV RING_BUFFER mode, X86 will dequeue status entries written by
    PSP after the corresponding command has been handled.
    
    Signed-off-by: fangbaoshun <fangbaoshun@hygon.cn>
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Shirong Hao <shirong@linux.alibaba.com>
    Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2913

--- drivers/crypto/ccp/psp-ringbuf.h
+++ drivers/crypto/ccp/psp-ringbuf.h
@@ -29,5 +29,7 @@ int csv_queue_init(struct csv_queue *queue,
 		   void *buffer, unsigned int size, size_t esize);
 unsigned int csv_enqueue_cmd(struct csv_queue *queue,
 			     const void *buf, unsigned int len);
+unsigned int csv_dequeue_stat(struct csv_queue *queue,
+			      void *buf, unsigned int len);
 
 #endif /* __PSP_RINGBUF_H__ */
