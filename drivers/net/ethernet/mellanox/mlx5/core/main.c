8dbb89ebbc8a159b67c9725e3f963f7be504fc9b
Leon Romanovsky <leonro@nvidia.com>
2021-08-02 16:39:54 +0300

    net/mlx5: Don't skip subfunction cleanup in case of error in module init
    ANBZ: #5737
    
    commit c633e799641cf13960bd83189b4d5b1b2adb0d4e upstream.
    
    Clean SF resources if mlx5 eth failed to initialize.
    
    Fixes: 1958fc2f0712 ("net/mlx5: SF, Add auxiliary device driver")
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Reviewed-by: Parav Pandit <parav@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    Signed-off-by: Dust Li <dust.li@linux.alibaba.com>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1735,12 +1735,14 @@ static int __init init(void)
 	if (err)
 		goto err_sf;
 
-#ifdef CONFIG_MLX5_CORE_EN
-	mlx5e_init();
-#endif
+	err = mlx5e_init();
+	if (err)
+		goto err_en;
 
 	return 0;
 
+err_en:
+	mlx5_sf_driver_unregister();
 err_sf:
 	pci_unregister_driver(&mlx5_core_driver);
 err_debug:
@@ -1750,9 +1752,7 @@ err_debug:
 
 static void __exit cleanup(void)
 {
-#ifdef CONFIG_MLX5_CORE_EN
 	mlx5e_cleanup();
-#endif
 	mlx5_sf_driver_unregister();
 	pci_unregister_driver(&mlx5_core_driver);
 	mlx5_unregister_debugfs();
