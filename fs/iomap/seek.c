ff0e55543a4b282166e1d73b3a49808480dfa018
Christoph Hellwig <hch@lst.de>
2021-08-10 18:33:11 -0700

    iomap: switch iomap_seek_hole to use iomap_iter
    ANBZ: #7740
    
    commit 40670d18e878160a170ba135c5d077471d7a9998 upstream.
    
    Rewrite iomap_seek_hole to use iomap_iter.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Conflicts:
    	fs/iomap/seek.c
    [ Gao Xiang: use the old page_cache_seek_hole_data() instead. ]
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fs/iomap/seek.c
+++ fs/iomap/seek.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2017 Red Hat, Inc.
- * Copyright (c) 2018 Christoph Hellwig.
+ * Copyright (c) 2018-2021 Christoph Hellwig.
  */
 #include <linux/module.h>
 #include <linux/compiler.h>
@@ -116,20 +116,21 @@ out:
 	return lastoff;
 }
 
-
-static loff_t
-iomap_seek_hole_actor(struct inode *inode, loff_t offset, loff_t length,
-		      void *data, struct iomap *iomap, struct iomap *srcmap)
+static loff_t iomap_seek_hole_iter(const struct iomap_iter *iter,
+		loff_t *hole_pos)
 {
-	switch (iomap->type) {
+	loff_t length = iomap_length(iter);
+	loff_t offset = iter->pos;
+
+	switch (iter->iomap.type) {
 	case IOMAP_UNWRITTEN:
-		offset = page_cache_seek_hole_data(inode, offset, length,
-						   SEEK_HOLE);
+		offset = page_cache_seek_hole_data(iter->inode,
+			iter->pos, length, SEEK_HOLE);
 		if (offset < 0)
 			return length;
 		fallthrough;
 	case IOMAP_HOLE:
-		*(loff_t *)data = offset;
+		*hole_pos = offset;
 		return 0;
 	default:
 		return length;
@@ -137,26 +138,28 @@ iomap_seek_hole_actor(struct inode *inode, loff_t offset, loff_t length,
 }
 
 loff_t
-iomap_seek_hole(struct inode *inode, loff_t offset, const struct iomap_ops *ops)
+iomap_seek_hole(struct inode *inode, loff_t pos, const struct iomap_ops *ops)
 {
 	loff_t size = i_size_read(inode);
-	loff_t ret;
+	struct iomap_iter iter = {
+		.inode	= inode,
+		.pos	= pos,
+		.flags	= IOMAP_REPORT,
+	};
+	int ret;
 
 	/* Nothing to be found before or beyond the end of the file. */
-	if (offset < 0 || offset >= size)
+	if (pos < 0 || pos >= size)
 		return -ENXIO;
 
-	while (offset < size) {
-		ret = iomap_apply(inode, offset, size - offset, IOMAP_REPORT,
-				  ops, &offset, iomap_seek_hole_actor);
-		if (ret < 0)
-			return ret;
-		if (ret == 0)
-			break;
-		offset += ret;
-	}
-
-	return offset;
+	iter.len = size - pos;
+	while ((ret = iomap_iter(&iter, ops)) > 0)
+		iter.processed = iomap_seek_hole_iter(&iter, &pos);
+	if (ret < 0)
+		return ret;
+	if (iter.len) /* found hole before EOF */
+		return pos;
+	return size;
 }
 EXPORT_SYMBOL_GPL(iomap_seek_hole);
 
