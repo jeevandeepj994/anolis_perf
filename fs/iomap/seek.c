46bf1467a76307155b07554c658e3a426e818b25
Christoph Hellwig <hch@lst.de>
2021-08-10 18:33:12 -0700

    iomap: switch iomap_seek_data to use iomap_iter
    ANBZ: #7740
    
    commit c4740bf1edad559c10b1d33c72e885b920bf6029 upstream.
    
    Rewrite iomap_seek_data to use iomap_iter.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Conflicts:
    	fs/iomap/seek.c
    [ Gao Xiang: use the old page_cache_seek_hole_data() instead. ]
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fs/iomap/seek.c
+++ fs/iomap/seek.c
@@ -163,45 +163,49 @@ iomap_seek_hole(struct inode *inode, loff_t pos, const struct iomap_ops *ops)
 }
 EXPORT_SYMBOL_GPL(iomap_seek_hole);
 
-static loff_t
-iomap_seek_data_actor(struct inode *inode, loff_t offset, loff_t length,
-		      void *data, struct iomap *iomap, struct iomap *srcmap)
+static loff_t iomap_seek_data_iter(const struct iomap_iter *iter,
+		loff_t *hole_pos)
 {
-	switch (iomap->type) {
+	loff_t length = iomap_length(iter);
+	loff_t offset = iter->pos;
+
+	switch (iter->iomap.type) {
 	case IOMAP_HOLE:
 		return length;
 	case IOMAP_UNWRITTEN:
-		offset = page_cache_seek_hole_data(inode, offset, length,
+		offset = page_cache_seek_hole_data(iter->inode, offset, length,
 						   SEEK_DATA);
 		if (offset < 0)
 			return length;
 		fallthrough;
 	default:
-		*(loff_t *)data = offset;
+		*hole_pos = offset;
 		return 0;
 	}
 }
 
 loff_t
-iomap_seek_data(struct inode *inode, loff_t offset, const struct iomap_ops *ops)
+iomap_seek_data(struct inode *inode, loff_t pos, const struct iomap_ops *ops)
 {
 	loff_t size = i_size_read(inode);
-	loff_t ret;
+	struct iomap_iter iter = {
+		.inode	= inode,
+		.pos	= pos,
+		.flags	= IOMAP_REPORT,
+	};
+	int ret;
 
 	/* Nothing to be found before or beyond the end of the file. */
-	if (offset < 0 || offset >= size)
+	if (pos < 0 || pos >= size)
 		return -ENXIO;
 
-	while (offset < size) {
-		ret = iomap_apply(inode, offset, size - offset, IOMAP_REPORT,
-				  ops, &offset, iomap_seek_data_actor);
-		if (ret < 0)
-			return ret;
-		if (ret == 0)
-			return offset;
-		offset += ret;
-	}
-
+	iter.len = size - pos;
+	while ((ret = iomap_iter(&iter, ops)) > 0)
+		iter.processed = iomap_seek_data_iter(&iter, &pos);
+	if (ret < 0)
+		return ret;
+	if (iter.len) /* found data before EOF */
+		return pos;
 	/* We've reached the end of the file without finding data */
 	return -ENXIO;
 }
