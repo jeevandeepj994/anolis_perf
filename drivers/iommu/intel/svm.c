12a2b8d9164cf06e33c9d638a0bfce7619a02cc2
Lu Baolu <baolu.lu@linux.intel.com>
2021-08-18 21:48:49 +0800

    iommu/vt-d: Allow devices to have more than 32 outstanding PRs
    ANBZ: #1672
    
    commit 48811c44349ffbb778d3e36b53beb03ad43a979c upstream
    
    The minimum per-IOMMU PRQ queue size is one 4K page, this is more entries
    than the hardcoded limit of 32 in the current VT-d code. Some devices can
    support up to 512 outstanding PRQs but underutilized by this limit of 32.
    Although, 32 gives some rough fairness when multiple devices share the same
    IOMMU PRQ queue, but far from optimal for customized use case. This extends
    the per-IOMMU PRQ queue size to four 4K pages and let the devices have as
    many outstanding page requests as they can.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Link: https://lore.kernel.org/r/20210720013856.4143880-1-baolu.lu@linux.intel.com
    Link: https://lore.kernel.org/r/20210818134852.1847070-7-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Artie Ding <artie.ding@linux.alibaba.com>

--- drivers/iommu/intel/svm.c
+++ drivers/iommu/intel/svm.c
@@ -31,8 +31,6 @@ static irqreturn_t prq_event_thread(int irq, void *d);
 static void intel_svm_drain_prq(struct device *dev, u32 pasid);
 #define to_intel_svm_dev(handle) container_of(handle, struct intel_svm_dev, sva)
 
-#define PRQ_ORDER 0
-
 static struct intel_svm_dev *
 svm_lookup_device_by_sid(struct intel_svm *svm, u16 sid)
 {
@@ -874,8 +872,6 @@ struct page_req_dsc {
 	};
 	u64 priv_data[2];
 };
-
-#define PRQ_RING_MASK	((0x1000 << PRQ_ORDER) - 0x20)
 
 static bool is_canonical_address(u64 addr)
 {
