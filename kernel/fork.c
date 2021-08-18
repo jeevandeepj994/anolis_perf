5f42838cd0d1bfd9ba1118848368a657a20c279f
Su Lifan <su-lifan@linux.alibaba.com>
2021-08-18 14:34:02 +0800

    ck: Revert "ck: kernel: Reduce tasklist_lock contention at fork and exit"
    to #36183774
    
    This reverts commit cd02b634b50e2de0dbaf84578e4f3c9c753d857e.
    
    On latest kernel or on baremetal machine, the commit cuases performance
    degration when processes are frequently created and destroyed.
    It caused 40% and 50% regression on unixbench spawn with all cpus on
    a dual Cascade Lake baremetal with 104c and an Ampere virtual machine
    with 64c, respectively.
    
    Revert the commit for general usage since the scenario addressed by the
    commit is rare.
    
    Signed-off-by: Su Lifan <su-lifan@linux.alibaba.com>
    Reviewed-by: James Wang <jnwnag@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1867,7 +1867,6 @@ static __latent_entropy struct task_struct *copy_process(
 	struct file *pidfile = NULL;
 	u64 clone_flags = args->flags;
 	struct nsproxy *nsp = current->nsproxy;
-	unsigned long flags;
 
 	/*
 	 * Don't allow sharing the root directory with processes in a different
@@ -2224,11 +2223,7 @@ static __latent_entropy struct task_struct *copy_process(
 	 * Make it visible to the rest of the system, but dont wake it up yet.
 	 * Need tasklist lock for parent etc handling!
 	 */
-retry:
-	if (!write_trylock_irqsave(&tasklist_lock, flags)) {
-		cond_resched();
-		goto retry;
-	}
+	write_lock_irq(&tasklist_lock);
 
 	/* CLONE_PARENT re-uses the old parent */
 	if (clone_flags & (CLONE_PARENT|CLONE_THREAD)) {
