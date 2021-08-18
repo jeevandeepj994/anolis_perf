79d3aaefc85ec7550d7466d31f970d015c100dc3
Gao Xiang <hsiangkao@linux.alibaba.com>
2021-08-18 23:22:31 +0800

    erofs: add support for the full decompressed length
    ANBZ: #8559
    
    commit d95ae5e25326092d61613acf98280270dde22778 upstream.
    
    Previously, there is no need to get the full decompressed length since
    EROFS supports partial decompression. However for some other cases
    such as fiemap, the full decompressed length is necessary for iomap to
    make it work properly.
    
    This patch adds a way to get the full decompressed length. Note that
    it takes more metadata overhead and it'd be avoided if possible in the
    performance sensitive scenario.
    
    Link: https://lore.kernel.org/r/20210818152231.243691-1-hsiangkao@linux.alibaba.com
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Acked-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2903

--- fs/erofs/internal.h
+++ fs/erofs/internal.h
@@ -418,6 +418,12 @@ struct erofs_map_blocks {
 
 int erofs_map_blocks(struct inode *inode, struct erofs_map_blocks *map);
 
+/*
+ * Used to get the exact decompressed length, e.g. fiemap (consider lookback
+ * approach instead if possible since it's more metadata lightweight.)
+ */
+#define EROFS_GET_BLOCKS_FIEMAP	0x0001
+
 /* zmap.c */
 #ifdef CONFIG_EROFS_FS_ZIP
 int z_erofs_fill_inode(struct inode *inode);
