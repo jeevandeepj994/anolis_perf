1fdc38452a18ac83e0e5b7c7ba4eb797505459a9
Tony Lu <tonylu@linux.alibaba.com>
2021-09-22 11:17:18 +0800

    anolis: net/smc: allow different subnet communication
    ANBZ: #51
    
    SMC checks prefix to ensure that peers are in the same subnet. But it is
    no need to check this for iWARP over ERDMA, for ERDMA can communicate
    each others beyound subnet. So we provide a sysctl knob
    allow_different_subnet to support it.
    
    Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
    Acked-by: Dust Li <dust.li@linux.alibaba.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -1878,6 +1878,7 @@ static int smc_listen_find_device(struct smc_sock *new_smc,
 				  struct smc_clc_msg_proposal *pclc,
 				  struct smc_init_info *ini)
 {
+	struct net *net = sock_net(&new_smc->sk);
 	int prfx_rc;
 
 	/* check for ISM device matching V2 proposed device */
@@ -1885,10 +1886,12 @@ static int smc_listen_find_device(struct smc_sock *new_smc,
 	if (ini->ism_dev[0])
 		return 0;
 
-	/* check for matching IP prefix and subnet length (V1) */
-	prfx_rc = smc_listen_prfx_check(new_smc, pclc);
-	if (prfx_rc)
-		smc_find_ism_store_rc(prfx_rc, ini);
+	if (!net->smc.sysctl_allow_different_subnet) {
+		/* check for matching IP prefix and subnet length (V1) */
+		prfx_rc = smc_listen_prfx_check(new_smc, pclc);
+		if (prfx_rc)
+			smc_find_ism_store_rc(prfx_rc, ini);
+	}
 
 	/* get vlan id from IP device */
 	if (smc_vlan_by_tcpsk(new_smc->clcsock, ini))
@@ -2745,6 +2748,7 @@ static __net_init int smc_net_init(struct net *net)
 			init_net.smc.sysctl_rmem_default;
 		net->smc.sysctl_tcp2smc = 0;
 		net->smc.sysctl_autocorking = 1;
+		net->smc.sysctl_allow_different_subnet = 0;
 	}
 
 	return smc_pnet_net_init(net);
@@ -2753,6 +2757,7 @@ static __net_init int smc_net_init(struct net *net)
 static void __net_exit smc_net_exit(struct net *net)
 {
 	net->smc.sysctl_tcp2smc = 0;
+	net->smc.sysctl_allow_different_subnet = 0;
 	smc_pnet_net_exit(net);
 }
 
@@ -2879,6 +2884,7 @@ static int __init smc_init(void)
 	init_net.smc.sysctl_rmem_default = 384 * 1024;
 	init_net.smc.sysctl_tcp2smc = 0;
 	init_net.smc.sysctl_autocorking = 1;
+	init_net.smc.sysctl_allow_different_subnet = 0;
 
 #ifdef CONFIG_SYSCTL
 	smc_sysctl_init();
