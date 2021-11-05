956e9d734d78faf0fe71b044bb6cbf02ad25d0de
Yang Shi <shy828301@gmail.com>
2021-11-05 13:43:35 -0700

    mm: migrate: make demotion knob depend on migration
    ANBZ: #80
    
    commit 20f9ba4f995247bb79e243741b8fdddbd76dd923 upstream
    
    The memory demotion needs to call migrate_pages() to do the jobs.  And
    it is controlled by a knob, however, the knob doesn't depend on
    CONFIG_MIGRATION.  The knob could be truned on even though MIGRATION is
    disabled, this will not cause any crash since migrate_pages() would just
    return -ENOSYS.  But it is definitely not optimal to go through demotion
    path then retry regular swap every time.
    
    And it doesn't make too much sense to have the knob visible to the users
    when !MIGRATION.  Move the related code from mempolicy.[h|c] to
    migrate.[h|c].
    
    Link: https://lkml.kernel.org/r/20211015005559.246709-1-shy828301@gmail.com
    Signed-off-by: Yang Shi <shy828301@gmail.com>
    Acked-by: "Huang, Ying" <ying.huang@intel.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Zhong Jiang <zhongjiang-ali@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/725
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- include/linux/mempolicy.h
+++ include/linux/mempolicy.h
@@ -205,8 +205,6 @@ static inline bool vma_migratable(struct vm_area_struct *vma)
 extern int mpol_misplaced(struct page *, struct vm_area_struct *, unsigned long);
 extern void mpol_put_task_policy(struct task_struct *);
 
-extern bool numa_demotion_enabled;
-
 #else
 
 struct mempolicy {};
@@ -311,6 +309,5 @@ static inline void mpol_put_task_policy(struct task_struct *task)
 {
 }
 
-#define numa_demotion_enabled  false
 #endif /* CONFIG_NUMA */
 #endif
