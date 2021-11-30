bfbad641b699762b76be099180bff6f807fb241f
Joanne Koong <joannekoong@fb.com>
2021-11-29 19:06:19 -0800

    bpf: Add bpf_loop helper
    ANBZ: #7670
    
    commit e6f2dd0f80674e9d5960337b3e9c2a242441b326 upstream.
    
    This patch adds the kernel-side and API changes for a new helper
    function, bpf_loop:
    
    long bpf_loop(u32 nr_loops, void *callback_fn, void *callback_ctx,
    u64 flags);
    
    where long (*callback_fn)(u32 index, void *ctx);
    
    bpf_loop invokes the "callback_fn" **nr_loops** times or until the
    callback_fn returns 1. The callback_fn can only return 0 or 1, and
    this is enforced by the verifier. The callback_fn index is zero-indexed.
    
    A few things to please note:
    ~ The "u64 flags" parameter is currently unused but is included in
    case a future use case for it arises.
    ~ In the kernel-side implementation of bpf_loop (kernel/bpf/bpf_iter.c),
    bpf_callback_t is used as the callback function cast.
    ~ A program can have nested bpf_loop calls but the program must
    still adhere to the verifier constraint of its stack depth (the stack depth
    cannot exceed MAX_BPF_STACK))
    ~ Recursive callback_fns do not pass the verifier, due to the call stack
    for these being too deep.
    ~ The next patch will include the tests and benchmark
    
    Signed-off-by: Joanne Koong <joannekoong@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20211130030622.4131246-2-joannekoong@fb.com
    [dtcccc: add missing types to bpf_helpers_doc.py to fix the build issue.
    b00628b1c7d5 ("bpf: Introduce bpf timers.") appended bpf_base_func_proto
    to bpf_tracing_func_proto, so that bpf tracing progs can use all base
    helpers now. So there is no need to add BPF_FUNC_xxx twice. Apply this
    change in this patch.]
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Qiao Ma <mqaio@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2528

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -2025,6 +2025,7 @@ extern const struct bpf_func_proto bpf_per_cpu_ptr_proto;
 extern const struct bpf_func_proto bpf_this_cpu_ptr_proto;
 extern const struct bpf_func_proto bpf_ktime_get_coarse_ns_proto;
 extern const struct bpf_func_proto bpf_for_each_map_elem_proto;
+extern const struct bpf_func_proto bpf_loop_proto;
 
 const struct bpf_func_proto *bpf_tracing_func_proto(
 	enum bpf_func_id func_id, const struct bpf_prog *prog);
