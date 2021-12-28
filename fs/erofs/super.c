11b22efa9ce6ee58a20b1735262255631d9b0085
Yue Hu <huyue2@yulong.com>
2021-12-28 13:46:04 +0800

    erofs: add on-disk compressed tail-packing inline support
    ANBZ: #8559
    
    commit ab92184ff8f12979f3d3dd5ed601ed85770d81ba upstream.
    
    Introduces erofs compressed tail-packing inline support.
    
    This approach adds a new field called `h_idata_size' in the
    per-file compression header to indicate the encoded size of
    each tail-packing pcluster.
    
    At runtime, it will find the start logical offset of the tail
    pcluster when initializing per-inode zmap and record such
    extent (headlcn, idataoff) information to the in-memory inode.
    Therefore, follow-on requests can directly recognize if one
    pcluster is a tail-packing inline pcluster or not.
    
    Link: https://lore.kernel.org/r/20211228054604.114518-6-hsiangkao@linux.alibaba.com
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Yue Hu <huyue2@yulong.com>
    Conflicts:
    	fs/erofs/internal.h
    	fs/erofs/super.c
    	fs/erofs/zmap.c
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -427,6 +427,9 @@ static int erofs_read_superblock(struct super_block *sb)
 
 	/* handle multiple devices */
 	ret = erofs_scan_devices(sb, dsb);
+
+	if (erofs_sb_has_ztailpacking(sbi))
+		erofs_info(sb, "EXPERIMENTAL compressed inline data feature in use. Use at your own risk!");
 out:
 	erofs_put_metabuf(&buf);
 	return ret;
