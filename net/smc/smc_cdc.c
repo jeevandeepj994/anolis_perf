ee01395d38fd20ad57f37f301603d195f41cdca8
Tony Lu <tonylu@linux.alibaba.com>
2022-01-07 20:01:07 +0800

    anolis: Revert "anolis: net/smc: support auto-cork with nagle algorithm"
    ANBZ: #264
    
    This reverts commit bc6e499af391de8119f2d71c27272b1dfe410cbf for
    backporting upstream patchs, and avoids code conflicts.
    
    [ Shile: fixes conflict with e8a5988a85c71 ]
    
    Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
    Acked-by: Dust Li <dust.li@linux.alibaba.com>
    Signed-off-by: Shile Zhang <shile.zhang@linux.alibaba.com>

--- net/smc/smc_cdc.c
+++ net/smc/smc_cdc.c
@@ -53,13 +53,6 @@ static void smc_cdc_tx_handler(struct smc_wr_tx_pend_priv *pnd_snd,
 		wake_up(&conn->cdc_pend_tx_wq);
 	WARN_ON(atomic_read(&conn->cdc_pend_tx_wr) < 0);
 
-	/* If this is the last pending WR complete, push them to prevent
-	 * no one trying to push when corked.
-	 */
-	if (likely(!cdcpend->validation) &&
-	    atomic_dec_and_test(&conn->cdc_pend_tx_wr))
-		smc_tx_sndbuf_nonempty(conn);
-
 	smc_tx_sndbuf_nonfull(smc);
 	bh_unlock_sock(&smc->sk);
 }
@@ -115,22 +108,18 @@ int smc_cdc_msg_send(struct smc_connection *conn,
 
 	conn->tx_cdc_seq++;
 	conn->local_tx_ctrl.seqno = conn->tx_cdc_seq;
-	atomic_inc(&conn->cdc_pend_tx_wr);
-	smp_mb__after_atomic(); /* Make sure cdc_pend_tx_wr added before post */
-
 	smc_host_msg_to_cdc((struct smc_cdc_msg *)wr_buf, conn, &cfed);
 
 	atomic_inc(&conn->cdc_pend_tx_wr);
 	smp_mb__after_atomic(); /* Make sure cdc_pend_tx_wr added before post */
 
 	rc = smc_wr_tx_send(link, (struct smc_wr_tx_pend_priv *)pend);
-	if (likely(!rc)) {
+	if (!rc) {
 		smc_curs_copy(&conn->rx_curs_confirmed, &cfed, conn);
 		conn->local_rx_ctrl.prod_flags.cons_curs_upd_req = 0;
 	} else {
 		conn->tx_cdc_seq--;
 		conn->local_tx_ctrl.seqno = conn->tx_cdc_seq;
-		atomic_dec(&conn->cdc_pend_tx_wr);
 	}
 
 	return rc;
@@ -152,7 +141,6 @@ int smcr_cdc_msg_send_validation(struct smc_connection *conn,
 	peer->seqno = htons(conn->tx_cdc_seq_fin); /* seqno last compl. tx */
 	peer->token = htonl(local->token);
 	peer->prod_flags.failover_validation = 1;
-	pend->validation = 1;
 
 	/* We need to set pend->conn here to make sure smc_cdc_tx_handler()
 	 * can handle properly
