jeevandeepj994/anolis_perf

commit 4fdf20bd83e59f82a8c7af9f339da36b22805216

anolis: kprobes: Check function patched by module without kprobe
ANBZ: #54

kprobe neglects the fact that some modules will patch some functions
directly with jmp instruction, not with kprobe.

In this case, kprobe_register succeeds without any error, while the
sequential kprobe_enable, kprobe_disable, kprobe_unregister all fail.
enable/disable fail because functions header was overwritten, and
unregister fails because kprobe_enable calls ftrace_kill.

The worst thing is, kprobe_unregister doesn't rollback but just aborts,
leaving corrupted linked list in kprobe_table, and may panic the kernel.

We should check them and backoff. Because the module either knows what
they're doing or they're malicious. In either case, we don't want two
different mechanisms to work together.

Fixes: 297f9233b53a0 ("kprobes: Propagate error from disarm_kprobe_ftrace()")
Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>
Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>

kernel/kprobes.c
@@ -1628,6 +1628,40 @@ int __weak arch_check_ftrace_location(struct kprobe *p)
 	return 0;
 }
 
+#ifdef CONFIG_FUNCTION_TRACER
+/*
+ * Check if some module bypassed kprobe, and directly patched this address.
+ *
+ * We'd better just reject kprobe_register. We don't want to mess around with
+ * these modules. They either know what they're doing and don't want kprobe
+ * to get invovled, or they're malicious and we should sense that and remove
+ * them. In the latter case, the warning senses it. And in both cases, we
+ * avoid them working together.
+ */
+static bool module_patched(unsigned long addr)
+{
+	unsigned char replaced[MCOUNT_INSN_SIZE];
+	unsigned long target_addr;
+
+	if (copy_from_kernel_nofault(replaced, (void *)addr, MCOUNT_INSN_SIZE))
+		return false;
+
+	if (replaced[0] != 0xe9)
+		return false;
+
+	target_addr = (*(int *)&replaced[1]) + addr + MCOUNT_INSN_SIZE;
+
+	if (is_module_address(target_addr)) {
+		WARN_ONCE(1, "0x%lx has been patched by a module. Don't probe it.\n", addr);
+		return true;
+	}
+
+	return false;
+}
+#else
+static inline bool module_patched(unsigned long addr) { return false; }
+#endif
+
 static int check_kprobe_address_safe(struct kprobe *p,
 				     struct module **probed_mod)
 {
@@ -1644,7 +1678,8 @@ static int check_kprobe_address_safe(struct kprobe *p,
 	    within_kprobe_blacklist((unsigned long) p->addr) ||
 	    jump_label_text_reserved(p->addr, p->addr) ||
 	    static_call_text_reserved(p->addr, p->addr) ||
-	    find_bug((unsigned long)p->addr)) {
+	    find_bug((unsigned long)p->addr) ||
+	    module_patched((unsigned long)p->addr)) {
 		ret = -EINVAL;
 		goto out;
 	}

commit ba987daf8169710daeecc47250b74f51bd295128

anolis: kprobes: fix ftrace conflict check for aarch64
ANBZ: #367

module_patched was not implemented for architectures other than x86_64.
But other architectures ran the x86_64 version by mistake.

This patch implements it for aarch64 too and disables it for other
architectures.

Another two changes,
   reuse x86's text_poke_insn to improve code
   rename "module" to "foreign" (to ftrace) because "module" is too narrow

Fixes: 4fdf20bd83e5 ("anolis: kprobes: Check function patched by module without kprobe")
Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>
Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>

arch/arm64/kernel/ftrace.c
@@ -303,3 +303,16 @@ int ftrace_disable_ftrace_graph_caller(void)
 }
 #endif /* CONFIG_DYNAMIC_FTRACE */
 #endif /* CONFIG_FUNCTION_GRAPH_TRACER */
+
+bool arch_foreign_patched(unsigned long addr)
+{
+	u32 replaced;
+
+	if (aarch64_insn_read((void *)addr, &replaced))
+		return false;
+	if (!aarch64_insn_is_branch_imm(replaced))
+		return false;
+	if (!is_module_address(addr + aarch64_get_branch_offset(replaced)))
+		return false;
+	return true;
+}

arch/x86/kernel/ftrace.c
@@ -684,3 +684,17 @@ void prepare_ftrace_return(unsigned long self_addr, unsigned long *parent,
 		*parent = old;
 }
 #endif /* CONFIG_FUNCTION_GRAPH_TRACER */
+
+bool arch_foreign_patched(unsigned long addr)
+{
+	union text_poke_insn replaced;
+
+	BUILD_BUG_ON(POKE_MAX_OPCODE_SIZE != MCOUNT_INSN_SIZE);
+	if (copy_from_kernel_nofault(&replaced.text, (void *)addr, MCOUNT_INSN_SIZE))
+		return false;
+	if (replaced.opcode != 0xe9)
+		return false;
+	if (!is_module_address(replaced.disp + addr + MCOUNT_INSN_SIZE))
+		return false;
+	return true;
+}

include/linux/ftrace.h
@@ -58,12 +58,18 @@ struct ftrace_direct_func;
 const char *
 ftrace_mod_address_lookup(unsigned long addr, unsigned long *size,
 		   unsigned long *off, char **modname, char *sym);
+bool foreign_patched(unsigned long addr);
 #else
 static inline const char *
 ftrace_mod_address_lookup(unsigned long addr, unsigned long *size,
 		   unsigned long *off, char **modname, char *sym)
 {
 	return NULL;
+}
+
+static inline bool foreign_patched(unsigned long addr)
+{
+	return false;
 }
 #endif
 

kernel/kprobes.c
@@ -1628,40 +1628,6 @@ int __weak arch_check_ftrace_location(struct kprobe *p)
 	return 0;
 }
 
-#ifdef CONFIG_FUNCTION_TRACER
-/*
- * Check if some module bypassed kprobe, and directly patched this address.
- *
- * We'd better just reject kprobe_register. We don't want to mess around with
- * these modules. They either know what they're doing and don't want kprobe
- * to get invovled, or they're malicious and we should sense that and remove
- * them. In the latter case, the warning senses it. And in both cases, we
- * avoid them working together.
- */
-static bool module_patched(unsigned long addr)
-{
-	unsigned char replaced[MCOUNT_INSN_SIZE];
-	unsigned long target_addr;
-
-	if (copy_from_kernel_nofault(replaced, (void *)addr, MCOUNT_INSN_SIZE))
-		return false;
-
-	if (replaced[0] != 0xe9)
-		return false;
-
-	target_addr = (*(int *)&replaced[1]) + addr + MCOUNT_INSN_SIZE;
-
-	if (is_module_address(target_addr)) {
-		WARN_ONCE(1, "0x%lx has been patched by a module. Don't probe it.\n", addr);
-		return true;
-	}
-
-	return false;
-}
-#else
-static inline bool module_patched(unsigned long addr) { return false; }
-#endif
-
 static int check_kprobe_address_safe(struct kprobe *p,
 				     struct module **probed_mod)
 {
@@ -1679,7 +1645,7 @@ static int check_kprobe_address_safe(struct kprobe *p,
 	    jump_label_text_reserved(p->addr, p->addr) ||
 	    static_call_text_reserved(p->addr, p->addr) ||
 	    find_bug((unsigned long)p->addr) ||
-	    module_patched((unsigned long)p->addr)) {
+	    foreign_patched((unsigned long)p->addr)) {
 		ret = -EINVAL;
 		goto out;
 	}

kernel/trace/ftrace.c
@@ -7640,3 +7640,27 @@ ftrace_enable_sysctl(struct ctl_table *table, int write,
 	mutex_unlock(&ftrace_lock);
 	return ret;
 }
+
+bool __weak arch_foreign_patched(unsigned long addr)
+{
+	return false;
+}
+
+/*
+ * Check if some foreign mechanism directly patched this address.
+ *
+ * We'd better just reject kprobe_register. We don't want ftrace to mess
+ * with what's foreign to it. Foreign mechanisms either know what they're
+ * doing and don't want ftrace to get invovled, or they're malicious and
+ * we should sense that and remove them. In the latter case, the warning
+ * senses it. And in both cases, we avoid them working together.
+ */
+bool foreign_patched(unsigned long addr)
+{
+	if (arch_foreign_patched(addr)) {
+		WARN_ONCE(1, "0x%lx patched by non ftrace mechanisms. Don't probe it.\n", addr);
+		return true;
+	}
+
+	return false;
+}