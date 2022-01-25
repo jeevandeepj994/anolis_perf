e56305739ff5561a28455cc33f38cfcff8dc165e
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2022-01-25 08:07:35 -0500

    x86/tdx: Extend the cc_platform_has() API to support TDX guests
    ANBZ: #523
    
    commit 40e556e5081735280496a5a6c0e47a7a7d1d8b72 intel-github.
    
    Confidential Computing (CC) features (like string I/O unroll support,
    memory encryption/decryption support, etc) are conditionally enabled
    in the kernel using cc_platform_has() API. Since TDX guests also need
    to use these CC features, extend cc_platform_has() API and add TDX
    guest-specific CC attributes support.
    
    Use is_tdx_guest() API to detect for the TDX guest status and return
    TDX-specific CC attributes. To enable use of CC APIs in the TDX guest,
    select ARCH_HAS_CC_PLATFORM in the CONFIG_INTEL_TDX_GUEST case.
    
    This is a preparatory patch and just creates the framework for adding
    TDX guest specific CC attributes.
    
    Since is_tdx_guest() function (through cc_platform_has() API) is used in
    the early boot code, disable the instrumentation flags and function
    tracer. This is similar to AMD SEV and cc_platform.c.
    
    Since intel_cc_platform_has() function only gets called when
    is_tdx_guest() is true (valid CONFIG_INTEL_TDX_GUEST case), remove the
    redundant #ifdef in intel_cc_platform_has().
    
    Intel-SIG: commit 40e556e50817 x86/tdx: Extend the cc_platform_has()
    API to support TDX guests
    Add TDX guest kernel support.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    [ Fan Du: amend commit log ]
    Signed-off-by: Fan Du <fan.du@intel.com>
    Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/coco/core.c
+++ arch/x86/coco/core.c
@@ -12,6 +12,7 @@
 
 #include <asm/coco.h>
 #include <asm/processor.h>
+#include <asm/tdx.h>
 
 static enum cc_vendor vendor __ro_after_init;
 static u64 cc_mask __ro_after_init;
@@ -87,9 +88,18 @@ EXPORT_SYMBOL_GPL(cc_platform_has);
 
 u64 cc_mkenc(u64 val)
 {
+	/*
+	 * Both AMD and Intel use a bit in the page table to indicate
+	 * encryption status of the page.
+	 *
+	 * - for AMD, bit *set* means the page is encrypted
+	 * - for Intel *clear* means encrypted.
+	 */
 	switch (vendor) {
 	case CC_VENDOR_AMD:
 		return val | cc_mask;
+	case CC_VENDOR_INTEL:
+		return val & ~cc_mask;
 	default:
 		return val;
 	}
@@ -97,9 +107,12 @@ u64 cc_mkenc(u64 val)
 
 u64 cc_mkdec(u64 val)
 {
+	/* See comment in cc_mkenc() */
 	switch (vendor) {
 	case CC_VENDOR_AMD:
 		return val & ~cc_mask;
+	case CC_VENDOR_INTEL:
+		return val | cc_mask;
 	default:
 		return val;
 	}
--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -5,7 +5,11 @@
 #define pr_fmt(fmt)     "tdx: " fmt
 
 #include <linux/cpufeature.h>
+#include <asm/coco.h>
 #include <asm/tdx.h>
+
+/* TDX module Call Leaf IDs */
+#define TDX_GET_INFO			1
 
 static bool tdx_guest_detected __ro_after_init;
 
@@ -32,6 +36,44 @@ static inline u64 _tdx_hypercall(u64 fn, u64 r12, u64 r13, u64 r14,
 	return out->r10;
 }
 
+/*
+ * Used for TDX guests to make calls directly to the TD module.  This
+ * should only be used for calls that have no legitimate reason to fail
+ * or where the kernel can not survive the call failing.
+ */
+static inline void tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+				   struct tdx_module_output *out)
+{
+	if (__tdx_module_call(fn, rcx, rdx, r8, r9, out))
+		panic("TDCALL %lld failed (Buggy TDX module!)\n", fn);
+}
+
+static u64 get_cc_mask(void)
+{
+	struct tdx_module_output out;
+	unsigned int gpa_width;
+
+	/*
+	 * TDINFO TDX module call is used to get the TD execution environment
+	 * information like GPA width, number of available vcpus, debug mode
+	 * information, etc. More details about the ABI can be found in TDX
+	 * Guest-Host-Communication Interface (GHCI), section 2.4.2 TDCALL
+	 * [TDG.VP.INFO].
+	 *
+	 * The GPA width that comes out of this call is critical. TDX guests
+	 * can not meaningfully run without it.
+	 */
+	tdx_module_call(TDX_GET_INFO, 0, 0, 0, 0, &out);
+
+	gpa_width = out.rcx & GENMASK(5, 0);
+
+	/*
+	 * The highest bit of a guest physical address is the "sharing" bit.
+	 * Set it for shared pages and clear it for private pages.
+	 */
+	return BIT_ULL(gpa_width - 1);
+}
+
 bool is_tdx_guest(void)
 {
 	return tdx_guest_detected;
@@ -40,6 +82,7 @@ bool is_tdx_guest(void)
 void __init tdx_early_init(void)
 {
 	u32 eax, sig[3];
+	u64 cc_mask;
 
 	cpuid_count(TDX_CPUID_LEAF_ID, 0, &eax, &sig[0], &sig[2],  &sig[1]);
 
@@ -49,6 +92,10 @@ void __init tdx_early_init(void)
 	tdx_guest_detected = true;
 
 	setup_force_cpu_cap(X86_FEATURE_TDX_GUEST);
+
+	cc_set_vendor(CC_VENDOR_INTEL);
+	cc_mask = get_cc_mask();
+	cc_set_mask(cc_mask);
 
 	pr_info("Guest detected\n");
 }
