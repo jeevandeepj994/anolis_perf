f38727ae6f91f4f28a15f203dd4c5ebc3ecd3fff
D. Wythe <alibuda@linux.alibaba.com>
2022-02-11 18:12:25 +0800

    anolis: net/smc: Add sysctl conrtol for handshake limiation
    ANBZ: #1742
    
    see commit: net/smc: Add global configure for handshake limitation by
    netlink
    
    This patch just add sysctl contoler for anolis.
    
    Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/577
    Link: https://gitee.com/anolis/cloud-kernel/pulls/906

--- include/net/netns/smc.h
+++ include/net/netns/smc.h
@@ -12,8 +12,7 @@ struct netns_smc {
 	/* protect fback_rsn */
 	struct mutex			mutex_fback_rsn;
 	struct smc_stats_rsn		*fback_rsn;
-
-	bool				limit_smc_hs;	/* constraint on handshake */
+	int				limit_smc_hs;	/* constraint on handshake */
 #ifdef CONFIG_SYSCTL
 	struct ctl_table_header		*smc_hdr;
 #endif
