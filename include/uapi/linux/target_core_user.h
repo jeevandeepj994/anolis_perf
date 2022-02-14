8026b0c90973208a7564d0a64c1e97058e1f97ed
Guixin Liu <kanie@linux.alibaba.com>
2022-02-14 15:59:11 +0800

    anolis: scsi:target: reduce one copy by using uio ioctl
    ANBZ: #8525
    
    Currently there are two copies between sg, tcmu data area and userspace
    buffer, if the backstore holds its own userspace buffer, we can use uio
    ioctl to copy between sg and userspace buffer directly to improve
    performance.
    
    Use tcm_loop and tcmu(backstore is file) to evaluate performance,
    fio job: fio -filename=/dev/sdb  -ioengine=libaio -direct=1 -size=2G
    -name=1 -thread -runtime=60 -time_based  -rw=randread -numjobs=16
    -iodepth=16 -bs=128k
    
    Without this patch:
        READ: bw=2511MiB/s (2633MB/s), 154MiB/s-158MiB/s (162MB/s-166MB/s),
    io=147GiB (158GB), run=60006-60008msec
    
    With this patch:
        READ: bw=2965MiB/s (3110MB/s), 183MiB/s-188MiB/s (192MB/s-197MB/s),
    io=174GiB (187GB), run=60005-60007msec
    
    There is about a 20% performance improvement in this case.
    
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Signed-off-by: Guixin Liu <kanie@linux.alibaba.com>

--- include/uapi/linux/target_core_user.h
+++ include/uapi/linux/target_core_user.h
@@ -185,4 +185,14 @@ enum tcmu_genl_attr {
 };
 #define TCMU_ATTR_MAX (__TCMU_ATTR_MAX - 1)
 
+struct tcmu_data_xfer {
+	__u16 cmd_id;
+	__u16 __pad1;
+	__u32 iov_cnt;
+	struct iovec __user *iovec;
+};
+
+#define TCMU_IOCTL_CMD_COPY_TO_SGL      _IOW('T', 0xe0, struct tcmu_data_xfer)
+#define TCMU_IOCTL_CMD_COPY_FROM_SGL    _IOR('T', 0xe1, struct tcmu_data_xfer)
+
 #endif
