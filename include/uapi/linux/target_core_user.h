a54db8665461beb2584c27ea9c813391a92e0e68
Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
2022-03-15 14:40:19 +0800

    anolis: scsi: target: tcmu: Support zero copy
    ANBZ: #8525
    
    Currently in tcmu, for READ commands, it copies user space backstore's
    data buffer to tcmu internal data area, then copies data in data area
    to READ commands sgl pages. For WRITE commands, tcmu copies sgl pages
    to tcmu internal data area, then copies data in data area to user space
    backstore. For both cases, there are obvious copy overhead, which impact
    io throughput, especially for large io size.
    
    To mitigate this issue, we implement zero copy feature to tcmu, which
    map sgl pages to user space backstore's address space. Currently only
    sgl pages's offset and length are both aligned to page size, can this
    command go into tcmu zero copy path.
    
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Signed-off-by: Guixin Liu <kanie@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2876

--- include/uapi/linux/target_core_user.h
+++ include/uapi/linux/target_core_user.h
@@ -73,6 +73,7 @@ enum tcmu_opcode {
 struct tcmu_cmd_entry_hdr {
 	__u32 len_op;
 	__u16 cmd_id;
+#define TCMU_KFLAG_ZERO_COPY  0x1
 	__u8 kflags;
 #define TCMU_UFLAG_UNKNOWN_OP 0x1
 #define TCMU_UFLAG_READ_LEN   0x2
@@ -194,5 +195,12 @@ struct tcmu_data_xfer {
 
 #define TCMU_IOCTL_CMD_COPY_TO_SGL      _IOW('T', 0xe0, struct tcmu_data_xfer)
 #define TCMU_IOCTL_CMD_COPY_FROM_SGL    _IOR('T', 0xe1, struct tcmu_data_xfer)
+#define TCMU_IOCTL_CMD_ZEROCOPY		_IOW('T', 0xe2, struct tcmu_cmd_zerocopy)
+
+struct tcmu_cmd_zerocopy {
+	struct iovec __user *iov;
+	__u32 iov_cnt;
+	__u16 cmd_id;
+};
 
 #endif
