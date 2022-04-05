c63cac06a3e092e70ad6dd4f7fd4443194301482
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2022-04-06 02:29:25 +0300

    x86/boot: Port I/O: Add decompression-time support for TDX
    ANBZ: #4805
    
    commit 4c5b9aac6cade51aef64cc6ed67f2ad5acda9aed upstream.
    
    Port I/O instructions trigger #VE in the TDX environment. In response to
    the exception, kernel emulates these instructions using hypercalls.
    
    But during early boot, on the decompression stage, it is cumbersome to
    deal with #VE. It is cleaner to go to hypercalls directly, bypassing #VE
    handling.
    
    Hook up TDX-specific port I/O helpers if booting in TDX environment.
    
    Intel-SIG: commit 4c5b9aac6cad x86/boot: Port I/O: Add decompression-time
    support for TDX.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: https://lkml.kernel.org/r/20220405232939.73860-17-kirill.shutemov@linux.intel.com
    [ zhiminghufighting: amend commit log ]
    Signed-off-by: zhiminghufighting <zhiming.hu@intel.com>
    Reviewed-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1645

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -7,11 +7,6 @@
 #include <asm/ptrace.h>
 #include <asm/shared/tdx.h>
 
-#define TDX_HYPERCALL_STANDARD  0
-
-#define TDX_HCALL_HAS_OUTPUT	BIT(0)
-#define TDX_HCALL_ISSUE_STI	BIT(1)
-
 /*
  * SW-defined error codes.
  *
@@ -48,21 +43,6 @@ struct tdx_module_output {
 };
 
 /*
- * Used in __tdx_hypercall() to pass down and get back registers' values of
- * the TDCALL instruction when requesting services from the VMM.
- *
- * This is a software only structure and not part of the TDX module/VMM ABI.
- */
-struct tdx_hypercall_args {
-	u64 r10;
-	u64 r11;
-	u64 r12;
-	u64 r13;
-	u64 r14;
-	u64 r15;
-};
-
-/*
  * Used by the #VE exception handler to gather the #VE exception
  * info from the TDX module. This is a software only structure
  * and not part of the TDX module/VMM ABI.
@@ -85,12 +65,6 @@ void __init tdx_early_init(void);
 /* Used to communicate with the TDX module */
 u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
 		      struct tdx_module_output *out);
-
-/* Used to request services from the VMM */
-u64 __tdx_hypercall(struct tdx_hypercall_args *args, unsigned long flags);
-
-/* Called from __tdx_hypercall() for unrecoverable failure */
-void __tdx_hypercall_failed(void);
 
 void tdx_get_ve_info(struct ve_info *ve);
 
