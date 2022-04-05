5967fda4014d0543cfa0d949218ac2b1ec803d0c
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2022-04-06 02:29:10 +0300

    x86/tdx: Detect running as a TDX guest in early boot
    ANBZ: #4805
    
    commit 59bd54a84d15e9335de5b8abe7b3b9713a36b99b upstream.
    
    In preparation of extending cc_platform_has() API to support TDX guest,
    use CPUID instruction to detect support for TDX guests in the early
    boot code (via tdx_early_init()). Since copy_bootdata() is the first
    user of cc_platform_has() API, detect the TDX guest status before it.
    
    Define a synthetic feature flag (X86_FEATURE_TDX_GUEST) and set this
    bit in a valid TDX guest platform.
    
    Intel-SIG: commit 59bd54a84d15 x86/tdx: Detect running as a TDX guest in
    early boot.
    To enable tdx guest, this patch set will continue to enable tdx specific
    features and capability in the next patches.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20220405232939.73860-2-kirill.shutemov@linux.intel.com
    [ zhiminghufighting: amend commit log ]
    Signed-off-by: zhiminghufighting <zhiming.hu@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -2,6 +2,11 @@
 #ifndef _ASM_X86_KVM_BOOT_H
 #define _ASM_X86_KVM_BOOT_H
 
+#include <linux/init.h>
+
+#define TDX_CPUID_LEAF_ID	0x21
+#define TDX_IDENT		"IntelTDX    "
+
 #ifdef CONFIG_KVM_INTEL_TDX
 bool platform_has_tdx(void);
 #else
@@ -9,5 +14,15 @@ static inline bool platform_has_tdx(void) { return false; }
 #endif
 
 extern enum tdx_module_status_t tdx_module_status;
+
+#ifdef CONFIG_INTEL_TDX_GUEST
+
+void __init tdx_early_init(void);
+
+#else
+
+static inline void tdx_early_init(void) { };
+
+#endif /* CONFIG_INTEL_TDX_GUEST */
 
 #endif /* _ASM_X86_KVM_BOOT_H */
