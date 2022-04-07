a30d306c95adf76a40c919a74ab6eab996fc280c
Xu Yu <xuyu@linux.alibaba.com>
2022-04-07 11:58:36 +0800

    userfaultfd/selftests: fixup supported features in userfaultfd_open
    ANBZ: #395
    
    This makes userfaultfd_open() fixs up features with the result of
    UFFDIO_API ioctl. As a result, the parameter of userfaultfd_open() is
    changed to int * from int.
    
    No functional change.
    
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Gang Deng <gavin.dg@linux.alibaba.com>

--- tools/testing/selftests/vm/userfaultfd.c
+++ tools/testing/selftests/vm/userfaultfd.c
@@ -778,7 +778,7 @@ static int stress(struct uffd_stats *uffd_stats)
 	return 0;
 }
 
-static int userfaultfd_open(int features)
+static int userfaultfd_open(int *features)
 {
 	struct uffdio_api uffdio_api;
 
@@ -791,7 +791,7 @@ static int userfaultfd_open(int features)
 	uffd_flags = fcntl(uffd, F_GETFD, NULL);
 
 	uffdio_api.api = UFFD_API;
-	uffdio_api.features = features;
+	uffdio_api.features = *features;
 	if (ioctl(uffd, UFFDIO_API, &uffdio_api)) {
 		fprintf(stderr, "UFFDIO_API\n");
 		return 1;
@@ -801,6 +801,7 @@ static int userfaultfd_open(int features)
 		return 1;
 	}
 
+	*features = uffdio_api.features;
 	return 0;
 }
 
@@ -1035,6 +1036,7 @@ static int userfaultfd_zeropage_test(void)
 {
 	struct uffdio_register uffdio_register;
 	unsigned long expected_ioctls;
+	int features = 0;
 
 	printf("testing UFFDIO_ZEROPAGE: ");
 	fflush(stdout);
@@ -1042,7 +1044,7 @@ static int userfaultfd_zeropage_test(void)
 	if (uffd_test_ops->release_pages(area_dst))
 		return 1;
 
-	if (userfaultfd_open(0) < 0)
+	if (userfaultfd_open(&features) < 0)
 		return 1;
 	uffdio_register.range.start = (unsigned long) area_dst;
 	uffdio_register.range.len = nr_pages * page_size;
@@ -1092,7 +1094,7 @@ static int userfaultfd_events_test(void)
 
 	features = UFFD_FEATURE_EVENT_FORK | UFFD_FEATURE_EVENT_REMAP |
 		UFFD_FEATURE_EVENT_REMOVE;
-	if (userfaultfd_open(features) < 0)
+	if (userfaultfd_open(&features) < 0)
 		return 1;
 	fcntl(uffd, F_SETFL, uffd_flags | O_NONBLOCK);
 
@@ -1164,7 +1166,7 @@ static int userfaultfd_sig_test(void)
 		return 1;
 
 	features = UFFD_FEATURE_EVENT_FORK|UFFD_FEATURE_SIGBUS;
-	if (userfaultfd_open(features) < 0)
+	if (userfaultfd_open(&features) < 0)
 		return 1;
 	fcntl(uffd, F_SETFL, uffd_flags | O_NONBLOCK);
 
@@ -1236,6 +1238,7 @@ static int userfaultfd_stress(void)
 	unsigned long cpu;
 	int err;
 	struct uffd_stats uffd_stats[nr_cpus];
+	int features = 0;
 
 	uffd_test_ops->allocate_area((void **)&area_src);
 	if (!area_src)
@@ -1244,7 +1247,7 @@ static int userfaultfd_stress(void)
 	if (!area_dst)
 		return 1;
 
-	if (userfaultfd_open(0) < 0)
+	if (userfaultfd_open(&features) < 0)
 		return 1;
 
 	count_verify = malloc(nr_pages * sizeof(unsigned long long));
