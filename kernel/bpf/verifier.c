7afd5170e73894fe360a7d2cf72a187913b787c2
Qiao Ma <mqaio@linux.alibaba.com>
2022-04-14 10:41:09 +0800

    Revert "bpf: convert explored_states to hash table"
    ANBZ: #845
    
    This reverts commit b5eb235c115ddd8b7824c92379851629338db4af.
    
    Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -5497,19 +5497,11 @@ static int *insn_stack;	/* stack of insns to process */
 static int cur_stack;	/* current stack index */
 static int *insn_state;
 
-static u32 state_htab_size(struct bpf_verifier_env *env)
-{
-	return env->prog->len;
-}
-
 static struct bpf_verifier_state_list **explored_state(
 					struct bpf_verifier_env *env,
 					int idx)
 {
-	struct bpf_verifier_state *cur = env->cur_state;
-	struct bpf_func_state *state = cur->frame[cur->curframe];
-
-	return &env->explored_states[(idx ^ state->callsite) % state_htab_size(env)];
+	return &env->explored_states[idx];
 }
 
 static void init_explored_state(struct bpf_verifier_env *env, int idx)
@@ -5808,8 +5800,7 @@ static void clean_live_states(struct bpf_verifier_env *env, int insn,
 
 	sl = *explored_state(env, insn);
 	while (sl) {
-		if (sl->state.insn_idx != insn ||
-		    sl->state.curframe != cur->curframe)
+		if (sl->state.curframe != cur->curframe)
 			goto next;
 		for (i = 0; i <= cur->curframe; i++)
 			if (sl->state.frame[i]->callsite != cur->frame[i]->callsite)
@@ -6140,9 +6131,6 @@ static int is_state_visited(struct bpf_verifier_env *env, int insn_idx)
 	clean_live_states(env, insn_idx, cur);
 
 	while (sl) {
-		states_cnt++;
-		if (sl->state.insn_idx != insn_idx)
-			goto next;
 		if (states_equal(env, &sl->state, cur)) {
 			sl->hit_cnt++;
 			/* reached equivalent register/stack state,
@@ -6160,6 +6148,7 @@ static int is_state_visited(struct bpf_verifier_env *env, int insn_idx)
 				return err;
 			return 1;
 		}
+		states_cnt++;
 		sl->miss_cnt++;
 		/* heuristic to determine whether this state is beneficial
 		 * to keep checking from state equivalence point of view.
@@ -6186,7 +6175,6 @@ static int is_state_visited(struct bpf_verifier_env *env, int insn_idx)
 			sl = *pprev;
 			continue;
 		}
-next:
 		pprev = &sl->next;
 		sl = *pprev;
 	}
@@ -6221,7 +6209,6 @@ next:
 	cur->parent = new;
 	cur->first_insn_idx = insn_idx;
 	clear_jmp_history(cur);
-	new->insn_idx = insn_idx;
 	new_sl->next = *explored_state(env, insn_idx);
 	*explored_state(env, insn_idx) = new_sl;
 	/* connect new state to parentage chain */
@@ -7517,7 +7504,7 @@ static void free_states(struct bpf_verifier_env *env)
 	if (!env->explored_states)
 		return;
 
-	for (i = 0; i < state_htab_size(env); i++) {
+	for (i = 0; i < env->prog->len; i++) {
 		sl = env->explored_states[i];
 
 		while (sl) {
@@ -7615,7 +7602,7 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr)
 			goto skip_full_check;
 	}
 
-	env->explored_states = kvcalloc(state_htab_size(env),
+	env->explored_states = kcalloc(env->prog->len,
 				       sizeof(struct bpf_verifier_state_list *),
 				       GFP_USER);
 	ret = -ENOMEM;
