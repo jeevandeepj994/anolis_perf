alibaba/cloud-kernel

commit 1d45c19e7f6b38301184909031e602d602b4ca16

ck: sched: make ID_LOOSE_EXPEL defined with CONFIG_GROUP_IDENTITY on
fix #34359599
fix #34361563

In commit b7fecff78665 ("ck: sched: fix the performence regression
caused by update_rq_on_expel()") , ID_LOOSE_EXPEL is defined without
#ifdef CONFIG_GROUP_IDENTITY, but actually, it is only effective while
CONFIG_GROUP_IDENTITY is on, and this patch is to fix it.

By the way, the unit of sysctl_sched_expel_update_interval should be
ms, this patch also fix the description and the logic.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Acked-by: Michael Wang <yun.wang@linux.alibaba.com>

kernel/sched/fair.c
@@ -143,12 +143,12 @@ unsigned int sysctl_sched_idle_saver_wmark;
 int sysctl_sched_expel_idle_balance_delay = -1;
 /*
  *  In order to prevent scheduling errors in the case of ipi failure,
- *  update_rq_on_expel() is called in function pick_next_task_fair(),
+ *  __update_rq_on_expel() is called in function pick_next_task_fair(),
  *  but this will cause performence regression. In order to prevent
  *  errors while ensuring performence, we introduce an interval to call
- *  update_rq_on_expel().
+ *  __update_rq_on_expel().
  *
- *  Default: 10, units: jiffy
+ *  Default: 10, units: ms
  */
 const_debug unsigned long sysctl_sched_expel_update_interval = 10;
 #endif
@@ -669,7 +669,7 @@ static inline bool need_expel(int this_cpu)
 	return false;
 }
 
-static inline void update_rq_on_expel(struct rq *rq)
+static inline void __update_rq_on_expel(struct rq *rq)
 {
 	bool ret = need_expel(rq->cpu);
 
@@ -685,18 +685,23 @@ static inline void update_rq_on_expel(struct rq *rq)
  * This funciton is called in pick_next_task_fair() if ID_LOOSE_EXPEL is
  * on to improve the performence. But there is a risk to do so.
  * Consider a scenario that if this cpu is executing pick_next_task_fair()
- * and skip update_rq_on_expel(), a reschedule IPI is arriving but local
+ * and skip __update_rq_on_expel(), a reschedule IPI is arriving but local
  * irq is disabled, so an underclass task is picked to run on this cpu until
  * reschedule, which will degrade the performence of highclass task.
  */
-static inline void loose_update_rq_on_expel(struct rq *rq)
+static inline void update_rq_on_expel(struct rq *rq)
 {
-	if (rq->nr_under_running &&
-	    time_after(jiffies, rq->next_expel_update)) {
-		update_rq_on_expel(rq);
-		rq->next_expel_update = jiffies +
-			msecs_to_jiffies(sysctl_sched_expel_update_interval);
-	}
+	if (!sched_feat(ID_LOOSE_EXPEL))
+		goto update;
+
+	if (!rq->nr_under_running
+	    || !time_after(jiffies, rq->next_expel_update))
+		return;
+	rq->next_expel_update = jiffies +
+		msecs_to_jiffies(sysctl_sched_expel_update_interval);
+
+update:
+	__update_rq_on_expel(rq);
 }
 
 static inline bool rq_on_expel(struct rq *rq)
@@ -774,11 +779,11 @@ static inline bool need_expel(int this_cpu)
 	return false;
 }
 
-static inline void update_rq_on_expel(struct rq *rq)
+static inline void __update_rq_on_expel(struct rq *rq)
 {
 }
 
-static inline void loose_update_rq_on_expel(struct rq *rq)
+static inline void update_rq_on_expel(struct rq *rq)
 {
 }
 
@@ -1619,7 +1624,7 @@ void handle_smt_expeller(void)
 {
 	struct rq *rq = this_rq();
 
-	update_rq_on_expel(rq);
+	__update_rq_on_expel(rq);
 
 	/* Safe since 'current' can't be changed during IPI */
 	if (expel_resched(rq) && !test_tsk_need_resched(rq->curr)) {
@@ -1690,6 +1695,10 @@ static inline bool should_expel_se(struct rq *rq, struct sched_entity *se)
 static inline bool expellee_only(struct rq *rq)
 {
 	return false;
+}
+
+static inline void __update_rq_on_expel(struct rq *rq)
+{
 }
 
 static inline void update_rq_on_expel(struct rq *rq)
@@ -8041,10 +8050,7 @@ again:
 	if (!cfs_rq->nr_running)
 		goto idle;
 
-	if (sched_feat(ID_LOOSE_EXPEL))
-		loose_update_rq_on_expel(rq);
-	else
-		update_rq_on_expel(rq);
+	update_rq_on_expel(rq);
 
 	if (expellee_only(rq)) {
 		/*

jeevandeepj994/anolis_perf

commit 85204625ca56978e291aed8fabb8b6c06dcf0c3e

anolis: sched: Fix the performence regression because of update_rq_on_expel()
ANBZ: #696

The performence of online task regressed because of update_rq_on_expel()
and __update_rq_on_expel() are not inlined in the compilation phase,
althrogh they have the attribute "inline".

And this patch is to fix this problem.

Here follows the test scenario and test result:

Online task: Floating-point number arithmetic task with 3 threads,
	calculating the square root from 1 to 400000000, and the bvt
	of its cgroup is 2.
Offline task: stress-ng -c $nr_cpus -l 70, and the bvt of its cgroup
	is -1.

1. Only online task running, online task performence:
quantile 0.5:  3.8557952085e+09 ns
quantile 0.90:  3.8581308506e+09 ns
quantile 0.95:  3.8589838853e+09 ns
quantile 0.99:  3.860188924e+09 ns

2. Online task + offline task before applying this patch, online task
performence:
quantile 0.5:  3.9636566725e+09 ns
quantile 0.90:  4.0008950969e+09 ns
quantile 0.95:  4.0098206868e+09 ns
quantile 0.99:  4.196525352e+09 ns

3. Online task + offline task after applying this patch, online task
performence:
quantile 0.5:  3.8602548115e+09 ns
quantile 0.90:  3.8633850429e+09 ns
quantile 0.95:  3.8649974241e+09 ns
quantile 0.99:  3.869420975e+09 ns

This patch meets our expection.

Fixes: 1d45c19e7f6b ("sched: make ID_LOOSE_EXPEL defined with CONFIG_GROUP_IDENTITY on")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Acked-by: Michael Wang <yun.wang@linux.alibaba.com>

kernel/sched/fair.c
@@ -673,7 +673,7 @@ static inline bool need_expel(int this_cpu)
 	return false;
 }
 
-static inline void __update_rq_on_expel(struct rq *rq)
+static __always_inline void __update_rq_on_expel(struct rq *rq)
 {
 	bool ret = need_expel(rq->cpu);
 
@@ -693,18 +693,16 @@ static inline void __update_rq_on_expel(struct rq *rq)
  * irq is disabled, so an underclass task is picked to run on this cpu until
  * reschedule, which will degrade the performence of highclass task.
  */
-static inline void update_rq_on_expel(struct rq *rq)
+static __always_inline void update_rq_on_expel(struct rq *rq)
 {
-	if (!sched_feat(ID_LOOSE_EXPEL))
-		goto update;
+	if (sched_feat(ID_LOOSE_EXPEL)) {
+		if (!rq->nr_under_running
+		    || !time_after(jiffies, rq->next_expel_update))
+			return;
 
-	if (!rq->nr_under_running
-	    || !time_after(jiffies, rq->next_expel_update))
-		return;
-	rq->next_expel_update = jiffies +
-		msecs_to_jiffies(sysctl_sched_expel_update_interval);
-
-update:
+		rq->next_expel_update = jiffies +
+			msecs_to_jiffies(sysctl_sched_expel_update_interval);
+	}
 	__update_rq_on_expel(rq);
 }
 