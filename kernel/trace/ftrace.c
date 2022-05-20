alibaba/cloud-kernel

commit d6507404723e282585ab5a5f02b62b8e1f28da2f

anolis: scripts: ftrace_init optimization about instruction substitution
OpenAnolis Bug Tracker: 0000558

In some business scenarios, GCC versions are so old that
optimizations in ftrace cannot be completed, such as
-mrecord-mcount and -mnop-mcount. The recordmcount in the
kernel is actually used. In this case, ftrace_init will
consume a period of time, usually around 9~12ms. Do nop
substitution in recordmcount.c to speed up ftrace_init.
Only for x86 now.

Signed-off-by: Yinan Liu <yinan@linux.alibaba.com>
Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>

kernel/trace/ftrace.c
@@ -5621,6 +5621,10 @@ static int ftrace_process_locs(struct module *mod,
 	 * until we are finished with it, and there's no
 	 * reason to cause large interrupt latencies while we do it.
 	 */
+#if defined CONFIG_X86 || defined CONFIG_X86_64
+	ret = 0;
+	goto out;
+#endif
 	if (!mod)
 		local_irq_save(flags);
 	ftrace_update_code(mod, start_pg);

Makefile
@@ -748,6 +748,7 @@ endif
 
 ifdef CONFIG_FUNCTION_TRACER
 ifdef CONFIG_FTRACE_MCOUNT_RECORD
+ifneq ($(ARCH),x86)
   # gcc 5 supports generating the mcount tables directly
   ifeq ($(call cc-option-yn,-mrecord-mcount),y)
     CC_FLAGS_FTRACE	+= -mrecord-mcount
@@ -759,6 +760,7 @@ ifdef CONFIG_FTRACE_MCOUNT_RECORD
       CC_FLAGS_USING	+= -DCC_USING_NOP_MCOUNT
     endif
   endif
+endif
 endif
 ifdef CONFIG_HAVE_FENTRY
   ifeq ($(call cc-option-yn, -mfentry),y)

scripts/recordmcount.h
@@ -310,6 +310,8 @@ static uint_t *sift_rel_mcount(uint_t *mlocp,
 			       uint_t const recval,
 			       unsigned const reltype)
 {
+	Elf_Shdr *const shdr0 = (Elf_Shdr *)(_w(ehdr->e_shoff) + (void *)ehdr);
+	Elf_Shdr const *const shdr = &shdr0[w(relhdr->sh_info)];
 	uint_t *const mloc0 = mlocp;
 	Elf_Rel *mrelp = *mrelpp;
 	Elf_Sym const *sym0;
@@ -323,6 +325,7 @@ static uint_t *sift_rel_mcount(uint_t *mlocp,
 	get_sym_str_and_relp(relhdr, ehdr, &sym0, &str0, &relp);
 
 	for (t = nrel; t; --t) {
+		int ret = -1;
 		if (!mcountsym)
 			mcountsym = get_mcountsym(sym0, relp, str0);
 
@@ -340,6 +343,18 @@ static uint_t *sift_rel_mcount(uint_t *mlocp,
 				*mlocp++ = addend;
 
 			mrelp = (Elf_Rel *)(rel_entsize + (void *)mrelp);
+			/* convert mcount into nop */
+			if (make_nop == make_nop_x86) {
+				ret = make_nop((void *)ehdr,
+						_w(shdr->sh_offset) + _w(relp->r_offset));
+				if (!ret) {
+					Elf_Rel rel;
+					rel = *(Elf_Rel *)relp;
+					Elf_r_info(&rel, Elf_r_sym(relp), rel_type_nop);
+					ulseek(fd_map, (void *)relp - (void *)ehdr, SEEK_SET);
+					uwrite(fd_map, &rel, sizeof(rel));
+				}
+			}
 		}
 		relp = (Elf_Rel const *)(rel_entsize + (void *)relp);
 	}

jeevandeepj994/anolis_perf

commit 740083764069b31f60d21cd48122517daeabb60d

anolis: ftrace: fix update_tot_cnt statistic for x86
ANBZ: #1273

Previously, on x86 platforms, we directly finished make_nop() for
ftrace at compile time to complete ftrace_init optimization.This
results in the absence of update_tot_cnt statistics. Enable
function_profile_enable will lead to OOM and then kernel crash.
This patch fixed this statistic, borrowing from the value setting
when dynamic ftrace is off.

Fixes: d6507404723e ("scripts: ftrace_init optimization about instruction substitution")
Signed-off-by: Yinan Liu <yinan@linux.alibaba.com>
Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>

kernel/trace/ftrace.c
@@ -5626,6 +5626,7 @@ static int ftrace_process_locs(struct module *mod,
 	 * reason to cause large interrupt latencies while we do it.
 	 */
 #ifdef CONFIG_X86_64
+	ftrace_update_tot_cnt = 20000;
 	ret = 0;
 	goto out;
 #endif