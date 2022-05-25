cd3a0859ab28d03fdd4b42797446b20ec0683466
Guangguan Wang <guangguan.wang@linux.alibaba.com>
2022-05-25 11:37:48 +0800

    anolis: net/smc: combine send cq and recv cq into one cq
    ANBZ: #1742
    
    SMC-R uses two CQs per link, one for SQ called SCQ and
    the other for RQ called RCQ. RDMA supports SCQ and RCQ
    are the same CQ. In RDMA, more CQs means more interrupts
    as less cqe polled out echo poll_cq. This patch combines
    send cq and recv cq into one cq. Because of halving the
    number of CQs, fewer interrupts are generated and hi usage
    is lower.
    
    Nginx benchmark shows 5.8% improvement in throughput:
    Server test command:
    smc_run nginx
    Client test command:
    smc_run /opt/wrk/wrk http://ip:port -t 32 -c 992 -d 30 --latency
    
    Before:
    Requests/sec: 1927316.76
    Transfer/sec:    295.92MB
    
    After:
    Requests/sec: 2039360.72
    Transfer/sec:    313.13MB
    
    Redis benchmark shows 8% improvement in cpu usage:
    Server test command:
    smc_run ./redis-server --save "" --appendonly no --protected-mode no
    sar 1 10 (Cpu usage collect command)
    Client test command:
    smc_run ./redis-benchmark -h <server ip> -q -t set -P 1 --threads 7\
    	-n 25000000 -c 100 -d 10
    
    Before:
    CPU     %user     %nice   %system   %iowait    %steal     %idle
    all      0.90      0.00      5.44      0.00      0.00     93.66
    
    After:
    CPU     %user     %nice   %system   %iowait    %steal     %idle
    all      0.87      0.00      5.00      0.00      0.00     94.13
    
    Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/577

--- net/smc/smc_ib.h
+++ net/smc/smc_ib.h
@@ -36,7 +36,6 @@ struct smc_ib_cq {				/* ib_cq wrapper for smc */
 	struct smc_ib_device	*smcibdev;	/* parent ib device */
 	struct ib_cq		*ib_cq;		/* real ib_cq for link */
 	struct tasklet_struct	tasklet;	/* tasklet for wr */
-	bool			is_send;	/* send for recv cq */
 	int			load;		/* load of current cq */
 };
 
@@ -45,9 +44,8 @@ struct smc_ib_device {				/* ib-device infos for smc */
 	struct ib_device	*ibdev;
 	struct ib_port_attr	pattr[SMC_MAX_PORTS];	/* ib dev. port attrs */
 	struct ib_event_handler	event_handler;	/* global ib_event handler */
-	int			num_cq_peer;	/* num of snd/rcv cq peer */
-	struct smc_ib_cq	*smcibcq_send;  /* send cqs */
-	struct smc_ib_cq	*smcibcq_recv;  /* recv cqs */
+	int			num_cq;	/* num of snd/rcv cq */
+	struct smc_ib_cq	*smcibcq;  /* send & recv cqs */
 	char			mac[SMC_MAX_PORTS][ETH_ALEN];
 						/* mac address per port*/
 	u8			pnetid[SMC_MAX_PORTS][SMC_MAX_PNETID_LEN];
