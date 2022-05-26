9700f88afeb035b3b959aa908cc8f429b04820f2
Zi Yan <ziy@nvidia.com>
2022-05-26 19:15:31 -0400

    mm: split free page with properly free memory accounting and without race
    commit 86d28b0709279ccc636ef9ba267b7f3bcef79a4b upstream
    
    ANBZ: #8409
    
    In isolate_single_pageblock(), free pages are checked without holding zone
    lock, but they can go away in split_free_page() when zone lock is held.
    Check the free page and its order again in split_free_page() when zone lock
    is held. Recheck the page if the free page is gone under zone lock.
    
    In addition, in split_free_page(), the free page was deleted from the page
    list without changing free page accounting. Add the missing free page
    accounting code.
    
    Fix the type of order parameter in split_free_page().
    
    Link: https://lore.kernel.org/lkml/20220525103621.987185e2ca0079f7b97b856d@linux-foundation.org/
    Link: https://lkml.kernel.org/r/20220526231531.2404977-2-zi.yan@sent.com
    Fixes: b2c9e2fbba32 ("mm: make alloc_contig_range work at pageblock granularity")
    Signed-off-by: Zi Yan <ziy@nvidia.com>
    Reported-by: Doug Berger <opendmb@gmail.com>
      Link: https://lore.kernel.org/linux-mm/c3932a6f-77fe-29f7-0c29-fe6b1c67ab7b@gmail.com/
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Qian Cai <quic_qiancai@quicinc.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Eric Ren <renzhengeek@gmail.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Michael Walle <michael@walle.cc>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>

--- mm/internal.h
+++ mm/internal.h
@@ -206,8 +206,8 @@ extern void free_unref_page_list(struct list_head *list);
 extern void zone_pcp_update(struct zone *zone);
 extern void zone_pcp_reset(struct zone *zone);
 
-void split_free_page(struct page *free_page,
-				int order, unsigned long split_pfn_offset);
+int split_free_page(struct page *free_page,
+			unsigned int order, unsigned long split_pfn_offset);
 
 #if defined CONFIG_COMPACTION || defined CONFIG_CMA
 
