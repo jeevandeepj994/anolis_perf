3155ec81bcbbe7eda9e9c6e0286f3511901c52af
Like Xu <likexu@tencent.com>
2022-06-01 11:19:25 +0800

    KVM: x86/pmu: Avoid exposing Intel BTS feature
    ANBZ: #9792
    
    commit b9181c8ef35636152facc72f801f27b4264df8c0 upstream
    
    The BTS feature (including the ability to set the BTS and BTINT
    bits in the DEBUGCTL MSR) is currently unsupported on KVM.
    
    But we may try using the BTS facility on a PEBS enabled guest like this:
        perf record -e branches:u -c 1 -d ls
    and then we would encounter the following call trace:
    
     [] unchecked MSR access error: WRMSR to 0x1d9 (tried to write 0x00000000000003c0)
            at rIP: 0xffffffff810745e4 (native_write_msr+0x4/0x20)
     [] Call Trace:
     []  intel_pmu_enable_bts+0x5d/0x70
     []  bts_event_add+0x54/0x70
     []  event_sched_in+0xee/0x290
    
    As it lacks any CPUID indicator or perf_capabilities valid bit
    fields to prompt for this information, the platform would hint
    the Intel BTS feature unavailable to guest by setting the
    BTS_UNAVAIL bit in the IA32_MISC_ENABLE.
    
    Signed-off-by: Like Xu <likexu@tencent.com>
    Message-Id: <20220601031925.59693-3-likexu@tencent.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Cheng Hong <chenpeihong.cph@alibaba-inc.com>

--- arch/x86/kvm/vmx/pmu_intel.c
+++ arch/x86/kvm/vmx/pmu_intel.c
@@ -375,6 +375,8 @@ static void intel_pmu_refresh(struct kvm_vcpu *vcpu)
 	pmu->pebs_enable_mask = ~0ull;
 	pmu->pebs_data_cfg_mask = ~0ull;
 
+	vcpu->arch.ia32_misc_enable_msr |= MSR_IA32_MISC_ENABLE_PMU_RO_MASK;
+
 	entry = kvm_find_cpuid_entry(vcpu, 0xa, 0);
 	if (!entry)
 		return;
@@ -450,7 +452,6 @@ static void intel_pmu_refresh(struct kvm_vcpu *vcpu)
 				~((1ull << pmu->nr_arch_gp_counters) - 1);
 		}
 	} else {
-		vcpu->arch.ia32_misc_enable_msr |= MSR_IA32_MISC_ENABLE_PEBS_UNAVAIL;
 		vcpu->arch.perf_capabilities &= ~PERF_CAP_PEBS_MASK;
 	}
 }
