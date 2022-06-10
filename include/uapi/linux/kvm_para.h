sugarfillet/linux

commit 975c30f231bcf4008895fe99cc285bb2638e9d33

anolis: KVM: x86: Support VM_ATTESTATION hypercall
ANBZ: #33

When sev guest wants to collect the attestation report, it cannot directly
communicate with psp. Add VM_ATTESTATION hypercall to allow sev guest to
tell host to help get the attestation report. Since sev guest memory is
encrypted, host cannot tamper with the report data.

Signed-off-by: hanliyang <hanliyang@hygon.cn>
Acked-by: Baolin Wang <baolin.wang@linux.alibaba.com>

include/uapi/linux/kvm_para.h
@@ -29,6 +29,7 @@
 #define KVM_HC_CLOCK_PAIRING		9
 #define KVM_HC_SEND_IPI		10
 #define KVM_HC_SCHED_YIELD		11
+#define KVM_HC_VM_ATTESTATION		12
 
 /*
  * hypercalls use architecture specific

arch/x86/include/asm/kvm_host.h
@@ -1282,6 +1282,8 @@ struct kvm_x86_ops {
 
 	void (*migrate_timers)(struct kvm_vcpu *vcpu);
 	void (*msr_filter_changed)(struct kvm_vcpu *vcpu);
+
+	int (*vm_attestation)(struct kvm *kvm, unsigned long gpa, unsigned long len);
 };
 
 struct kvm_x86_nested_ops {

arch/x86/kvm/svm/sev.c
@@ -27,6 +27,8 @@ static unsigned long *sev_asid_bitmap;
 static unsigned long *sev_reclaim_asid_bitmap;
 #define __sme_page_pa(x) __sme_set(page_to_pfn(x) << PAGE_SHIFT)
 
+static const char sev_vm_mnonce[] = "VM_ATTESTATION";
+
 struct enc_region {
 	struct list_head list;
 	unsigned long npages;
@@ -1068,6 +1070,71 @@ int svm_mem_enc_op(struct kvm *kvm, void __user *argp)
 out:
 	mutex_unlock(&kvm->lock);
 	return r;
+}
+
+int sev_vm_attestation(struct kvm *kvm, unsigned long gpa, unsigned long len)
+{
+	struct kvm_sev_info *sev = &to_kvm_svm(kvm)->sev_info;
+	struct sev_data_attestation_report *data = NULL;
+	struct page **pages;
+	unsigned long guest_uaddr, n;
+	int ret = 0, offset, error;
+
+	if (!sev_guest(kvm) || (boot_cpu_data.x86_vendor != X86_VENDOR_HYGON))
+		return -ENOTTY;
+
+	/*
+	 * The physical address of guest must valid and page aligned, and
+	 * the length of guest memory region must be page size aligned.
+	 */
+	if (!gpa || (gpa & ~PAGE_MASK) || (len & ~PAGE_MASK)) {
+		pr_err("invalid guest address or length\n");
+		return -EFAULT;
+	}
+
+	guest_uaddr = gfn_to_hva(kvm, gpa_to_gfn(gpa));
+	pages = sev_pin_memory(kvm, guest_uaddr, len, &n, 1);
+	if (IS_ERR(pages))
+		return PTR_ERR(pages);
+
+	/*
+	 * The attestation report must be copied into contiguous memory region,
+	 * lets verify that userspace memory pages are contiguous before we
+	 * issue commmand.
+	 */
+	if (get_num_contig_pages(0, pages, n) != n) {
+		ret = -EINVAL;
+		goto e_unpin_memory;
+	}
+
+	ret = -ENOMEM;
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
+	if (!data)
+		goto e_unpin_memory;
+
+	/* sev_vm_mnonce indicates attestation request from guest */
+	if (sizeof(sev_vm_mnonce) >= sizeof(data->mnonce)) {
+		ret = -EINVAL;
+		goto e_free;
+	}
+
+	memcpy(data->mnonce, sev_vm_mnonce, sizeof(sev_vm_mnonce));
+
+	offset = guest_uaddr & (PAGE_SIZE - 1);
+	data->address = __sme_page_pa(pages[0]) + offset;
+	data->len = len;
+
+	data->handle = sev->handle;
+	ret = sev_issue_cmd(kvm, SEV_CMD_ATTESTATION_REPORT, data, &error);
+
+	if (ret)
+		pr_err("vm attestation ret %#x, error %#x\n", ret, error);
+
+e_free:
+	kfree(data);
+e_unpin_memory:
+	sev_unpin_memory(kvm, pages, n);
+	return ret;
 }
 
 int svm_register_enc_region(struct kvm *kvm,

arch/x86/kvm/svm/svm.c
@@ -4331,6 +4331,8 @@ static struct kvm_x86_ops svm_x86_ops __initdata = {
 	.apic_init_signal_blocked = svm_apic_init_signal_blocked,
 
 	.msr_filter_changed = svm_msr_filter_changed,
+
+	.vm_attestation = sev_vm_attestation,
 };
 
 static struct kvm_x86_init_ops svm_init_ops __initdata = {

arch/x86/kvm/svm/svm.h
@@ -491,6 +491,7 @@ int svm_register_enc_region(struct kvm *kvm,
 			    struct kvm_enc_region *range);
 int svm_unregister_enc_region(struct kvm *kvm,
 			      struct kvm_enc_region *range);
+int sev_vm_attestation(struct kvm *kvm, unsigned long gpa, unsigned long len);
 void pre_sev_run(struct vcpu_svm *svm, int cpu);
 int __init sev_hardware_setup(void);
 void sev_hardware_teardown(void);

arch/x86/kvm/x86.c
@@ -8203,7 +8203,7 @@ int kvm_emulate_hypercall(struct kvm_vcpu *vcpu)
 		a3 &= 0xFFFFFFFF;
 	}
 
-	if (kvm_x86_ops.get_cpl(vcpu) != 0) {
+	if (kvm_x86_ops.get_cpl(vcpu) != 0 && nr != KVM_HC_VM_ATTESTATION) {
 		ret = -KVM_EPERM;
 		goto out;
 	}
@@ -8239,6 +8239,11 @@ int kvm_emulate_hypercall(struct kvm_vcpu *vcpu)
 
 		kvm_sched_yield(vcpu->kvm, a0);
 		ret = 0;
+		break;
+	case KVM_HC_VM_ATTESTATION:
+		ret = -KVM_ENOSYS;
+		if (kvm_x86_ops.vm_attestation)
+			ret = kvm_x86_ops.vm_attestation(vcpu->kvm, a0, a1);
 		break;
 	default:
 		ret = -KVM_ENOSYS;

jeevandeepj994/anolis_perf

commit 169e2baf417c7bcad2e40181289a0363ac2dd85c

anolis: KVM: x86: adjust VM_ATTESTATION hypercall id
ANBZ: #1428

To avoid conflicts with the hypercall id of the Linux mainline, we define
a macro HYPERCALL_ANOLIS. The KVM_HC_VM_ATTESTATION was adjusted and
became the first one to use it.

Fixes: 975c30f231bc ("KVM: x86: Support VM_ATTESTATION hypercall")
Signed-off-by: zhangzeyu.zzy <zhangzeyu.zzy@alibaba-inc.com>

include/uapi/linux/kvm_para.h
@@ -18,6 +18,8 @@
 #define KVM_EPERM		EPERM
 #define KVM_EOPNOTSUPP		95
 
+#define HYERCALL_ANOLIS  1000
+
 #define KVM_HC_VAPIC_POLL_IRQ		1
 #define KVM_HC_MMU_OP			2
 #define KVM_HC_FEATURES			3
@@ -29,7 +31,11 @@
 #define KVM_HC_CLOCK_PAIRING		9
 #define KVM_HC_SEND_IPI		10
 #define KVM_HC_SCHED_YIELD		11
-#define KVM_HC_VM_ATTESTATION		12
+
+/*
+ * Anolis specific hypercall
+ */
+#define KVM_HC_VM_ATTESTATION   HYERCALL_ANOLIS + 1
 
 /*
  * hypercalls use architecture specific