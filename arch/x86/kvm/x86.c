688b52ae45070460b03d9c09e503e4647e58408c
Wu Hao <hao.wu@intel.com>
2022-07-12 13:47:26 +0800

    kvm: x86: enqcmd: pass through MSR_IA32_PASID
    ANBZ: #1660
    
    commit f67ec7d480cbd32166b8fde3d7cd987fea3b7f84 intel-github.
    
    Each execution of ENQCMD results in an enqueue store with a PASID value.
    ENQCMD obtains the PASID value from MSR_IA32_PASID. When PASID translation
    is enabled, this PASID value from MSR_IA32_PASID is interpreted as a guest
    PASID, and the guest PASID is converted to a host PASID, and the enqueue
    store uses the host PASID for the command data that it writes.
    
    This enables direct pass through MSR_IA32_PASID to guest, so that user can
    program guest PASID values into MSR_IA32_PASID directly.
    
    This patch enables the virtualization support for XSAVE extension of PASID
    supervisor state component, so guest can use this extension to save/restore
    PASID state (MSR_IA32_PASID) per context switch.
    
    v3: merge XSAVES PASID state component patch
        rebase on top of msr filter changes
    
    Intel-SIG: commit f67ec7d480cb kvm: x86: enqcmd: pass through
    MSR_IA32_PASID. Necessary to support enqcmd/s.
    
    Signed-off-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
    [ Yi Sun: amend commit log ]
    Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/510
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1570,7 +1570,7 @@ bool kvm_msr_allowed(struct kvm_vcpu *vcpu, u32 index, u32 type)
 	u32 i;
 
 	/* x2APIC MSRs do not support filtering. */
-	if (index >= 0x800 && index <= 0x8ff)
+	if ((index >= 0x800 && index <= 0x8ff) || index == MSR_IA32_PASID)
 		return true;
 
 	idx = srcu_read_lock(&kvm->srcu);
@@ -10546,6 +10546,9 @@ int kvm_arch_hardware_setup(void *opaque)
 		supported_xss = 0;
 	else
 		supported_xss &= host_xss;
+
+	if (!kvm_pasid_supported())
+		kvm_cpu_cap_clear(X86_FEATURE_ENQCMD);
 
 #define __kvm_cpu_cap_has(UNUSED_, f) kvm_cpu_cap_has(f)
 	cr4_reserved_bits = __cr4_reserved_bits(__kvm_cpu_cap_has, UNUSED_);
