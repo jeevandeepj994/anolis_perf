d33b2b3bf562c1081f8efd8f9f49f8886df96312
Jiawen Wu <jiawenwu@trustnetic.com>
2022-07-13 14:06:33 +0800

    anolis: net: txgbe: Add ethtool support
    ANBZ: #1715
    
    Add to support the following ethtool commands:
    
    ethtool -i|--driver DEVNAME
    ethtool -s|--change DEVNAME
    ethtool -a|--show-pause DEVNAME
    ethtool -A|--pause DEVNAME
    ethtool -c|--show-coalesce DEVNAME
    ethtool -C|--coalesce DEVNAME
    ethtool -g|--show-ring DEVNAME
    ethtool -G|--set-ring DEVNAME
    ethtool -k|--show-features DEVNAME
    ethtool -K|--features DEVNAME
    ethtool -d|--register-dump DEVNAME
    ethtool -e|--eeprom-dump DEVNAME
    ethtool -E|--change-eeprom DEVNAME
    ethtool -r|--negotiate DEVNAME
    ethtool -p|--identify DEVNAME
    ethtool -t|--test DEVNAME
    ethtool -n|-u|--show-nfc|--show-ntuple DEVNAME
    ethtool -N|-U|--config-nfc|--config-ntuple DEVNAME
    ethtool -T|--show-time-stamping DEVNAME
    ethtool -x|--show-rxfh-indir|--show-rxfh DEVNAME
    ethtool -X|--set-rxfh-indir|--rxfh DEVNAME
    ethtool -f|--flash DEVNAME
    ethtool -l|--show-channels DEVNAME
    ethtool -L|--set-channels DEVNAME
    ethtool -m|--dump-module-eeprom|--module-info DEVNAME
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/709
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

--- drivers/net/ethernet/wangxun/txgbe/txgbe_phy.h
+++ drivers/net/ethernet/wangxun/txgbe/txgbe_phy.h
@@ -7,6 +7,7 @@
 #include "txgbe.h"
 
 #define TXGBE_I2C_EEPROM_DEV_ADDR       0xA0
+#define TXGBE_I2C_EEPROM_DEV_ADDR2      0xA2
 
 /* EEPROM byte offsets */
 #define TXGBE_SFF_IDENTIFIER            0x0
@@ -18,6 +19,8 @@
 #define TXGBE_SFF_10GBE_COMP_CODES      0x3
 #define TXGBE_SFF_CABLE_TECHNOLOGY      0x8
 #define TXGBE_SFF_CABLE_SPEC_COMP       0x3C
+#define TXGBE_SFF_SFF_8472_SWAP         0x5C
+#define TXGBE_SFF_SFF_8472_COMP         0x5E
 
 /* Bitmasks */
 #define TXGBE_SFF_DA_PASSIVE_CABLE      0x4
@@ -28,6 +31,7 @@
 #define TXGBE_SFF_1GBASET_CAPABLE       0x8
 #define TXGBE_SFF_10GBASESR_CAPABLE     0x10
 #define TXGBE_SFF_10GBASELR_CAPABLE     0x20
+#define TXGBE_SFF_ADDRESSING_MODE       0x4
 /* Bit-shift macros */
 #define TXGBE_SFF_VENDOR_OUI_BYTE0_SHIFT        24
 #define TXGBE_SFF_VENDOR_OUI_BYTE1_SHIFT        16
@@ -38,6 +42,9 @@
 #define TXGBE_SFF_VENDOR_OUI_FTL        0x00906500
 #define TXGBE_SFF_VENDOR_OUI_AVAGO      0x00176A00
 #define TXGBE_SFF_VENDOR_OUI_INTEL      0x001B2100
+
+/* SFP+ SFF-8472 Compliance */
+#define TXGBE_SFF_SFF_8472_UNSUP        0x00
 
 s32 txgbe_check_reset_blocked(struct txgbe_hw *hw);
 
@@ -51,5 +58,7 @@ s32 txgbe_read_i2c_byte(struct txgbe_hw *hw, u8 byte_offset,
 
 s32 txgbe_read_i2c_eeprom(struct txgbe_hw *hw, u8 byte_offset,
 			  u8 *eeprom_data);
+s32 txgbe_read_i2c_sff8472(struct txgbe_hw *hw, u8 byte_offset,
+			   u8 *sff8472_data);
 
 #endif /* _TXGBE_PHY_H_ */
