6ce347738b980d005d573e38b946eb38a3ff8458
D. Wythe <alibuda@linux.alibaba.com>
2022-07-18 19:58:40 +0800

    anolis: net/smc: remove locks smc_client_lgr_pending and smc_server_lgr_pending
    ANBZ: #1742
    
    This patch attempts to remove locks named smc_client_lgr_pending and
    smc_server_lgr_pending, which aim to serialize the creation of link
    group. However, once link group existed already, those locks are
    meaningless, worse still, they make incoming connections have to be
    queued one after the other.
    
    Now, the creation of  link group is no longer generated by competition,
    but allocated through following strategy.
    
    1. Try to find a suitable link group, if successd, current connection
    is considered as non first contact connection. ends.
    
    2. Check the number of connections currently waiting for a suitable
    link group to be created, if it is not less that the number of link
    groups to be created multiplied by SMC_RMBS_PER_LGR_MAX, goto 5.
    
    3. increase the number of connections currently waiting, and wait
    for woken up.
    
    4. goto 1
    
    5. Increase the number of link groups to be created, current
    connection is considered as the first contact connection. ends.
    
    We wake up the connection that was put to sleep in the above strategy
    through the SMC link state change event. Once the link moves out of
    the SMC_LNK_ACTIVATING state, decrease the number of link groups to
    be created, and then wake up at most SMC_RMBS_PER_LGR_MAX connections.
    
    A potential optimization is that when link transitions from SMC_LNK_ACTIVATING
    state to non SMC_LINK_ACTIVE state, only to wake up one connection and
    force it to be the first contact. This is already on the TODO list
    
    Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/577

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -1262,10 +1262,8 @@ static int smc_connect_rdma(struct smc_sock *smc,
 	if (reason_code)
 		return reason_code;
 
-	mutex_lock(&smc_client_lgr_pending);
 	reason_code = smc_conn_create(smc, ini);
 	if (reason_code) {
-		mutex_unlock(&smc_client_lgr_pending);
 		return reason_code;
 	}
 
@@ -1362,7 +1360,6 @@ static int smc_connect_rdma(struct smc_sock *smc,
 		if (reason_code)
 			goto connect_abort;
 	}
-	mutex_unlock(&smc_client_lgr_pending);
 
 	smc_copy_sock_settings_to_clc(smc);
 	smc->connect_nonblock = 0;
@@ -1372,7 +1369,6 @@ static int smc_connect_rdma(struct smc_sock *smc,
 	return 0;
 connect_abort:
 	smc_conn_abort(smc, ini->first_contact_local);
-	mutex_unlock(&smc_client_lgr_pending);
 	smc->connect_nonblock = 0;
 
 	return reason_code;
@@ -2464,7 +2460,8 @@ static void smc_listen_work(struct work_struct *work)
 	if (rc)
 		goto out_decl;
 
-	mutex_lock(&smc_server_lgr_pending);
+	if (ini->is_smcd)
+		mutex_lock(&smc_server_lgr_pending);
 	smc_close_init(new_smc);
 	smc_rx_init(new_smc);
 	smc_tx_init(new_smc);
@@ -2502,7 +2499,6 @@ static void smc_listen_work(struct work_struct *work)
 					    ini->first_contact_local, ini);
 		if (rc)
 			goto out_unlock;
-		mutex_unlock(&smc_server_lgr_pending);
 	}
 	smc_conn_save_peer_info(new_smc, cclc);
 	smc_listen_out_connected(new_smc);
@@ -2510,7 +2506,8 @@ static void smc_listen_work(struct work_struct *work)
 	goto out_free;
 
 out_unlock:
-	mutex_unlock(&smc_server_lgr_pending);
+	if (ini->is_smcd)
+		mutex_unlock(&smc_server_lgr_pending);
 out_decl:
 	smc_listen_decline(new_smc, rc, ini ? ini->first_contact_local : 0,
 			   proposal_version);
