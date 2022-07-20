sugarfillet/linux

commit 34a7935182584f2261330caab47d8dde90071a91

vfio: vfio fixups for idxd mdev
ANBZ: #1087

commit b7ee3b100a8681e753ed8b3c599e6f6fe8980f49 intel-github.

Intel-SIG: commit b7ee3b100a86 vfio: vfio fixups for idxd mdev.
SIOV VDCM feature backporting.

Signed-off-by: Dave Jiang <dave.jiang@intel.com>
[ Fengqian Gao: amend commit log ]
Signed-off-by: Fengqian Gao <fengqian.gao@intel.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

drivers/vfio/pci/vfio_pci.c
@@ -427,7 +427,7 @@ unlock:
 
 #define DMA_FAULT_RING_LENGTH 512
 
-static int vfio_pci_dma_fault_init(struct vfio_pci_device *vdev)
+int vfio_pci_dma_fault_init(struct vfio_pci_device *vdev,  bool register_fault)
 {
 	struct vfio_region_dma_fault *header;
 	size_t size;
@@ -461,17 +461,20 @@ static int vfio_pci_dma_fault_init(struct vfio_pci_device *vdev)
 	header->nb_entries = DMA_FAULT_RING_LENGTH;
 	header->offset = PAGE_SIZE;
 
-	ret = iommu_register_device_fault_handler(&vdev->pdev->dev,
-					vfio_pci_iommu_dev_fault_handler,
-					vdev);
-	if (ret) /* the dma fault region is freed in vfio_pci_disable() */
-		goto out;
+	if (register_fault) {
+		ret = iommu_register_device_fault_handler(&vdev->pdev->dev,
+							  vfio_pci_iommu_dev_fault_handler,
+							  vdev);
+		if (ret) /* the dma fault region is freed in vfio_pci_disable() */
+			goto out;
+	}
 
 	return 0;
 out:
 	kfree(vdev->fault_pages);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(vfio_pci_dma_fault_init);
 
 static int vfio_pci_enable(struct vfio_pci_device *vdev)
 {
@@ -571,7 +574,7 @@ static int vfio_pci_enable(struct vfio_pci_device *vdev)
 		}
 	}
 
-	ret = vfio_pci_dma_fault_init(vdev);
+	ret = vfio_pci_dma_fault_init(vdev, true);
 	if (ret)
 		goto disable_exit;
 

drivers/vfio/pci/vfio_pci_intrs.c
@@ -636,7 +636,7 @@ static int vfio_pci_set_req_trigger(struct vfio_pci_device *vdev,
 					       count, flags, data);
 }
 
-static int vfio_pci_set_ext_irq_trigger(struct vfio_pci_device *vdev,
+int vfio_pci_set_ext_irq_trigger(struct vfio_pci_device *vdev,
 					unsigned int index, unsigned int start,
 					unsigned int count, uint32_t flags,
 					void *data)
@@ -653,6 +653,7 @@ static int vfio_pci_set_ext_irq_trigger(struct vfio_pci_device *vdev,
 	return vfio_pci_set_ctx_trigger_single(&vdev->ext_irqs[i].trigger,
 					       count, flags, data);
 }
+EXPORT_SYMBOL_GPL(vfio_pci_set_ext_irq_trigger);
 
 int vfio_pci_set_irqs_ioctl(struct vfio_pci_device *vdev, uint32_t flags,
 			    unsigned index, unsigned start, unsigned count,

drivers/vfio/pci/vfio_pci_private.h
@@ -246,4 +246,9 @@ static inline int vfio_pci_info_zdev_add_caps(struct vfio_pci_device *vdev,
 }
 #endif
 
+extern int vfio_pci_dma_fault_init(struct vfio_pci_device *vdev, bool register_fault);
+extern int vfio_pci_set_ext_irq_trigger(struct vfio_pci_device *vdev,
+					unsigned int index, unsigned int start,
+					unsigned int count, uint32_t flags, void *data);
+
 #endif /* VFIO_PCI_PRIVATE_H */

jeevandeepj994/anolis_perf

commit 98105a608d345e21d292459412dac439e06f2a7e

anolis: vfio/pci: legacy support for vfio noiommu device
ANBZ: #1691

There is no necessary for vfio noiommu device to register
device fault handler. Just skip vfio_pci_dma_fault_init
directly.

Fixes: 34a793518258 (vfio: vfio fixups for idxd mdev)
Link: https://gitee.com/anolis/cloud-kernel/pulls/543
Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>

drivers/vfio/pci/vfio_pci.c
@@ -682,9 +682,11 @@ static int vfio_pci_enable(struct vfio_pci_device *vdev)
 		}
 	}
 
-	ret = vfio_pci_dma_fault_init(vdev, true);
-	if (ret)
-		goto disable_exit;
+	if (pdev->dev.iommu) {
+		ret = vfio_pci_dma_fault_init(vdev, true);
+		if (ret)
+			goto disable_exit;
+	}
 
 	vfio_pci_probe_mmaps(vdev);
 
@@ -710,7 +712,8 @@ static void vfio_pci_disable(struct vfio_pci_device *vdev)
 					VFIO_IRQ_SET_ACTION_TRIGGER,
 					vdev->irq_type, 0, 0, NULL);
 
-	WARN_ON(iommu_unregister_device_fault_handler(&vdev->pdev->dev));
+	if (pdev->dev.iommu)
+		WARN_ON(iommu_unregister_device_fault_handler(&vdev->pdev->dev));
 
 	if (vdev->ext_irqs) {
 		for (i = 0; i < vdev->num_ext_irqs; i++)