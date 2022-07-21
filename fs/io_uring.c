3f9df0ab75d97552cbe5e16e73692c12086043bb
Baolin Wang <baolin.wang@linux.alibaba.com>
2022-07-21 19:37:01 +0800

    anolis: riscv: Fix building issues
    ANBZ: #3085
    
    Fix building issues on RISC-V platform.
    
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -10046,7 +10046,9 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 		sqd = *per_cpu_ptr(percpu_sqd, cpu);
 		io_sq_thread_park(sqd);
 		sqd->thread->flags &= ~PF_NO_SETAFFINITY;
+#ifdef CONFIG_CGROUP_CPUACCT
 		ret = io_sq_attach_cpu_cgroup(sqd->thread);
+#endif
 		sqd->thread->flags |= PF_NO_SETAFFINITY;
 		io_sq_thread_unpark(sqd);
 		mutex_unlock(&percpu_sqd_lock);
