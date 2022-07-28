55ef8399f44797c91098fe4a697a208139b5c1c4
John Garry <john.garry@huawei.com>
2022-07-28 13:34:19 +0800

    iova: Make has_iova_flush_queue() private
    ANBZ: #1940
    
    commit 9cc0aaeb96e7f894d4735f069174948c1516fea7 upstream.
    
    Function has_iova_flush_queue() has no users outside iova.c, so make it
    private.
    
    Intel-SIG: commit 9cc0aaeb96e7 iova: Make has_iova_flush_queue() private.
    
    Signed-off-by: John Garry <john.garry@huawei.com>
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/1609940111-28563-2-git-send-email-john.garry@huawei.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    [ Fengqian Gao: amend commit log ]
    Signed-off-by: Fengqian Gao <fengqian.gao@intel.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/658
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- include/linux/iova.h
+++ include/linux/iova.h
@@ -152,7 +152,6 @@ struct iova *reserve_iova(struct iova_domain *iovad, unsigned long pfn_lo,
 	unsigned long pfn_hi);
 void init_iova_domain(struct iova_domain *iovad, unsigned long granule,
 	unsigned long start_pfn);
-bool has_iova_flush_queue(struct iova_domain *iovad);
 int init_iova_flush_queue(struct iova_domain *iovad,
 			  iova_flush_cb flush_cb, iova_entry_dtor entry_dtor);
 struct iova *find_iova(struct iova_domain *iovad, unsigned long pfn);
@@ -214,11 +213,6 @@ static inline void init_iova_domain(struct iova_domain *iovad,
 				    unsigned long granule,
 				    unsigned long start_pfn)
 {
-}
-
-static inline bool has_iova_flush_queue(struct iova_domain *iovad)
-{
-	return false;
 }
 
 static inline int init_iova_flush_queue(struct iova_domain *iovad,
