jeevandeepj994/anolis_perf

commit cb7d7bec75075423e466cabd2f3aab8c286390a7

anolis: fuse: Introduce fd attach ioctl to retrieve a fuse device
ANBZ: #1576

Many userspace filesystems based on linux fuse driver, suffer from lacking of the
failover capability when the fuse daemon goes down. Because the fuse device of
the daemon can only be created when the filesystem is mounted, and be closed
after the filesystem is down, the new daemon can not create another fuse device
in the fuse connection even the mountpoint is still there.

This patch introduces a new fd attachment ioctl. The daemon will open /dev/fuse
to get a file descriptor, before calling attach ioctl on this fd, together with
a tag string parameter. The tag parameter is used to find the correct mountpoint,
a.k.a fuse connection. Then fuse driver will allocate a new fuse device to the
file and link the fuse device to the fuse connection devices list.

Signed-off-by: Ma Jie Yue <majieyue@linux.alibaba.com>
[ joe: add OPT_TAG and re-write ioctl and fuse_resend_init() ]
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Jeffle Xu <jefflexu@linux.alibaba.com>
Acked-by: Gao Xiang <hsiangkao@linux.alibaba.com>

include/uapi/linux/fuse.h
@@ -791,6 +791,13 @@ struct fuse_ioctl_out {
 	uint32_t	out_iovs;
 };
 
+#define FUSE_TAG_NAME_MAX		128
+
+struct fuse_ioctl_attach {
+	unsigned char	tag[FUSE_TAG_NAME_MAX];
+	uint64_t		dev;
+};
+
 struct fuse_poll_in {
 	uint64_t	fh;
 	uint64_t	kh;
@@ -905,6 +912,7 @@ struct fuse_notify_retrieve_in {
 #define FUSE_DEV_IOC_CLONE		_IOR(FUSE_DEV_IOC_MAGIC, 0, uint32_t)
 #define FUSE_DEV_IOC_PASSTHROUGH_OPEN_V0	_IOW(FUSE_DEV_IOC_MAGIC, 100, uint32_t)
 #define FUSE_DEV_IOC_PASSTHROUGH_WRITE_OPEN_V0	_IOW(FUSE_DEV_IOC_MAGIC, 101, uint32_t)
+#define FUSE_DEV_IOC_ATTACH			_IOWR(FUSE_DEV_IOC_MAGIC, 200, uint32_t)
 
 struct fuse_lseek_in {
 	uint64_t	fh;

fs/fuse/dev.c
@@ -610,6 +610,44 @@ static int fuse_simple_notify_reply(struct fuse_mount *fm,
 	return err;
 }
 
+int fuse_request_queue(struct fuse_mount *fm, struct fuse_args *args)
+{
+	struct fuse_req *req;
+	struct fuse_conn *fc = fm->fc;
+	struct fuse_iqueue *fiq = &fc->iq;
+	int err = 0;
+
+	req = fuse_get_req(fm, false);
+	if (IS_ERR(req))
+		return PTR_ERR(req);
+
+	fuse_args_to_req(req, args);
+
+	spin_lock(&fc->lock);
+	if (!fc->connected) {
+		err = -ENOTCONN;
+		fuse_put_request(req);
+		goto out;
+	}
+
+	__set_bit(FR_ISREPLY, &req->flags);
+
+	spin_lock(&fiq->lock);
+	if (!fiq->connected) {
+		err = -ENODEV;
+		spin_unlock(&fiq->lock);
+		fuse_put_request(req);
+		goto out;
+	}
+
+	req->in.h.unique = fuse_get_unique(fiq);
+	queue_request_and_unlock(fiq, req);
+out:
+	spin_unlock(&fc->lock);
+	return err;
+}
+EXPORT_SYMBOL_GPL(fuse_request_queue);
+
 /*
  * Lock the request.  Up to the next unlock_request() there mustn't be
  * anything that could cause a page-fault.  If the request was already
@@ -2350,6 +2388,20 @@ static int fuse_device_clone(struct fuse_conn *fc, struct file *new)
 	return 0;
 }
 
+void fuse_reset_conn(struct fuse_conn *fc)
+{
+	struct fuse_iqueue *fiq;
+
+	spin_lock(&fc->lock);
+	fiq = &fc->iq;
+	spin_lock(&fiq->lock);
+	fiq->connected = 1;
+	spin_unlock(&fiq->lock);
+	fc->connected = 1;
+	spin_unlock(&fc->lock);
+}
+EXPORT_SYMBOL_GPL(fuse_reset_conn);
+
 static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 			   unsigned long arg)
 {
@@ -2357,6 +2409,9 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 	int fd;
 	struct fuse_dev *fud = NULL;
 	bool passthrough_write_only = false;
+	struct fuse_ioctl_attach attach_info;
+	struct fuse_conn *fc;
+	struct fuse_mount *fm, *tmp_fm;
 
 	switch (cmd) {
 	case FUSE_DEV_IOC_CLONE:
@@ -2395,6 +2450,80 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 				res = fuse_passthrough_open(fud, fd,
 						passthrough_write_only);
 		}
+		break;
+	case FUSE_DEV_IOC_ATTACH:
+		if (copy_from_user(&attach_info, (__u32 __user *)arg,
+				   sizeof(attach_info))) {
+			res = -EFAULT;
+			goto out;
+		}
+
+		if (attach_info.tag[0] == '\0') {
+			res = -EINVAL;
+			goto out;
+		}
+
+		if (!file) {
+			res = -EBADF;
+			goto out;
+		}
+
+		if (file->f_op != &fuse_dev_operations ||
+		    file->private_data) {
+			res = -EBADF;
+			goto out;
+		}
+
+		res = -EINVAL;
+		mutex_lock(&fuse_mutex);
+		list_for_each_entry(fc, &fuse_conn_list, entry) {
+			if (strncmp(fc->tag, attach_info.tag,
+				    FUSE_TAG_NAME_MAX - 1))
+				continue;
+
+			attach_info.dev = fc->dev;
+			if (copy_to_user((void __user *)arg, &attach_info,
+					 sizeof(attach_info))) {
+				res = -EFAULT;
+				mutex_unlock(&fuse_mutex);
+				goto out;
+			}
+
+			fud = fuse_dev_alloc_install(fc);
+			if (!fud) {
+				res = -ENOMEM;
+				mutex_unlock(&fuse_mutex);
+				goto out;
+			}
+
+			down_read(&fc->killsb);
+			if (list_empty(&fc->mounts)) {
+				res = -EINVAL;
+				up_read(&fc->killsb);
+				fuse_dev_free(fud);
+				mutex_unlock(&fuse_mutex);
+				goto out;
+			}
+
+			file->private_data = fud;
+			atomic_inc(&fc->dev_count);
+			tmp_fm = list_first_entry(&fc->mounts,
+						  struct fuse_mount,
+						  fc_entry);
+			/* Get the main fuse_mount through super block to avoid
+			 * dependence on list fc->mounts sequence
+			 */
+			fm = get_fuse_mount_super(tmp_fm->sb);
+			up_read(&fc->killsb);
+
+			fuse_reset_conn(fc);
+			mutex_unlock(&fuse_mutex);
+			fuse_resend_init(fm);
+			res = 0;
+			goto out;
+		}
+		mutex_unlock(&fuse_mutex);
+out:
 		break;
 	default:
 		res = -ENOTTY;

fs/fuse/fuse_i.h
@@ -518,11 +518,13 @@ static inline bool fuse_is_inode_dax_mode(enum fuse_dax_mode mode)
 
 struct fuse_fs_context {
 	int fd;
+	const char *tag;
 	unsigned int rootmode;
 	kuid_t user_id;
 	kgid_t group_id;
 	bool is_bdev:1;
 	bool fd_present:1;
+	bool tag_present:1;
 	bool rootmode_present:1;
 	bool user_id_present:1;
 	bool group_id_present:1;
@@ -830,6 +832,9 @@ struct fuse_conn {
 	/** Optional io metrics counter */
 	struct fuse_io_counter *io_counter;
 
+	/* fuse connection tag */
+	char tag[FUSE_TAG_NAME_MAX];
+
 #ifdef CONFIG_FUSE_DAX
 	/* Dax mode */
 	enum fuse_dax_mode dax_mode;
@@ -955,6 +960,10 @@ struct fuse_forget_link *fuse_alloc_forget(void);
 struct fuse_forget_link *fuse_dequeue_forget(struct fuse_iqueue *fiq,
 					     unsigned int max,
 					     unsigned int *countp);
+/**
+ * Send FUSE_INIT command
+ */
+void fuse_queue_init(struct fuse_iqueue *fiq, struct fuse_req *req);
 
 /*
  * Initialize READ or READDIR request
@@ -1056,6 +1065,7 @@ void __exit fuse_ctl_cleanup(void);
 ssize_t fuse_simple_request(struct fuse_mount *fm, struct fuse_args *args);
 int fuse_simple_background(struct fuse_mount *fm, struct fuse_args *args,
 			   gfp_t gfp_flags);
+int fuse_request_queue(struct fuse_mount *fm, struct fuse_args *args);
 
 /**
  * End a finished request
@@ -1064,6 +1074,7 @@ void fuse_request_end(struct fuse_req *req);
 
 /* Abort all requests */
 void fuse_abort_conn(struct fuse_conn *fc);
+void fuse_reset_conn(struct fuse_conn *fc);
 void fuse_wait_aborted(struct fuse_conn *fc);
 
 /* Flush all requests in processing queue */
@@ -1116,6 +1127,7 @@ struct fuse_dev *fuse_dev_alloc(void);
 void fuse_dev_install(struct fuse_dev *fud, struct fuse_conn *fc);
 void fuse_dev_free(struct fuse_dev *fud);
 void fuse_send_init(struct fuse_mount *fm);
+void fuse_resend_init(struct fuse_mount *fm);
 
 /**
  * Fill in superblock and initialize fuse connection

fs/fuse/inode.c
@@ -505,6 +505,7 @@ enum {
 	OPT_SOURCE,
 	OPT_SUBTYPE,
 	OPT_FD,
+	OPT_TAG,
 	OPT_ROOTMODE,
 	OPT_USER_ID,
 	OPT_GROUP_ID,
@@ -518,6 +519,7 @@ enum {
 static const struct fs_parameter_spec fuse_fs_parameters[] = {
 	fsparam_string	("source",		OPT_SOURCE),
 	fsparam_u32	("fd",			OPT_FD),
+	fsparam_string	("tag",			OPT_TAG),
 	fsparam_u32oct	("rootmode",		OPT_ROOTMODE),
 	fsparam_u32	("user_id",		OPT_USER_ID),
 	fsparam_u32	("group_id",		OPT_GROUP_ID),
@@ -568,6 +570,12 @@ static int fuse_parse_param(struct fs_context *fc, struct fs_parameter *param)
 	case OPT_FD:
 		ctx->fd = result.uint_32;
 		ctx->fd_present = true;
+		break;
+
+	case OPT_TAG:
+		ctx->tag = param->string;
+		ctx->tag_present = true;
+		param->string = NULL;
 		break;
 
 	case OPT_ROOTMODE:
@@ -622,6 +630,7 @@ static void fuse_free_fc(struct fs_context *fc)
 
 	if (ctx) {
 		kfree(ctx->subtype);
+		kfree(ctx->tag);
 		kfree(ctx);
 	}
 }
@@ -1104,12 +1113,10 @@ static void process_init_reply(struct fuse_mount *fm, struct fuse_args *args,
 	wake_up_all(&fc->blocked_waitq);
 }
 
-void fuse_send_init(struct fuse_mount *fm)
+static void fuse_prepare_send_init(struct fuse_mount *fm,
+				   struct fuse_init_args *ia)
 {
-	struct fuse_init_args *ia;
 	u64 flags;
-
-	ia = kzalloc(sizeof(*ia), GFP_KERNEL | __GFP_NOFAIL);
 
 	ia->in.major = FUSE_KERNEL_VERSION;
 	ia->in.minor = FUSE_KERNEL_MINOR_VERSION;
@@ -1151,11 +1158,30 @@ void fuse_send_init(struct fuse_mount *fm)
 	ia->args.force = true;
 	ia->args.nocreds = true;
 	ia->args.end = process_init_reply;
+}
+
+void fuse_send_init(struct fuse_mount *fm)
+{
+	struct fuse_init_args *ia;
+
+	ia = kzalloc(sizeof(*ia), GFP_KERNEL | __GFP_NOFAIL);
+	fuse_prepare_send_init(fm, ia);
 
 	if (fuse_simple_background(fm, &ia->args, GFP_KERNEL) != 0)
 		process_init_reply(fm, &ia->args, -ENOTCONN);
 }
 EXPORT_SYMBOL_GPL(fuse_send_init);
+
+void fuse_resend_init(struct fuse_mount *fm)
+{
+	struct fuse_init_args *ia;
+
+	ia = kzalloc(sizeof(*ia), GFP_KERNEL | __GFP_NOFAIL);
+	fuse_prepare_send_init(fm, ia);
+
+	if (fuse_request_queue(fm, &ia->args))
+		process_init_reply(fm, &ia->args, -ENOTCONN);
+}
 
 static int free_fuse_passthrough(int id, void *p, void *data)
 {
@@ -1463,6 +1489,22 @@ int fuse_fill_super_common(struct super_block *sb, struct fuse_fs_context *ctx)
 }
 EXPORT_SYMBOL_GPL(fuse_fill_super_common);
 
+static bool fuse_find_instance(const char *tag)
+{
+	struct fuse_conn *fc;
+
+	mutex_lock(&fuse_mutex);
+	list_for_each_entry(fc, &fuse_conn_list, entry) {
+		if (!strncmp(fc->tag, tag, FUSE_TAG_NAME_MAX - 1)) {
+			mutex_unlock(&fuse_mutex);
+			return true;
+		}
+	}
+	mutex_unlock(&fuse_mutex);
+
+	return false;
+}
+
 static int fuse_fill_super(struct super_block *sb, struct fs_context *fsc)
 {
 	struct fuse_fs_context *ctx = fsc->fs_private;
@@ -1471,10 +1513,20 @@ static int fuse_fill_super(struct super_block *sb, struct fs_context *fsc)
 	struct fuse_conn *fc;
 	struct fuse_mount *fm;
 
+	if (ctx->tag_present) {
+		if (fuse_find_instance(ctx->tag)) {
+			err = -EEXIST;
+			pr_err("fuse: tag %s already exist\n", ctx->tag);
+			goto err;
+		}
+	}
+
 	err = -EINVAL;
 	file = fget(ctx->fd);
-	if (!file)
+	if (!file) {
+		pr_err("fuse: invalid fd option\n");
 		goto err;
+	}
 
 	/*
 	 * Require mount to happen from the same user namespace which
@@ -1497,6 +1549,8 @@ static int fuse_fill_super(struct super_block *sb, struct fs_context *fsc)
 	}
 
 	fuse_conn_init(fc, fm, sb->s_user_ns, &fuse_dev_fiq_ops, NULL);
+	if (ctx->tag_present)
+		strncpy(fc->tag, ctx->tag, FUSE_TAG_NAME_MAX - 1);
 	fc->release = fuse_free_conn;
 
 	sb->s_fs_info = fm;

commit 4d7cb46664871fbd903564196a56cd6dfd8d36bf

anolis: correct fd attach ioctl arg type
ANBZ: #1754

fd attach ioctl arg type should be fuse_ioctl_attach, but it mistakenly
used uint32_t before, so fix it.

Fixes: cb7d7bec7507 ("anolis: fuse: Introduce fd attach ioctl to retrieve a fuse device")
Link: https://gitee.com/anolis/cloud-kernel/pulls/586
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Jeffle Xu <jefflexu@linux.alibaba.com>
Acked-by: Gao Xiang <hsiangkao@linux.alibaba.com>

include/uapi/linux/fuse.h
@@ -912,7 +912,7 @@ struct fuse_notify_retrieve_in {
 #define FUSE_DEV_IOC_CLONE		_IOR(FUSE_DEV_IOC_MAGIC, 0, uint32_t)
 #define FUSE_DEV_IOC_PASSTHROUGH_OPEN_V0	_IOW(FUSE_DEV_IOC_MAGIC, 100, uint32_t)
 #define FUSE_DEV_IOC_PASSTHROUGH_WRITE_OPEN_V0	_IOW(FUSE_DEV_IOC_MAGIC, 101, uint32_t)
-#define FUSE_DEV_IOC_ATTACH			_IOWR(FUSE_DEV_IOC_MAGIC, 200, uint32_t)
+#define FUSE_DEV_IOC_ATTACH		_IOWR(FUSE_DEV_IOC_MAGIC, 200, struct fuse_ioctl_attach)
 
 struct fuse_lseek_in {
 	uint64_t	fh;