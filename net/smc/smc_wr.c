0ba7f424fdd239a3a682d31ab76407879f9a5b16
Guangguan Wang <guangguan.wang@linux.alibaba.com>
2022-08-02 16:22:37 +0800

    anolis: net/smc: Introduce smc dim
    ANBZ: #1879
    
    Dynamic interrupt moderation can coalesce interrupts and reduce cpu
    utilization.
    
    Nginx benchmark shows 13.7% improvement in throughput:
    Server test command:
    smc_run nginx
    Client test command:
    smc_run /opt/wrk/wrk http://ip:port -t 32 -c 992 -d 30 --latency
    
    Before:
    Requests/sec: 2467285.48
    
    After:
    Requests/sec: 2804945.57
    
    Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/623
    Reviewed-by: D. Wythe <alibuda@linux.alibaba.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>

--- net/smc/smc_wr.c
+++ net/smc/smc_wr.c
@@ -30,6 +30,7 @@
 
 #include "smc.h"
 #include "smc_wr.h"
+#include "smc_dim.h"
 
 #define SMC_WR_MAX_POLL_CQE 10	/* max. # of compl. queue elements in 1 poll */
 
@@ -459,7 +460,7 @@ static void smc_wr_tasklet_fn(struct tasklet_struct *t)
 {
 	struct smc_ib_cq *smcibcq = from_tasklet(smcibcq, t, tasklet);
 	struct ib_wc wc[SMC_WR_MAX_POLL_CQE];
-	int i, rc;
+	int i, rc, completed = 0;
 
 again:
 	do {
@@ -480,6 +481,9 @@ again:
 				break;
 			}
 		}
+
+		if (rc > 0)
+			completed += rc;
 	} while (rc > 0);
 
 	/* With IB_CQ_REPORT_MISSED_EVENTS, if ib_req_notify_cq() returns 0,
@@ -490,6 +494,9 @@ again:
 			     IB_CQ_NEXT_COMP |
 			     IB_CQ_REPORT_MISSED_EVENTS) > 0)
 		goto again;
+
+	if (smcibcq->ib_cq->dim)
+		smc_dim(smcibcq->ib_cq->dim, completed);
 }
 
 void smc_wr_cq_handler(struct ib_cq *ib_cq, void *cq_context)
