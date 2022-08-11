jeevandeepj994/anolis_perf

commit 4995c319c5b9a6586aa06d2247ff0c4609c0673b

anolis: mm: fcm: introduce fast copy mm interface
ANBZ: #1745

Under normal circumstances, parent will be blocked synchronously in
copy_mm() when fork(2) is called, and the duration may be long with
large memory usage. It will cause the parent out of service from the
user's view. For example, a redis instance creates a memory snapshot
based on fork(2), it's trival to produce latency spikes.

Fast copy mm is designed to reduce block duration when parent process
calls fork(2). It can be controlled by each memcg and can be switched
on/off in the fly. It's disabled by default, user can enable it by:

  # echo 1 > /path/to/memcg/memory.fast_copy_mm

And disable it by:

  # echo 0 > /path/to/memcg/memory.fast_copy_mm

Child memcg will inherit parent memcg's configuration.

This interfaces the interfaces and stubs of fast copy mm, which split
the copy_page_range() function into fast path and slow path. The parent
is expected to excute only the fast path in fork(2), while the child
process will excute the slow path before return to user mode.

Signed-off-by: Gang Deng <gavin.dg@linux.alibaba.com>
Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/578

include/linux/mm.h
@@ -3357,5 +3357,53 @@ static inline int seal_check_future_write(int seals, struct vm_area_struct *vma)
 
 extern int sysctl_enable_multithread_ra_boost;
 
+#ifdef CONFIG_FAST_COPY_MM
+#define FCM_CANDIDATE		0
+DECLARE_STATIC_KEY_FALSE(fcm_enabled_key);
+static inline bool fcm_enabled(void)
+{
+	return static_branch_unlikely(&fcm_enabled_key);
+}
+int fcm_cpr_fast(struct vm_area_struct *vma, struct vm_area_struct *mpnt);
+void fcm_cpr_bind(struct mm_struct *oldmm, struct mm_struct *mm, int err);
+void fcm_cpr_rest(void);
+void fcm_cpr_done(struct mm_struct *mm, bool r, bool l);
+
+bool maybe_pmd_fcm(pmd_t pmd);
+void fcm_fixup_pmd(struct vm_area_struct *mpnt, pmd_t *pmd, unsigned long addr);
+void fcm_fixup_vma(struct vm_area_struct *mpnt);
+#else
+static inline bool fcm_enabled(void)
+{
+	return false;
+}
+static inline int fcm_cpr_fast(struct vm_area_struct *vma,
+			       struct vm_area_struct *mpnt)
+{
+	return -EOPNOTSUPP;
+}
+static inline void fcm_cpr_bind(struct mm_struct *oldmm, struct mm_struct *mm,
+				int err)
+{
+}
+static inline void fcm_cpr_rest(void)
+{
+}
+static inline void fcm_cpr_done(struct mm_struct *mm, bool r, bool l)
+{
+}
+static inline bool maybe_pmd_fcm(pmd_t pmd)
+{
+	return false;
+}
+static inline void fcm_fixup_pmd(struct vm_area_struct *mpnt, pmd_t *pmd,
+				 unsigned long addr)
+{
+}
+static inline void fcm_fixup_vma(struct vm_area_struct *mpnt)
+{
+}
+#endif
+
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */

kernel/fork.c
@@ -362,6 +362,8 @@ struct vm_area_struct *vm_area_dup(struct vm_area_struct *orig)
 {
 	struct vm_area_struct *new = kmem_cache_alloc(vm_area_cachep, GFP_KERNEL);
 
+	fcm_fixup_vma(orig);
+
 	if (new) {
 		ASSERT_EXCLUSIVE_WRITER(orig->vm_flags);
 		ASSERT_EXCLUSIVE_WRITER(orig->vm_file);
@@ -481,12 +483,23 @@ static __latent_entropy int dup_mmap(struct mm_struct *mm,
 	int retval;
 	unsigned long charge;
 	LIST_HEAD(uf);
+#ifdef CONFIG_FAST_COPY_MM
+	unsigned long fast_copy_mm;
+#endif
 
 	uprobe_start_dup_mmap();
 	if (mmap_write_lock_killable(oldmm)) {
 		retval = -EINTR;
 		goto fail_uprobe_end;
 	}
+#ifdef CONFIG_FAST_COPY_MM
+	/* Get task_fast_copy_mm with oldmm's mmap write lock hold. */
+	rcu_read_lock();
+	fast_copy_mm = task_fast_copy_mm(current);
+	if (fast_copy_mm)
+		set_bit(FCM_CANDIDATE, &oldmm->fcm_flags);
+	rcu_read_unlock();
+#endif
 	flush_cache_dup_mm(oldmm);
 	uprobe_dup_mmap(oldmm, mm);
 	/*
@@ -596,8 +609,16 @@ static __latent_entropy int dup_mmap(struct mm_struct *mm,
 		rb_parent = &tmp->vm_rb;
 
 		mm->map_count++;
-		if (!(tmp->vm_flags & VM_WIPEONFORK))
+		if (!(tmp->vm_flags & VM_WIPEONFORK)) {
+#ifdef CONFIG_FAST_COPY_MM
+			if (fast_copy_mm)
+				retval = fcm_cpr_fast(tmp, mpnt);
+			else
+				retval = copy_page_range(tmp, mpnt);
+#else
 			retval = copy_page_range(tmp, mpnt);
+#endif
+		}
 
 		if (tmp->vm_ops && tmp->vm_ops->open)
 			tmp->vm_ops->open(tmp);
@@ -610,6 +631,10 @@ static __latent_entropy int dup_mmap(struct mm_struct *mm,
 out:
 	mmap_write_unlock(mm);
 	flush_tlb_mm(oldmm);
+#ifdef CONFIG_FAST_COPY_MM
+	if (fast_copy_mm)
+		fcm_cpr_bind(oldmm, mm, retval);
+#endif
 	mmap_write_unlock(oldmm);
 	dup_userfaultfd_complete(&uf);
 fail_uprobe_end:
@@ -692,6 +717,9 @@ void __mmdrop(struct mm_struct *mm)
 	BUG_ON(mm == &init_mm);
 	WARN_ON_ONCE(mm == current->mm);
 	WARN_ON_ONCE(mm == current->active_mm);
+#ifdef CONFIG_FAST_COPY_MM
+	BUG_ON(mm->fcm_mm);
+#endif
 	mm_free_pgd(mm);
 	destroy_context(mm);
 	mmu_notifier_subscriptions_destroy(mm);
@@ -1048,6 +1076,11 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 #endif
 	mm_init_uprobes_state(mm);
 	hugetlb_count_init(mm);
+
+#ifdef CONFIG_FAST_COPY_MM
+	mm->fcm_mm = NULL;
+	mm->fcm_flags = 0;
+#endif
 
 	if (current->mm) {
 		mm->flags = current->mm->flags & MMF_INIT_MASK;
@@ -2358,6 +2391,12 @@ bad_fork_cleanup_namespaces:
 	exit_task_namespaces(p);
 bad_fork_cleanup_mm:
 	if (p->mm) {
+#ifdef CONFIG_FAST_COPY_MM
+		if (p->mm->fcm_mm) {
+			WARN_ON_ONCE(clone_flags & CLONE_VM);
+			fcm_cpr_done(p->mm, true, false);
+		}
+#endif
 		mm_clear_owner(p->mm, p);
 		mmput(p->mm);
 	}

mm/madvise.c
@@ -927,6 +927,8 @@ static long
 madvise_vma(struct vm_area_struct *vma, struct vm_area_struct **prev,
 		unsigned long start, unsigned long end, int behavior)
 {
+	fcm_fixup_vma(vma);
+
 	switch (behavior) {
 	case MADV_REMOVE:
 		return madvise_remove(vma, prev, start, end);

mm/mempolicy.c
@@ -655,6 +655,8 @@ unsigned long change_prot_numa(struct vm_area_struct *vma,
 {
 	int nr_updated;
 
+	fcm_fixup_vma(vma);
+
 	nr_updated = change_protection(vma, addr, end, PAGE_NONE, MM_CP_PROT_NUMA);
 	if (nr_updated)
 		count_vm_numa_events(NUMA_PTE_UPDATES, nr_updated);

mm/mlock.c
@@ -543,6 +543,8 @@ int mlock_fixup(struct vm_area_struct *vma, struct vm_area_struct **prev,
 		/* don't set VM_LOCKED or VM_LOCKONFAULT and don't count */
 		goto out;
 
+	fcm_fixup_vma(vma);
+
 	pgoff = vma->vm_pgoff + ((start - vma->vm_start) >> PAGE_SHIFT);
 	*prev = vma_merge(mm, *prev, start, end, newflags, vma->anon_vma,
 			  vma->vm_file, pgoff, vma_policy(vma),

mm/mmap.c
@@ -511,6 +511,10 @@ anon_vma_interval_tree_pre_update_vma(struct vm_area_struct *vma)
 {
 	struct anon_vma_chain *avc;
 
+#ifdef CONFIG_FAST_COPY_MM
+	WARN_ON_ONCE(vma->fcm_vma);
+#endif
+
 	list_for_each_entry(avc, &vma->anon_vma_chain, same_vma)
 		anon_vma_interval_tree_remove(avc, &avc->anon_vma->rb_root);
 }
@@ -1172,10 +1176,14 @@ struct vm_area_struct *vma_merge(struct mm_struct *mm,
 	if (vm_flags & VM_SPECIAL)
 		return NULL;
 
+	if (prev)
+		fcm_fixup_vma(prev);
 	next = vma_next(mm, prev);
 	area = next;
 	if (area && area->vm_end == end)		/* cases 6, 7, 8 */
 		next = next->vm_next;
+	if (next)
+		fcm_fixup_vma(next);
 
 	/* verify some invariant that must be enforced by the caller */
 	VM_WARN_ON(prev && addr <= prev->vm_start);
@@ -2444,6 +2452,8 @@ int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 	if (unlikely(anon_vma_prepare(vma)))
 		return -ENOMEM;
 
+	fcm_fixup_vma(vma);
+
 	/*
 	 * vma->vm_start/vm_end cannot change under us because the caller
 	 * is required to hold the mmap_lock in read mode.  We need the
@@ -2523,6 +2533,8 @@ int expand_downwards(struct vm_area_struct *vma,
 	/* We must make sure the anon_vma is allocated. */
 	if (unlikely(anon_vma_prepare(vma)))
 		return -ENOMEM;
+
+	fcm_fixup_vma(vma);
 
 	/*
 	 * vma->vm_start/vm_end cannot change under us because the caller
@@ -2702,6 +2714,7 @@ detach_vmas_to_be_unmapped(struct mm_struct *mm, struct vm_area_struct *vma,
 	insertion_point = (prev ? &prev->vm_next : &mm->mmap);
 	vma->vm_prev = NULL;
 	do {
+		fcm_fixup_vma(vma);
 		vma_rb_erase(vma, &mm->mm_rb);
 		mm->map_count--;
 		tail_vma = vma;
@@ -2739,6 +2752,8 @@ int __split_vma(struct mm_struct *mm, struct vm_area_struct *vma,
 {
 	struct vm_area_struct *new;
 	int err;
+
+	fcm_fixup_vma(vma);
 
 	if (vma->vm_ops && vma->vm_ops->split) {
 		err = vma->vm_ops->split(vma, addr);

mm/mprotect.c
@@ -579,6 +579,7 @@ static int do_mprotect_pkey(unsigned long start, size_t len,
 		unsigned long newflags;
 		int new_vma_pkey;
 
+		fcm_fixup_vma(vma);
 		/* Here we know that vma->vm_start <= nstart < vma->vm_end. */
 
 		/* Does the application expect PROT_READ to imply PROT_EXEC */

mm/mremap.c
@@ -485,6 +485,8 @@ static struct vm_area_struct *vma_to_resize(unsigned long addr,
 	if (!vma || vma->vm_start > addr)
 		return ERR_PTR(-EFAULT);
 
+	fcm_fixup_vma(vma);
+
 	/*
 	 * !old_len is a special case where an attempt is made to 'duplicate'
 	 * a mapping.  This makes no sense for private mappings as it will

commit d8843e0411bd0d37c1110e4b5af5641f09e055fe

anolis: mm: fcm: do not unconditionally call fcm_fixup_vma
ANBZ: #1881

The fcm (fast copy mm) feature places stubs in the mm core path, whose
overhead should be negligible.

However, the fcm_fixup_vma stub is unconditionally called whenever a vma
is about to be changed. This resulted in a performance degradation of up
to 10% for will-it-scale/mmap testcase.

This fixes the performance degradation by adding proper conditions
before calling fcm_fixup_vma.

Fixes: 4995c319c5b9 ("anolis: mm: fcm: introduce fast copy mm interface")
Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
Reviewed-by: zhong jiang <zhongjiang-ali@linux.alibaba.com>
Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/625

include/linux/mm.h
@@ -3372,6 +3372,10 @@ void fcm_cpr_done(struct mm_struct *mm, bool r, bool l);
 bool maybe_pmd_fcm(pmd_t pmd);
 void fcm_fixup_pmd(struct vm_area_struct *mpnt, pmd_t *pmd, unsigned long addr);
 void fcm_fixup_vma(struct vm_area_struct *mpnt);
+#define fcm_sync_vma(mpnt)	do {		\
+	if (unlikely(READ_ONCE(mpnt->fcm_vma)))	\
+		fcm_fixup_vma(mpnt);		\
+} while (0)
 #else
 static inline bool fcm_enabled(void)
 {
@@ -3403,6 +3407,7 @@ static inline void fcm_fixup_pmd(struct vm_area_struct *mpnt, pmd_t *pmd,
 static inline void fcm_fixup_vma(struct vm_area_struct *mpnt)
 {
 }
+#define fcm_sync_vma(mpnt)	do { } while (0)
 #endif
 
 #endif /* __KERNEL__ */

kernel/fork.c
@@ -362,7 +362,7 @@ struct vm_area_struct *vm_area_dup(struct vm_area_struct *orig)
 {
 	struct vm_area_struct *new = kmem_cache_alloc(vm_area_cachep, GFP_KERNEL);
 
-	fcm_fixup_vma(orig);
+	fcm_sync_vma(orig);
 
 	if (new) {
 		ASSERT_EXCLUSIVE_WRITER(orig->vm_flags);

mm/madvise.c
@@ -927,7 +927,7 @@ static long
 madvise_vma(struct vm_area_struct *vma, struct vm_area_struct **prev,
 		unsigned long start, unsigned long end, int behavior)
 {
-	fcm_fixup_vma(vma);
+	fcm_sync_vma(vma);
 
 	switch (behavior) {
 	case MADV_REMOVE:

mm/mempolicy.c
@@ -655,7 +655,7 @@ unsigned long change_prot_numa(struct vm_area_struct *vma,
 {
 	int nr_updated;
 
-	fcm_fixup_vma(vma);
+	fcm_sync_vma(vma);
 
 	nr_updated = change_protection(vma, addr, end, PAGE_NONE, MM_CP_PROT_NUMA);
 	if (nr_updated)

mm/mlock.c
@@ -543,7 +543,7 @@ int mlock_fixup(struct vm_area_struct *vma, struct vm_area_struct **prev,
 		/* don't set VM_LOCKED or VM_LOCKONFAULT and don't count */
 		goto out;
 
-	fcm_fixup_vma(vma);
+	fcm_sync_vma(vma);
 
 	pgoff = vma->vm_pgoff + ((start - vma->vm_start) >> PAGE_SHIFT);
 	*prev = vma_merge(mm, *prev, start, end, newflags, vma->anon_vma,

mm/mmap.c
@@ -1177,13 +1177,13 @@ struct vm_area_struct *vma_merge(struct mm_struct *mm,
 		return NULL;
 
 	if (prev)
-		fcm_fixup_vma(prev);
+		fcm_sync_vma(prev);
 	next = vma_next(mm, prev);
 	area = next;
 	if (area && area->vm_end == end)		/* cases 6, 7, 8 */
 		next = next->vm_next;
 	if (next)
-		fcm_fixup_vma(next);
+		fcm_sync_vma(next);
 
 	/* verify some invariant that must be enforced by the caller */
 	VM_WARN_ON(prev && addr <= prev->vm_start);
@@ -2452,7 +2452,7 @@ int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 	if (unlikely(anon_vma_prepare(vma)))
 		return -ENOMEM;
 
-	fcm_fixup_vma(vma);
+	fcm_sync_vma(vma);
 
 	/*
 	 * vma->vm_start/vm_end cannot change under us because the caller
@@ -2534,7 +2534,7 @@ int expand_downwards(struct vm_area_struct *vma,
 	if (unlikely(anon_vma_prepare(vma)))
 		return -ENOMEM;
 
-	fcm_fixup_vma(vma);
+	fcm_sync_vma(vma);
 
 	/*
 	 * vma->vm_start/vm_end cannot change under us because the caller
@@ -2714,7 +2714,7 @@ detach_vmas_to_be_unmapped(struct mm_struct *mm, struct vm_area_struct *vma,
 	insertion_point = (prev ? &prev->vm_next : &mm->mmap);
 	vma->vm_prev = NULL;
 	do {
-		fcm_fixup_vma(vma);
+		fcm_sync_vma(vma);
 		vma_rb_erase(vma, &mm->mm_rb);
 		mm->map_count--;
 		tail_vma = vma;
@@ -2753,7 +2753,7 @@ int __split_vma(struct mm_struct *mm, struct vm_area_struct *vma,
 	struct vm_area_struct *new;
 	int err;
 
-	fcm_fixup_vma(vma);
+	fcm_sync_vma(vma);
 
 	if (vma->vm_ops && vma->vm_ops->split) {
 		err = vma->vm_ops->split(vma, addr);

mm/mprotect.c
@@ -579,7 +579,7 @@ static int do_mprotect_pkey(unsigned long start, size_t len,
 		unsigned long newflags;
 		int new_vma_pkey;
 
-		fcm_fixup_vma(vma);
+		fcm_sync_vma(vma);
 		/* Here we know that vma->vm_start <= nstart < vma->vm_end. */
 
 		/* Does the application expect PROT_READ to imply PROT_EXEC */

mm/mremap.c
@@ -485,7 +485,7 @@ static struct vm_area_struct *vma_to_resize(unsigned long addr,
 	if (!vma || vma->vm_start > addr)
 		return ERR_PTR(-EFAULT);
 
-	fcm_fixup_vma(vma);
+	fcm_sync_vma(vma);
 
 	/*
 	 * !old_len is a special case where an attempt is made to 'duplicate'