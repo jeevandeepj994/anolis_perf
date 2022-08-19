2b46410a5ef28846e47ab7f4fb83a2e69ccb0659
Gu Mi <gumi@linux.alibaba.com>
2022-08-19 14:39:20 +0800

    scsi: mpt3sas: Handle RDPQ DMA allocation in same 4G region
    ANBZ: #1924
    
    commit 8012209eb26b7819385a6ec6eae4b1d0a0dbe585 upstream.
    
    For INVADER_SERIES, each set of 8 reply queues (0 - 7, 8 - 15,..), and
    for
    VENTURA_SERIES, each set of 16 reply queues (0 - 15, 16 - 31,..) need to
    be
    within the same 4 GB boundary. Driver uses limitation of VENTURA_SERIES
    to
    manage INVADER_SERIES as well. The driver is allocating the DMA able
    memory for RDPQs accordingly.
    
    1) At driver load, set DMA mask to 64 and allocate memory for RDPQs
    
    2) Check if allocated resources for RDPQ are in the same 4GB range
    
    3) If #2 is true, continue with 64 bit DMA and go to #6
    
    4) If #2 is false, then free all the resources from #1
    
    5) Set DMA mask to 32 and allocate RDPQs
    
    6) Proceed with driver loading and other allocations
    
    Link: https://lore.kernel.org/r/1587626596-1044-5-git-send-email
    -suganath-prabu.subramani@broadcom.com
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Suganath Prabu <suganath-prabu.subramani@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Gu Mi <gumi@linux.alibaba.com>

--- drivers/scsi/mpt3sas/mpt3sas_base.h
+++ drivers/scsi/mpt3sas/mpt3sas_base.h
@@ -159,7 +159,14 @@ struct mpt3sas_nvme_cmd {
  * logging format
  */
 #define MPT3SAS_FMT			"%s: "
-
+#define ioc_err(ioc, fmt, ...)						\
+	pr_err("%s: " fmt, (ioc)->name, ##__VA_ARGS__)
+#define ioc_notice(ioc, fmt, ...)					\
+	pr_notice("%s: " fmt, (ioc)->name, ##__VA_ARGS__)
+#define ioc_warn(ioc, fmt, ...)						\
+	pr_warn("%s: " fmt, (ioc)->name, ##__VA_ARGS__)
+#define ioc_info(ioc, fmt, ...)						\
+	pr_info("%s: " fmt, (ioc)->name, ##__VA_ARGS__)
 /*
  *  WarpDrive Specific Log codes
  */
@@ -339,6 +346,7 @@ struct mpt3sas_nvme_cmd {
 #define MFG10_GF0_SINGLE_DRIVE_R0              (0x00000010)
 
 #define VIRTUAL_IO_FAILED_RETRY			(0x32010081)
+#define RDPQ_MAX_INDEX_IN_ONE_CHUNK		16
 
 /* OEM Specific Flags will come from OEM specific header files */
 struct Mpi2ManufacturingPage10_t {
@@ -1001,6 +1009,7 @@ typedef void (*MPT3SAS_FLUSH_RUNNING_CMDS)(struct MPT3SAS_ADAPTER *ioc);
  * @cpu_msix_table: table for mapping cpus to msix index
  * @cpu_msix_table_sz: table size
  * @schedule_dead_ioc_flush_running_cmds: callback to flush pending commands
+ * @use_32bit_dma: Flag to use 32 bit consistent dma mask
  * @scsi_io_cb_idx: shost generated commands
  * @tm_cb_idx: task management commands
  * @scsih_cb_idx: scsih internal commands
@@ -1175,6 +1184,7 @@ struct MPT3SAS_ADAPTER {
 	u32		ioc_reset_count;
 	MPT3SAS_FLUSH_RUNNING_CMDS schedule_dead_ioc_flush_running_cmds;
 	u32             non_operational_loop;
+	bool	use_32bit_dma;
 
 	/* internal commands, callback index */
 	u8		scsi_io_cb_idx;
