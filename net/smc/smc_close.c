863c6c626c8d0174c432cd214fd6edb95be2d41f
Wen Gu <guwen@linux.alibaba.com>
2022-08-23 21:00:09 +0800

    anolis: Revert "anolis: net/smc: Keep first contact clcsock"
    ANBZ: #2003
    
    This reverts commit 54f567f5124636820094f774cb60cd42ccb0c672.
    
    Conflicts:
    	include/net/netns/smc.h
    	net/smc/af_smc.c
    	net/smc/smc_close.c
    	net/smc/smc_sysctl.c
    
    Link: https://gitee.com/anolis/cloud-kernel/pulls/677
    Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>

--- net/smc/smc_close.c
+++ net/smc/smc_close.c
@@ -29,12 +29,10 @@ void smc_clcsock_release(struct smc_sock *smc)
 	    current_work() != &smc->smc_listen_work)
 		cancel_work_sync(&smc->smc_listen_work);
 	down_write(&smc->clcsock_release_lock);
-	/* don't release clcsock for eRDMA */
 	if (smc->clcsock) {
 		tcp = smc->clcsock;
 		smc->clcsock = NULL;
-		if (!smc->keep_clcsock)
-			sock_release(tcp);
+		sock_release(tcp);
 	}
 	up_write(&smc->clcsock_release_lock);
 }
@@ -244,8 +242,7 @@ again:
 			/* actively shutdown clcsock before peer close it,
 			 * prevent peer from entering TIME_WAIT state.
 			 */
-			if (smc->clcsock && smc->clcsock->sk &&
-			    !smc->keep_clcsock) {
+			if (smc->clcsock && smc->clcsock->sk) {
 				rc1 = kernel_sock_shutdown(smc->clcsock,
 							   SHUT_RDWR);
 				rc = rc ? rc : rc1;
