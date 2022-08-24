cb8638fbfeed92502360221f514c33fca1251fc2
Tony Lu <tonylu@linux.alibaba.com>
2022-08-24 14:19:08 +0800

    anolis: Revert "anolis: net/smc: remove locks smc_client_lgr_pending and smc_server_lgr_pending"
    ANBZ: #2003
    
    This reverts commit 616bb8eb7cab515001e4ad242cbd4bee051dfd15.
    
    Link: https://gitee.com/anolis/cloud-kernel/pulls/677
    Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
    Reviewed-by: Wen Gu <guwen@linux.alibaba.com>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -1268,8 +1268,10 @@ static int smc_connect_rdma(struct smc_sock *smc,
 	if (reason_code)
 		return reason_code;
 
+	mutex_lock(&smc_client_lgr_pending);
 	reason_code = smc_conn_create(smc, ini);
 	if (reason_code) {
+		mutex_unlock(&smc_client_lgr_pending);
 		return reason_code;
 	}
 
@@ -1366,6 +1368,7 @@ static int smc_connect_rdma(struct smc_sock *smc,
 		if (reason_code)
 			goto connect_abort;
 	}
+	mutex_unlock(&smc_client_lgr_pending);
 
 	smc_copy_sock_settings_to_clc(smc);
 	smc->connect_nonblock = 0;
@@ -1375,6 +1378,7 @@ static int smc_connect_rdma(struct smc_sock *smc,
 	return 0;
 connect_abort:
 	smc_conn_abort(smc, ini->first_contact_local);
+	mutex_unlock(&smc_client_lgr_pending);
 	smc->connect_nonblock = 0;
 
 	return reason_code;
@@ -2467,8 +2471,7 @@ static void smc_listen_work(struct work_struct *work)
 	if (rc)
 		goto out_decl;
 
-	if (ini->is_smcd)
-		mutex_lock(&smc_server_lgr_pending);
+	mutex_lock(&smc_server_lgr_pending);
 	smc_close_init(new_smc);
 	smc_rx_init(new_smc);
 	smc_tx_init(new_smc);
@@ -2506,6 +2509,7 @@ static void smc_listen_work(struct work_struct *work)
 					    ini->first_contact_local, ini);
 		if (rc)
 			goto out_unlock;
+		mutex_unlock(&smc_server_lgr_pending);
 	}
 	smc_conn_save_peer_info(new_smc, cclc);
 	smc_listen_out_connected(new_smc);
@@ -2513,8 +2517,7 @@ static void smc_listen_work(struct work_struct *work)
 	goto out_free;
 
 out_unlock:
-	if (ini->is_smcd)
-		mutex_unlock(&smc_server_lgr_pending);
+	mutex_unlock(&smc_server_lgr_pending);
 out_decl:
 	smc_listen_decline(new_smc, rc, ini ? ini->first_contact_local : 0,
 			   proposal_version);
