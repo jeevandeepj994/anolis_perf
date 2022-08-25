b56e2f16a568ca2cc256b6891e7852a799b218b2
Xu Yu <xuyu@linux.alibaba.com>
2022-08-25 10:04:33 +0800

    anolis: mm: fcm: do not unconditionally call fixup ops
    ANBZ: #1881
    
    The fcm (fast copy mm) feature places stubs in the mm core path, whose
    overhead should be negligible.
    
    However, the fcm_fixup_{vma,pmd} is unconditionally called whenever a
    vma or pmd is about to be changed. This resulted in a performance
    degradation of up to 10% for will-it-scale/mmap testcase.
    
    This fixes the performance degradation by adding a static key. There is
    no need to call fixup ops when the static key is disabled.
    
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/665

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3360,23 +3360,53 @@ extern int sysctl_enable_multithread_ra_boost;
 #ifdef CONFIG_FAST_COPY_MM
 #define FCM_CANDIDATE		0
 DECLARE_STATIC_KEY_FALSE(fcm_enabled_key);
+DECLARE_STATIC_KEY_FALSE(fcm_staging_key);
 static inline bool fcm_enabled(void)
 {
 	return static_branch_unlikely(&fcm_enabled_key);
 }
+static inline bool fcm_staging(void)
+{
+	return static_branch_unlikely(&fcm_staging_key);
+}
+
 int fcm_cpr_fast(struct vm_area_struct *vma, struct vm_area_struct *mpnt);
 void fcm_cpr_bind(struct mm_struct *oldmm, struct mm_struct *mm, int err);
 void fcm_cpr_rest(void);
 void fcm_cpr_done(struct mm_struct *mm, bool r, bool l);
 
-bool maybe_pmd_fcm(pmd_t pmd);
-void fcm_fixup_pmd(struct vm_area_struct *mpnt, pmd_t *pmd, unsigned long addr);
-void fcm_fixup_vma(struct vm_area_struct *mpnt);
+bool __is_pmd_fcm(pmd_t pmd);
+void __fcm_fixup_pmd(struct vm_area_struct *mpnt, pmd_t *pmd,
+		     unsigned long addr);
+void __fcm_fixup_vma(struct vm_area_struct *mpnt);
+
+static inline bool is_pmd_fcm(pmd_t pmd)
+{
+	if (fcm_staging())
+		return __is_pmd_fcm(pmd);
+	return false;
+}
+static inline void fcm_fixup_pmd(struct vm_area_struct *mpnt, pmd_t *pmd,
+				 unsigned long addr)
+{
+	if (fcm_staging())
+		__fcm_fixup_pmd(mpnt, pmd, addr);
+}
+static inline void fcm_fixup_vma(struct vm_area_struct *mpnt)
+{
+	if (fcm_staging())
+		__fcm_fixup_vma(mpnt);
+}
 #else
 static inline bool fcm_enabled(void)
 {
 	return false;
 }
+static inline bool fcm_staging(void)
+{
+	return false;
+}
+
 static inline int fcm_cpr_fast(struct vm_area_struct *vma,
 			       struct vm_area_struct *mpnt)
 {
@@ -3392,7 +3422,8 @@ static inline void fcm_cpr_rest(void)
 static inline void fcm_cpr_done(struct mm_struct *mm, bool r, bool l)
 {
 }
-static inline bool maybe_pmd_fcm(pmd_t pmd)
+
+static inline bool is_pmd_fcm(pmd_t pmd)
 {
 	return false;
 }
