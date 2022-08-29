2a848e170bb10d403b4c763eef59b77c24cdf2c3
Kaihao Bai <carlo.bai@linux.alibaba.com>
2022-08-29 11:26:42 +0800

    anolis: mm: add switch for file zero page
    ANBZ: #2229
    
    On top of the filling the zero page of the file hole, a switch is
    added to provide the function of turning on/off at runtime. When
    turning off, all zero page mappings are evicted to ensure correctness.
    
    Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
    Reviewed-by: zhong jiang <zhongjiang-ali@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/737

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1506,6 +1506,11 @@ extern int mlock_fixup(struct vm_area_struct *vma,
 		struct vm_area_struct **prev,
 		unsigned long start, unsigned long end,
 		vm_flags_t newflags);
+/*
+ * Zap flag definitions
+ * @ZAP_ZEROPAGE: Only unmap the zeropages in the indicated range.
+ */
+#define ZAP_ZEROPAGE			0x01
 
 /*
  * Parameter block passed down to zap_pte_range in exceptional cases.
@@ -1514,6 +1519,8 @@ struct zap_details {
 	struct address_space *check_mapping;	/* Check page->mapping if set */
 	pgoff_t	first_index;			/* Lowest page->index to unmap */
 	pgoff_t last_index;			/* Highest page->index to unmap */
+	unsigned int flags;			/* Flags to indicate pages to unmap */
+
 };
 
 struct page *_vm_normal_page(struct vm_area_struct *vma, unsigned long addr,
@@ -1606,6 +1613,7 @@ extern int fixup_user_fault(struct task_struct *tsk, struct mm_struct *mm,
 			    bool *unlocked);
 void unmap_mapping_pages(struct address_space *mapping,
 		pgoff_t start, pgoff_t nr, bool even_cows);
+void unmap_mapping_zeropages(struct address_space *mapping);
 void unmap_mapping_range(struct address_space *mapping,
 		loff_t const holebegin, loff_t const holelen, int even_cows);
 #else
@@ -1626,6 +1634,7 @@ static inline int fixup_user_fault(struct task_struct *tsk,
 }
 static inline void unmap_mapping_pages(struct address_space *mapping,
 		pgoff_t start, pgoff_t nr, bool even_cows) { }
+static inline void unmap_mapping_zeropages(struct address_space *mapping) { }
 static inline void unmap_mapping_range(struct address_space *mapping,
 		loff_t const holebegin, loff_t const holelen, int even_cows) { }
 #endif
