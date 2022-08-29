e07a87fc526ee4ff5a0e1702945598940b64c332
Kaihao Bai <carlo.bai@linux.alibaba.com>
2022-08-29 11:11:26 +0800

    anolis: mm: avoid MMAP_POPULATE filling zero page
    ANBZ: #2229
    
    When mmap a file with MMAP_POPULATE/MMAP_LOCK, the page cache should be
    pre-allocated. It should avoid filling zero page.
    
    Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -356,6 +356,7 @@ extern pgprot_t protection_map[16];
 #define FAULT_FLAG_USER		0x40	/* The fault originated in userspace */
 #define FAULT_FLAG_REMOTE	0x80	/* faulting for non current tsk/mm */
 #define FAULT_FLAG_INSTRUCTION  0x100	/* The fault was during an instruction fetch */
+#define FAULT_FLAG_NONZEROPAGE  0x400   /* The fault can not be filled with zero page*/
 
 #define FAULT_FLAG_TRACE \
 	{ FAULT_FLAG_WRITE,		"WRITE" }, \
