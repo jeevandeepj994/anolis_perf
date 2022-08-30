e19c7094e7a57849fc1d2154db18b9d26fac40e9
Xiaochen Shen <xiaochen.shen@intel.com>
2022-08-30 15:09:06 +0800

    anolis: mm: prezero: add pre-zeroed page statistics
    ANBZ: #2100
    
    This adds pre-zeroed page statistics in multiple system interfaces as
    follows.
    
    /proc/vmstat
    - nr_zeroed_pages
    - prezero_alloc
    - prezero_alloc_pages
    
    /proc/meminfo
    - MemZeroed
    
    /proc/zoneinfo
    - zeroed
    - nr_zeroed_pages
    
    /proc/zerobuddyinfo
    - zeroed version of /proc/buddyinfo
    
    Signed-off-by: Xiaochen Shen <xiaochen.shen@intel.com>
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/702

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -98,6 +98,9 @@ extern int page_group_by_mobility_disabled;
 struct free_area {
 	struct list_head	free_list[MIGRATE_TYPES];
 	unsigned long		nr_free;
+#ifdef CONFIG_PAGE_PREZERO
+	unsigned long		nr_zeroed;  /* Pre-zeroed pages */
+#endif
 };
 
 static inline struct page *get_page_from_free_area(struct free_area *area,
@@ -161,6 +164,9 @@ enum zone_stat_item {
 	NR_ZSPAGES,		/* allocated in zsmalloc */
 #endif
 	NR_FREE_CMA_PAGES,
+#ifdef CONFIG_PAGE_PREZERO
+	NR_ZEROED_PAGES,	/* Pre-zeroed pages */
+#endif
 	NR_VM_ZONE_STAT_ITEMS };
 
 enum node_stat_item {
