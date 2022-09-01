dbe85a02824ca3674b090eeea9ad765058f74f05
Pavel Begunkov <asml.silence@gmail.com>
2022-09-01 16:16:10 +0100

    io_uring: disable polling pollfree files
    ANBZ: #2253
    
    commit 28d8d2737e82fc29ff9e788597661abecc7f7994 linux-stable 5.10.
    
    Older kernels lack io_uring POLLFREE handling. As only affected files
    are signalfd and android binder the safest option would be to disable
    polling those files via io_uring and hope there are no users.
    
    Fixes: 221c5eb233823 ("io_uring: add support for IORING_OP_POLL")
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Fixes: CVE-2022-3176
    Link: https://gitee.com/anolis/cloud-kernel/pulls/735
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5616,6 +5616,7 @@ static const struct file_operations binder_fops = {
 	.open = binder_open,
 	.flush = binder_flush,
 	.release = binder_release,
+	.may_pollfree = true,
 };
 
 BINDER_DEBUG_ENTRY(state);
