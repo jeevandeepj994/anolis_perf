alibaba/cloud-kernel

commit d6507404723e282585ab5a5f02b62b8e1f28da2f

anolis: scripts: ftrace_init optimization about instruction substitution
OpenAnolis Bug Tracker: 0000558

In some business scenarios, GCC versions are so old that
optimizations in ftrace cannot be completed, such as
-mrecord-mcount and -mnop-mcount. The recordmcount in the
kernel is actually used. In this case, ftrace_init will
consume a period of time, usually around 9~12ms. Do nop
substitution in recordmcount.c to speed up ftrace_init.
Only for x86 now.

Signed-off-by: Yinan Liu <yinan@linux.alibaba.com>
Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>

Makefile
@@ -748,6 +748,7 @@ endif
 
 ifdef CONFIG_FUNCTION_TRACER
 ifdef CONFIG_FTRACE_MCOUNT_RECORD
+ifneq ($(ARCH),x86)
   # gcc 5 supports generating the mcount tables directly
   ifeq ($(call cc-option-yn,-mrecord-mcount),y)
     CC_FLAGS_FTRACE	+= -mrecord-mcount
@@ -759,6 +760,7 @@ ifdef CONFIG_FTRACE_MCOUNT_RECORD
       CC_FLAGS_USING	+= -DCC_USING_NOP_MCOUNT
     endif
   endif
+endif
 endif
 ifdef CONFIG_HAVE_FENTRY
   ifeq ($(call cc-option-yn, -mfentry),y)

kernel/trace/ftrace.c
@@ -5621,6 +5621,10 @@ static int ftrace_process_locs(struct module *mod,
 	 * until we are finished with it, and there's no
 	 * reason to cause large interrupt latencies while we do it.
 	 */
+#if defined CONFIG_X86 || defined CONFIG_X86_64
+	ret = 0;
+	goto out;
+#endif
 	if (!mod)
 		local_irq_save(flags);
 	ftrace_update_code(mod, start_pg);

scripts/recordmcount.h
@@ -310,6 +310,8 @@ static uint_t *sift_rel_mcount(uint_t *mlocp,
 			       uint_t const recval,
 			       unsigned const reltype)
 {
+	Elf_Shdr *const shdr0 = (Elf_Shdr *)(_w(ehdr->e_shoff) + (void *)ehdr);
+	Elf_Shdr const *const shdr = &shdr0[w(relhdr->sh_info)];
 	uint_t *const mloc0 = mlocp;
 	Elf_Rel *mrelp = *mrelpp;
 	Elf_Sym const *sym0;
@@ -323,6 +325,7 @@ static uint_t *sift_rel_mcount(uint_t *mlocp,
 	get_sym_str_and_relp(relhdr, ehdr, &sym0, &str0, &relp);
 
 	for (t = nrel; t; --t) {
+		int ret = -1;
 		if (!mcountsym)
 			mcountsym = get_mcountsym(sym0, relp, str0);
 
@@ -340,6 +343,18 @@ static uint_t *sift_rel_mcount(uint_t *mlocp,
 				*mlocp++ = addend;
 
 			mrelp = (Elf_Rel *)(rel_entsize + (void *)mrelp);
+			/* convert mcount into nop */
+			if (make_nop == make_nop_x86) {
+				ret = make_nop((void *)ehdr,
+						_w(shdr->sh_offset) + _w(relp->r_offset));
+				if (!ret) {
+					Elf_Rel rel;
+					rel = *(Elf_Rel *)relp;
+					Elf_r_info(&rel, Elf_r_sym(relp), rel_type_nop);
+					ulseek(fd_map, (void *)relp - (void *)ehdr, SEEK_SET);
+					uwrite(fd_map, &rel, sizeof(rel));
+				}
+			}
 		}
 		relp = (Elf_Rel const *)(rel_entsize + (void *)relp);
 	}

jeevandeepj994/anolis_perf

commit 33f4300054e5dc76ae971d2f73bdbcd0f51aa137

anolis: ftrace: fix the failure of nop insertion at compile time
ANBZ: #2025

There are some bugs in the Makefile to determine whether to insert nop,
which does not take effect on the anolis builder, so there is a performance
degradation problem. The reason is that nop was not inserted during compilation,
and __fentry__ was not replaced with nop during startup. We found a 10% drop
in unixbench.

patch effect comparison on unixbench (1 concurrent) in Hygon:

with patch:
----------------------------------------------------------------------------
Dhrystone 2 using register variables         116700.0   23227050.6   1990.3
Double-Precision Whetstone                       55.0       4145.7    753.8
Execl Throughput                                 43.0       2883.1    670.5
File Copy 1024 bufsize 2000 maxblocks          3960.0     589247.0   1488.0
File Copy 256 bufsize 500 maxblocks            1655.0     159633.0    964.5
File Copy 4096 bufsize 8000 maxblocks          5800.0    1306677.0   2252.9
Pipe Throughput                               12440.0     876388.6    704.5
Pipe-based Context Switching                   4000.0     125126.6    312.8
Process Creation                                126.0       5189.1    411.8
Shell Scripts (1 concurrent)                     42.4       4217.7    994.7
Shell Scripts (8 concurrent)                      6.0       2060.1   3433.4
System Call Overhead                          15000.0    1048371.6    698.9
                                                                   ========
System Benchmarks Index Score                                         971.2
----------------------------------------------------------------------------
without patch:
----------------------------------------------------------------------------
System Benchmarks Index Values               BASELINE       RESULT    INDEX
Dhrystone 2 using register variables         116700.0   23119573.0   1981.1
Double-Precision Whetstone                       55.0       4162.0    756.7
Execl Throughput                                 43.0       2644.3    615.0
File Copy 1024 bufsize 2000 maxblocks          3960.0     466160.0   1177.2
File Copy 256 bufsize 500 maxblocks            1655.0     127535.0    770.6
File Copy 4096 bufsize 8000 maxblocks          5800.0    1463008.0   2522.4
Pipe Throughput                               12440.0     722198.4    580.5
Pipe-based Context Switching                   4000.0     114129.2    285.3
Process Creation                                126.0       5076.2    402.9
Shell Scripts (1 concurrent)                     42.4       4112.4    969.9
Shell Scripts (8 concurrent)                      6.0       2015.6   3359.4
System Call Overhead                          15000.0     857828.8    571.9
                                                                   ========
System Benchmarks Index Score                                         894.5
----------------------------------------------------------------------------

Fixes: d6507404723e ("scripts: ftrace_init optimization about instruction substitution")

Makefile
@@ -747,8 +747,15 @@ KBUILD_CFLAGS 	+= $(call cc-option, -femit-struct-debug-baseonly) \
 endif
 
 ifdef CONFIG_FUNCTION_TRACER
+ifdef CONFIG_HAVE_BUILDTIME_NOP_INSERT
+ifdef CONFIG_HAVE_FENTRY
+  ifeq ($(call cc-option-yn, -mfentry),y)
+    CC_FLAGS_FTRACE     += -mfentry
+    CC_FLAGS_USING      += -DCC_USING_FENTRY
+  endif
+endif
+else
 ifdef CONFIG_FTRACE_MCOUNT_RECORD
-ifneq ($(ARCH),x86_64)
   # gcc 5 supports generating the mcount tables directly
   ifeq ($(call cc-option-yn,-mrecord-mcount),y)
     CC_FLAGS_FTRACE	+= -mrecord-mcount
@@ -761,12 +768,12 @@ ifneq ($(ARCH),x86_64)
     endif
   endif
 endif
-endif
 ifdef CONFIG_HAVE_FENTRY
   ifeq ($(call cc-option-yn, -mfentry),y)
     CC_FLAGS_FTRACE	+= -mfentry
     CC_FLAGS_USING	+= -DCC_USING_FENTRY
   endif
+endif
 endif
 export CC_FLAGS_FTRACE
 KBUILD_CFLAGS	+= $(CC_FLAGS_FTRACE) $(CC_FLAGS_USING)

arch/x86/Kconfig
@@ -136,6 +136,7 @@ config X86
 	select HAVE_COPY_THREAD_TLS
 	select HAVE_C_RECORDMCOUNT
 	select HAVE_BUILDTIME_MCOUNT_SORT
+	select HAVE_BUILDTIME_NOP_INSERT
 	select HAVE_DEBUG_KMEMLEAK
 	select HAVE_DEBUG_STACKOVERFLOW
 	select HAVE_DMA_CONTIGUOUS

arch/x86/kernel/ftrace.c
@@ -127,10 +127,6 @@ ftrace_modify_code_direct(unsigned long ip, unsigned const char *old_code,
 	if (probe_kernel_read(replaced, (void *)ip, MCOUNT_INSN_SIZE))
 		return -EFAULT;
 
-	/* Make sure it is what we expect it to be */
-	if (memcmp(replaced, old_code, MCOUNT_INSN_SIZE) != 0)
-		return -EINVAL;
-
 	ip = text_ip_addr(ip);
 
 	/* replace the text with the new text */

kernel/trace/Kconfig
@@ -63,6 +63,13 @@ config HAVE_BUILDTIME_MCOUNT_SORT
 	  An architecture selects this if it sorts the mcount_loc section
 	  at build time.
 
+config HAVE_BUILDTIME_NOP_INSERT
+	bool
+	depends on DYNAMIC_FTRACE
+	help
+	  An architecture selects this if it insert nop with recordmcount.c
+	  at build time
+
 config BUILDTIME_MCOUNT_SORT
 	bool
 	default y

kernel/trace/ftrace.c
@@ -5626,9 +5626,11 @@ static int ftrace_process_locs(struct module *mod,
 	 * reason to cause large interrupt latencies while we do it.
 	 */
 #ifdef CONFIG_X86_64
-	ftrace_update_tot_cnt = 20000;
-	ret = 0;
-	goto out;
+	if (!mod) {
+		ftrace_update_tot_cnt = 20000;
+		ret = 0;
+		goto out;
+	}
 #endif
 	if (!mod)
 		local_irq_save(flags);