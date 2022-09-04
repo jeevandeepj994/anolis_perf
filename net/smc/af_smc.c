jeevandeepj994/anolis_perf

commit d6b4db06f3b81aa27646073f4ee0f67018d79b6a

anolis: net/smc: Keep first contact clcsock
ANBZ: #2003

This introduces a work around for eRDMA. eRDMA reuse the first TCP
tuple to create QP, and don't want to release it. But SMC will release
this tuple when first contact connection is shutdown.

This patch keeps the first contact connection, and delay the shutdown
work to link (QP) release progress.

Be careful, this patch reverses TCP close process, which means server
side closes clcsock when link clears.

Link: https://gitee.com/anolis/cloud-kernel/pulls/677
Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
Reviewed-by: Wen Gu <guwen@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>

net/smc/af_smc.c
@@ -417,6 +417,7 @@ static struct sock *smc_sock_alloc(struct net *net, struct socket *sock,
 	sk->sk_sndbuf = net->smc.sysctl_wmem_default;
 	sk->sk_rcvbuf = net->smc.sysctl_rmem_default;
 	smc = smc_sk(sk);
+	smc->keep_clcsock = false;
 	for (i = 0; i < SMC_MAX_TCP_LISTEN_WORKS; i++) {
 		smc->tcp_listen_works[i].smc = smc;
 		INIT_WORK(&smc->tcp_listen_works[i].work, smc_tcp_listen_work);
@@ -1833,7 +1834,8 @@ struct sock *smc_accept_dequeue(struct sock *parent,
 			new_sk->sk_prot->unhash(new_sk);
 			down_write(&isk->clcsock_release_lock);
 			if (isk->clcsock) {
-				sock_release(isk->clcsock);
+				if (!isk->keep_clcsock)
+					sock_release(isk->clcsock);
 				isk->clcsock = NULL;
 			}
 			up_write(&isk->clcsock_release_lock);
@@ -2944,7 +2946,7 @@ static int smc_shutdown(struct socket *sock, int how)
 		/* nothing more to do because peer is not involved */
 		break;
 	}
-	if (do_shutdown && smc->clcsock)
+	if (do_shutdown && smc->clcsock && !smc->keep_clcsock)
 		rc1 = kernel_sock_shutdown(smc->clcsock, how);
 	/* map sock_shutdown_cmd constants to sk_shutdown value range */
 	sk->sk_shutdown |= how + 1;

net/smc/smc_core.c
@@ -1201,6 +1201,8 @@ static int smc_lgr_create(struct smc_sock *smc, struct smc_init_info *ini)
 		link_idx = SMC_SINGLE_LINK;
 		lnk = &lgr->lnk[link_idx];
 		smcr_link_iw_extension(&lnk->iw_conn_param, smc->clcsock->sk);
+		if (smc->keep_clcsock)
+			lnk->clcsock = smc->clcsock;
 
 		rc = smcr_link_init(lgr, lnk, link_idx, ini);
 		if (rc) {
@@ -1546,13 +1548,17 @@ static void __smcr_link_clear(struct smc_link *lnk)
 {
 	struct smc_link_group *lgr = lnk->lgr;
 	struct smc_ib_device *smcibdev;
+	struct socket *clcsock;
 
 	smc_wr_free_link_mem(lnk);
 	smc_ibdev_cnt_dec(lnk);
+	clcsock = lnk->clcsock;
 	put_device(&lnk->smcibdev->ibdev->dev);
 	smcibdev = lnk->smcibdev;
 	memset(lnk, 0, sizeof(struct smc_link));
 	lnk->state = SMC_LNK_UNUSED;
+	if (clcsock)
+		sock_release(clcsock);
 	if (!atomic_dec_return(&smcibdev->lnk_cnt))
 		wake_up(&smcibdev->lnks_deleted);
 	smc_lgr_put(lgr); /* lgr_hold in smcr_link_init() */
@@ -2284,6 +2290,9 @@ again:
 
 create:
 	if (ini->first_contact_local) {
+		/* keep this clcsock for QP reuse */
+		if (net->smc.sysctl_keep_first_contact_clcsock)
+			smc->keep_clcsock = true;
 		rc = smc_lgr_create(smc, ini);
 		if (rc)
 			goto out;

include/net/netns/smc.h
@@ -31,6 +31,7 @@ struct netns_smc {
 	int				sysctl_tcp2smc;
 	int				sysctl_allow_different_subnet;
 	int				sysctl_disable_multiple_link;
+	int				sysctl_keep_first_contact_clcsock;
 	/* allow simplify rkey exchange when single link */
 	unsigned int			sysctl_simplify_rkey_exhcange;
 	unsigned int			sysctl_smc_fastopen;

net/smc/smc.h
@@ -260,6 +260,7 @@ struct smc_sock {				/* smc sock container */
 						/* original error_report fct. */
 	struct smc_connection	conn;		/* smc connection */
 	struct smc_sock		*listen_smc;	/* listen parent */
+	bool			keep_clcsock;
 	struct work_struct	connect_work;	/* handle non-blocking connect*/
 	struct smc_tcp_listen_work	tcp_listen_works[SMC_MAX_TCP_LISTEN_WORKS];
 						/* handle tcp socket accepts */

net/smc/smc_close.c
@@ -29,10 +29,12 @@ void smc_clcsock_release(struct smc_sock *smc)
 	    current_work() != &smc->smc_listen_work)
 		cancel_work_sync(&smc->smc_listen_work);
 	down_write(&smc->clcsock_release_lock);
+	/* don't release clcsock for eRDMA */
 	if (smc->clcsock) {
 		tcp = smc->clcsock;
 		smc->clcsock = NULL;
-		sock_release(tcp);
+		if (!smc->keep_clcsock)
+			sock_release(tcp);
 	}
 	up_write(&smc->clcsock_release_lock);
 }
@@ -242,7 +244,8 @@ again:
 			/* actively shutdown clcsock before peer close it,
 			 * prevent peer from entering TIME_WAIT state.
 			 */
-			if (smc->clcsock && smc->clcsock->sk) {
+			if (smc->clcsock && smc->clcsock->sk &&
+			    !smc->keep_clcsock) {
 				rc1 = kernel_sock_shutdown(smc->clcsock,
 							   SHUT_RDWR);
 				rc = rc ? rc : rc1;

net/smc/smc_core.h
@@ -214,6 +214,7 @@ struct smc_link {
 	struct completion	llc_testlink_resp; /* wait for rx of testlink */
 	int			llc_testlink_time; /* testlink interval */
 	atomic_t		conn_cnt; /* connections on this link */
+	struct socket		*clcsock;	/* keep for eRDMA */
 };
 
 /* For now we just allow one parallel link per link group. The SMC protocol

net/smc/smc_sysctl.c
@@ -118,6 +118,15 @@ static struct ctl_table smc_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
+	{
+		.procname	= "keep_first_contact_clcsock",
+		.data		= &init_net.smc.sysctl_keep_first_contact_clcsock,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
 	{  }
 };
 
@@ -147,6 +156,7 @@ int __net_init smc_sysctl_net_init(struct net *net)
 	net->smc.sysctl_rmem_default = 384 * 1024;
 	net->smc.sysctl_tcp2smc = 0;
 	net->smc.sysctl_allow_different_subnet = 1;
+	net->smc.sysctl_keep_first_contact_clcsock = 1;
 	net->smc.sysctl_disable_multiple_link = 1;
 	/* default on */
 	net->smc.sysctl_simplify_rkey_exhcange = 1;

commit 52046358c1e212feeaf05cace4dc5797399c6853

anolis: net/smc: Fix UAF when fallback after lgr create
ANBZ: #2045

This fixes use-after-free issue when lgr creates failed or RDMA setup
failed, it switch to fallback and should not release clcsock.

Fixes: d6b4db06f3b8 ("anolis: net/smc: Keep first contact clcsock")
Link: https://gitee.com/anolis/cloud-kernel/pulls/688
Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
Reviewed-by: Wen Gu <guwen@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>

net/smc/af_smc.c
@@ -1022,8 +1022,10 @@ static void smc_conn_abort(struct smc_sock *smc, int local_first)
 		lgr_valid = true;
 
 	smc_conn_free(conn);
-	if (local_first && lgr_valid)
+	if (local_first && lgr_valid) {
+		smc->keep_clcsock = false;
 		smc_lgr_cleanup_early(lgr);
+	}
 }
 
 /* check if there is a rdma device available for this connection. */

net/smc/smc_core.c
@@ -880,10 +880,19 @@ int smcd_nl_get_lgr(struct sk_buff *skb, struct netlink_callback *cb)
 
 void smc_lgr_cleanup_early(struct smc_link_group *lgr)
 {
+	struct smc_link *link;
 	spinlock_t *lgr_lock;
+	u8 link_idx;
 
 	if (!lgr)
 		return;
+
+	/* ONLY one link expected */
+	link_idx = SMC_SINGLE_LINK;
+	link = &lgr->lnk[link_idx];
+	if (link)
+		/* current is fallback, do not release clcsock */
+		link->clcsock = NULL;
 
 	smc_lgr_list_head(lgr, &lgr_lock);
 	spin_lock_bh(lgr_lock);
@@ -2301,6 +2310,7 @@ create:
 		rc = smc_lgr_register_conn(conn, true);
 		write_unlock_bh(&lgr->conns_lock);
 		if (rc) {
+			smc->keep_clcsock = false;
 			smc_lgr_cleanup_early(lgr);
 			goto out;
 		}