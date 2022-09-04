jeevandeepj994/anolis_perf

commit dea7289b8bb2d1e84abd39240ebfda0e86efb945

anolis: net/smc: Reuse wr_id to recognize wc for tx or rx
ANBZ: #2003

Currently, SMC sq and rq is combined to improve performance. It works
good except when QP entered into error state. The RDMA device returns wc
with status IB_WC_WR_FLUSH_ERR, but opcode of wc is always 0. So we use
wr_id by parity to recognize tx or rx.

Fixes: a0aa581d0179 ("anolis: net/smc: combine send cq and recv cq into one cq")
Link: https://gitee.com/anolis/cloud-kernel/pulls/677
Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
Reviewed-by: Wen Gu <guwen@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>

net/smc/smc_wr.c
@@ -467,7 +467,8 @@ again:
 		memset(&wc, 0, sizeof(wc));
 		rc = ib_poll_cq(smcibcq->ib_cq, SMC_WR_MAX_POLL_CQE, wc);
 		for (i = 0; i < rc; i++) {
-			if (wc[i].opcode & IB_WC_RECV)
+			if ((wc[i].opcode & IB_WC_RECV) ||
+			    (wc[i].opcode == 0 && smc_wr_id_is_rx(wc[i].wr_id)))
 				smc_wr_rx_process_cqe(&wc[i]);
 			else
 				smc_wr_tx_process_cqe(&wc[i]);
@@ -834,7 +835,7 @@ int smc_wr_create_link(struct smc_link *lnk)
 	int rc = 0;
 
 	smc_wr_tx_set_wr_id(&lnk->wr_tx_id, 0);
-	lnk->wr_rx_id = 0;
+	lnk->wr_rx_id = 1;
 	lnk->wr_rx_dma_addr = ib_dma_map_single(
 		ibdev, lnk->wr_rx_bufs,	SMC_WR_BUF_SIZE * lnk->wr_rx_cnt,
 		DMA_FROM_DEVICE);

net/smc/smc_wr.h
@@ -56,7 +56,7 @@ struct smc_wr_rx_handler {
  */
 static inline long smc_wr_tx_get_next_wr_id(struct smc_link *link)
 {
-	return atomic_long_inc_return(&link->wr_tx_id);
+	return atomic_long_add_return(2, &link->wr_tx_id);
 }
 
 static inline void smc_wr_tx_set_wr_id(atomic_long_t *wr_tx_id, long val)
@@ -151,7 +151,8 @@ static inline int smc_wr_rx_post(struct smc_link *link)
 	u64 wr_id, temp_wr_id;
 	u32 index;
 
-	wr_id = ++link->wr_rx_id; /* tasklet context, thus not atomic */
+	link->wr_rx_id += 2;
+	wr_id = link->wr_rx_id; /* tasklet context, thus not atomic */
 	temp_wr_id = wr_id;
 	index = do_div(temp_wr_id, link->wr_rx_cnt);
 	link->wr_rx_ibs[index].wr_id = wr_id;
@@ -159,6 +160,11 @@ static inline int smc_wr_rx_post(struct smc_link *link)
 	if (!rc)
 		smc_wr_rx_put_credits(link, 1);
 	return rc;
+}
+
+static inline bool smc_wr_id_is_rx(u64 wr_id)
+{
+	return wr_id % 2;
 }
 
 int smc_wr_create_link(struct smc_link *lnk);

commit a4f1f43bbfb1d3bd667176f782c0c7f7e668ccb8

anolis: net/smc: Restore rx queue depth
ANBZ: #2045

This restore the rx queue depth when divides wr_id by parity. In order
to help rq credit recover as quickly as possible to avoid potential
timeout when tx get free slot, especially in short-live connection cases.

Fixes: dea7289b8bb2 ("anolis: net/smc: Reuse wr_id to recognize wc for tx or rx")
Link: https://gitee.com/anolis/cloud-kernel/pulls/688
Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
Reviewed-by: Wen Gu <guwen@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>

net/smc/smc_wr.c
@@ -406,7 +406,7 @@ static inline void smc_wr_rx_demultiplex(struct ib_wc *wc)
 
 	if (wc->byte_len < sizeof(*wr_rx))
 		return; /* short message */
-	temp_wr_id = wc->wr_id;
+	temp_wr_id = wc->wr_id / 2;
 	index = do_div(temp_wr_id, link->wr_rx_cnt);
 	wr_rx = (struct smc_wr_rx_hdr *)&link->wr_rx_bufs[index];
 	hash_for_each_possible(smc_wr_rx_hash, handler, list, wr_rx->type) {
@@ -467,8 +467,7 @@ again:
 		memset(&wc, 0, sizeof(wc));
 		rc = ib_poll_cq(smcibcq->ib_cq, SMC_WR_MAX_POLL_CQE, wc);
 		for (i = 0; i < rc; i++) {
-			if ((wc[i].opcode & IB_WC_RECV) ||
-			    (wc[i].opcode == 0 && smc_wr_id_is_rx(wc[i].wr_id)))
+			if (smc_wr_id_is_rx(wc[i].wr_id))
 				smc_wr_rx_process_cqe(&wc[i]);
 			else
 				smc_wr_tx_process_cqe(&wc[i]);

net/smc/smc_wr.h
@@ -153,7 +153,7 @@ static inline int smc_wr_rx_post(struct smc_link *link)
 
 	link->wr_rx_id += 2;
 	wr_id = link->wr_rx_id; /* tasklet context, thus not atomic */
-	temp_wr_id = wr_id;
+	temp_wr_id = wr_id / 2;
 	index = do_div(temp_wr_id, link->wr_rx_cnt);
 	link->wr_rx_ibs[index].wr_id = wr_id;
 	rc = ib_post_recv(link->roce_qp, &link->wr_rx_ibs[index], NULL);