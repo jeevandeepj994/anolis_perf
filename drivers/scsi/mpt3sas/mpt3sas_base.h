2530512110376caff2f0cefd1d731770fd9dabe9
Gu Mi <gumi@linux.alibaba.com>
2022-09-13 16:35:07 +0800

    scsi: mpt3sas: Improve the threshold value and introduce module param
    ANBZ: #2110
    
    commit 288addd63b2478e8bd6d0bb7786a2eeb53bb2986 upstream.
    
    * Reduce the threshold value to 1/4 of the queue depth.
    
    * With this FW can find enough entries to post the Reply Descriptors in
    * the
      reply descriptor post queue.
    
    * With module param, user can play with threshold value, the same
      irqpoll_weight is used as the budget in processing of reply descriptor
      post queues in _base_process_reply_queue.
    
    Signed-off-by: Suganath Prabu <suganath-prabu.subramani@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Gu Mi <gumi@linux.alibaba.com>

--- drivers/scsi/mpt3sas/mpt3sas_base.h
+++ drivers/scsi/mpt3sas/mpt3sas_base.h
@@ -1029,6 +1029,8 @@ typedef void (*MPT3SAS_FLUSH_RUNNING_CMDS)(struct MPT3SAS_ADAPTER *ioc);
  * @msix_load_balance: Enables load balancing of interrupts across
  * the multiple MSIXs
  * @schedule_dead_ioc_flush_running_cmds: callback to flush pending commands
+ * @thresh_hold: Max number of reply descriptors processed
+ *				before updating Host Index
  * @use_32bit_dma: Flag to use 32 bit consistent dma mask
  * @scsi_io_cb_idx: shost generated commands
  * @tm_cb_idx: task management commands
@@ -1207,6 +1209,7 @@ struct MPT3SAS_ADAPTER {
 	bool	use_32bit_dma;
 	atomic64_t      total_io_cnt;
 	bool            msix_load_balance;
+	u16		thresh_hold;
 
 	/* internal commands, callback index */
 	u8		scsi_io_cb_idx;
