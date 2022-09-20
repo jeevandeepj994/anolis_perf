5ce2e550ad50f3b4a7d24213741b7fbd59cdddbe
Duanqiang Wen <duanqiangwen@net-swift.com>
2022-09-20 11:12:09 +0800

    anolis: net: add hardware phy initialization
    ANBZ: #1516
    
    add hardware and phy initialization
    
    Signed-off-by: Duanqiang Wen <duanqiangwen@net-swift.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1043

--- drivers/net/ethernet/wangxun/ngbe/ngbe.h
+++ drivers/net/ethernet/wangxun/ngbe/ngbe.h
@@ -6,10 +6,42 @@
 
 #include "ngbe_type.h"
 
-#define NGBE_MAX_FDIR_INDICES		7
+#define NGBE_MAX_FDIR_INDICES		        7
+#define NGBE_MAX_MSIX_Q_VECTORS_EMERALD     9
 
 #define NGBE_MAX_RX_QUEUES		(NGBE_MAX_FDIR_INDICES + 1)
 #define NGBE_MAX_TX_QUEUES		(NGBE_MAX_FDIR_INDICES + 1)
+
+#define NGBE_FLAG_NEED_LINK_UPDATE             (u32)(1 << 13)
+#define NGBE_FLAG_NEED_ANC_CHECK               (u32)(1 << 14)
+
+#define NGBE_FLAG2_SEARCH_FOR_SFP              (u32)(1 << 5)
+
+#define NGBE_INTR_ALL                  0x1FF
+
+/* TX/RX descriptor defines */
+#define NGBE_DEFAULT_TXD               512 /* default ring size */
+#define NGBE_DEFAULT_TX_WORK           256
+#define NGBE_DEFAULT_RXD               512 /* default ring size */
+#define NGBE_DEFAULT_RX_WORK           256
+
+enum ngbe_state_t {
+	__NGBE_TESTING,
+	__NGBE_RESETTING,
+	__NGBE_DOWN,
+	__NGBE_HANGING,
+	__NGBE_DISABLED,
+	__NGBE_REMOVING,
+	__NGBE_SERVICE_SCHED,
+	__NGBE_SERVICE_INITED,
+	__NGBE_IN_SFP_INIT,
+};
+
+struct ngbe_mac_addr {
+	u8 addr[ETH_ALEN];
+	u16 state; /* bitmask */
+	u64 pools;
+};
 
 /* board specific private data structure */
 struct ngbe_adapter {
@@ -17,8 +49,119 @@ struct ngbe_adapter {
 	/* OS defined structs */
 	struct net_device *netdev;
 	struct pci_dev *pdev;
+
+	int max_q_vectors;      /* upper limit of q_vectors for device */
+	u32 flags;
+	u32 flags2;
+	unsigned long state;
+
+	unsigned int tx_ring_count;
+	unsigned int rx_ring_count;
+
+	/* Tx fast path data */
+	u16 tx_work_limit;
+
+	/* Rx fast path data */
+	u16 rx_work_limit;
+
+	/* structs defined in ngbe_hw.h */
+	struct ngbe_hw hw;
+	u16 msg_enable;
+
+	struct ngbe_mac_addr *mac_table;
+
+	u32 tx_timeout_recovery_level;
+};
+
+struct ngbe_msg {
+	u16 msg_enable;
 };
 
 extern char ngbe_driver_name[];
+
+static struct net_device *ngbe_hw_to_netdev(const struct ngbe_hw *hw)
+{
+	return ((struct ngbe_adapter *)hw->back)->netdev;
+}
+
+static struct ngbe_msg *ngbe_hw_to_msg(const struct ngbe_hw *hw)
+{
+	struct ngbe_adapter *adapter =
+		container_of(hw, struct ngbe_adapter, hw);
+	return (struct ngbe_msg *)&adapter->msg_enable;
+}
+
+static inline void ngbe_intr_disable(struct ngbe_hw *hw, u64 qmask)
+{
+	u32 mask;
+
+	mask = (qmask & 0xFFFFFFFF);
+	if (mask)
+		wr32(hw, NGBE_PX_IMS, mask);
+}
+
+static inline struct device *pci_dev_to_dev(struct pci_dev *pdev)
+{
+	return &pdev->dev;
+}
+
+enum {
+	NGBE_ERROR_SOFTWARE,
+	NGBE_ERROR_POLLING,
+	NGBE_ERROR_INVALID_STATE,
+	NGBE_ERROR_UNSUPPORTED,
+	NGBE_ERROR_ARGUMENT,
+	NGBE_ERROR_CAUTION,
+};
+
+#define ERROR_REPORT(hw, level, format, arg...) do {                      \
+	switch (level) {                                                      \
+	case NGBE_ERROR_SOFTWARE:                                             \
+		/* fallthrough */                                                \
+	case NGBE_ERROR_CAUTION:                                              \
+		/* fallthrough */                                                \
+	case NGBE_ERROR_POLLING:                                              \
+		netif_warn(ngbe_hw_to_msg(hw), drv, ngbe_hw_to_netdev(hw),        \
+				format, ## arg);                                          \
+		break;                                                            \
+	case NGBE_ERROR_INVALID_STATE:                                        \
+		/* fallthrough */                                                \
+	case NGBE_ERROR_UNSUPPORTED:                                          \
+		/* fallthrough */                                                \
+	case NGBE_ERROR_ARGUMENT:                                             \
+		netif_err(ngbe_hw_to_msg(hw), hw, ngbe_hw_to_netdev(hw),          \
+				format, ## arg);                                          \
+		break;                                                            \
+	default:                                                              \
+		break;                                                            \
+	}                                                                     \
+} while (0)
+
+#define ERROR_REPORT1 ERROR_REPORT
+#define ERROR_REPORT2 ERROR_REPORT
+#define ERROR_REPORT3 ERROR_REPORT
+
+#define hw_dbg(hw, format, arg...) \
+	netdev_dbg(ngbe_hw_to_netdev(hw), format, ## arg)
+#define hw_err(hw, format, arg...) \
+	netdev_err(ngbe_hw_to_netdev(hw), format, ## arg)
+#define e_dev_info(format, arg...) \
+	dev_info(pci_dev_to_dev(adapter->pdev), format, ## arg)
+#define e_dev_warn(format, arg...) \
+	dev_warn(pci_dev_to_dev(adapter->pdev), format, ## arg)
+#define e_dev_err(format, arg...) \
+	dev_err(pci_dev_to_dev(adapter->pdev), format, ## arg)
+#define e_dev_notice(format, arg...) \
+	dev_notice(pci_dev_to_dev(adapter->pdev), format, ## arg)
+#define e_dbg(msglvl, format, arg...) \
+	netif_dbg(adapter, msglvl, adapter->netdev, format, ## arg)
+#define e_info(msglvl, format, arg...) \
+	netif_info(adapter, msglvl, adapter->netdev, format, ## arg)
+#define e_err(msglvl, format, arg...) \
+	netif_err(adapter, msglvl, adapter->netdev, format, ## arg)
+#define e_warn(msglvl, format, arg...) \
+	netif_warn(adapter, msglvl, adapter->netdev, format, ## arg)
+#define e_crit(msglvl, format, arg...) \
+	netif_crit(adapter, msglvl, adapter->netdev, format, ## arg)
 
 #endif /* _NGBE_H_ */
--- drivers/net/ethernet/wangxun/ngbe/ngbe_main.c
+++ drivers/net/ethernet/wangxun/ngbe/ngbe_main.c
@@ -10,6 +10,9 @@
 #include <linux/etherdevice.h>
 
 #include "ngbe.h"
+#include "ngbe_phy.h"
+#include "ngbe_hw.h"
+
 char ngbe_driver_name[] = "ngbe";
 
 /* ngbe_pci_tbl - PCI Device ID Table
@@ -34,6 +37,8 @@ static const struct pci_device_id ngbe_pci_tbl[] = {
 	{ .device = 0 }
 };
 
+#define DEFAULT_DEBUG_LEVEL_SHIFT 3
+
 static void ngbe_dev_shutdown(struct pci_dev *pdev, bool *enable_wake)
 {
 	struct ngbe_adapter *adapter = pci_get_drvdata(pdev);
@@ -57,6 +62,148 @@ static void ngbe_shutdown(struct pci_dev *pdev)
 }
 
 /**
+ *  ngbe_init_ops - Inits func ptrs and MAC type
+ *  @hw: pointer to hardware structure
+ **/
+s32 ngbe_init_ops(struct ngbe_hw *hw)
+{
+	ngbe_init_phy_ops_common(hw);
+	ngbe_init_ops_common(hw);
+	ngbe_init_external_phy_ops_common(hw);
+
+	return NGBE_OK;
+}
+
+/**
+ *  ngbe_init_shared_code - Initialize the shared code
+ *  @hw: pointer to hardware structure
+ **/
+s32 ngbe_init_shared_code(struct ngbe_hw *hw)
+{
+	if ((hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_M88E1512_SFP ||
+	    (hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_LY_M88E1512_SFP)
+		hw->phy.type = ngbe_phy_m88e1512_sfi;
+	else if ((hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_M88E1512_RJ45 ||
+		 (hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_M88E1512_RJ45)
+		hw->phy.type = ngbe_phy_m88e1512;
+	else if ((hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_M88E1512_MIX)
+		hw->phy.type = ngbe_phy_m88e1512_unknown;
+	else if ((hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_YT8521S_SFP ||
+		 (hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_YT8521S_SFP_GPIO ||
+		(hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_LY_YT8521S_SFP)
+		hw->phy.type = ngbe_phy_yt8521s_sfi;
+	else if ((hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_INTERNAL_YT8521S_SFP ||
+		 (hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_INTERNAL_YT8521S_SFP_GPIO)
+		hw->phy.type = ngbe_phy_internal_yt8521s_sfi;
+	else
+		hw->phy.type = ngbe_phy_internal;
+
+	if ((hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_LY_M88E1512_SFP ||
+	    (hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_LY_YT8521S_SFP ||
+		(hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_YT8521S_SFP_GPIO ||
+		(hw->subsystem_device_id & NGBE_OEM_MASK) == NGBE_SUBID_INTERNAL_YT8521S_SFP_GPIO)
+		hw->gpio_ctl = 1;
+
+	/* select claus22 */
+	wr32(hw, NGBE_MDIO_CLAUSE_SELECT, 0xF);
+
+	return ngbe_init_ops(hw);
+}
+
+static int ngbe_sw_init(struct ngbe_adapter *adapter)
+{
+	struct ngbe_hw *hw = &adapter->hw;
+	struct pci_dev *pdev = adapter->pdev;
+	int err;
+	u32 ssid = 0;
+
+	/* PCI config space info */
+	hw->vendor_id = pdev->vendor;
+	hw->device_id = pdev->device;
+	hw->revision_id = pdev->revision;
+	hw->oem_svid = pdev->subsystem_vendor;
+	hw->oem_ssid = pdev->subsystem_device;
+
+	if (pdev->subsystem_vendor == PCI_VENDOR_ID_WANGXUN) {
+		hw->subsystem_vendor_id = pdev->subsystem_vendor;
+		hw->subsystem_device_id = pdev->subsystem_device;
+	} else {
+		ssid = ngbe_flash_read_dword(hw, 0xfffdc);
+		if (ssid == 0x1) {
+			e_err(probe, "read of internal subsystem device id failed\n");
+			err = -ENODEV;
+			goto out;
+		}
+		hw->subsystem_device_id = ssid >> 8 | ssid << 8;
+	}
+
+	/* phy type, phy ops, mac ops */
+	err = ngbe_init_shared_code(hw);
+	if (err) {
+		e_err(probe, "init_shared_code failed: %d\n", err);
+		goto out;
+	}
+
+	adapter->mac_table = kzalloc(sizeof(*adapter->mac_table) *
+						 hw->mac.num_rar_entries,
+						 GFP_ATOMIC);
+	if (!adapter->mac_table) {
+		err = NGBE_ERR_OUT_OF_MEM;
+		e_err(probe, "mac_table allocation failed: %d\n", err);
+		goto out;
+	}
+
+	/* Set common capability flags and settings */
+	adapter->max_q_vectors = NGBE_MAX_MSIX_Q_VECTORS_EMERALD;
+
+	/* set default ring sizes */
+	adapter->tx_ring_count = NGBE_DEFAULT_TXD;
+	adapter->rx_ring_count = NGBE_DEFAULT_RXD;
+
+	/* set default work limits */
+	adapter->tx_work_limit = NGBE_DEFAULT_TX_WORK;
+	adapter->rx_work_limit = NGBE_DEFAULT_RX_WORK;
+
+	adapter->tx_timeout_recovery_level = 0;
+
+	set_bit(__NGBE_DOWN, &adapter->state);
+out:
+	return err;
+}
+
+/**
+ * ngbe_open - Called when a network interface is made active
+ * @netdev: network interface device structure
+ *
+ * Returns 0 on success, negative value on failure
+ **/
+int ngbe_open(struct net_device *netdev)
+{
+	return 0;
+}
+
+/**
+ * ngbe_close - Disables a network interface
+ * @netdev: network interface device structure
+ *
+ * Returns 0, this is not allowed to fail
+ **/
+int ngbe_close(struct net_device *netdev)
+{
+	return 0;
+}
+
+static const struct net_device_ops ngbe_netdev_ops = {
+	.ndo_open = ngbe_open,
+	.ndo_stop = ngbe_close,
+};
+
+void ngbe_assign_netdev_ops(struct net_device *dev)
+{
+	dev->netdev_ops = &ngbe_netdev_ops;
+}
+
+/**
  * ngbe_probe - Device Initialization Routine
  * @pdev: PCI device information struct
  * @ent: entry in ngbe_pci_tbl
@@ -72,7 +219,10 @@ static int ngbe_probe(struct pci_dev *pdev,
 {
 	struct ngbe_adapter *adapter = NULL;
 	struct net_device *netdev;
+	struct ngbe_hw *hw = NULL;
+	struct device *dev = &pdev->dev;
 	int err;
+	int size;
 
 	err = pci_enable_device_mem(pdev);
 	if (err)
@@ -97,10 +247,16 @@ static int ngbe_probe(struct pci_dev *pdev,
 	pci_enable_pcie_error_reporting(pdev);
 	pci_set_master(pdev);
 
-	netdev = devm_alloc_etherdev_mqs(&pdev->dev,
-					 sizeof(struct ngbe_adapter),
-					 NGBE_MAX_TX_QUEUES,
-					 NGBE_MAX_RX_QUEUES);
+	/* errata 16 */
+	pcie_capability_clear_and_set_word(pdev, PCI_EXP_DEVCTL,
+					   PCI_EXP_DEVCTL_READRQ,
+									   0x1000);
+
+	size = sizeof(struct ngbe_adapter);
+	netdev = devm_alloc_etherdev_mqs(dev,
+					 size,
+									 NGBE_MAX_TX_QUEUES,
+									 NGBE_MAX_RX_QUEUES);
 	if (!netdev) {
 		err = -ENOMEM;
 		goto err_pci_release_regions;
@@ -111,6 +267,9 @@ static int ngbe_probe(struct pci_dev *pdev,
 	adapter = netdev_priv(netdev);
 	adapter->netdev = netdev;
 	adapter->pdev = pdev;
+	hw = &adapter->hw;
+	hw->back = adapter;
+	adapter->msg_enable = (1 << DEFAULT_DEBUG_LEVEL_SHIFT) - 1;
 
 	adapter->io_addr = devm_ioremap(&pdev->dev,
 					pci_resource_start(pdev, 0),
@@ -120,9 +279,29 @@ static int ngbe_probe(struct pci_dev *pdev,
 		goto err_pci_release_regions;
 	}
 
+	/* default config: 10/100/1000M autoneg on */
+	hw->mac.autoneg = true;
+	hw->phy.autoneg_advertised = NGBE_LINK_SPEED_AUTONEG;
+	hw->phy.force_speed = NGBE_LINK_SPEED_UNKNOWN;
+	/* assign netdev ops and ethtool ops */
+	ngbe_assign_netdev_ops(netdev);
+
+	/* setup the private structure */
+	err = ngbe_sw_init(adapter);
+	if (err)
+		goto err_sw_init;
+
 	netdev->features |= NETIF_F_HIGHDMA;
 
 	pci_set_drvdata(pdev, adapter);
+
+	hw->phy.reset_if_overtemp = true;
+	err = TCALL(hw, mac.ops.reset_hw);
+	hw->phy.reset_if_overtemp = false;
+	if (err) {
+		e_dev_err("HW reset failed: %d\n", err);
+		goto err_sw_init;
+	}
 
 	return 0;
 
@@ -132,6 +311,11 @@ err_pci_release_regions:
 				     pci_select_bars(pdev, IORESOURCE_MEM));
 err_pci_disable_dev:
 	pci_disable_device(pdev);
+err_sw_init:
+	adapter->flags2 &= ~NGBE_FLAG2_SEARCH_FOR_SFP;
+	kfree(adapter->mac_table);
+	iounmap(adapter->io_addr);
+
 	return err;
 }
 
