kernelorg-mirror/linux_kernel_git_vishal_tiering

memory tiering: add file pages demotion counter
To distinguish the file pages demotion from the anonymous pages
demotion.  This can be used to check the balance between the
file/anonymous pages demotion.

Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

mm/vmscan.c
@@ -1146,10 +1146,12 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 	int target_nid = next_demotion_node(pgdat->node_id);
 	unsigned int nr_succeeded = 0;
 	int err;
+	bool file_lru;
 
 	if (list_empty(demote_pages))
 		return 0;
 
+	file_lru = page_is_file_lru(lru_to_page(demote_pages));
 	/* Demotion ignores all cpuset and mempolicy settings */
 	err = migrate_pages(demote_pages, alloc_demote_page, NULL,
 			    target_nid, MIGRATE_ASYNC, MR_DEMOTION,
@@ -1159,6 +1161,8 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 		__count_vm_events(PGDEMOTE_KSWAPD, nr_succeeded);
 	else
 		__count_vm_events(PGDEMOTE_DIRECT, nr_succeeded);
+	if (file_lru)
+		__count_vm_events(PGDEMOTE_FILE, nr_succeeded);
 
 	return nr_succeeded;
 }

jeevandeepj994/anolis_perf

ANBZ: #80
cherry-picked from
https://git.kernel.org/pub/scm/linux/kernel/git/vishal/tiering.git/commit/?h=tiering-0.72&id=3a47eee0d0df82e219ae46ea0793b3bcde98
Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Reviewed-by: Zhong Jiang <zhongjiang-ali@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/725
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

mm/vmscan.c
@@ -1252,6 +1252,7 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 	int target_nid = next_demotion_node(pgdat->node_id);
 	unsigned int nr_succeeded;
 	int err;
+	bool file_lru;
 
 	if (list_empty(demote_pages))
 		return 0;
@@ -1259,6 +1260,7 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 	if (target_nid == NUMA_NO_NODE)
 		return 0;
 
+	file_lru = page_is_file_cache(lru_to_page(demote_pages));
 	/* Demotion ignores all cpuset and mempolicy settings */
 	err = migrate_pages(demote_pages, alloc_demote_page, NULL,
 			    target_nid, MIGRATE_ASYNC, MR_DEMOTION,
@@ -1268,6 +1270,8 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 		__count_vm_events(PGDEMOTE_KSWAPD, nr_succeeded);
 	else
 		__count_vm_events(PGDEMOTE_DIRECT, nr_succeeded);
+	if (file_lru)
+		__count_vm_events(PGDEMOTE_FILE, nr_succeeded);
 
 	return nr_succeeded;
 }