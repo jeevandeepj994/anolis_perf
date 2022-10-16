alibaba/cloud-kernel

commit 33b8fbc444c8c08982462ebd602cc696b8eb1bcd

anolis: erofs,virtiofs,dax: pin dax mapping when doing fault-in
ANBZ: #228

Otherwise, when virtiofs starts reclaiming dmap, erofs may access
invalid address.

Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>
Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/fuse/dax.c
@@ -12,6 +12,7 @@
 #include <linux/pfn_t.h>
 #include <linux/iomap.h>
 #include <linux/interval_tree.h>
+#include <linux/magic.h>
 
 /*
  * Default memory range size.  A power of 2 so it agrees with common FUSE_INIT
@@ -652,9 +653,51 @@ static int fuse_iomap_end(struct inode *inode, loff_t pos, loff_t length,
 	return 0;
 }
 
+struct virtiofs_dmap_bitmap {
+	unsigned int magic;	/* 0x01020304 */
+	struct fuse_conn *fc;
+	refcount_t count;
+	unsigned long bitmap[];
+};
+
+static const struct vm_operations_struct fuse_dax_vm_ops;
+
+static void fuse_iomap_save_private(struct vm_area_struct *vma,
+				    struct iomap *iomap)
+{
+	struct fuse_dax_mapping *dmap = iomap->private;
+	unsigned int nr = dmap->window_offset / FUSE_DAX_SZ;
+	struct virtiofs_dmap_bitmap *db = vma->vm_private_data;
+
+	/* check if the vma belongs erofs */
+	if (vma->vm_ops == &fuse_dax_vm_ops)
+		return;
+
+	if (!dmap)
+		return;
+
+	if (!db || db->magic != 0x01020304) {
+		WARN_ON(1);
+		return;
+	}
+
+	WARN_ON_ONCE(dmap->window_offset % FUSE_DAX_SZ);
+
+	/* (used by erofs) atomic set bitmap in vma->vm_private_data */
+	if (!test_and_set_bit(nr, db->bitmap)) {
+		pr_debug("erofs pinned memory range window_offset=0x%llx length=0x%llx\n",
+			 dmap->window_offset, dmap->length);
+
+		/* increase refcnt so that erofs's vma mapping won't be reclaimed. */
+		WARN_ON_ONCE(refcount_read(&dmap->refcnt) < 2);
+		refcount_inc(&dmap->refcnt);
+	}
+}
+
 static const struct iomap_ops fuse_iomap_ops = {
 	.iomap_begin = fuse_iomap_begin,
 	.iomap_end = fuse_iomap_end,
+	.iomap_save_private = fuse_iomap_save_private,
 };
 
 static void fuse_wait_dax_page(struct inode *inode)
@@ -860,15 +903,85 @@ static vm_fault_t fuse_dax_pfn_mkwrite(struct vm_fault *vmf)
 	return __fuse_dax_fault(vmf, PE_SIZE_PTE, true);
 }
 
+static void fuse_dax_vma_close(struct vm_area_struct *vma)
+{
+	struct fuse_dax_mapping *dmap;
+	struct virtiofs_dmap_bitmap *db = vma->vm_private_data;
+
+	/* check if the vma belongs erofs */
+	if (vma->vm_ops == &fuse_dax_vm_ops)
+		return;
+	if (!vma->vm_file || !db || db->magic != 0x01020304) {
+		WARN_ON(1);
+		return;
+	}
+
+	vma->vm_private_data = NULL;
+	if (!refcount_dec_and_test(&db->count))
+		return;
+	spin_lock(&db->fc->lock);
+	list_for_each_entry(dmap, &db->fc->dax->busy_ranges, busy_list) {
+		unsigned int nr = dmap->window_offset / FUSE_DAX_SZ;
+
+		if (!test_bit(nr, db->bitmap))
+			continue;
+
+		if (refcount_dec_and_test(&dmap->refcnt)) {
+			/*
+			 * refcount should not hit 0. This object only goes
+			 * away when fuse connection goes away
+			 */
+			WARN_ON_ONCE(1);
+		}
+	}
+	spin_unlock(&db->fc->lock);
+	kfree(db);
+}
+
+static void fuse_dax_vma_open(struct vm_area_struct *vma)
+{
+	struct virtiofs_dmap_bitmap *db = vma->vm_private_data;
+
+	/* check if the vma belongs erofs */
+	if (vma->vm_ops == &fuse_dax_vm_ops)
+		return;
+	if (!vma->vm_file || !db || db->magic != 0x01020304) {
+		WARN_ON(1);
+		return;
+	}
+	refcount_inc(&db->count);
+
+}
+
 static const struct vm_operations_struct fuse_dax_vm_ops = {
 	.fault		= fuse_dax_fault,
 	.huge_fault	= fuse_dax_huge_fault,
 	.page_mkwrite	= fuse_dax_page_mkwrite,
 	.pfn_mkwrite	= fuse_dax_pfn_mkwrite,
+	.open		= fuse_dax_vma_open,
+	.close		= fuse_dax_vma_close,
 };
 
 int fuse_dax_mmap(struct file *file, struct vm_area_struct *vma)
 {
+	if (vma->vm_ops != &fuse_dax_vm_ops &&
+	    file != vma->vm_file &&
+	    file_inode(vma->vm_file)->i_sb->s_magic == EROFS_SUPER_MAGIC_V1) {
+		struct fuse_conn *fc = get_fuse_conn(file_inode(file));
+		unsigned int size = DIV_ROUND_UP(fc->dax->nr_ranges,
+						 sizeof(unsigned long));
+		struct virtiofs_dmap_bitmap *db =
+			kzalloc(struct_size(db, bitmap, size), GFP_NOFS);
+
+		WARN_ON(vma->vm_private_data);
+		if (!db)
+			return -ENOMEM;
+		db->magic = 0x01020304;
+		db->fc = fc;
+		refcount_set(&db->count, 1);
+		vma->vm_private_data = db;
+	}
+
 	file_accessed(file);
 	vma->vm_ops = &fuse_dax_vm_ops;
 	vma->vm_flags |= VM_MIXEDMAP | VM_HUGEPAGE;

fs/dax.c
@@ -1529,6 +1529,8 @@ static vm_fault_t dax_iomap_pte_fault(struct vm_fault *vmf, pfn_t *pfnp,
 
 		if (ret & VM_FAULT_ERROR)
 			copied = 0;
+		else if (ops->iomap_save_private)
+			ops->iomap_save_private(vma, &iomap);
 		/*
 		 * The fault is done by now and there's no way back (other
 		 * thread may be already happily using PTE we have installed).

fs/erofs/erofs_fs.h
@@ -9,7 +9,6 @@
 #ifndef __EROFS_FS_H
 #define __EROFS_FS_H
 
-#define EROFS_SUPER_MAGIC_V1	0xE0F5E1E2
 #define EROFS_SUPER_OFFSET      1024
 
 #define EROFS_FEATURE_COMPAT_SB_CHKSUM          0x00000001

fs/erofs/inode.c
@@ -514,6 +514,7 @@ static vm_fault_t rafs_v6_filemap_fault(struct vm_fault *vmf)
 		return VM_FAULT_SIGBUS;
 
 	memcpy(&lower_vma, vmf->vma, sizeof(lower_vma));
+	WARN_ON_ONCE(lower_vma.vm_private_data != vma->vm_private_data);
 
 	/* TODO: check if chunk is available for us to read. */
 	map.m_la = orig_pgoff << PAGE_SHIFT;
@@ -539,8 +540,40 @@ static vm_fault_t rafs_v6_filemap_fault(struct vm_fault *vmf)
 	return ret;
 }
 
+static void rafs_v6_vm_close(struct vm_area_struct *vma)
+{
+	struct inode *inode;
+
+	if (!vma || !vma->vm_file) {
+		WARN_ON_ONCE(1);
+		return;
+	}
+
+	inode = file_inode(vma->vm_file);
+	if (EROFS_I(inode)->lower_vm_ops && EROFS_I(inode)->lower_vm_ops->close)
+		EROFS_I(inode)->lower_vm_ops->close(vma);
+
+	WARN_ON(vma->vm_private_data);
+}
+
+static void rafs_v6_vm_open(struct vm_area_struct *vma)
+{
+	struct inode *inode;
+
+	if (!vma || !vma->vm_file) {
+		WARN_ON_ONCE(1);
+		return;
+	}
+
+	inode = file_inode(vma->vm_file);
+	if (EROFS_I(inode)->lower_vm_ops && EROFS_I(inode)->lower_vm_ops->open)
+		EROFS_I(inode)->lower_vm_ops->open(vma);
+}
+
 static const struct vm_operations_struct rafs_v6_vm_ops = {
 	.fault	= rafs_v6_filemap_fault,
+	.close	= rafs_v6_vm_close,
+	.open	= rafs_v6_vm_open,
 };
 
 static int rafs_v6_file_mmap(struct file *file, struct vm_area_struct *vma)
@@ -548,7 +581,13 @@ static int rafs_v6_file_mmap(struct file *file, struct vm_area_struct *vma)
 	struct inode *inode = file_inode(file);
 	struct erofs_inode *vi = EROFS_I(inode);
 	const struct vm_operations_struct *lower_vm_ops;
+	struct file *realfile = EROFS_I_SB(inode)->bootstrap;
 	int ret;
+
+	if (!realfile || !realfile->f_op->mmap) {
+		pr_err("%s: no bootstrap or mmap\n", __func__);
+		return -EOPNOTSUPP;
+	}
 
 	ret = call_mmap(EROFS_I_SB(inode)->bootstrap, vma);
 	if (ret) {

include/linux/iomap.h
@@ -100,6 +100,10 @@ struct iomap_ops {
 	 */
 	int (*iomap_end)(struct inode *inode, loff_t pos, loff_t length,
 			ssize_t written, unsigned flags, struct iomap *iomap);
+
+	/* Save an object in vma->private_data */
+	void (*iomap_save_private)(struct vm_area_struct *vma,
+				   struct iomap *iomap);
 };
 
 /*

include/uapi/linux/magic.h
@@ -19,6 +19,7 @@
 #define SQUASHFS_MAGIC		0x73717368
 #define ECRYPTFS_SUPER_MAGIC	0xf15f
 #define EFS_SUPER_MAGIC		0x414A53
+#define EROFS_SUPER_MAGIC_V1    0xE0F5E1E2
 #define EXT2_SUPER_MAGIC	0xEF53
 #define EXT3_SUPER_MAGIC	0xEF53
 #define XENFS_SUPER_MAGIC	0xabba1974

jeevandeepj994/anolis_perf

commit 8a47cbe16f12c650bcadae9b726ec9ed773c5f29

anolis: erofs, virtiofs, dax: fix incorrect locking around busy_ranges
ANBZ: #2403

busy_range should be protected by fuse_conn_dax lock rather than
fuse_conn lock.

Fixes: 33b8fbc444c8 ("anolis: erofs,virtiofs,dax: pin dax mapping when doing fault-in")
Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Reviewed-by: Liu Bo <bo.liu@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/771

fs/fuse/dax.c
@@ -682,6 +682,7 @@ static void fuse_iomap_save_private(struct vm_area_struct *vma,
 	}
 
 	WARN_ON_ONCE(dmap->window_offset % FUSE_DAX_SZ);
+	WARN_ON_ONCE(nr >= db->fc->dax->nr_ranges);
 
 	/* (used by erofs) atomic set bitmap in vma->vm_private_data */
 	if (!test_and_set_bit(nr, db->bitmap)) {
@@ -941,6 +942,7 @@ static void fuse_dax_vma_close(struct vm_area_struct *vma)
 {
 	struct fuse_dax_mapping *dmap;
 	struct virtiofs_dmap_bitmap *db = vma->vm_private_data;
+	struct fuse_conn_dax *fcd;
 
 	/* check if the vma belongs erofs */
 	if (vma->vm_ops == &fuse_dax_vm_ops)
@@ -953,10 +955,12 @@ static void fuse_dax_vma_close(struct vm_area_struct *vma)
 	vma->vm_private_data = NULL;
 	if (!refcount_dec_and_test(&db->count))
 		return;
-	spin_lock(&db->fc->lock);
-	list_for_each_entry(dmap, &db->fc->dax->busy_ranges, busy_list) {
+	fcd = db->fc->dax;
+	spin_lock(&fcd->lock);
+	list_for_each_entry(dmap, &fcd->busy_ranges, busy_list) {
 		unsigned int nr = dmap->window_offset / FUSE_DAX_SZ;
 
+		WARN_ON_ONCE(dmap->window_offset % FUSE_DAX_SZ);
 		if (!test_bit(nr, db->bitmap))
 			continue;
 
@@ -968,7 +972,7 @@ static void fuse_dax_vma_close(struct vm_area_struct *vma)
 			WARN_ON_ONCE(1);
 		}
 	}
-	spin_unlock(&db->fc->lock);
+	spin_unlock(&fcd->lock);
 	kfree(db);
 }
 