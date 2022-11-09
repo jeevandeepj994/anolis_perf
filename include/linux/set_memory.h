7d2ecb754888ff5108cf102d20784f67beacb6ee
Kaihao Bai <carlo.bai@linux.alibaba.com>
2022-11-09 11:17:05 +0800

    anolis: arm64: add the helper to set no-present page table
    ANBZ: #8540
    
    Add a helper set_memory_np to make the page table corresponding to
    the virtual address not present.
    
    Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>

--- include/linux/set_memory.h
+++ include/linux/set_memory.h
@@ -12,6 +12,7 @@ static inline int set_memory_ro(unsigned long addr, int numpages) { return 0; }
 static inline int set_memory_rw(unsigned long addr, int numpages) { return 0; }
 static inline int set_memory_x(unsigned long addr,  int numpages) { return 0; }
 static inline int set_memory_nx(unsigned long addr, int numpages) { return 0; }
+static inline int set_memory_np(unsigned long addr, int numpages) { return 0; }
 #endif
 
 #ifndef set_memory_rox
