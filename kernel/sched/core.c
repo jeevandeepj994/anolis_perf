93bde07179f607c14b2b8b2e517708c2ab9d974d
Yi Tao <escape@linux.alibaba.com>
2022-11-11 15:04:55 +0800

    anolis: sched: Support cpu burst in cgroup v2
    ANBZ: #3019
    
    Some cloud native components are supporting cgroup v2 and depend on
    cpu burst, so support cpu burst in cgroup v2.
    
    Signed-off-by: Yi Tao <escape@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8542,16 +8542,22 @@ static int cpu_extra_stat_show(struct seq_file *sf,
 	{
 		struct task_group *tg = css_tg(css);
 		struct cfs_bandwidth *cfs_b = &tg->cfs_bandwidth;
-		u64 throttled_usec;
+		u64 throttled_usec, burst_usec;
 
 		throttled_usec = cfs_b->throttled_time;
 		do_div(throttled_usec, NSEC_PER_USEC);
+		burst_usec = cfs_b->burst_time;
+		do_div(burst_usec, NSEC_PER_USEC);
 
 		seq_printf(sf, "nr_periods %d\n"
 			   "nr_throttled %d\n"
-			   "throttled_usec %llu\n",
+			   "throttled_usec %llu\n"
+			   "current_bw %llu\n"
+			   "nr_bursts %d\n"
+			   "burst_usec %llu\n",
 			   cfs_b->nr_periods, cfs_b->nr_throttled,
-			   throttled_usec);
+			   throttled_usec, cfs_b->runtime, cfs_b->nr_burst,
+			   burst_usec);
 	}
 #endif
 	return 0;
@@ -8697,6 +8703,18 @@ static struct cftype cpu_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.seq_show = cpu_max_show,
 		.write = cpu_max_write,
+	},
+	{
+		.name = "max.burst",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_s64 = cpu_cfs_burst_read_s64,
+		.write_s64 = cpu_cfs_burst_write_s64,
+	},
+	{
+		.name = "max.init_buffer",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_s64 = cpu_cfs_init_buffer_read_s64,
+		.write_s64 = cpu_cfs_init_buffer_write_s64,
 	},
 #endif
 #ifdef CONFIG_UCLAMP_TASK_GROUP
