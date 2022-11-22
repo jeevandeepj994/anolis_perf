jeevandeepj994/anolis_perf

commit 8bac736a3db281494cf4e77423021182282f4bdd

anolis: kbuild: move elflib check out of KBUILD_EXTMOD
ANBZ: #744

Make external module with "make -C XXX M=XXX" will not check whether
libelf development package is installed when CONFIG_UN_WINDER_ORC is
on. As a result, the ko could not access ORC metadata and then fail
with related functions.

Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/769
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

Makefile
@@ -962,12 +962,22 @@ export mod_sign_cmd
 
 HOST_LIBELF_LIBS = $(shell pkg-config libelf --libs 2>/dev/null || echo -lelf)
 
+has_libelf = $(call try-run,\
+               echo "int main() {}" | $(HOSTCC) -xc -o /dev/null $(HOST_LIBELF_LIBS) -,1,0)
+
 ifdef CONFIG_STACK_VALIDATION
-  has_libelf := $(call try-run,\
-		echo "int main() {}" | $(HOSTCC) -xc -o /dev/null $(HOST_LIBELF_LIBS) -,1,0)
   ifeq ($(has_libelf),1)
     objtool_target := tools/objtool FORCE
   else
+    # CONFIG_STACK_VALIDATION is not yet support by CONFIG_X86_PIE and warning is displayed before.
+    ifndef CONFIG_X86_PIE
+      ifdef CONFIG_UNWINDER_ORC
+        @echo "error: Cannot generate ORC metadata for CONFIG_UNWINDER_ORC=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel" >&2
+        @false
+      else
+        @echo "warning: Cannot use CONFIG_STACK_VALIDATION=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel" >&2
+      endif
+    endif
     SKIP_STACK_VALIDATION := 1
     export SKIP_STACK_VALIDATION
   endif
@@ -1124,17 +1134,6 @@ uapi-asm-generic:
 
 PHONY += prepare-objtool
 prepare-objtool: $(objtool_target)
-ifeq ($(SKIP_STACK_VALIDATION),1)
-# CONFIG_STACK_VALIDATION is not yet support by CONFIG_X86_PIE and warning is displayed before.
-ifndef CONFIG_X86_PIE
-ifdef CONFIG_UNWINDER_ORC
-	@echo "error: Cannot generate ORC metadata for CONFIG_UNWINDER_ORC=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel" >&2
-	@false
-else
-	@echo "warning: Cannot use CONFIG_STACK_VALIDATION=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel" >&2
-endif
-endif
-endif
 
 # Generate some files
 # ---------------------------------------------------------------------------

commit b6d2d564bfbe746a3a54f9013a2393a80c7c9481

anolis: kbuild: fix error remind bug for libelf check
ANBZ: #3223

When libelf rpm package is not installed, the building will fail with
"Makefile:976: *** missing separator (did you mean TAB instead of
8 spaces?).  Stop."

Fix this by replace 8 spaces with TAB and reshape error and warning reminder.

Fixes: 8bac736a3db2 ("anolis: kbuild: move elflib check out of KBUILD_EXTMOD")
Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>

Makefile
@@ -972,10 +972,9 @@ ifdef CONFIG_STACK_VALIDATION
     # CONFIG_STACK_VALIDATION is not yet support by CONFIG_X86_PIE and warning is displayed before.
     ifndef CONFIG_X86_PIE
       ifdef CONFIG_UNWINDER_ORC
-        @echo "error: Cannot generate ORC metadata for CONFIG_UNWINDER_ORC=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel" >&2
-        @false
+        $(error "Cannot generate ORC metadata for CONFIG_UNWINDER_ORC=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel")
       else
-        @echo "warning: Cannot use CONFIG_STACK_VALIDATION=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel" >&2
+        $(warning "Cannot use CONFIG_STACK_VALIDATION=y, please install libelf-dev, libelf-devel or elfutils-libelf-devel")
       endif
     endif
     SKIP_STACK_VALIDATION := 1