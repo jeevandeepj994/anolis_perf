jeevandeepj994/anolis_perf

commit 474750dd8d40f56776cc9ca79563ff1dbb1f4437

anolis: net/smc: Introduce a sysctl to disable {a}symmetric link group
ANBZ: #1742

ANBZ: #264

When smc uses erdma as underlay implementation, smc link (rdma
connection) is created according to five-tupe of tcp connection.
However, the second smc link can't be created correctly if there
is no another tcp connection.

So we decide to disable {a}symmetric link group defaultly in erdma
environment by a sysctl as a workaround.

Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
Acked-by: Tony Lu <tonylu@linux.alibaba.com>
Acked-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/577

net/smc/af_smc.c
@@ -556,6 +556,7 @@ out:
 static int smcr_clnt_conf_first_link(struct smc_sock *smc)
 {
 	struct smc_link *link = smc->conn.lnk;
+	struct net *net = sock_net(&smc->sk);
 	struct smc_llc_qentry *qentry;
 	int rc;
 
@@ -602,20 +603,22 @@ static int smcr_clnt_conf_first_link(struct smc_sock *smc)
 	smc_llc_link_active(link);
 	smcr_lgr_set_type(link->lgr, SMC_LGR_SINGLE);
 
-	/* optional 2nd link, receive ADD LINK request from server */
-	qentry = smc_llc_wait(link->lgr, NULL, SMC_LLC_WAIT_TIME,
-			      SMC_LLC_ADD_LINK);
-	if (!qentry) {
-		struct smc_clc_msg_decline dclc;
+	if (!net->smc.sysctl_disable_multiple_link) {
+		/* optional 2nd link, receive ADD LINK request from server */
+		qentry = smc_llc_wait(link->lgr, NULL, SMC_LLC_WAIT_TIME,
+				      SMC_LLC_ADD_LINK);
+		if (!qentry) {
+			struct smc_clc_msg_decline dclc;
 
-		rc = smc_clc_wait_msg(smc, &dclc, sizeof(dclc),
-				      SMC_CLC_DECLINE, CLC_WAIT_TIME_SHORT);
-		if (rc == -EAGAIN)
-			rc = 0; /* no DECLINE received, go with one link */
-		return rc;
+			rc = smc_clc_wait_msg(smc, &dclc, sizeof(dclc),
+					      SMC_CLC_DECLINE, CLC_WAIT_TIME_SHORT);
+			if (rc == -EAGAIN)
+				rc = 0; /* no DECLINE received, go with one link */
+			return rc;
+		}
+		smc_llc_flow_qentry_clr(&link->lgr->llc_flow_lcl);
+		smc_llc_cli_add_link(link, qentry);
 	}
-	smc_llc_flow_qentry_clr(&link->lgr->llc_flow_lcl);
-	smc_llc_cli_add_link(link, qentry);
 	return 0;
 }
 
@@ -1802,6 +1805,7 @@ void smc_close_non_accepted(struct sock *sk)
 static int smcr_serv_conf_first_link(struct smc_sock *smc)
 {
 	struct smc_link *link = smc->conn.lnk;
+	struct net *net = sock_net(&smc->sk);
 	struct smc_llc_qentry *qentry;
 	int rc;
 
@@ -1842,8 +1846,10 @@ static int smcr_serv_conf_first_link(struct smc_sock *smc)
 	smc_llc_link_active(link);
 	smcr_lgr_set_type(link->lgr, SMC_LGR_SINGLE);
 
-	/* initial contact - try to establish second link */
-	smc_llc_srv_add_link(link, NULL);
+	if (!net->smc.sysctl_disable_multiple_link) {
+		/* initial contact - try to establish second link */
+		smc_llc_srv_add_link(link, NULL);
+	}
 	return 0;
 }
 

net/smc/smc_core.c
@@ -1678,6 +1678,9 @@ void smcr_port_add(struct smc_ib_device *smcibdev, u8 ibport)
 		    lgr->type == SMC_LGR_ASYMMETRIC_PEER ||
 		    !rdma_dev_access_netns(smcibdev->ibdev, lgr->net))
 			continue;
+		if (lgr->type == SMC_LGR_SINGLE &&
+		    lgr->net->smc.sysctl_disable_multiple_link)
+			continue;
 
 		/* trigger local add link processing */
 		link = smc_llc_usable_link(lgr);

net/smc/smc_llc.c
@@ -1090,6 +1090,9 @@ int smc_llc_cli_add_link(struct smc_link *link, struct smc_llc_qentry *qentry)
 		rc = -ENOMEM;
 		goto out_reject;
 	}
+	if (lgr->type == SMC_LGR_SINGLE &&
+	    lgr->net->smc.sysctl_disable_multiple_link)
+		goto out_reject;
 
 	ini->vlan_id = lgr->vlan_id;
 	if (lgr->smc_version == SMC_V2) {
@@ -1216,6 +1219,9 @@ static void smc_llc_cli_add_link_invite(struct smc_link *link,
 
 	if (lgr->type == SMC_LGR_SYMMETRIC ||
 	    lgr->type == SMC_LGR_ASYMMETRIC_PEER)
+		goto out;
+	if (lgr->type == SMC_LGR_SINGLE &&
+	    lgr->net->smc.sysctl_disable_multiple_link)
 		goto out;
 
 	ini = kzalloc(sizeof(*ini), GFP_KERNEL);
@@ -1462,6 +1468,9 @@ int smc_llc_srv_add_link(struct smc_link *link,
 		rc = -ENOMEM;
 		goto out;
 	}
+	if (lgr->type == SMC_LGR_SINGLE &&
+	    lgr->net->smc.sysctl_disable_multiple_link)
+		goto out;
 
 	/* ignore client add link recommendation, start new flow */
 	ini->vlan_id = lgr->vlan_id;

include/net/netns/smc.h
@@ -31,5 +31,6 @@ struct netns_smc {
 	int				sysctl_tcp2smc;
 	int				sysctl_allow_different_subnet;
 	int				sysctl_keep_first_contact_clcsock;
+	int				sysctl_disable_multiple_link;
 };
 #endif

net/smc/smc_sysctl.c
@@ -91,6 +91,15 @@ static struct ctl_table smc_table[] = {
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
+	{
+		.procname	= "disable_multiple_link",
+		.data		= &init_net.smc.sysctl_disable_multiple_link,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
 	{  }
 };
 
@@ -121,6 +130,7 @@ int __net_init smc_sysctl_net_init(struct net *net)
 	net->smc.sysctl_tcp2smc = 0;
 	net->smc.sysctl_allow_different_subnet = 1;
 	net->smc.sysctl_keep_first_contact_clcsock = 1;
+	net->smc.sysctl_disable_multiple_link = 1;
 
 	return 0;
 

commit 105f59f5f98b4aa3746fa1a4e55bf7f990e3251a

anolis: net/smc: disable multiple flag should keep in lgr struct
ANBZ: #3261

Fixes: 474750dd8d40 (net/smc: Introduce a sysctl to disable {a}symmetric link group)
Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>

net/smc/af_smc.c
@@ -615,7 +615,6 @@ out:
 static int smcr_clnt_conf_first_link(struct smc_sock *smc)
 {
 	struct smc_link *link = smc->conn.lnk;
-	struct net *net = sock_net(&smc->sk);
 	struct smc_llc_qentry *qentry;
 	int rc;
 
@@ -662,7 +661,7 @@ static int smcr_clnt_conf_first_link(struct smc_sock *smc)
 	smc_llc_link_active(link);
 	smcr_lgr_set_type(link->lgr, SMC_LGR_SINGLE);
 
-	if (!net->smc.sysctl_disable_multiple_link) {
+	if (!link->lgr->disable_multiple_link) {
 		/* optional 2nd link, receive ADD LINK request from server */
 		qentry = smc_llc_wait(link->lgr, NULL, SMC_LLC_WAIT_TIME,
 				      SMC_LLC_ADD_LINK);
@@ -1894,7 +1893,6 @@ void smc_close_non_accepted(struct sock *sk)
 static int smcr_serv_conf_first_link(struct smc_sock *smc)
 {
 	struct smc_link *link = smc->conn.lnk;
-	struct net *net = sock_net(&smc->sk);
 	struct smc_llc_qentry *qentry;
 	int rc;
 
@@ -1935,7 +1933,7 @@ static int smcr_serv_conf_first_link(struct smc_sock *smc)
 	smc_llc_link_active(link);
 	smcr_lgr_set_type(link->lgr, SMC_LGR_SINGLE);
 
-	if (!net->smc.sysctl_disable_multiple_link) {
+	if (!link->lgr->disable_multiple_link) {
 		/* initial contact - try to establish second link */
 		smc_llc_srv_add_link(link, NULL);
 	}

net/smc/smc_core.c
@@ -929,6 +929,8 @@ static int smc_lgr_create(struct smc_sock *smc, struct smc_init_info *ini)
 			goto free_wq;
 		smc_llc_lgr_init(lgr, smc);
 
+		lgr->disable_multiple_link =
+			!!sock_net(&smc->sk)->smc.sysctl_disable_multiple_link;
 		link_idx = SMC_SINGLE_LINK;
 		lnk = &lgr->lnk[link_idx];
 		smcr_link_iw_extension(&lnk->iw_conn_param, smc->clcsock->sk);
@@ -1708,7 +1710,7 @@ void smcr_port_add(struct smc_ib_device *smcibdev, u8 ibport)
 		    !rdma_dev_access_netns(smcibdev->ibdev, lgr->net))
 			continue;
 		if (lgr->type == SMC_LGR_SINGLE &&
-		    lgr->net->smc.sysctl_disable_multiple_link)
+		    lgr->disable_multiple_link)
 			continue;
 
 		/* trigger local add link processing */

net/smc/smc_core.h
@@ -339,6 +339,7 @@ struct smc_link_group {
 						/* rsn code for termination */
 			u8			nexthop_mac[ETH_ALEN];
 			u8			uses_gateway;
+			bool		disable_multiple_link;
 			__be32			saddr;
 						/* net namespace */
 			struct net		*net;

net/smc/smc_llc.c
@@ -1091,7 +1091,7 @@ int smc_llc_cli_add_link(struct smc_link *link, struct smc_llc_qentry *qentry)
 		goto out_reject;
 	}
 	if (lgr->type == SMC_LGR_SINGLE &&
-	    lgr->net->smc.sysctl_disable_multiple_link)
+	    lgr->disable_multiple_link)
 		goto out_reject;
 
 	ini->vlan_id = lgr->vlan_id;
@@ -1219,7 +1219,7 @@ static void smc_llc_cli_add_link_invite(struct smc_link *link,
 	    lgr->type == SMC_LGR_ASYMMETRIC_PEER)
 		goto out;
 	if (lgr->type == SMC_LGR_SINGLE &&
-	    lgr->net->smc.sysctl_disable_multiple_link)
+	    lgr->disable_multiple_link)
 		goto out;
 
 	ini = kzalloc(sizeof(*ini), GFP_KERNEL);
@@ -1467,7 +1467,7 @@ int smc_llc_srv_add_link(struct smc_link *link,
 		goto out;
 	}
 	if (lgr->type == SMC_LGR_SINGLE &&
-	    lgr->net->smc.sysctl_disable_multiple_link)
+	    lgr->disable_multiple_link)
 		goto out;
 
 	/* ignore client add link recommendation, start new flow */