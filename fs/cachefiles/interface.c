jeevandeepj994/anolis_perf

commit 679445f70359e0af0e069ec49db60d6d59229ce0

anolis: cachefiles: extract ondemand info field from cachefiles_object
ANBZ: #3213

cherry-picked from https://lore.kernel.org/lkml/20221014080559.42108-5-zhujia.zj@bytedance.com/T/#m77ed5d2ac5a3b779402ff50c1cb66e281d7cb3b9

We'll introduce a @work_struct field for @object in subsequent patches,
it will enlarge the size of @object.
As the result of that, this commit extracts ondemand info field from
@object.

Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Acked-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/894

fs/cachefiles/interface.c
@@ -51,6 +51,9 @@ static struct fscache_object *cachefiles_alloc_object(
 
 	fscache_object_init(&object->fscache, cookie, &cache->cache);
 
+	if (cachefiles_ondemand_init_obj_info(object))
+		goto nomem_obj_info;
+
 	object->type = cookie->def->type;
 
 	/* get hold of the raw key
@@ -102,6 +105,8 @@ static struct fscache_object *cachefiles_alloc_object(
 nomem_key:
 	kfree(buffer);
 nomem_buffer:
+	kfree(object->private);
+nomem_obj_info:
 	BUG_ON(test_bit(CACHEFILES_OBJECT_ACTIVE, &object->flags));
 	kmem_cache_free(cachefiles_object_jar, object);
 	fscache_object_destroyed(&cache->cache);
@@ -373,6 +378,7 @@ static void cachefiles_put_object(struct fscache_object *_object,
 		}
 
 		cache = object->fscache.cache;
+		kfree(object->private);
 		fscache_object_destroy(&object->fscache);
 		kmem_cache_free(cachefiles_object_jar, object);
 		fscache_object_destroyed(cache);

fs/cachefiles/internal.h
@@ -36,6 +36,12 @@ enum cachefiles_object_state {
 	CACHEFILES_ONDEMAND_OBJSTATE_open, /* Anonymous fd associated with object is available */
 };
 
+struct cachefiles_ondemand_info {
+	int				ondemand_id;
+	enum cachefiles_object_state	state;
+	struct cachefiles_object	*object;
+};
+
 /*
  * node records
  */
@@ -53,10 +59,7 @@ struct cachefiles_object {
 	uint8_t				new;		/* T if object new */
 	spinlock_t			work_lock;
 	struct rb_node			active_node;	/* link in active tree (dentry is key) */
-#ifdef CONFIG_CACHEFILES_ONDEMAND
-	int				ondemand_id;
-	enum cachefiles_object_state	state;
-#endif
+	struct cachefiles_ondemand_info	*private;
 };
 
 extern struct kmem_cache *cachefiles_object_jar;
@@ -270,17 +273,19 @@ extern void cachefiles_ondemand_clean_object(struct cachefiles_object *object);
 extern int cachefiles_ondemand_read(struct cachefiles_object *object,
 			     loff_t pos, size_t len);
 
+extern int cachefiles_ondemand_init_obj_info(struct cachefiles_object *object);
+
 #define CACHEFILES_OBJECT_STATE_FUNCS(_state)	\
 static inline bool								\
 cachefiles_ondemand_object_is_##_state(const struct cachefiles_object *object) \
 {												\
-	return object->state == CACHEFILES_ONDEMAND_OBJSTATE_##_state; \
+	return object->private->state == CACHEFILES_ONDEMAND_OBJSTATE_##_state; \
 }												\
 												\
 static inline void								\
 cachefiles_ondemand_set_object_##_state(struct cachefiles_object *object) \
 {												\
-	object->state = CACHEFILES_ONDEMAND_OBJSTATE_##_state; \
+	object->private->state = CACHEFILES_ONDEMAND_OBJSTATE_##_state; \
 }
 
 CACHEFILES_OBJECT_STATE_FUNCS(open);
@@ -304,6 +309,11 @@ static inline int cachefiles_ondemand_read(struct cachefiles_object *object,
 					   loff_t pos, size_t len)
 {
 	return -EOPNOTSUPP;
+}
+
+static inline int cachefiles_ondemand_init_obj_info(struct cachefiles_object *object)
+{
+	return 0;
 }
 #endif
 

fs/cachefiles/ondemand.c
@@ -9,17 +9,18 @@ static int cachefiles_ondemand_fd_release(struct inode *inode,
 					  struct file *file)
 {
 	struct cachefiles_object *object = file->private_data;
-	int object_id = object->ondemand_id;
 	struct cachefiles_cache *cache;
 	void **slot;
 	struct radix_tree_iter iter;
+	struct cachefiles_ondemand_info *info = object->private;
+	int object_id = info->ondemand_id;
 	struct cachefiles_req *req;
 
 	cache = container_of(object->fscache.cache,
 			     struct cachefiles_cache, cache);
 
 	xa_lock(&cache->reqs);
-	object->ondemand_id = CACHEFILES_ONDEMAND_ID_CLOSED;
+	info->ondemand_id = CACHEFILES_ONDEMAND_ID_CLOSED;
 	cachefiles_ondemand_set_object_close(object);
 
 	/*
@@ -221,7 +222,7 @@ static int cachefiles_ondemand_get_fd(struct cachefiles_req *req)
 	load = (void *)req->msg.data;
 	load->fd = fd;
 	req->msg.object_id = object_id;
-	object->ondemand_id = object_id;
+	object->private->ondemand_id = object_id;
 
 	cachefiles_get_unbind_pincount(cache);
 	return 0;
@@ -390,7 +391,7 @@ static int cachefiles_ondemand_send_req(struct cachefiles_object *object,
 	smp_mb();
 	if (opcode != CACHEFILES_OP_OPEN &&
 		!cachefiles_ondemand_object_is_open(object)) {
-		WARN_ON_ONCE(object->ondemand_id == 0);
+		WARN_ON_ONCE(object->private->ondemand_id == 0);
 		xa_unlock(&cache->reqs);
 		ret = -EIO;
 		goto out;
@@ -457,7 +458,7 @@ static int cachefiles_ondemand_init_close_req(struct cachefiles_req *req,
 	if (!cachefiles_ondemand_object_is_open(object))
 		return -ENOENT;
 
-	req->msg.object_id = object->ondemand_id;
+	req->msg.object_id = object->private->ondemand_id;
 	return 0;
 }
 
@@ -472,7 +473,7 @@ static int cachefiles_ondemand_init_read_req(struct cachefiles_req *req,
 	struct cachefiles_object *object = req->object;
 	struct cachefiles_read *load = (void *)req->msg.data;
 	struct cachefiles_read_ctx *read_ctx = private;
-	int object_id = object->ondemand_id;
+	int object_id = object->private->ondemand_id;
 
 	/* Stop enqueuing requests when daemon has closed anon_fd. */
 	if (!cachefiles_ondemand_object_is_open(object)) {
@@ -524,4 +525,20 @@ int cachefiles_ondemand_read(struct cachefiles_object *object,
 	return cachefiles_ondemand_send_req(object, CACHEFILES_OP_READ,
 			sizeof(struct cachefiles_read),
 			cachefiles_ondemand_init_read_req, &read_ctx);
+}
+
+int cachefiles_ondemand_init_obj_info(struct cachefiles_object *object)
+{
+	struct cachefiles_cache *cache;
+
+	cache = container_of(object->fscache.cache, struct cachefiles_cache, cache);
+	if (!cachefiles_in_ondemand_mode(cache))
+		return 0;
+
+	object->private = kzalloc(sizeof(struct cachefiles_ondemand_info), GFP_KERNEL);
+	if (!object->private)
+		return -ENOMEM;
+
+	object->private->object = object;
+	return 0;
 }

commit 843e0aecf076e5cbd4f3f05890231bf971d28be5

anolis: cachefiles: reset object->private to NULL when it's freed
ANBZ: #3213

cachefiles_object is allocated from cachefiles_object_jar slab cache
without zeroing.

Apart from cachefiles_alloc_object(), cachefiles_daemon_add_cache() also
allocates cachefiles_object directly from cachefiles_object_jar slab
cache, in which object->private is not initialized, while the allocated
cachefiles_object is still freed in cachefiles_put_object().  This is
reasonable since the cachefiles_object allocated in
cachefiles_daemon_add_cache() represents a directory rather than a data
file, while object->private is only used for data files.

However, if object->private is not reset to NULL when cachefiles_object
is freed, and then the cachefiles_object is allocated again in
cachefiles_alloc_object(), a wild pointer is exposed in object->private,
which can cause double-free or use-after-free.

Fixes: 679445f70359 ("anolis: cachefiles: extract ondemand info field from cachefiles_object")
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

fs/cachefiles/interface.c
@@ -106,6 +106,7 @@ nomem_key:
 	kfree(buffer);
 nomem_buffer:
 	kfree(object->private);
+	object->private = NULL;
 nomem_obj_info:
 	BUG_ON(test_bit(CACHEFILES_OBJECT_ACTIVE, &object->flags));
 	kmem_cache_free(cachefiles_object_jar, object);
@@ -379,6 +380,7 @@ static void cachefiles_put_object(struct fscache_object *_object,
 
 		cache = object->fscache.cache;
 		kfree(object->private);
+		object->private = NULL;
 		fscache_object_destroy(&object->fscache);
 		kmem_cache_free(cachefiles_object_jar, object);
 		fscache_object_destroyed(cache);