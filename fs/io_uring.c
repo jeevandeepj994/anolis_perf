jeevandeepj994/anolis_perf

commit cae05f877e94355dd268ee06994d8a38c15ec21d

anolis: io_uring: allow sqthread to inherit cpuacct from its creator
OpenAnolis Bug Tracker: 0000463

sqthread is forked from kthreadd, now we do some work to let it inherit
cpuacct from io_uring creator.

Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
Signed-off-by: Hao Xu <haoxu@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/io_uring.c
@@ -82,6 +82,7 @@
 #include <linux/io_uring.h>
 #include <linux/blk-cgroup.h>
 #include <linux/audit.h>
+#include <linux/cgroup.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/io_uring.h>
@@ -6905,6 +6906,92 @@ static void io_sqd_init_new(struct io_sq_data *sqd)
 	io_sqd_update_thread_idle(sqd);
 }
 
+struct cgrp_migr_info {
+	pid_t pid;
+	char *path;
+	struct work_struct work;
+};
+
+static void io_sq_cgrp_migr_work(struct work_struct *work)
+{
+	struct cgrp_migr_info *info;
+	struct file *file;
+	char pid_str[16] = {0};
+	loff_t pos = 0;
+
+	info = container_of(work, struct cgrp_migr_info, work);
+
+	file = filp_open(info->path, O_RDWR, 0600);
+	if (IS_ERR(file)) {
+		kfree(info->path);
+		printk(KERN_ERR "io_uring: open %s failed, error code:%ld\n",
+		       info->path, PTR_ERR(file));
+		return;
+	}
+
+	snprintf(pid_str, sizeof(pid_str), "%d", info->pid);
+	kernel_write(file, pid_str, strlen(pid_str), &pos);
+	filp_close(file, NULL);
+	kfree(info->path);
+}
+
+static int io_sq_attach_cpu_cgroup(struct task_struct *sqo_thread)
+{
+	struct cgroup_subsys_state *css_owner, *css_sq;
+	char *buf = NULL;
+	int ret = 0, len, size = 0;
+	struct cgrp_migr_info info;
+
+	/*
+	 * if they are in the same cgroup, just return.
+	 * two cases lead to the same cgroup situation:
+	 * - we've done the cgroup move before
+	 * - the cgroup of current task is in root cgroup
+	 *   (as sqthread is in root cgroup)
+	 */
+	css_owner = task_get_css(current, cpuacct_cgrp_id);
+	css_sq = task_get_css(sqo_thread, cpuacct_cgrp_id);
+	if (css_owner->cgroup == css_sq->cgroup)
+		goto out;
+
+	buf = kzalloc(4096, GFP_KERNEL);
+	if (!buf) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	size = snprintf(buf, 4096, "/sys/fs/cgroup/cpuacct");
+	ret = cgroup_path(css_owner->cgroup, buf + size, 4096 - size);
+	if (ret < 0)
+		goto out;
+	else if (ret >= (4096 - size)) {
+		ret = -ENAMETOOLONG;
+		goto out;
+	}
+	size += ret;
+
+	len = strlen("/tasks") + 1;
+	if (len > (4096 - size)) {
+		ret = -ENAMETOOLONG;
+		goto out;
+	}
+	snprintf(buf + size, len, "/tasks");
+
+	info.pid = task_pid_nr(sqo_thread);
+	info.path = buf;
+	INIT_WORK(&info.work, io_sq_cgrp_migr_work);
+
+	schedule_work(&info.work);
+	flush_scheduled_work();
+
+out:
+	css_put(css_owner);
+	css_put(css_sq);
+	if (ret < 0 && buf)
+		kfree(buf);
+	return ret;
+}
+
 static int io_sq_thread(void *data)
 {
 	struct cgroup_subsys_state *cur_css = NULL;
@@ -9691,6 +9778,23 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 
 	if (!(p->flags & IORING_SETUP_R_DISABLED))
 		io_sq_offload_start(ctx);
+
+	if ((p->flags & IORING_SETUP_SQ_AFF) &&
+	    (ctx->flags & IORING_SETUP_SQPOLL_PERCPU) && percpu_sqd) {
+		int cpu = p->sq_thread_cpu;
+		struct io_sq_data *sqd;
+
+		mutex_lock(&percpu_sqd_lock);
+		sqd = *per_cpu_ptr(percpu_sqd, cpu);
+		io_sq_thread_park(sqd);
+		sqd->thread->flags &= ~PF_NO_SETAFFINITY;
+		ret = io_sq_attach_cpu_cgroup(sqd->thread);
+		sqd->thread->flags |= PF_NO_SETAFFINITY;
+		io_sq_thread_unpark(sqd);
+		mutex_unlock(&percpu_sqd_lock);
+		if (ret < 0)
+			goto err;
+	}
 
 	memset(&p->sq_off, 0, sizeof(p->sq_off));
 	p->sq_off.head = offsetof(struct io_rings, sq.head);

commit 385323bd62f057b1ec54285e5b8906ba61f94179

anolis: io_uring: fix compile error when cgroup disabled
ANBZ: #3341

Fix the following compiling errors when disable cgroup:

fs/io_uring.c: In function ‘io_sq_attach_cpu_cgroup’:
fs/io_uring.c:7094:14: error: implicit declaration of function ‘task_get_css’ [-Werror=implicit-function-declaration]
 7094 |  css_owner = task_get_css(current, cpuacct_cgrp_id);
      |              ^~~~~~~~~~~~
fs/io_uring.c:7094:36: error: ‘cpuacct_cgrp_id’ undeclared (first use in this function)
 7094 |  css_owner = task_get_css(current, cpuacct_cgrp_id);
      |                                    ^~~~~~~~~~~~~~~
fs/io_uring.c:7094:36: note: each undeclared identifier is reported only once for each function it appears in
fs/io_uring.c:7096:15: error: dereferencing pointer to incomplete type ‘struct cgroup_subsys_state’
 7096 |  if (css_owner->cgroup == css_sq->cgroup)
      |               ^~
fs/io_uring.c:7106:8: error: implicit declaration of function ‘cgroup_path’; did you mean ‘cgroup_psi’? [-Werror=implicit-function-declaration]
 7106 |  ret = cgroup_path(css_owner->cgroup, buf + size, 4096 - size);
      |        ^~~~~~~~~~~
      |        cgroup_psi

Fixes: cae05f877e943 ("anolis: io_uring: allow sqthread to inherit cpuacct from its creator")
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/968

fs/io_uring.c
@@ -6977,6 +6977,7 @@ static void io_sqd_init_new(struct io_sq_data *sqd)
 	io_sqd_update_thread_idle(sqd);
 }
 
+#ifdef CONFIG_CGROUP_CPUACCT
 struct cgrp_migr_info {
 	pid_t pid;
 	char *path;
@@ -7062,6 +7063,7 @@ out:
 		kfree(buf);
 	return ret;
 }
+#endif /* CONFIG_CGROUP_CPUACCT */
 
 static int io_sq_thread(void *data)
 {
@@ -9917,6 +9919,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	if (!(p->flags & IORING_SETUP_R_DISABLED))
 		io_sq_offload_start(ctx);
 
+#ifdef CONFIG_CGROUP_CPUACCT
 	if ((p->flags & IORING_SETUP_SQ_AFF) &&
 	    (ctx->flags & IORING_SETUP_SQPOLL_PERCPU) && percpu_sqd) {
 		int cpu = p->sq_thread_cpu;
@@ -9933,6 +9936,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 		if (ret < 0)
 			goto err;
 	}
+#endif /* CONFIG_CGROUP_CPUACCT */
 
 	memset(&p->sq_off, 0, sizeof(p->sq_off));
 	p->sq_off.head = offsetof(struct io_rings, sq.head);