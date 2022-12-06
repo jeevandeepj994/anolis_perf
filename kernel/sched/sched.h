jeevandeepj994/anolis_perf

commit 6988de8b870cd8d7baa8e41337eadd9ae8daec69

ck: psi: Support PSI under cgroup v1
to #32751521

Export "cpu|io|memory.pressure" under cgroup v1 "cpuacct" subsystem.

Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>
Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>

kernel/sched/sched.h
@@ -2654,5 +2654,9 @@ static inline bool is_per_cpu_kthread(struct task_struct *p)
 }
 #endif
 
+#ifdef CONFIG_PSI
+extern struct cftype cgroup_v1_psi_files[];
+#endif
+
 void swake_up_all_locked(struct swait_queue_head *q);
 void __prepare_to_swait(struct swait_queue_head *q, struct swait_queue *wait);

include/linux/psi.h
@@ -14,6 +14,8 @@ struct css_set;
 extern struct static_key_false psi_disabled;
 extern struct psi_group psi_system;
 
+extern struct static_key_false psi_v1_disabled;
+
 void psi_init(void);
 
 void psi_task_change(struct task_struct *task, int clear, int set);

kernel/cgroup/cgroup.c
@@ -3677,6 +3677,34 @@ static void cgroup_pressure_release(struct kernfs_open_file *of)
 
 	psi_trigger_destroy(ctx->psi.trigger);
 }
+
+struct cftype cgroup_v1_psi_files[] = {
+	{
+		.name = "io.pressure",
+		.flags = CFTYPE_NO_PREFIX,
+		.seq_show = cgroup_io_pressure_show,
+		.write = cgroup_io_pressure_write,
+		.poll = cgroup_pressure_poll,
+		.release = cgroup_pressure_release,
+	},
+	{
+		.name = "memory.pressure",
+		.flags = CFTYPE_NO_PREFIX,
+		.seq_show = cgroup_memory_pressure_show,
+		.write = cgroup_memory_pressure_write,
+		.poll = cgroup_pressure_poll,
+		.release = cgroup_pressure_release,
+	},
+	{
+		.name = "cpu.pressure",
+		.flags = CFTYPE_NO_PREFIX,
+		.seq_show = cgroup_cpu_pressure_show,
+		.write = cgroup_cpu_pressure_write,
+		.poll = cgroup_pressure_poll,
+		.release = cgroup_pressure_release,
+	},
+	{ } /* terminate */
+};
 #endif /* CONFIG_PSI */
 
 static int cgroup_freeze_show(struct seq_file *seq, void *v)

kernel/sched/cpuacct.c
@@ -379,3 +379,26 @@ struct cgroup_subsys cpuacct_cgrp_subsys = {
 	.legacy_cftypes	= files,
 	.early_init	= true,
 };
+
+#ifdef CONFIG_PSI
+
+static bool psi_v1_enable;
+static int __init setup_psi_v1(char *str)
+{
+	return kstrtobool(str, &psi_v1_enable) == 0;
+}
+__setup("psi_v1=", setup_psi_v1);
+
+static int __init cgroup_v1_psi_init(void)
+{
+	if (!psi_v1_enable) {
+		static_branch_enable(&psi_v1_disabled);
+		return 0;
+	}
+
+	cgroup_add_legacy_cftypes(&cpuacct_cgrp_subsys, cgroup_v1_psi_files);
+	return 0;
+}
+
+late_initcall_sync(cgroup_v1_psi_init);
+#endif

kernel/sched/psi.c
@@ -145,6 +145,7 @@
 static int psi_bug __read_mostly;
 
 DEFINE_STATIC_KEY_FALSE(psi_disabled);
+DEFINE_STATIC_KEY_FALSE(psi_v1_disabled);
 
 #ifdef CONFIG_PSI_DEFAULT_DISABLED
 static bool psi_enable;
@@ -751,9 +752,16 @@ static struct psi_group *iterate_groups(struct task_struct *task, void **iter)
 #ifdef CONFIG_CGROUPS
 	struct cgroup *cgroup = NULL;
 
-	if (!*iter)
-		cgroup = task->cgroups->dfl_cgrp;
-	else if (*iter == &psi_system)
+	if (!*iter) {
+		if (static_branch_likely(&psi_v1_disabled))
+			cgroup = task->cgroups->dfl_cgrp;
+		else
+#ifdef CONFIG_CGROUP_CPUACCT
+			cgroup = task_cgroup(task, cpuacct_cgrp_id);
+#else
+			cgroup = NULL;
+#endif
+	} else if (*iter == &psi_system)
 		return NULL;
 	else
 		cgroup = cgroup_parent(*iter);

commit 57cb4b5f3fbe16d8cc33899db1fd7d4471fee59a

anolis: psi: fix compile error when cgroup disabled
ANBZ: #3341

Fix the following compiling errors when disable cgroup:

In file included from fs/io-wq.c:24:
fs/../kernel/sched/sched.h:2898:22: error: array type has incomplete element type ‘struct cftype’
 2898 | extern struct cftype cgroup_v1_psi_files[];
      |                      ^~~~~~~~~~~~~~~~~~~

Fixes: 6988de8b870cd ("ck: psi: Support PSI under cgroup v1")
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/968

kernel/sched/sched.h
@@ -2895,8 +2895,10 @@ static inline bool async_load_calc_enabled(void)
 #endif
 
 #ifdef CONFIG_PSI
+#ifdef CONFIG_CGROUPS
 extern struct cftype cgroup_v1_psi_files[];
-#endif
+#endif /* CONFIG_CGROUPS */
+#endif /* CONFIG_PSI */
 
 #ifndef CONFIG_RICH_CONTAINER_CG_SWITCH
 long tg_get_cfs_quota(struct task_group *tg);