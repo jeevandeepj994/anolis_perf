jeevandeepj994/anolis_perf

commit e3ff139aa09c14a395a6e0fa0f0f30b2e15e0536

erofs: add fscache mode check helper
ANBZ: #1507

commit 93b856bb5f66ae149ed91876b17c8c3fca576615 upstream.

Until then erofs is exactly blockdev based filesystem.

A new fscache-based mode is going to be introduced for erofs to support
scenarios where on-demand read semantics is needed, e.g. container
image distribution. In this case, erofs could be mounted from data blobs
through fscache.

Add a helper checking which mode erofs works in, and twist the code in
preparation for the upcoming fscache mode.

Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>
Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Link: https://lore.kernel.org/r/20220425122143.56815-11-jefflexu@linux.alibaba.com
Acked-by: Chao Yu <chao@kernel.org>
Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/erofs/internal.h
@@ -128,6 +128,11 @@ struct erofs_sb_info {
 #define set_opt(sbi, option)	((sbi)->mount_opt |= EROFS_MOUNT_##option)
 #define test_opt(sbi, option)	((sbi)->mount_opt & EROFS_MOUNT_##option)
 
+static inline bool erofs_is_fscache_mode(struct super_block *sb)
+{
+	return IS_ENABLED(CONFIG_EROFS_FS_ONDEMAND) && !sb->s_bdev;
+}
+
 #ifdef CONFIG_EROFS_FS_ZIP
 enum {
 	EROFS_ZIP_CACHE_DISABLED,

fs/erofs/super.c
@@ -168,24 +168,26 @@ static int erofs_init_devices(struct super_block *sb,
 		}
 		dis = ptr + erofs_blkoff(pos);
 
-		if (!sbi->bootstrap) {
-			bdev = blkdev_get_by_path(dif->path,
-						  FMODE_READ | FMODE_EXCL,
-						  sb->s_type);
-			if (IS_ERR(bdev)) {
-				err = PTR_ERR(bdev);
-				goto err_out;
-			}
-			dif->bdev = bdev;
-		} else {
-			struct file *f;
+		if (!erofs_is_fscache_mode(sb)) {
+			if (!sbi->bootstrap) {
+				bdev = blkdev_get_by_path(dif->path,
+							  FMODE_READ | FMODE_EXCL,
+							  sb->s_type);
+				if (IS_ERR(bdev)) {
+					err = PTR_ERR(bdev);
+					goto err_out;
+				}
+				dif->bdev = bdev;
+			} else {
+				struct file *f;
 
-			f = filp_open(dif->path, O_RDONLY | O_LARGEFILE, 0);
-			if (IS_ERR(f)) {
-				err = PTR_ERR(f);
-				goto err_out;
+				f = filp_open(dif->path, O_RDONLY | O_LARGEFILE, 0);
+				if (IS_ERR(f)) {
+					err = PTR_ERR(f);
+					goto err_out;
+				}
+				dif->blobfile = f;
 			}
-			dif->blobfile = f;
 		}
 		dif->blocks = le32_to_cpu(dis->blocks);
 		dif->mapped_blkaddr = le32_to_cpu(dis->mapped_blkaddr);

commit 50449439529fa3f6012d888a7dd9847631cb8cfd

anolis: erofs: fix erofs_is_fscache_mode()
ANBZ: #3269

Both fscache and RAFS mode work without bdev, thus enhance
erofs_is_fscache_mode() helper to make it distinguish from RAFS mode.

Otherwise it can cause NULL crash when attempt to mount in RAFS mode,
e.g. "mount -t erofs -o bootstrap_path=... none /mnt".

BUG: unable to handle kernel NULL pointer dereference at 0000000000000008
RIP: 0010:erofs_read_metabuf+0x145/0x190 [erofs]
Call Trace:
 erofs_read_superblock+0x57/0x2a0 [erofs]
 erofs_fill_super+0x18b/0x280 [erofs]
 mount_nodev+0x48/0xa0
 erofs_mount+0xbd/0x100 [erofs]
 mount_fs+0x35/0x160
 vfs_kern_mount.part.9+0x54/0x110
 do_mount+0x5af/0xc20
 ksys_mount+0x80/0xd0
 __x64_sys_mount+0x21/0x30
 do_syscall_64+0x5b/0x1d0
 entry_SYSCALL_64_after_hwframe+0x44/0xa9

Reported-by: Hao Gou <gouhao@uniontech.com>
Fixes: e3ff139aa09c ("erofs: add fscache mode check helper")
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

fs/erofs/internal.h
@@ -146,7 +146,9 @@ struct erofs_sb_info {
 
 static inline bool erofs_is_fscache_mode(struct super_block *sb)
 {
-	return IS_ENABLED(CONFIG_EROFS_FS_ONDEMAND) && !sb->s_bdev;
+	/* to distinguish from rafsv6 which also works in nodev mode */
+	return IS_ENABLED(CONFIG_EROFS_FS_ONDEMAND) && !sb->s_bdev &&
+	       EROFS_SB(sb)->fsid;
 }
 
 #ifdef CONFIG_EROFS_FS_ZIP