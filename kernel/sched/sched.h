alibaba/cloud-kernel

commit 1f49a73850325bb93601cc2240236114a0a7ca9a

alinux: psi: Support PSI under cgroup v1
Export "cpu|io|memory.pressure" under cgroup v1 "cpuacct" subsystem.

Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>

kernel/sched/sched.h
@@ -2251,3 +2251,7 @@ unsigned long scale_irq_capacity(unsigned long util, unsigned long irq, unsigned
 	return util;
 }
 #endif
+
+#ifdef CONFIG_PSI
+extern struct cftype cgroup_v1_psi_files[];
+#endif

init/Kconfig
@@ -532,6 +532,16 @@ config PSI_DEFAULT_DISABLED
 
 	  Say N if unsure.
 
+config PSI_CGROUP_V1
+	bool "Support PSI under cgroup v1"
+	default Y
+	depends on PSI
+	help
+	  If set, pressure stall information tracking will be used
+	  for cgroup v1 other than v2.
+
+	  Say N if unsure.
+
 endmenu # "CPU/Task time and stats accounting"
 
 config CPU_ISOLATION

kernel/cgroup/cgroup.c
@@ -3518,6 +3518,34 @@ static void cgroup_pressure_release(struct kernfs_open_file *of)
 {
 	psi_trigger_replace(&of->priv, NULL);
 }
+
+struct cftype cgroup_v1_psi_files[] = {
+	{
+		.name = "io.pressure",
+		.flags = CFTYPE_NO_PREFIX,
+		.seq_show = cgroup_io_pressure_show,
+		.write = cgroup_io_pressure_write,
+		.poll = cgroup_pressure_poll,
+		.release = cgroup_pressure_release,
+	},
+	{
+		.name = "memory.pressure",
+		.flags = CFTYPE_NO_PREFIX,
+		.seq_show = cgroup_memory_pressure_show,
+		.write = cgroup_memory_pressure_write,
+		.poll = cgroup_pressure_poll,
+		.release = cgroup_pressure_release,
+	},
+	{
+		.name = "cpu.pressure",
+		.flags = CFTYPE_NO_PREFIX,
+		.seq_show = cgroup_cpu_pressure_show,
+		.write = cgroup_cpu_pressure_write,
+		.poll = cgroup_pressure_poll,
+		.release = cgroup_pressure_release,
+	},
+	{ } /* terminate */
+};
 #endif /* CONFIG_PSI */
 
 static int cgroup_file_open(struct kernfs_open_file *of)

kernel/sched/cpuacct.c
@@ -373,3 +373,13 @@ struct cgroup_subsys cpuacct_cgrp_subsys = {
 	.legacy_cftypes	= files,
 	.early_init	= true,
 };
+
+#ifdef CONFIG_PSI
+static int __init cgroup_v1_psi_init(void)
+{
+	cgroup_add_legacy_cftypes(&cpuacct_cgrp_subsys, cgroup_v1_psi_files);
+	return 0;
+}
+
+late_initcall_sync(cgroup_v1_psi_init);
+#endif

kernel/sched/psi.c
@@ -725,7 +725,11 @@ static struct psi_group *iterate_groups(struct task_struct *task, void **iter)
 	struct cgroup *cgroup = NULL;
 
 	if (!*iter)
+#ifdef CONFIG_PSI_CGROUP_V1
+		cgroup = task_cgroup(task, cpuacct_cgrp_id);
+#else
 		cgroup = task->cgroups->dfl_cgrp;
+#endif
 	else if (*iter == &psi_system)
 		return NULL;
 	else

jeevandeepj994/anolis_perf

commit 5d0f0e710b44d2d8256e5f61c65be4968886c58a

anolis: psi: fix compile error when cgroup disabled
ANBZ: #3342

Fix the following compiling errors when disable cgroup:

In file included from kernel/livepatch/transition.c:27:
kernel/livepatch/../sched/sched.h:2503:22: error: array type has incomplete element type ‘struct cftype’
 2503 | extern struct cftype cgroup_v1_psi_files[];

Fixes: 1f49a7385032 ("alinux: psi: Support PSI under cgroup v1")
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

kernel/sched/sched.h
@@ -2499,8 +2499,10 @@ static inline void task_ca_update_block(struct task_struct *tsk,
 #endif
 
 #ifdef CONFIG_PSI
+#ifdef CONFIG_CGROUPS
 extern struct cftype cgroup_v1_psi_files[];
-#endif
+#endif /* CONFIG_CGROUPS */
+#endif /* CONFIG_PSI */
 
 #ifndef CONFIG_RICH_CONTAINER_CG_SWITCH
 long tg_get_cfs_quota(struct task_group *tg);