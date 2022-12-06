alibaba/cloud-kernel

commit 40af84221636ec53d288b4c5ad7033fb7bc6c749

alinux: io_uring: allow sqthread to inherit cpuacct from its creator
to #33617702

sqthread is forked from kthreadd, now we do some work to let it inherit
cpuacct from io_uring creator.

Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
Signed-off-by: Hao Xu <haoxu@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/io_uring.c
@@ -80,6 +80,7 @@
 #include <linux/splice.h>
 #include <linux/task_work.h>
 #include <linux/pagemap.h>
+#include <linux/cgroup.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/io_uring.h>
@@ -6793,6 +6794,123 @@ static void io_sqd_init_new(struct sq_thread_percpu *t)
 	io_sqd_update_thread_idle(t);
 }
 
+static inline void io_sq_thread_unpark(struct sq_thread_percpu *t)
+{
+	kthread_unpark(t->sqo_thread);
+}
+
+static inline void io_sq_thread_park(struct sq_thread_percpu *t)
+{
+	kthread_park(t->sqo_thread);
+}
+
+struct cgrp_migr_info {
+	int cpu;
+	char *path;
+	struct work_struct work;
+};
+
+static void io_sq_cgrp_migr_work(struct work_struct *work)
+{
+	struct cgrp_migr_info *info;
+	struct sq_thread_percpu *t;
+	struct file *file;
+	pid_t pid;
+	char pid_str[16] = {0};
+	loff_t pos = 0;
+
+	info = container_of(work, struct cgrp_migr_info, work);
+	t = per_cpu_ptr(percpu_threads, info->cpu);
+	file = filp_open(info->path, O_RDWR, 0600);
+	if (IS_ERR(file)) {
+		kfree(info->path);
+		printk(KERN_ERR "io_uring: open %s failed, error code:%ld\n",
+		       info->path, PTR_ERR(file));
+		return;
+	}
+	mutex_lock(&t->lock);
+	if (!t->sqo_thread)
+		goto out;
+
+	pid = task_pid_nr(t->sqo_thread);
+	snprintf(pid_str, sizeof(pid_str), "%d", pid);
+	io_sq_thread_park(t);
+	t->sqo_thread->flags &= ~PF_NO_SETAFFINITY;
+	kernel_write(file, pid_str, strlen(pid_str), &pos);
+	t->sqo_thread->flags |= PF_NO_SETAFFINITY;
+	io_sq_thread_unpark(t);
+
+out:
+	mutex_unlock(&t->lock);
+	filp_close(file, NULL);
+	kfree(info->path);
+}
+
+static int io_sq_attach_cpu_cgroup(int cpu)
+{
+	struct cgroup_subsys_state *css_owner, *css_sq;
+	char *buf = NULL;
+	int ret = 0, len, size = 0;
+	struct cgrp_migr_info info;
+	struct sq_thread_percpu *t = per_cpu_ptr(percpu_threads, cpu);
+
+	mutex_lock(&t->lock);
+	if (!t->sqo_thread) {
+		mutex_unlock(&t->lock);
+		return 0;
+	}
+
+	/*
+	 * if they are in the same cgroup, just return.
+	 * two cases lead to the same cgroup situation:
+	 * - we've done the cgroup move before
+	 * - the cgroup of current task is in root cgroup
+	 *   (as sqthread is in root cgroup)
+	 */
+	css_owner = task_get_css(current, cpuacct_cgrp_id);
+	css_sq = task_get_css(t->sqo_thread, cpuacct_cgrp_id);
+	mutex_unlock(&t->lock);
+	if (css_owner->cgroup == css_sq->cgroup)
+		goto out;
+
+	buf = kzalloc(4096, GFP_KERNEL);
+	if (!buf) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	size = snprintf(buf, 4096, "/sys/fs/cgroup/cpuacct");
+	ret = cgroup_path(css_owner->cgroup, buf + size, 4096 - size);
+	if (ret < 0)
+		goto out;
+	else if (ret >= (4096 - size)) {
+		ret = -ENAMETOOLONG;
+		goto out;
+	}
+	size += ret;
+
+	len = strlen("/tasks") + 1;
+	if (len > (4096 - size)) {
+		ret = -ENAMETOOLONG;
+		goto out;
+	}
+	snprintf(buf + size, len, "/tasks");
+
+	info.cpu = cpu;
+	info.path = buf;
+	INIT_WORK(&info.work, io_sq_cgrp_migr_work);
+
+	schedule_work(&info.work);
+	flush_work(&info.work);
+
+out:
+	css_put(css_owner);
+	css_put(css_sq);
+	if (ret < 0 && buf)
+		kfree(buf);
+	return ret;
+}
+
 static int io_sq_thread_percpu(void *data)
 {
 	struct sq_thread_percpu *t = data;
@@ -7709,16 +7827,6 @@ static int io_init_wq_offload(struct io_ring_ctx *ctx,
 out_fput:
 	fdput(f);
 	return ret;
-}
-
-static inline void io_sq_thread_unpark(struct sq_thread_percpu *t)
-{
-	kthread_unpark(t->sqo_thread);
-}
-
-static inline void io_sq_thread_park(struct sq_thread_percpu *t)
-{
-	kthread_park(t->sqo_thread);
 }
 
 static void create_sq_thread_percpu(struct io_ring_ctx *ctx, int cpu)
@@ -8937,6 +9045,13 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	ret = io_sq_offload_start(ctx, p);
 	if (ret)
 		goto err;
+
+	if ((p->flags & IORING_SETUP_SQ_AFF) &&
+	    (ctx->flags & IORING_SETUP_SQPOLL_PERCPU) && percpu_threads) {
+		ret = io_sq_attach_cpu_cgroup(p->sq_thread_cpu);
+		if (ret < 0)
+			goto err;
+	}
 
 	memset(&p->sq_off, 0, sizeof(p->sq_off));
 	p->sq_off.head = offsetof(struct io_rings, sq.head);

jeevandeepj994/anolis_perf

commit 09bc59663ee270cbc28b1a430be2f0d5c2b7aad0

anolis: io_uring: fix compile error when cgroup disabled
ANBZ: #3342

Fix the following compiling errors when disable cgroup:

fs/io_uring.c: In function ‘io_sq_attach_cpu_cgroup’:
fs/io_uring.c:6865:14: error: implicit declaration of function ‘task_get_css’ [-Werror=implicit-function-declaration]
 6865 |  css_owner = task_get_css(current, cpuacct_cgrp_id);
      |              ^~~~~~~~~~~~
fs/io_uring.c:6865:36: error: ‘cpuacct_cgrp_id’ undeclared (first use in this function)
 6865 |  css_owner = task_get_css(current, cpuacct_cgrp_id);
      |                                    ^~~~~~~~~~~~~~~
fs/io_uring.c:6865:36: note: each undeclared identifier is reported only once for each function it appears in
fs/io_uring.c:6868:15: error: dereferencing pointer to incomplete type ‘struct cgroup_subsys_state’
 6868 |  if (css_owner->cgroup == css_sq->cgroup)
      |               ^~
fs/io_uring.c:6878:8: error: implicit declaration of function ‘cgroup_path’; did you mean ‘cgroup_psi’? [-Werror=implicit-function-declaration]
 6878 |  ret = cgroup_path(css_owner->cgroup, buf + size, 4096 - size);
      |        ^~~~~~~~~~~
      |        cgroup_psi

Fixes: 40af84221636 ("alinux: io_uring: allow sqthread to inherit cpuacct from its creator")
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/io_uring.c
@@ -6798,6 +6798,7 @@ static inline void io_sq_thread_park(struct sq_thread_percpu *t)
 	kthread_park(t->sqo_thread);
 }
 
+#ifdef CONFIG_CGROUP_CPUACCT
 struct cgrp_migr_info {
 	int cpu;
 	char *path;
@@ -6904,6 +6905,7 @@ out:
 		kfree(buf);
 	return ret;
 }
+#endif /* CONFIG_CGROUP_CPUACCT */
 
 static int io_sq_thread_percpu(void *data)
 {
@@ -9040,12 +9042,14 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	if (ret)
 		goto err;
 
+#ifdef CONFIG_CGROUP_CPUACCT
 	if ((p->flags & IORING_SETUP_SQ_AFF) &&
 	    (ctx->flags & IORING_SETUP_SQPOLL_PERCPU) && percpu_threads) {
 		ret = io_sq_attach_cpu_cgroup(p->sq_thread_cpu);
 		if (ret < 0)
 			goto err;
 	}
+#endif /* CONFIG_CGROUP_CPUACCT */
 
 	memset(&p->sq_off, 0, sizeof(p->sq_off));
 	p->sq_off.head = offsetof(struct io_rings, sq.head);