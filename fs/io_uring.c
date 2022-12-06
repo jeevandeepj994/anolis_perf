7a8aad98d1a5c319ef27b44248b23744ee7c923d
Joseph Qi <joseph.qi@linux.alibaba.com>
2022-12-06 10:19:15 +0800

    anolis: io_uring: fix compile error when cgroup disabled
    ANBZ: #3341
    
    Fix the following compiling errors when disable cgroup:
    
    fs/io_uring.c: In function ‘io_sq_attach_cpu_cgroup’:
    fs/io_uring.c:7094:14: error: implicit declaration of function ‘task_get_css’ [-Werror=implicit-function-declaration]
     7094 |  css_owner = task_get_css(current, cpuacct_cgrp_id);
          |              ^~~~~~~~~~~~
    fs/io_uring.c:7094:36: error: ‘cpuacct_cgrp_id’ undeclared (first use in this function)
     7094 |  css_owner = task_get_css(current, cpuacct_cgrp_id);
          |                                    ^~~~~~~~~~~~~~~
    fs/io_uring.c:7094:36: note: each undeclared identifier is reported only once for each function it appears in
    fs/io_uring.c:7096:15: error: dereferencing pointer to incomplete type ‘struct cgroup_subsys_state’
     7096 |  if (css_owner->cgroup == css_sq->cgroup)
          |               ^~
    fs/io_uring.c:7106:8: error: implicit declaration of function ‘cgroup_path’; did you mean ‘cgroup_psi’? [-Werror=implicit-function-declaration]
     7106 |  ret = cgroup_path(css_owner->cgroup, buf + size, 4096 - size);
          |        ^~~~~~~~~~~
          |        cgroup_psi
    
    Fixes: cae05f877e943 ("anolis: io_uring: allow sqthread to inherit cpuacct from its creator")
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -6977,6 +6977,7 @@ static void io_sqd_init_new(struct io_sq_data *sqd)
 	io_sqd_update_thread_idle(sqd);
 }
 
+#ifdef CONFIG_CGROUP_CPUACCT
 struct cgrp_migr_info {
 	pid_t pid;
 	char *path;
@@ -7062,6 +7063,7 @@ out:
 		kfree(buf);
 	return ret;
 }
+#endif /* CONFIG_CGROUP_CPUACCT */
 
 static int io_sq_thread(void *data)
 {
@@ -9917,6 +9919,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	if (!(p->flags & IORING_SETUP_R_DISABLED))
 		io_sq_offload_start(ctx);
 
+#ifdef CONFIG_CGROUP_CPUACCT
 	if ((p->flags & IORING_SETUP_SQ_AFF) &&
 	    (ctx->flags & IORING_SETUP_SQPOLL_PERCPU) && percpu_sqd) {
 		int cpu = p->sq_thread_cpu;
@@ -9933,6 +9936,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 		if (ret < 0)
 			goto err;
 	}
+#endif /* CONFIG_CGROUP_CPUACCT */
 
 	memset(&p->sq_off, 0, sizeof(p->sq_off));
 	p->sq_off.head = offsetof(struct io_rings, sq.head);
