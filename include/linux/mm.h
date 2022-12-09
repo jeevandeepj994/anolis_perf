jeevandeepj994/anolis_perf

commit fea195bb4ff307305ce5a27db44ca7d300f77e07

anolis: mm: gup: allow to follow _PAGE_DEVMAP && !ZONE_DEVICE pages optionally
ANBZ: #2586

Calling get_user_pages() on a range of user memory that has been mmaped from a
DAX file will fail when there are no 'struct page' to describe those pages.
This problem has been addressed in some device drivers by adding optional struct
page support for pages under the control of the driver. To be specific, gup woud
query pgmap_radix tree first and acquire a reference against a pgmap instance
before it ever touches the page. This is to be sure the struct page won't be
released under the nose of gup. And to add any memory into pgmap_radix, they
mustn't be overlapped with the existing PFNs.

However, there are cases that the users need to map the normal memory via DAX,
and without losing the support of gup. Usually this could be done with a memmap
parameter appended into the kernel cmdline to produce a fake PMEM device. Which
is not flexible and sometimes unpractical (imagine how could you expose a BRD
via DAX? A BRD device can't be stacked onto another PMEM device anyway).

Given that what gup really cares is merely to make sure the existence of the
page, and if the struct pages behind the DAX entries (_PAGE_DEVMAP entries) are
actually from the normal memory, gup won't need to bother if they will be
released on the fly. Therefore, it should be fine to fix the code like below,

static inline bool pg_stable_pfn(unsigned long pfn)
{
	int nid = pfn_to_nid(pfn);
	struct zone *zone_device = &NODE_DATA(nid)->node_zones[ZONE_DEVICE];

	if (zone_spans_pfn(zone_device, pfn))
		return false;
	else
		return true;
}

[and later in gup]
	page = vm_normal_page(vma, address, pte);
	if (!page && pte_devmap(pte) && (flags & FOLL_GET)) {
		if (pg_stable_pfn(pte_pfn(pte)))
			page = pte_page(pte);
		else {
			/*
			 * Only return device mapping pages in the FOLL_GET case
			 * since they are only valid while holding the pgmap
			 * reference.
			 */

include/linux/mm.h
@@ -2771,6 +2771,7 @@ static inline struct page *follow_page(struct vm_area_struct *vma,
 #define FOLL_REMOTE	0x2000	/* we are working on non-current tsk/mm */
 #define FOLL_COW	0x4000	/* internal GUP flag */
 #define FOLL_ANON	0x8000	/* don't do file mappings */
+#define FOLL_GET_PGSTABLE 0x80000000	/* do get_page on page not in ZONE_DEVICE */
 
 static inline int vm_fault_to_errno(vm_fault_t vm_fault, int foll_flags)
 {

mm/gup.c
@@ -79,6 +79,28 @@ static inline bool should_force_cow_break(struct vm_area_struct *vma, unsigned i
 	return is_cow_mapping(vma->vm_flags) && (flags & FOLL_GET);
 }
 
+#ifdef CONFIG_ZONE_DEVICE
+static inline bool pg_stable_pfn(unsigned long pfn)
+{
+	int nid = pfn_to_nid(pfn);
+	struct zone *zone_device = &NODE_DATA(nid)->node_zones[ZONE_DEVICE];
+
+	/*
+	 * The other zones' pages are not kept alive by pgmap
+	 * but memory hotplug instead.
+	 */
+	if (zone_spans_pfn(zone_device, pfn))
+		return false;
+	else
+		return true;
+}
+#else
+static inline bool pg_stable_pfn(unsigned long pfn)
+{
+	return false;
+}
+#endif
+
 static struct page *follow_page_pte(struct vm_area_struct *vma,
 		unsigned long address, pmd_t *pmd, unsigned int flags)
 {
@@ -122,8 +144,24 @@ retry:
 	page = vm_normal_page(vma, address, pte);
 	if (!page && pte_devmap(pte) && (flags & FOLL_GET)) {
 		/*
-		 * Only return device mapping pages in the FOLL_GET case since
-		 * they are only valid while holding the pgmap reference.
+		 * The pages which are not in ZONE_DEVICE don't depend on
+		 * the pgmap ref to keep alive (stable). Therefore if we are
+		 * looking at such a page and the caller explicitly claimed that
+		 * he doesn't really care whether this page is from ZONE_DEVICE
+		 * or not - either a page from ZONE_NORMAL ZONE_MOVABLE etc or
+		 * a page from ZONE_DEVICE are both fine - we could simply
+		 * get_page for those stable ones straightly.
+		 */
+		if ((flags & FOLL_GET_PGSTABLE) &&
+			pg_stable_pfn(pte_pfn(pte))) {
+			page = pte_page(pte);
+			goto page_required;
+		}
+
+		/*
+		 * Only return device mapping pages in the FOLL_GET case
+		 * since they are only valid while holding the pgmap
+		 * reference.
 		 */
 		pgmap = get_dev_pagemap(pte_pfn(pte), NULL);
 		if (pgmap)
@@ -148,6 +186,7 @@ retry:
 		}
 	}
 
+page_required:
 	if (flags & FOLL_SPLIT && PageTransCompound(page)) {
 		int ret;
 		get_page(page);

virt/kvm/kvm_main.c
@@ -1448,7 +1448,8 @@ static bool hva_to_pfn_fast(unsigned long addr, bool write_fault,
 static int hva_to_pfn_slow(unsigned long addr, bool *async, bool write_fault,
 			   bool *writable, kvm_pfn_t *pfn)
 {
-	unsigned int flags = FOLL_HWPOISON;
+	/* The purpose of calling GUP below is only to get a reference. */
+	unsigned int flags = FOLL_HWPOISON | FOLL_GET_PGSTABLE;
 	struct page *page;
 	int npages = 0;
 

commit e2e5d099d47295cbde284cca455132e09a0512be

anolis: mm: gup:remove FOLL_GET_PGSTABLE flag checking
ANBZ: #3400

When supporting gup of normal memory via DAX case, a FOLL_GET_PGSTABLE
flag is marked to restrict the pg_stable_pfn() usage on GUP to KVM only.
But it is not right to assume that only KVM will use the GUP, given the
memory is shared by various tasks such as io thread.

Actually if dax gup page is not in ZONE_DEVICE, it is safe to decide that
page has a stable page struct.

This patch removes the checking of FOLL_GET_PGSTABLE flag.

Fixes: fea195bb4ff3 ("anolis: mm: gup: allow to follow _PAGE_DEVMAP && !ZONE_DEVICE pages optionally")
Signed-off-by: Simon Guo <wei.guo.simon@linux.alibaba.com>
Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
Reviewed-by: Zhu Yanhai <gaoyang.zyh@taobao.com>

include/linux/mm.h
@@ -2957,7 +2957,6 @@ struct page *follow_page(struct vm_area_struct *vma, unsigned long address,
 #define FOLL_SPLIT_PMD	0x20000	/* split huge pmd before returning */
 #define FOLL_PIN	0x40000	/* pages must be released via unpin_user_page */
 #define FOLL_FAST_ONLY	0x80000	/* gup_fast: prevent fall-back to slow gup */
-#define FOLL_GET_PGSTABLE 0x80000000	/* do get_page on page not in ZONE_DEVICE */
 
 /*
  * FOLL_PIN and FOLL_LONGTERM may be used in various combinations with each

mm/gup.c
@@ -482,14 +482,10 @@ retry:
 		/*
 		 * The pages which are not in ZONE_DEVICE don't depend on
 		 * the pgmap ref to keep alive (stable). Therefore if we are
-		 * looking at such a page and the caller explicitly claimed that
-		 * he doesn't really care whether this page is from ZONE_DEVICE
-		 * or not - either a page from ZONE_NORMAL ZONE_MOVABLE etc or
-		 * a page from ZONE_DEVICE are both fine - we could simply
-		 * get_page for those stable ones straightly.
+		 * looking at such a page - we could simply get_page for
+		 * those stable ones straightly.
 		 */
-		if ((flags & FOLL_GET_PGSTABLE) &&
-			pg_stable_pfn(pte_pfn(pte))) {
+		if (pg_stable_pfn(pte_pfn(pte))) {
 			page = pte_page(pte);
 			goto page_required;
 		}

virt/kvm/kvm_main.c
@@ -1867,8 +1867,7 @@ static bool hva_to_pfn_fast(unsigned long addr, bool write_fault,
 static int hva_to_pfn_slow(unsigned long addr, bool *async, bool write_fault,
 			   bool *writable, kvm_pfn_t *pfn)
 {
-	/* The purpose of calling GUP below is only to get a reference. */
-	unsigned int flags = FOLL_HWPOISON | FOLL_GET_PGSTABLE;
+	unsigned int flags = FOLL_HWPOISON;
 	struct page *page;
 	int npages = 0;
 