jeevandeepj994/anolis_perf

commit f8ecbc39993a49aadf4ccce20e6e5eed68bf9c9c

anolis: cachefiles: narrow the scope of triggering EPOLLIN events in ondemand mode
ANBZ: #3213

cherry-picked from https://lore.kernel.org/lkml/20221014080559.42108-5-zhujia.zj@bytedance.com/T/#m9cb8de9d34a8e366bbec1bc74e6ccf769fff6360

Don't trigger EPOLLIN when there are only reopening read requests in
xarray.

Suggested-by: Xin Yin <yinxin.x@bytedance.com>
Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Acked-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/894

fs/cachefiles/daemon.c
@@ -359,14 +359,26 @@ static __poll_t cachefiles_daemon_poll(struct file *file,
 					   struct poll_table_struct *poll)
 {
 	struct cachefiles_cache *cache = file->private_data;
+	struct cachefiles_req *req;
+	struct radix_tree_iter iter;
 	__poll_t mask;
+	void **slot;
 
 	poll_wait(file, &cache->daemon_pollwq, poll);
 	mask = 0;
 
 	if (cachefiles_in_ondemand_mode(cache)) {
-		if (!radix_tree_empty(&cache->reqs))
-			mask |= EPOLLIN;
+		if (!radix_tree_empty(&cache->reqs)) {
+			radix_tree_for_each_tagged(slot, &cache->reqs, &iter, 0,
+					CACHEFILES_REQ_NEW) {
+				req = radix_tree_deref_slot_protected(slot,
+						&cache->reqs.xa_lock);
+				if (!cachefiles_ondemand_is_reopening_read(req)) {
+					mask |= EPOLLIN;
+					break;
+				}
+			}
+		}
 	} else {
 		if (test_bit(CACHEFILES_STATE_CHANGED, &cache->flags))
 			mask |= EPOLLIN;

fs/cachefiles/internal.h
@@ -293,6 +293,13 @@ cachefiles_ondemand_set_object_##_state(struct cachefiles_object *object) \
 CACHEFILES_OBJECT_STATE_FUNCS(open);
 CACHEFILES_OBJECT_STATE_FUNCS(close);
 CACHEFILES_OBJECT_STATE_FUNCS(reopening);
+
+static inline bool cachefiles_ondemand_is_reopening_read(struct cachefiles_req *req)
+{
+	return cachefiles_ondemand_object_is_reopening(req->object) &&
+			req->msg.opcode == CACHEFILES_OP_READ;
+}
+
 #else
 static inline ssize_t cachefiles_ondemand_daemon_read(struct cachefiles_cache *cache,
 				char __user *_buffer, size_t buflen, loff_t *pos)
@@ -317,6 +324,11 @@ static inline int cachefiles_ondemand_read(struct cachefiles_object *object,
 static inline int cachefiles_ondemand_init_obj_info(struct cachefiles_object *object)
 {
 	return 0;
+}
+
+static inline bool cachefiles_ondemand_is_reopening_read(struct cachefiles_req *req)
+{
+	return false;
 }
 #endif
 

commit 324ecaaa10fefb0e3d94b547e3170e40b90cda1f

anolis: cachefiles: add missing lock protection when polling
ANBZ: #3213

Add missing lock protection in poll routine when iterating xarray,
otherwise:

1. The radix_tree API itself doesn't imply RCU read lock, thus we may
encounter UAF when dereferencing slot without radix_tree's lock held.

2. Even with RCU read lock held, only the slot of the radix tree is
ensured to be pinned there, while the data structure (e.g. struct
cachefiles_req) stored in the slot has no such guarantee.  The poll
routine will iterate the radix tree and dereference cachefiles_req
accordingly.  Thus RCU read lock is not adequate in this case and
spinlock is needed here.

3. Otherwise, radix_tree_deref_slot_protected() will fail
lockdep_is_held() checking.

Fixes: f8ecbc39993a ("anolis: cachefiles: narrow the scope of triggering EPOLLIN events in ondemand mode")
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

fs/cachefiles/daemon.c
@@ -370,6 +370,7 @@ static __poll_t cachefiles_daemon_poll(struct file *file,
 
 	if (cachefiles_in_ondemand_mode(cache)) {
 		if (!radix_tree_empty(&cache->reqs)) {
+			xa_lock(&cache->reqs);
 			radix_tree_for_each_tagged(slot, &cache->reqs, &iter, 0,
 					CACHEFILES_REQ_NEW) {
 				req = radix_tree_deref_slot_protected(slot,
@@ -379,6 +380,7 @@ static __poll_t cachefiles_daemon_poll(struct file *file,
 					break;
 				}
 			}
+			xa_unlock(&cache->reqs);
 		}
 	} else {
 		if (test_bit(CACHEFILES_STATE_CHANGED, &cache->flags))