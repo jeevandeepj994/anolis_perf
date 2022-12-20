d1f2fa9474b379ebc089bd77f5bda228c0cd2ec1
Zelin Deng <zelin.deng@linux.alibaba.com>
2022-12-20 14:43:16 +0800

    anolis: x86/setup: No extra memory for crashkernel for high and @offset
    ANBZ: #3435
    
    Commit 332a8143515c ("anolis: x86/setup: reserve extra memory for swiotlb when sme/sev is enabled")
    reserves extra memory for crashkernel when sev/sme is enabled. However
    if crashkernel=xxx,high is set, reserve_crashkernel_low() will allocate
    extra memory for swiotlb later, so that there's no need to reserve more
    extra memory for it.
    
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Guanjun <guanjun@linux.alibaba.com>
    Acked-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1014

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -518,6 +518,7 @@ static int __init reserve_crashkernel_low(void)
 static void __init reserve_crashkernel(void)
 {
 	unsigned long long crash_size, crash_base, total_mem;
+	unsigned long long swiotlb_sz = 0;
 	bool high = false;
 	int ret;
 
@@ -539,14 +540,9 @@ static void __init reserve_crashkernel(void)
 		return;
 	}
 
-	if (sev_active() || sme_active()) {
-		unsigned long long swiotlb_sz;
-
+	/* NOTE: Only reserve extra memory under 4G and without @offset */
+	if (sev_active() || sme_active())
 		swiotlb_sz = ALIGN(swiotlb_size_or_default(), SZ_1M);
-		crash_size += swiotlb_sz;
-		pr_info("Reserving %ldMB of extra memory for crashkernel\n",
-			(unsigned long)swiotlb_sz >> 20);
-	}
 
 	/* 0 means: find the address automatically */
 	if (!crash_base) {
@@ -558,14 +554,24 @@ static void __init reserve_crashkernel(void)
 		 * So try low memory first and fall back to high memory
 		 * unless "crashkernel=size[KMG],high" is specified.
 		 */
-		if (!high)
+		if (!high) {
+			crash_size += swiotlb_sz;
 			crash_base = memblock_phys_alloc_range(crash_size,
 						CRASH_ALIGN, CRASH_ALIGN,
 						CRASH_ADDR_LOW_MAX);
-		if (!crash_base)
+			pr_info("Reserving %ldMB of extra memory for crashkernel\n",
+				(unsigned long)swiotlb_sz >> 20);
+		}
+		if (!crash_base) {
+			/*
+			 * Failed to reserve under 4G then try high,
+			 * so crash_size needs to fall back
+			 */
+			crash_size -= swiotlb_sz;
 			crash_base = memblock_phys_alloc_range(crash_size,
 						CRASH_ALIGN, CRASH_ALIGN,
 						CRASH_ADDR_HIGH_MAX);
+		}
 		if (!crash_base) {
 			pr_info("crashkernel reservation failed - No suitable area found.\n");
 			return;
@@ -581,6 +587,7 @@ static void __init reserve_crashkernel(void)
 		}
 	}
 
+	/* If reserves above 4G, reserve extra mem for DMA & swiotlb under 4G */
 	if (crash_base >= (1ULL << 32) && reserve_crashkernel_low()) {
 		memblock_free(crash_base, crash_size);
 		return;
