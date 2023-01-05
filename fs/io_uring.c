jeevandeepj994/anolis_perf

commit 7e895139db59e8fd0149d3f67a834e46fecab97d

io_uring: add support for 128-byte SQEs
ANBZ: #3541

commit ebdeb7c01d025cb059f05dc26b9dc914e46dd43f upstream

Normal SQEs are 64-bytes in length, which is fine for all the commands
we support. However, in preparation for supporting passthrough IO,
provide an option for setting up a ring with 128-byte SQEs.

We continue to use the same type for io_uring_sqe, it's marked and
commented with a zero sized array pad at the end. This provides up
to 80 bytes of data for a passthrough command - 64 bytes for the
extra added data, and 16 bytes available at the end of the existing
SQE.

Signed-off-by: Jens Axboe <axboe@kernel.dk>
[Ziyang Zhang: modify ring_pages() for SQE128]
Signed-off-by: Ziyang Zhang <ZiyangZhang@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1017

fs/io_uring.c
@@ -6697,8 +6697,12 @@ static const struct io_uring_sqe *io_get_sqe(struct io_ring_ctx *ctx)
 	 *    though the application is the one updating it.
 	 */
 	head = READ_ONCE(sq_array[ctx->cached_sq_head & ctx->sq_mask]);
-	if (likely(head < ctx->sq_entries))
+	if (likely(head < ctx->sq_entries)) {
+		/* double index for 128-byte SQEs, twice as long */
+		if (ctx->flags & IORING_SETUP_SQE128)
+			head <<= 1;
 		return &ctx->sq_sqes[head];
+	}
 
 	/* drop invalid entries */
 	ctx->cached_sq_dropped++;
@@ -8495,14 +8499,19 @@ static unsigned long rings_size(unsigned sq_entries, unsigned cq_entries,
 	return off;
 }
 
-static unsigned long ring_pages(unsigned sq_entries, unsigned cq_entries)
+static unsigned long ring_pages(struct io_ring_ctx *ctx, unsigned sq_entries,
+				  unsigned cq_entries)
 {
 	size_t pages;
 
 	pages = (size_t)1 << get_order(
 		rings_size(sq_entries, cq_entries, NULL));
-	pages += (size_t)1 << get_order(
-		array_size(sizeof(struct io_uring_sqe), sq_entries));
+	if (ctx->flags & IORING_SETUP_SQE128)
+		pages += (size_t)1 << get_order(
+			array_size(2 * sizeof(struct io_uring_sqe), sq_entries));
+	else
+		pages += (size_t)1 << get_order(
+			array_size(sizeof(struct io_uring_sqe), sq_entries));
 
 	return pages;
 }
@@ -8975,7 +8984,7 @@ static void io_ring_ctx_wait_and_kill(struct io_ring_ctx *ctx)
 	 * is closed but resources aren't reaped yet. This can cause
 	 * spurious failure in setting up a new ring.
 	 */
-	io_unaccount_mem(ctx, ring_pages(ctx->sq_entries, ctx->cq_entries),
+	io_unaccount_mem(ctx, ring_pages(ctx, ctx->sq_entries, ctx->cq_entries),
 			 ACCT_LOCKED);
 
 	INIT_WORK(&ctx->exit_work, io_ring_exit_work);
@@ -9730,7 +9739,10 @@ static int io_allocate_scq_urings(struct io_ring_ctx *ctx,
 	ctx->sq_mask = rings->sq_ring_mask;
 	ctx->cq_mask = rings->cq_ring_mask;
 
-	size = array_size(sizeof(struct io_uring_sqe), p->sq_entries);
+	if (p->flags & IORING_SETUP_SQE128)
+		size = array_size(2 * sizeof(struct io_uring_sqe), p->sq_entries);
+	else
+		size = array_size(sizeof(struct io_uring_sqe), p->sq_entries);
 	if (size == SIZE_MAX) {
 		io_mem_free(ctx->rings);
 		ctx->rings = NULL;
@@ -9846,7 +9858,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 
 	if (limit_mem) {
 		ret = __io_account_mem(user,
-				ring_pages(p->sq_entries, p->cq_entries));
+				ring_pages(ctx, p->sq_entries, p->cq_entries));
 		if (ret) {
 			free_uid(user);
 			return ret;
@@ -9856,7 +9868,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	ctx = io_ring_ctx_alloc(p);
 	if (!ctx) {
 		if (limit_mem)
-			__io_unaccount_mem(user, ring_pages(p->sq_entries,
+			__io_unaccount_mem(user, ring_pages(ctx, p->sq_entries,
 								p->cq_entries));
 		free_uid(user);
 		return -ENOMEM;
@@ -9904,7 +9916,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	 * do this before hitting the general error path, as ring freeing
 	 * will un-account as well.
 	 */
-	io_account_mem(ctx, ring_pages(p->sq_entries, p->cq_entries),
+	io_account_mem(ctx, ring_pages(ctx, p->sq_entries, p->cq_entries),
 		       ACCT_LOCKED);
 	ctx->limit_mem = limit_mem;
 
@@ -10013,8 +10025,8 @@ static long io_uring_setup(u32 entries, struct io_uring_params __user *params)
 	if (p.flags & ~(IORING_SETUP_IOPOLL | IORING_SETUP_SQPOLL |
 			IORING_SETUP_SQ_AFF | IORING_SETUP_CQSIZE |
 			IORING_SETUP_CLAMP | IORING_SETUP_ATTACH_WQ |
-			IORING_SETUP_R_DISABLED | IORING_SETUP_SQPOLL_PERCPU |
-			IORING_SETUP_IDLE_US))
+			IORING_SETUP_R_DISABLED | IORING_SETUP_SQE128 |
+			IORING_SETUP_SQPOLL_PERCPU | IORING_SETUP_IDLE_US))
 		return -EINVAL;
 
 	return  io_uring_create(entries, &p, params);

include/uapi/linux/io_uring.h
@@ -55,6 +55,11 @@ struct io_uring_sqe {
 	__u16	personality;
 	__s32	splice_fd_in;
 	__u64	__pad2[2];
+	/*
+	 * If the ring is initialized with IORING_SETUP_SQE128, then this field
+	 * contains 64-bytes of padding, doubling the size of the SQE.
+	 */
+	__u64	__big_sqe_pad[0];
 };
 
 enum {
@@ -92,6 +97,7 @@ enum {
 #define IORING_SETUP_CLAMP	(1U << 4)	/* clamp SQ/CQ ring sizes */
 #define IORING_SETUP_ATTACH_WQ	(1U << 5)	/* attach to existing wq */
 #define IORING_SETUP_R_DISABLED	(1U << 6)	/* start with ring disabled */
+#define IORING_SETUP_SQE128		(1U << 10) /* SQEs are 128 byte */
 #define IORING_SETUP_IDLE_US	(1U << 30)	/* unit of thread_idle is macrosecond */
 #define IORING_SETUP_SQPOLL_PERCPU	(1U << 31)	/* use percpu SQ poll thread */
 

commit 07ad77e109d68ce484542a3048c1b5fc9c22f509

anolis: io_uring: fix referencing uninitialized io_uring_ctx in ring_pages()
ANBZ: #3613

ring_pages() called in io_uring_create() references ctx->flags while
ctx is not allocated yet.

Fix this bug by directly passing flags to ring_pages. In
io_uring_create() we pass p->flags.

Fixes: 7e895139db59 ("io_uring: add support for 128-byte SQEs")
Signed-off-by: Ziyang Zhang <ZiyangZhang@linux.alibaba.com>

fs/io_uring.c
@@ -8601,14 +8601,14 @@ static unsigned long rings_size(unsigned sq_entries, unsigned cq_entries,
 	return off;
 }
 
-static unsigned long ring_pages(struct io_ring_ctx *ctx, unsigned sq_entries,
-				  unsigned cq_entries)
+static unsigned long ring_pages(unsigned sq_entries, unsigned cq_entries,
+				unsigned int flags)
 {
 	size_t pages;
 
 	pages = (size_t)1 << get_order(
 		rings_size(sq_entries, cq_entries, NULL));
-	if (ctx->flags & IORING_SETUP_SQE128)
+	if (flags & IORING_SETUP_SQE128)
 		pages += (size_t)1 << get_order(
 			array_size(2 * sizeof(struct io_uring_sqe), sq_entries));
 	else
@@ -9086,7 +9086,8 @@ static void io_ring_ctx_wait_and_kill(struct io_ring_ctx *ctx)
 	 * is closed but resources aren't reaped yet. This can cause
 	 * spurious failure in setting up a new ring.
 	 */
-	io_unaccount_mem(ctx, ring_pages(ctx, ctx->sq_entries, ctx->cq_entries),
+	io_unaccount_mem(ctx, ring_pages(ctx->sq_entries, ctx->cq_entries,
+					 ctx->flags),
 			 ACCT_LOCKED);
 
 	INIT_WORK(&ctx->exit_work, io_ring_exit_work);
@@ -9959,8 +9960,9 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	limit_mem = !capable(CAP_IPC_LOCK);
 
 	if (limit_mem) {
-		ret = __io_account_mem(user,
-				ring_pages(ctx, p->sq_entries, p->cq_entries));
+		ret = __io_account_mem(user, ring_pages(p->sq_entries,
+							p->cq_entries,
+							p->flags));
 		if (ret) {
 			free_uid(user);
 			return ret;
@@ -9970,8 +9972,9 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	ctx = io_ring_ctx_alloc(p);
 	if (!ctx) {
 		if (limit_mem)
-			__io_unaccount_mem(user, ring_pages(ctx, p->sq_entries,
-								p->cq_entries));
+			__io_unaccount_mem(user, ring_pages(p->sq_entries,
+							    p->cq_entries,
+							    p->flags));
 		free_uid(user);
 		return -ENOMEM;
 	}
@@ -10018,7 +10021,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	 * do this before hitting the general error path, as ring freeing
 	 * will un-account as well.
 	 */
-	io_account_mem(ctx, ring_pages(ctx, p->sq_entries, p->cq_entries),
+	io_account_mem(ctx, ring_pages(p->sq_entries, p->cq_entries, p->flags),
 		       ACCT_LOCKED);
 	ctx->limit_mem = limit_mem;
 