f5e688cd121942da6f554e54fd7d7806d41830f8
Shawn Wang <shawnwang@linux.alibaba.com>
2023-01-30 14:42:58 +0800

    anolis: arm64: Speed up address bits (physical/virtual) reading in /proc/cpuinfo
    ANBZ: #9696
    
    commit 0f4ab4308b4e ("anolis: arm64: Expose address bits (physical/virtual) via cpuinfo")
    exposes the physical and virtual address bits for ARM CPUs in
    /proc/cpuinfo. Each acquisition of these values will read
    SYS_ID_AA64MMFR0_EL1 and SYS_ID_AA64MMFR2_EL1 registers by
    read_sanitised_ftr_reg(), which could be slow when /proc/cpuinfo is
    accessed frequently during startup. Since the values of these registers
    will not be changed, speed up reading of /proc/cpuinfo by using the
    stored register values in struct cpuinfo_arm64.
    
    Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1136
    Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3657

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -862,12 +862,10 @@ static inline u32 id_aa64mmfr0_parange_to_phys_shift(int parange)
 	}
 }
 
-static inline u32 id_aa64mmfr0_pa_range_bits(void)
+static inline u32 id_aa64mmfr0_pa_range_bits(u64 mmfr0)
 {
-	u64 mmfr0;
 	u32 parange;
 
-	mmfr0 = read_sanitised_ftr_reg(SYS_ID_AA64MMFR0_EL1);
 	parange = cpuid_feature_extract_unsigned_field(mmfr0,
 				ID_AA64MMFR0_EL1_PARANGE_SHIFT);
 	return id_aa64mmfr0_parange_to_phys_shift(parange);
@@ -882,12 +880,10 @@ static inline u32 id_aa64mmfr2_varange_to_virt_shift(int varange)
 	}
 }
 
-static inline u32 id_aa64mmfr2_va_range_bits(void)
+static inline u32 id_aa64mmfr2_va_range_bits(u64 mmfr2)
 {
-	u64 mmfr2;
 	u32 varange;
 
-	mmfr2 = read_sanitised_ftr_reg(SYS_ID_AA64MMFR2_EL1);
 	varange = cpuid_feature_extract_unsigned_field(mmfr2, ID_AA64MMFR2_EL1_VARange_SHIFT);
 	return id_aa64mmfr2_varange_to_virt_shift(varange);
 }
