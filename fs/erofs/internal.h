4590e91946874cd84c8df1946f008d1d987fbd54
Jingbo Xu <jefflexu@linux.alibaba.com>
2023-02-09 10:48:25 +0800

    erofs: remove unused EROFS_GET_BLOCKS_RAW flag
    ANBZ: #4627
    
    commit 8b58f9f02162124c2149779af401c8115c70b649 upstream.
    
    For erofs_map_blocks() and erofs_map_blocks_flatmode(), the flags
    argument is always EROFS_GET_BLOCKS_RAW.  Thus remove the unused flags
    parameter for these two functions.
    
    Besides EROFS_GET_BLOCKS_RAW is originally introduced for reading
    compressed (raw) data for compressed files.  However it's never used
    actually and let's remove it now.
    
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Yue Hu <huyue2@coolpad.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Link: https://lore.kernel.org/r/20230209024825.17335-2-jefflexu@linux.alibaba.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1561

--- fs/erofs/internal.h
+++ fs/erofs/internal.h
@@ -424,9 +424,6 @@ struct erofs_map_blocks {
 	unsigned int m_flags;
 };
 
-/* Flags used by erofs_map_blocks_flatmode() */
-#define EROFS_GET_BLOCKS_RAW    0x0001
-
 /* zmap.c */
 #ifdef CONFIG_EROFS_FS_ZIP
 int z_erofs_fill_inode(struct inode *inode);
@@ -459,8 +456,7 @@ void *erofs_bread(struct erofs_buf *buf, struct inode *inode,
 void *erofs_read_metabuf(struct erofs_buf *buf, struct super_block *sb,
 			 erofs_blk_t blkaddr, enum erofs_kmap_type type);
 int erofs_map_dev(struct super_block *sb, struct erofs_map_dev *dev);
-int erofs_map_blocks(struct inode *inode,
-		     struct erofs_map_blocks *map, int flags);
+int erofs_map_blocks(struct inode *inode, struct erofs_map_blocks *map);
 extern const struct file_operations erofs_file_fops;
 
 /* inode.c */
