jeevandeepj994/anolis_perf

commit a07f55b5f686677333b96a8c188d6c6c6cd6e006

ck: fuse: add sysfs api to flush processing queue requests
fix #32727882

The failover of fuse userspace daemon will reuse the existing fuse conn,
without unmounting it, during daemon crashing and recovery procedure.
But some requests might be in process in the daemon before sending out reply,
when the crash happens. This will stuck the application since it will
never get the reply after the failover.

We add the sysfs api to flush these requests, after the daemon crash, before
recovery. It is easy to reproduce the issue in the fuse userspace daemon,
just exit after receiving the request and before sending the reply back.
The application will hang up in some read/write operation, before
echo 1 > /sys/fs/fuse/connection/xxx/flush. The flush operation will make
the io fail and return the error to the application.

Signed-off-by: Ma Jie Yue <majieyue@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
Acked-by: Caspar Zhang <caspar@linux.alibaba.com>
Signed-off-by: Hao Xu <haoxu@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/fuse/dev.c
@@ -2232,6 +2232,41 @@ int fuse_dev_release(struct inode *inode, struct file *file)
 }
 EXPORT_SYMBOL_GPL(fuse_dev_release);
 
+/*
+ * Flush all pending processing requests.
+ *
+ * The failover procedure will reuse the fuse_conn after usersapce crash and
+ * recovery. But the reqs in the processing queue will never get the reply,
+ * making the application stuck forever.
+ *
+ * So we need to flush these reqs by sysfs api. We only flush the reqs in
+ * the processing queue, because these reqs have been sent to userspace.
+ * Firstly we need to dequeue the req from the processing queue, and secondly
+ * we need to call request_end to finish it.
+ */
+void fuse_flush_pq(struct fuse_conn *fc)
+{
+	struct fuse_dev *fud;
+	LIST_HEAD(to_end);
+
+	spin_lock(&fc->lock);
+	if (!fc->connected) {
+		spin_unlock(&fc->lock);
+		return;
+	}
+	list_for_each_entry(fud, &fc->devices, entry) {
+		struct fuse_pqueue *fpq = &fud->pq;
+
+		spin_lock(&fpq->lock);
+		WARN_ON(!list_empty(&fpq->io));
+		list_splice_init(fpq->processing, &to_end);
+		spin_unlock(&fpq->lock);
+	}
+	spin_unlock(&fc->lock);
+
+	end_requests(&to_end);
+}
+
 static int fuse_dev_fasync(int fd, struct file *file, int on)
 {
 	struct fuse_dev *fud = fuse_get_dev(file);

fs/fuse/control.c
@@ -44,6 +44,18 @@ static ssize_t fuse_conn_abort_write(struct file *file, const char __user *buf,
 	return count;
 }
 
+static ssize_t fuse_conn_flush_write(struct file *file, const char __user *buf,
+					 size_t count, loff_t *ppos)
+{
+	struct fuse_conn *fc = fuse_ctl_file_conn_get(file);
+
+	if (fc) {
+		fuse_flush_pq(fc);
+		fuse_conn_put(fc);
+	}
+	return count;
+}
+
 static ssize_t fuse_conn_waiting_read(struct file *file, char __user *buf,
 				      size_t len, loff_t *ppos)
 {
@@ -207,6 +219,12 @@ static const struct file_operations fuse_ctl_abort_ops = {
 	.llseek = no_llseek,
 };
 
+static const struct file_operations fuse_ctl_flush_ops = {
+	.open = nonseekable_open,
+	.write = fuse_conn_flush_write,
+	.llseek = no_llseek,
+};
+
 static const struct file_operations fuse_ctl_waiting_ops = {
 	.open = nonseekable_open,
 	.read = fuse_conn_waiting_read,
@@ -291,6 +309,8 @@ int fuse_ctl_add_conn(struct fuse_conn *fc)
 				 NULL, &fuse_ctl_waiting_ops) ||
 	    !fuse_ctl_add_dentry(parent, fc, "abort", S_IFREG | 0200, 1,
 				 NULL, &fuse_ctl_abort_ops) ||
+	    !fuse_ctl_add_dentry(parent, fc, "flush", S_IFREG | 0200, 1,
+				 NULL, &fuse_ctl_flush_ops) ||
 	    !fuse_ctl_add_dentry(parent, fc, "max_background", S_IFREG | 0600,
 				 1, NULL, &fuse_conn_max_background_ops) ||
 	    !fuse_ctl_add_dentry(parent, fc, "congestion_threshold",

fs/fuse/fuse_i.h
@@ -45,7 +45,7 @@
 #define FUSE_NAME_MAX 1024
 
 /** Number of dentries for each connection in the control filesystem */
-#define FUSE_CTL_NUM_DENTRIES 5
+#define FUSE_CTL_NUM_DENTRIES 6
 
 /** List of active connections */
 extern struct list_head fuse_conn_list;
@@ -1016,6 +1016,9 @@ void fuse_request_end(struct fuse_req *req);
 /* Abort all requests */
 void fuse_abort_conn(struct fuse_conn *fc);
 void fuse_wait_aborted(struct fuse_conn *fc);
+
+/* Flush all requests in processing queue */
+void fuse_flush_pq(struct fuse_conn *fc);
 
 /**
  * Invalidate inode attributes

commit 50fdd732db0ab28a061f4eb9429beb631daa2c20

anolis: fuse: fix sysfs fuse connection flush and resend api
ANBZ: #4259

Due to the change of the kernel data structure "struct fuse_pqueue",
its structure member processing has changed from a linked list to a
hash table. The old implementation method cannot correctly traverse all
requests in the processing queue, resulting in invalid operation of the
flush and resend APIs of the sysfs.

This patch fixes this problem.

Fixes: a07f55b5f686 ("ck: fuse: add sysfs api to flush processing queue requests")
Fixes: 017eaddf79d7 ("anolis: fuse: add sysfs api to resend processing queue requests")

Signed-off-by: Chen Zhao <winters.zc@antgroup.com>

fs/fuse/dev.c
@@ -2286,6 +2286,7 @@ void fuse_flush_pq(struct fuse_conn *fc)
 {
 	struct fuse_dev *fud;
 	LIST_HEAD(to_end);
+	unsigned int i;
 
 	spin_lock(&fc->lock);
 	if (!fc->connected) {
@@ -2297,7 +2298,8 @@ void fuse_flush_pq(struct fuse_conn *fc)
 
 		spin_lock(&fpq->lock);
 		WARN_ON(!list_empty(&fpq->io));
-		list_splice_init(fpq->processing, &to_end);
+		for (i = 0; i < FUSE_PQ_HASH_SIZE; i++)
+			list_splice_init(&fpq->processing[i], &to_end);
 		spin_unlock(&fpq->lock);
 	}
 	spin_unlock(&fc->lock);
@@ -2326,6 +2328,7 @@ void fuse_resend_pqueue(struct fuse_conn *fc)
 	struct fuse_req *req, *next;
 	struct fuse_iqueue *fiq = &fc->iq;
 	LIST_HEAD(to_queue);
+	unsigned int i;
 
 	spin_lock(&fc->lock);
 	if (!fc->connected) {
@@ -2345,7 +2348,8 @@ void fuse_resend_pqueue(struct fuse_conn *fc)
 			}
 			spin_unlock(&req->waitq.lock);
 		}
-		list_splice_tail_init(fpq->processing, &to_queue);
+		for (i = 0; i < FUSE_PQ_HASH_SIZE; i++)
+			list_splice_tail_init(&fpq->processing[i], &to_queue);
 		spin_unlock(&fpq->lock);
 	}
 	spin_unlock(&fc->lock);