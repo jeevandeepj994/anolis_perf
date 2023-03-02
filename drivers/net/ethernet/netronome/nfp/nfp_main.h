fc130e70cb4c03fb1ef456b5742912c7c64bbc80
Yinjun Zhang <yinjun.zhang@corigine.com>
2023-03-02 18:28:06 +0800

    anolis: nfp: redefine PF id used to format symbols
    ANBZ: #7792
    
    Taking account that NFP3800 supports 4 physcial functions per
    controller, now recalcuate PF id that used to format symbols
    to communicate with application firmware.
    
    Signed-off-by: Yinjun Zhang <yinjun.zhang@corigine.com>

--- drivers/net/ethernet/netronome/nfp/nfp_main.h
+++ drivers/net/ethernet/netronome/nfp/nfp_main.h
@@ -206,4 +206,6 @@ int nfp_shared_buf_pool_set(struct nfp_pf *pf, unsigned int sb,
 
 int nfp_devlink_params_register(struct nfp_pf *pf);
 void nfp_devlink_params_unregister(struct nfp_pf *pf);
+
+u8 nfp_get_pf_id(struct nfp_pf *pf);
 #endif /* NFP_MAIN_H */
