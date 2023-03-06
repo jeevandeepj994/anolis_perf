jeevandeepj994/anolis_perf

commit dbc6d94883be519b6ef81c5f39440d67ef48be4e

anolis: mm: support filling in zero page if read fault in file hole
ANBZ: #2510

The valid range might be only a small part of the sparse file, the reset
are ranges all filled by 0, which is called hole. For file mapping, if
a read fault occurs at the range of file hole, it also allocates page
and add it in the page cache, which caused the waste of memory.

If a read fault of a tmpfs file hole occurs, we fill it with the system
zero page to avoid memory waste. Judging if the corresponding vma is
MAP_PRIVATE and the fault type is not VMFAULT_WRITE, this pte can be
assigned to the system zero page.

Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Reviewed-by: zhong jiang <zhongjiang-ali@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/785
Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>

mm/memory.c
@@ -3746,6 +3746,10 @@ static vm_fault_t __do_fault(struct vm_fault *vmf)
 		return poisonret;
 	}
 
+	/* Do not lock the zero page */
+	if (unlikely(is_zero_page(vmf->page)))
+		return ret;
+
 	if (unlikely(!(ret & VM_FAULT_LOCKED)))
 		lock_page(vmf->page);
 	else
@@ -3935,11 +3939,41 @@ vm_fault_t alloc_set_pte(struct vm_fault *vmf, struct page *page)
 
 	flush_icache_page(vma, page);
 	entry = mk_pte(page, vma->vm_page_prot);
+
+	/*
+	 * If it's zero page, vmf->ptl should be held to avoid other VMAs that share
+	 * the same xarray do the same page fault simultaneously, which may
+	 * leads to wrong semantic of MMAP_PRIVATE.
+	 *
+	 * E.g:
+	 *          MMAP_PRIVATE                         MMAP_SHARED
+	 *          do_read_fault
+	 *						do_shared_fault
+	 *	    check pagecache
+	 *						   alloc_page
+	 *						add_to_pagecache
+	 *					      try_to_unmap_zeropage
+	 *		set_pte
+	 *
+	 * In this scenario, zero page can not be unmapped.
+	 */
+	if (unlikely(is_zero_page(page))) {
+		/*
+		 * If found the page cache entry here, corresponding page cache
+		 * has been set. Thus retry it to get the valid page cache not
+		 * the zero page.
+		 */
+		struct page *new_page = find_get_entry(vma->vm_file->f_mapping,
+						       vmf->pgoff);
+		if (new_page)
+			return VM_FAULT_RETRY;
+
+		entry = pte_mkspecial(entry);
+	}
+
 	entry = pte_sw_mkyoung(entry);
 	if (write)
 		entry = maybe_mkwrite(pte_mkdirty(entry), vma);
-	if (unlikely(is_zero_page(page)))
-		entry = pte_mkspecial(entry);
 	/* copy-on-write page */
 	if (write && !(vma->vm_flags & VM_SHARED)) {
 		inc_mm_counter_fast(vma->vm_mm, MM_ANONPAGES);
@@ -4154,7 +4188,8 @@ static vm_fault_t do_read_fault(struct vm_fault *vmf)
 #endif
 
 	ret |= finish_fault(vmf);
-	unlock_page(vmf->page);
+	if (likely(!is_zero_page(vmf->page)))
+		unlock_page(vmf->page);
 #ifdef CONFIG_DUPTEXT
 	if (vmf->dup_page)
 		put_page(vmf->page);
@@ -4196,7 +4231,8 @@ static vm_fault_t do_cow_fault(struct vm_fault *vmf)
 	__SetPageUptodate(vmf->cow_page);
 
 	ret |= finish_fault(vmf);
-	unlock_page(vmf->page);
+	if (likely(!is_zero_page(vmf->page)))
+		unlock_page(vmf->page);
 	put_page(vmf->page);
 	if (unlikely(ret & (VM_FAULT_ERROR | VM_FAULT_NOPAGE | VM_FAULT_RETRY)))
 		goto uncharge_out;

mm/page_dup.c
@@ -290,6 +290,9 @@ struct page *__dup_page(struct page *page, struct vm_area_struct *vma)
 
 	VM_BUG_ON_PAGE(!PageLocked(hpage), hpage);
 
+	if (is_zero_page(page))
+		return NULL;
+
 	if (!node_isset(target_node, cpuset_current_mems_allowed)) {
 		if (!node_isset(page_node, cpuset_current_mems_allowed))
 			target_node = first_node(cpuset_current_mems_allowed);

mm/shmem.c
@@ -1898,6 +1898,12 @@ alloc_huge:
 	page = shmem_alloc_and_acct_page(gfp, inode, index, true);
 	if (IS_ERR(page)) {
 alloc_nohuge:
+		if (vmf && !mm_forbids_zeropage(vma->vm_mm) &&
+		    !(vma->vm_flags & VM_SHARED)) {
+			page = ZERO_PAGE(0);
+			get_page(page);
+			goto out;
+		}
 		page = shmem_alloc_and_acct_page(gfp, inode,
 						 index, false);
 	}
@@ -2000,6 +2006,14 @@ clear:
 		error = -EINVAL;
 		goto unlock;
 	}
+	/*
+	 * If the VMA that fault page belongs to is VM_SHARED, we should unmap all
+	 * zero page mappings to make the MMAP_PRIVATE VMA do page fault again
+	 * to catch page cache.
+	 */
+	if (page && vmf && (vma->vm_flags & VM_SHARED))
+		try_to_unmap_zeropage(page, TTU_ZEROPAGE);
+
 out:
 	*pagep = page + index - hindex;
 	return 0;
@@ -2016,6 +2030,9 @@ unacct:
 		goto alloc_nohuge;
 	}
 unlock:
+	if (page && vmf && (vma->vm_flags & VM_SHARED))
+		try_to_unmap_zeropage(page, TTU_ZEROPAGE);
+
 	if (page) {
 		unlock_page(page);
 		put_page(page);

commit 314595c19b37e298096776acfd4090ed55cb2a29

anolis: mm: fix incorrect refcount increasement of zero page
ANBZ: #4358

The refcount of zero page can not be decreased through zap_page_range,
thus avoid the get/put opertions of  the refcount when filling the zero
page into file hole.

Fixes: dbc6d94883be (anolis: mm: support filling in zero page if read fault in file hole)

Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>

mm/file_zeropage.c
@@ -22,10 +22,8 @@ struct page *__alloc_zeropage(struct vm_area_struct *vma, struct vm_fault *vmf)
 
 	if (vmf && !mm_forbids_zeropage(vma->vm_mm) &&
 	    !(vma->vm_flags & VM_SHARED) &&
-	    !(vmf->flags & FAULT_FLAG_NONZEROPAGE)) {
+	    !(vmf->flags & FAULT_FLAG_NONZEROPAGE))
 		page = ZERO_PAGE(0);
-		get_page(page);
-	}
 
 	return page;
 }

mm/memory.c
@@ -4428,8 +4428,10 @@ static vm_fault_t do_read_fault(struct vm_fault *vmf)
 #endif
 
 	ret |= finish_fault(vmf);
-	if (likely(!is_zero_page(vmf->page)))
-		unlock_page(vmf->page);
+	if (unlikely(is_zero_page(vmf->page)))
+		return ret;
+
+	unlock_page(vmf->page);
 #ifdef CONFIG_DUPTEXT
 	if (vmf->dup_page)
 		put_page(vmf->page);
@@ -4471,9 +4473,10 @@ static vm_fault_t do_cow_fault(struct vm_fault *vmf)
 	__SetPageUptodate(vmf->cow_page);
 
 	ret |= finish_fault(vmf);
-	if (likely(!is_zero_page(vmf->page)))
+	if (likely(!is_zero_page(vmf->page))) {
 		unlock_page(vmf->page);
-	put_page(vmf->page);
+		put_page(vmf->page);
+	}
 	if (unlikely(ret & (VM_FAULT_ERROR | VM_FAULT_NOPAGE | VM_FAULT_RETRY)))
 		goto uncharge_out;
 	return ret;