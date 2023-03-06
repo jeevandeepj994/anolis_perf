bcdc2cc705f74f912fbac59be56633ac03ae80f3
tiancyin <tianci.yin@amd.com>
2023-03-06 11:05:23 +0800

    drm/amd/display: fix dm irq error message in gpu recover
    ANBZ: #4362
    
    commit ca87c9ae70566c651dcf09c1b080db259e20f9ee upstream.
    
    [Why]
    Variable adev->crtc_irq.num_types was initialized as the value of
    adev->mode_info.num_crtc at early_init stage, later at hw_init stage,
    the num_crtc changed due to the display pipe harvest on some SKUs,
    but the num_types was not updated accordingly, that cause below error
    in gpu recover.
    
      *ERROR* amdgpu_dm_set_crtc_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_crtc_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_crtc_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_pflip_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_pflip_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_pflip_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_pflip_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_vupdate_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_vupdate_irq_state: crtc is NULL at id :3
      *ERROR* amdgpu_dm_set_vupdate_irq_state: crtc is NULL at id :3
    
    [How]
    Defer the initialization of num_types to eliminate the error logs.
    
    Signed-off-by: tiancyin <tianci.yin@amd.com>
    Reviewed-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    
    Signed-off-by: Qiangwei Zhang <zhang.qiangwei@zte.com.cn>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -3329,6 +3329,8 @@ static int amdgpu_dm_initialize_drm_device(struct amdgpu_device *adev)
 	/* Update the actual used number of crtc */
 	adev->mode_info.num_crtc = adev->dm.display_indexes_num;
 
+	amdgpu_dm_set_irq_funcs(adev);
+
 	link_cnt = dm->dc->caps.max_links;
 	if (amdgpu_dm_mode_config_init(dm->adev)) {
 		DRM_ERROR("DM: Failed to initialize mode config\n");
@@ -3669,8 +3671,6 @@ static int dm_early_init(void *handle)
 		DRM_ERROR("Unsupported ASIC type: 0x%X\n", adev->asic_type);
 		return -EINVAL;
 	}
-
-	amdgpu_dm_set_irq_funcs(adev);
 
 	if (adev->mode_info.funcs == NULL)
 		adev->mode_info.funcs = &dm_display_funcs;
