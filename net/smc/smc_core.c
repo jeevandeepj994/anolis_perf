jeevandeepj994/anolis_perf

commit 45c50da25f1796b0a8e110be55b1a1f6f38e84a5

anolis: net/smc: Do not clear snd buffer when buffer reuse
ANBZ: #3261

There is no data security problem with the residual
dirty data in the send buffer, so it does not need
to be cleared when reused.

Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/914

net/smc/smc_core.c
@@ -1151,9 +1151,11 @@ static void smcr_buf_unuse(struct smc_buf_desc *buf_desc, bool is_rmb,
 
 		smc_buf_free(lgr, is_rmb, buf_desc);
 	} else {
-		/* memzero_explicit provides potential memory barrier semantics */
-		memzero_explicit(buf_desc->cpu_addr, buf_desc->len);
-		WRITE_ONCE(buf_desc->used, 0);
+		if (is_rmb) {
+			/* memzero_explicit provides potential memory barrier semantics */
+			memzero_explicit(buf_desc->cpu_addr, buf_desc->len);
+			WRITE_ONCE(buf_desc->used, 0);
+		}
 	}
 }
 
@@ -1164,7 +1166,6 @@ static void smc_buf_unuse(struct smc_connection *conn,
 		if (!lgr->is_smcd && conn->sndbuf_desc->is_vm) {
 			smcr_buf_unuse(conn->sndbuf_desc, false, lgr);
 		} else {
-			memzero_explicit(conn->sndbuf_desc->cpu_addr, conn->sndbuf_desc->len);
 			WRITE_ONCE(conn->sndbuf_desc->used, 0);
 		}
 	}

commit 65844f6a20c8984f108ce6ab3fcc5776d93de84d

anolis: net/smc: Fix merge mistake of smcr_buf_unuse
ANBZ: #4487

The buf_desc->used should be cleared for both sndbuf and RMB.

Fixes: 45c50da25f17 ("anolis: net/smc: Do not clear snd buffer when buffer reuse")
Signed-off-by: Wen Gu <guwen@linux.alibaba.com>

net/smc/smc_core.c
@@ -1478,11 +1478,10 @@ static void smcr_buf_unuse(struct smc_buf_desc *buf_desc, bool is_rmb,
 
 		smc_buf_free(lgr, is_rmb, buf_desc);
 	} else {
-		if (is_rmb) {
+		if (is_rmb)
 			/* memzero_explicit provides potential memory barrier semantics */
 			memzero_explicit(buf_desc->cpu_addr, buf_desc->len);
-			WRITE_ONCE(buf_desc->used, 0);
-		}
+		WRITE_ONCE(buf_desc->used, 0);
 	}
 }
 