jeevandeepj994/anolis_perf

commit 069de53d9286aa5419085099e7b82e8b8ad9309c

printk: Avoid livelock with heavy printk during panic
ANBZ: #3035

commit 13fb0f74d7029df3b8137f11ef955e578a4a4a60 upstream

During panic(), if another CPU is writing heavily the kernel log (e.g.
via /dev/kmsg), then the panic CPU may livelock writing out its messages
to the console. Note when too many messages are dropped during panic and
suppress further printk, except from the panic CPU. This could result in
some important messages being dropped. However, messages are already
being dropped, so this approach at least prevents a livelock.

Reviewed-by: Petr Mladek <pmladek@suse.com>
Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
Signed-off-by: Petr Mladek <pmladek@suse.com>
Link: https://lore.kernel.org/r/20220202171821.179394-4-stephen.s.brennan@oracle.com
Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/901

kernel/printk/printk.c
@@ -84,6 +84,12 @@ static DEFINE_SEMAPHORE(console_sem);
 struct console *console_drivers;
 EXPORT_SYMBOL_GPL(console_drivers);
 
+/*
+ * During panic, heavy printk by other CPUs can delay the
+ * panic and risk deadlock on console resources.
+ */
+static int __read_mostly suppress_panic_printk;
+
 #ifdef CONFIG_LOCKDEP
 static struct lockdep_map console_lock_dep_map = {
 	.name = "console_lock"
@@ -1920,6 +1926,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 	unsigned long flags;
 	u64 curr_log_seq;
 
+        if (unlikely(suppress_panic_printk) &&
+            atomic_read(&panic_cpu) != raw_smp_processor_id())
+                return 0;
+
 	if (level == LOGLEVEL_SCHED) {
 		level = LOGLEVEL_DEFAULT;
 		in_sched = true;
@@ -2338,6 +2348,7 @@ void console_unlock(void)
 {
 	static char ext_text[CONSOLE_EXT_LOG_MAX];
 	static char text[LOG_LINE_MAX + PREFIX_MAX];
+	static char panic_console_dropped;
 	unsigned long flags;
 	bool do_cond_resched, retry;
 
@@ -2390,6 +2401,10 @@ again:
 			/* messages are gone, move to first one */
 			console_seq = log_first_seq;
 			console_idx = log_first_idx;
+			if (panic_in_progress() && panic_console_dropped++ > 10) {
+				suppress_panic_printk = 1;
+				pr_warn_once("Too many dropped messages. Suppress messages on non-panic CPUs to prevent livelock.\n");
+			}
 		} else {
 			len = 0;
 		}

commit 3cdd4b30fdfc0b3139c4e3d0604c2159177c06ed

anolis: printk: remove printk call from logbuf_lock
ANBZ: #3035

Prior commit adds another printk call at the critical section of
logbuf_lock, which causes deadlock if kernel alreadly panicked and
reserved many message to print. To avoid that, remove the printk
call from logbuf_lock range.

Fixes: 069de53d9286 ("printk: Avoid livelock with heavy printk during panic")
Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Tested-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>

kernel/printk/printk.c
@@ -1926,9 +1926,11 @@ asmlinkage int vprintk_emit(int facility, int level,
 	unsigned long flags;
 	u64 curr_log_seq;
 
-        if (unlikely(suppress_panic_printk) &&
-            atomic_read(&panic_cpu) != raw_smp_processor_id())
-                return 0;
+	if (unlikely(suppress_panic_printk) &&
+	    atomic_read(&panic_cpu) != raw_smp_processor_id()) {
+		pr_warn_once("Too many dropped messages. Suppress messages on non-panic CPUs to prevent livelock.\n");
+		return 0;
+	}
 
 	if (level == LOGLEVEL_SCHED) {
 		level = LOGLEVEL_DEFAULT;
@@ -2420,10 +2422,8 @@ again:
 			/* messages are gone, move to first one */
 			console_seq = log_first_seq;
 			console_idx = log_first_idx;
-			if (panic_in_progress() && panic_console_dropped++ > 10) {
+			if (panic_in_progress() && panic_console_dropped++ > 10)
 				suppress_panic_printk = 1;
-				pr_warn_once("Too many dropped messages. Suppress messages on non-panic CPUs to prevent livelock.\n");
-			}
 		} else {
 			len = 0;
 		}