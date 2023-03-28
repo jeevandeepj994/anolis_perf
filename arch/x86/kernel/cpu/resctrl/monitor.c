jeevandeepj994/anolis_perf

commit 8a7494c439cce56cac52bbd67995df202aa23a4e

x86/resctrl: Make __rmid_read() retry when it is interrupted
ANBZ: #1598

cherry-picked from https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git

__rmid_read() could be called by resctrl in process context, and then
called by the PMU driver from irq context on the same CPU.

The struct arch_mbm_state holds both the previous msr value, and a count
of the number of chunks. These two fields need to be updated atomically.

Read the prev_msr before accessing the hardware, and cmpxchg() the value
back. If the value has changed, the whole thing is re-attempted.

Signed-off-by: James Morse <james.morse@arm.com>
Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
Tested-by: Shawn Wang <shawnwang@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>

arch/x86/kernel/cpu/resctrl/monitor.c
@@ -149,13 +149,16 @@ struct __rmid_read_arg
 	u64 val;
 };
 
-static void __rmid_read(void *_arg)
+static int ___rmid_read(struct __rmid_read_arg *arg)
 {
-	struct __rmid_read_arg *arg = _arg;
 	enum resctrl_event_id eventid = arg->eventid;
+	u64 prev_msr, msr_val, chunks;
 	struct arch_mbm_state *am;
 	u32 rmid = arg->rmid;
-	u64 msr_val, chunks;
+
+	am = get_arch_mbm_state(arg->hw_dom, rmid, eventid);
+	if (am)
+		prev_msr = atomic64_read(&am->prev_msr);
 
 	/*
 	 * As per the SDM, when IA32_QM_EVTSEL.EvtID (bits 7:0) is configured
@@ -168,26 +171,36 @@ static void __rmid_read(void *_arg)
 	wrmsr(MSR_IA32_QM_EVTSEL, eventid, rmid);
 	rdmsrl(MSR_IA32_QM_CTR, msr_val);
 
-	if (msr_val & RMID_VAL_ERROR) {
-		arg->err = -EIO;
-		return;
-	}
-	if (msr_val & RMID_VAL_UNAVAIL) {
-		arg->err = -EINVAL;
-		return;
-	}
+	if (msr_val & RMID_VAL_ERROR)
+		return -EIO;
+	if (msr_val & RMID_VAL_UNAVAIL)
+		return -EINVAL;
 
-	am = get_arch_mbm_state(arg->hw_dom, rmid, eventid);
 	if (am) {
-		am->chunks += mbm_overflow_count(am->prev_msr, msr_val,
-						 arg->hw_res->mbm_width);
-		chunks = get_corrected_mbm_count(rmid, am->chunks);
-		am->prev_msr = msr_val;
+		chunks = mbm_overflow_count(prev_msr, msr_val,
+					    arg->hw_res->mbm_width);
+
+		if (atomic64_cmpxchg(&am->prev_msr, prev_msr, msr_val) != prev_msr)
+			return -EINTR;
+
+		chunks = atomic64_add_return(chunks, &am->chunks);
+		chunks = get_corrected_mbm_count(rmid, chunks);
 	} else {
 		chunks = msr_val;
 	}
 
 	arg->val = chunks * arg->hw_res->mon_scale;
+
+	return 0;
+}
+
+static void __rmid_read(void *_arg)
+{
+	struct __rmid_read_arg *arg = _arg;
+
+	do {
+		arg->err = ___rmid_read(arg);
+	} while (arg->err && arg->err != -EINTR);
 }
 
 int resctrl_arch_rmid_read(struct rdt_resource *r, struct rdt_domain *d,
@@ -201,7 +214,6 @@ int resctrl_arch_rmid_read(struct rdt_resource *r, struct rdt_domain *d,
 	arg.eventid = eventid;
 	arg.hw_res = resctrl_to_arch_res(r);
 	arg.hw_dom = resctrl_to_arch_dom(d);
-	arg.err = 0;
 
 	err = smp_call_function_any(&d->cpu_mask, __rmid_read, &arg, true);
 	if (err)

arch/x86/kernel/cpu/resctrl/internal.h
@@ -8,6 +8,7 @@
 #include <linux/fs_context.h>
 #include <linux/jump_label.h>
 
+#include <asm/atomic.h>
 #include <asm/resctrl.h>
 
 #define MSR_IA32_L3_QOS_CFG		0xc81
@@ -44,8 +45,8 @@
  *		find this struct.
  */
 struct arch_mbm_state {
-	u64	chunks;
-	u64	prev_msr;
+	atomic64_t	chunks;
+	atomic64_t	prev_msr;
 };
 
 /**

commit 93df48339f112c8181da42a730c47ff3c1e9ed69

anolis: x86/resctrl: Fix the wrong condition which could cause softlockup in __rmid_read()
ANBZ: #4612

__rmid_read() should only retry when it is interrupted, that is, when
the EINTR error occurs, not when other errors occur. The wrong condition in
current __rmid_read() could cause softlockup problem.

Fixes: 8a7494c439cc ("x86/resctrl: Make __rmid_read() retry when it is interrupted")
Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>

arch/x86/kernel/cpu/resctrl/monitor.c
@@ -200,7 +200,7 @@ static void __rmid_read(void *_arg)
 
 	do {
 		arg->err = ___rmid_read(arg);
-	} while (arg->err && arg->err != -EINTR);
+	} while (arg->err && arg->err == -EINTR);
 }
 
 int resctrl_arch_rmid_read(struct rdt_resource *r, struct rdt_domain *d,