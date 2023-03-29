jeevandeepj994/anolis_perf

commit e777fc6b981edc10d355a8ff9f1e85786a8ea061

anolis: Add a boot command line for rich container
ANBZ: #1996

The command line can enable/disable the rich container at the boot time.
The way of enable is add the "rich_container" to the comline, if not
disabled.

If enabled, the sched_schestats will be enabled default and don't set
the schedstats=disable.

Signed-off-by: Erwei Deng <erwei@linux.alibaba.com>
Reviewed-by: Yihao Wu <wuyihao@linux.alibaba.com>
Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>
Reviewed-by: Yihao Wu <wuyihao@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/672
Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>

kernel/pid_namespace.c
@@ -29,6 +29,24 @@ int sysctl_rich_container_enable = 1;
 #elif defined(CONFIG_RICH_CONTAINER)
 int sysctl_rich_container_enable;
 int sysctl_rich_container_source; /* 0 - current; 1 - child_reaper */
+
+static int __init rich_container_enable(char *str)
+{
+	sysctl_rich_container_enable = 1;
+
+#ifndef CONFIG_SCHEDSTATS_HOST
+	/* If enable the rich container, the schedstats should be enabled.
+	 * We can't change the static branch directly of sched_schedstats
+	 * because the jump label have been not set up. So we change the
+	 * __sched_schedstats var and set up the static key later in the
+	 * init_schedstats().
+	 */
+	__sched_schedstats = true;
+#endif
+
+	return 0;
+}
+__setup("rich_container", rich_container_enable);
 #endif
 
 static DEFINE_MUTEX(pid_caches_mutex);

include/linux/pid_namespace.h
@@ -90,6 +90,9 @@ void pid_idr_init(void);
 
 #ifdef CONFIG_RICH_CONTAINER
 extern int sysctl_rich_container_enable;
+#ifndef CONFIG_SCHEDSTATS_HOST
+extern bool __sched_schedstats;
+#endif
 #ifndef CONFIG_RICH_CONTAINER_CG_SWITCH
 extern int sysctl_rich_container_source;
 extern int sysctl_rich_container_cpuinfo_source;

kernel/sched/core.c
@@ -3196,7 +3196,7 @@ DEFINE_STATIC_KEY_TRUE(sched_schedstats);
 static bool __sched_schedstats __initdata = true;
 #else
 DEFINE_STATIC_KEY_FALSE(sched_schedstats);
-static bool __initdata __sched_schedstats = false;
+bool __initdata __sched_schedstats;
 #endif
 
 static void set_schedstats(bool enabled)

commit f8418f6ee907061a7e0c72aeef503f12e003137d

anolis: Fix compilation error with rich container
ANBZ: #4631

The rich container boot cmdline parameter handle rich_container_enable()
tries to set __sched_schedstats as well, but that variable won't exist
if CONFIG_SCHEDSTATS is not set. This causes a compilation error as:

```
kernel/pid_namespace.o: In function `rich_container_enable':
pid_namespace.c:(.init.text+0x13): undefined reference to `__sched_schedstats'
make: *** [Makefile:1188: vmlinux] Error 1
```

Fixes: e777fc6b981e ("anolis: Add a boot command line for rich container")
Signed-off-by: Liu Song <liu.song13@zte.com.cn>

kernel/pid_namespace.c
@@ -34,7 +34,7 @@ static int __init rich_container_enable(char *str)
 {
 	sysctl_rich_container_enable = 1;
 
-#ifndef CONFIG_SCHEDSTATS_HOST
+#if defined(CONFIG_SCHEDSTATS) && !defined(CONFIG_SCHEDSTATS_HOST)
 	/* If enable the rich container, the schedstats should be enabled.
 	 * We can't change the static branch directly of sched_schedstats
 	 * because the jump label have been not set up. So we change the