jeevandeepj994/anolis_perf

commit e463f700ccecd0cfa6fd34f85578c25b5c19957e

anolis: net/smc: Fix NULL sndbuf_desc in smc_cdc_tx_handler()
ANBZ: #2045

When performing a stress test on SMC-R by rmmod mlx5_ib driver
during the wrk/nginx test, we found that there is a probability
of triggering a panic while terminating all link groups.

This issue dues to the race between smc_smcr_terminate_all()
and smc_buf_create().

			smc_smcr_terminate_all

smc_buf_create

			__smc_lgr_terminate
				smc_conn_kill
					smc_close_abort
						smc_cdc_get_slot_and_msg_send

			__softirqentry_text_start
				smc_wr_tx_process_cqe
					smc_cdc_tx_handler
						READ(conn->sndbuf_desc->len);
						/* panic dues to NULL sndbuf_desc */

conn->sndbuf_desc = xxx;

This patch tries to fix the issue by always to check the sndbuf_desc
before send any cdc msg.

Fixes: 0b29ec643613 ("net/smc: immediate termination for SMCR link groups")
Link: https://gitee.com/anolis/cloud-kernel/pulls/688
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Wen Gu <guwen@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/906
Link: https://gitee.com/anolis/cloud-kernel/pulls/1281

net/smc/smc_cdc.c
@@ -114,6 +114,9 @@ int smc_cdc_msg_send(struct smc_connection *conn,
 	union smc_host_cursor cfed;
 	int rc;
 
+	if (unlikely(!READ_ONCE(conn->sndbuf_desc)))
+		return -EINVAL;
+
 	smc_cdc_add_pending_send(conn, pend);
 
 	conn->tx_cdc_seq++;

commit b3e7359655116593a64d02600cd029344b7dd475

anolis: net/smc: fix imcomplete bugfix dues to NULL sndbuf_desc
ANBZ: #4638

When performing a stress test on SMC-R by rmmod mlx5_ib driver
during the wrk/nginx test, we found that there is a probability
of triggering a panic while terminating all link groups.

However, previous fixes have not completely resolved this issue, and
this patch directly check if sndbuf_desc is NULL to completely resolve the
possibility of being present.

This issue dues to the race between smc_smcr_terminate_all()
and smc_buf_create().

                        smc_smcr_terminate_all

    smc_buf_create

                        __smc_lgr_terminate
                                smc_conn_kill
                                        smc_close_abort
                                                smc_cdc_get_slot_and_msg_send

                        __softirqentry_text_start
                                smc_wr_tx_process_cqe
                                        smc_cdc_tx_handler
                                                READ(conn->sndbuf_desc->len);
                                                /* panic dues to NULL
  /* sndbuf_desc */
  conn->sndbuf_desc = xxx;

Fixes: e463f700ccec ("net/smc: net/smc: Fix NULL sndbuf_desc in smc_cdc_tx_handler())
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>

net/smc/smc_cdc.c
@@ -28,13 +28,15 @@ static void smc_cdc_tx_handler(struct smc_wr_tx_pend_priv *pnd_snd,
 {
 	struct smc_cdc_tx_pend *cdcpend = (struct smc_cdc_tx_pend *)pnd_snd;
 	struct smc_connection *conn = cdcpend->conn;
+	struct smc_buf_desc *sndbuf_desc;
 	struct smc_sock *smc;
 	int diff;
 
+	sndbuf_desc = conn->sndbuf_desc;
 	smc = container_of(conn, struct smc_sock, conn);
 	bh_lock_sock(&smc->sk);
-	if (!wc_status) {
-		diff = smc_curs_diff(cdcpend->conn->sndbuf_desc->len,
+	if (!wc_status && sndbuf_desc) {
+		diff = smc_curs_diff(sndbuf_desc->len,
 				     &cdcpend->conn->tx_curs_fin,
 				     &cdcpend->cursor);
 		/* sndbuf_space is decreased in smc_sendmsg */
@@ -113,9 +115,6 @@ int smc_cdc_msg_send(struct smc_connection *conn,
 	struct smc_link *link = conn->lnk;
 	union smc_host_cursor cfed;
 	int rc;
-
-	if (unlikely(!READ_ONCE(conn->sndbuf_desc)))
-		return -EINVAL;
 
 	smc_cdc_add_pending_send(conn, pend);
 