jeevandeepj994/anolis_perf

commit bcc044643be6e94843831916cece93068739631c

KVM: x86: add system attribute to retrieve full set of supported xsave states
ANBZ: #475

commit dd6e631220181162478984d2d46dd979e04d8e75 upstream.

Because KVM_GET_SUPPORTED_CPUID is meant to be passed (by simple-minded
VMMs) to KVM_SET_CPUID2, it cannot include any dynamic xsave states that
have not been enabled.  Probing those, for example so that they can be
passed to ARCH_REQ_XCOMP_GUEST_PERM, requires a new ioctl or arch_prctl.
The latter is in fact worse, even though that is what the rest of the
API uses, because it would require supported_xcr0 to be moved from the
KVM module to the kernel just for this use.  In addition, the value
would be nonsensical (or an error would have to be returned) until
the KVM module is loaded in.

Therefore, to limit the growth of system ioctls, add a /dev/kvm
variant of KVM_{GET,HAS}_DEVICE_ATTR, and implement it in x86
with just one group (0) and attribute (KVM_X86_XCOMP_GUEST_SUPP).

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
Signed-off-by: Hao Xiang <hao.xiang@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

arch/x86/kvm/x86.c
@@ -3834,6 +3834,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_X86_USER_SPACE_MSR:
 	case KVM_CAP_X86_MSR_FILTER:
 	case KVM_CAP_ENFORCE_PV_FEATURE_CPUID:
+	case KVM_CAP_SYS_ATTRIBUTES:
 		r = 1;
 		break;
 	case KVM_CAP_SYNC_REGS:
@@ -3924,6 +3925,40 @@ static inline void __user *kvm_get_attr_addr(struct kvm_device_attr *attr)
 	if ((u64)(unsigned long)uaddr != attr->addr)
 		return ERR_PTR(-EFAULT);
 	return uaddr;
+}
+
+static int kvm_x86_dev_get_attr(struct kvm_device_attr *attr)
+{
+	u64 __user *uaddr = kvm_get_attr_addr(attr);
+
+	if (attr->group)
+		return -ENXIO;
+
+	if (IS_ERR(uaddr))
+		return PTR_ERR(uaddr);
+
+	switch (attr->attr) {
+	case KVM_X86_XCOMP_GUEST_SUPP:
+		if (put_user(supported_xcr0, uaddr))
+			return -EFAULT;
+		return 0;
+	default:
+		return -ENXIO;
+		break;
+	}
+}
+
+static int kvm_x86_dev_has_attr(struct kvm_device_attr *attr)
+{
+	if (attr->group)
+		return -ENXIO;
+	
+	switch (attr->attr) {
+	case KVM_X86_XCOMP_GUEST_SUPP:
+		return 0;
+	default:
+		return -ENXIO;
+	}
 }
 
 long kvm_arch_dev_ioctl(struct file *filp,
@@ -5038,6 +5073,22 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 		if (copy_to_user(cpuid_arg, &cpuid, sizeof(cpuid)))
 			goto out;
 		r = 0;
+		break;
+	}
+	case KVM_GET_DEVICE_ATTR: {
+		struct kvm_device_attr attr;
+		r = -EFAULT;
+		if (copy_from_user(&attr, (void __user *)arg, sizeof(attr)))
+			break;
+		r = kvm_x86_dev_get_attr(&attr);
+		break;
+	}
+	case KVM_HAS_DEVICE_ATTR: {
+		struct kvm_device_attr attr;
+		r = -EFAULT;
+		if (copy_from_user(&attr, (void __user *)arg, sizeof(attr)))
+			break;
+		r = kvm_x86_dev_has_attr(&attr);
 		break;
 	}
 	default:

arch/x86/include/uapi/asm/kvm.h
@@ -436,6 +436,9 @@ struct kvm_sync_regs {
 
 #define KVM_STATE_VMX_PREEMPTION_TIMER_DEADLINE	0x00000001
 
+/* attributes for system fd (group 0) */
+#define KVM_X86_XCOMP_GUEST_SUPP	0
+
 struct kvm_vmx_nested_state_data {
 	__u8 vmcs12[KVM_STATE_NESTED_VMX_VMCS_SIZE];
 	__u8 shadow_vmcs12[KVM_STATE_NESTED_VMX_VMCS_SIZE];

include/uapi/linux/kvm.h
@@ -1055,6 +1055,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_ENFORCE_PV_FEATURE_CPUID 190
 
 #define KVM_CAP_XSAVE2 208
+#define KVM_CAP_SYS_ATTRIBUTES 209
 
 #ifdef KVM_CAP_IRQ_ROUTING
 

commit 8391b100732e47a0581ed80d0182e3e9215ec348

anolis: kvm: x86: move KVM_GET_DEVICE_ATTR to kvm_arch_dev_ioctl()
ANBZ: #4694

KVM_GET_DEVICE_ATTR is ioctl cmd of kvm_dev_ioctl which will be used in
arch specific dev ioctl function: kvm_arch_dev_ioctl().
However commit bcc044643be6 ("KVM: x86: add system attribute to retrieve
full set of supported xsave states") which introduced this ioctl cmd
placed it in kvm_arch_vcpu_ioctl() function, KVM_GET_DEVICE_ATTR cannot
be called any more by qemu.

Fixes: bcc044643be6 ("KVM: x86: add system attribute to retrieve
full set of supported xsave states")

Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>

arch/x86/kvm/x86.c
@@ -4122,6 +4122,24 @@ long kvm_arch_dev_ioctl(struct file *filp,
 	case KVM_GET_MSRS:
 		r = msr_io(NULL, argp, do_get_msr_feature, 1);
 		break;
+	case KVM_GET_DEVICE_ATTR: {
+		struct kvm_device_attr attr;
+
+		r = -EFAULT;
+		if (copy_from_user(&attr, (void __user *)arg, sizeof(attr)))
+			break;
+		r = kvm_x86_dev_get_attr(&attr);
+		break;
+	}
+	case KVM_HAS_DEVICE_ATTR: {
+		struct kvm_device_attr attr;
+
+		r = -EFAULT;
+		if (copy_from_user(&attr, (void __user *)arg, sizeof(attr)))
+			break;
+		r = kvm_x86_dev_has_attr(&attr);
+		break;
+	}
 	default:
 		r = -EINVAL;
 		break;
@@ -5181,22 +5199,6 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 		if (copy_to_user(cpuid_arg, &cpuid, sizeof(cpuid)))
 			goto out;
 		r = 0;
-		break;
-	}
-	case KVM_GET_DEVICE_ATTR: {
-		struct kvm_device_attr attr;
-		r = -EFAULT;
-		if (copy_from_user(&attr, (void __user *)arg, sizeof(attr)))
-			break;
-		r = kvm_x86_dev_get_attr(&attr);
-		break;
-	}
-	case KVM_HAS_DEVICE_ATTR: {
-		struct kvm_device_attr attr;
-		r = -EFAULT;
-		if (copy_from_user(&attr, (void __user *)arg, sizeof(attr)))
-			break;
-		r = kvm_x86_dev_has_attr(&attr);
 		break;
 	}
 	default: