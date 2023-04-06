ca814b0f13091f4f7280d059cebbe4b3daf12fca
leoliu-oc <leoliu-oc@zhaoxin.com>
2023-04-06 17:48:29 +0800

    anolis: Fix streaming DMA writes disorder issue for Zhaoxin's KH-40000 platform
    ANBZ: #4718
    
    The Zhaoxin KH-40000 platform can't keep the PCIE transaction order for
    DMA writes, whose target addresses are located in different NUMA nodes,
    from the same device. Patch this issue by flushing the target DMA write
    with a subsequent PCIE configuration space read operation.
    
    For streaming dma ops callback .unmap_page/sg and .sync_sg/single_for_cpu,
    add PCIE configuration space read operation to flush the target DMA write.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- arch/x86/include/asm/dma-mapping.h
+++ arch/x86/include/asm/dma-mapping.h
@@ -11,6 +11,20 @@
 #include <asm/io.h>
 #include <asm/swiotlb.h>
 
+#ifdef CONFIG_PCI
+
+#define ZHAOXIN_P2CW_NODE_CHECK         BIT(0)
+#define ZHAOXIN_PATCH_CODE_DEFAULT   ZHAOXIN_P2CW_NODE_CHECK
+#define ZHAOXIN_PATCH_CODE_MAX       ZHAOXIN_P2CW_NODE_CHECK
+extern unsigned long zhaoxin_patch_code;
+extern bool zhaoxin_p2cw_patch_en;
+
+void patch_p2cw_single_map(struct device *dev, dma_addr_t paddr,
+		enum dma_data_direction dir, const struct dma_map_ops *ops);
+void patch_p2cw_sg_map(struct device *dev, struct scatterlist *sglist,
+		int nelems, enum dma_data_direction dir, const struct dma_map_ops *ops);
+#endif
+
 extern int iommu_merge;
 extern int panic_on_overflow;
 
