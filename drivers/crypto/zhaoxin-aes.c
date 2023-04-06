6a31fbde8c75f2f7fef5f5a4a0b94357e9631947
leoliu-oc <leoliu-oc@zhaoxin.com>
2023-04-06 16:25:19 +0800

    anolis: Driver for Zhaoxin AES and SHA algorithm
    ANBZ: #4715
    
    Some Zhaoxin processors come with an integrated crypto engine (so called
    Zhaoxin ACE, Advanced Cryptography Engine) that provides instructions for
    very fast cryptographic operations with supported AES & SHA1/SHA256
    algorithms.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- drivers/crypto/zhaoxin-aes.c
+++ drivers/crypto/zhaoxin-aes.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/* 
+/*
  * Cryptographic API.
  *
- * Support for VIA PadLock hardware crypto engine.
+ * Support for ACE hardware crypto engine.
  *
  * Copyright (c) 2004  Michal Ludvig <michal@logix.cz>
  *
@@ -474,22 +474,22 @@ static struct skcipher_alg cbc_aes_alg = {
 	.decrypt		=	cbc_aes_decrypt,
 };
 
-static const struct x86_cpu_id padlock_cpu_id[] = {
-	{ X86_VENDOR_CENTAUR, 6, X86_MODEL_ANY, X86_FEATURE_XCRYPT },
+static const struct x86_cpu_id zhaoxin_cpu_id[] = {
+	{ X86_VENDOR_CENTAUR, 7, X86_MODEL_ANY, X86_STEPPING_ANY, X86_FEATURE_XCRYPT },
+	{ X86_VENDOR_ZHAOXIN, 7, X86_MODEL_ANY, X86_STEPPING_ANY, X86_FEATURE_XCRYPT },
 	{}
 };
-MODULE_DEVICE_TABLE(x86cpu, padlock_cpu_id);
+MODULE_DEVICE_TABLE(x86cpu, zhaoxin_cpu_id);
 
 static int __init padlock_init(void)
 {
 	int ret;
-	struct cpuinfo_x86 *c = &cpu_data(0);
 
-	if (!x86_match_cpu(padlock_cpu_id))
+	if (!x86_match_cpu(zhaoxin_cpu_id))
 		return -ENODEV;
 
 	if (!boot_cpu_has(X86_FEATURE_XCRYPT_EN)) {
-		printk(KERN_NOTICE PFX "VIA PadLock detected, but not enabled. Hmm, strange...\n");
+		printk(KERN_NOTICE "ACE detected, but not enabled. Hmm, strange...\n");
 		return -ENODEV;
 	}
 
@@ -502,13 +502,7 @@ static int __init padlock_init(void)
 	if ((ret = crypto_register_skcipher(&cbc_aes_alg)) != 0)
 		goto cbc_aes_err;
 
-	printk(KERN_NOTICE PFX "Using VIA PadLock ACE for AES algorithm.\n");
-
-	if (c->x86 == 6 && c->x86_model == 15 && c->x86_stepping == 2) {
-		ecb_fetch_blocks = MAX_ECB_FETCH_BLOCKS;
-		cbc_fetch_blocks = MAX_CBC_FETCH_BLOCKS;
-		printk(KERN_NOTICE PFX "VIA Nano stepping 2 detected: enabling workaround.\n");
-	}
+	printk(KERN_NOTICE "Using ACE for AES algorithm.\n");
 
 out:
 	return ret;
@@ -518,7 +512,7 @@ cbc_aes_err:
 ecb_aes_err:
 	crypto_unregister_alg(&aes_alg);
 aes_err:
-	printk(KERN_ERR PFX "VIA PadLock AES initialization failed.\n");
+	printk(KERN_ERR "ACE AES initialization failed.\n");
 	goto out;
 }
 
@@ -532,7 +526,7 @@ static void __exit padlock_fini(void)
 module_init(padlock_init);
 module_exit(padlock_fini);
 
-MODULE_DESCRIPTION("VIA PadLock AES algorithm support");
+MODULE_DESCRIPTION("ACE AES algorithm support");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michal Ludvig");
 
