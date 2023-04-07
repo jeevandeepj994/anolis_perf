b81c228103af7b260a3aa577e4872f5ebdf1e47f
Dave Jiang <dave.jiang@intel.com>
2023-04-07 13:31:30 -0700

    dmaengine: idxd: setup event log configuration
    ANBZ: #8224
    
    commit 244da66cda359227d80ccb41dbcb99da40eae186 upstream.
    
    Add setup of event log feature for supported device. Event log addresses
    error reporting that was lacking in gen 1 DSA devices where a second error
    event does not get reported when a first event is pending software
    handling. The event log allows a circular buffer that the device can push
    error events to. It is up to the user to create a large enough event log
    ring in order to capture the expected events. The evl size can be set in
    the device sysfs attribute. By default 64 entries are supported as minimal
    when event log is enabled.
    
    Tested-by: Tony Zhu <tony.zhu@intel.com>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Co-developed-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Link: https://lore.kernel.org/r/20230407203143.2189681-4-fenghua.yu@intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2826

--- drivers/dma/idxd/idxd.h
+++ drivers/dma/idxd/idxd.h
@@ -279,7 +279,15 @@ struct idxd_driver_data {
 };
 
 struct idxd_evl {
+	/* Lock to protect event log access. */
+	spinlock_t lock;
+	void *log;
+	dma_addr_t dma;
+	/* Total size of event log = number of entries * entry size. */
+	unsigned int log_size;
+	/* The number of entries in the event log. */
 	u16 size;
+	u16 head;
 };
 
 struct idxd_device {
@@ -345,6 +353,17 @@ struct idxd_device {
 	unsigned long *opcap_bmap;
 	struct idxd_evl *evl;
 };
+
+static inline unsigned int evl_ent_size(struct idxd_device *idxd)
+{
+	return idxd->hw.gen_cap.evl_support ?
+	       (32 * (1 << idxd->hw.gen_cap.evl_support)) : 0;
+}
+
+static inline unsigned int evl_size(struct idxd_device *idxd)
+{
+	return idxd->evl->size * evl_ent_size(idxd);
+}
 
 /**
  * IDXD batch field for SW Batch descriptor
