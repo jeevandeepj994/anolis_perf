jeevandeepj994/anolis_perf

commit 7be77e1dcf2f99eb318da6c0b6e8a7d572a6c7b0

nfsd: persist nfsd filesystem across mounts
ANBZ: #1849

commit 2c830dd7209bf67863ae0b83c7dde847967ea43c upstream.

Keep around one internal mount of the nfsd filesystem so that we can add
stuff to it when clients come and go, regardless of whether anyone has
it mounted.

Signed-off-by: J. Bruce Fields <bfields@redhat.com>
Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/631
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

fs/nfsd/nfsctl.c
@@ -1231,6 +1231,7 @@ unsigned int nfsd_net_id;
 static __net_init int nfsd_init_net(struct net *net)
 {
 	int retval;
+	struct vfsmount *mnt;
 	struct nfsd_net *nn = net_generic(net, nfsd_net_id);
 
 	retval = nfsd_export_init(net);
@@ -1247,8 +1248,16 @@ static __net_init int nfsd_init_net(struct net *net)
 
 	atomic_set(&nn->ntf_refcnt, 0);
 	init_waitqueue_head(&nn->ntf_wq);
+	mnt =  vfs_kern_mount(&nfsd_fs_type, SB_KERNMOUNT, "nfsd", NULL);
+	if (IS_ERR(mnt)) {
+		retval = PTR_ERR(mnt);
+		goto out_mount_err;
+	}
+	nn->nfsd_mnt = mnt;
 	return 0;
 
+out_mount_err:
+	nfsd_reply_cache_shutdown();
 out_idmap_error:
 	nfsd_export_shutdown(net);
 out_export_error:
@@ -1257,6 +1266,10 @@ out_export_error:
 
 static __net_exit void nfsd_exit_net(struct net *net)
 {
+	struct nfsd_net *nn = net_generic(net, nfsd_net_id);
+
+	mntput(nn->nfsd_mnt);
+	nfsd_reply_cache_shutdown();
 	nfsd_idmap_shutdown(net);
 	nfsd_export_shutdown(net);
 }

fs/nfsd/netns.h
@@ -55,6 +55,9 @@ struct nfsd_net {
 	bool grace_ended;
 	time_t boot_time;
 
+	/* internal mount of the "nfsd" pseudofilesystem: */
+	struct vfsmount *nfsd_mnt;
+
 	/*
 	 * reclaim_str_hashtbl[] holds known client info from previous reset/reboot
 	 * used in reboot/reset lease grace period processing

commit 62df73190909aeca872bdfbebc182ef4b3ada63d

anolis: nfsd: remove nfsd_reply_cache_shutdown() in nfsd_exit_net
ANBZ: #4801

nfsd reply cache should not be shutdown in nfsd_exit_net as it
is global. This is mistakenly introduced when backported
"nfsd: persist nfsd filesystem across mounts".

Fixes: 7be77e1dcf2f (nfsd: persist nfsd filesystem across mounts)
Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1590

fs/nfsd/nfsctl.c
@@ -1478,7 +1478,6 @@ out_export_error:
 
 static __net_exit void nfsd_exit_net(struct net *net)
 {
-	nfsd_reply_cache_shutdown();
 	nfsd_idmap_shutdown(net);
 	nfsd_export_shutdown(net);
 }