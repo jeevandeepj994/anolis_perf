jeevandeepj994/anolis_perf

commit 08f0850561f2f59ec945e4c5927a4fe0315d23f1

printk: Drop console_sem during panic
ANBZ: #1895

commit 8ebc476fd51e6c0fd3174ec1959a20ba99d4c5e5 upstream

If another CPU is in panic, we are about to be halted. Try to gracefully
abandon the console_sem, leaving it free for the panic CPU to grab.

Suggested-by: Petr Mladek <pmladek@suse.com>
Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
Reviewed-by: Petr Mladek <pmladek@suse.com>
Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
Signed-off-by: Petr Mladek <pmladek@suse.com>
Link: https://lore.kernel.org/r/20220202171821.179394-5-stephen.s.brennan@oracle.com
Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/633

kernel/printk/printk.c
@@ -2424,6 +2424,25 @@ static int have_callable_console(void)
 }
 
 /*
+ * Return true when this CPU should unlock console_sem without pushing all
+ * messages to the console. This reduces the chance that the console is
+ * locked when the panic CPU tries to use it.
+ */
+static bool abandon_console_lock_in_panic(void)
+{
+	if (!panic_in_progress())
+		return false;
+
+	/*
+	 * We can use raw_smp_processor_id() here because it is impossible for
+	 * the task to be migrated to the panic_cpu, or away from it. If
+	 * panic_cpu has already been set, and we're not currently executing on
+	 * that CPU, then we never will be.
+	 */
+	return atomic_read(&panic_cpu) != raw_smp_processor_id();
+}
+
+/*
  * Can we actually use the console at this time on this cpu?
  *
  * Console drivers may assume that per-cpu resources have been allocated. So
@@ -2569,6 +2588,10 @@ skip:
 
 		printk_safe_exit_irqrestore(flags);
 
+		/* Allow panic_cpu to take over the consoles safely */
+		if (abandon_console_lock_in_panic())
+			break;
+
 		if (do_cond_resched)
 			cond_resched();
 	}
@@ -2590,7 +2613,7 @@ skip:
 	raw_spin_unlock(&logbuf_lock);
 	printk_safe_exit_irqrestore(flags);
 
-	if (retry && console_trylock())
+	if (retry && !abandon_console_lock_in_panic() && console_trylock())
 		goto again;
 }
 EXPORT_SYMBOL(console_unlock);

commit d10c80ae5caed6d735aede89dc0bbb69377d5686

anolis: mm: avoid double unlock logbuf_lock
ANBZ: #4609

logbuf_lock has been released while other cpus in panic, do not release
it again when panic.

Fixes: 08f0850561f2(printk: Drop console_sem during panic)
Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>

kernel/printk/printk.c
@@ -2598,7 +2598,8 @@ skip:
 
 	console_locked = 0;
 
-	raw_spin_unlock(&logbuf_lock);
+	if (!abandon_console_lock_in_panic())
+		raw_spin_unlock(&logbuf_lock);
 
 	up_console_sem();
 
@@ -2611,7 +2612,9 @@ skip:
 	raw_spin_lock(&logbuf_lock);
 	retry = prb_read_valid(prb, console_seq, NULL);
 	raw_spin_unlock(&logbuf_lock);
-	printk_safe_exit_irqrestore(flags);
+
+	if (!abandon_console_lock_in_panic())
+		printk_safe_exit_irqrestore(flags);
 
 	if (retry && !abandon_console_lock_in_panic() && console_trylock())
 		goto again;