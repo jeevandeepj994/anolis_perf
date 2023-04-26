7904d8d54509147a80835d64a602343bf18e1550
leoliu-oc <leoliu-oc@zhaoxin.com>
2023-04-26 16:20:30 +0800

    anolis: Fix coherent DMA writes disorder issue for Zhaoxin's KH-40000 platform
    ANBZ: #4330
    
    The Zhaoxin KH-40000 platform can't keep the PCIE transaction order for
    DMA writes, whose target addresses are located in different NUMA nodes,
    from the same device. Patch this issue by flushing the target DMA write
    with a subsequent PCIE configuration space read operation.
    
    For coherent DMA map, limit the DMA buffer that the device driver applies
    to to be on the same node as the device.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1342

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -3751,6 +3751,11 @@ static void *intel_alloc_coherent(struct device *dev, size_t size,
 {
 	struct page *page = NULL;
 	int order;
+#if defined(CONFIG_X86) && defined(CONFIG_HAS_DMA)
+	nodemask_t nodemask;
+
+	nodes_clear(nodemask);
+#endif
 
 	size = PAGE_ALIGN(size);
 	order = get_order(size);
@@ -3776,9 +3781,28 @@ static void *intel_alloc_coherent(struct device *dev, size_t size,
 		}
 	}
 
-	if (!page)
+	if (!page) {
+#if defined(CONFIG_X86) && defined(CONFIG_HAS_DMA)
+		if (zhaoxin_p2cw_patch_en == true) {
+			if (dev_to_node(dev) == NUMA_NO_NODE) {
+				__alloc_pages_nodemask(flags | __GFP_HIGH, order,
+					numa_mem_id(), NULL);
+			} else {
+				if (!(flags & (GFP_DMA | GFP_DMA32))) {
+					node_set(dev_to_node(dev), nodemask);
+					page = __alloc_pages_nodemask(flags | __GFP_HIGH, order,
+						dev_to_node(dev), &nodemask);
+				} else
+					page = __alloc_pages_nodemask(flags | __GFP_HIGH, order,
+						dev_to_node(dev), NULL);
+			}
+			goto check_alloc;
+		}
+#endif
 		page = alloc_pages(flags, order);
+	}
 
+check_alloc:
 	if (!page)
 		return NULL;
 	memset(page_address(page), 0, size);
