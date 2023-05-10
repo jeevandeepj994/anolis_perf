288f196cc72b488e1a79e7174d188ff5f2e25945
Liu Song <liusong@linux.alibaba.com>
2023-05-10 14:49:57 +0800

    arm64: mark all the GICC nodes in MADT as possible cpu
    ANBZ: #4896
    
    commit 121f2a1c7cdaad45cde226306331261af7e757db openEuler.
    commit efd5d6325050cb3592d830c02c83046029c23971 openEuler.
    
    We set 'cpu_possible_mask' based on the enabled GICC node in MADT. If
    the GICC node is disabled, we will skip initializing the kernel data
    structure for that CPU.
    
    To support CPU hotplug, we need to initialize some CPU related data
    structure in advance. This patch mark all the GICC nodes as possible
    CPU and only these enabled GICC nodes as present CPU.
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Acked-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Wang ShaoBo <bobo.shaobowang@huawei.com>
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Liu Song <liusong@linux.alibaba.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -577,15 +577,13 @@ acpi_map_gic_cpu_interface(struct acpi_madt_generic_interrupt *processor)
 {
 	u64 hwid = processor->arm_mpidr;
 
-	if (!(processor->flags & ACPI_MADT_ENABLED)) {
-		pr_debug("skipping disabled CPU entry with 0x%llx MPIDR\n", hwid);
-		return;
-	}
-
 	if (hwid & ~MPIDR_HWID_BITMASK || hwid == INVALID_HWID) {
 		pr_err("skipping CPU entry with invalid MPIDR 0x%llx\n", hwid);
 		return;
 	}
+
+	if (!(processor->flags & ACPI_MADT_ENABLED))
+		pr_debug("disabled CPU entry with 0x%llx MPIDR\n", hwid);
 
 	if (is_mpidr_duplicate(cpu_count, hwid)) {
 		pr_err("duplicate CPU MPIDR 0x%llx in MADT\n", hwid);
@@ -814,7 +812,17 @@ void __init smp_prepare_cpus(unsigned int max_cpus)
 		if (err)
 			continue;
 
+#ifdef CONFIG_ACPI
+		if (!acpi_disabled) {
+			if ((cpu_madt_gicc[cpu].flags & ACPI_MADT_ENABLED))
+				set_cpu_present(cpu, true);
+		} else {
+			set_cpu_present(cpu, true);
+		}
+#else
 		set_cpu_present(cpu, true);
+#endif
+
 		numa_store_cpu_info(cpu);
 	}
 #ifdef CONFIG_NUMA_AWARE_SPINLOCKS
