jeevandeepj994/anolis_perf

commit faf3e5e56720ca8aa538e27dd65f1ee31ca0f825

anolis: fuse: add fuse_mount_callback hook
ANBZ: #4851

Add a hook which can be called when mounting fuse.

Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1606

fs/fuse/inode.c
@@ -59,6 +59,9 @@ MODULE_PARM_DESC(max_user_congthresh,
 /** Congestion starts at 75% of maximum */
 #define FUSE_DEFAULT_CONGESTION_THRESHOLD (FUSE_DEFAULT_MAX_BACKGROUND * 3 / 4)
 
+fuse_mount_cb_t fuse_mount_callback;
+EXPORT_SYMBOL_GPL(fuse_mount_callback);
+
 struct fuse_forget_link *fuse_alloc_forget(void)
 {
 	return kzalloc(sizeof(struct fuse_forget_link), GFP_KERNEL);
@@ -1481,6 +1484,7 @@ static struct dentry *fuse_try_mount(struct file_system_type *fs_type,
 	struct fuse_mount_data d;
 	struct dentry *root;
 	char *opts;
+	int ret;
 
 	opts = kstrdup(raw_data, GFP_KERNEL);
 	if (!opts)
@@ -1523,6 +1527,17 @@ static struct dentry *fuse_try_mount(struct file_system_type *fs_type,
 			root = ERR_CAST(sb);
 		else
 			root = dget(sb->s_root);
+	}
+
+	if (IS_ERR(root) || WARN_ON(!fuse_mount_callback))
+		goto out_fput;
+
+	ret = fuse_mount_callback(file);
+	if (ret) {
+		sb = root->d_sb;
+		dput(root);
+		deactivate_locked_super(sb);
+		root = ERR_PTR(ret);
 	}
 
 out_fput:

fs/fuse/fuse_i.h
@@ -1279,6 +1279,9 @@ void fuse_dax_dontcache(struct inode *inode, bool newdax);
 bool fuse_dax_check_alignment(struct fuse_conn *fc, unsigned int map_alignment);
 void fuse_dax_cancel_work(struct fuse_conn *fc);
 
+typedef int (*fuse_mount_cb_t)(struct file *file);
+extern fuse_mount_cb_t fuse_mount_callback;
+
 static inline bool is_virtfuse_device(struct file *file) { return false; }
 
 #endif /* _FS_FUSE_I_H */

commit 38c7eae1a1484ce590f9d0e15004f6bd4a5b5753

anolis: virtfuse: refuse mount when fuse_mount_callback not registered yet
ANBZ: #5299

Since currently fuse_mount_callback() is only used by virtfuse, refuse
mount and return error when fuse_mount_callback() is not registered yet.

Fixes: faf3e5e56720 ("anolis: fuse: add fuse_mount_callback hook")
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

fs/fuse/inode.c
@@ -1529,11 +1529,13 @@ static struct dentry *fuse_try_mount(struct file_system_type *fs_type,
 		else
 			root = dget(sb->s_root);
 	}
-
-	if (IS_ERR(root) || WARN_ON(!fuse_mount_callback))
+	if (IS_ERR(root))
 		goto out_fput;
 
-	ret = fuse_mount_callback(file);
+	if (WARN_ON(!fuse_mount_callback))
+		ret = -EINVAL;
+	else
+		ret = fuse_mount_callback(file);
 	if (ret) {
 		sb = root->d_sb;
 		dput(root);