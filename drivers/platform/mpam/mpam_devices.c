jeevandeepj994/anolis_perf

commit 9899206c666ae9037151fc9ea9cb9a231b395725

anolis: arm_mpam: Fix some bugs for MBWU function adaptation on Yitian
ANBZ: #1697

Yitian uses several custom registers to implement the memory bandwidth
monitoring function. Therefore, we make some modifications to the community
code.

1. Add definitions and r/w operations of these custom registers.
2. Split function `__ris_msmon_read()` into function `__ris_msmon_csu_read()`
and function `__ris_msmon_mbwu_read()`. The acquisition code of the custom
memory bandwidth is implemented in function `__ris_msmon_mbwu_read()`.
3. Migrate `mpam_feat_msmon_mbwu`, `mbm_total_event` and `mbm_local_event` from
resource L3 to resource MB.

Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
Reviewed-by: Xin Hao <xhao@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/561

drivers/platform/mpam/mpam_devices.c
@@ -33,6 +33,8 @@
 
 #include "mpam_internal.h"
 
+extern int ddr_cpufreq;
+
 DEFINE_STATIC_KEY_FALSE(mpam_enabled);
 
 /*
@@ -665,6 +667,22 @@ static void mpam_ris_hw_probe(struct mpam_msc_ris *ris)
 		mpam_set_feature(mpam_feat_partid_nrw, props);
 		msc->partid_max = min(msc->partid_max, partid_max);
 	}
+
+	/*
+	 * FIXME: Yitian does not have MPAMF_MSMON_IDR_MSMON_MBWU register, it
+	 * uses MPAMF_IDR_HAS_IMPL_IDR and some custom registers to get mbwu
+	 * monitor information instead.
+	 */
+	if (FIELD_GET(MPAMF_IDR_HAS_IMPL_IDR, ris->idr) && class->type == MPAM_CLASS_MEMORY) {
+		/*
+		 * resctrl expects the bandwidth counters to be free running,
+		 * which means to expose the files in the filesystem we need
+		 * as many monitors as resctrl has control/monitor groups.
+		 * Here, we set the two values to be equal by default.
+		 */
+		props->num_mbwu_mon = resctrl_arch_system_num_rmid_idx();
+		mpam_set_feature(mpam_feat_msmon_mbwu, props);
+	}
 }
 
 static int mpam_msc_hw_probe(struct mpam_msc *msc)
@@ -824,7 +842,12 @@ static u64 mpam_msmon_overflow_val(struct mpam_msc_ris *ris)
 	return GENMASK_ULL(30,0);
 }
 
-static void __ris_msmon_read(void *arg)
+/*
+ * FIXME: Yitian use several custom registers to implement mbwu monitor feature instead
+ * of MSMON_MBWU. So we split __ris_msmon_read() into __ris_msmon_csu_read() and
+ * __ris_msmon_mbwu_read().
+ */
+static void __maybe_unused __ris_msmon_read(void *arg)
 {
 	bool nrdy = false;
 	unsigned long flags;
@@ -905,6 +928,100 @@ static void __ris_msmon_read(void *arg)
 	*(m->val) += now;
 }
 
+static void __ris_msmon_csu_read(void *arg)
+{
+	bool nrdy = false;
+	unsigned long flags;
+	bool config_mismatch;
+	struct mon_read *m = arg;
+	u64 now;
+	struct mon_cfg *ctx = m->ctx;
+	struct mpam_msc *msc = m->ris->msc;
+	u32 mon_sel, ctl_val, flt_val, cur_ctl, cur_flt;
+
+	assert_spin_locked(&msc->lock);
+
+	spin_lock_irqsave(&msc->mon_sel_lock, flags);
+	mon_sel = FIELD_PREP(MSMON_CFG_MON_SEL_MON_SEL, ctx->mon) |
+		  FIELD_PREP(MSMON_CFG_MON_SEL_RIS, m->ris->ris_idx);
+	mpam_write_monsel_reg(msc, CFG_MON_SEL, mon_sel);
+
+	/*
+	 * Read the existing configuration to avoid re-writing the same values.
+	 * This saves waiting for 'nrdy' on subsequent reads.
+	 */
+	read_msmon_ctl_flt_vals(m, &cur_ctl, &cur_flt);
+	gen_msmon_ctl_flt_vals(m, &ctl_val, &flt_val);
+	config_mismatch = cur_flt != flt_val ||
+			  cur_ctl != (ctl_val | MSMON_CFG_x_CTL_EN);
+
+	if (config_mismatch)
+		write_msmon_ctl_flt_vals(m, ctl_val, flt_val);
+
+	now = mpam_read_monsel_reg(msc, CSU);
+	nrdy = now & MSMON___NRDY;
+	now = FIELD_GET(MSMON___VALUE, now);
+
+	spin_unlock_irqrestore(&msc->mon_sel_lock, flags);
+
+	if (nrdy) {
+		m->err = -EBUSY;
+		return;
+	}
+
+	*(m->val) += now;
+}
+
+#define MBWU_MASK GENMASK(23, 0)
+#define MBWU_GET(v) ((v) & MBWU_MASK)
+#define MPAMF_CUST_MBWC_OFFSET 0x08
+#define MPAMF_CUST_WINDW_OFFSET 0x0C
+
+static void __ris_msmon_mbwu_read(void *arg)
+{
+	unsigned long flags;
+	struct mon_read *m = arg;
+	u64 mb_val = 0;
+	struct mon_cfg *ctx = m->ctx;
+	bool reset_on_next_read = false;
+	struct mpam_msc_ris *ris = m->ris;
+	struct mpam_msc *msc = m->ris->msc;
+	struct msmon_mbwu_state *mbwu_state;
+	u32 custom_reg_base_addr, cycle, val;
+
+	assert_spin_locked(&msc->lock);
+
+	spin_lock_irqsave(&msc->mon_sel_lock, flags);
+
+	__mpam_write_reg(msc, MPAMCFG_PART_SEL, ctx->mon);
+
+	mbwu_state = &ris->mbwu_state[ctx->mon];
+	if (mbwu_state) {
+		reset_on_next_read = mbwu_state->reset_on_next_read;
+		mbwu_state->reset_on_next_read = false;
+	}
+
+	custom_reg_base_addr = __mpam_read_reg(msc, MPAMF_IMPL_IDR);
+
+	cycle = __mpam_read_reg(msc, custom_reg_base_addr + MPAMF_CUST_WINDW_OFFSET);
+	val = __mpam_read_reg(msc, custom_reg_base_addr + MPAMF_CUST_MBWC_OFFSET);
+
+	spin_unlock_irqrestore(&msc->mon_sel_lock, flags);
+
+	if (val & MSMON___NRDY) {
+		m->err = -EBUSY;
+		return;
+	}
+
+	mb_val = MBWU_GET(val);
+
+	mb_val = mb_val * 32 * ddr_cpufreq * 1000000 / cycle; /* B/s */
+	*(m->val) += mb_val;
+
+	/* Include bandwidth consumed before the last hardware reset */
+	*(m->val) += mbwu_state->correction;
+}
+
 static int _msmon_read(struct mpam_component *comp, struct mon_read *arg)
 {
 	int err, cpu;
@@ -928,11 +1045,24 @@ static int _msmon_read(struct mpam_component *comp, struct mon_read *arg)
 		spin_lock(&msc->lock);
 		cpu = get_cpu();
 		if (cpumask_test_cpu(cpu, mask)) {
-			__ris_msmon_read(arg);
-			err = 0;
-		} else if (!irqs_disabled())
-			err = smp_call_function_any(mask, __ris_msmon_read,
-						    arg, true);
+			if (arg->type == mpam_feat_msmon_csu) {
+				__ris_msmon_csu_read(arg);
+				err = 0;
+			} else if (arg->type == mpam_feat_msmon_mbwu) {
+				__ris_msmon_mbwu_read(arg);
+				err = 0;
+			} else
+				err = -EOPNOTSUPP;
+		} else if (!irqs_disabled()) {
+			if (arg->type == mpam_feat_msmon_csu)
+				err = smp_call_function_any(&msc->accessibility,
+						__ris_msmon_csu_read, arg, true);
+			else if (arg->type == mpam_feat_msmon_mbwu)
+				err = smp_call_function_any(&msc->accessibility,
+						__ris_msmon_mbwu_read, arg, true);
+			else
+				err = -EOPNOTSUPP;
+		}
 		put_cpu();
 		spin_unlock(&msc->lock);
 
@@ -1136,7 +1266,7 @@ static void mpam_restore_mbwu_state(void *_ris)
 			mwbu_arg.ctx = &ris->mbwu_state[i].cfg;
 			mwbu_arg.type = mpam_feat_msmon_mbwu;
 
-			__ris_msmon_read(&mwbu_arg);
+			__ris_msmon_mbwu_read(&mwbu_arg);
 		}
 	}
 }

drivers/acpi/arm64/mpam.c
@@ -15,6 +15,8 @@
 
 #include <asm/mpam.h>
 
+int ddr_cpufreq;
+
 static bool frob_irq(struct platform_device *pdev, int intid, u32 flags,
 		     int *irq, u32 processor_container_uid)
 {
@@ -105,6 +107,7 @@ static int acpi_mpam_parse_resource(struct mpam_msc *msc,
 		return mpam_ris_create(msc, res->ris_index, MPAM_CLASS_CACHE,
 				       level, cache_id);
 	case ACPI_MPAM_LOCATION_TYPE_MEMORY:
+		ddr_cpufreq = res->locator2;
 		return mpam_ris_create(msc, res->ris_index, MPAM_CLASS_MEMORY,
 				       255, res->locator1);
 	default:

drivers/platform/mpam/mpam_resctrl.c
@@ -691,6 +691,22 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 			r->alloc_capable = true;
 			exposed_alloc_capable = true;
 		}
+
+		if (class_has_usable_mbwu(class)) {
+			r->mon_capable = true;
+			exposed_mon_capable = true;
+
+			/*
+			 * Unfortunately, num_rmid doesn't mean anything for
+			 * mpam, and its exposed to user-space!
+			 * num-rmid is supposed to mean the number of groups
+			 * that can be created, both control or monitor groups.
+			 * For mpam, each control group has its own pmg/rmid
+			 * space.
+			 */
+			r->num_rmid = 1;
+			mbm_local_class = class;
+		}
 	}
 
 	return 0;

fs/resctrl/monitor.c
@@ -727,11 +727,31 @@ static void l3_mon_evt_init(struct rdt_resource *r)
 
 	if (resctrl_arch_is_llc_occupancy_enabled())
 		list_add_tail(&llc_occupancy_event.list, &r->evt_list);
+/*
+ * FIXME: There are some differences on Yitian ARM64 and X86_64 platform.
+ * X86_64's MBM events are covered by RDT_RESOURCE_L3, while Yitian ARM64's MBM
+ * events exist in RDT_RESOURCE_MBA. So we migrate them to a new function
+ * mba_mon_evt_init() by adding CONFIG_X86_64 and CONFIG_ARM64.
+ */
+#ifdef CONFIG_X86_64
+	if (resctrl_arch_is_mbm_total_enabled())
+		list_add_tail(&mbm_total_event.list, &r->evt_list);
+	if (resctrl_arch_is_mbm_local_enabled())
+		list_add_tail(&mbm_local_event.list, &r->evt_list);
+#endif
+}
+
+#ifdef CONFIG_ARM64
+static void mba_mon_evt_init(struct rdt_resource *r)
+{
+	INIT_LIST_HEAD(&r->evt_list);
+
 	if (resctrl_arch_is_mbm_total_enabled())
 		list_add_tail(&mbm_total_event.list, &r->evt_list);
 	if (resctrl_arch_is_mbm_local_enabled())
 		list_add_tail(&mbm_local_event.list, &r->evt_list);
 }
+#endif
 
 int resctrl_mon_resource_init(void)
 {
@@ -745,6 +765,15 @@ int resctrl_mon_resource_init(void)
 	if (r->mon_capable)
 		l3_mon_evt_init(r);
 
+#ifdef CONFIG_ARM64
+	r = resctrl_arch_get_resource(RDT_RESOURCE_MBA);
+
+	if (ret)
+		return ret;
+
+	if (r->mon_capable)
+		mba_mon_evt_init(r);
+#endif
 	return 0;
 }
 

commit f130d0f917eb31d9e164a9b2c881d509df1fae6f

anolis: arm_mpam: Fix the potential wrong MB monitoring value on Yitian710
ANBZ: #5225

Yitian710's vendor-defined MB monitoring feature uses PARTID instead of
the monitor id to get the monitor's value. Thus msc->part_sel_lock should
be locked to avoid reading potential wrong value caused by contention for
writing PART_SEL. Fix the incorrect use of msc->mon_sel_lock.

Fixes: 9899206c666a ("anolis: arm_mpam: Fix some bugs for MBWU function adaptation on Yitian")
Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
Reviewed-by: Xin Hao <xhao@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>

drivers/platform/mpam/mpam_devices.c
@@ -1002,7 +1002,7 @@ static void __ris_msmon_mbwu_read(void *arg)
 
 	assert_spin_locked(&msc->lock);
 
-	spin_lock_irqsave(&msc->mon_sel_lock, flags);
+	spin_lock_irqsave(&msc->part_sel_lock, flags);
 
 	__mpam_write_reg(msc, MPAMCFG_PART_SEL, ctx->mon);
 
@@ -1017,7 +1017,7 @@ static void __ris_msmon_mbwu_read(void *arg)
 	cycle = __mpam_read_reg(msc, custom_reg_base_addr + MPAMF_CUST_WINDW_OFFSET);
 	val = __mpam_read_reg(msc, custom_reg_base_addr + MPAMF_CUST_MBWC_OFFSET);
 
-	spin_unlock_irqrestore(&msc->mon_sel_lock, flags);
+	spin_unlock_irqrestore(&msc->part_sel_lock, flags);
 
 	if (val & MSMON___NRDY) {
 		m->err = -EBUSY;