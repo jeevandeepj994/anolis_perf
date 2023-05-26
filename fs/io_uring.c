jeevandeepj994/anolis_perf

commit 33846a1a34887733df20346841c27f6e3a63e737

io_uring: fix submission-failure handling for uring-cmd
ANBZ: #5226

commit a9c3eda7eada94e8cf29cb102aa80e1370d8fa2e upstream

If ->uring_cmd returned an error value different from -EAGAIN or
-EIOCBQUEUED, it gets overridden with IOU_OK. This invites trouble
as caller (io_uring core code) handles IOU_OK differently than other
error codes.
Fix this by returning the actual error code.

Signed-off-by: Kanchan Joshi <joshi.k@samsung.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>
Signed-off-by: Ziyang Zhang <ZiyangZhang@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Acked-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1644

fs/io_uring.c
@@ -4014,8 +4014,10 @@ static int io_uring_cmd(struct io_kiocb *req, unsigned int issue_flags)
 		return -EAGAIN;
 	}
 
-	if (ret != -EIOCBQUEUED)
+	if (ret != -EIOCBQUEUED) {
 		io_uring_cmd_done(ioucmd, ret, 0);
+		return ret;
+	}
 	return 0;
 }
 

commit 71b2b4de6a3a0f4925c1a8bab5bcd324306ce07c

anolis: io_uring: do not call io_uring_cmd_done() in case of error in io_uring_cmd()
ANBZ: #5226

io_uring_cmd() will complete req by io_uring_cmd_done() if ->uring_cmd()
returns error(e.g. -EINVAL). Then in __io_queue_sqe(), the same req will
completed again for a non-zero return value from io_uring_cmd(). This
could cause a UAF bug. See:

[Thu May 18 10:38:19 2023] ------------[ cut here ]------------
[Thu May 18 10:38:19 2023] refcount_t: underflow; use-after-free.
[Thu May 18 10:38:19 2023] WARNING: CPU: 14 PID: 8465 at lib/refcount.c:28 refcount_warn_saturate+0xba/0x110
[Thu May 18 10:38:19 2023] Modules linked in: tcp_diag(E) inet_diag(E) rfkill(E) mousedev(E) intel_rapl_msr(E) intel_rapl_common(E) n
fit(E) intel_powerclamp(E) crct10dif_pclmul(E) crc32_pclmul(E) ghash_clmulni_intel(E) rapl(E) psmouse(E) nfsd(E) pcspkr(E) virtio_balloon
(E) i2c_piix4(E) auth_rpcgss(E) nfs_acl(E) lockd(E) grace(E) sunrpc(E) cirrus(E) drm_kms_helper(E) syscopyarea(E) sysfillrect(E) sysimgbl
t(E) fb_sys_fops(E) nvme(E) virtio_net(E) crc32c_intel(E) drm(E) net_failover(E) nvme_core(E) serio_raw(E) failover(E) t10_pi(E) virtio_c
onsole(E) i2c_core(E)
[Thu May 18 10:38:19 2023] CPU: 14 PID: 8465 Comm: io_uring_passth Kdump: loaded Tainted: G            E     5.10.134+ #5
[Thu May 18 10:38:19 2023] Hardware name: Alibaba Cloud Alibaba Cloud ECS, BIOS 9e9f1cc 04/01/2014
[Thu May 18 10:38:19 2023] RIP: 0010:refcount_warn_saturate+0xba/0x110
[Thu May 18 10:38:19 2023] Code: 01 01 e8 cc ba 4c 00 0f 0b c3 cc cc cc cc 80 3d ed 20 d6 01 00 75 85 48 c7 c7 e0 45 3a b1 c6 05 dd 2
0 d6 01 01 e8 a9 ba 4c 00 <0f> 0b c3 cc cc cc cc 80 3d c8 20 d6 01 00 0f 85 5e ff ff ff 48 c7
[Thu May 18 10:38:19 2023] RSP: 0018:ffffbb83d14a7e00 EFLAGS: 00010286
[Thu May 18 10:38:19 2023] RAX: 0000000000000026 RBX: 00000000ffffffa1 RCX: ffff92040251fc48
[Thu May 18 10:38:19 2023] RDX: 00000000ffffffd8 RSI: 0000000000000027 RDI: ffff92040251fc40
[Thu May 18 10:38:19 2023] RBP: ffff91c6c5895d00 R08: 0000000000000000 R09: ffffbb83d14a7c58
[Thu May 18 10:38:19 2023] R10: ffffbb83d14a7c50 R11: ffffffffb2165268 R12: ffff91c6c8d4a000
[Thu May 18 10:38:19 2023] R13: 0000000000000000 R14: 0000000000000202 R15: ffff91c6c8d4a280
[Thu May 18 10:38:19 2023] FS:  00007f15c35da740(0000) GS:ffff920402500000(0000) knlGS:0000000000000000
[Thu May 18 10:38:19 2023] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
[Thu May 18 10:38:19 2023] CR2: 00007f15c3653da0 CR3: 00000001383c8004 CR4: 00000000007706e0
[Thu May 18 10:38:19 2023] PKRU: 55555554
[Thu May 18 10:38:19 2023] Call Trace:
[Thu May 18 10:38:19 2023]  __io_queue_sqe+0x150/0x240
[Thu May 18 10:38:19 2023]  io_submit_sqe+0x1d3/0x260
[Thu May 18 10:38:19 2023]  io_submit_sqes+0x156/0x470
[Thu May 18 10:38:19 2023]  ? strncpy_from_user+0x4e/0x150
[Thu May 18 10:38:19 2023]  __do_sys_io_uring_enter+0x2d8/0x490
[Thu May 18 10:38:19 2023]  do_syscall_64+0x30/0x40
[Thu May 18 10:38:19 2023]  entry_SYSCALL_64_after_hwframe+0x61/0xc6
[Thu May 18 10:38:19 2023] RIP: 0033:0x4038d5
[Thu May 18 10:38:19 2023] Code: 41 89 d2 41 b9 08 00 00 00 b8 aa 01 00 00 41 83 ca 10 f6 87 d0 00 00 00 01 8b bf cc 00 00 00 44 0f 4
4 d2 45 31 c0 31 d2 0f 05 <c3> 66 2e 0f 1f 84 00 00 00 00 00 89 32 48 8b 17 8b 87 c0 00 00 00
[Thu May 18 10:38:19 2023] RSP: 002b:00007ffd078b61b8 EFLAGS: 00000246 ORIG_RAX: 00000000000001aa
[Thu May 18 10:38:19 2023] RAX: ffffffffffffffda RBX: 0000000000000000 RCX: 00000000004038d5
[Thu May 18 10:38:19 2023] RDX: 0000000000000000 RSI: 0000000000000001 RDI: 0000000000000003
[Thu May 18 10:38:19 2023] RBP: 0000000000000005 R08: 0000000000000000 R09: 0000000000000008
[Thu May 18 10:38:19 2023] R10: 0000000000000001 R11: 0000000000000246 R12: 0000000000000001
[Thu May 18 10:38:19 2023] R13: 0000000000000004 R14: 00000000001c0000 R15: 0000000000000000
[Thu May 18 10:38:19 2023] ---[ end trace c360dac843bd8087 ]---

Fixes: 33846a1a3488("io_uring: fix submission-failure handling for uring-cmd")

fs/io_uring.c
@@ -4014,10 +4014,9 @@ static int io_uring_cmd(struct io_kiocb *req, unsigned int issue_flags)
 		return -EAGAIN;
 	}
 
-	if (ret != -EIOCBQUEUED) {
-		io_uring_cmd_done(ioucmd, ret, 0);
+	if (ret != -EIOCBQUEUED)
 		return ret;
-	}
+
 	return 0;
 }
 