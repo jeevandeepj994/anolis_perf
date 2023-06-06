1394d0136627ca5e7791cbad6a81a2480ef7b445
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2023-06-06 17:26:35 +0300

    x86/tdx: Make _tdx_hypercall() and __tdx_module_call() available in boot stub
    ANBZ: #9469
    
    commit ff40b5769a50fab654a70575ff0f49853b799b0e upstream
    
    Memory acceptance requires a hypercall and one or multiple module calls.
    
    Make helpers for the calls available in boot stub. It has to accept
    memory where kernel image and initrd are placed.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: https://lore.kernel.org/r/20230606142637.5171-8-kirill.shutemov@linux.intel.com
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -28,21 +28,6 @@ static inline bool platform_has_tdx(void) { return false; }
 extern enum tdx_module_status_t tdx_module_status;
 
 /*
- * Used to gather the output registers values of the TDCALL and SEAMCALL
- * instructions when requesting services from the TDX module.
- *
- * This is a software only structure and not part of the TDX module/VMM ABI.
- */
-struct tdx_module_output {
-	u64 rcx;
-	u64 rdx;
-	u64 r8;
-	u64 r9;
-	u64 r10;
-	u64 r11;
-};
-
-/*
  * Used by the #VE exception handler to gather the #VE exception
  * info from the TDX module. This is a software only structure
  * and not part of the TDX module/VMM ABI.
@@ -61,10 +46,6 @@ struct ve_info {
 #ifdef CONFIG_INTEL_TDX_GUEST
 
 void __init tdx_early_init(void);
-
-/* Used to communicate with the TDX module */
-u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
-		      struct tdx_module_output *out);
 
 void tdx_get_ve_info(struct ve_info *ve);
 
