30f1db83e0474d3f4aea582639acad9b13c18b2e
Jakub Kicinski <jakub.kicinski@netronome.com>
2023-06-19 15:55:48 +0800

    tools: bpftool: use 4 context mode for the NFP disasm
    ANBZ: #5530
    
    commit 3ddeac6705aba31b7528c7d7a528eabb74475622 upstream.
    
    The nfp driver is currently always JITing the BPF for 4 context/thread
    mode of the NFP flow processors.  Tell this to the disassembler,
    otherwise some registers may be incorrectly decoded.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Jiong Wang <jiong.wang@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Xuezhi Zhao <zhaoxuezhi@uniontech.com>
    Acked-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2035

--- tools/bpf/bpftool/jit_disasm.c
+++ tools/bpf/bpftool/jit_disasm.c
@@ -80,7 +80,7 @@ static int fprintf_json(void *out, const char *fmt, ...)
 }
 
 void disasm_print_insn(unsigned char *image, ssize_t len, int opcodes,
-		       const char *arch)
+		       const char *arch, const char *disassembler_options)
 {
 	disassembler_ftype disassemble;
 	struct disassemble_info info;
@@ -119,6 +119,8 @@ void disasm_print_insn(unsigned char *image, ssize_t len, int opcodes,
 
 	info.arch = bfd_get_arch(bfdf);
 	info.mach = bfd_get_mach(bfdf);
+	if (disassembler_options)
+		info.disassembler_options = disassembler_options;
 	info.buffer = image;
 	info.buffer_length = len;
 
