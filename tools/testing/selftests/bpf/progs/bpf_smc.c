031dd81469179db3cbe947d7ddf74fe0fffdbaa0
D. Wythe <alibuda@linux.alibaba.com>
2023-06-20 12:39:24 +0800

    anolis: bpf/selftests: fix compile error on bpf_smc
    ANBZ: #5562
    
    fix compile error on bpf_smc.c, remove unnecessary
    code.
    
    Fixes: a81fa9db4db5 ("anolis: bpf/selftests: add selftest for SMC bpf capability")
    Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1768

--- tools/testing/selftests/bpf/progs/bpf_smc.c
+++ tools/testing/selftests/bpf/progs/bpf_smc.c
@@ -26,31 +26,40 @@ struct smc_strategy {
 	__u8   reserved1;
 	/* how many rounds for long cc */
 	__u16	rtt_threshold;
-	/* For every productivity[0] connections with
-	 * productivity[1] long connections,
-	 * generate one credits. When productivity[0] is 0, it is equivalent to
-	 * every productivity[1] long connections can generate one credits.
-	 */
-	__u16	smc_productivity[2];
-	__u16   tcp_productivity[2];
+	/* low = 0, hi = 1, N = 2 */
+	__u16	smc_productivity[4];
+	__u16   tcp_productivity[4];
+#define LOW_WATER_LEVEL(domain)		domain##_productivity[0]
+#define HI_WATER_LEVEL(domain)		domain##_productivity[1]
+#define EVERY_N(domain)			domain##_productivity[2]
 	/* max value of credits, limit the totol smc-r */
 	__u32   max_credits;
+	/* Initial value of credits */
+	__u32	initial_credits;
 	/* max burst in one slice */
 	__u32	max_pacing_burst;
 	/* fixed pacing delta */
 	__u64	pacing_delta;
 };
 
+/* maps for smc_strategy */
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1024);
+	__type(key, __u16);
+	__type(value, struct smc_strategy);
+} smc_strategies SEC(".maps");
+
 struct smc_prediction {
-	/* smc_strategy for this predictor */
-	struct smc_strategy strategy;
 	/* count to allow smc */
 	__u32 credits;
 	__u32 pacing_burst;
-	/* produce */
-	__u16 smc_producer[2];
-	__u16 tcp_producer[2];
-	__u16 tcp_continuous_hits;
+	/* count for smc conn */
+	__u16 count_total_smc_conn;
+	__u16 count_matched_smc_conn;
+	/* count fot tcp conn */
+	__u16 count_total_tcp_conn;
+	__u16 count_matched_tcp_conn;
 	/* last pick timestamp */
 	__u64 last_tstamp;
 	/* protection for smc_prediction */
@@ -59,48 +68,74 @@ struct smc_prediction {
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 64);
+	__uint(max_entries, 1024);
 	__type(key, __u16);
 	__type(value, struct smc_prediction);
-} negotiator_map SEC(".maps");
+} smc_predictors SEC(".maps");
 
-
-static inline struct smc_prediction *smc_prediction_get(__u16 key, __u64 tstamp)
+static inline struct smc_prediction *smc_prediction_get(__u16 key, struct smc_strategy *strategy)
 {
 	struct smc_prediction *smc_predictor;
 
-	smc_predictor = bpf_map_lookup_elem(&negotiator_map, &key);
+	smc_predictor = bpf_map_lookup_elem(&smc_predictors, &key);
+	if (!smc_predictor) {
+		struct smc_prediction init = {
+			.credits = strategy->initial_credits,
+		};
+		bpf_map_update_elem(&smc_predictors, &key, &init, BPF_NOEXIST);
+		smc_predictor = bpf_map_lookup_elem(&smc_predictors, &key);
+	}
 	return smc_predictor;
+}
+
+static inline struct smc_strategy *smc_strategy_get(__u16 key)
+{
+	struct smc_strategy *strategy;
+
+	strategy = bpf_map_lookup_elem(&smc_strategies, &key);
+	if (!strategy && key != 0) {
+		/* search for default */
+		key = 0;
+		strategy = bpf_map_lookup_elem(&smc_strategies, &key);
+	}
+	return strategy;
 }
 
 int SEC("struct_ops/bpf_smc_negotiate")
 BPF_PROG(bpf_smc_negotiate, struct sock *sk)
 {
 	struct smc_prediction *smc_predictor;
+	struct smc_strategy *strategy;
+	struct smc_sock *smc;
 	struct tcp_sock *tp;
 	__u64 now;
 	__u16 key;
 
-	/* client side */
-	if (sk == NULL || sk->__sk_common.skc_state != SMC_LISTEN) {
-		/* use Global smc_predictor */
-		key = 0;
-	} else {	/* server side */
-		tp = bpf_skc_to_tcp_sock(sk);
-		if (!tp)
-			goto error;
-		key = tp->inet_conn.icsk_inet.sk.__sk_common.skc_num;
+	if (!sk)
+		return SK_DROP;
+
+	smc = smc_sk(sk);
+
+	/* for client side */
+	if (!smc->listen_smc && smc->sk.__sk_common.skc_state != SMC_LISTEN) {
+		/* client always say yes */
+		return SK_PASS;
 	}
 
-	smc_predictor = smc_prediction_get(key, bpf_jiffies64());
-	/* whitelist mode */
-	if (!smc_predictor)
+	/* every full smc sock should contains a tcp sock */
+	tp = bpf_skc_to_tcp_sock(sk);
+
+	/* local port as key */
+	key = tp ? tp->inet_conn.icsk_inet.sk.__sk_common.skc_num : 0;
+
+	strategy = smc_strategy_get(key);
+	if (!strategy)
 		return SK_DROP;
 
 #define DENYLIST_MODE	(0)
-#define AUTO_MODE	(1)
+#define AUTO_MODE		(1)
 #define ALLOWLIST_MODE	(2)
-	switch (smc_predictor->strategy.mode) {
+	switch (strategy->mode) {
 	case AUTO_MODE:
 		break;
 	case ALLOWLIST_MODE:
@@ -113,6 +148,10 @@ BPF_PROG(bpf_smc_negotiate, struct sock *sk)
 #undef AUTO_MODE
 #undef DENYLIST_MODE
 
+	smc_predictor = smc_prediction_get(key, strategy);
+	if (!smc_predictor)
+		return SK_DROP;
+
 	now = bpf_jiffies64();
 
 	bpf_spin_lock(&smc_predictor->lock);
@@ -120,14 +159,14 @@ BPF_PROG(bpf_smc_negotiate, struct sock *sk)
 		goto out_locked_drop;
 out_locked_pass:
 	/* pacing incoming rate */
-	if (now - smc_predictor->last_tstamp < smc_predictor->strategy.pacing_delta) {
+	if (now - smc_predictor->last_tstamp < strategy->pacing_delta) {
 pacing:
 		if (!smc_predictor->pacing_burst)
 			goto out_locked_drop;
 		smc_predictor->pacing_burst--;
 	} else {
 		smc_predictor->last_tstamp = now;
-		smc_predictor->pacing_burst = smc_predictor->strategy.max_pacing_burst;
+		smc_predictor->pacing_burst = strategy->max_pacing_burst;
 		goto pacing;
 	}
 	smc_predictor->credits--;
@@ -144,11 +183,15 @@ BPF_PROG(bpf_smc_collect_info, struct sock *sk, int timing)
 {
 	bool match = false, smc_traffic = false;
 	struct smc_prediction *smc_predictor;
+	struct smc_strategy *strategy;
 	struct smc_sock *smc;
 	struct tcp_sock *tp;
+	__u64 delta = 0;
+	__u16 key;
 
-	/* no info can collect */
-	if (sk == NULL)
+	/* smc sock */
+	smc = smc_sk(sk);
+	if (!smc)
 		return;
 
 	/* only fouces on closed */
@@ -160,67 +203,82 @@ BPF_PROG(bpf_smc_collect_info, struct sock *sk, int timing)
 	if (!tp)
 		return;
 
-	/* get */
-	smc_predictor = smc_prediction_get(tp->inet_conn.icsk_inet.sk.__sk_common.skc_num, 0);
-	/* whitelist mode */
+	if (!smc->listen_smc)
+		/* only monitor passive open for server */
+		return;
+
+	/* local port as key */
+	key = tp->inet_conn.icsk_inet.sk.__sk_common.skc_num;
+	if (key == 0)
+		return;
+
+	strategy = smc_strategy_get(key);
+	if (!strategy)
+		return;
+
+	smc_predictor = smc_prediction_get(key, strategy);
 	if (!smc_predictor)
 		return;
 
-	/* smc sock */
-	if (sk->__sk_common.skc_family == AF_SMC) {
-		smc = smc_sk(sk);
-		if (smc->use_fallback)
-			goto fallback;
-		smc_traffic = true;
-		/* full rtt  */
-		match = smc->conn.tx_cdc_seq > smc_predictor->strategy.rtt_threshold;
-	} else {
-fallback:
-		match = tp->data_segs_out > tp->snd_cwnd * smc_predictor->strategy.rtt_threshold;
+	switch (sk->__sk_common.skc_family) {
+	case AF_INET:
+		if (sk != &tp->inet_conn.icsk_inet.sk)
+			return;
+	case AF_SMC:
+		if (!smc->use_fallback) {
+			smc_traffic = true;
+			/* full rtt*/
+			match = smc->conn.tx_cdc_seq > strategy->rtt_threshold;
+			break;
+		}
+	default:
+		match = tp->data_segs_out > tp->snd_cwnd * strategy->rtt_threshold;
+		break;
 	}
 
 	bpf_spin_lock(&smc_predictor->lock);
 	if (smc_traffic) {
-		if (++smc_predictor->smc_producer[0] >
-				smc_predictor->strategy.smc_productivity[0]) {
-			/* burst trafiic */
-			if (smc_predictor->smc_producer[1] <
-					smc_predictor->strategy.smc_productivity[1])
-				smc_predictor->credits = 0;
-			goto reset_smc_producer;
-		}
-		if (match) {
-			/* return back credits */
-			smc_predictor->credits++;
-			if (++smc_predictor->smc_producer[1] >=
-					smc_predictor->strategy.smc_productivity[1]) {
-				smc_predictor->credits = min(smc_predictor->strategy.max_credits,
-					smc_predictor->credits + 1);
-reset_smc_producer:
-				smc_predictor->smc_producer[0] = 0;
-				smc_predictor->smc_producer[1] = 0;
-			}
+		/* matched smc connection */
+		if (match)
+			++smc_predictor->count_matched_smc_conn;
+		/* For every N smc connection，matched connection in
+		 * [0, LOW_WATER_LEVEL)			: smc_predictor->credits >> 1;
+		 * [LOW_WATER_LEVEL, HI_WATER_LEVEL)	: no impact;
+		 * [HI_WATER_LEVEL, )			: inc smc_predictor->credits
+		 */
+		if (++smc_predictor->count_total_smc_conn >= strategy->EVERY_N(smc)) {
+			/* fast down-grade */
+			if (smc_predictor->count_matched_smc_conn < strategy->LOW_WATER_LEVEL(smc))
+				smc_predictor->credits = smc_predictor->credits >> 1;
+			else if (smc_predictor->count_matched_smc_conn >=
+					strategy->HI_WATER_LEVEL(smc))
+				/* return back */
+				smc_predictor->credits = min(smc_predictor->credits + 1,
+					strategy->max_credits);
+			/* reset smc_producer */
+			smc_predictor->count_total_smc_conn = 0;
+			smc_predictor->count_matched_smc_conn = 0;
 		}
 	} else {
-		/* only update when no credits */
-		if (++smc_predictor->tcp_producer[0] >
-				smc_predictor->strategy.tcp_productivity[0]) {
-			smc_predictor->tcp_continuous_hits = 0;
-			goto reset_tcp_producer;
-		}
-		if (match) {
-			if (++smc_predictor->tcp_producer[1]
-					>= smc_predictor->strategy.tcp_productivity[1]) {
-				smc_predictor->credits +=
-					min(smc_predictor->strategy.tcp_productivity[1],
-						1 << smc_predictor->tcp_continuous_hits++);
-				smc_predictor->credits = min(smc_predictor->credits,
-						smc_predictor->strategy.max_credits);
-reset_tcp_producer:
-				smc_predictor->tcp_producer[0] = 0;
-				smc_predictor->tcp_producer[1] = 0;
-
-			}
+		if (match)
+			/* matched tcp connection */
+			++smc_predictor->count_matched_tcp_conn;
+		/* For every N tcp connection，matched connection in
+		 * [0, LOW_WATER_LEVEL)			: no impact
+		 * [LOW_WATER_LEVEL, HI_WATER_LEVEL)	: inc smc_predictor->credits
+		 * [HI_WATER_LEVEL, )			: add smc_predictor->credits by n.
+		 */
+		if (++smc_predictor->count_total_tcp_conn >= strategy->EVERY_N(tcp)) {
+			if (smc_predictor->count_matched_tcp_conn >= strategy->HI_WATER_LEVEL(tcp))
+				delta = smc_predictor->count_matched_tcp_conn;
+			else if (smc_predictor->count_matched_tcp_conn >=
+					strategy->LOW_WATER_LEVEL(tcp))
+				delta = 1;
+			smc_predictor->credits = min(smc_predictor->credits + delta,
+				strategy->max_credits);
+			/* reset tcp_producer */
+			smc_predictor->count_total_tcp_conn = 0;
+			smc_predictor->count_matched_tcp_conn = 0;
 		}
 	}
 	bpf_spin_unlock(&smc_predictor->lock);
@@ -229,7 +287,7 @@ error:
 }
 
 SEC(".struct_ops")
-struct smc_sock_negotiator_ops smc_negotiator = {
+struct smc_sock_negotiator_ops anolis_smc = {
 	.name = "anolis",
 	.negotiate	= (void *)bpf_smc_negotiate,
 	.collect_info	= (void *)bpf_smc_collect_info,
