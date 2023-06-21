feba311c99c53c057bffe8af35829fbb0fa10642
Stanislav Fomichev <sdf@google.com>
2023-06-21 10:28:44 +0800

    libbpf: add internal pin_name
    ANBZ: #5530
    
    commit 33a2c75c55e24aa30ff9fed805ae8bea13c1e2a3 upstream.
    
    pin_name is the same as section_name where '/' is replaced
    by '_'. bpf_object__pin_programs is converted to use pin_name
    to avoid the situation where section_name would require creating another
    subdirectory for a pin (as, for example, when calling bpf_object__pin_programs
    for programs in sections like "cgroup/connect6").
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Xuezhi Zhao <zhaoxuezhi@uniontech.com>
    Acked-by: Tony Lu <tonylu@linux.alibaba.com>
    Reviewed-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2060

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -126,6 +126,10 @@ struct bpf_program {
 	char *name;
 	int prog_ifindex;
 	char *section_name;
+	/* section_name with / replaced by _; makes recursive pinning
+	 * in bpf_object__pin_programs easier
+	 */
+	char *pin_name;
 	struct bpf_insn *insns;
 	size_t insns_cnt, main_prog_cnt;
 	enum bpf_prog_type type;
@@ -255,12 +259,24 @@ static void bpf_program__exit(struct bpf_program *prog)
 	bpf_program__unload(prog);
 	zfree(&prog->name);
 	zfree(&prog->section_name);
+	zfree(&prog->pin_name);
 	zfree(&prog->insns);
 	zfree(&prog->reloc_desc);
 
 	prog->nr_reloc = 0;
 	prog->insns_cnt = 0;
 	prog->idx = -1;
+}
+
+static char *__bpf_program__pin_name(struct bpf_program *prog)
+{
+	char *name, *p;
+
+	name = p = strdup(prog->section_name);
+	while ((p = strchr(p, '/')))
+		*p = '_';
+
+	return name;
 }
 
 static int
@@ -277,6 +293,13 @@ bpf_program__init(void *data, size_t size, char *section_name, int idx,
 	prog->section_name = strdup(section_name);
 	if (!prog->section_name) {
 		pr_warning("failed to alloc name for prog under section(%d) %s\n",
+			   idx, section_name);
+		goto errout;
+	}
+
+	prog->pin_name = __bpf_program__pin_name(prog);
+	if (!prog->pin_name) {
+		pr_warning("failed to alloc pin name for prog under section(%d) %s\n",
 			   idx, section_name);
 		goto errout;
 	}
@@ -2008,7 +2031,7 @@ int bpf_object__pin_programs(struct bpf_object *obj, const char *path)
 		int len;
 
 		len = snprintf(buf, PATH_MAX, "%s/%s", path,
-			       prog->section_name);
+			       prog->pin_name);
 		if (len < 0) {
 			err = -EINVAL;
 			goto err_unpin_programs;
@@ -2030,7 +2053,7 @@ err_unpin_programs:
 		int len;
 
 		len = snprintf(buf, PATH_MAX, "%s/%s", path,
-			       prog->section_name);
+			       prog->pin_name);
 		if (len < 0)
 			continue;
 		else if (len >= PATH_MAX)
@@ -2055,7 +2078,7 @@ int bpf_object__unpin_programs(struct bpf_object *obj, const char *path)
 		int len;
 
 		len = snprintf(buf, PATH_MAX, "%s/%s", path,
-			       prog->section_name);
+			       prog->pin_name);
 		if (len < 0)
 			return -EINVAL;
 		else if (len >= PATH_MAX)
