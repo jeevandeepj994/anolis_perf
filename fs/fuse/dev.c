jeevandeepj994/anolis_perf

commit a88ac284ae44c3b79f049fb1b56e13b15e36d1b9

anolis: fuse: add fusectl interface to stats
ANBZ: #5290

Add statistics summary in /sys/fs/fuse/connections.
The statistics about the historical request processed by
filesystem daemon, including total request count and processing
time, grouping by different common fuse opcodes.

Signed-off-by: Xu Ji <laoji.jx@alibaba-inc.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1670

fs/fuse/dev.c
@@ -40,6 +40,11 @@ static struct fuse_dev *fuse_get_dev(struct file *file)
 	return READ_ONCE(file->private_data);
 }
 
+static inline uint64_t get_time_now_us(void)
+{
+	return ktime_to_us(ktime_get());
+}
+
 static void fuse_request_init(struct fuse_mount *fm, struct fuse_req *req)
 {
 	INIT_LIST_HEAD(&req->list);
@@ -269,6 +274,19 @@ static void flush_bg_queue(struct fuse_conn *fc)
 	}
 }
 
+static void fuse_update_stats(struct fuse_conn *fc, int opcode, uint64_t send_time)
+{
+	uint64_t delta_time;
+
+	delta_time = get_time_now_us() - send_time;
+
+	atomic64_add(delta_time, &fc->stats.req_time[FUSE_SUMMARY]);
+	atomic64_add(delta_time, &fc->stats.req_time[opcode]);
+
+	atomic64_inc(&fc->stats.req_cnts[FUSE_SUMMARY]);
+	atomic64_inc(&fc->stats.req_cnts[opcode]);
+}
+
 /*
  * This function is called when a request is finished.  Either a reply
  * has arrived or it was aborted (and not yet sent) or some error
@@ -299,6 +317,8 @@ void fuse_request_end(struct fuse_req *req)
 	WARN_ON(test_bit(FR_PENDING, &req->flags));
 	WARN_ON(test_bit(FR_SENT, &req->flags));
 	if (test_bit(FR_BACKGROUND, &req->flags)) {
+		fuse_update_stats(fc, req->in.h.opcode, req->send_time);
+
 		spin_lock(&fc->bg_lock);
 		clear_bit(FR_BACKGROUND, &req->flags);
 		if (fc->num_background == fc->max_background) {
@@ -414,6 +434,7 @@ static void request_wait_answer(struct fuse_req *req)
 static void __fuse_request_send(struct fuse_req *req)
 {
 	struct fuse_iqueue *fiq = &req->fm->fc->iq;
+	struct fuse_conn *fc = req->fm->fc;
 
 	BUG_ON(test_bit(FR_BACKGROUND, &req->flags));
 	spin_lock(&fiq->lock);
@@ -421,6 +442,7 @@ static void __fuse_request_send(struct fuse_req *req)
 		spin_unlock(&fiq->lock);
 		req->out.h.error = -ENOTCONN;
 	} else {
+		req->send_time = get_time_now_us();
 		req->in.h.unique = fuse_get_unique(fiq);
 		/* acquire extra reference, since request is still needed
 		   after fuse_request_end() */
@@ -430,6 +452,8 @@ static void __fuse_request_send(struct fuse_req *req)
 		request_wait_answer(req);
 		/* Pairs with smp_wmb() in fuse_request_end() */
 		smp_rmb();
+
+		fuse_update_stats(fc, req->in.h.opcode, req->send_time);
 	}
 }
 
@@ -535,6 +559,9 @@ static bool fuse_request_queue_background(struct fuse_req *req)
 		atomic_inc(&fc->num_waiting);
 	}
 	__set_bit(FR_ISREPLY, &req->flags);
+
+	req->send_time = get_time_now_us();
+
 	spin_lock(&fc->bg_lock);
 	if (likely(fc->connected)) {
 		fc->num_background++;

fs/fuse/control.c
@@ -14,6 +14,35 @@
 
 #define FUSE_CTL_SUPER_MAGIC 0x65735543
 
+#define PRINT_LINE(buff, remain, ...)					\
+	do {								\
+		if (remain) {                                           \
+			size_t used = snprintf(buff, remain, ##__VA_ARGS__); \
+			if (used >= remain) {				\
+				remain = 0;				\
+				buff = NULL;				\
+			} else {					\
+				remain -= used;				\
+				buff += used;				\
+			}						\
+		}                                                       \
+	} while (0)
+
+#define PRINT_HEADER(buff, remain, header)	\
+	PRINT_LINE(buff, remain, header)
+
+#define PRINT_STATS(__opname, fc, remain, buff)				\
+	do {								\
+		uint64_t tot_time = atomic64_read(&fc->stats.req_time[__opname]); \
+		uint64_t cnt = atomic64_read(&fc->stats.req_cnts[__opname]); \
+		PRINT_LINE(buff, remain, "%s: "				\
+			"total reqs: %llu, "				\
+			"tot_time_avg: %llu\n",				\
+			# __opname,					\
+			cnt,						\
+			tot_time / (cnt + 1));				\
+	} while (0)
+
 /*
  * This is non-NULL when the single instance of the control filesystem
  * exists.  Protected by fuse_mutex
@@ -29,6 +58,70 @@ static struct fuse_conn *fuse_ctl_file_conn_get(struct file *file)
 		fc = fuse_conn_get(fc);
 	mutex_unlock(&fuse_mutex);
 	return fc;
+}
+
+static ssize_t fuse_conn_stats_write(struct file *file, const char __user *buffer,
+				size_t count, loff_t *f_pos)
+{
+	int i;
+	struct fuse_conn *fc = fuse_ctl_file_conn_get(file);
+
+	if (!fc)
+		return 0;
+
+	for (i = 0; i < FUSE_OP_MAX; i++) {
+		atomic64_set(&fc->stats.req_time[i], 0);
+		atomic64_set(&fc->stats.req_cnts[i], 0);
+	}
+	fuse_conn_put(fc);
+
+	return count;
+}
+
+static ssize_t fuse_conn_stats_read(struct file *file, char __user *buf,
+				size_t len, loff_t *ppos)
+{
+	char *data;
+	char *buff;
+	uint64_t remain = PAGE_SIZE;
+	ssize_t ret;
+	struct fuse_conn *fc = fuse_ctl_file_conn_get(file);
+
+	if (!fc)
+		return 0;
+
+	data = kmalloc(PAGE_SIZE, GFP_KERNEL);
+	if (!data) {
+		fuse_conn_put(fc);
+		return 0;
+	}
+	buff = data;
+
+	PRINT_STATS(FUSE_LOOKUP, fc, remain, buff);
+	PRINT_STATS(FUSE_MKNOD, fc, remain, buff);
+	PRINT_STATS(FUSE_MKDIR, fc, remain, buff);
+	PRINT_STATS(FUSE_OPEN, fc, remain, buff);
+	PRINT_STATS(FUSE_CREATE, fc, remain, buff);
+	PRINT_STATS(FUSE_WRITE, fc, remain, buff);
+	PRINT_STATS(FUSE_GETATTR, fc, remain, buff);
+	PRINT_STATS(FUSE_SETATTR, fc, remain, buff);
+	PRINT_STATS(FUSE_ACCESS, fc, remain, buff);
+	PRINT_STATS(FUSE_UNLINK, fc, remain, buff);
+	PRINT_STATS(FUSE_RMDIR, fc, remain, buff);
+	PRINT_STATS(FUSE_RENAME, fc, remain, buff);
+	PRINT_STATS(FUSE_RELEASE, fc, remain, buff);
+	PRINT_STATS(FUSE_FLUSH, fc, remain, buff);
+	PRINT_STATS(FUSE_FSYNC, fc, remain, buff);
+	PRINT_STATS(FUSE_READ, fc, remain, buff);
+	PRINT_STATS(FUSE_READDIR, fc, remain, buff);
+	PRINT_STATS(FUSE_READDIRPLUS, fc, remain, buff);
+	PRINT_STATS(FUSE_SUMMARY, fc, remain, buff);
+
+	fuse_conn_put(fc);
+	ret = simple_read_from_buffer(buf, len, ppos, data, PAGE_SIZE - remain);
+	kfree(data);
+
+	return ret;
 }
 
 static ssize_t fuse_conn_abort_write(struct file *file, const char __user *buf,
@@ -363,6 +456,13 @@ static const struct file_operations fuse_conn_max_background_ops = {
 	.llseek = no_llseek,
 };
 
+static const struct file_operations fuse_conn_stats_ops = {
+	.open = nonseekable_open,
+	.read = fuse_conn_stats_read,
+	.write = fuse_conn_stats_write,
+	.llseek = no_llseek,
+};
+
 static const struct file_operations fuse_conn_congestion_threshold_ops = {
 	.open = nonseekable_open,
 	.read = fuse_conn_congestion_threshold_read,
@@ -445,6 +545,8 @@ int fuse_ctl_add_conn(struct fuse_conn *fc)
 				 NULL, &fuse_ctl_flush_ops) ||
 	    !fuse_ctl_add_dentry(parent, fc, "resend", S_IFREG | 0200, 1,
 				 NULL, &fuse_ctl_resend_ops) ||
+	    !fuse_ctl_add_dentry(parent, fc, "stats", S_IFREG | 0600, 1,
+				 NULL, &fuse_conn_stats_ops) ||
 	    !fuse_ctl_add_dentry(parent, fc, "max_background", S_IFREG | 0600,
 				 1, NULL, &fuse_conn_max_background_ops) ||
 	    !fuse_ctl_add_dentry(parent, fc, "passthrough", S_IFREG | 0600,

fs/fuse/fuse_i.h
@@ -46,7 +46,7 @@
 #define FUSE_NAME_MAX 1024
 
 /** Number of dentries for each connection in the control filesystem */
-#define FUSE_CTL_NUM_DENTRIES 9
+#define FUSE_CTL_NUM_DENTRIES 10
 
 /** List of active connections */
 extern struct list_head fuse_conn_list;
@@ -391,6 +391,9 @@ struct fuse_req {
 	/** Used to wake up the task waiting for completion of request*/
 	wait_queue_head_t waitq;
 
+	/** send time*/
+	uint64_t send_time;
+
 #if IS_ENABLED(CONFIG_VIRTIO_FS)
 	/** virtio-fs's physically contiguous buffer for in and out args */
 	void *argbuf;
@@ -545,6 +548,14 @@ struct fuse_fs_context {
 
 	/* fuse_dev pointer to fill in, should contain NULL on entry */
 	void **fudptr;
+};
+
+/**
+ *  Fuse stats for debuging.
+ */
+struct fuse_stats {
+	atomic64_t req_time[FUSE_OP_MAX];
+	atomic64_t req_cnts[FUSE_OP_MAX];
 };
 
 /**
@@ -846,6 +857,9 @@ struct fuse_conn {
 
 	/* fuse connection tag */
 	char tag[FUSE_TAG_NAME_MAX];
+
+	/** fuse stats **/
+	struct fuse_stats stats;
 
 #ifdef CONFIG_FUSE_DAX
 	/* Dax mode */

include/uapi/linux/fuse.h
@@ -467,6 +467,7 @@ struct fuse_file_lock {
 #define FUSE_OPEN_KILL_SUIDGID	(1 << 0)
 
 enum fuse_opcode {
+	FUSE_SUMMARY            = 0,
 	FUSE_LOOKUP		= 1,
 	FUSE_FORGET		= 2,  /* no reply */
 	FUSE_GETATTR		= 3,
@@ -514,6 +515,7 @@ enum fuse_opcode {
 	FUSE_COPY_FILE_RANGE	= 47,
 	FUSE_SETUPMAPPING	= 48,
 	FUSE_REMOVEMAPPING	= 49,
+	FUSE_OP_MAX,
 
 	/* CUSE specific operations */
 	CUSE_INIT		= 4096,

commit f43f4bb5ccc795807cb0da65e97e5df992df3fc6

anolis: fuse: fix array-index-out-of-bounds in fuse_update_stats()
ANBZ: #5579

The following array-index-out-of-bounds is caught on cuse:

UBSAN: array-index-out-of-bounds in fs/fuse/dev.c:284:46
index 4096 is out of range for type 'atomic64_t [50]'
Call trace:
 dump_backtrace+0x0/0x3b0
 show_stack+0x1c/0x2c
 dump_stack+0x144/0x1b8
 ubsan_epilogue+0xc/0x4c
 __ubsan_handle_out_of_bounds+0x78/0x80
 fuse_update_stats+0x148/0x164 [fuse]
 fuse_request_end+0x174/0x780 [fuse]
 end_requests+0x128/0x1d0 [fuse]
 fuse_abort_conn+0x7a8/0x9f0 [fuse]
 fuse_dev_release+0x220/0x3e0 [fuse]
 cuse_channel_release+0x1d0/0x270 [cuse]
 __fput+0x1ac/0x6b4
 ____fput+0x14/0x20
 task_work_run+0xd8/0x1f0
 do_exit+0x530/0xc9c
 do_group_exit+0xd0/0x240
 __arm64_sys_exit_group+0x40/0x50
 el0_svc_common+0x154/0x520
 do_el0_svc+0xac/0xd4
 el0_svc+0x1c/0x30
 el0_sync_handler+0xa8/0xac
 el0_sync+0x168/0x180

This is because the cuse device node shares the same file_operations,
i.e. fuse_dev_operations, with the fuse device node, and
fuse_update_stats() is called on CUSE_INIT (opcode 4096) message.

Fix this by only allowing fuse_update_stats() on normal fuse messages.

Fixes: a88ac284ae44 ("anolis: fuse: add fusectl interface to stats")
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

fs/fuse/dev.c
@@ -274,17 +274,20 @@ static void flush_bg_queue(struct fuse_conn *fc)
 	}
 }
 
-static void fuse_update_stats(struct fuse_conn *fc, int opcode, uint64_t send_time)
+static void fuse_update_stats(struct fuse_conn *fc, struct fuse_req *req)
 {
+	unsigned int opcode = req->in.h.opcode;
 	uint64_t delta_time;
 
-	delta_time = get_time_now_us() - send_time;
+	if (opcode < FUSE_OP_MAX) {
+		delta_time = get_time_now_us() - req->send_time;
 
-	atomic64_add(delta_time, &fc->stats.req_time[FUSE_SUMMARY]);
-	atomic64_add(delta_time, &fc->stats.req_time[opcode]);
+		atomic64_add(delta_time, &fc->stats.req_time[FUSE_SUMMARY]);
+		atomic64_add(delta_time, &fc->stats.req_time[opcode]);
 
-	atomic64_inc(&fc->stats.req_cnts[FUSE_SUMMARY]);
-	atomic64_inc(&fc->stats.req_cnts[opcode]);
+		atomic64_inc(&fc->stats.req_cnts[FUSE_SUMMARY]);
+		atomic64_inc(&fc->stats.req_cnts[opcode]);
+	}
 }
 
 /*
@@ -317,7 +320,7 @@ void fuse_request_end(struct fuse_req *req)
 	WARN_ON(test_bit(FR_PENDING, &req->flags));
 	WARN_ON(test_bit(FR_SENT, &req->flags));
 	if (test_bit(FR_BACKGROUND, &req->flags)) {
-		fuse_update_stats(fc, req->in.h.opcode, req->send_time);
+		fuse_update_stats(fc, req);
 
 		spin_lock(&fc->bg_lock);
 		clear_bit(FR_BACKGROUND, &req->flags);
@@ -453,7 +456,7 @@ static void __fuse_request_send(struct fuse_req *req)
 		/* Pairs with smp_wmb() in fuse_request_end() */
 		smp_rmb();
 
-		fuse_update_stats(fc, req->in.h.opcode, req->send_time);
+		fuse_update_stats(fc, req);
 	}
 }
 