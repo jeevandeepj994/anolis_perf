jeevandeepj994/anolis_perf

commit 6157772a81240b70cd7e2f568897b6f882670638

anolis: riscv: Fix building issues
ANBZ: #3085

Fix building issues on RISC-V platform.

Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1264

fs/io_uring.c
@@ -10046,7 +10046,9 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 		sqd = *per_cpu_ptr(percpu_sqd, cpu);
 		io_sq_thread_park(sqd);
 		sqd->thread->flags &= ~PF_NO_SETAFFINITY;
+#ifdef CONFIG_CGROUP_CPUACCT
 		ret = io_sq_attach_cpu_cgroup(sqd->thread);
+#endif
 		sqd->thread->flags |= PF_NO_SETAFFINITY;
 		io_sq_thread_unpark(sqd);
 		mutex_unlock(&percpu_sqd_lock);

arch/riscv/kernel/process.c
@@ -7,6 +7,7 @@
  * Copyright (C) 2017 SiFive
  */
 
+#include <linux/compat.h>
 #include <linux/cpu.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>

include/linux/module.h
@@ -860,9 +860,6 @@ extern int module_sysfs_initialized;
 #ifdef CONFIG_STRICT_MODULE_RWX
 extern void module_enable_ro(const struct module *mod, bool after_init);
 extern void module_disable_ro(const struct module *mod);
-#else
-static inline void module_enable_ro(const struct module *mod, bool after_init) { }
-static inline void module_disable_ro(const struct module *mod) { }
 #endif
 
 #ifdef CONFIG_GENERIC_BUG

commit 899aea3edbfaea32b8fbae527f020669a81c18cc

anolis: io_uring: remove redundant CONFIG_CGROUP_CPUACCT
ANBZ: #5818

When reading code in io_uring_create(), I've found that there is a
redundant macro "#ifdef CONFIG_CGROUP_CPUACCT" which was introduced by:
6157772a81240 "anolis: riscv: Fix building issues"

So remove it.

Fixes: 6157772a81240 ("anolis: riscv: Fix building issues")
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/io_uring.c
@@ -10219,9 +10219,7 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 		sqd = *per_cpu_ptr(percpu_sqd, cpu);
 		io_sq_thread_park(sqd);
 		sqd->thread->flags &= ~PF_NO_SETAFFINITY;
-#ifdef CONFIG_CGROUP_CPUACCT
 		ret = io_sq_attach_cpu_cgroup(sqd->thread);
-#endif
 		sqd->thread->flags |= PF_NO_SETAFFINITY;
 		io_sq_thread_unpark(sqd);
 		mutex_unlock(&percpu_sqd_lock);