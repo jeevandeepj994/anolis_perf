7a814915b4c1d7cb2545fd1ec139a178fb4f407c
Fenghua Yu <fenghua.yu@intel.com>
2023-07-12 10:44:35 -0700

    dmaengine: idxd: Simplify WQ attribute visibility checks
    ANBZ: #8450
    
    commit 97b1185fe54c8ce94104e3c7fa4ee0bbedd85920 upstream.
    
    The functions that check if WQ attributes are invisible are almost
    duplicate. Define a helper to simplify these functions and future
    WQ attribute visibility checks as well.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Link: https://lore.kernel.org/r/20230712174436.3435088-1-fenghua.yu@intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2827

--- drivers/dma/idxd/sysfs.c
+++ drivers/dma/idxd/sysfs.c
@@ -1303,12 +1303,9 @@ static struct attribute *idxd_wq_attributes[] = {
 	NULL,
 };
 
-static bool idxd_wq_attr_op_config_invisible(struct attribute *attr,
-					     struct idxd_device *idxd)
-{
-	return attr == &dev_attr_wq_op_config.attr &&
-	       !idxd->hw.wq_cap.op_config;
-}
+/*  A WQ attr is invisible if the feature is not supported in WQCAP. */
+#define idxd_wq_attr_invisible(name, cap_field, a, idxd)		\
+	((a) == &dev_attr_wq_##name.attr && !(idxd)->hw.wq_cap.cap_field)
 
 static bool idxd_wq_attr_max_batch_size_invisible(struct attribute *attr,
 						  struct idxd_device *idxd)
@@ -1316,13 +1313,6 @@ static bool idxd_wq_attr_max_batch_size_invisible(struct attribute *attr,
 	/* Intel IAA does not support batch processing, make it invisible */
 	return attr == &dev_attr_wq_max_batch_size.attr &&
 	       idxd->data->type == IDXD_TYPE_IAX;
-}
-
-static bool idxd_wq_attr_wq_prs_disable_invisible(struct attribute *attr,
-						  struct idxd_device *idxd)
-{
-	return attr == &dev_attr_wq_prs_disable.attr &&
-	       !idxd->hw.wq_cap.wq_prs_support;
 }
 
 static umode_t idxd_wq_attr_visible(struct kobject *kobj,
@@ -1335,10 +1325,10 @@ static umode_t idxd_wq_attr_visible(struct kobject *kobj,
 	if (idxd_wq_attr_max_batch_size_invisible(attr, idxd))
 		return 0;
 
-	if (idxd_wq_attr_op_config_invisible(attr, idxd))
+	if (idxd_wq_attr_invisible(op_config, op_config, attr, idxd))
 		return 0;
 
-	if (idxd_wq_attr_wq_prs_disable_invisible(attr, idxd))
+	if (idxd_wq_attr_invisible(prs_disable, wq_prs_support, attr, idxd))
 		return 0;
 
 	return attr->mode;
