jeevandeepj994/anolis_perf

commit fb089a35ce59ed11ddb440c0c9c6fa4aca05ae87

anolis: net/smc: optimize SMC_CONN_PER_LGR_MAX and SMC_WR_BUF_CNT for better performance
ANBZ: #5551

Shrink SMC_CONN_PER_LGR_MAX to 32 and enlarge SMC_WR_BUF_CNT
to 64 for better performance. Setting SMC_WR_BUF_CNT larger than
SMC_CONN_PER_LGR_MAX. For tx process, it can reduce the posibility
of waiting in smc_wr_tx_get_free_slot. For rx process, it can reduce
the posibility of RNR occurrence. As waiting in tx and RNR in rx may
result in bad performance.

The test of redis-benchmark shows that more than 3X rps improvement in
SET and more than 7X rps improvement in GET.
 Test command:
 redis-server --save "" --appendonly no --protected-mode no
 	--io-threads 7 --io-threads-do-reads yes
 redis-benchmark -h 192.168.26.36 -q -t set,get -P 1 --threads 7
 	-n 2000000 -c 500 -d 10

 Before:
 SET: 173325.25 requests per second, p50=2.703 msec
 GET: 81383.52 requests per second, p50=5.575 msec

 After:
 SET: 554323.69 requests per second, p50=0.959 msec
 GET: 604741.19 requests per second, p50=0.855 msec

Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1759

net/smc/smc_core.h
@@ -23,7 +23,12 @@
 #include "smc_stats.h"
 
 #define SMC_RMBS_PER_LGR_MAX	255	/* max. # of RMBs per link group */
-#define SMC_CONN_PER_LGR_MAX	255	/* max. # of connections per link group */
+#define SMC_CONN_PER_LGR_MAX	32	/* max. # of connections per link group.
+					 * Correspondingly, SMC_WR_BUF_CNT should not be less than
+					 * 2 * SMC_CONN_PER_LGR_MAX, since every connection at
+					 * least has two rq/sq credits in average, otherwise
+					 * may result in waiting for credits in sending process.
+					 */
 
 struct smc_lgr_list {			/* list of link group definition */
 	struct list_head	list;

net/smc/smc_wr.c
@@ -534,7 +534,7 @@ void smc_wr_remember_qp_attr(struct smc_link *lnk)
 
 	lnk->wr_tx_cnt = min_t(size_t, SMC_WR_BUF_CNT,
 			       lnk->qp_attr.cap.max_send_wr);
-	lnk->wr_rx_cnt = min_t(size_t, SMC_WR_BUF_CNT * 3,
+	lnk->wr_rx_cnt = min_t(size_t, SMC_WR_BUF_CNT,
 			       lnk->qp_attr.cap.max_recv_wr);
 	lnk_stats->qpn = lnk->roce_qp->qp_num;
 }
@@ -718,7 +718,7 @@ int smc_wr_alloc_link_mem(struct smc_link *link)
 	link->wr_tx_bufs = kcalloc(SMC_WR_BUF_CNT, SMC_WR_BUF_SIZE, GFP_KERNEL);
 	if (!link->wr_tx_bufs)
 		goto no_mem;
-	link->wr_rx_bufs = kcalloc(SMC_WR_BUF_CNT * 3, rx_buf_size,
+	link->wr_rx_bufs = kcalloc(SMC_WR_BUF_CNT, rx_buf_size,
 				   GFP_KERNEL);
 	if (!link->wr_rx_bufs)
 		goto no_mem_wr_tx_bufs;
@@ -726,7 +726,7 @@ int smc_wr_alloc_link_mem(struct smc_link *link)
 				  GFP_KERNEL);
 	if (!link->wr_tx_ibs)
 		goto no_mem_wr_rx_bufs;
-	link->wr_rx_ibs = kcalloc(SMC_WR_BUF_CNT * 3,
+	link->wr_rx_ibs = kcalloc(SMC_WR_BUF_CNT,
 				  sizeof(link->wr_rx_ibs[0]),
 				  GFP_KERNEL);
 	if (!link->wr_rx_ibs)
@@ -745,7 +745,7 @@ int smc_wr_alloc_link_mem(struct smc_link *link)
 				   GFP_KERNEL);
 	if (!link->wr_tx_sges)
 		goto no_mem_wr_tx_rdma_sges;
-	link->wr_rx_sges = kcalloc(SMC_WR_BUF_CNT * 3, sizeof(link->wr_rx_sges[0]),
+	link->wr_rx_sges = kcalloc(SMC_WR_BUF_CNT, sizeof(link->wr_rx_sges[0]),
 				   GFP_KERNEL);
 	if (!link->wr_rx_sges)
 		goto no_mem_wr_tx_sges;

net/smc/smc_wr.h
@@ -19,7 +19,12 @@
 #include "smc.h"
 #include "smc_core.h"
 
-#define SMC_WR_BUF_CNT 16	/* # of ctrl buffers per link */
+#define SMC_WR_BUF_CNT 64	/* # of ctrl buffers per link, SMC_WR_BUF_CNT
+				 * should not be less than 2 * SMC_RMBS_PER_LGR_MAX,
+				 * since every connection at least has two rq/sq
+				 * credits in average, otherwise may result in
+				 * waiting for credits in sending process.
+				 */
 
 #define SMC_WR_TX_WAIT_FREE_SLOT_TIME	(10 * HZ)
 

net/smc/smc_ib.c
@@ -816,10 +816,12 @@ int smc_ib_create_queue_pair(struct smc_link *lnk)
 		.srq = NULL,
 		.cap = {
 				/* include unsolicited rdma_writes as well,
-				 * there are max. 2 RDMA_WRITE per 1 WR_SEND
+				 * there are max. 2 RDMA_WRITE per 1 WR_SEND.
+				 * RDMA_WRITE consumes send queue entities,
+				 * without recv queue entities.
 				 */
 			.max_send_wr = SMC_WR_BUF_CNT * 3,
-			.max_recv_wr = SMC_WR_BUF_CNT * 3,
+			.max_recv_wr = SMC_WR_BUF_CNT,
 			.max_send_sge = SMC_IB_MAX_SEND_SGE,
 			.max_recv_sge = 1,
 			.max_inline_data = 0,

commit 440937314222c5c6c9dc022a7ececec2baaa5a11

anolis: net/smc: Split wr_rx_buf into SMC_WR_BUF_CNT
ANBZ: #5791

In the workaround of SMCv2 supporting eRDMA device, we expand wr_rx_buf
from 44B to 8KB, and the Rx depth now is SMC_WR_BUF_CNT.

So if we alloc the whole wr_rx_buf at one time, it will need SMC_WR_BUF_CNT
* 8K (total 512KB) physical continuous memory, which is likely to fail. So
this patch splits wr_rx_buf into SMC_WR_BUF_CNT to reduce the probability
of failure to alloc wr_rx_buf.

Fixes: fb22f8b8c244 ("anolis: net/smc: Workaround for SMCv2 support with eRDMA devices")
Fixes: fb089a35ce59 ("anolis: net/smc: optimize SMC_CONN_PER_LGR_MAX and SMC_WR_BUF_CNT for better performance").
Signed-off-by: Wen Gu <guwen@linux.alibaba.com>

net/smc/smc_core.h
@@ -50,7 +50,12 @@ enum smc_link_state {			/* possible states of a link */
 
 #define SMC_WR_BUF_SIZE		48	/* size of work request buffer */
 #define SMC_WR_BUF_V2_SIZE	8192	/* size of v2 work request buffer */
-
+#define SMC_WR_BUF_CNT		64	/* # of ctrl buffers per link, SMC_WR_BUF_CNT
+					 * should not be less than 2 * SMC_RMBS_PER_LGR_MAX,
+					 * since every connection at least has two rq/sq
+					 * credits in average, otherwise may result in
+					 * waiting for credits in sending process.
+					 */
 struct smc_wr_buf {
 	u8	raw[SMC_WR_BUF_SIZE];
 };
@@ -123,11 +128,13 @@ struct smc_link {
 	struct completion	tx_ref_comp;
 	atomic_t		tx_inflight_credit;
 
-	struct smc_wr_buf	*wr_rx_bufs;	/* WR recv payload buffers */
+	struct smc_wr_buf	*wr_rx_bufs[SMC_WR_BUF_CNT];
+						/* WR recv payload buffers */
 	struct ib_recv_wr	*wr_rx_ibs;	/* WR recv meta data */
 	struct ib_sge		*wr_rx_sges;	/* WR recv scatter meta data */
 	/* above three vectors have wr_rx_cnt elements and use the same index */
-	dma_addr_t		wr_rx_dma_addr;	/* DMA address of wr_rx_bufs */
+	dma_addr_t		wr_rx_dma_addr[SMC_WR_BUF_CNT];
+						/* DMA address of wr_rx_bufs */
 	u64			wr_rx_id;	/* seq # of last recv WR */
 	u32			wr_rx_cnt;	/* number of WR recv buffers */
 	unsigned long		wr_rx_tstamp;	/* jiffies when last buf rx */

net/smc/smc_wr.c
@@ -419,8 +419,6 @@ out_unlock:
 static inline void smc_wr_rx_demultiplex(struct ib_wc *wc)
 {
 	struct smc_link *link = (struct smc_link *)wc->qp->qp_context;
-	int rx_buf_size = (link->lgr->smc_version == SMC_V2) ?
-			  SMC_WR_BUF_V2_SIZE : SMC_WR_BUF_SIZE;
 	struct smc_wr_rx_handler *handler;
 	struct smc_wr_rx_hdr *wr_rx;
 	u64 temp_wr_id;
@@ -430,7 +428,7 @@ static inline void smc_wr_rx_demultiplex(struct ib_wc *wc)
 		return; /* short message */
 	temp_wr_id = wc->wr_id / 2;
 	index = do_div(temp_wr_id, link->wr_rx_cnt);
-	wr_rx = (struct smc_wr_rx_hdr *)((u8 *)link->wr_rx_bufs + index * rx_buf_size);
+	wr_rx = (struct smc_wr_rx_hdr *)(link->wr_rx_bufs[index]);
 	hash_for_each_possible(smc_wr_rx_hash, handler, list, wr_rx->type) {
 		if (handler->type == wr_rx->type)
 			handler->handler(wc, wr_rx);
@@ -633,11 +631,8 @@ static void smc_wr_init_sge(struct smc_link *lnk)
 	for (i = 0; i < lnk->wr_rx_cnt; i++) {
 		int rx_msg_size = (lnk->lgr->smc_version == SMC_V2) ?
 				  SMC_WR_BUF_V2_SIZE : SMC_WR_TX_SIZE;
-		int rx_buf_size = (lnk->lgr->smc_version == SMC_V2) ?
-				  SMC_WR_BUF_V2_SIZE : SMC_WR_BUF_SIZE;
 
-		lnk->wr_rx_sges[i].addr =
-			lnk->wr_rx_dma_addr + i * rx_buf_size;
+		lnk->wr_rx_sges[i].addr = lnk->wr_rx_dma_addr[i];
 		lnk->wr_rx_sges[i].length = rx_msg_size;
 		lnk->wr_rx_sges[i].lkey = lnk->roce_pd->local_dma_lkey;
 		lnk->wr_rx_ibs[i].next = NULL;
@@ -656,6 +651,7 @@ void smc_wr_free_link(struct smc_link *lnk)
 	int rx_buf_size = (lnk->lgr->smc_version == SMC_V2) ?
 			  SMC_WR_BUF_V2_SIZE : SMC_WR_BUF_SIZE;
 	struct ib_device *ibdev;
+	int i;
 
 	if (!lnk->smcibdev)
 		return;
@@ -670,11 +666,12 @@ void smc_wr_free_link(struct smc_link *lnk)
 	percpu_ref_kill(&lnk->wr_tx_refs);
 	wait_for_completion(&lnk->tx_ref_comp);
 
-	if (lnk->wr_rx_dma_addr) {
-		ib_dma_unmap_single(ibdev, lnk->wr_rx_dma_addr,
-				    rx_buf_size * lnk->wr_rx_cnt,
-				    DMA_FROM_DEVICE);
-		lnk->wr_rx_dma_addr = 0;
+	for (i = 0; i < lnk->wr_rx_cnt; i++) {
+		if (lnk->wr_rx_dma_addr[i]) {
+			ib_dma_unmap_single(ibdev, lnk->wr_rx_dma_addr[i],
+					    rx_buf_size, DMA_FROM_DEVICE);
+			lnk->wr_rx_dma_addr[i] = 0;
+		}
 	}
 	if (lnk->wr_tx_dma_addr) {
 		ib_dma_unmap_single(ibdev, lnk->wr_tx_dma_addr,
@@ -701,6 +698,8 @@ void smc_wr_free_lgr_mem(struct smc_link_group *lgr)
 
 void smc_wr_free_link_mem(struct smc_link *lnk)
 {
+	int i;
+
 	kfree(lnk->wr_tx_v2_ib);
 	lnk->wr_tx_v2_ib = NULL;
 	kfree(lnk->wr_tx_v2_sge);
@@ -727,8 +726,10 @@ void smc_wr_free_link_mem(struct smc_link *lnk)
 	lnk->wr_tx_ibs = NULL;
 	kfree(lnk->wr_tx_bufs);
 	lnk->wr_tx_bufs = NULL;
-	kfree(lnk->wr_rx_bufs);
-	lnk->wr_rx_bufs = NULL;
+	for (i = 0 ; i < SMC_WR_BUF_CNT; i++) {
+		kfree(lnk->wr_rx_bufs[i]);
+		lnk->wr_rx_bufs[i] = NULL;
+	}
 }
 
 int smc_wr_alloc_lgr_mem(struct smc_link_group *lgr)
@@ -746,15 +747,23 @@ int smc_wr_alloc_link_mem(struct smc_link *link)
 {
 	int rx_buf_size = (link->lgr->smc_version == SMC_V2) ?
 			  SMC_WR_BUF_V2_SIZE : SMC_WR_BUF_SIZE;
+	int i, j;
 
 	/* allocate link related memory */
 	link->wr_tx_bufs = kcalloc(SMC_WR_BUF_CNT, SMC_WR_BUF_SIZE, GFP_KERNEL);
 	if (!link->wr_tx_bufs)
 		goto no_mem;
-	link->wr_rx_bufs = kcalloc(SMC_WR_BUF_CNT, rx_buf_size,
-				   GFP_KERNEL);
-	if (!link->wr_rx_bufs)
-		goto no_mem_wr_tx_bufs;
+
+	for (i = 0; i < SMC_WR_BUF_CNT; i++) {
+		link->wr_rx_bufs[i] = kzalloc(rx_buf_size, GFP_KERNEL);
+		if (!link->wr_rx_bufs[i]) {
+			for (j = i - 1; j >= 0; j--) {
+				kfree(link->wr_rx_bufs[j]);
+				link->wr_rx_bufs[j] = NULL;
+			}
+			goto no_mem_wr_tx_bufs;
+		}
+	}
 	link->wr_tx_ibs = kcalloc(SMC_WR_BUF_CNT, sizeof(link->wr_tx_ibs[0]),
 				  GFP_KERNEL);
 	if (!link->wr_tx_ibs)
@@ -835,7 +844,8 @@ no_mem_wr_rx_ibs:
 no_mem_wr_tx_ibs:
 	kfree(link->wr_tx_ibs);
 no_mem_wr_rx_bufs:
-	kfree(link->wr_rx_bufs);
+	for (i = 0; i < SMC_WR_BUF_CNT; i++)
+		kfree(link->wr_rx_bufs[i]);
 no_mem_wr_tx_bufs:
 	kfree(link->wr_tx_bufs);
 no_mem:
@@ -879,17 +889,25 @@ int smc_wr_create_link(struct smc_link *lnk)
 	int rx_buf_size = (lnk->lgr->smc_version == SMC_V2) ?
 			  SMC_WR_BUF_V2_SIZE : SMC_WR_BUF_SIZE;
 	struct ib_device *ibdev = lnk->smcibdev->ibdev;
-	int rc = 0;
+	int i, j, rc = 0;
 
 	smc_wr_tx_set_wr_id(&lnk->wr_tx_id, 0);
 	lnk->wr_rx_id = 1;
-	lnk->wr_rx_dma_addr = ib_dma_map_single(
-		ibdev, lnk->wr_rx_bufs,	rx_buf_size * lnk->wr_rx_cnt,
-		DMA_FROM_DEVICE);
-	if (ib_dma_mapping_error(ibdev, lnk->wr_rx_dma_addr)) {
-		lnk->wr_rx_dma_addr = 0;
-		rc = -EIO;
-		goto out;
+
+	for (i = 0; i < lnk->wr_rx_cnt; i++) {
+		lnk->wr_rx_dma_addr[i] =
+			ib_dma_map_single(ibdev, lnk->wr_rx_bufs[i],
+					  rx_buf_size, DMA_FROM_DEVICE);
+		if (ib_dma_mapping_error(ibdev, lnk->wr_rx_dma_addr[i])) {
+			lnk->wr_rx_dma_addr[i] = 0;
+			for (j = i - 1; j >= 0; j--) {
+				ib_dma_unmap_single(ibdev, lnk->wr_rx_dma_addr[j],
+						    rx_buf_size, DMA_FROM_DEVICE);
+				lnk->wr_rx_dma_addr[j] = 0;
+			}
+			rc = -EIO;
+			goto out;
+		}
 	}
 	if (lnk->lgr->smc_version == SMC_V2) {
 		lnk->wr_tx_v2_dma_addr = ib_dma_map_single(ibdev,
@@ -940,10 +958,11 @@ dma_unmap:
 				    DMA_TO_DEVICE);
 		lnk->wr_tx_v2_dma_addr = 0;
 	}
-	ib_dma_unmap_single(ibdev, lnk->wr_rx_dma_addr,
-			    rx_buf_size * lnk->wr_rx_cnt,
-			    DMA_FROM_DEVICE);
-	lnk->wr_rx_dma_addr = 0;
+	for (i = 0; i < lnk->wr_rx_cnt; i++) {
+		ib_dma_unmap_single(ibdev, lnk->wr_rx_dma_addr[i],
+				    rx_buf_size, DMA_FROM_DEVICE);
+		lnk->wr_rx_dma_addr[i] = 0;
+	}
 out:
 	return rc;
 }

net/smc/smc_wr.h
@@ -19,13 +19,6 @@
 #include "smc.h"
 #include "smc_core.h"
 
-#define SMC_WR_BUF_CNT 64	/* # of ctrl buffers per link, SMC_WR_BUF_CNT
-				 * should not be less than 2 * SMC_RMBS_PER_LGR_MAX,
-				 * since every connection at least has two rq/sq
-				 * credits in average, otherwise may result in
-				 * waiting for credits in sending process.
-				 */
-
 #define SMC_WR_TX_WAIT_FREE_SLOT_TIME	(10 * HZ)
 
 #define SMC_WR_TX_SIZE 44 /* actual size of wr_send data (<=SMC_WR_BUF_SIZE) */