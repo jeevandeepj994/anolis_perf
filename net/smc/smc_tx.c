jeevandeepj994/anolis_perf

commit b4919d778d9ac38c36c9323b0c6b9fba4425f0af

anolis: net/smc: Introduce rtoken validity check before sending
ANBZ: #5550

The local peer might be still sending data when receiving remote
peer requests for deleting rtoken. So the local peer might use an
already deleted rkey for rdma write operation.

In eRDMA scenario, this may cause a hung because eRDMA driver won't
generate CQEs for sending with wrong rkey and cdc_pend_tx_wr won't
reach zero anymore.

So this patch tries to fix this by checking rtoken validity before
rdma write operation. This won't cause data loss because at this
moment, the remote peer must be SMC_CLOSE state and no longer want
to receive any data.

Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1754

net/smc/smc_tx.c
@@ -347,6 +347,12 @@ static int smc_tx_rdma_write(struct smc_connection *conn, int peer_rmbe_offset,
 		/* offset within RMBE */
 		peer_rmbe_offset;
 	rdma_wr->rkey = lgr->rtokens[conn->rtoken_idx][link->link_idx].rkey;
+	/* rtoken might be deleted if peer freed connection */
+	if (!rdma_wr->rkey ||
+	    (rdma_wr->remote_addr == (conn->tx_off + peer_rmbe_offset))) {
+		pr_warn_ratelimited("smc: unexpected sends during connection termination flow\n");
+		return -EINVAL;
+	}
 	rc = ib_post_send(link->roce_qp, &rdma_wr->wr, NULL);
 	if (rc)
 		smcr_link_down_cond_sched(link);

commit 6a3f03907d94ead281a34af5bbe81fb0eabacc27

anolis: net/smc: do not validate rkey by testing rkey value
ANBZ: #5791

When sending rdma_write data, checking whether the rkey is valid
is needed. The value of valid range of rkey depends on the vendor
implementation of RNIC. So validate rkey by testing rtokens_used_mask
instead of testing rkey value.

Fixes: b4919d778d9a ("anolis: net/smc: Introduce rtoken validity check before sending")
Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1894

net/smc/smc_tx.c
@@ -349,7 +349,7 @@ static int smc_tx_rdma_write(struct smc_connection *conn, int peer_rmbe_offset,
 		peer_rmbe_offset;
 	rdma_wr->rkey = lgr->rtokens[conn->rtoken_idx][link->link_idx].rkey;
 	/* rtoken might be deleted if peer freed connection */
-	if (!rdma_wr->rkey ||
+	if (!test_bit(conn->rtoken_idx, lgr->rtokens_used_mask) ||
 	    (rdma_wr->remote_addr == (conn->tx_off + peer_rmbe_offset))) {
 		pr_warn_ratelimited("smc: unexpected sends during connection termination flow\n");
 		return -EINVAL;