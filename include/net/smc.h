jeevandeepj994/anolis_perf

commit 8bde38f5cd611f4186229d825f1ad751bf25e5f3

anolis: bpf: add smc negotiator support in BPF struct_ops
ANBZ: #5552

This PATCH attempts to introduce BPF injection capability for SMC.
Considering that the SMC protocol is not suitable for all scenarios,
especially for short-lived. However, for most applications, they cannot
guarantee that there are no such scenarios at all. Therefore, apps
may need some specific strategies to decide shall we need to use SMC
or not, for example, apps can limit the scope of the SMC to a specific
IP address or port.

Based on the consideration of transparent replacement, we hope that apps
can remain transparent even if they need to formulate some specific
strategies for SMC using. That is, do not need to recompile their code.

On the other hand, we need to ensure the scalability of strategies
implementation. Although it is simple to use socket options or sysctl,
it will bring more complexity to subsequent expansion.

Fortunately, BPF can solve these concerns very well, users can write
thire own strategies in eBPF to choose whether to use SMC or not.
And it's quite easy for them to modify their strategies in the future.

This PATCH implement injection capability for SMC via struct_ops.
In that way, we can add new injection scenarios in the future.

Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1762

net/smc/bpf_smc.c
@@ -7,14 +7,20 @@
  *  Author(s):  D. Wythe <alibuda@linux.alibaba.com>
  */
 
+#include <linux/bpf_verifier.h>
+#include <linux/btf_ids.h>
 #include <linux/kernel.h>
 #include <linux/bpf.h>
+#include <linux/btf.h>
 #include <linux/smc.h>
 #include <net/sock.h>
 #include "smc.h"
 
+struct bpf_struct_ops bpf_smc_sock_negotiator_ops;
+
 static DEFINE_SPINLOCK(smc_sock_negotiator_list_lock);
 static LIST_HEAD(smc_sock_negotiator_list);
+static u32 smc_sock_id, sock_id;
 
 /* required smc_sock_negotiator_list_lock locked */
 static struct smc_sock_negotiator_ops *smc_negotiator_ops_get_by_key(u32 key)
@@ -198,3 +204,149 @@ void smc_sock_clone_negotiator_ops(struct sock *parent, struct sock *child)
 	rcu_read_unlock();
 }
 EXPORT_SYMBOL_GPL(smc_sock_clone_negotiator_ops);
+
+static int bpf_smc_negotiator_init(struct btf *btf)
+{
+	s32 type_id;
+
+	type_id = btf_find_by_name_kind(btf, "sock", BTF_KIND_STRUCT);
+	if (type_id < 0)
+		return -EINVAL;
+	sock_id = type_id;
+
+	type_id = btf_find_by_name_kind(btf, "smc_sock", BTF_KIND_STRUCT);
+	if (type_id < 0)
+		return -EINVAL;
+	smc_sock_id = type_id;
+
+	return 0;
+}
+
+/* register ops */
+static int bpf_smc_negotiator_reg(void *kdata)
+{
+	return smc_sock_register_negotiator_ops(kdata);
+}
+
+/* unregister ops */
+static void bpf_smc_negotiator_unreg(void *kdata)
+{
+	smc_sock_unregister_negotiator_ops(kdata);
+}
+
+static int bpf_smc_negotiator_check_member(const struct btf_type *t,
+					   const struct btf_member *member)
+{
+	return 0;
+}
+
+static int bpf_smc_negotiator_init_member(const struct btf_type *t,
+					  const struct btf_member *member,
+					  void *kdata, const void *udata)
+{
+	const struct smc_sock_negotiator_ops *uops;
+	struct smc_sock_negotiator_ops *ops;
+	u32 moff;
+
+	uops = (const struct smc_sock_negotiator_ops *)udata;
+	ops = (struct smc_sock_negotiator_ops *)kdata;
+
+	moff = btf_member_bit_offset(t, member) / 8;
+
+	/* init name */
+	if (moff ==  offsetof(struct smc_sock_negotiator_ops, name)) {
+		if (bpf_obj_name_cpy(ops->name, uops->name,
+				     sizeof(uops->name)) <= 0)
+			return -EINVAL;
+		return 1;
+	}
+
+	return 0;
+}
+
+BPF_CALL_1(bpf_smc_skc_to_tcp_sock, struct sock *, sk)
+{
+	if (sk && sk_fullsock(sk)) {
+		if (inet_sk(sk)->is_icsk)
+			return (unsigned long)sk;
+		return (unsigned long)((struct smc_sock *)(sk))->clcsock->sk;
+	}
+	return (unsigned long)NULL;
+}
+
+const struct bpf_func_proto bpf_smc_skc_to_tcp_sock_proto = {
+	.func			= bpf_smc_skc_to_tcp_sock,
+	.gpl_only		= false,
+	.ret_type		= RET_PTR_TO_BTF_ID_OR_NULL,
+	.arg1_type		= ARG_PTR_TO_BTF_ID_SOCK_COMMON,
+	.ret_btf_id		= &btf_sock_ids[BTF_SOCK_TYPE_TCP],
+};
+
+static const struct bpf_func_proto *
+smc_negotiator_prog_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
+{
+	const struct btf_member *m;
+	const struct btf_type *t;
+	u32 midx, moff;
+
+	midx = prog->expected_attach_type;
+	t = bpf_smc_sock_negotiator_ops.type;
+	m = &btf_type_member(t)[midx];
+
+	moff = btf_member_bit_offset(t, m) / 8;
+
+	switch (func_id) {
+	case BPF_FUNC_skc_to_tcp_sock:
+		return &bpf_smc_skc_to_tcp_sock_proto;
+	default:
+		return bpf_base_func_proto(func_id);
+	}
+}
+
+static bool smc_negotiator_prog_is_valid_access(int off, int size, enum bpf_access_type type,
+						const struct bpf_prog *prog,
+						struct bpf_insn_access_aux *info)
+{
+	if (off < 0 || off >= sizeof(__u64) * MAX_BPF_FUNC_ARGS)
+		return false;
+	if (type != BPF_READ)
+		return false;
+	if (off % size != 0)
+		return false;
+
+	if (!btf_ctx_access(off, size, type, prog, info))
+		return false;
+
+	/* promote it to smc_sock */
+	if (base_type(info->reg_type) == PTR_TO_BTF_ID &&
+	    info->btf_id == sock_id)
+		info->btf_id = smc_sock_id;
+
+	return true;
+}
+
+static int smc_negotiator_prog_btf_struct_access(struct bpf_verifier_log *log,
+						 const struct btf_type *t, int off,
+						 int size, enum bpf_access_type atype,
+						 u32 *next_btf_id)
+{
+	if (atype == BPF_READ)
+		return btf_struct_access(log, t, off, size, atype, next_btf_id);
+	return -EACCES;
+}
+
+static const struct bpf_verifier_ops bpf_smc_negotiator_verifier_ops = {
+	.get_func_proto  = smc_negotiator_prog_func_proto,
+	.is_valid_access = smc_negotiator_prog_is_valid_access,
+	.btf_struct_access = smc_negotiator_prog_btf_struct_access,
+};
+
+struct bpf_struct_ops bpf_smc_sock_negotiator_ops = {
+	.verifier_ops = &bpf_smc_negotiator_verifier_ops,
+	.init = bpf_smc_negotiator_init,
+	.check_member = bpf_smc_negotiator_check_member,
+	.init_member = bpf_smc_negotiator_init_member,
+	.reg = bpf_smc_negotiator_reg,
+	.unreg = bpf_smc_negotiator_unreg,
+	.name = "smc_sock_negotiator_ops",
+};

kernel/bpf/bpf_struct_ops_types.h
@@ -6,4 +6,8 @@
 #include <net/tcp.h>
 BPF_STRUCT_OPS_TYPE(tcp_congestion_ops)
 #endif
+#if IS_ENABLED(CONFIG_SMC)
+#include <net/smc.h>
+BPF_STRUCT_OPS_TYPE(smc_sock_negotiator_ops)
+#endif
 #endif

commit 1d75050a06a29d166906bc00efb45afe182a85d7

anolis: net/smc: fix bpf compile error
ANBZ: #5791

Fix compiler error when BPF_SYSCALL is not enabled.

Fixes: 8bde38f5cd61("anolis: bpf: add smc negotiator support in BPF struct_ops")
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1894

include/net/smc.h
@@ -376,14 +376,18 @@ int smc_sock_register_negotiator_ops(struct smc_sock_negotiator_ops *ops);
 int smc_sock_update_negotiator_ops(struct smc_sock_negotiator_ops *ops,
 					  struct smc_sock_negotiator_ops *old_ops);
 void smc_sock_unregister_negotiator_ops(struct smc_sock_negotiator_ops *ops);
-int smc_sock_assign_negotiator_ops(struct smc_sock *smc, const char *name);
 
 #ifdef CONFIG_BPF_SYSCALL
 void smc_sock_cleanup_negotiator_ops(struct smc_sock *smc, int in_release);
 void smc_sock_clone_negotiator_ops(struct sock *parent, struct sock *child);
+int smc_sock_assign_negotiator_ops(struct smc_sock *smc, const char *name);
 #else
 static inline void smc_sock_cleanup_negotiator_ops(struct smc_sock *smc, int in_release) {}
 static inline void smc_sock_clone_negotiator_ops(struct sock *parent, struct sock *child) {}
+static inline int smc_sock_assign_negotiator_ops(struct smc_sock *smc, const char *name)
+{
+	return -EOPNOTSUPP;
+}
 #endif
 
 #endif	/* _SMC_H */

net/smc/bpf_smc.c
@@ -77,7 +77,6 @@ int smc_sock_register_negotiator_ops(struct smc_sock_negotiator_ops *ops)
 	spin_unlock(&smc_sock_negotiator_list_lock);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(smc_sock_register_negotiator_ops);
 
 /* unregister ops */
 void smc_sock_unregister_negotiator_ops(struct smc_sock_negotiator_ops *ops)
@@ -91,7 +90,6 @@ void smc_sock_unregister_negotiator_ops(struct smc_sock_negotiator_ops *ops)
 	 */
 	synchronize_rcu();
 }
-EXPORT_SYMBOL_GPL(smc_sock_unregister_negotiator_ops);
 
 int smc_sock_update_negotiator_ops(struct smc_sock_negotiator_ops *ops,
 				   struct smc_sock_negotiator_ops *old_ops)
@@ -126,7 +124,6 @@ int smc_sock_update_negotiator_ops(struct smc_sock_negotiator_ops *ops,
 	synchronize_rcu();
 	return 0;
 }
-EXPORT_SYMBOL_GPL(smc_sock_update_negotiator_ops);
 
 /* assign ops to sock */
 int smc_sock_assign_negotiator_ops(struct smc_sock *smc, const char *name)