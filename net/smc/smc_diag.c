jeevandeepj994/anolis_perf

commit 4ea2493c1da3745b41c8203c77488f59f6884553

anolis: net/smc: support smc diag via INET
ANBZ: #5551

Support for SMC inet socket monitoring via netlink sockets of protocol
NETLINK_SOCK_DIAG.

Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1759

net/smc/smc_diag.c
@@ -21,6 +21,7 @@
 
 #include "smc.h"
 #include "smc_core.h"
+#include "smc_inet.h"
 
 struct smc_diag_dump_ctx {
 	int pos[2];
@@ -34,24 +35,42 @@ static struct smc_diag_dump_ctx *smc_dump_context(struct netlink_callback *cb)
 static void smc_diag_msg_common_fill(struct smc_diag_msg *r, struct sock *sk)
 {
 	struct smc_sock *smc = smc_sk(sk);
+	struct sock *clcsk;
+	bool is_v4, is_v6;
+
+	if (smc_sock_is_inet_sock(sk))
+		clcsk = sk;
+	else if (smc->clcsock)
+		clcsk = smc->clcsock->sk;
+	else
+		return;
 
 	memset(r, 0, sizeof(*r));
 	r->diag_family = sk->sk_family;
 	sock_diag_save_cookie(sk, r->id.idiag_cookie);
-	if (!smc->clcsock)
-		return;
-	r->id.idiag_sport = htons(smc->clcsock->sk->sk_num);
-	r->id.idiag_dport = smc->clcsock->sk->sk_dport;
-	r->id.idiag_if = smc->clcsock->sk->sk_bound_dev_if;
-	if (sk->sk_protocol == SMCPROTO_SMC) {
-		r->id.idiag_src[0] = smc->clcsock->sk->sk_rcv_saddr;
-		r->id.idiag_dst[0] = smc->clcsock->sk->sk_daddr;
+
+	r->id.idiag_sport = htons(clcsk->sk_num);
+	r->id.idiag_dport = clcsk->sk_dport;
+	r->id.idiag_if = clcsk->sk_bound_dev_if;
+
+	is_v4 = smc_sock_is_inet_sock(sk) ? clcsk->sk_family == AF_INET :
+		sk->sk_protocol == SMCPROTO_SMC;
 #if IS_ENABLED(CONFIG_IPV6)
-	} else if (sk->sk_protocol == SMCPROTO_SMC6) {
-		memcpy(&r->id.idiag_src, &smc->clcsock->sk->sk_v6_rcv_saddr,
-		       sizeof(smc->clcsock->sk->sk_v6_rcv_saddr));
-		memcpy(&r->id.idiag_dst, &smc->clcsock->sk->sk_v6_daddr,
-		       sizeof(smc->clcsock->sk->sk_v6_daddr));
+	is_v6 = smc_sock_is_inet_sock(sk) ? clcsk->sk_family == AF_INET6 :
+		sk->sk_protocol == SMCPROTO_SMC6;
+#else
+	is_v6 = false;
+#endif
+
+	if (is_v4) {
+		r->id.idiag_src[0] = clcsk->sk_rcv_saddr;
+		r->id.idiag_dst[0] = clcsk->sk_daddr;
+#if IS_ENABLED(CONFIG_IPV6)
+	} else if (is_v6) {
+		memcpy(&r->id.idiag_src, &clcsk->sk_v6_rcv_saddr,
+		       sizeof(clcsk->sk_v6_rcv_saddr));
+		memcpy(&r->id.idiag_dst, &clcsk->sk_v6_daddr,
+		       sizeof(clcsk->sk_v6_daddr));
 #endif
 	}
 }
@@ -189,6 +208,75 @@ errout:
 	return -EMSGSIZE;
 }
 
+static int smc_diag_dump_inet_proto(struct inet_hashinfo *hashinfo, struct sk_buff *skb,
+				    struct netlink_callback *cb, int p_type)
+{
+	struct smc_diag_dump_ctx *cb_ctx = smc_dump_context(cb);
+	struct net *net = sock_net(skb->sk);
+	int snum = cb_ctx->pos[p_type];
+	struct nlattr *bc = NULL;
+	int rc = 0, num = 0, i;
+	struct sock *sk;
+
+	for (i = 0; i < INET_LHTABLE_SIZE; i++) {
+		struct inet_listen_hashbucket *ilb;
+		struct hlist_nulls_node *node;
+
+		ilb = &hashinfo->listening_hash[i];
+		spin_lock(&ilb->lock);
+		sk_nulls_for_each(sk, node, &ilb->nulls_head) {
+			if (!net_eq(sock_net(sk), net))
+				continue;
+			if (sk->sk_prot != &smc_inet_prot)
+				continue;
+			if (num < snum)
+				goto next_ls;
+			rc = __smc_diag_dump(sk, skb, cb, nlmsg_data(cb->nlh), bc);
+			if (rc < 0) {
+				spin_unlock(&ilb->lock);
+				goto out;
+			}
+next_ls:
+			num++;
+		}
+		spin_unlock(&ilb->lock);
+	}
+
+	for (i = 0; i <= hashinfo->ehash_mask; i++) {
+		struct inet_ehash_bucket *head = &hashinfo->ehash[i];
+		spinlock_t *lock = inet_ehash_lockp(hashinfo, i);
+		struct hlist_nulls_node *node;
+
+		if (hlist_nulls_empty(&head->chain))
+			continue;
+
+		spin_lock_bh(lock);
+		sk_nulls_for_each(sk, node, &head->chain) {
+			if (!net_eq(sock_net(sk), net))
+				continue;
+			if (sk->sk_state == TCP_TIME_WAIT)
+				continue;
+			if (sk->sk_state == TCP_NEW_SYN_RECV)
+				continue;
+			if (sk->sk_prot != &smc_inet_prot)
+				continue;
+			if (num < snum)
+				goto next;
+			rc = __smc_diag_dump(sk, skb, cb, nlmsg_data(cb->nlh), bc);
+			if (rc < 0) {
+				spin_unlock_bh(lock);
+				goto out;
+			}
+next:
+			num++;
+		}
+		spin_unlock_bh(lock);
+	}
+out:
+	cb_ctx->pos[p_type] += num;
+	return rc;
+}
+
 static int smc_diag_dump_proto(struct proto *prot, struct sk_buff *skb,
 			       struct netlink_callback *cb, int p_type)
 {
@@ -220,7 +308,7 @@ next:
 
 out:
 	read_unlock(&prot->h.smc_hash->lock);
-	cb_ctx->pos[p_type] = num;
+	cb_ctx->pos[p_type] += num;
 	return rc;
 }
 
@@ -230,7 +318,13 @@ static int smc_diag_dump(struct sk_buff *skb, struct netlink_callback *cb)
 
 	rc = smc_diag_dump_proto(&smc_proto, skb, cb, SMCPROTO_SMC);
 	if (!rc)
-		smc_diag_dump_proto(&smc_proto6, skb, cb, SMCPROTO_SMC6);
+		rc = smc_diag_dump_proto(&smc_proto6, skb, cb, SMCPROTO_SMC6);
+	if (!rc)
+		rc = smc_diag_dump_inet_proto(smc_inet_prot.h.hashinfo, skb, cb, SMCPROTO_SMC);
+#if IS_ENABLED(CONFIG_IPV6)
+	if (!rc)
+		rc = smc_diag_dump_inet_proto(smc_inet6_prot.h.hashinfo, skb, cb, SMCPROTO_SMC6);
+#endif
 	return skb->len;
 }
 

net/smc/smc_inet.c
@@ -68,6 +68,7 @@ struct proto smc_inet_prot = {
 	.no_autobind		= true,
 	.diag_destroy		= tcp_abort,
 };
+EXPORT_SYMBOL_GPL(smc_inet_prot);
 
 const struct proto_ops smc_inet_stream_ops = {
 	.family		   = PF_INET,
@@ -155,6 +156,7 @@ struct proto smc_inet6_prot = {
 	.no_autobind		= true,
 	.diag_destroy		= tcp_abort,
 };
+EXPORT_SYMBOL_GPL(smc_inet6_prot);
 
 const struct proto_ops smc_inet6_stream_ops = {
 	.family		   = PF_INET6,

commit e4501f7243b0f447ab09b7908b3b0e20cad38f1b

anolis: net/smc: fix dup smc diag info
ANBZ: #5791

When attempting to dump ipv6 related socks, smc_diag_dump_inet_proto
still processing ipv4 socks, which could result in duplicate socks
information.

Fixes: 4ea2493c1da("anolis: net/smc: support smc diag via INET")
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1894

net/smc/smc_diag.c
@@ -25,6 +25,7 @@
 
 struct smc_diag_dump_ctx {
 	int pos[2];
+	int inet_pos[2];
 };
 
 static struct smc_diag_dump_ctx *smc_dump_context(struct netlink_callback *cb)
@@ -213,10 +214,17 @@ static int smc_diag_dump_inet_proto(struct inet_hashinfo *hashinfo, struct sk_bu
 {
 	struct smc_diag_dump_ctx *cb_ctx = smc_dump_context(cb);
 	struct net *net = sock_net(skb->sk);
-	int snum = cb_ctx->pos[p_type];
+	int snum = cb_ctx->inet_pos[p_type];
 	struct nlattr *bc = NULL;
 	int rc = 0, num = 0, i;
+	struct proto *target_proto;
 	struct sock *sk;
+
+#if IS_ENABLED(CONFIG_IPV6)
+	target_proto = (p_type == SMCPROTO_SMC6) ? &smc_inet6_prot : &smc_inet_prot;
+#else
+	target_proto = &smc_inet_prot;
+#endif
 
 	for (i = 0; i < INET_LHTABLE_SIZE; i++) {
 		struct inet_listen_hashbucket *ilb;
@@ -227,7 +235,7 @@ static int smc_diag_dump_inet_proto(struct inet_hashinfo *hashinfo, struct sk_bu
 		sk_nulls_for_each(sk, node, &ilb->nulls_head) {
 			if (!net_eq(sock_net(sk), net))
 				continue;
-			if (sk->sk_prot != &smc_inet_prot)
+			if (sk->sk_prot != target_proto)
 				continue;
 			if (num < snum)
 				goto next_ls;
@@ -258,7 +266,7 @@ next_ls:
 				continue;
 			if (sk->sk_state == TCP_NEW_SYN_RECV)
 				continue;
-			if (sk->sk_prot != &smc_inet_prot)
+			if (sk->sk_prot != target_proto)
 				continue;
 			if (num < snum)
 				goto next;
@@ -273,7 +281,7 @@ next:
 		spin_unlock_bh(lock);
 	}
 out:
-	cb_ctx->pos[p_type] += num;
+	cb_ctx->inet_pos[p_type] = num;
 	return rc;
 }
 
@@ -308,7 +316,7 @@ next:
 
 out:
 	read_unlock(&prot->h.smc_hash->lock);
-	cb_ctx->pos[p_type] += num;
+	cb_ctx->pos[p_type] = num;
 	return rc;
 }
 
@@ -317,13 +325,20 @@ static int smc_diag_dump(struct sk_buff *skb, struct netlink_callback *cb)
 	int rc = 0;
 
 	rc = smc_diag_dump_proto(&smc_proto, skb, cb, SMCPROTO_SMC);
-	if (!rc)
-		rc = smc_diag_dump_proto(&smc_proto6, skb, cb, SMCPROTO_SMC6);
-	if (!rc)
-		rc = smc_diag_dump_inet_proto(smc_inet_prot.h.hashinfo, skb, cb, SMCPROTO_SMC);
+	if (rc)
+		return rc;
 #if IS_ENABLED(CONFIG_IPV6)
-	if (!rc)
-		rc = smc_diag_dump_inet_proto(smc_inet6_prot.h.hashinfo, skb, cb, SMCPROTO_SMC6);
+	rc = smc_diag_dump_proto(&smc_proto6, skb, cb, SMCPROTO_SMC6);
+	if (rc)
+		return rc;
+#endif
+	rc = smc_diag_dump_inet_proto(smc_inet_prot.h.hashinfo, skb, cb, SMCPROTO_SMC);
+	if (rc)
+		return rc;
+#if IS_ENABLED(CONFIG_IPV6)
+	rc = smc_diag_dump_inet_proto(smc_inet6_prot.h.hashinfo, skb, cb, SMCPROTO_SMC6);
+	if (rc)
+		return rc;
 #endif
 	return skb->len;
 }