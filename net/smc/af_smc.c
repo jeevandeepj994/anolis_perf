jeevandeepj994/anolis_perf

commit 6cc12dc9090689e73859d93a0f77f5adf0917390

anolis: net/smc: Adjust reserved-ports range and add warning print
ANBZ: #5791

According to IANA, ports [49152, 65535] are used as dynamic ports. To
avoid conflicts with well-known ports and reduce the chance of conflicts
with ports used by other applications, we adjust SMC-R reserved-ports to
[65500, 65515].

Besides, it will print warning when reserving ports fails in reserve_mode
for debugging convenience.

Signed-off-by: Wen Gu <guwen@linux.alibaba.com>

net/smc/smc_core.c
@@ -817,6 +817,8 @@ int smcr_iw_net_reserve_ports(struct net *net)
 	return 0;
 
 release:
+	pr_warn_ratelimited("warning: smc: netns %pK reserved ports %d FAIL for eRDMA OOB\n",
+			    net, SMC_IWARP_RSVD_PORTS_BASE + i);
 	for (j = 0; j < i; j++) {
 		sock_release(net->smc.rsvd_sock[j]);
 		net->smc.rsvd_sock[j] = NULL;

net/smc/smc_ib.h
@@ -23,7 +23,7 @@
 #define SMC_GID_SIZE			sizeof(union ib_gid)
 
 #define SMC_IB_MAX_SEND_SGE		2
-#define SMC_IWARP_RSVD_PORTS_BASE	33800
+#define SMC_IWARP_RSVD_PORTS_BASE	65500
 
 struct smc_ib_devices {			/* list of smc ib devices definition */
 	struct list_head	list;

commit 79648d614aa2c06969f962daaef7da3ea08daee7

anolis: net/smc: Improve print and remove module params of reserve_mode
ANBZ: #5889

Add the print of ports range reserved for eRDMA OOB. And remove
the module param 'reserve_mode' since 'reserve_mode' is the only
mode used in eRDMA scenario.

Fixes: 6cc12dc90906 ("anolis: net/smc: Adjust reserved-ports range and add warning print")
Fixes: 48d5d861a58c ("anolis: net/smc: Introduce a new mode for eRDMA iWARP")
Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1901

net/smc/af_smc.c
@@ -80,8 +80,6 @@ static int smc_inet_sock_sort_csk_queue(struct sock *parent);
 
 /* default use reserve_mode */
 bool reserve_mode = true;
-module_param(reserve_mode, bool, 0444);
-MODULE_PARM_DESC(reserve_mode, "reserve mode support and keep-first-contact disable");
 
 /* rsvd_ports_base must less than (u16 MAX - 8) */
 u16 rsvd_ports_base = SMC_IWARP_RSVD_PORTS_BASE;

net/smc/smc_core.c
@@ -815,12 +815,13 @@ int smcr_iw_net_reserve_ports(struct net *net)
 			goto release;
 		}
 	}
-	pr_info_ratelimited("smc: netns %pK reserved ports for eRDMA OOB\n", net);
+	pr_info_ratelimited("smc: netns %pK reserved ports [%d ~ %d] for eRDMA OOB\n",
+			    net, ports_base, ports_base + SMC_IWARP_RSVD_PORTS_NUM - 1);
 	return 0;
 
 release:
 	pr_warn_ratelimited("warning: smc: netns %pK reserved ports %d FAIL for eRDMA OOB\n",
-			    net, SMC_IWARP_RSVD_PORTS_BASE + i);
+			    net, ports_base + i);
 	for (j = 0; j < i; j++) {
 		sock_release(net->smc.rsvd_sock[j]);
 		net->smc.rsvd_sock[j] = NULL;
@@ -836,7 +837,9 @@ void smcr_iw_net_release_ports(struct net *net)
 		sock_release(net->smc.rsvd_sock[i]);
 		net->smc.rsvd_sock[i] = NULL;
 	}
-	pr_info_ratelimited("smc: netns %pK released ports used by eRDMA OOB\n", net);
+	pr_info_ratelimited("smc: netns %pK released ports [%d ~ %d] used by eRDMA OOB\n",
+			    net, rsvd_ports_base,
+			    rsvd_ports_base + SMC_IWARP_RSVD_PORTS_NUM - 1);
 }
 
 static void smcr_link_iw_extension(struct iw_ext_conn_param *iw_param, struct sock *clcsk)