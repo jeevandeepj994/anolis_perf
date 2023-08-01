5505ece454f3884b7f475fbc29856d23e72e6a6b
Hao Xu <howeyxu@tencent.com>
2023-08-01 16:06:46 +0800

    fuse: add a new fuse init flag to relax restrictions in no cache mode
    ANBZ: #6331
    
    commit e78662e818f9478e70912cc2970ca632ec9f3635 upstream.
    
    FOPEN_DIRECT_IO is usually set by fuse daemon to indicate need of strong
    coherency, e.g. network filesystems.  Thus shared mmap is disabled since it
    leverages page cache and may write to it, which may cause inconsistence.
    
    But FOPEN_DIRECT_IO can be used not for coherency but to reduce memory
    footprint as well, e.g. reduce guest memory usage with virtiofs.
    Therefore, add a new fuse init flag FUSE_DIRECT_IO_RELAX to relax
    restrictions in that mode, currently, it allows shared mmap.  One thing to
    note is to make sure it doesn't break coherency in your use case.
    
    Signed-off-by: Hao Xu <howeyxu@tencent.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -184,6 +184,9 @@
  *  - extend fuse_init_in with reserved fields, add FUSE_INIT_EXT init flag
  *  - add flags2 to fuse_init_in and fuse_init_out
  *  - add FUSE_HAS_INODE_DAX, FUSE_ATTR_DAX
+ *
+ *  7.39
+ *  - add FUSE_DIRECT_IO_RELAX
  */
 
 #ifndef _LINUX_FUSE_H
@@ -338,6 +341,8 @@ struct fuse_file_lock {
  * FUSE_INIT_EXT: extended fuse_init_in request
  * FUSE_INIT_RESERVED: reserved, do not use
  * FUSE_HAS_INODE_DAX:  use per inode DAX
+ * FUSE_DIRECT_IO_RELAX: relax restrictions in FOPEN_DIRECT_IO mode, for now
+ *                       allow shared mmap
  */
 #define FUSE_ASYNC_READ		(1 << 0)
 #define FUSE_POSIX_LOCKS	(1 << 1)
@@ -372,6 +377,7 @@ struct fuse_file_lock {
 #define FUSE_INIT_RESERVED	(1 << 31)
 /* bits 32..63 get shifted down 32 bits into the flags2 field */
 #define FUSE_HAS_INODE_DAX	(1ULL << 33)
+#define FUSE_DIRECT_IO_RELAX	(1ULL << 36)
 #define FUSE_DELETE_STALE	(1ULL << 58)
 /* The 59th bit is left to FUSE_DIO_SHARED_MMAP */
 #define FUSE_INVAL_CACHE_INFAIL	(1ULL << 60)
