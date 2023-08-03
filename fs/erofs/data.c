jeevandeepj994/anolis_perf

commit 37bc6e053254d5fb801367126b6bcc382f63a47e

anolis: erofs: support passing in struct file * for FUSE
ANBZ: #5553

FUSE / Virtiofs need struct file * to handle its page cache
requests.

Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Tested-by: Liu Bo <bo.liu@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Huang Jianan <jnhuang@linux.alibaba.com>
Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1747

fs/erofs/data.c
@@ -9,6 +9,7 @@
 #include <linux/iomap.h>
 #include <linux/uio.h>
 #include <trace/events/erofs.h>
+#include <linux/sched/mm.h>
 
 void erofs_unmap_metabuf(struct erofs_buf *buf)
 {
@@ -33,8 +34,9 @@ void erofs_put_metabuf(struct erofs_buf *buf)
  * Derive the block size from inode->i_blkbits to make compatible with
  * anonymous inode in fscache mode.
  */
-void *erofs_bread(struct erofs_buf *buf, struct inode *inode,
-		  erofs_blk_t blkaddr, enum erofs_kmap_type type)
+void *__erofs_bread(struct super_block *sb, struct erofs_buf *buf,
+		    struct inode *inode, erofs_blk_t blkaddr,
+		    enum erofs_kmap_type type)
 {
 	erofs_off_t offset = (erofs_off_t)blkaddr << inode->i_blkbits;
 	struct address_space *const mapping = inode->i_mapping;
@@ -43,8 +45,18 @@ void *erofs_bread(struct erofs_buf *buf, struct inode *inode,
 
 	if (!page || page->index != index) {
 		erofs_put_metabuf(buf);
-		page = read_cache_page_gfp(mapping, index,
+		if (EROFS_SB(sb)->bootstrap) {
+			unsigned int nofs_flag;
+
+			nofs_flag = memalloc_nofs_save();
+			page = read_cache_page(mapping, index,
+					       (filler_t *)mapping->a_ops->readpage,
+					       EROFS_SB(sb)->bootstrap);
+			memalloc_nofs_restore(nofs_flag);
+		} else {
+			page = read_cache_page_gfp(mapping, index,
 				   mapping_gfp_constraint(mapping, ~__GFP_FS));
+		}
 		if (IS_ERR(page))
 			return page;
 		/* should already be PageUptodate, no need to lock page */
@@ -65,12 +77,18 @@ void *erofs_bread(struct erofs_buf *buf, struct inode *inode,
 	return buf->base + (offset & ~PAGE_MASK);
 }
 
+void *erofs_bread(struct erofs_buf *buf, struct inode *inode,
+		  erofs_blk_t blkaddr, enum erofs_kmap_type type)
+{
+	return __erofs_bread(NULL, buf, inode, blkaddr, type);
+}
+
 void *erofs_read_metabuf(struct erofs_buf *buf, struct super_block *sb,
 			 erofs_blk_t blkaddr, enum erofs_kmap_type type)
 {
 	if (EROFS_SB(sb)->bootstrap)
-		return erofs_bread(buf, EROFS_SB(sb)->bootstrap->f_inode,
-				   blkaddr, type);
+		return __erofs_bread(sb, buf, EROFS_SB(sb)->bootstrap->f_inode,
+				     blkaddr, type);
 
 	if (erofs_is_fscache_mode(sb))
 		return erofs_bread(buf, EROFS_SB(sb)->s_fscache->inode,

commit 96abb3c39a76f610b473b11efdb22d1db02203dd

anolis: erofs: fix EROFS completely broken due to RAFS v6 support
ANBZ: #6079

Any erofs will crash the kernel when using block interface to mount:

    [exception RIP: __erofs_bread+89]
    RIP: ffffffffc0735dd9  RSP: ffffba1240f9bd60  RFLAGS: 00010246
    RAX: 0000000000000000  RBX: 0000000000000000  RCX: 000000000000000c
    RDX: ffff9a7f40480ed8  RSI: 0000000000000000  RDI: 0000000000000000
    RBP: ffffba1240f9bda8   R8: 0000000000000001   R9: ffffba1240f9bda8
    R10: ffff9a7f40480ed8  R11: ffffba1240f9bde0  R12: ffff9a7f40481050
    R13: 0000000000000001  R14: ffff9a7f40480ed8  R15: 0000000000000000
    ORIG_RAX: ffffffffffffffff  CS: 0010  SS: 0018
 #5 [ffffba1240f9bd98] erofs_read_superblock at ffffffffc073424e [erofs]
 #6 [ffffba1240f9be38] erofs_fc_fill_super at ffffffffc073451a [erofs]
 #7 [ffffba1240f9be60] get_tree_bdev at ffffffff8c39086f
 #8 [ffffba1240f9bea0] vfs_get_tree at ffffffff8c38f245
 #9 [ffffba1240f9bec0] do_new_mount at ffffffff8c3b69a6

Fixes: 37bc6e053254 ("anolis: erofs: support passing in struct file * for FUSE")
Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

fs/erofs/data.c
@@ -56,7 +56,7 @@ void *__erofs_bread(struct super_block *sb, struct erofs_buf *buf,
 
 	if (!page || page->index != index) {
 		erofs_put_metabuf(buf);
-		if (EROFS_SB(sb)->bootstrap) {
+		if (sb && EROFS_SB(sb)->bootstrap) {
 			unsigned int nofs_flag;
 
 			nofs_flag = memalloc_nofs_save();