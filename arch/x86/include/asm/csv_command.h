89c17c0cee5a19162d8a3d0d804444ffd8f74445
Xin Jiang <jiangxin@hygon.cn>
2023-08-10 16:20:19 +0800

    anolis: x86: Add support for changing the memory attribute for CSV
    ANBZ: #773
    
    Add support for changing the memory to private or shared memory for
    multiple pages if CSV is active.
    
    When CSV guest wants to share data with host like SWIOTLB or change
    the unused shared memory to private memory, it must perform an secure
    call command to the secure processor to update mapping in nested page
    table.
    
    Signed-off-by: Xin Jiang <jiangxin@hygon.cn>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2277

--- arch/x86/include/asm/csv_command.h
+++ arch/x86/include/asm/csv_command.h
@@ -19,6 +19,8 @@ void __init csv_early_update_memory_dec(u64 vaddr, u64 pages);
 
 void __init csv_early_memory_enc_dec(u64 vaddr, u64 size, bool enc);
 
+void csv_memory_enc_dec(u64 vaddr, u64 pages, bool enc);
+
 #else	/* !CONFIG_HYGON_CSV */
 
 static inline bool csv_active(void) { return false; }
@@ -30,6 +32,8 @@ static inline void __init csv_early_update_memory_dec(u64 vaddr, u64 pages) { }
 
 static inline void __init csv_early_memory_enc_dec(u64 vaddr, u64 size,
 						   bool enc) { }
+
+static inline void csv_memory_enc_dec(u64 vaddr, u64 pages, bool enc) { }
 
 #endif	/* CONFIG_HYGON_CSV */
 
