6a661cffbeb06c0f90e8caf49cfa927ff243cc13
Xin Jiang <jiangxin@hygon.cn>
2023-08-10 19:41:34 +0800

    anolis: x86/boot/compressed/64: Init CSV secure call pages
    ANBZ: #773
    
    CSV secure call is a method to communicate with the dedicated
    secure processor that host cannot tamper with. We declare two
    dedicated pages named secure call pages to hold the command which
    guest wants to send to the secure processor. The secure processor
    always sets only one page of the two as present in nested page
    table.
    
    Read/write action on the two pages will triger NPF then host must
    issue an external command to the secure processor. The secure
    processor gets the guest's command if the fault address is secure
    call page.
    
    CSV secure call command is used to set specified memory as shared
    or private in usual.
    
    Signed-off-by: Xin Jiang <jiangxin@hygon.cn>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2277

--- arch/x86/boot/compressed/csv_command.c
+++ arch/x86/boot/compressed/csv_command.c
@@ -9,9 +9,38 @@
 
 #include "misc.h"
 
+#undef __init
+#undef __initdata
+#undef __pa
+#define __init
+#define __initdata
+#define __pa(x)	((unsigned long)(x))
+
 #include "../../kernel/csv_command_common.c"
 
+static unsigned int csv_secure_call_init;
 static unsigned int csv_enabled __section(".data");
+
+/* Invoke it before jump to real kernel in case secure call pages are not mapped
+ * in the identity page table.
+ *
+ * If no #VC happens, there is no identity mapping in page table for secure call
+ * pages. And page fault is not supported in the early stage when real kernel is
+ * running. As a result, CSV guest will shutdown when access secure call pages
+ * by then.
+ */
+void csv_init_secure_call_pages(void *boot_params)
+{
+	if (csv_enabled &&!csv_secure_call_init) {
+		/*
+		 * boot_params may be not sanitized, but it's OK to access
+		 * e820_table field.
+		 */
+		csv_scan_secure_call_pages(boot_params);
+		csv_early_secure_call(0, 0, CSV_SECURE_CMD_RESET);
+		csv_secure_call_init = 1;
+	}
+}
 
 void set_csv_status(void)
 {
@@ -40,6 +69,7 @@ void set_csv_status(void)
 
 #else /* !CONFIG_HYGON_CSV */
 
+void csv_init_secure_call_pages(void *boot_params) { }
 void set_csv_status(void) { }
 
 #endif /* CONFIG_HYGON_CSV */
