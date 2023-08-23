jeevandeepj994/anolis_perf

commit c17dfe45b03530609bb59ed9af171a6be0d91fd7

anolis: spec: add function to build rpm package
ANBZ: #6189

This commit adds a script to build kernel rpm package.

The script comes from:
- repo: https://gitee.com/src-anolis-sig/ck-build.git
- branch: an8-5.10

It will generates kernel rpms in $(DIST_OUTPUT).

Run it with `make dist-rpms`

Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>
Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

anolis/Makefile
@@ -11,6 +11,9 @@ dist-genspec:
 dist-genrpmtree:
 	sh genrpmtree.sh
 
+dist-rpms: dist-genrpmtree
+	sh buildpkg.sh
+
 clean:
 	rm -rf $(DIST_OUTPUT)
 

anolis/buildpkg.sh
@@ -1,0 +1,84 @@
+set -xe
+
+function do_rpmbuild() {
+	if [ -z "$DIST_OFFICIAL_BUILD" ]; then
+		CMD="-bb"
+	else
+		CMD="-ba"
+	fi
+
+	# Now we have:
+	#  + variants: default, only-debug, with-debug
+	#  + extras: base, with-debuginfo, full
+	#  + modes: nightly, dev, pre-release
+	#TODO: add with-gcov
+	#
+	# Matrix
+	#
+	# | Extra\Var | Default  | Only-debug | With-debug |
+	# |-----------|----------|------------|------------|
+	# | Base      | +default | -default   | +default   |
+	# |           | -debug   | +debug     | +debug     |
+	# |           |            +headers                |
+	# |-----------|------------------------------------|
+	# | debuginfo |            +debuginfo              |
+	# |-----------|------------------------------------|
+	# | full      |         +tools +doc +perf          |
+	#
+	# Note: pre-release mode will always be "full" and "with-debug" by default
+
+	build_opts="--with headers --without bpftool --without signmodules"
+
+	if [ "_${DIST_BUILD_VARIANT}" == "_only-debug" ]; then
+		build_opts="$build_opts --without default --with debug"
+	elif [ "_${DIST_BUILD_VARIANT}" == "_with-debug" ]; then
+		build_opts="$build_opts --with default --with debug"
+	else # assume default
+		build_opts="$build_opts --with default --without debug"
+	fi
+
+	if [ "_${DIST_BUILD_EXTRA}" == "_debuginfo" ]; then
+		build_opts="$build_opts --with debuginfo --without tools --without doc --without perf"
+	elif [ "_${DIST_BUILD_EXTRA}" == "_base" ]; then
+		build_opts="$build_opts --without debuginfo --without tools --without doc --without perf"
+	else # assume full
+		build_opts="$build_opts --with debuginfo --with tools --with doc --with perf"
+	fi
+
+    # launch a new shell to clear current environment variables passed by Makefile
+	 rpmbuild \
+		--define "%_smp_mflags -j$(nproc)" \
+		--define "%packager <alicloud-linux@alibaba-inc.com>" \
+		--define "%_topdir ${DIST_RPMBUILDDIR_OUTPUT}" \
+		${build_opts} \
+		${CMD} ${DIST_RPMBUILDDIR_OUTPUT}/SPECS/kernel.spec \
+		--target=$(uname -m) || exit 1
+}
+
+function output() {
+	if [ -z "$DIST_OFFICIAL_BUILD" ]; then
+		targetdir=${DIST_BUILD_NUMBER}
+	else
+		targetdir=${DIST_ANOLIS_VERSION}
+	fi
+
+	mkdir -p ${DIST_OUTPUT}/${targetdir}
+
+	cp ${DIST_RPMBUILDDIR_OUTPUT}/RPMS/$(uname -m)/*.rpm ${DIST_OUTPUT}/${targetdir}/
+
+	# copy srpm packages if and only if they exist.
+	if [ -f ${DIST_RPMBUILDDIR_OUTPUT}/SRPMS/*.rpm ]; then
+		cp ${DIST_RPMBUILDDIR_OUTPUT}/SRPMS/*.rpm ${DIST_OUTPUT}/${targetdir}
+	fi
+
+	i=0
+	for p in ${DIST_OUTPUT}/${targetdir}/*.rpm; do
+		echo $p
+		((i++))
+	done
+
+	echo "$i rpm(s) copied."
+}
+
+do_rpmbuild
+output

commit f63f2e947e6842216ef759dc3e8367ec902d4e52

anolis: spec: extends DIST_OFFICIAL_BUILD to DIST_BUILD_MODE
ANBZ: #6189

Nightly build requires generate source rpm, but there are no way to
control generating source rpm without setting DIST_OFFICIAL_BUILD.

The old way to let srpm appear is using `sed -i 's/-bb/-ba/g'` to modify
build.sh script in the
ck-build(https://gitee.com/src-anolis-sig/ck-build) repo, but it is not elegant.

This commit extends DIST_OFFICIAL_BUILD to DIST_BUILD_MODE, to
explicitly let nightly build generate source rpm.

Fixes: c17dfe45b035 ("anolis: spec: add function to build rpm package")
Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>
Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

anolis/Makefile
@@ -36,8 +36,11 @@ help:
 	@echo '    DIST                 - the distribution suffix, eg: .an7, .an8, .an23'
 	@echo '    DIST_DO_SIGNMODULES  - should sign kernel modules. optional: 0/1, default: 0'
 	@echo '    DIST_OUTPUT          - the output directory, default: $(DIST_SHORT_OUTPUT)'
-	@echo '    DIST_OFFICIAL_BUILD  - if offical build, the kernel version looks like: 5.10.134-15.1_rc1,'
-	@echo '                           else it looks like: 5.10.134-1.git.6235a991a61d'
+	@echo '    DIST_BUILD_MODE      - the build mode. optional: official/nightly/devel'
+	@echo '                           !!! NOTE: BE CAUTIOUS ABOUT USING official BUILD !!!'
+	@echo '                             - official build. kernel version: $(DIST_KERNELVERSION)-$(DIST_OFFICIAL_PKGRELEASEVERION), with srpm'
+	@echo '                             - nightly build. kernel version: $(DIST_KERNELVERSION)-$(DIST_UNOFFICIAL_PKGRELEASEVERION), with srpm'
+	@echo '                             - devel build. kernel version: $(DIST_KERNELVERSION)-$(DIST_UNOFFICIAL_PKGRELEASEVERION), without srpm'
 	@echo '    DIST_BUILD_NUMBER    - the build number for unofficial build, eg: 1/2'
 	@echo '    DIST_BUILD_VARIANT & DIST_BUILD_EXTRA  - see comments in buildpkg.sh'
 

anolis/Makefile.variables
@@ -4,9 +4,11 @@
 # the dist suffix, eg: an7, an8, an23
 DIST ?= .an8
 
-# if offical build, the kernel version looks like: 5.10.134-15.1_rc1
-# else it looks like: 5.10.134-1.git.6235a991a61d
-DIST_OFFICIAL_BUILD ?= 
+# build mode:
+# - official build, the kernel version looks like: 5.10.134-15.1_rc1, and also generate source rpm
+# - nightly build, the kernel version looks like: 5.10.134-1.git.6235a991a61d, and also generate source rpm
+# - devel build, same as nightly build, without source rpm
+DIST_BUILD_MODE ?= devel
 
 # the package release version.
 # eg: for ANCK 5.10-015.1, the major version is 15, the minor version is 1
@@ -62,7 +64,11 @@ DIST_GIT_HEAD_FULL_COMMIT_ID = $(shell git rev-parse HEAD)
 DIST_UNOFFICIAL_PKGRELEASEVERION = ${DIST_BUILD_NUMBER}.git.$(DIST_GIT_HEAD_SHORT_COMMIT_ID)
 
 # final kernel version
-DIST_PKGRELEASEVERION = $(if $(DIST_OFFICIAL_BUILD),$(DIST_OFFICIAL_PKGRELEASEVERION),$(DIST_UNOFFICIAL_PKGRELEASEVERION))
+ifeq ("${DIST_BUILD_MODE}", "official")
+DIST_PKGRELEASEVERION = $(DIST_OFFICIAL_PKGRELEASEVERION)
+else
+DIST_PKGRELEASEVERION = $(DIST_UNOFFICIAL_PKGRELEASEVERION)
+endif
 DIST_ANOLIS_VERSION = $(DIST_KERNELVERSION)-$(DIST_PKGRELEASEVERION)
 
 # the package id used for compress kernel tarball:

anolis/buildpkg.sh
@@ -1,19 +1,25 @@
 set -xe
 
 function do_rpmbuild() {
-	if [ -z "$DIST_OFFICIAL_BUILD" ]; then
-		CMD="-bb"
-	else
+	if [ "$DIST_BUILD_MODE" == "official" ] || [ "$DIST_BUILD_MODE" == "nightly" ]; then
 		CMD="-ba"
+	else
+		CMD="-bb"
 	fi
 
 	# Now we have:
 	#  + variants: default, only-debug, with-debug
 	#  + extras: base, with-debuginfo, full
-	#  + modes: nightly, dev, pre-release
+	#  + modes: official, nightly, dev
 	#TODO: add with-gcov
 	#
 	# Matrix
+	#
+	# | BuildMode | KernelName      | GenerateSrpm |
+	# |-----------|-----------------|--------------|
+	# | official  | without sha id  | Yes          |
+	# | nightly   | with git sha id | Yes          |
+	# | devel     | with git sha id | No           |
 	#
 	# | Extra\Var | Default  | Only-debug | With-debug |
 	# |-----------|----------|------------|------------|