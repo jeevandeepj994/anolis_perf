jeevandeepj994/anolis_perf

commit ce6b528f9aeefb10637e242ff8b2c1c0b85cf27a

anolis: pci: fix quirk for Yitian 710 to support AER fatal error recovery
ANBZ: #4399

When a fatal error has been detected, the AER driver will reset the PCIe
physical link to recover the unreliable link. It is a hot reset triggered
by setting secondary bus reset (Bit 6) in bridge control register and
resulting in a link down. (NOTE: this type of link down will not be
reported by a suprise link down error)

However, on the Alibaba Yitian 710 SoC, the hardware clears the PCIe config
space and some key registers after a link down, preventing the OS from
recovering the error and resulting in an unexpected SError.
Commit ae314b6cdbc3 ("anolis: pci: Add a quirk for ALIBABA yitian710 to
recover fatal aer") adds a quirk to address the SError problem but fatal
errors are still not recovered.

The root cause is that the cleared registers are not saved/restored
correctly. To fix this issue, pcie_capability_read_word() and similar
interfaces are used to proform the save/restore procedures.

In addition, Root Error Command Register (Offset 2Ch in Advanced Error
Reporting Extended Capability) is also cleared so that save/restore
procedures are applied to it.

Fixes:ae314b6cdbc3 ("anolis: pci: Add a quirk for ALIBABA yitian710 to recover fatal aer")
Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1508Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1384

drivers/pci/pci.c
@@ -4890,7 +4890,6 @@ static void pci_save_yitian710_regs(struct pci_dev *dev,
 				    struct pci_saved_regs *saved)
 {
 	int i;
-	u16 ctrl, ctrl2;
 
 	/* if not yitian 710, should return here */
 	if (!dev->broken_bus_reset)
@@ -4900,49 +4899,48 @@ static void pci_save_yitian710_regs(struct pci_dev *dev,
 	for (i = 0; i < 16; i++)
 		pci_read_config_dword(dev, i * 4, &dev->saved_config_space[i]);
 
-	pci_read_config_word(dev, PCI_EXP_DEVCTL, &ctrl);
-	pci_read_config_word(dev, PCI_EXP_DEVCTL2, &ctrl2);
+	pcie_capability_read_word(dev, PCI_EXP_DEVCTL, &saved->dev_ctrl);
+	pcie_capability_read_word(dev, PCI_EXP_RTCTL, &saved->root_ctrl);
+	pcie_capability_read_word(dev, PCI_EXP_DEVCTL2, &saved->dev_ctrl2);
 
-	saved->mps = (ctrl & PCI_EXP_DEVCTL_PAYLOAD) >> 5;
-	saved->mrrs = (ctrl & PCI_EXP_DEVCTL_READRQ) >> 12;
-	saved->comp_timeout_val = ctrl2 & PCI_EXP_DEVCTL2_COMP_TIMEOUT;
-	saved->comp_timeout_dis = (ctrl2 & PCI_EXP_DEVCTL2_COMP_TMOUT_DIS) >> 4;
 	if (dev->acs_cap)
 		pci_read_config_dword(dev, dev->acs_cap + PCI_ACS_CAP,
 				      &saved->acs_cap_ctrl);
+	if (dev->aer_cap)
+		pci_read_config_dword(dev, dev->aer_cap + PCI_ERR_ROOT_COMMAND,
+				      &saved->root_err_cmd);
 
-	pci_read_config_dword(dev, PCI_EXP_SLTCTL, &saved->slot_ctrl_status);
+	pcie_capability_read_word(dev, PCI_EXP_SLTCTL, &saved->slot_ctrl);
 }
 
 static void pci_restore_yitian710_regs(struct pci_dev *dev,
 				       struct pci_saved_regs *saved)
 {
-	u16 regval;
-
 	if (!dev->broken_bus_reset)
 		return;
 
 	/* restore pcie type1 config space header */
 	pci_restore_config_space_range(dev, 0, 15, 0, false);
 
-	regval = (saved->mps) << 5;
-	pcie_capability_clear_and_set_word(dev, PCI_EXP_DEVCTL,
-					   PCI_EXP_DEVCTL_PAYLOAD, regval);
-	regval = (saved->mrrs) << 12;
-	pcie_capability_clear_and_set_word(dev, PCI_EXP_DEVCTL,
-					   PCI_EXP_DEVCTL_READRQ, regval);
-	regval = saved->comp_timeout_val;
-	pcie_capability_clear_and_set_word(dev, PCI_EXP_DEVCTL2,
-					   PCI_EXP_DEVCTL2_COMP_TIMEOUT, regval);
-	regval = (saved->comp_timeout_dis) << 4;
-	pcie_capability_clear_and_set_word(dev, PCI_EXP_DEVCTL2,
-					   PCI_EXP_DEVCTL2_COMP_TMOUT_DIS, regval);
+	/*
+	 * restore Device Control, Root Control Register and Device Control 2
+	 * in PCI Express Capability
+	 */
+	pcie_capability_write_word(dev, PCI_EXP_DEVCTL, saved->dev_ctrl);
+	pcie_capability_write_word(dev, PCI_EXP_RTCTL, saved->root_ctrl);
+	pcie_capability_write_word(dev, PCI_EXP_DEVCTL2, saved->dev_ctrl2);
 
+	/* restore ACS Capability Register */
 	if (dev->acs_cap)
 		pci_write_config_dword(dev, dev->acs_cap + PCI_ACS_CAP,
 				       saved->acs_cap_ctrl);
+	/* restore AER Root Error Command Register */
+	if (dev->aer_cap)
+		pci_write_config_dword(dev, dev->aer_cap + PCI_ERR_ROOT_COMMAND,
+				       saved->root_err_cmd);
 
-	pci_write_config_dword(dev, PCI_EXP_SLTCTL, saved->slot_ctrl_status);
+	/* restore Slot Control Register */
+	pcie_capability_write_word(dev, PCI_EXP_SLTCTL, saved->slot_ctrl);
 }
 
 void pci_reset_secondary_bus(struct pci_dev *dev)

include/linux/pci.h
@@ -304,12 +304,12 @@ struct pci_cap_saved_state {
 
 /* The structure describes the regs to be saved for yitian710 SoC. */
 struct pci_saved_regs {
-	u8		mps;
-	u8		mrrs;
-	u8		comp_timeout_val;
-	u8		comp_timeout_dis;
+	u16		dev_ctrl;
+	u16		dev_ctrl2;
 	u32		acs_cap_ctrl;
-	u32		slot_ctrl_status;	/* should be the last register to restore */
+	u32		root_err_cmd;
+	u16		root_ctrl;
+	u16		slot_ctrl;	/* should be the last register to restore */
 };
 
 struct irq_affinity;

commit 2f504cb82eb18694691afc9822af8f7817289560

anolis: add AER config dependency sanity check to fix build error
ANBZ: #6230

The AER cached capability position is defined only when CONFIG_PCIEAER is
selected on. Commit ce6b528f9aee ("anolis: pci: fix quirk for Yitian 710 to
support AER fatal error recovery") add a quirk to save/restore PCIe AER
Capability register without CONFIG_PCIEAER sanity check. As a result, the
kernel build fails when configuration is based on 'x86_64_defconfig'

To fix it, add AER config dependency sanity check.

Fixes: ce6b528f9aee ("anolis: pci: fix quirk for Yitian 710 to support AER fatal error recovery")
Fixes: ae314b6cdbc3 (anolis: pci: Add a quirk for ALIBABA yitian710 to recover fatal aer)
Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>

drivers/pci/pci.c
@@ -4918,9 +4918,12 @@ static void pci_save_yitian710_regs(struct pci_dev *dev,
 	if (dev->acs_cap)
 		pci_read_config_dword(dev, dev->acs_cap + PCI_ACS_CAP,
 				      &saved->acs_cap_ctrl);
+
+#ifdef CONFIG_PCIEAER
 	if (dev->aer_cap)
 		pci_read_config_dword(dev, dev->aer_cap + PCI_ERR_ROOT_COMMAND,
 				      &saved->root_err_cmd);
+#endif
 
 	pcie_capability_read_word(dev, PCI_EXP_SLTCTL, &saved->slot_ctrl);
 }
@@ -4946,10 +4949,13 @@ static void pci_restore_yitian710_regs(struct pci_dev *dev,
 	if (dev->acs_cap)
 		pci_write_config_dword(dev, dev->acs_cap + PCI_ACS_CAP,
 				       saved->acs_cap_ctrl);
+
+#ifdef CONFIG_PCIEAER
 	/* restore AER Root Error Command Register */
 	if (dev->aer_cap)
 		pci_write_config_dword(dev, dev->aer_cap + PCI_ERR_ROOT_COMMAND,
 				       saved->root_err_cmd);
+#endif
 
 	/* restore Slot Control Register */
 	pcie_capability_write_word(dev, PCI_EXP_SLTCTL, saved->slot_ctrl);