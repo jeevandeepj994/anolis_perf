jeevandeepj994/anolis_perf

commit c17dfe45b03530609bb59ed9af171a6be0d91fd7

anolis: spec: add function to build rpm package
ANBZ: #6189

This commit adds a script to build kernel rpm package.

The script comes from:
- repo: https://gitee.com/src-anolis-sig/ck-build.git
- branch: an8-5.10

It will generates kernel rpms in $(DIST_OUTPUT).

Run it with `make dist-rpms`

Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>
Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

anolis/buildpkg.sh
@@ -1,0 +1,84 @@
+set -xe
+
+function do_rpmbuild() {
+	if [ -z "$DIST_OFFICIAL_BUILD" ]; then
+		CMD="-bb"
+	else
+		CMD="-ba"
+	fi
+
+	# Now we have:
+	#  + variants: default, only-debug, with-debug
+	#  + extras: base, with-debuginfo, full
+	#  + modes: nightly, dev, pre-release
+	#TODO: add with-gcov
+	#
+	# Matrix
+	#
+	# | Extra\Var | Default  | Only-debug | With-debug |
+	# |-----------|----------|------------|------------|
+	# | Base      | +default | -default   | +default   |
+	# |           | -debug   | +debug     | +debug     |
+	# |           |            +headers                |
+	# |-----------|------------------------------------|
+	# | debuginfo |            +debuginfo              |
+	# |-----------|------------------------------------|
+	# | full      |         +tools +doc +perf          |
+	#
+	# Note: pre-release mode will always be "full" and "with-debug" by default
+
+	build_opts="--with headers --without bpftool --without signmodules"
+
+	if [ "_${DIST_BUILD_VARIANT}" == "_only-debug" ]; then
+		build_opts="$build_opts --without default --with debug"
+	elif [ "_${DIST_BUILD_VARIANT}" == "_with-debug" ]; then
+		build_opts="$build_opts --with default --with debug"
+	else # assume default
+		build_opts="$build_opts --with default --without debug"
+	fi
+
+	if [ "_${DIST_BUILD_EXTRA}" == "_debuginfo" ]; then
+		build_opts="$build_opts --with debuginfo --without tools --without doc --without perf"
+	elif [ "_${DIST_BUILD_EXTRA}" == "_base" ]; then
+		build_opts="$build_opts --without debuginfo --without tools --without doc --without perf"
+	else # assume full
+		build_opts="$build_opts --with debuginfo --with tools --with doc --with perf"
+	fi
+
+    # launch a new shell to clear current environment variables passed by Makefile
+	 rpmbuild \
+		--define "%_smp_mflags -j$(nproc)" \
+		--define "%packager <alicloud-linux@alibaba-inc.com>" \
+		--define "%_topdir ${DIST_RPMBUILDDIR_OUTPUT}" \
+		${build_opts} \
+		${CMD} ${DIST_RPMBUILDDIR_OUTPUT}/SPECS/kernel.spec \
+		--target=$(uname -m) || exit 1
+}
+
+function output() {
+	if [ -z "$DIST_OFFICIAL_BUILD" ]; then
+		targetdir=${DIST_BUILD_NUMBER}
+	else
+		targetdir=${DIST_ANOLIS_VERSION}
+	fi
+
+	mkdir -p ${DIST_OUTPUT}/${targetdir}
+
+	cp ${DIST_RPMBUILDDIR_OUTPUT}/RPMS/$(uname -m)/*.rpm ${DIST_OUTPUT}/${targetdir}/
+
+	# copy srpm packages if and only if they exist.
+	if [ -f ${DIST_RPMBUILDDIR_OUTPUT}/SRPMS/*.rpm ]; then
+		cp ${DIST_RPMBUILDDIR_OUTPUT}/SRPMS/*.rpm ${DIST_OUTPUT}/${targetdir}
+	fi
+
+	i=0
+	for p in ${DIST_OUTPUT}/${targetdir}/*.rpm; do
+		echo $p
+		((i++))
+	done
+
+	echo "$i rpm(s) copied."
+}
+
+do_rpmbuild
+output

anolis/Makefile
@@ -11,6 +11,9 @@ dist-genspec:
 dist-genrpmtree:
 	sh genrpmtree.sh
 
+dist-rpms: dist-genrpmtree
+	sh buildpkg.sh
+
 clean:
 	rm -rf $(DIST_OUTPUT)
 

commit 64d67eb9fed7e87ff9e95471604a6e7caa977da6

anolis: spec: fix a bash gramma problem
ANBZ: #6189

Building kernel package with `make dist-rpms` always failed due to
the bash error:
> + i=0
> + for p in ${DIST_OUTPUT}/${targetdir}/*.rpm
> + echo /cloud-kernel/anolis/output//1/kernel-5.10.134-1.git.54639b688f01.an23.x86_64.rpm
> /cloud-kernel/anolis/output//1/kernel-5.10.134-1.git.54639b688f01.an23.x86_64.rpm
> + (( i++ ))
> make: *** [Makefile:15: dist-rpms] Error 1

This failure impacts nothing, but it is not expected, so fix it.

Fixes: c17dfe45b035 ("anolis: spec: add function to build rpm package")
Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2096

anolis/buildpkg.sh
@@ -77,13 +77,10 @@ function output() {
 		cp ${DIST_RPMBUILDDIR_OUTPUT}/SRPMS/*.rpm ${DIST_OUTPUT}/${targetdir}
 	fi
 
-	i=0
-	for p in ${DIST_OUTPUT}/${targetdir}/*.rpm; do
-		echo $p
-		((i++))
-	done
+	ls ${DIST_OUTPUT}/${targetdir}/*.rpm
 
-	echo "$i rpm(s) copied."
+	rpm_num=$(ls ${DIST_OUTPUT}/${targetdir}/*.rpm | wc -l)
+	echo "${rpm_num} rpm(s) copied."
 }
 
 do_rpmbuild