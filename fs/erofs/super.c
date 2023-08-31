jeevandeepj994/anolis_perf

commit e1f156dedfe8cc9a3c636726b12e4b924aa21055

anolis: erofs: fix unclean iomap and uninitialized erofs_buf
ANBZ: #5553

erofs_buf should be cleaned with __EROFS_BUF_INITIALIZER in
erofs_read_superblock().

Also, iomap structure should be cleared in erofs_put_metabuf()
as well. Otherweise, it will report:
[    5.401041] WARNING: CPU: 1 PID: 116 at fs/fuse/file.c:1879 fuse_fill_iomap.isra.9+0xe9/0xf0
[    5.401615] Modules linked in:
[    5.401835] CPU: 1 PID: 116 Comm: kata-agent Not tainted 4.19.91+ #41
[    5.402267] RIP: 0010:fuse_fill_iomap.isra.9+0xe9/0xf0

Fixes: 2161b622f65d ("anolis: erofs, virtiofs, dax: fix meta race with virtiofs dax reclaim")
Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Reviewed-by: Liu Bo <bo.liu@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Huang Jianan <jnhuang@linux.alibaba.com>
Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1747

fs/erofs/super.c
@@ -470,6 +470,12 @@ static int erofs_fc_parse_param(struct fs_context *fc,
 	default:
 		return -ENOPARAM;
 	}
+
+	if (ctx->blob_dir_path && !ctx->bootstrap_path) {
+		errorfc(fc, "bootstrap_path required in RAFS mode");
+		return -EINVAL;
+	}
+
 	return 0;
 }
 
@@ -864,11 +870,10 @@ static void erofs_kill_sb(struct super_block *sb)
 	erofs_free_dev_context(sbi->devs);
 	if (sbi->bootstrap)
 		filp_close(sbi->bootstrap, NULL);
-	if (sbi->blob_dir_path) {
+	if (sbi->blob_dir_path)
 		path_put(&sbi->blob_dir);
-		kfree(sbi->blob_dir_path);
-	}
 	kfree(sbi->bootstrap_path);
+	kfree(sbi->blob_dir_path);
 	erofs_fscache_unregister_fs(sb);
 	kfree(sbi->fsid);
 	kfree(sbi->domain_id);

fs/erofs/data.c
@@ -37,6 +37,7 @@ void erofs_put_metabuf(struct erofs_buf *buf)
 		buf->mapping->a_ops->endpfn(buf->mapping, index,
 				&buf->iomap, 0);
 		buf->mapping = NULL;
+		memset(&buf->iomap, 0, sizeof(buf->iomap));
 	}
 }
 

commit b7d8c74881562b4e5d7ac604a6997bb1021e6459

anolis: erofs: move option exclusion check to .get_tree()
ANBZ: #6284

erofs_fc_parse_param() is called every time one mount option is parsed,
and thus if "bootstrap_path" option is specified after "blob_dir_path"
option, the mount will unexpectedly fail.

Fixes this by moving the checking into .get_tree(), i.e. after all mount
options have been parsed.

Fixes: e1f156dedfe8 ("anolis: erofs: fix unclean iomap and uninitialized erofs_buf")
Fixes: 3259f1c2326a ("anolis: erofs: make fscache and RAFS mode mutually exclusive")
Reported-by: Gou Hao <gouhao@uniontech.com>
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2109

fs/erofs/super.c
@@ -574,16 +574,6 @@ static int erofs_fc_parse_param(struct fs_context *fc,
 		return -ENOPARAM;
 	}
 
-	if (ctx->blob_dir_path && !ctx->bootstrap_path) {
-		errorfc(fc, "bootstrap_path required in RAFS mode");
-		return -EINVAL;
-	}
-
-	if (ctx->bootstrap_path && ctx->fsid) {
-		errorfc(fc, "fscache/RAFS modes are mutually exclusive");
-		return -EINVAL;
-	}
-
 	return 0;
 }
 
@@ -854,6 +844,16 @@ static int erofs_fc_anon_get_tree(struct fs_context *fc)
 static int erofs_fc_get_tree(struct fs_context *fc)
 {
 	struct erofs_fs_context *ctx = fc->fs_private;
+
+	if (ctx->blob_dir_path && !ctx->bootstrap_path) {
+		errorfc(fc, "bootstrap_path required in RAFS mode");
+		return -EINVAL;
+	}
+
+	if (ctx->bootstrap_path && ctx->fsid) {
+		errorfc(fc, "fscache/RAFS modes are mutually exclusive");
+		return -EINVAL;
+	}
 
 	if (IS_ENABLED(CONFIG_EROFS_FS_ONDEMAND) && ctx->fsid)
 		return get_tree_nodev(fc, erofs_fc_fill_super);