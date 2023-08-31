jeevandeepj994/anolis_perf

commit 20af86836304d039c0724f09d2b39505cc8fc586

anolis: erofs: support RAFS v6 over virtiofs
ANBZ: #5553

RAFS v6 is an EROFS-compatible on-disk format but has slightly
differnet runtime requirements for now.

Previously EROFS was a pure disk filesystem by using bio interfaces,
but RAFS v6 on runV requires EROFS over virtiofs.

Let's read bootstrap & blobs from underlayfs directly. Laterly,
fscache native support will be used for better cleanup and
upstreaming.

Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Tested-by: Liu Bo <bo.liu@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Huang Jianan <jnhuang@linux.alibaba.com>
Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1747

fs/erofs/super.c
@@ -327,6 +327,7 @@ enum {
 	Opt_device,
 	Opt_fsid,
 	Opt_domain_id,
+	Opt_bootstrap_path,
 	Opt_opt_creds,
 	Opt_err
 };
@@ -339,14 +340,15 @@ static const struct constant_table erofs_param_cache_strategy[] = {
 };
 
 static const struct fs_parameter_spec erofs_fs_parameters[] = {
-	fsparam_flag_no("user_xattr",	Opt_user_xattr),
-	fsparam_flag_no("acl",		Opt_acl),
-	fsparam_enum("cache_strategy",	Opt_cache_strategy,
+	fsparam_flag_no("user_xattr",		Opt_user_xattr),
+	fsparam_flag_no("acl",			Opt_acl),
+	fsparam_enum("cache_strategy",		Opt_cache_strategy,
 		     erofs_param_cache_strategy),
-	fsparam_string("device",	Opt_device),
-	fsparam_string("fsid",		Opt_fsid),
-	fsparam_string("domain_id",	Opt_domain_id),
-	fsparam_string("opt_creds",	Opt_opt_creds),
+	fsparam_string("device",		Opt_device),
+	fsparam_string("fsid",			Opt_fsid),
+	fsparam_string("domain_id",		Opt_domain_id),
+	fsparam_string("bootstrap_path",	Opt_bootstrap_path),
+	fsparam_string("opt_creds",		Opt_opt_creds),
 	{}
 };
 
@@ -429,6 +431,13 @@ static int erofs_fc_parse_param(struct fs_context *fc,
 #else
 		errorfc(fc, "domain_id option not supported");
 #endif
+		break;
+	case Opt_bootstrap_path:
+		kfree(ctx->bootstrap_path);
+		ctx->bootstrap_path = kstrdup(param->string, GFP_KERNEL);
+		if (!ctx->bootstrap_path)
+			return -ENOMEM;
+		infofc(fc, "RAFS bootstrap_path %s", ctx->bootstrap_path);
 		break;
 	case Opt_opt_creds:
 		if (!strcmp(param->string, "on")) {
@@ -551,6 +560,22 @@ static int erofs_fc_fill_pseudo_super(struct super_block *sb, struct fs_context 
 	return simple_fill_super(sb, EROFS_SUPER_MAGIC, &empty_descr);
 }
 
+static int rafs_v6_fill_super(struct super_block *sb)
+{
+	struct erofs_sb_info *sbi = EROFS_SB(sb);
+
+	if (sbi->bootstrap_path) {
+		struct file *f;
+
+		f = filp_open(sbi->bootstrap_path, O_RDONLY, 0644);
+		if (IS_ERR(f))
+			return PTR_ERR(f);
+		sbi->bootstrap = f;
+	}
+	/* TODO: open each blobfiles */
+	return 0;
+}
+
 static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 {
 	struct inode *inode;
@@ -575,6 +600,8 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 	ctx->fsid = NULL;
 	sbi->domain_id = ctx->domain_id;
 	ctx->domain_id = NULL;
+	sbi->bootstrap_path = ctx->bootstrap_path;
+	ctx->bootstrap_path = NULL;
 
 	sbi->blkszbits = PAGE_SHIFT;
 	if (erofs_is_fscache_mode(sb)) {
@@ -604,11 +631,18 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 			errorfc(fc, "unsupported blksize for fscache mode");
 			return -EINVAL;
 		}
-		if (!sb_set_blocksize(sb, 1 << sbi->blkszbits)) {
+		if (sb->s_bdev && !sb_set_blocksize(sb, 1 << sbi->blkszbits)) {
 			errorfc(fc, "failed to set erofs blksize");
 			return -EINVAL;
+		} else {
+			sb->s_blocksize =  1 << sbi->blkszbits;
+			sb->s_blocksize_bits = sbi->blkszbits;
 		}
 	}
+
+	err = rafs_v6_fill_super(sb);
+	if (err)
+		return err;
 
 	sb->s_time_gran = 1;
 	sb->s_xattr = erofs_xattr_handlers;
@@ -666,6 +700,9 @@ static int erofs_fc_get_tree(struct fs_context *fc)
 	struct erofs_fs_context *ctx = fc->fs_private;
 
 	if (IS_ENABLED(CONFIG_EROFS_FS_ONDEMAND) && ctx->fsid)
+		return get_tree_nodev(fc, erofs_fc_fill_super);
+
+	if (ctx->bootstrap_path)
 		return get_tree_nodev(fc, erofs_fc_fill_super);
 
 	return get_tree_bdev(fc, erofs_fc_fill_super);
@@ -784,10 +821,10 @@ static void erofs_kill_sb(struct super_block *sb)
 		return;
 	}
 
-	if (erofs_is_fscache_mode(sb))
-		kill_anon_super(sb);
-	else
+	if (sb->s_bdev)
 		kill_block_super(sb);
+	else
+		kill_anon_super(sb);
 
 	sbi = EROFS_SB(sb);
 	if (!sbi)
@@ -807,6 +844,9 @@ static void erofs_put_super(struct super_block *sb)
 
 	DBG_BUGON(!sbi);
 
+	if (sbi->bootstrap)
+		filp_close(sbi->bootstrap, NULL);
+	kfree(sbi->bootstrap_path);
 	erofs_shrinker_unregister(sb);
 #ifdef CONFIG_EROFS_FS_ZIP
 	iput(sbi->managed_cache);
@@ -889,7 +929,7 @@ static int erofs_statfs(struct dentry *dentry, struct kstatfs *buf)
 	struct erofs_sb_info *sbi = EROFS_SB(sb);
 	u64 id = 0;
 
-	if (!erofs_is_fscache_mode(sb))
+	if (sb->s_bdev)
 		id = huge_encode_dev(sb->s_bdev->bd_dev);
 
 	buf->f_type = sb->s_magic;

fs/erofs/data.c
@@ -68,6 +68,10 @@ void *erofs_bread(struct erofs_buf *buf, struct inode *inode,
 void *erofs_read_metabuf(struct erofs_buf *buf, struct super_block *sb,
 			 erofs_blk_t blkaddr, enum erofs_kmap_type type)
 {
+	if (EROFS_SB(sb)->bootstrap)
+		return erofs_bread(buf, EROFS_SB(sb)->bootstrap->f_inode,
+				   blkaddr, type);
+
 	if (erofs_is_fscache_mode(sb))
 		return erofs_bread(buf, EROFS_SB(sb)->s_fscache->inode,
 				   blkaddr, type);
@@ -75,8 +79,7 @@ void *erofs_read_metabuf(struct erofs_buf *buf, struct super_block *sb,
 	return erofs_bread(buf, sb->s_bdev->bd_inode, blkaddr, type);
 }
 
-static int erofs_map_blocks_flatmode(struct inode *inode,
-				     struct erofs_map_blocks *map)
+int erofs_map_blocks_flatmode(struct inode *inode, struct erofs_map_blocks *map)
 {
 	erofs_blk_t nblocks, lastblk;
 	u64 offset = map->m_la;
@@ -199,6 +202,7 @@ int erofs_map_dev(struct super_block *sb, struct erofs_map_dev *map)
 	int id;
 
 	map->m_bdev = sb->s_bdev;
+	map->m_fp = EROFS_SB(sb)->bootstrap;
 	map->m_fscache = EROFS_SB(sb)->s_fscache;
 
 	if (map->m_deviceid) {

fs/erofs/inode.c
@@ -5,8 +5,11 @@
  * Copyright (C) 2021, Alibaba Cloud
  */
 #include "xattr.h"
-
+#include <linux/uio.h>
 #include <trace/events/erofs.h>
+
+const struct file_operations rafs_v6_file_ro_fops;
+const struct address_space_operations rafs_v6_aops;
 
 /*
  * if inode is successfully read, return its inode page (or sometimes
@@ -247,6 +250,8 @@ static int erofs_fill_inode(struct inode *inode)
 {
 	struct erofs_inode *vi = EROFS_I(inode);
 	struct erofs_buf buf = __EROFS_BUF_INITIALIZER;
+	struct super_block *sb = inode->i_sb;
+	struct erofs_sb_info *sbi = EROFS_SB(sb);
 	void *kaddr;
 	unsigned int ofs;
 	int err = 0;
@@ -262,10 +267,14 @@ static int erofs_fill_inode(struct inode *inode)
 	switch (inode->i_mode & S_IFMT) {
 	case S_IFREG:
 		inode->i_op = &erofs_generic_iops;
-		if (erofs_inode_is_data_compressed(vi->datalayout))
+		if (erofs_inode_is_data_compressed(vi->datalayout)) {
 			inode->i_fop = &generic_ro_fops;
-		else
-			inode->i_fop = &erofs_file_fops;
+		} else {
+			if (sbi->bootstrap)
+				inode->i_fop = &rafs_v6_file_ro_fops;
+			else
+				inode->i_fop = &erofs_file_fops;
+		}
 		break;
 	case S_IFDIR:
 		inode->i_op = &erofs_dir_iops;
@@ -297,11 +306,16 @@ static int erofs_fill_inode(struct inode *inode)
 			err = -EOPNOTSUPP;
 		goto out_unlock;
 	}
-	inode->i_mapping->a_ops = &erofs_raw_access_aops;
+	if (sbi->bootstrap && !S_ISREG(inode->i_mode)) {
+		inode_nohighmem(inode);
+		inode->i_mapping->a_ops = &rafs_v6_aops;
+	} else if (inode->i_sb->s_bdev) {
+		inode->i_mapping->a_ops = &erofs_raw_access_aops;
 #ifdef CONFIG_EROFS_FS_ONDEMAND
-	if (erofs_is_fscache_mode(inode->i_sb))
+	} else if (erofs_is_fscache_mode(inode->i_sb)) {
 		inode->i_mapping->a_ops = &erofs_fscache_access_aops;
 #endif
+	}
 
 out_unlock:
 	erofs_put_metabuf(&buf);
@@ -388,4 +402,195 @@ const struct inode_operations erofs_fast_symlink_iops = {
 	.getattr = erofs_getattr,
 	.listxattr = erofs_listxattr,
 	.get_acl = erofs_get_acl,
+};
+
+static ssize_t rafs_v6_read_chunk(struct super_block *sb,
+				  struct iov_iter *to, u64 off, u64 size,
+				  unsigned int device_id)
+{
+	struct iov_iter titer;
+	ssize_t read = 0;
+
+	do {
+		struct iovec iovec = iov_iter_iovec(to);
+		ssize_t ret;
+
+		if (iovec.iov_len > size)
+			iovec.iov_len = size;
+
+		pr_debug("%s: off %llu size %llu blob_index %u\n", __func__, off,
+			 size, device_id);
+
+		/* TODO async */
+		iov_iter_init(&titer, READ, &iovec, 1, iovec.iov_len);
+		ret = vfs_iter_read(EROFS_SB(sb)->bootstrap, &titer, &off, 0);
+		if (ret < 0) {
+			pr_err("%s: failed to read blob ret %ld\n", __func__, ret);
+			return ret;
+		} else if (ret < iovec.iov_len) {
+			return read;
+		}
+		iov_iter_advance(to, ret);
+		read += ret;
+	} while (read < size);
+
+	return read;
+}
+
+static ssize_t rafs_v6_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
+{
+	struct inode *inode = file_inode(iocb->ki_filp);
+	struct erofs_map_blocks map = { 0 };
+	ssize_t bytes = 0;
+	u64 total = min_t(u64, iov_iter_count(to),
+			  inode->i_size - iocb->ki_pos);
+
+	while (total) {
+		erofs_off_t pos = iocb->ki_pos;
+		u64 delta, size;
+		ssize_t read;
+
+		if (map.m_la < pos || map.m_la + map.m_llen >= pos) {
+			int err;
+
+			map.m_la = pos;
+			err = erofs_map_blocks(inode, &map);
+			if (err)
+				return err;
+			if (map.m_la >= inode->i_size)
+				break;
+		}
+		delta = pos - map.m_la;
+		size = min_t(u64, map.m_llen - delta, total);
+		read = rafs_v6_read_chunk(inode->i_sb, to, map.m_pa + delta,
+					  size, map.m_deviceid);
+		if (read < size) {
+			erofs_err(inode->i_sb,
+				  "short read %ld pos %llu size %llu @ nid %llu",
+				  read, pos, size, EROFS_I(inode)->nid);
+			return -EIO;
+		}
+		iocb->ki_pos += read;
+		bytes += read;
+		total -= read;
+	}
+	return bytes;
+}
+
+static vm_fault_t rafs_v6_filemap_fault(struct vm_fault *vmf)
+{
+	struct vm_area_struct *vma = vmf->vma;
+	struct inode *inode = file_inode(vma->vm_file);
+	pgoff_t npages, orig_pgoff = vmf->pgoff;
+	erofs_off_t pos;
+	struct erofs_map_blocks map = {0};
+	struct vm_area_struct lower_vma;
+	int err;
+	vm_fault_t ret;
+
+	npages = DIV_ROUND_UP(i_size_read(inode), PAGE_SIZE);
+	if (unlikely(orig_pgoff >= npages))
+		return VM_FAULT_SIGBUS;
+
+	memcpy(&lower_vma, vmf->vma, sizeof(lower_vma));
+
+	/* TODO: check if chunk is available for us to read. */
+	map.m_la = orig_pgoff << PAGE_SHIFT;
+	pos = map.m_la;
+	err = erofs_map_blocks(inode, &map);
+	if (err)
+		return vmf_error(err);
+
+	lower_vma.vm_file = EROFS_I_SB(inode)->bootstrap;
+	vmf->pgoff = (map.m_pa + (pos - map.m_la)) >> PAGE_SHIFT;
+	vmf->vma = &lower_vma; /* override vma temporarily */
+	ret = EROFS_I(inode)->lower_vm_ops->fault(vmf);
+	vmf->vma = vma;
+	vmf->pgoff = orig_pgoff;
+	return ret;
+}
+
+static const struct vm_operations_struct rafs_v6_vm_ops = {
+	.fault	= rafs_v6_filemap_fault,
+};
+
+static int rafs_v6_file_mmap(struct file *file, struct vm_area_struct *vma)
+{
+	struct inode *inode = file_inode(file);
+	struct erofs_inode *vi = EROFS_I(inode);
+	const struct vm_operations_struct *lower_vm_ops;
+	int ret;
+
+	ret = call_mmap(EROFS_I_SB(inode)->bootstrap, vma);
+	if (ret) {
+		pr_err("%s: call_mmap failed ret %d\n", __func__, ret);
+		return ret;
+	}
+
+	/* set fs's vm_ops which is used in fault(). */
+	lower_vm_ops = vma->vm_ops;
+
+	if (vi->lower_vm_ops && vi->lower_vm_ops != lower_vm_ops) {
+		WARN_ON_ONCE(1);
+		return -EOPNOTSUPP;
+	}
+	/* fault() must exist in order to proceed. */
+	if (!lower_vm_ops || !lower_vm_ops->fault) {
+		WARN_ON_ONCE(1);
+		return -EOPNOTSUPP;
+	}
+	vi->lower_vm_ops = lower_vm_ops;
+	vma->vm_flags &= ~VM_HUGEPAGE;	/* dont use huge page */
+	vma->vm_ops = &rafs_v6_vm_ops;
+	return 0;
+}
+
+const struct file_operations rafs_v6_file_ro_fops = {
+	.llseek		= generic_file_llseek,
+	.read_iter	= rafs_v6_file_read_iter,
+	.mmap		= rafs_v6_file_mmap,
+//	.mmap		= generic_file_readonly_mmap,
+	.splice_read	= generic_file_splice_read,
+};
+
+static int rafs_v6_readpage(struct file *file, struct page *page)
+{
+	struct kvec iov = {
+		.iov_base	= page_address(page),
+	};
+	struct inode *inode = page->mapping->host;
+	struct super_block *sb = inode->i_sb;
+	erofs_off_t pos = page->index << PAGE_SHIFT;
+	struct erofs_map_blocks map = { .m_la = pos };
+	struct kiocb kiocb;
+	struct iov_iter iter;
+	int err;
+
+	err = erofs_map_blocks(inode, &map);
+	if (err)
+		goto err_out;
+
+	iov.iov_len = min_t(u64, PAGE_SIZE, map.m_plen - (pos - map.m_la));
+	init_sync_kiocb(&kiocb, EROFS_SB(sb)->bootstrap);
+	kiocb.ki_pos = map.m_pa + (pos - map.m_la);
+//	if (!(kiocb.ki_pos & ~PAGE_MASK) && iov.iov_len == PAGE_SIZE)
+//		kiocb.ki_flags |= IOCB_DIRECT;
+	iov_iter_kvec(&iter, READ, &iov, 1, iov.iov_len);
+	err = kiocb.ki_filp->f_op->read_iter(&kiocb, &iter);
+	if (err < iov.iov_len)
+		goto err_out;
+	if (iov.iov_len < PAGE_SIZE)
+		memset(iov.iov_base + iov.iov_len, 0,
+		       PAGE_SIZE - iov.iov_len);
+	SetPageUptodate(page);
+	unlock_page(page);
+	return 0;
+err_out:
+	SetPageError(page);
+	unlock_page(page);
+	return err;
+}
+
+const struct address_space_operations rafs_v6_aops = {
+	.readpage = rafs_v6_readpage,
 };

fs/erofs/internal.h
@@ -80,6 +80,7 @@ struct erofs_fs_context {
 	struct erofs_dev_context *devs;
 	char *fsid;
 	char *domain_id;
+	char *bootstrap_path;
 };
 
 struct erofs_domain {
@@ -112,6 +113,8 @@ struct erofs_sb_info {
 	/* pseudo inode to manage cached pages */
 	struct inode *managed_cache;
 #endif	/* CONFIG_EROFS_FS_ZIP */
+	struct file *bootstrap;
+	char *bootstrap_path;
 	struct erofs_dev_context *devs;
 	u64 total_blocks;
 	u32 primarydevice_blocks;
@@ -300,6 +303,7 @@ struct erofs_inode {
 
 	unsigned int xattr_shared_count;
 	unsigned int *xattr_shared_xattrs;
+	const struct vm_operations_struct *lower_vm_ops;
 
 	union {
 		erofs_blk_t raw_blkaddr;
@@ -424,6 +428,7 @@ static inline int z_erofs_map_blocks_iter(struct inode *inode,
 struct erofs_map_dev {
 	struct erofs_fscache *m_fscache;
 	struct block_device *m_bdev;
+	struct file *m_fp;
 
 	erofs_off_t m_pa;
 	unsigned int m_deviceid;

commit 29152fa7d70178143bcaec008157e76f7e05984e

anolis: erofs: disable flatdev in rafsv6 mode
ANBZ: #6284

flatdev only applies to the bdev based mode.

Reported-by: Liu Bo <bo.liu@linux.alibaba.com>
Fixes: 20af86836304d ("anolis: erofs: support RAFS v6 over virtiofs")
Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2109

fs/erofs/super.c
@@ -207,7 +207,7 @@ static int erofs_scan_devices(struct super_block *sb,
 	if (!ondisk_extradevs)
 		return 0;
 
-	if (!sbi->devs->extra_devices && !erofs_is_fscache_mode(sb))
+	if (!sbi->devs->extra_devices && sb->s_bdev)
 		sbi->devs->flatdev = true;
 
 	sbi->device_id_mask = roundup_pow_of_two(ondisk_extradevs + 1) - 1;