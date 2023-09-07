jeevandeepj994/anolis_perf

commit 0ea04a076150d75b54ecfbae2e5f4d1f156fdd9b

anolis: x86/cpu: Detect memory encryption features on Hygon CPUs
ANBZ: #5960

Hygon SME is identified by CPUID 0x8000001f, but requires BIOS support
to enable it (set bit 23 of MSR_K8_SYSCFG). Hygon CSV and CSV2 are
identified by CPUID 0x8000001f, but requires BIOS support to enable it
(set bit 23 of MSR_K8_SYSCFG and set bit 0 of MSR_K7_HWCR). Only show
the SME, CSV, CSV2 features as available if reported by CPUID and
enabled by BIOS.

Signed-off-by: hanliyang <hanliyang@hygon.cn>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1970

arch/x86/kernel/cpu/hygon.c
@@ -270,6 +270,76 @@ static void bsp_init_hygon(struct cpuinfo_x86 *c)
 	}
 }
 
+static void early_detect_mem_encrypt(struct cpuinfo_x86 *c)
+{
+	u64 msr;
+	u32 eax;
+	char *cap;
+	const char *csv_flag = "csv";
+	const char *csv2_flag = "csv2";
+
+	eax = cpuid_eax(0x8000001f);
+
+	/* Check whether SME or CSV is supported */
+	if (!(eax & (BIT(0) | BIT(1))))
+		return;
+
+	/* If BIOS has not enabled SME then don't advertise the SME feature. */
+	rdmsrl(MSR_K8_SYSCFG, msr);
+	if (!(msr & MSR_K8_SYSCFG_MEM_ENCRYPT))
+		goto clear_all;
+
+	/*
+	 * Always adjust physical address bits. Even though this will be a
+	 * value above 32-bits this is still done for CONFIG_X86_32 so that
+	 * accurate values are reported.
+	 */
+	c->x86_phys_bits -= (cpuid_ebx(0x8000001f) >> 6) & 0x3f;
+
+	/* Don't advertise SME and CSV features under CONFIG_X86_32. */
+	if (IS_ENABLED(CONFIG_X86_32))
+		goto clear_all;
+
+	/*
+	 * If BIOS has not enabled CSV then don't advertise the CSV and CSV2
+	 * feature.
+	 */
+	rdmsrl(MSR_K7_HWCR, msr);
+	if (!(msr & MSR_K7_HWCR_SMMLOCK))
+		goto clear_csv;
+
+#ifdef CONFIG_X86_FEATURE_NAMES
+	if (eax & BIT(1)) {
+		if ((x86_cap_flags[X86_FEATURE_SEV] != NULL) &&
+		    (!WARN_ON(strlen(csv_flag) >
+			      strlen(x86_cap_flags[X86_FEATURE_SEV])))) {
+			cap = (char *)x86_cap_flags[X86_FEATURE_SEV];
+
+			memset(cap, 0, strlen(x86_cap_flags[X86_FEATURE_SEV]));
+			memcpy(cap, csv_flag, strlen(csv_flag));
+		}
+	}
+	if (eax & BIT(3)) {
+		if ((x86_cap_flags[X86_FEATURE_SEV_ES] != NULL) &&
+		    (!WARN_ON(strlen(csv2_flag) >
+			      strlen(x86_cap_flags[X86_FEATURE_SEV_ES])))) {
+			cap = (char *)x86_cap_flags[X86_FEATURE_SEV_ES];
+
+			memset(cap, 0, strlen(x86_cap_flags[X86_FEATURE_SEV_ES]));
+			memcpy(cap, csv2_flag, strlen(csv2_flag));
+		}
+	}
+#endif
+
+	return;
+
+clear_all:
+	setup_clear_cpu_cap(X86_FEATURE_SME);
+clear_csv:
+	setup_clear_cpu_cap(X86_FEATURE_SEV);
+	setup_clear_cpu_cap(X86_FEATURE_SEV_ES);
+}
+
 static void early_init_hygon(struct cpuinfo_x86 *c)
 {
 	u32 dummy;
@@ -314,6 +384,8 @@ static void early_init_hygon(struct cpuinfo_x86 *c)
 	set_cpu_cap(c, X86_FEATURE_VMMCALL);
 
 	hygon_get_topology_early(c);
+
+	early_detect_mem_encrypt(c);
 }
 
 static void init_hygon(struct cpuinfo_x86 *c)

commit 06e020f30d4e1f8bb295c145d9eff3274853fe4e

anolis: x86/hygon: x86_cap_flags should now be written as it is RO
ANBZ: #6376

When executing CPU hotplug operation, the HYGON CPU will write the
string in x86_cap_flags which are RO. This will lead to permissions
violation:

 [ 2569.758696] BUG: unable to handle page fault for address: ffffffff8b37a210
 [ 2569.766383] #PF: supervisor write access in kernel mode
 [ 2569.772217] #PF: error_code(0x0003) - permissions violation
 [ 2569.778440] PGD 1814a10067 P4D 1814a10067 PUD 1814a11063 PMD 80000018142001e1
 [ 2569.786508] Oops: 0003 [#1] SMP
 [ 2569.790016] CPU: 1 PID: 0 Comm: swapper/1 Kdump: loaded Tainted: G           OE
 [ 2569.802734] Hardware name: Sugon H620-G30A/65N32, BIOS 1.3.HZ.SG.X.004.00 09/10/2020
 [ 2569.811388] RIP: 0010:__memset+0x29/0x30
 [ 2569.815768] Code: cc 0f 1f 44 00 00 49 89 f9 48 89 d1 83 e2 07 48 c1 e9 03 40 0f b6 f6 48 b8 01 01 01 01 01 01 01 01 48 0f af c6 f3 48 ab 89 d1 <f3> aa 4c 89 c8 c3 90 49 89 f9 40 88 f0 48 89 d1 f3 aa 4c   89 c8 c3
 [ 2569.836727] RSP: 0000:ffffb3548022feb8 EFLAGS: 00010056
 [ 2569.842561] RAX: 0000000000000000 RBX: ffffffff8b37a210 RCX: 0000000000000003
 [ 2569.850527] RDX: 0000000000000003 RSI: 0000000000000000 RDI: ffffffff8b37a210
 [ 2569.858482] RBP: 000000000000000f R08: 0000000000000000 R09: ffffffff8b37a210
 [ 2569.866448] R10: 000000000000000f R11: ffffb3548022fe51 R12: ffff8a22bea99460
 [ 2569.874412] R13: 0000000000000000 R14: 0000000000000000 R15: 0000000000000000
 [ 2569.882379] FS:  0000000000000000(0000) GS:ffff8a22bea80000(0000) knlGS:0000000000000000
 [ 2569.891412] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
 [ 2569.897826] CR2: ffffffff8b37a210 CR3: 0000001814a0f000 CR4: 00000000003506a0
 [ 2569.905793] Call Trace:
 [ 2569.908531]  early_detect_mem_encrypt+0xa8/0x160
 [ 2569.913688]  init_hygon+0xf/0x100
 [ 2569.917389]  identify_cpu+0x10d/0x4a0
 [ 2569.921478]  identify_secondary_cpu+0x12/0x30
 [ 2569.926338]  smp_store_cpu_info+0x45/0x50
 [ 2569.930815]  smp_callin+0x32/0xd0
 [ 2569.934516]  start_secondary+0x1a/0x80
 [ 2569.938703]  secondary_startup_64_no_verify+0xcf/0xdb

Now the x86_cap_flags are only used to show cpuinfo, so we add the check
in show_cpuinfo.

Fixes: 0ea04a076150 ("anolis: x86/cpu: Detect memory encryption features on Hygon CPUs")

arch/x86/kernel/cpu/hygon.c
@@ -286,9 +286,6 @@ static void early_detect_mem_encrypt(struct cpuinfo_x86 *c)
 {
 	u64 msr;
 	u32 eax;
-	char *cap;
-	const char *csv_flag = "csv";
-	const char *csv2_flag = "csv2";
 
 	eax = cpuid_eax(0x8000001f);
 
@@ -319,29 +316,6 @@ static void early_detect_mem_encrypt(struct cpuinfo_x86 *c)
 	rdmsrl(MSR_K7_HWCR, msr);
 	if (!(msr & MSR_K7_HWCR_SMMLOCK))
 		goto clear_csv;
-
-#ifdef CONFIG_X86_FEATURE_NAMES
-	if (eax & BIT(1)) {
-		if ((x86_cap_flags[X86_FEATURE_SEV] != NULL) &&
-		    (!WARN_ON(strlen(csv_flag) >
-			      strlen(x86_cap_flags[X86_FEATURE_SEV])))) {
-			cap = (char *)x86_cap_flags[X86_FEATURE_SEV];
-
-			memset(cap, 0, strlen(x86_cap_flags[X86_FEATURE_SEV]));
-			memcpy(cap, csv_flag, strlen(csv_flag));
-		}
-	}
-	if (eax & BIT(3)) {
-		if ((x86_cap_flags[X86_FEATURE_SEV_ES] != NULL) &&
-		    (!WARN_ON(strlen(csv2_flag) >
-			      strlen(x86_cap_flags[X86_FEATURE_SEV_ES])))) {
-			cap = (char *)x86_cap_flags[X86_FEATURE_SEV_ES];
-
-			memset(cap, 0, strlen(x86_cap_flags[X86_FEATURE_SEV_ES]));
-			memcpy(cap, csv2_flag, strlen(csv2_flag));
-		}
-	}
-#endif
 
 	return;
 

arch/x86/kernel/cpu/proc.c
@@ -127,8 +127,14 @@ static int show_cpuinfo(struct seq_file *m, void *v)
 
 	seq_puts(m, "flags\t\t:");
 	for (i = 0; i < 32*NCAPINTS; i++)
-		if (cpu_has(c, i) && x86_cap_flags[i] != NULL)
-			seq_printf(m, " %s", x86_cap_flags[i]);
+		if (cpu_has(c, i) && x86_cap_flags[i] != NULL) {
+			if (c->x86_vendor == X86_VENDOR_HYGON)
+				seq_printf(m, " %s", i == X86_FEATURE_SEV ? "csv" :
+					   (i == X86_FEATURE_SEV_ES ? "csv2" :
+					    x86_cap_flags[i]));
+			else
+				seq_printf(m, " %s", x86_cap_flags[i]);
+		}
 
 #ifdef CONFIG_X86_VMX_FEATURE_NAMES
 	if (cpu_has(c, X86_FEATURE_VMX) && c->vmx_capability[0]) {