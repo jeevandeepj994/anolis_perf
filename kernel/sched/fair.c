jeevandeepj994/anolis_perf

commit 0f7376d5b84212258518d48858a0ada7974835ea

ck: sched: make up nr_high/under_running for cfs bandwidth
fix #34030593

After we introduce nr_running make up mechanism, we still find out
that nr_high/under_running is incorrect in the throttle scenario.
In order to fix this problem, we improve the nr_running make up
mechanism in this patch. We record the delta of tasks when throttle/
unthrottle, and if se is NULL, we commit the delta to
nr_high/under_running.

Signed-off-by: Michael Wang <yun.wang@linux.alibaba.com>
Signed-off-by:Cruz Zhao <CruzZhao@linux.alibaba.com>
Acked-by: Michael Wang <yun.wang@linux.alibaba.com>

kernel/sched/fair.c
@@ -976,28 +976,27 @@ id_idle_cpu(struct task_struct *p, int cpu, bool expellee, bool *idle)
 }
 
 static __always_inline void
-id_make_up_nr_running(struct task_group *tg, struct rq *rq, long delta)
+id_update_make_up(struct task_group *tg, struct rq *rq, long delta)
 {
 	struct sched_entity *se = tg->se[cpu_of(rq)];
 
-	/*
-	 * The only case we should skip make up is
-	 * when enqueue_entity() triggered throttle.
-	 *
-	 * At that point the new arrived task has
-	 * not yet been accounted into rq, and will
-	 * not until unthrottled, so just skip the
-	 * make up and let unthrottle process do the
-	 * job.
-	 */
-	if (rq->skip_make_up || !delta)
-		return;
-
 	if (is_highclass(se))
-		rq->nr_high_running += delta;
+		rq->nr_high_make_up += delta;
 
 	if (is_underclass(se))
-		rq->nr_under_running += delta;
+		rq->nr_under_make_up += delta;
+}
+
+static __always_inline void
+id_commit_make_up(struct rq *rq, bool commit)
+{
+	if (commit) {
+		rq->nr_high_running += rq->nr_high_make_up;
+		rq->nr_under_running += rq->nr_under_make_up;
+	}
+
+	rq->nr_high_make_up = 0;
+	rq->nr_under_make_up = 0;
 }
 
 static __always_inline void
@@ -1783,8 +1782,13 @@ id_preempt_all(struct sched_entity *curr, struct sched_entity *se)
 	return 0;
 }
 
-static __always_inline
-void id_make_up_nr_running(struct task_group *tg, struct rq *rq, long delta)
+static inline void
+id_update_make_up(struct task_group *tg, struct rq *rq, long delta)
+{
+}
+
+static inline void
+id_commit_make_up(struct rq *rq, bool commit)
 {
 }
 
@@ -6136,9 +6140,7 @@ static int tg_unthrottle_up(struct task_group *tg, void *data)
 		/* Add cfs_rq with already running entity in the list */
 		if (cfs_rq->nr_running >= 1)
 			list_add_leaf_cfs_rq(cfs_rq);
-#ifdef CONFIG_GROUP_IDENTITY
-		id_make_up_nr_running(tg, rq, cfs_rq->nr_tasks);
-#endif
+		id_update_make_up(tg, rq, cfs_rq->nr_tasks);
 	}
 
 	return 0;
@@ -6153,9 +6155,7 @@ static int tg_throttle_down(struct task_group *tg, void *data)
 	if (!cfs_rq->throttle_count) {
 		cfs_rq->throttled_clock_pelt = rq_clock_pelt(rq);
 		list_del_leaf_cfs_rq(cfs_rq);
-#ifdef CONFIG_GROUP_IDENTITY
-		id_make_up_nr_running(tg, rq, -(cfs_rq->nr_tasks));
-#endif
+		id_update_make_up(tg, rq, -(cfs_rq->nr_tasks));
 	}
 	cfs_rq->throttle_count++;
 
@@ -6226,6 +6226,8 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 	}
 
 	sub_nr_running(rq, task_delta);
+
+	id_commit_make_up(rq, !se);
 
 	/*
 	 * Note: distribution will already see us throttled via the
@@ -6308,6 +6310,8 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 
 	/* At this point se is NULL and we are at root level*/
 	add_nr_running(rq, task_delta);
+
+	id_commit_make_up(rq, !se);
 
 unthrottle_throttle:
 	/*
@@ -6567,15 +6571,8 @@ static void check_enqueue_throttle(struct cfs_rq *cfs_rq)
 
 	/* update runtime allocation */
 	account_cfs_rq_runtime(cfs_rq, 0);
-	if (cfs_rq->runtime_remaining <= 0) {
-#ifdef CONFIG_GROUP_IDENTITY
-		rq_of(cfs_rq)->skip_make_up = true;
-#endif
+	if (cfs_rq->runtime_remaining <= 0)
 		throttle_cfs_rq(cfs_rq);
-#ifdef CONFIG_GROUP_IDENTITY
-		rq_of(cfs_rq)->skip_make_up = false;
-#endif
-	}
 }
 
 static void sync_throttle(struct task_group *tg, int cpu)

kernel/sched/sched.h
@@ -1006,7 +1006,8 @@ struct rq {
 	unsigned int		nr_high_running;
 	unsigned int		nr_under_running;
 	unsigned int		nr_expel_immune;
-	bool			skip_make_up;
+	long			nr_high_make_up;
+	long			nr_under_make_up;
 	bool			smt_expeller;
 	bool			smt_expellee;
 	bool			on_expel;

commit 1cc6a64ad640dc38e33121d59be56f56cc4f6047

anolis: sched/fair: place id_commit_make_up() correctly
ANBZ: #6529

id_commit_make_up() will miss updated if an ancestor task group is still
throttled and goto unthrottle_throttle (i.e., se is not NULL).

Fixes: 0f7376d5b842 ("ck: sched: make up nr_high/under_running for cfs bandwidth")
Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

kernel/sched/fair.c
@@ -6781,9 +6781,9 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 	/* At this point se is NULL and we are at root level*/
 	add_nr_running(rq, task_delta);
 
+unthrottle_throttle:
 	id_commit_make_up(rq, !se);
 
-unthrottle_throttle:
 	/*
 	 * The cfs_rq_throttled() breaks in the above iteration can result in
 	 * incomplete leaf list maintenance, resulting in triggering the