e1d4ea07d4207b2b175be1d4c03e99312597c6c2
Binbin Wu <binbin.wu@linux.intel.com>
2023-09-13 20:42:19 +0800

    KVM: x86: Introduce get_untagged_addr() in kvm_x86_ops and call it in emulator
    ANBZ: #8355
    
    commit 37a41847b770c722e98ace72f3851fb49b360c08 upstream.
    
    Introduce a new interface get_untagged_addr() to kvm_x86_ops to untag
    the metadata from linear address.  Call the interface in linearization
    of instruction emulator for 64-bit mode.
    
    When enabled feature like Intel Linear Address Masking (LAM) or AMD Upper
    Address Ignore (UAI), linear addresses may be tagged with metadata that
    needs to be dropped prior to canonicality checks, i.e. the metadata is
    ignored.
    
    Introduce get_untagged_addr() to kvm_x86_ops to hide the vendor specific
    code, as sadly LAM and UAI have different semantics.  Pass the emulator
    flags to allow vendor specific implementation to precisely identify the
    access type (LAM doesn't untag certain accesses).
    
    Intel-SIG: commit 37a41847b770 KVM: x86: Introduce get_untagged_addr()
    in kvm_x86_ops and call it in emulator
    Backport KVM Linear Address Masking (LAM) support.
    
    Signed-off-by: Binbin Wu <binbin.wu@linux.intel.com>
    Reviewed-by: Chao Gao <chao.gao@intel.com>
    Tested-by: Xuelian Guo <xuelian.guo@intel.com>
    Link: https://lore.kernel.org/r/20230913124227.12574-9-binbin.wu@linux.intel.com
    [sean: massage changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [ Zhiquan Li: amend commit log and resolve the conflict ]
    Signed-off-by: Zhiquan Li <zhiquan1.li@intel.com>
    Reviewed-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>
    Acked-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2805

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1751,6 +1751,8 @@ struct kvm_x86_ops {
 	 */
 	unsigned long (*vcpu_get_apicv_inhibit_reasons)(struct kvm_vcpu *vcpu);
 
+	gva_t (*get_untagged_addr)(struct kvm_vcpu *vcpu, gva_t gva, unsigned int flags);
+
 	int (*vm_attestation)(struct kvm *kvm, unsigned long gpa, unsigned long len);
 	int (*control_pre_system_reset)(struct kvm *kvm);
 	int (*control_post_system_reset)(struct kvm *kvm);
