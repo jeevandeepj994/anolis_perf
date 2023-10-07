jeevandeepj994/anolis_perf

commit a0606e53a2f98ab9ed125e7421ca6357a9437513

anolis: net/smc: support rq flow control for rwwi
ANBZ: #5551

This patch supports rq flow control for rwwi io path in smc-r
link layer, which has already implemented for cdc io path.
Rq flow control uses credits to control the behavior of sending,
and is used to avoid the occurrence of RNR, which may result in
performance reduction.

Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1759

net/smc/smc_tx.c
@@ -361,16 +361,18 @@ static int smc_tx_rdma_write(struct smc_connection *conn, int peer_rmbe_offset,
 }
 
 static int __smcr_tx_rdma_writes_rwwi(struct smc_connection *conn, int dst_off,
-				      int dst_len, int num_sges, bool inflight_credit_flag,
-				      struct ib_rdma_wr *wr)
+				      int dst_len, int num_sges, struct ib_rdma_wr *wr)
 {
 	struct smc_cdc_producer_flags *pflags;
-	bool update_rx_curs_confirmed = false;
+	bool update_rx_curs_confirmed = true;
+	struct smc_link *link = conn->lnk;
 	union smc_host_cursor cons_old;
 	union smc_wr_rwwi_tx_id wr_id;
 	union smc_wr_imm_msg imm_msg;
 	union smc_host_cursor cfed;
 	u8 urg_flags, prod_flags;
+	u8 saved_credits = 0;
+	bool cr_flag = false;
 	u8 conn_state_flags;
 	int diff_cons, rc;
 
@@ -383,8 +385,9 @@ static int __smcr_tx_rdma_writes_rwwi(struct smc_connection *conn, int dst_off,
 
 	atomic_inc(&conn->cdc_pend_tx_wr);
 	smp_mb__after_atomic(); /* Make sure cdc_pend_tx_wr added before post */
+	cr_flag = smc_wr_rx_credits_need_announce_frequent(link);
 	imm_msg.hdr.token = conn->local_tx_ctrl.token;
-	imm_msg.hdr.opcode = SMC_WR_OP_DATA;
+	imm_msg.hdr.opcode = cr_flag ? SMC_WR_OP_DATA_CR : SMC_WR_OP_DATA;
 
 	conn_state_flags = *((u8 *)&conn->local_tx_ctrl.conn_state_flags);
 	prod_flags = *((u8 *)pflags);
@@ -398,7 +401,8 @@ static int __smcr_tx_rdma_writes_rwwi(struct smc_connection *conn, int dst_off,
 	 * 3. transfer diff_cons without flags.
 	 */
 	if (urg_flags)
-		imm_msg.hdr.opcode = SMC_WR_OP_DATA_WITH_FLAGS;
+		imm_msg.hdr.opcode = cr_flag ?
+				     SMC_WR_OP_DATA_WITH_FLAGS_CR : SMC_WR_OP_DATA_WITH_FLAGS;
 	/* conn_state_flags or prod_flags (except urg_flags) is set */
 	if (conn_state_flags || prod_flags != urg_flags)
 		imm_msg.hdr.opcode = SMC_WR_OP_CTRL;
@@ -412,11 +416,11 @@ static int __smcr_tx_rdma_writes_rwwi(struct smc_connection *conn, int dst_off,
 		if (diff_cons > SMC_DATA_MAX_DIFF_CONS)
 			diff_cons = SMC_DATA_MAX_DIFF_CONS;
 		imm_msg.data.diff_cons = diff_cons;
-		update_rx_curs_confirmed = true;
 		break;
 	case SMC_WR_OP_CTRL:
 		imm_msg.ctrl.pflags = conn->local_tx_ctrl.prod_flags;
 		imm_msg.ctrl.csflags = conn->local_tx_ctrl.conn_state_flags;
+		update_rx_curs_confirmed = false;
 		break;
 	case SMC_WR_OP_DATA_WITH_FLAGS:
 		if (diff_cons > SMC_DATA_WITH_FLAGS_MAX_DIFF_CONS)
@@ -425,12 +429,27 @@ static int __smcr_tx_rdma_writes_rwwi(struct smc_connection *conn, int dst_off,
 		imm_msg.data_with_flags.urg_data_present = pflags->urg_data_present;
 		imm_msg.data_with_flags.urg_data_pending = pflags->urg_data_pending;
 		imm_msg.data_with_flags.diff_cons = diff_cons;
-		update_rx_curs_confirmed = true;
+		break;
+	case SMC_WR_OP_DATA_CR:
+		if (diff_cons > SMC_DATA_CR_MAX_DIFF_CONS)
+			diff_cons = SMC_DATA_CR_MAX_DIFF_CONS;
+		imm_msg.data_cr.diff_cons = diff_cons;
+		saved_credits = (u8)smc_wr_rx_get_credits(link);
+		imm_msg.data_cr.credits = saved_credits;
+		break;
+	case SMC_WR_OP_DATA_WITH_FLAGS_CR:
+		if (diff_cons > SMC_DATA_WITH_FLAGS_CR_MAX_DIFF_CONS)
+			diff_cons = SMC_DATA_WITH_FLAGS_CR_MAX_DIFF_CONS;
+		imm_msg.data_with_flags_cr.write_blocked = pflags->write_blocked;
+		imm_msg.data_with_flags_cr.urg_data_present = pflags->urg_data_present;
+		imm_msg.data_with_flags_cr.urg_data_pending = pflags->urg_data_pending;
+		imm_msg.data_with_flags_cr.diff_cons = diff_cons;
+		saved_credits = (u8)smc_wr_rx_get_credits(link);
+		imm_msg.data_with_flags_cr.credits = saved_credits;
 		break;
 	}
 
 	wr_id.rwwi_flag = 1;
-	wr_id.inflight_credit_flag = inflight_credit_flag;
 	wr_id.token = conn->alert_token_local;
 	wr_id.inflight_sent = dst_len;
 	wr->wr.wr_id = wr_id.data;
@@ -444,6 +463,7 @@ static int __smcr_tx_rdma_writes_rwwi(struct smc_connection *conn, int dst_off,
 		if (update_rx_curs_confirmed)
 			smc_curs_add(conn->rmb_desc->len, &conn->rx_curs_confirmed, diff_cons);
 	} else {
+		smc_wr_rx_put_credits(link, saved_credits);
 		atomic_dec(&conn->cdc_pend_tx_wr);
 	}
 
@@ -466,20 +486,36 @@ static inline void smc_tx_advance_cursors(struct smc_connection *conn,
 	smc_curs_add(conn->sndbuf_desc->len, sent, len);
 }
 
-static int smc_tx_get_inflight_credit(struct smc_link *link,
-				      struct smc_connection *conn)
+static inline int __smc_get_free_slot_rwwi(struct smc_link *link)
+{
+	if (!smc_link_sendable(link))
+		return -ENOLINK;
+
+	if (atomic_dec_if_positive(&link->tx_inflight_credit) < 0)
+		return -EBUSY;
+
+	if (smc_wr_tx_get_credit(link))
+		return 0;
+
+	atomic_inc(&link->tx_inflight_credit);
+	return -EBUSY;
+}
+
+static int smc_tx_get_free_slot_rwwi(struct smc_link *link,
+				     struct smc_connection *conn)
 {
 	struct smc_link_group *lgr = link->lgr;
 	int rc;
 
 	if (in_softirq() || lgr->terminating) {
-		if (atomic_dec_if_positive(&link->tx_inflight_credit) < 0)
-			return -EBUSY;
+		rc = __smc_get_free_slot_rwwi(link);
+		if (rc)
+			return rc;
 	} else {
 		rc = wait_event_interruptible_timeout(link->wr_tx_wait,
 						      !smc_link_sendable(link) ||
 						      lgr->terminating ||
-				(atomic_dec_if_positive(&link->tx_inflight_credit) >= 0),
+						      (__smc_get_free_slot_rwwi(link) != -EBUSY),
 						      SMC_WR_TX_WAIT_FREE_SLOT_TIME);
 		if (!rc) {
 			/* timeout - terminate link */
@@ -496,8 +532,11 @@ static int smc_tx_get_inflight_credit(struct smc_link *link,
 	return 0;
 }
 
-void smc_tx_put_inflight_credit(struct smc_link *link)
+void smc_tx_put_free_slot_rwwi(struct smc_link *link, bool complete)
 {
+	if (!complete)
+		smc_wr_tx_put_credits(link, 1, true);
+
 	atomic_inc(&link->tx_inflight_credit);
 }
 
@@ -585,42 +624,94 @@ static int smcr_tx_rdma_writes(struct smc_connection *conn, size_t len,
 	return 0;
 }
 
-static int smcr_tx_rdma_writes_rwwi(struct smc_connection *conn, size_t len,
-				    size_t src_off, size_t src_len,
-				    size_t dst_off, size_t dst_len)
+static int smcr_tx_rdma_writes_rwwi(struct smc_connection *conn)
 {
-	int src_len_sum = src_len, dst_len_sum = dst_len;
-	int sent_count = src_off;
+	union smc_host_cursor sent, prep, prod, cons;
+	int to_send, rmbespace, src_off, num_sges;
+	struct smc_cdc_producer_flags *pflags;
+	size_t len, src_len, dst_len; /* current chunk values */
 	struct ib_rdma_wr wr;
 	struct ib_sge sge[2];
-	int *src_offset;
-	int dstchunk;
-	int num_sges;
 	int rc;
 
-	src_offset = (int *)&src_off;
-	for (dstchunk = 0; dstchunk < 2; dstchunk++) {
-		memset(&wr, 0, sizeof(wr));
-		wr.wr.send_flags = IB_SEND_SIGNALED;
-		wr.wr.opcode = IB_WR_RDMA_WRITE_WITH_IMM;
+	conn->unwrap_remaining = 0;
+	/* source: sndbuf */
+	smc_curs_copy(&sent, &conn->tx_curs_sent, conn);
+	smc_curs_copy(&prep, &conn->tx_curs_prep, conn);
+	/* cf. wmem_alloc - (snd_max - snd_una) */
+	to_send = smc_curs_diff(conn->sndbuf_desc->len, &sent, &prep);
+	if (to_send <= 0)
+		return 0;
 
-		num_sges = smc_tx_fill_wr(conn, src_offset, src_len, dst_len, &wr, sge, true);
-		wr.wr.sg_list = sge;
-
-		rc = __smcr_tx_rdma_writes_rwwi(conn, dst_off, dst_len, num_sges,
-						(dst_len_sum == len), &wr);
-		if (rc)
-			return rc;
-		if (dst_len_sum == len)
-			break; /* either on 1st or 2nd iteration */
-		/* prepare next (== 2nd) iteration */
-		dst_off = 0; /* modulo offset in RMBE ring buffer */
-		dst_len = len - dst_len; /* remainder */
-		dst_len_sum += dst_len;
-		src_len = min_t(int, dst_len, conn->sndbuf_desc->len -
-				sent_count);
-		src_len_sum = src_len;
+	/* destination: RMBE */
+	/* cf. snd_wnd */
+	rmbespace = atomic_read(&conn->peer_rmbe_space);
+	if (rmbespace <= 0) {
+		struct smc_sock *smc = container_of(conn, struct smc_sock,
+						    conn);
+		SMC_STAT_RMB_TX_PEER_FULL(smc, !conn->lnk);
+		return 0;
 	}
+	smc_curs_copy(&prod, &conn->local_tx_ctrl.prod, conn);
+	smc_curs_copy(&cons, &conn->local_rx_ctrl.cons, conn);
+
+	/* if usable snd_wnd closes ask peer to advertise once it opens again */
+	pflags = &conn->local_tx_ctrl.prod_flags;
+	pflags->write_blocked = (to_send >= rmbespace);
+	/* cf. usable snd_wnd */
+	len = min(to_send, rmbespace);
+
+	if (prod.wrap == cons.wrap) {
+		/* the filled destination area is unwrapped,
+		 * hence the available free destination space is wrapped
+		 * and we need 2 destination chunks of sum len; start with 1st
+		 * which is limited by what's available in sndbuf
+		 */
+		dst_len = min_t(size_t, conn->peer_rmbe_size - prod.count, len);
+	} else {
+		/* the filled destination area is wrapped,
+		 * hence the available free destination space is unwrapped
+		 * and we need a single destination chunk of entire len
+		 */
+		dst_len = len;
+	}
+	/* dst_len determines the maximum src_len */
+	if (sent.count + dst_len <= conn->sndbuf_desc->len) {
+		/* unwrapped src case: single chunk of entire dst_len */
+		src_len = dst_len;
+	} else {
+		/* wrapped src case: 2 chunks of sum dst_len; start with 1st: */
+		src_len = conn->sndbuf_desc->len - sent.count;
+	}
+
+	/* if the filled destination area is unwrapped, set unwrap_remaining flag and
+	 * the remaining data will send the next time.
+	 */
+	if (dst_len < len)
+		conn->unwrap_remaining = 1;
+
+	/* update urg_data_present in advance since this info needs
+	 * to transfer to remote by write with imm
+	 */
+	if (conn->urg_tx_pend && dst_len == to_send)
+		pflags->urg_data_present = 1;
+
+	src_off = sent.count;
+	memset(&wr, 0, sizeof(wr));
+	wr.wr.send_flags = IB_SEND_SIGNALED;
+	wr.wr.opcode = IB_WR_RDMA_WRITE_WITH_IMM;
+	num_sges = smc_tx_fill_wr(conn, &src_off, src_len, dst_len, &wr, sge, true);
+	wr.wr.sg_list = sge;
+	rc = __smcr_tx_rdma_writes_rwwi(conn, prod.count, dst_len, num_sges, &wr);
+	if (rc)
+		return rc;
+
+	smc_tx_advance_cursors(conn, &prod, &sent, dst_len);
+	/* update connection's cursors with advanced local cursors */
+	smc_curs_copy(&conn->local_tx_ctrl.prod, &prod, conn);
+							/* dst: peer RMBE */
+	smc_curs_copy(&conn->tx_curs_sent, &sent, conn);/* src: local sndbuf */
+
 	return 0;
 }
 
@@ -643,7 +734,7 @@ again:
 	wr.wr.send_flags = IB_SEND_SIGNALED;
 	wr.wr.opcode = IB_WR_RDMA_WRITE_WITH_IMM;
 
-	rc = smc_tx_get_inflight_credit(link, conn);
+	rc = smc_tx_get_free_slot_rwwi(link, conn);
 	if (rc)
 		goto put_out;
 
@@ -651,7 +742,7 @@ again:
 	if (link != conn->lnk) {
 		/* link of connection changed, try again one time*/
 		spin_unlock_bh(&conn->send_lock);
-		smc_tx_put_inflight_credit(link);
+		smc_tx_put_free_slot_rwwi(link, false);
 		smc_wr_tx_link_put(link);
 		if (again)
 			return -ENOLINK;
@@ -659,9 +750,9 @@ again:
 		goto again;
 	}
 
-	rc = __smcr_tx_rdma_writes_rwwi(conn, 0, 0, 0, true, &wr);
+	rc = __smcr_tx_rdma_writes_rwwi(conn, 0, 0, 0, &wr);
 	if (rc)
-		smc_tx_put_inflight_credit(link);
+		smc_tx_put_free_slot_rwwi(link, false);
 	spin_unlock_bh(&conn->send_lock);
 put_out:
 	smc_wr_tx_link_put(link);
@@ -772,22 +863,12 @@ static int smc_tx_rdma_writes(struct smc_connection *conn,
 		src_len = conn->sndbuf_desc->len - sent.count;
 	}
 
-	if (conn->lgr->is_smcd) {
+	if (conn->lgr->is_smcd)
 		rc = smcd_tx_rdma_writes(conn, len, sent.count, src_len,
 					 dst_off, dst_len);
-	} else if (conn->lgr->use_rwwi) {
-		/*update urg_data_present in advance since this info needs
-		 * to transfer to remote by write with imm
-		 */
-		if (conn->urg_tx_pend && len == to_send)
-			pflags->urg_data_present = 1;
-		rc = smcr_tx_rdma_writes_rwwi(conn, len, sent.count, src_len,
-					      dst_off, dst_len);
-	} else {
+	else
 		rc = smcr_tx_rdma_writes(conn, len, sent.count, src_len,
 					 dst_off, dst_len, wr_rdma_buf);
-	}
-
 	if (rc)
 		return rc;
 
@@ -871,8 +952,10 @@ static int smcr_tx_sndbuf_nonempty_rwwi(struct smc_connection *conn)
 
 	if (!link || !smc_wr_tx_link_hold(link))
 		return -ENOLINK;
+
+again:
 	/*count the num of infight io and limit it*/
-	rc = smc_tx_get_inflight_credit(link, conn);
+	rc = smc_tx_get_free_slot_rwwi(link, conn);
 	if (rc < 0) {
 		smc_wr_tx_link_put(link);
 		if (rc == -EBUSY) {
@@ -893,20 +976,25 @@ static int smcr_tx_sndbuf_nonempty_rwwi(struct smc_connection *conn)
 	spin_lock_bh(&conn->send_lock);
 	if (link != conn->lnk) {
 		/* link of connection changed, tx_work will restart */
-		smc_tx_put_inflight_credit(link);
+		smc_tx_put_free_slot_rwwi(link, false);
 		rc = -ENOLINK;
 		goto out_unlock;
 	}
 
-	rc = smc_tx_rdma_writes(conn, NULL);
+	rc = smcr_tx_rdma_writes_rwwi(conn);
 	if (rc) {
-		smc_tx_put_inflight_credit(link);
+		smc_tx_put_free_slot_rwwi(link, false);
 		goto out_unlock;
 	}
 
 	if (pflags->urg_data_present) {
 		pflags->urg_data_pending = 0;
 		pflags->urg_data_present = 0;
+	}
+
+	if (unlikely(conn->unwrap_remaining)) {
+		spin_unlock_bh(&conn->send_lock);
+		goto again;
 	}
 
 out_unlock:

include/net/smc.h
@@ -272,6 +272,9 @@ struct smc_connection {
 	u8			killed : 1;	/* abnormal termination */
 	u8			freed : 1;	/* normal termiation */
 	u8			out_of_sync : 1; /* out of sync with peer */
+	u8			unwrap_remaining : 1; /* have remaining data to
+						       * send when RMB unwrapped
+						       */
 };
 
 struct smc_sock {				/* smc sock container */

net/smc/smc_cdc.c
@@ -595,6 +595,54 @@ static void smc_cdc_handle_rwwi_data_with_flags_msg(struct smc_sock *smc,
 	__smc_cdc_msg_recv_action(smc, diff_prod, diff_cons);
 }
 
+static void smc_cdc_handle_rwwi_data_cr_msg(struct smc_sock *smc,
+					    union smc_wr_imm_msg *imm_msg, int diff_prod)
+{
+	struct smc_connection *conn = &smc->conn;
+	int diff_cons;
+
+	if (imm_msg->data_cr.credits)
+		smc_wr_tx_put_credits(conn->lnk, imm_msg->data_cr.credits, true);
+
+	diff_cons = imm_msg->data_cr.diff_cons;
+	if (diff_cons)
+		smc_curs_add(conn->peer_rmbe_size, &conn->local_rx_ctrl.cons, diff_cons);
+	/* cause this imm_data contains no conn_state_flags and prod_flags info, clean them */
+	memset(&conn->local_rx_ctrl.conn_state_flags, 0,
+	       sizeof(struct smc_cdc_conn_state_flags));
+	memset(&conn->local_rx_ctrl.prod_flags, 0,
+	       sizeof(struct smc_cdc_producer_flags));
+
+	__smc_cdc_msg_recv_action(smc, diff_prod, diff_cons);
+}
+
+static void smc_cdc_handle_rwwi_data_with_flags_cr_msg(struct smc_sock *smc,
+						       union smc_wr_imm_msg *imm_msg, int diff_prod)
+{
+	struct smc_connection *conn = &smc->conn;
+	struct smc_cdc_producer_flags *pflags;
+	int diff_cons;
+
+	if (imm_msg->data_with_flags_cr.credits)
+		smc_wr_tx_put_credits(conn->lnk, imm_msg->data_with_flags_cr.credits, true);
+
+	diff_cons = imm_msg->data_with_flags_cr.diff_cons;
+	if (diff_cons)
+		smc_curs_add(conn->peer_rmbe_size, &conn->local_rx_ctrl.cons, diff_cons);
+	/* clean prod_flags that are not carried by this imm_data */
+	memset(&conn->local_rx_ctrl.prod_flags, 0,
+	       sizeof(struct smc_cdc_producer_flags));
+	pflags = &conn->local_rx_ctrl.prod_flags;
+	pflags->write_blocked = imm_msg->data_with_flags_cr.write_blocked;
+	pflags->urg_data_present = imm_msg->data_with_flags_cr.urg_data_present;
+	pflags->urg_data_pending = imm_msg->data_with_flags_cr.urg_data_pending;
+	/* cause this imm_data contains no conn_state_flagsinfo, clean it */
+	memset(&conn->local_rx_ctrl.conn_state_flags, 0,
+	       sizeof(struct smc_cdc_conn_state_flags));
+
+	__smc_cdc_msg_recv_action(smc, diff_prod, diff_cons);
+}
+
 static void smc_cdc_handle_rwwi_ctrl_msg(struct smc_sock *smc,
 					 union smc_wr_imm_msg *imm_msg, int diff_prod)
 {
@@ -644,6 +692,12 @@ void smc_cdc_rx_handler_rwwi(struct ib_wc *wc)
 		break;
 	case SMC_WR_OP_CTRL:
 		smc_cdc_handle_rwwi_ctrl_msg(smc, &imm_msg, diff_prod);
+		break;
+	case SMC_WR_OP_DATA_CR:
+		smc_cdc_handle_rwwi_data_cr_msg(smc, &imm_msg, diff_prod);
+		break;
+	case SMC_WR_OP_DATA_WITH_FLAGS_CR:
+		smc_cdc_handle_rwwi_data_with_flags_cr_msg(smc, &imm_msg, diff_prod);
 		break;
 	}
 

net/smc/smc_tx.h
@@ -38,6 +38,6 @@ void smc_tx_sndbuf_nonfull(struct smc_sock *smc);
 void smc_tx_consumer_update(struct smc_connection *conn, bool force);
 int smcd_tx_ism_write(struct smc_connection *conn, void *data, size_t len,
 		      u32 offset, int signal);
-void smc_tx_put_inflight_credit(struct smc_link *link);
+void smc_tx_put_free_slot_rwwi(struct smc_link *link, bool complete);
 int smc_tx_rdma_write_with_no_data_rwwi(struct smc_connection *conn);
 #endif /* SMC_TX_H */

net/smc/smc_wr.c
@@ -87,8 +87,7 @@ static inline void smc_wr_tx_process_cqe(struct ib_wc *wc, bool is_rwwi)
 	link = wc->qp->qp_context;
 	if (is_rwwi) {
 		wr_id.data = wc->wr_id;
-		if (wr_id.inflight_credit_flag)
-			smc_tx_put_inflight_credit(link);
+		smc_tx_put_free_slot_rwwi(link, true);
 		if (wc->status)
 			smcr_link_down_cond_sched(link);
 		smc_cdc_tx_handler_rwwi(wc);
@@ -441,10 +440,6 @@ static inline void smc_wr_rx_demultiplex(struct ib_wc *wc)
 static inline void smc_wr_rx_process_cqe(struct ib_wc *wc)
 {
 	struct smc_link *link = wc->qp->qp_context;
-	bool credit_update = true;
-
-	if (wc->wc_flags & IB_WC_WITH_IMM)
-		credit_update = false;
 
 	if (wc->status == IB_WC_SUCCESS) {
 		link->wr_rx_tstamp = jiffies;
@@ -452,7 +447,7 @@ static inline void smc_wr_rx_process_cqe(struct ib_wc *wc)
 			smc_cdc_rx_handler_rwwi(wc);
 		else
 			smc_wr_rx_demultiplex(wc);
-		smc_wr_rx_post(link, credit_update); /* refill WR RX */
+		smc_wr_rx_post(link); /* refill WR RX */
 	} else {
 		/* handle status errors */
 		switch (wc->status) {
@@ -462,7 +457,7 @@ static inline void smc_wr_rx_process_cqe(struct ib_wc *wc)
 			smcr_link_down_cond_sched(link);
 			break;
 		default:
-			smc_wr_rx_post(link, credit_update); /* refill WR RX */
+			smc_wr_rx_post(link); /* refill WR RX */
 			break;
 		}
 	}
@@ -533,7 +528,7 @@ int smc_wr_rx_post_init(struct smc_link *link)
 	int rc = 0;
 
 	for (i = 0; i < link->wr_rx_cnt; i++)
-		rc = smc_wr_rx_post(link, true);
+		rc = smc_wr_rx_post(link);
 	/* credits have already been announced to peer */
 	atomic_set(&link->local_rq_credits, 0);
 	return rc;

net/smc/smc_wr.h
@@ -51,11 +51,14 @@ struct smc_wr_rx_handler {
 	u8			type;
 };
 
+/* SMC_WR_OP_xx should not exceed 7, as op field is 3bits */
 enum {
 	SMC_WR_OP_DATA = 0,
 	SMC_WR_OP_DATA_WITH_FLAGS,
 	SMC_WR_OP_CTRL,
-	SMC_WR_OP_RSVD
+	SMC_WR_OP_RSVD,
+	SMC_WR_OP_DATA_CR = 6,
+	SMC_WR_OP_DATA_WITH_FLAGS_CR = 7
 };
 
 /* used to replace member 'data' in union smc_wr_imm_msg
@@ -109,6 +112,40 @@ struct smc_wr_imm_ctrl_msg {
 		u8 token;
 } __packed;
 
+struct smc_wr_imm_data_cr_msg {
+#if defined(__BIG_ENDIAN_BITFIELD)
+		u32 token : 8;
+		u32	opcode : 3;
+		u32 credits : 8;
+		u32	diff_cons : 13;
+#elif defined(__LITTLE_ENDIAN_BITFIELD)
+		u32	diff_cons : 13;
+		u32 credits : 8;
+		u32	opcode : 3;
+		u32	token : 8;
+#endif
+} __packed;
+
+struct smc_wr_imm_data_with_flags_cr_msg {
+#if defined(__BIG_ENDIAN_BITFIELD)
+		u32 token : 8;
+		u32 opcode : 3;
+		u32	write_blocked : 1;
+		u32	urg_data_pending : 1;
+		u32	urg_data_present : 1;
+		u32 credits : 8;
+		u32 diff_cons : 10;
+#elif defined(__LITTLE_ENDIAN_BITFIELD)
+		u32 diff_cons : 10;
+		u32 credits : 8;
+		u32	urg_data_present : 1;
+		u32	urg_data_pending : 1;
+		u32	write_blocked : 1;
+		u32 opcode : 3;
+		u32 token : 8;
+#endif
+} __packed;
+
 struct smc_wr_imm_header {
 #if defined(__BIG_ENDIAN_BITFIELD)
 		u32 token : 8;
@@ -131,6 +168,8 @@ union smc_wr_imm_msg {
 	struct smc_wr_imm_data_msg data;
 	struct smc_wr_imm_data_with_flags_msg data_with_flags;
 	struct smc_wr_imm_ctrl_msg ctrl;
+	struct smc_wr_imm_data_cr_msg data_cr;
+	struct smc_wr_imm_data_with_flags_cr_msg data_with_flags_cr;
 };
 
 union smc_wr_rwwi_tx_id {
@@ -138,8 +177,7 @@ union smc_wr_rwwi_tx_id {
 	struct {
 #if defined(__BIG_ENDIAN_BITFIELD)
 		u64	rwwi_flag : 1;
-		u64 inflight_credit_flag : 1;	/* put inflight credit or not when scqe comes */
-		u64	reserved : 6;
+		u64	reserved : 7;
 		u64	token : 8;
 		u64	inflight_cons : 24;
 		u64	inflight_sent : 24;
@@ -147,8 +185,7 @@ union smc_wr_rwwi_tx_id {
 		u64	inflight_sent : 24;
 		u64	inflight_cons : 24;
 		u64	token : 8;
-		u64	reserved : 6;
-		u64 inflight_credit_flag : 1;	/* put inflight credit or not when scqe comes */
+		u64	reserved : 7;
 		u64	rwwi_flag : 1;
 #endif
 	};
@@ -161,6 +198,8 @@ union smc_wr_rwwi_tx_id {
  */
 #define SMC_DATA_MAX_DIFF_CONS				((1 << 21) - 1)
 #define SMC_DATA_WITH_FLAGS_MAX_DIFF_CONS	((1 << 17) - 1)
+#define SMC_DATA_CR_MAX_DIFF_CONS				((1 << 13) - 1)
+#define SMC_DATA_WITH_FLAGS_CR_MAX_DIFF_CONS	((1 << 10) - 1)
 #define SMC_PROD_FLAGS_MASK				0xE0
 #define SMC_WR_ID_SEQ_MASK				((uint64_t)0x7FFFFFFFFFFFFFFFULL)
 #define SMC_WR_ID_FLAG_RWWI				(((uint64_t)1) << 63)
@@ -264,7 +303,7 @@ static inline int smc_wr_rx_credits_need_announce_frequent(struct smc_link *link
 }
 
 /* post a new receive work request to fill a completed old work request entry */
-static inline int smc_wr_rx_post(struct smc_link *link, bool create_update)
+static inline int smc_wr_rx_post(struct smc_link *link)
 {
 	struct smc_link_stats *lnk_stats =
 		&link->lgr->lnk_stats[link->link_idx];
@@ -280,8 +319,7 @@ static inline int smc_wr_rx_post(struct smc_link *link, bool create_update)
 	rc = ib_post_recv(link->roce_qp, &link->wr_rx_ibs[index], NULL);
 	if (!rc) {
 		SMC_LINK_STAT_WR(lnk_stats, 0, 1);
-		if (create_update)
-			smc_wr_rx_put_credits(link, 1);
+		smc_wr_rx_put_credits(link, 1);
 	}
 	return rc;
 }

commit 0361d27888a21b930c57559143e378850cc203b6

anolis: net/smc: fix for credit leak when get free slot but the connection is killed
ANBZ: #6725

When connection get free slot, but killed, the occupied slot and credit
should be freed, otherwise will lead to resource leak.

Fixes: a0606e53a2f9 ("anolis: net/smc: support rq flow control for rwwi")
Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>

net/smc/smc_tx.c
@@ -509,6 +509,13 @@ static inline int __smc_get_free_slot_rwwi(struct smc_link *link)
 	return -EBUSY;
 }
 
+void smc_tx_put_free_slot_rwwi(struct smc_link *link, bool complete)
+{
+	atomic_inc(&link->tx_inflight_credit);
+	if (!complete)
+		smc_wr_tx_put_credits(link, 1, true);
+}
+
 static int smc_tx_get_free_slot_rwwi(struct smc_link *link,
 				     struct smc_connection *conn)
 {
@@ -534,18 +541,10 @@ static int smc_tx_get_free_slot_rwwi(struct smc_link *link,
 			return -EPIPE;
 	}
 	if (conn->killed) {
-		atomic_inc(&link->tx_inflight_credit);
+		smc_tx_put_free_slot_rwwi(link, false);
 		return -EPIPE;
 	}
 	return 0;
-}
-
-void smc_tx_put_free_slot_rwwi(struct smc_link *link, bool complete)
-{
-	if (!complete)
-		smc_wr_tx_put_credits(link, 1, true);
-
-	atomic_inc(&link->tx_inflight_credit);
 }
 
 static int smc_tx_fill_wr(struct smc_connection *conn, int *src_off,