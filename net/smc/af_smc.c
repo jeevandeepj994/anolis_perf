jeevandeepj994/anolis_perf

commit e3c8269a359297a6e10323f0dbe372d7b629be80

anolis: net/smc: Fix race between ibdev remove and lgr initialization
ANBZ: #5551

Several issues were found in fault-injected test by insmod/rmmod IB
device driver during the SMC-R communication. such as:

1. hung in smc_smcr_terminate_all()

 INFO: task rmmod:730273 blocked for more than 311 seconds.
 Call trace:
  __switch_to+0x108/0x168
  __schedule+0x2e0/0x7c0
  schedule+0x48/0xd0
  smc_smcr_terminate_all+0x350/0x380 [smc]
  smc_ib_remove_dev+0xa0/0x1e8 [smc]
  remove_client_context+0xa0/0xe8 [ib_core]
  disable_device+0x98/0x130 [ib_core]
  __ib_unregister_device+0x48/0xb8 [ib_core]
  ib_unregister_device+0x2c/0x40 [ib_core]
  erdma_ib_device_remove+0x28/0xc0 [erdma]
  erdma_remove+0x1c/0x30 [erdma]
  pci_device_remove+0x44/0xe8
  device_release_driver_internal+0x110/0x1f0
  driver_detach+0xb0/0x178
  bus_remove_driver+0x84/0xf0
  driver_unregister+0x34/0x60
  pci_unregister_driver+0x28/0x118
  erdma_exit_module+0x18/0xa78 [erdma]
  __arm64_sys_delete_module+0x1ac/0x290
  el0_svc_common+0xb8/0x208
  do_el0_svc+0x88/0x90
  el0_svc+0x1c/0x28
  el0_sync_handler+0x88/0xb0
  el0_sync+0x148/0x180
 Kernel panic - not syncing: hung_task: blocked tasks

The race can be concluded as:

remove IB devices                  | CLC handshake
---------------------------------------------------------------
                                   | find IB devices
                                   |
for links in smcibdev              |
        schedule links down        |
                                   | smcr_link_init
                                   |  \- smcibdev->lnk_cnt++
                                   |
wait for smcibdev->lnk_cnt == 0    |
(hung until new lgr free)          |

or

remove IB devices                  | CLC handshake
---------------------------------------------------------------
                                   | find IB devices
                                   |
                                   | smcr_link_init

net/smc/af_smc.c
@@ -1106,6 +1106,20 @@ static int smc_find_proposal_devices(struct smc_sock *smc,
 		ini->smcr_version &= ~SMC_V1;
 	/* else RDMA is supported for this connection */
 
+	/* make sure SMC_V1 ibdev still available */
+	if (ini->smcr_version & SMC_V1) {
+		mutex_lock(&smc_ib_devices.mutex);
+		if (list_empty(&ini->ib_dev->list)) {
+			ini->ib_dev = NULL;
+			ini->ib_port = 0;
+			ini->smcr_version &= ~SMC_V1;
+		} else {
+			/* put in __smc_connect */
+			smc_ib_get_pending_device(ini->ib_dev);
+		}
+		mutex_unlock(&smc_ib_devices.mutex);
+	}
+
 	ini->smc_type_v1 = smc_indicated_type(ini->smcd_version & SMC_V1,
 					      ini->smcr_version & SMC_V1);
 
@@ -1124,6 +1138,20 @@ static int smc_find_proposal_devices(struct smc_sock *smc,
 	    smc_find_rdma_device(smc, ini))
 		ini->smcr_version &= ~SMC_V2;
 	ini->check_smcrv2 = false;
+
+	/* make sure SMC_V2 ibdev still available */
+	if (ini->smcr_version & SMC_V2) {
+		mutex_lock(&smc_ib_devices.mutex);
+		if (list_empty(&ini->smcrv2.ib_dev_v2->list)) {
+			ini->smcrv2.ib_dev_v2 = NULL;
+			ini->smcrv2.ib_port_v2 = 0;
+			ini->smcr_version &= ~SMC_V2;
+		} else {
+			/* put in __smc_connect */
+			smc_ib_get_pending_device(ini->smcrv2.ib_dev_v2);
+		}
+		mutex_unlock(&smc_ib_devices.mutex);
+	}
 
 	ini->smc_type_v2 = smc_indicated_type(ini->smcd_version & SMC_V2,
 					      ini->smcr_version & SMC_V2);
@@ -1581,6 +1609,10 @@ static int __smc_connect(struct smc_sock *smc)
 	if (rc)
 		goto vlan_cleanup;
 
+	if (ini->smcrv2.ib_dev_v2)
+		smc_ib_put_pending_device(ini->smcrv2.ib_dev_v2);
+	if (ini->ib_dev)
+		smc_ib_put_pending_device(ini->ib_dev);
 	SMC_STAT_CLNT_SUCC_INC(sock_net(smc->clcsock->sk), aclc);
 	smc_connect_ism_vlan_cleanup(smc, ini);
 	kfree(buf);
@@ -1591,6 +1623,10 @@ vlan_cleanup:
 	smc_connect_ism_vlan_cleanup(smc, ini);
 	kfree(buf);
 fallback:
+	if (ini->smcrv2.ib_dev_v2)
+		smc_ib_put_pending_device(ini->smcrv2.ib_dev_v2);
+	if (ini->ib_dev)
+		smc_ib_put_pending_device(ini->ib_dev);
 	kfree(ini);
 	return smc_connect_decline_fallback(smc, rc, version);
 }
@@ -2295,6 +2331,17 @@ static void smc_find_rdma_v2_device_serv(struct smc_sock *new_smc,
 		smc_find_ism_store_rc(rc, ini);
 		goto not_found;
 	}
+	/* make sure SMC_V2 ibdev still available */
+	mutex_lock(&smc_ib_devices.mutex);
+	if (list_empty(&ini->smcrv2.ib_dev_v2->list)) {
+		smc_find_ism_store_rc(SMC_CLC_DECL_NOSMCRDEV, ini);
+		goto not_found;
+	} else {
+		/* put below or in smc_listen_work */
+		smc_ib_get_pending_device(ini->smcrv2.ib_dev_v2);
+	}
+	mutex_unlock(&smc_ib_devices.mutex);
+
 	if (!ini->smcrv2.uses_gateway)
 		memcpy(ini->smcrv2.nexthop_mac, pclc->lcl.mac, ETH_ALEN);
 
@@ -2310,6 +2357,7 @@ static void smc_find_rdma_v2_device_serv(struct smc_sock *new_smc,
 		return;
 	ini->smcr_version = smcr_version;
 	smc_find_ism_store_rc(rc, ini);
+	smc_ib_put_pending_device(ini->smcrv2.ib_dev_v2);
 
 not_found:
 	ini->smcr_version &= ~SMC_V2;
@@ -2335,6 +2383,18 @@ static int smc_find_rdma_v1_device_serv(struct smc_sock *new_smc,
 		/* no RDMA device found */
 		return SMC_CLC_DECL_NOSMCDEV;
 	}
+	/* make sure SMC_V1 ibdev still available */
+	mutex_lock(&smc_ib_devices.mutex);
+	if (list_empty(&ini->ib_dev->list)) {
+		ini->ib_dev = NULL;
+		ini->ib_port = 0;
+		mutex_unlock(&smc_ib_devices.mutex);
+		return SMC_CLC_DECL_NOSMCDEV;
+	}
+	/* put in smc_listen_work */
+	smc_ib_get_pending_device(ini->ib_dev);
+	mutex_unlock(&smc_ib_devices.mutex);
+
 	rc = smc_listen_rdma_init(new_smc, ini);
 	if (rc)
 		return rc;
@@ -2537,6 +2597,10 @@ static void smc_listen_work(struct work_struct *work)
 			goto out_unlock;
 		mutex_unlock(&smc_server_lgr_pending);
 	}
+	if (ini->smcrv2.ib_dev_v2)
+		smc_ib_put_pending_device(ini->smcrv2.ib_dev_v2);
+	if (ini->ib_dev)
+		smc_ib_put_pending_device(ini->ib_dev);
 	smc_conn_save_peer_info(new_smc, cclc);
 	smc_listen_out_connected(new_smc);
 	if (newclcsock->sk)
@@ -2544,6 +2608,10 @@ static void smc_listen_work(struct work_struct *work)
 	goto out_free;
 
 out_unlock:
+	if (ini->smcrv2.ib_dev_v2)
+		smc_ib_put_pending_device(ini->smcrv2.ib_dev_v2);
+	if (ini->ib_dev)
+		smc_ib_put_pending_device(ini->ib_dev);
 	mutex_unlock(&smc_server_lgr_pending);
 out_decl:
 	smc_listen_decline(new_smc, rc, ini ? ini->first_contact_local : 0,

net/smc/smc_ib.c
@@ -1142,6 +1142,17 @@ bool smc_ib_is_iwarp(struct ib_device *ibdev, u8 ibport)
 	return rdma_protocol_iwarp(ibdev, ibport);
 }
 
+void smc_ib_get_pending_device(struct smc_ib_device *smcibdev)
+{
+	refcount_inc(&smcibdev->lnk_pending_cnt);
+}
+
+void smc_ib_put_pending_device(struct smc_ib_device *smcibdev)
+{
+	if (refcount_dec_and_test(&smcibdev->lnk_pending_cnt))
+		wake_up(&smcibdev->lnks_pending);
+}
+
 /* Reserve socket ports of each net namespace which can be accessed
  * by eRDMA (iWARP) device for out-bound RC establishment.
  */
@@ -1236,7 +1247,9 @@ static int smc_ib_add_dev(struct ib_device *ibdev)
 	}
 	INIT_WORK(&smcibdev->port_event_work, smc_ib_port_event_work);
 	atomic_set(&smcibdev->lnk_cnt, 0);
+	refcount_set(&smcibdev->lnk_pending_cnt, 1);
 	init_waitqueue_head(&smcibdev->lnks_deleted);
+	init_waitqueue_head(&smcibdev->lnks_pending);
 	mutex_init(&smcibdev->mutex);
 	mutex_lock(&smc_ib_devices.mutex);
 	list_add_tail(&smcibdev->list, &smc_ib_devices.list);
@@ -1279,6 +1292,9 @@ static void smc_ib_remove_dev(struct ib_device *ibdev, void *client_data)
 	mutex_lock(&smc_ib_devices.mutex);
 	list_del_init(&smcibdev->list); /* remove from smc_ib_devices */
 	mutex_unlock(&smc_ib_devices.mutex);
+	smc_ib_put_pending_device(smcibdev);
+	wait_event(smcibdev->lnks_pending,	/* wait for no pending usage */
+		   !refcount_read(&smcibdev->lnk_pending_cnt));
 	pr_info_ratelimited("smc: removing ib device %s\n",
 			    smcibdev->ibdev->name);
 	smc_smcr_terminate_all(smcibdev);

net/smc/smc_ib.h
@@ -59,7 +59,9 @@ struct smc_ib_device {				/* ib-device infos for smc */
 	unsigned long		port_event_mask;
 	DECLARE_BITMAP(ports_going_away, SMC_MAX_PORTS);
 	atomic_t		lnk_cnt;	/* number of links on ibdev */
+	refcount_t		lnk_pending_cnt;/* number of links attempt to use ibdev */
 	wait_queue_head_t	lnks_deleted;	/* wait 4 removal of all links*/
+	wait_queue_head_t	lnks_pending;	/* wait 4 pending establish of links */
 	struct mutex		mutex;		/* protect dev setup+cleanup */
 	atomic_t		lnk_cnt_by_port[SMC_MAX_PORTS];
 						/* number of links per port */
@@ -124,5 +126,7 @@ int smc_ib_find_route(__be32 saddr, __be32 daddr,
 		      u8 nexthop_mac[], u8 *uses_gateway);
 bool smc_ib_is_valid_local_systemid(void);
 bool smc_ib_is_iwarp(struct ib_device *ibdev, u8 ibport);
+void smc_ib_get_pending_device(struct smc_ib_device *smcibdev);
+void smc_ib_put_pending_device(struct smc_ib_device *smcibdev);
 int smcr_nl_get_device(struct sk_buff *skb, struct netlink_callback *cb);
 #endif

net/smc/smc_llc.c
@@ -1098,6 +1098,7 @@ int smc_llc_cli_add_link(struct smc_link *link, struct smc_llc_qentry *qentry)
 	struct smc_llc_msg_add_link *llc = &qentry->msg.add_link;
 	enum smc_lgr_type lgr_new_t = SMC_LGR_SYMMETRIC;
 	struct smc_link_group *lgr = smc_get_lgr(link);
+	struct smc_ib_device *ibdev_selected = NULL;
 	struct smc_init_info *ini = NULL;
 	struct smc_link *lnk_new = NULL;
 	int lnk_idx, rc = 0;
@@ -1137,14 +1138,29 @@ int smc_llc_cli_add_link(struct smc_link *link, struct smc_llc_qentry *qentry)
 		ini->ib_dev = link->smcibdev;
 		ini->ib_port = link->ibport;
 	}
+
+	mutex_lock(&smc_ib_devices.mutex);
+	if (lgr->smc_version == SMC_V2)
+		ibdev_selected = ini->smcrv2.ib_dev_v2;
+	else if (lgr->smc_version < SMC_V2)
+		ibdev_selected = ini->ib_dev;
+	if (list_empty(&ibdev_selected->list)) {
+		rc = -ENODEV;
+		ibdev_selected = NULL;
+		mutex_unlock(&smc_ib_devices.mutex);
+		goto out_reject;
+	}
+	smc_ib_get_pending_device(ibdev_selected);	/* put below */
+	mutex_unlock(&smc_ib_devices.mutex);
+
 	lnk_idx = smc_llc_alloc_alt_link(lgr, lgr_new_t);
 	if (lnk_idx < 0)
-		goto out_reject;
+		goto out_pending_dev;
 	lnk_new = &lgr->lnk[lnk_idx];
 	lnk_new->iw_conn_param = link->iw_conn_param;
 	rc = smcr_link_init(lgr, lnk_new, lnk_idx, ini);
 	if (rc)
-		goto out_reject;
+		goto out_pending_dev;
 	smc_llc_save_add_link_info(lnk_new, llc);
 	lnk_new->link_id = llc->link_num;	/* SMC server assigns link id */
 	smc_llc_link_set_uid(lnk_new);
@@ -1172,11 +1188,15 @@ int smc_llc_cli_add_link(struct smc_link *link, struct smc_llc_qentry *qentry)
 		}
 	}
 	rc = smc_llc_cli_conf_link(link, ini, lnk_new, lgr_new_t);
-	if (!rc)
+	if (!rc) {
+		smc_ib_put_pending_device(ibdev_selected);
 		goto out;
+	}
 out_clear_lnk:
 	lnk_new->state = SMC_LNK_INACTIVE;
 	smcr_link_clear(lnk_new, false);
+out_pending_dev:
+	smc_ib_put_pending_device(ibdev_selected);
 out_reject:
 	smc_llc_cli_add_link_reject(qentry);
 out:
@@ -1469,6 +1489,7 @@ int smc_llc_srv_add_link(struct smc_link *link,
 			 struct smc_llc_qentry *req_qentry)
 {
 	enum smc_lgr_type lgr_new_t = SMC_LGR_SYMMETRIC;
+	struct smc_ib_device *ibdev_selected = NULL;
 	struct smc_link_group *lgr = link->lgr;
 	struct smc_llc_msg_add_link *add_llc;
 	struct smc_llc_qentry *qentry = NULL;
@@ -1512,16 +1533,31 @@ int smc_llc_srv_add_link(struct smc_link *link,
 		ini->ib_dev = link->smcibdev;
 		ini->ib_port = link->ibport;
 	}
+
+	mutex_lock(&smc_ib_devices.mutex);
+	if (lgr->smc_version == SMC_V2)
+		ibdev_selected = ini->smcrv2.ib_dev_v2;
+	else if (lgr->smc_version < SMC_V2)
+		ibdev_selected = ini->ib_dev;
+	if (list_empty(&ibdev_selected->list)) {
+		rc = -ENODEV;
+		ibdev_selected = NULL;
+		mutex_unlock(&smc_ib_devices.mutex);
+		goto out;
+	}
+	smc_ib_get_pending_device(ibdev_selected);	/* put below */
+	mutex_unlock(&smc_ib_devices.mutex);
+
 	lnk_idx = smc_llc_alloc_alt_link(lgr, lgr_new_t);
 	if (lnk_idx < 0) {
 		rc = 0;
-		goto out;
+		goto out_dev;
 	}
 
 	lgr->lnk[lnk_idx].iw_conn_param = link->iw_conn_param;
 	rc = smcr_link_init(lgr, &lgr->lnk[lnk_idx], lnk_idx, ini);
 	if (rc)
-		goto out;
+		goto out_dev;
 	link_new = &lgr->lnk[lnk_idx];
 
 	rc = smcr_buf_map_lgr(link_new);
@@ -1571,6 +1607,8 @@ int smc_llc_srv_add_link(struct smc_link *link,
 	rc = smc_llc_srv_conf_link(link, link_new, lgr_new_t);
 	if (rc)
 		goto out_err;
+
+	smc_ib_put_pending_device(ibdev_selected);
 	kfree(ini);
 	return 0;
 out_err:
@@ -1578,6 +1616,8 @@ out_err:
 		link_new->state = SMC_LNK_INACTIVE;
 		smcr_link_clear(link_new, false);
 	}
+out_dev:
+	smc_ib_put_pending_device(ibdev_selected);
 out:
 	kfree(ini);
 	if (send_req_add_link_resp)

commit 37616da64f00a31f4ecff44be89d575206fe931d

anolis: net/smc: Fix missing mutex_unlock when finding SMCv2 device
ANBZ: #6742

A mutex_unlock was missed in smc_find_rdma_v2_device_serv() when finding
ini->smcrv2.ib_dev_v2->list is empty. So fix it.

Fixes: e3c8269a3592 (anolis: net/smc: Fix race between ibdev remove and lgr initialization)
Signed-off-by: Wen Gu <guwen@linux.alibaba.com>

net/smc/af_smc.c
@@ -2564,6 +2564,7 @@ static void smc_find_rdma_v2_device_serv(struct smc_sock *new_smc,
 	mutex_lock(&smc_ib_devices.mutex);
 	if (list_empty(&ini->smcrv2.ib_dev_v2->list)) {
 		smc_find_ism_store_rc(SMC_CLC_DECL_NOSMCRDEV, ini);
+		mutex_unlock(&smc_ib_devices.mutex);
 		goto not_found;
 	} else {
 		/* put below or in smc_listen_work */