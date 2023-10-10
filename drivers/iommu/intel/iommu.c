jeevandeepj994/anolis_perf

commit 3ca49517707e544f83e01ef8371a9673c2825810

iommu/vt-d: Check model and stepping for PASID PT SRE support
ANBZ: #1675

commit cdaf9980a697833a3bd136be6654b17af3a8ee64 intel-github.

Before SPR E stepping, PASID pass-through with supervisor request is not
supported. Warn if used on the wrong CPUs.

Intel-SIG: commit cdaf9980a697 iommu/vt-d: Check model and stepping for
PASID PT SRE support.
Necessary fix to VM boot succesffuly with DSA PF pass through in SWQ mode.

Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
[ Aubrey Li: amend commit log ]
Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>
Signed-off-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/522
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

drivers/iommu/intel/iommu.c
@@ -6397,6 +6397,19 @@ out:
 	return ret;
 }
 
+/* Temporary check */
+static inline bool check_pasid_pt_sre(void)
+{
+	struct cpuinfo_x86 *c = &cpu_data(0);
+
+	if (c->x86_model == 0x8f && c->x86_stepping >= 4) {
+               pr_debug("SPR E0+, PASID PT SRE enabled");
+               return true;
+	}
+	pr_alert("No PASID PT SRE, in-kernel PASID DMA not supported!!!");
+	return false;
+}
+
 static int intel_iommu_attach_dev_pasid(struct iommu_domain *domain,
 					struct device *dev,
 					ioasid_t pasid)
@@ -6406,6 +6419,12 @@ static int intel_iommu_attach_dev_pasid(struct iommu_domain *domain,
 	struct intel_iommu *iommu = info->iommu;
 	unsigned long flags;
 	int ret = 0;
+
+	/*
+	 * We don't bail here in that some drivers tie user SVM with
+	 * kernel PASID support.
+	 */
+	check_pasid_pt_sre();
 
 	if (!sm_supported(iommu) || !info)
 		return -ENODEV;

commit f0a4c93e6f260e10d87ffe1727e3c25d471db576

anolis: iommu/vt-d: fix alert message of PASID PT SRE checking for EMR
ANBZ: #6748

PASID pass-through with supervisor request is supported on EMR platform.
The alert message should not be printed in check_pasid_pt_sre function.

Intel-SIG: fix alert message of PASID PT SRE checking for EMR platform.

Fixes: 3ca49517707e ("iommu/vt-d: Check model and stepping for PASID PT SRE support")
Signed-off-by: Fengqian Gao <fengqian.gao@intel.com>
Signed-off-by: Xiaochen Shen <xiaochen.shen@intel.com>
Reviewed-by: Aubrey Li <aubrey.li@linux.intel.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2275

drivers/iommu/intel/iommu.c
@@ -6404,9 +6404,9 @@ static inline bool check_pasid_pt_sre(void)
 {
 	struct cpuinfo_x86 *c = &cpu_data(0);
 
-	if (c->x86_model == 0x8f && c->x86_stepping >= 4) {
-               pr_debug("SPR E0+, PASID PT SRE enabled");
-               return true;
+	if ((c->x86_model == 0x8f && c->x86_stepping >= 4) || c->x86_model == 0xcf) {
+		pr_debug("Platform PASID PT SRE enabled");
+		return true;
 	}
 	pr_alert("No PASID PT SRE, in-kernel PASID DMA not supported!!!");
 	return false;