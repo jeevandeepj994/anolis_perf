jeevandeepj994/anolis_perf

commit 29dffa8bd544be60710e7f84f92ce98319693ea0

anolis: arm64: kfence: allow allocating kfence pool after system boot stage
ANBZ: #6171

If split mapping is enabled, kfence pool can be initialized after system boot.
However, previous commits disable allocating kfence pool if already reserved
at system boot stage. Remove corresponding judgements to enable kfence init late.

Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2057

mm/kfence/core.c
@@ -141,13 +141,6 @@ static int param_set_num_objects(const char *val, const struct kernel_param *kp)
 	if (READ_ONCE(kfence_enabled) || !num)
 		return -EINVAL; /* can not change num_objects when enabled */
 
-	/*
-	 * If the kfence pool has been initialized eariler, do not change the
-	 * value of kfence_num_objects.
-	 */
-	if (__kfence_pool_early_init)
-		return (system_state == SYSTEM_BOOTING) ? 0 : -EINVAL;
-
 	*((unsigned long *)kp->arg) = num;
 	return 0;
 }
@@ -955,7 +948,7 @@ static bool __kfence_init_pool_area(struct kfence_pool_area *kpa)
 	struct page *pages;
 	int i;
 
-	if (!arch_kfence_init_pool(kpa))
+	if (!__kfence_pool_early_init && !arch_kfence_init_pool(kpa))
 		goto err;
 
 	pages = virt_to_page(addr);
@@ -1827,8 +1820,12 @@ void kfence_init_late(void)
 	if (!READ_ONCE(kfence_sample_interval))
 		return;
 
-	if (__kfence_pool_early_init)
-		return;
+	/*
+	 * If kfence pool is initialized later, the early init kfence pool has
+	 * been released, reset the pointer here to avoid re-initialization if
+	 * split_linear_mapping disabled.
+	 */
+	__kfence_pool_early_init = NULL;
 
 	mutex_lock(&kfence_mutex);
 

commit 8a91b2c71bc337c0ef776ef93c4ebe5146e2d098

anolis: mm: kfence: avoid error caused by multiple assignment
ANBZ: #6778

If kfence_num_objects has been assignd during system booting and its
value greater than PUD_SIZE. Finally, the value is always equal to the
command line setting. However, if the time that kfence_sample_interval
has been parsed after kfence_num_objects, it would trigger the initialized
pool size doesn't equal to the value of kfence_num_objects.

Fixes: 29dffa8bd544 ("anolis: arm64: kfence: allow allocating kfence pool after system boot stage")
Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2276

mm/kfence/core.c
@@ -138,6 +138,11 @@ static int param_set_num_objects(const char *val, const struct kernel_param *kp)
 	if (ret < 0)
 		return ret;
 
+#ifdef CONFIG_ARM64
+	if (system_state == SYSTEM_BOOTING)
+		return 0;
+#endif
+
 	if (READ_ONCE(kfence_enabled) || !num)
 		return -EINVAL; /* can not change num_objects when enabled */
 