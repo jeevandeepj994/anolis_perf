jeevandeepj994/anolis_perf

commit c02f4ea4082402d128afacbe2b4d2fc976a449ef

anolis: sched/core: introduce sysctl_sched_core
ANBZ: #6201

In order to be able to control the switch of core sched, we introduce
sysctl_sched_core. When turned on, it's allowed to give/share cookie
to tasks. when turned off, the cookie of all cookies will be cleared,
and it's not allowed to give/share cookie to tasks (only get cookie
is allowed).

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Reviewed-by: Yi Tao <escape@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2243

kernel/sched/core_sched.c
@@ -128,6 +128,9 @@ static void __sched_core_set(struct task_struct *p, unsigned long cookie)
 	sched_core_put_cookie(cookie);
 }
 
+int sysctl_sched_core;
+DEFINE_STATIC_KEY_FALSE(__sysctl_sched_core_enabled);
+
 /* Called from prctl interface: PR_SCHED_CORE */
 int sched_core_share_pid(unsigned int cmd, pid_t pid, enum pid_type type,
 			 unsigned long uaddr)
@@ -139,6 +142,10 @@ int sched_core_share_pid(unsigned int cmd, pid_t pid, enum pid_type type,
 
 	if (!static_branch_likely(&sched_smt_present))
 		return -ENODEV;
+
+	if (!static_branch_likely(&__sysctl_sched_core_enabled) &&
+	    cmd != PR_SCHED_CORE_GET)
+		return -EPERM;
 
 	BUILD_BUG_ON(PR_SCHED_CORE_SCOPE_THREAD != PIDTYPE_PID);
 	BUILD_BUG_ON(PR_SCHED_CORE_SCOPE_THREAD_GROUP != PIDTYPE_TGID);
@@ -241,6 +248,56 @@ out_tasklist:
 out:
 	sched_core_put_cookie(cookie);
 	put_task_struct(task);
+	return err;
+}
+
+void clear_all_cookie(void)
+{
+	int cpu;
+	struct task_struct *p, *t;
+	int ret;
+
+	read_lock(&tasklist_lock);
+	for_each_process_thread(p, t)
+		sched_core_share_pid(PR_SCHED_CORE_CLEAR, t->pid, PIDTYPE_PID, 0);
+	read_unlock(&tasklist_lock);
+
+	/*
+	 * When sched_core_count become zero, the work which turn off __sched_core_enabled
+	 * will queue in system_wq, so we flush it to make sure that __sched_core_enabled
+	 * is really turned off.
+	 */
+	flush_workqueue(system_wq);
+}
+
+int sysctl_sched_core_handler(struct ctl_table *table, int write,
+				void __user *buffer, size_t *lenp, loff_t *ppos)
+{
+	unsigned int old, new;
+	int err;
+
+	if (!write) {
+		err = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
+		return err;
+	}
+	old = sysctl_sched_core;
+	err = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
+	new = sysctl_sched_core;
+
+	if (old == new)
+		return err;
+
+	if (new) {
+		static_branch_enable(&__sysctl_sched_core_enabled);
+	} else {
+		static_branch_disable(&__sysctl_sched_core_enabled);
+		/*
+		 * When turn off sched_core, we need to clear cookie of all the tasks, to
+		 * make __sched_core_enabled off.
+		 */
+		clear_all_cookie();
+	}
+
 	return err;
 }
 

include/linux/sched/sysctl.h
@@ -89,6 +89,12 @@ extern unsigned int sysctl_sched_expel_update_interval;
 #endif
 #endif
 
+#ifdef CONFIG_SCHED_CORE
+extern int sysctl_sched_core;
+extern int sysctl_sched_core_handler(struct ctl_table *table, int write,
+				     void __user *buffer, size_t *lenp, loff_t *ppos);
+#endif
+
 #ifdef CONFIG_SCHED_AUTOGROUP
 extern unsigned int sysctl_sched_autogroup_enabled;
 #endif

kernel/sysctl.c
@@ -2027,6 +2027,17 @@ static struct ctl_table kern_table[] = {
 	},
 #endif
 #endif
+#ifdef CONFIG_SCHED_CORE
+	{
+		.procname	= "sched_core",
+		.data		= &sysctl_sched_core,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= sysctl_sched_core_handler,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+#endif
 #ifdef CONFIG_PROVE_LOCKING
 	{
 		.procname	= "prove_locking",

commit 403ae745815929e5b8f2e2514863574496ded202

anolis: sched/core: remove unused variable of clear_all_cookie()
ANBZ: #6835

In function clear_all_cookie(), variable cpu and ret are not used,
so remove them to avoid warning report.

Fixes: c02f4ea40824 ("anolis: sched/core: introduce sysctl_sched_core")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/core_sched.c
@@ -253,9 +253,7 @@ out:
 
 void clear_all_cookie(void)
 {
-	int cpu;
 	struct task_struct *p, *t;
-	int ret;
 
 	read_lock(&tasklist_lock);
 	for_each_process_thread(p, t)