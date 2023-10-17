jeevandeepj994/anolis_perf

commit ab07564f5416082dbbc65519af507908c6ef5ae5

anolis: sched: account sibidle for core scheduling
ANBZ: #6729

Accounting for sibling idle time for core scheduling, which is time
where a cookie'd task running with SMT sibling idle, including sibling
forced idle time and sibling real idle time, collectively called sibidle.

A few details:
 - For SMT > 2, we scale the amount of idle charged based on the number
   of idle siblings in function account_sibidle_time(). Additionally,
   we split the time up and evenly charge it to all running tasks, as
   each is equally responsible forthe idle.
 - When core sched is enabled and sibidle count is not zero, we account
   sibidle in function account_sibidle_time(), otherwise in function
   update_acpu().

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2260

kernel/sched/core_sched.c
@@ -304,7 +304,7 @@ int sysctl_sched_core_handler(struct ctl_table *table, int write,
 #ifdef CONFIG_SCHEDSTATS
 
 /* REQUIRES: rq->core's clock recently updated. */
-void __sched_core_account_forceidle(struct rq *rq)
+void __sched_core_account_sibidle(struct rq *rq)
 {
 	const struct cpumask *smt_mask = cpu_smt_mask(cpu_of(rq));
 	u64 delta, now = rq_clock(rq->core);
@@ -314,28 +314,28 @@ void __sched_core_account_forceidle(struct rq *rq)
 
 	lockdep_assert_rq_held(rq);
 
-	WARN_ON_ONCE(!rq->core->core_forceidle_count);
+	WARN_ON_ONCE(!rq->core->core_sibidle_count);
 
-	if (rq->core->core_forceidle_start == 0)
-		return;
+	if (rq->core->core_sibidle_start == 0)
+		goto out;
 
-	delta = now - rq->core->core_forceidle_start;
+	delta = now - rq->core->core_sibidle_start;
 	if (unlikely((s64)delta <= 0))
-		return;
+		goto out;
 
-	rq->core->core_forceidle_start = now;
+	rq->core->core_sibidle_start = now;
 
-	if (WARN_ON_ONCE(!rq->core->core_forceidle_occupation)) {
+	if (WARN_ON_ONCE(!rq->core->core_sibidle_occupation)) {
 		/* can't be forced idle without a running task */
-	} else if (rq->core->core_forceidle_count > 1 ||
-		   rq->core->core_forceidle_occupation > 1) {
+	} else if (rq->core->core_sibidle_count > 1 ||
+		   rq->core->core_sibidle_occupation > 1) {
 		/*
 		 * For larger SMT configurations, we need to scale the charged
 		 * forced idle amount since there can be more than one forced
 		 * idle sibling and more than one running cookied task.
 		 */
-		delta *= rq->core->core_forceidle_count;
-		delta = div_u64(delta, rq->core->core_forceidle_occupation);
+		delta *= rq->core->core_sibidle_count;
+		delta = div_u64(delta, rq->core->core_sibidle_occupation);
 	}
 
 	for_each_cpu(i, smt_mask) {
@@ -349,19 +349,27 @@ void __sched_core_account_forceidle(struct rq *rq)
 		 * Note: this will account forceidle to the current cpu, even
 		 * if it comes from our SMT sibling.
 		 */
-		__account_forceidle_time(p, delta);
+		__account_sibidle_time(p, delta, !!rq->core->core_forceidle_count);
 	}
+
+out:
+#ifdef CONFIG_SCHED_ACPU
+	for_each_cpu(i, smt_mask) {
+		rq_i = cpu_rq(i);
+		rq->last_acpu_update_time = now;
+	}
+#endif
 }
 
 void __sched_core_tick(struct rq *rq)
 {
-	if (!rq->core->core_forceidle_count)
+	if (!rq->core->core_sibidle_count)
 		return;
 
 	if (rq != rq->core)
 		update_rq_clock(rq->core);
 
-	__sched_core_account_forceidle(rq);
+	__sched_core_account_sibidle(rq);
 }
 
 #endif /* CONFIG_SCHEDSTATS */

include/linux/kernel_stat.h
@@ -31,7 +31,7 @@ enum cpu_usage_stat {
 #ifdef CONFIG_SCHED_CORE
 	CPUTIME_FORCEIDLE,
 #endif
-#ifdef CONFIG_SCHED_ACPU
+#if defined(CONFIG_SCHED_ACPU) || defined(CONFIG_SCHED_CORE)
 	CPUTIME_SIBIDLE,
 #endif
 	NR_STATS,
@@ -122,11 +122,8 @@ extern void account_process_tick(struct task_struct *, int user);
 
 extern void account_idle_ticks(unsigned long ticks);
 
-#ifdef CONFIG_SCHED_CORE
-extern void __account_forceidle_time(struct task_struct *tsk, u64 delta);
-#endif
-#ifdef CONFIG_SCHED_ACPU
-extern void __account_sibidle_time(struct task_struct *tsk, u64 delta);
+#if defined(CONFIG_SCHED_ACPU) || defined(CONFIG_SCHED_CORE)
+extern void __account_sibidle_time(struct task_struct *tsk, u64 delta, bool fi);
 #endif
 
 #endif /* _LINUX_KERNEL_STAT_H */

include/linux/sched.h
@@ -500,7 +500,7 @@ struct sched_statistics {
 	u64				core_forceidle_sum;
 #endif
 
-#ifdef CONFIG_SCHED_ACPU
+#if defined(CONFIG_SCHED_ACPU) || defined(CONFIG_SCHED_ACPU)
 	u64				core_sibidle_sum;
 #endif
 

kernel/sched/core.c
@@ -305,7 +305,7 @@ static void __sched_core_flip(bool enabled)
 		for_each_cpu(t, smt_mask)
 			cpu_rq(t)->core_enabled = enabled;
 
-		cpu_rq(cpu)->core->core_forceidle_start = 0;
+		cpu_rq(cpu)->core->core_sibidle_start = 0;
 
 		sched_core_unlock(cpu, &flags);
 
@@ -3973,6 +3973,15 @@ static void update_acpu(struct rq *rq, struct task_struct *prev, struct task_str
 	if (!static_branch_likely(&acpu_enabled) || !schedstat_enabled())
 		return;
 
+	/*
+	 * If core sched is enabled and core_sibidle_count is not zero, we update sibidle
+	 * time in function __sched_core_account_sibidle().
+	 */
+#ifdef CONFIG_SCHED_CORE
+	if (rq->core->core_sibidle_count)
+		goto out;
+#endif
+
 	/* Update idle sum and busy sum for current rq. */
 	delta = now - rq->last_acpu_update_time;
 	if (prev == rq->idle)
@@ -4011,7 +4020,7 @@ static void update_acpu(struct rq *rq, struct task_struct *prev, struct task_str
 	if (prev != rq->idle) {
 		delta = sibidle_sum - rq->sibidle_sum;
 		delta = delta > 0 ? delta : 0;
-		__account_sibidle_time(prev, delta);
+		__account_sibidle_time(prev, delta, false);
 	}
 
 	if (next != rq->idle)
@@ -5036,18 +5045,21 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 
 	/* reset state */
 	rq->core->core_cookie = 0UL;
-	if (rq->core->core_forceidle_count) {
+	if (rq->core->core_sibidle_count) {
 		if (!core_clock_updated) {
 			update_rq_clock(rq->core);
 			core_clock_updated = true;
 		}
-		sched_core_account_forceidle(rq);
+		sched_core_account_sibidle(rq);
 		/* reset after accounting force idle */
-		rq->core->core_forceidle_start = 0;
-		rq->core->core_forceidle_count = 0;
-		rq->core->core_forceidle_occupation = 0;
-		need_sync = true;
-		fi_before = true;
+		rq->core->core_sibidle_start = 0;
+		rq->core->core_sibidle_count = 0;
+		rq->core->core_sibidle_occupation = 0;
+		if (rq->core->core_forceidle_count) {
+			rq->core->core_forceidle_count = 0;
+			need_sync = true;
+			fi_before = true;
+		}
 	}
 
 	/*
@@ -5123,6 +5135,7 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		rq_i->core_pick = p;
 
 		if (p == rq_i->idle) {
+			rq->core->core_sibidle_count++;
 			if (rq_i->nr_running) {
 				rq->core->core_forceidle_count++;
 				if (!fi_before)
@@ -5133,9 +5146,9 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		}
 	}
 
-	if (schedstat_enabled() && rq->core->core_forceidle_count) {
-		rq->core->core_forceidle_start = rq_clock(rq->core);
-		rq->core->core_forceidle_occupation = occ;
+	if (schedstat_enabled() && rq->core->core_sibidle_count) {
+		rq->core->core_sibidle_start = rq_clock(rq->core);
+		rq->core->core_sibidle_occupation = occ;
 	}
 
 	rq->core->core_pick_seq = rq->core->core_task_seq;
@@ -5177,7 +5190,8 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		if (!(fi_before && rq->core->core_forceidle_count))
 			task_vruntime_update(rq_i, rq_i->core_pick, !!rq->core->core_forceidle_count);
 
-		rq_i->core_pick->core_occupation = occ;
+		if (rq->core->core_forceidle_count)
+			rq_i->core_pick->core_occupation = occ;
 
 		if (i == cpu) {
 			rq_i->core_pick = NULL;
@@ -5397,14 +5411,15 @@ static void sched_core_cpu_deactivate(unsigned int cpu)
 	core_rq->core_cookie               = rq->core_cookie;
 	core_rq->core_forceidle_count      = rq->core_forceidle_count;
 	core_rq->core_forceidle_seq        = rq->core_forceidle_seq;
-	core_rq->core_forceidle_occupation = rq->core_forceidle_occupation;
+	core_rq->core_sibidle_occupation   = rq->core_sibidle_occupation;
+	core_rq->core_sibidle_count        = rq->core_sibidle_count;
 
 	/*
 	 * Accounting edge for forced idle is handled in pick_next_task().
 	 * Don't need another one here, since the hotplug thread shouldn't
 	 * have a cookie.
 	 */
-	core_rq->core_forceidle_start = 0;
+	core_rq->core_sibidle_start = 0;
 
 	/* install new leader */
 	for_each_cpu(t, smt_mask) {
@@ -8305,8 +8320,9 @@ void __init sched_init(void)
 		rq->core_enabled = 0;
 		rq->core_tree = RB_ROOT;
 		rq->core_forceidle_count = 0;
-		rq->core_forceidle_occupation = 0;
-		rq->core_forceidle_start = 0;
+		rq->core_sibidle_count = 0;
+		rq->core_sibidle_occupation = 0;
+		rq->core_sibidle_start = 0;
 
 		rq->core_cookie = 0UL;
 #endif

kernel/sched/cputime.c
@@ -225,25 +225,20 @@ void account_idle_time(u64 cputime)
 }
 
 
-#ifdef CONFIG_SCHED_CORE
+#if defined(CONFIG_SCHED_ACPU) || defined(CONFIG_SCHED_ACPU)
 /*
- * Account for forceidle time due to core scheduling.
+ * Account for sibidle, and for forceidle time due to core scheduling.
  *
  * REQUIRES: schedstat is enabled.
  */
-void __account_forceidle_time(struct task_struct *p, u64 delta)
-{
-	__schedstat_add(p->se.statistics.core_forceidle_sum, delta);
-
-	task_group_account_field(p, CPUTIME_FORCEIDLE, delta);
-}
-#endif
-#ifdef CONFIG_SCHED_ACPU
-void __account_sibidle_time(struct task_struct *p, u64 delta)
+void __account_sibidle_time(struct task_struct *p, u64 delta, bool fi)
 {
 	__schedstat_add(p->se.statistics.core_sibidle_sum, delta);
-
 	task_group_account_field(p, CPUTIME_SIBIDLE, delta);
+	if (fi) {
+		__schedstat_add(p->se.statistics.core_forceidle_sum, delta);
+		task_group_account_field(p, CPUTIME_FORCEIDLE, delta);
+	}
 }
 #endif
 

kernel/sched/sched.h
@@ -1308,15 +1308,11 @@ struct rq {
 	unsigned long		core_cookie;
 	unsigned int		core_forceidle_count;
 	unsigned int		core_forceidle_seq;
-	unsigned int		core_forceidle_occupation;
-	u64			core_forceidle_start;
+	unsigned int		core_sibidle_occupation;
+	u64			core_sibidle_start;
 	unsigned int		core_id;
-	unsigned int		core_realidle_count;
-	unsigned int		core_realidle_occupation;
-	u64			core_realidle_start;
-	u64			rq_realidle_time;
+	unsigned int		core_sibidle_count;
 	bool			in_forceidle;
-	bool			in_realidle;
 	struct task_struct	*force_idled_core_pick;
 #endif
 
@@ -2103,12 +2099,12 @@ static inline void flush_smp_call_function_from_idle(void) { }
 
 #if defined(CONFIG_SCHED_CORE) && defined(CONFIG_SCHEDSTATS)
 
-extern void __sched_core_account_forceidle(struct rq *rq);
+extern void __sched_core_account_sibidle(struct rq *rq);
 
-static inline void sched_core_account_forceidle(struct rq *rq)
+static inline void sched_core_account_sibidle(struct rq *rq)
 {
 	if (schedstat_enabled())
-		__sched_core_account_forceidle(rq);
+		__sched_core_account_sibidle(rq);
 }
 
 extern void __sched_core_tick(struct rq *rq);
@@ -2121,7 +2117,7 @@ static inline void sched_core_tick(struct rq *rq)
 
 #else
 
-static inline void sched_core_account_forceidle(struct rq *rq) {}
+static inline void sched_core_account_sibidle(struct rq *rq) {}
 
 static inline void sched_core_tick(struct rq *rq) {}
 

commit d897f068a2fcb0361c8ace2ab739ae6be7b8dc44

anolis: sched/core: fix warning when occ is zero when account sibidle
ANBZ: #6883

If the cookie of the core is not zero before, and the next tasks of
core are all idle, core_sibidle_occupation will be zero, and
core_sibidle_count is not zero, which will cause warning.

To fix this problem, we just goto out when core_sibidle_occupation is
zero.

By the way, fix the comment about forceidle and sibidle in function
__sched_core_account_sibidle().

Fixes: ab07564f5416("anolis: sched: account sibidle for core scheduling")

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/core_sched.c
@@ -314,7 +314,12 @@ void __sched_core_account_sibidle(struct rq *rq)
 
 	WARN_ON_ONCE(!rq->core->core_sibidle_count);
 
-	if (rq->core->core_sibidle_start == 0)
+	/* can't be forced idle without a running task */
+	WARN_ON_ONCE(!rq->core->core_sibidle_occupation &&
+		     rq->core->core_forceidle_count);
+
+	if (rq->core->core_sibidle_start == 0 ||
+	    rq->core->core_sibidle_occupation == 0)
 		goto out;
 
 	delta = now - rq->core->core_sibidle_start;
@@ -323,10 +328,8 @@ void __sched_core_account_sibidle(struct rq *rq)
 
 	rq->core->core_sibidle_start = now;
 
-	if (WARN_ON_ONCE(!rq->core->core_sibidle_occupation)) {
-		/* can't be forced idle without a running task */
-	} else if (rq->core->core_sibidle_count > 1 ||
-		   rq->core->core_sibidle_occupation > 1) {
+	if (rq->core->core_sibidle_count > 1 ||
+	    rq->core->core_sibidle_occupation > 1) {
 		/*
 		 * For larger SMT configurations, we need to scale the charged
 		 * forced idle amount since there can be more than one forced
@@ -344,7 +347,7 @@ void __sched_core_account_sibidle(struct rq *rq)
 			continue;
 
 		/*
-		 * Note: this will account forceidle to the current cpu, even
+		 * Note: this will account sibidle to the current cpu, even
 		 * if it comes from our SMT sibling.
 		 */
 		__account_sibidle_time(p, delta, !!rq->core->core_forceidle_count);