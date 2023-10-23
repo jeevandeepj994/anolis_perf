jeevandeepj994/anolis_perf

commit 7b8bfa4f9804124900a019a3ebfdbbf9cbdb0fae

anolis: net/smc: Use diff TCP EXPR MAGIC to avoid network middleware do simple echo
ANBZ: #6257

We found some network middleware will echo unknows TCP options, which
will confuse SMC client.

Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2097

net/smc/smc_sysctl.c
@@ -86,6 +86,14 @@ static struct ctl_table smc_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_douintvec,
 	},
+	{
+		.procname	= "experiment_syn_smc",
+		.data		= &init_net.smc.sysctl_experiment_syn_smc,
+		.maxlen		= sizeof(init_net.smc.sysctl_experiment_syn_smc),
+		.proc_handler   = proc_dointvec_minmax,
+		.extra1         = SYSCTL_ZERO,
+		.extra2         = SYSCTL_ONE,
+	},
 	{  }
 };
 
@@ -118,6 +126,8 @@ int __net_init smc_sysctl_net_init(struct net *net)
 	net->smc.sysctl_tcp2smc = 0;
 	/* enable handshake limitation by default */
 	net->smc.limit_smc_hs = 1;
+	/* enable experiment_syn_smc by default */
+	net->smc.sysctl_experiment_syn_smc = 1;
 	return 0;
 
 err_reg:

include/net/netns/smc.h
@@ -28,8 +28,8 @@ struct netns_smc {
 	int				sysctl_wmem;
 	int				sysctl_rmem;
 	int				sysctl_tcp2smc;
-
-	CK_KABI_RESERVE(1)
+	/* use diff TCP experiment magic code */
+	CK_KABI_USE_SPLIT(1, unsigned int sysctl_experiment_syn_smc)
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)
 	CK_KABI_RESERVE(4)
@@ -61,5 +61,6 @@ struct netns_smc {
 	CK_KABI_RESERVE(30)
 	CK_KABI_RESERVE(31)
 	CK_KABI_RESERVE(32)
+
 };
 #endif

include/net/tcp.h
@@ -196,6 +196,8 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
  */
 #define TCPOPT_FASTOPEN_MAGIC	0xF989
 #define TCPOPT_SMC_MAGIC	0xE2D4C3D9
+/* "SMCO" in EBCDIC encoding */
+#define TCPOPT_SMC_OK_MAGIC	0xE2D4C3D6
 
 /*
  *     TCP option lengths

net/ipv4/tcp_input.c
@@ -3912,15 +3912,26 @@ static void tcp_parse_fastopen_option(int len, const unsigned char *cookie,
 static bool smc_parse_options(const struct tcphdr *th,
 			      struct tcp_options_received *opt_rx,
 			      const unsigned char *ptr,
+			      const struct net *net,
 			      int opsize)
 {
 #if IS_ENABLED(CONFIG_SMC)
 	if (static_branch_unlikely(&tcp_have_smc)) {
 		if (th->syn && !(opsize & 1) &&
-		    opsize >= TCPOLEN_EXP_SMC_BASE &&
-		    get_unaligned_be32(ptr) == TCPOPT_SMC_MAGIC) {
-			opt_rx->smc_ok = 1;
-			return true;
+		    opsize >= TCPOLEN_EXP_SMC_BASE) {
+			/* syn ack */
+			if (th->ack && net->smc.sysctl_experiment_syn_smc) {
+				if (get_unaligned_be32(ptr) == TCPOPT_SMC_OK_MAGIC) {
+					opt_rx->smc_ok = 1;
+					return true;
+				}
+				return false;
+			}
+			/* syn only */
+			if (get_unaligned_be32(ptr) == TCPOPT_SMC_MAGIC) {
+				opt_rx->smc_ok = 1;
+				return true;
+			}
 		}
 	}
 #endif
@@ -4083,7 +4094,7 @@ void tcp_parse_options(const struct net *net,
 					break;
 				}
 
-				if (smc_parse_options(th, opt_rx, ptr, opsize))
+				if (smc_parse_options(th, opt_rx, ptr, net, opsize))
 					break;
 
 				opt_rx->saw_unknown = 1;

net/ipv4/tcp_output.c
@@ -417,6 +417,7 @@ static inline bool tcp_urg_mode(const struct tcp_sock *tp)
 #define OPTION_FAST_OPEN_COOKIE	(1 << 8)
 #define OPTION_SMC		(1 << 9)
 #define OPTION_MPTCP		(1 << 10)
+#define OPTION_SMC_OK		BIT(11)
 
 static void smc_options_write(__be32 *ptr, u16 *options)
 {
@@ -428,6 +429,12 @@ static void smc_options_write(__be32 *ptr, u16 *options)
 				       (TCPOPT_EXP <<  8) |
 				       (TCPOLEN_EXP_SMC_BASE));
 			*ptr++ = htonl(TCPOPT_SMC_MAGIC);
+		} else if (OPTION_SMC_OK & *options) {
+			*ptr++ = htonl((TCPOPT_NOP  << 24) |
+				       (TCPOPT_NOP  << 16) |
+				       (TCPOPT_EXP <<  8) |
+				       (TCPOLEN_EXP_SMC_BASE));
+			*ptr++ = htonl(TCPOPT_SMC_OK_MAGIC);
 		}
 	}
 #endif
@@ -727,10 +734,15 @@ static void smc_set_option_cond(const struct tcp_sock *tp,
 				unsigned int *remaining)
 {
 #if IS_ENABLED(CONFIG_SMC)
+	const struct sock *sk;
+
+	sk = &tp->inet_conn.icsk_inet.sk;
+
 	if (static_branch_unlikely(&tcp_have_smc)) {
 		if (tp->syn_smc && ireq->smc_ok) {
 			if (*remaining >= TCPOLEN_EXP_SMC_BASE_ALIGNED) {
-				opts->options |= OPTION_SMC;
+				opts->options |= sock_net(sk)->smc.sysctl_experiment_syn_smc ?
+					OPTION_SMC_OK : OPTION_SMC;
 				*remaining -= TCPOLEN_EXP_SMC_BASE_ALIGNED;
 			}
 		}

commit b38e4c1cf99d0f6c9976bc92a78728ca7d503dfe

anolis: fix invisible experiment_syn_smc
ANBZ: #7041

Add mode as 0644 explicitly to make experiment_syn_smc
visible to userspace, otherwise it can not be changed
by user.

Fixes: 7b8bfa4f9804 ("anolis: net/smc: Use diff TCP EXPR MAGIC to avoid network middleware do simple echo")
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>

net/smc/smc_sysctl.c
@@ -90,6 +90,7 @@ static struct ctl_table smc_table[] = {
 		.procname	= "experiment_syn_smc",
 		.data		= &init_net.smc.sysctl_experiment_syn_smc,
 		.maxlen		= sizeof(init_net.smc.sysctl_experiment_syn_smc),
+		.mode		= 0644,
 		.proc_handler   = proc_dointvec_minmax,
 		.extra1         = SYSCTL_ZERO,
 		.extra2         = SYSCTL_ONE,