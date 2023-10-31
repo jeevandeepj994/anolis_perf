jeevandeepj994/anolis_perf

commit 4031fe7bdb2535646d75a7388577910a0693769d

anolis: sched/core: introduce ht-aware-quota
ANBZ: #6729

With acpu accounting, we are able to assess how long the task is running
with sibling idle and how long with sibling busy. To make the computing
power of tasks stable, we need the tasks to execute a similar number of
instructions in each scheduling cycle.

To achieve this goal, we introduce ht-aware-quota. When task is running
with sibling idle, we consider the task to have execute more
instructions, with a certain ratio, and the sibling idle time * ratio
will be account its cfs_rq_runtime, not just siblind idle time. The
ratio can be configured from /sys/fs/cgroup/<cgroup>/cpu.ht_ratio,
unit: percentage, range: [100, 200], default: 100.

As for now, ht-aware-quota is only valid for cookie'd tasks, as when the
sibling is busy, we know what task is running. And sched_feat
SCHED_CORE_HT_AWARE_QUOTA is required to be enable.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2260

kernel/sched/fair.c
@@ -13054,6 +13054,25 @@ static int task_is_throttled_fair(struct task_struct *p, int cpu)
 #endif
 	return throttled_hierarchy(cfs_rq);
 }
+
+void account_ht_aware_quota(struct task_struct *p, u64 delta)
+{
+	struct sched_entity *se;
+	unsigned int ht_ratio;
+	struct cfs_rq *cfs_rq;
+
+	/* We only account ht_aware_quota for cookied task. */
+	if (sched_feat(SCHED_CORE_HT_AWARE_QUOTA) && p->core_cookie) {
+		se = &p->se;
+		cfs_rq = task_cfs_rq(p);
+
+		if (se->parent) {
+			ht_ratio = se->parent->ht_ratio;
+			if (ht_ratio >= 100 && ht_ratio <= 200)
+				account_cfs_rq_runtime(cfs_rq, delta * (ht_ratio - 100) / 100);
+		}
+	}
+}
 #else
 static inline void task_tick_core(struct rq *rq, struct task_struct *curr) {}
 #endif
@@ -13487,6 +13506,9 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 		init_cfs_rq(cfs_rq);
 		init_tg_cfs_entry(tg, cfs_rq, se, i, parent->se[i]);
 		init_entity_runnable_average(se);
+#ifdef CONFIG_SCHED_CORE
+		se->ht_ratio = 100;
+#endif
 	}
 
 	return 1;

include/linux/sched.h
@@ -579,7 +579,7 @@ struct sched_entity {
 
 #ifdef CONFIG_SCHED_CORE
 	u64				core_vruntime;
-	unsigned int			ht_aware_quota_coefficient;
+	unsigned int			ht_ratio;
 #endif
 
 	CK_KABI_RESERVE(1)

kernel/sched/core.c
@@ -8604,6 +8604,9 @@ struct task_group *sched_create_group(struct task_group *parent)
 
 	alloc_uclamp_sched_group(tg, parent);
 
+#ifdef CONFIG_SCHED_CORE
+	tg->ht_ratio = 100;
+#endif
 	return tg;
 
 err:
@@ -9534,6 +9537,38 @@ static s64 cpu_identity_read_s64(struct cgroup_subsys_state *css,
 }
 #endif
 
+#ifdef CONFIG_SCHED_CORE
+static int cpu_ht_ratio_write(struct cgroup_subsys_state *css,
+			      struct cftype *cftype, u64 ht_ratio)
+{
+	struct task_group *tg = css_tg(css);
+	int cpu;
+
+	if (ht_ratio < 100 || ht_ratio > 200)
+		return -1;
+
+	if (tg == &root_task_group)
+		return -1;
+
+	tg->ht_ratio = ht_ratio;
+	for_each_online_cpu(cpu) {
+		struct sched_entity *se = tg->se[cpu];
+
+		se->ht_ratio = ht_ratio;
+	}
+
+	return 0;
+}
+
+static u64 cpu_ht_ratio_read(struct cgroup_subsys_state *css,
+					       struct cftype *cft)
+{
+	struct task_group *tg = css_tg(css);
+
+	return tg->ht_ratio;
+}
+#endif
+
 static struct cftype cpu_legacy_files[] = {
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	{
@@ -9640,6 +9675,13 @@ static struct cftype cpu_legacy_files[] = {
 		.name = "ht_stable",
 		.read_u64 = cpu_ht_stable_read_u64,
 		.write_u64 = cpu_ht_stable_write_u64,
+	},
+#endif
+#ifdef CONFIG_SCHED_CORE
+	{
+		.name = "ht_ratio",
+		.read_u64 = cpu_ht_ratio_read,
+		.write_u64 = cpu_ht_ratio_write,
 	},
 #endif
 	{ }	/* Terminate */
@@ -10203,6 +10245,13 @@ static struct cftype cpu_files[] = {
 		.private = SCHED_LAT_IOBLOCK,
 		.write_u64 = sched_lat_stat_write,
 		.seq_show = sched_lat_stat_show
+	},
+#endif
+#ifdef CONFIG_SCHED_CORE
+	{
+		.name = "ht_ratio",
+		.read_u64 = cpu_ht_ratio_read,
+		.write_u64 = cpu_ht_ratio_write,
 	},
 #endif
 	{ }	/* terminate */

kernel/sched/core_sched.c
@@ -350,6 +350,7 @@ void __sched_core_account_sibidle(struct rq *rq)
 		 * if it comes from our SMT sibling.
 		 */
 		__account_sibidle_time(p, delta, !!rq->core->core_forceidle_count);
+		account_ht_aware_quota(p, delta);
 	}
 
 out:

kernel/sched/features.h
@@ -106,3 +106,7 @@ SCHED_FEAT(ID_LAST_HIGHCLASS_STAY, false)
 SCHED_FEAT(ID_LOOSE_EXPEL, false)
 SCHED_FEAT(ID_EXPELLER_SHARE_CORE, true)
 #endif
+
+#ifdef CONFIG_SCHED_CORE
+SCHED_FEAT(SCHED_CORE_HT_AWARE_QUOTA, false)
+#endif

kernel/sched/sched.h
@@ -545,6 +545,9 @@ struct task_group {
 	struct sched_cgroup_lat_stat_cpu __percpu *lat_stat_cpu;
 #endif
 
+#ifdef CONFIG_SCHED_CORE
+	unsigned int		ht_ratio;
+#endif
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)
@@ -1486,6 +1489,7 @@ extern void sched_core_dequeue(struct rq *rq, struct task_struct *p, int flags);
 extern void sched_core_get(void);
 extern void sched_core_put(void);
 
+extern void account_ht_aware_quota(struct task_struct *p, u64 delta);
 #else /* !CONFIG_SCHED_CORE */
 
 static inline bool sched_core_enabled(struct rq *rq)

commit c9172c31ec4d7d8b2a26d286f6f644f9b4ab1399

anolis: sched: don't init se->ht_ratio when alloc fair group
ANBZ: #6981

When init fair group, se->ht_ratio need to be set to 100, and will
cause perfermonce regression because of cacheline alignment. To
avoid this problem, we cancel the initialization of se->ht_ratio,
and check whether it's legality when read, if not, copy it from
task group.

Fixes: 4031fe7bdb25 ("anolis: sched/core: introduce ht-aware-quota")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/fair.c
@@ -13060,6 +13060,7 @@ void account_ht_aware_quota(struct task_struct *p, u64 delta)
 	struct sched_entity *se;
 	unsigned int ht_ratio;
 	struct cfs_rq *cfs_rq;
+	struct task_group *tg;
 
 	/* We only account ht_aware_quota for cookied task. */
 	if (sched_feat(SCHED_CORE_HT_AWARE_QUOTA) && p->core_cookie) {
@@ -13068,7 +13069,19 @@ void account_ht_aware_quota(struct task_struct *p, u64 delta)
 
 		if (se->parent) {
 			ht_ratio = se->parent->ht_ratio;
-			if (ht_ratio >= 100 && ht_ratio <= 200)
+			/* Copy ht_ratio from tg when use. */
+			if (unlikely(ht_ratio < 100 || ht_ratio > 200)) {
+				tg = cfs_rq->tg;
+				ht_ratio = tg->ht_ratio;
+
+				if (likely(ht_ratio >= 100 && ht_ratio <= 200))
+					se->parent->ht_ratio = ht_ratio;
+				else
+					return;
+
+			}
+
+			if (ht_ratio != 100)
 				account_cfs_rq_runtime(cfs_rq, delta * (ht_ratio - 100) / 100);
 		}
 	}
@@ -13506,9 +13519,6 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 		init_cfs_rq(cfs_rq);
 		init_tg_cfs_entry(tg, cfs_rq, se, i, parent->se[i]);
 		init_entity_runnable_average(se);
-#ifdef CONFIG_SCHED_CORE
-		se->ht_ratio = 100;
-#endif
 	}
 
 	return 1;