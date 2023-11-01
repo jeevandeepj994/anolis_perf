jeevandeepj994/anolis_perf

commit f3b29e8ae1e8c2748721c4b369af457ea141e631

anolis: net/smc: protect connection state transitions in listen work
ANBZ: #5791

Consider the following scenario:

				smc_close_passive_work
smc_listen_out_connected
				lock_sock()
if (state  == SMC_INIT)
				if (state  == SMC_INIT)
					state = SMC_APPCLOSEWAIT1;
	state = SMC_ACTIVE
				release_sock()

This would cause the state machine of the connection to be corrupted,
this patch solves this problem by adding a lock on modification.

Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1894

net/smc/af_smc.c
@@ -2190,8 +2190,10 @@ static void smc_listen_out_connected(struct smc_sock *new_smc)
 {
 	struct sock *newsmcsk = &new_smc->sk;
 
+	lock_sock(newsmcsk);
 	if (smc_sk_state(newsmcsk) == SMC_INIT)
 		smc_sk_set_state(newsmcsk, SMC_ACTIVE);
+	release_sock(newsmcsk);
 
 	smc_listen_out(new_smc);
 }
@@ -2203,9 +2205,13 @@ static void smc_listen_out_err(struct smc_sock *new_smc)
 	struct net *net = sock_net(newsmcsk);
 
 	this_cpu_inc(net->smc.smc_stats->srv_hshake_err_cnt);
-	if (smc_sk_state(newsmcsk) == SMC_INIT)
+
+	lock_sock(newsmcsk);
+	if (smc_sk_state(newsmcsk) == SMC_INIT) {
 		sock_put(&new_smc->sk); /* passive closing */
-	smc_sk_set_state(newsmcsk, SMC_CLOSED);
+		smc_sk_set_state(newsmcsk, SMC_CLOSED);
+	}
+	release_sock(newsmcsk);
 
 	smc_listen_out(new_smc);
 }

commit fb98c1b5c51f4ca0c500492d709d40ba67942ca4

anolis: net/smc: force SMC_CLOSED after smc_listen_out_err
ANBZ: #7110

Keep state transitions the same as upstream, that is
make state awlays to be state SMC_CLOSED. It will prevert
userspace call sendmsg() in advance.

Fixes: f3b29e8ae1("anolis: net/smc: protect connection state transitions in listen work")
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>

net/smc/af_smc.c
@@ -2233,10 +2233,9 @@ static void smc_listen_out_err(struct smc_sock *new_smc)
 	this_cpu_inc(net->smc.smc_stats->srv_hshake_err_cnt);
 
 	lock_sock(newsmcsk);
-	if (smc_sk_state(newsmcsk) == SMC_INIT) {
+	if (smc_sk_state(newsmcsk) != SMC_CLOSED)
 		sock_put(&new_smc->sk); /* passive closing */
-		smc_sk_set_state(newsmcsk, SMC_CLOSED);
-	}
+	smc_sk_set_state(newsmcsk, SMC_CLOSED);
 	release_sock(newsmcsk);
 
 	smc_listen_out(new_smc);