jeevandeepj994/anolis_perf

commit 22f7ee8314a97e85a05433f49ee52bd06fce05e9

anolis: sched/core: introduce PR_SCHED_CORE_CLEAR
ANBZ: #6201

Core scheduling doesn't provide an interface to clear the cookie of
the specific tasks, and once we wanna clear the cookie of a task, we
have to stop it. This patch provides the prctl interface
PR_SCHED_CORE_CLEAR to solve this problem.

API:

  prctl(PR_SCHED_CORE, PR_SCHED_CORE_CLEAR, tgtpid, pidtype, &cookie)

where 'tgtpid == 0' implies the current process and pidtype is kernel
enum pid_type {PIDTYPE_PID, PIDTYPE_TGID, PIDTYPE_PGID, ...}.

The value of PR_SCHED_CORE_CLEAR is 0x1000, to avoid conflict with new
command of upstream.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Reviewed-by: Yi Tao <escape@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2243

tools/include/uapi/linux/prctl.h
@@ -253,7 +253,9 @@ struct prctl_mm_map {
 # define PR_SCHED_CORE_CREATE		1 /* create unique core_sched cookie */
 # define PR_SCHED_CORE_SHARE_TO		2 /* push core_sched cookie to pid */
 # define PR_SCHED_CORE_SHARE_FROM	3 /* pull core_sched cookie to pid */
-# define PR_SCHED_CORE_MAX		4
+# define PR_SCHED_CORE_CLEAR		4 /* clear the cookie of pid*/
+# define PR_SCHED_CORE_CLEAR		0x1000 /* clear the cookie of pid*/
+# define PR_SCHED_CORE_MAX		0x1001
 # define PR_SCHED_CORE_SCOPE_THREAD		0
 # define PR_SCHED_CORE_SCOPE_THREAD_GROUP	1
 # define PR_SCHED_CORE_SCOPE_PROCESS_GROUP	2

include/uapi/linux/prctl.h
@@ -253,7 +253,8 @@ struct prctl_mm_map {
 # define PR_SCHED_CORE_CREATE		1 /* create unique core_sched cookie */
 # define PR_SCHED_CORE_SHARE_TO		2 /* push core_sched cookie to pid */
 # define PR_SCHED_CORE_SHARE_FROM	3 /* pull core_sched cookie to pid */
-# define PR_SCHED_CORE_MAX		4
+# define PR_SCHED_CORE_CLEAR		0x1000 /* clear the cookie of pid*/
+# define PR_SCHED_CORE_MAX		0x1001
 # define PR_SCHED_CORE_SCOPE_THREAD		0
 # define PR_SCHED_CORE_SCOPE_THREAD_GROUP	1
 # define PR_SCHED_CORE_SCOPE_PROCESS_GROUP	2

kernel/sched/core_sched.c
@@ -148,6 +148,9 @@ int sched_core_share_pid(unsigned int cmd, pid_t pid, enum pid_type type,
 	    (cmd != PR_SCHED_CORE_GET && uaddr))
 		return -EINVAL;
 
+	if (cmd > PR_SCHED_CORE_SHARE_FROM && cmd < PR_SCHED_CORE_CLEAR)
+		return -EINVAL;
+
 	rcu_read_lock();
 	if (pid == 0) {
 		task = current;
@@ -204,6 +207,10 @@ int sched_core_share_pid(unsigned int cmd, pid_t pid, enum pid_type type,
 		cookie = sched_core_clone_cookie(task);
 		__sched_core_set(current, cookie);
 		goto out;
+
+	case PR_SCHED_CORE_CLEAR:
+		cookie = 0;
+		break;
 
 	default:
 		err = -EINVAL;

commit 1bb649afe3b638a8d3e7744709baa502f80bb6c3

anolis: sched: fix duplicate define of PR_SCHED_CORE_CLEAR
ANBZ: #6201

PR_SCHED_CORE_CLEAR is defined twice in tools/include/upai/linux/prctl.h,
and this patch is to fix this problem.

Fixes: 22f7ee8314a9 ("anolis: sched/core: introduce PR_SCHED_CORE_CLEAR")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

tools/include/uapi/linux/prctl.h
@@ -253,7 +253,6 @@ struct prctl_mm_map {
 # define PR_SCHED_CORE_CREATE		1 /* create unique core_sched cookie */
 # define PR_SCHED_CORE_SHARE_TO		2 /* push core_sched cookie to pid */
 # define PR_SCHED_CORE_SHARE_FROM	3 /* pull core_sched cookie to pid */
-# define PR_SCHED_CORE_CLEAR		4 /* clear the cookie of pid*/
 # define PR_SCHED_CORE_CLEAR		0x1000 /* clear the cookie of pid*/
 # define PR_SCHED_CORE_MAX		0x1001
 # define PR_SCHED_CORE_SCOPE_THREAD		0