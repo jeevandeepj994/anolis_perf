7ea638598253ad58ab6b1d9167ae99e16eb0112c
Kaihao Bai <carlo.bai@linux.alibaba.com>
2023-11-16 11:46:16 +0800

    anolis: arm64: replace can_set_direct_map by splitting linear mapping
    ANBZ: #8540
    
    Currently, can_set_direct_map has no callers any more, replace it
    with another function to indicate the linear region can be split.
    
    Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2885

--- arch/arm64/mm/pageattr.c
+++ arch/arm64/mm/pageattr.c
@@ -20,19 +20,6 @@ struct page_change_data {
 
 bool rodata_full __ro_after_init = IS_ENABLED(CONFIG_RODATA_FULL_DEFAULT_ENABLED);
 
-bool can_set_direct_map(void)
-{
-	/*
-	 * rodata_full and DEBUG_PAGEALLOC require linear map to be
-	 * mapped at page granularity, so that it is possible to
-	 * protect/unprotect single pages.
-	 *
-	 * KFENCE pool requires page-granular mapping if initialized late.
-	 */
-	return rodata_full || debug_pagealloc_enabled() ||
-	       arm64_kfence_can_set_direct_map();
-}
-
 /*
  * If rodata_full is enabled, the mapping of linear mapping range can also be
  * block & cont mapping, here decouples the rodata_full and debug_pagealloc.
@@ -188,8 +175,9 @@ int set_direct_map_invalid_noflush(struct page *page)
 		.clear_mask = __pgprot(PTE_VALID),
 	};
 
-	if (!can_set_direct_map())
-		return 0;
+	if (can_set_block_and_cont_map())
+		split_linear_mapping_after_init((unsigned long)page_address(page),
+						PAGE_SIZE, PAGE_KERNEL);
 
 	return apply_to_page_range(&init_mm,
 				   (unsigned long)page_address(page),
@@ -203,8 +191,9 @@ int set_direct_map_default_noflush(struct page *page)
 		.clear_mask = __pgprot(PTE_RDONLY),
 	};
 
-	if (!can_set_direct_map())
-		return 0;
+	if (can_set_block_and_cont_map())
+		split_linear_mapping_after_init((unsigned long)page_address(page),
+						PAGE_SIZE, PAGE_KERNEL);
 
 	return apply_to_page_range(&init_mm,
 				   (unsigned long)page_address(page),
@@ -214,7 +203,7 @@ int set_direct_map_default_noflush(struct page *page)
 #ifdef CONFIG_DEBUG_PAGEALLOC
 void __kernel_map_pages(struct page *page, int numpages, int enable)
 {
-	if (!can_set_direct_map())
+	if (can_set_block_and_cont_map())
 		return;
 
 	set_memory_valid((unsigned long)page_address(page), numpages, enable);
@@ -237,9 +226,6 @@ bool kernel_page_present(struct page *page)
 	pmd_t *pmdp, pmd;
 	pte_t *ptep;
 	unsigned long addr = (unsigned long)page_address(page);
-
-	if (!can_set_direct_map())
-		return true;
 
 	pgdp = pgd_offset_k(addr);
 	if (pgd_none(READ_ONCE(*pgdp)))
