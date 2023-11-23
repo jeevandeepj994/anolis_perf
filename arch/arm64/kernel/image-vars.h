jeevandeepj994/anolis_perf

commit dda408b60b40edaf1a07cd97b9d30d8b2abfc71b

KVM: arm64: Trap guest accesses to the MPAM registers
ANBZ: #3888

cherry-picked from https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git

Previously we masked the MPAM feature out of the ID registers.
Now plumb the three guest-accessible MPAM registers into KVM as trap_undef.

We can trap the MPAMy_ELx registers if ARM64_HAS_MPAM, twiddle the
bits in MPAM2_EL2 to enable this.

With MPAM v1.0 we can trap the MPAMIDR_EL1 register only if
ARM64_HAS_MPAM_HCR, with v1.1 an additional MPAM2_EL2.TIDR bit traps
MPAMIDR_EL1 on platforms that don't have MPAMHCR_EL2. Enable one of
these if either is supported. If neither is supported, the guest can
discover that the CPU has MPAM support, and how many partid etc the
host has ... but it can't influence anything, so its harmless.

Full support for the feature would only expose MPAM to the guest
if a psuedo-device has been created to describe the virt->phys partid
mapping the VMM expects. This will depend on ARM64_HAS_MPAM_HCR.

Signed-off-by: James Morse <james.morse@arm.com>
Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1092

arch/arm64/kernel/image-vars.h
@@ -88,6 +88,11 @@ KVM_NVHE_ALIAS(__icache_flags);
 KVM_NVHE_ALIAS(arm64_const_caps_ready);
 KVM_NVHE_ALIAS(cpu_hwcap_keys);
 
+/* Additional static keys for cpufeatures */
+#ifdef CONFIG_ARM64_MPAM
+KVM_NVHE_ALIAS(arm64_mpam_has_hcr);
+#endif
+
 /* Static keys which are set if a vGIC trap should be handled in hyp. */
 KVM_NVHE_ALIAS(vgic_v2_cpuif_trap);
 KVM_NVHE_ALIAS(vgic_v3_cpuif_trap);

arch/arm64/kvm/hyp/include/hyp/switch.h
@@ -23,6 +23,7 @@
 #include <asm/kvm_emulate.h>
 #include <asm/kvm_hyp.h>
 #include <asm/kvm_mmu.h>
+#include <asm/mpam.h>
 #include <asm/fpsimd.h>
 #include <asm/debug-monitors.h>
 #include <asm/processor.h>
@@ -81,6 +82,35 @@ static inline void __activate_traps_fpsimd32(struct kvm_vcpu *vcpu)
 	}
 }
 
+static inline void  __activate_traps_mpam(struct kvm_vcpu *vcpu)
+{
+	u64 r = MPAM_SYSREG_TRAP_MPAM0_EL1 | MPAM_SYSREG_TRAP_MPAM1_EL1;
+
+	if (!mpam_cpus_have_feature())
+		return;
+
+	/* trap guest access to MPAMIDR_EL1 */
+	if (mpam_cpus_have_mpam_hcr()) {
+		write_sysreg_s(MPAMHCR_TRAP_MPAMIDR, SYS_MPAMHCR_EL2);
+	} else {
+		/* From v1.1 TIDR can trap MPAMIDR, set it unconditionally */
+		r |= MPAM_SYSREG_TRAP_IDR;
+	}
+
+	write_sysreg_s(r, SYS_MPAM2_EL2);
+}
+
+static inline void __deactivate_traps_mpam(void)
+{
+	if (!mpam_cpus_have_feature())
+		return;
+
+	write_sysreg_s(0, SYS_MPAM2_EL2);
+
+	if (mpam_cpus_have_mpam_hcr())
+		write_sysreg_s(MPAMHCR_HOST_FLAGS, SYS_MPAMHCR_EL2);
+}
+
 static inline void __activate_traps_common(struct kvm_vcpu *vcpu)
 {
 	/* Trap on AArch32 cp15 c15 (impdef sysregs) accesses (EL1 or EL0) */
@@ -95,12 +125,16 @@ static inline void __activate_traps_common(struct kvm_vcpu *vcpu)
 	write_sysreg(0, pmselr_el0);
 	write_sysreg(ARMV8_PMU_USERENR_MASK, pmuserenr_el0);
 	write_sysreg(vcpu->arch.mdcr_el2, mdcr_el2);
+
+	__activate_traps_mpam(vcpu);
 }
 
 static inline void __deactivate_traps_common(void)
 {
 	write_sysreg(0, hstr_el2);
 	write_sysreg(0, pmuserenr_el0);
+
+	__deactivate_traps_mpam();
 }
 
 static inline void ___activate_traps(struct kvm_vcpu *vcpu)

arch/arm64/include/asm/kvm_arm.h
@@ -82,6 +82,8 @@
 #define HCR_HOST_NVHE_FLAGS (HCR_RW | HCR_API | HCR_APK | HCR_ATA)
 #define HCR_HOST_VHE_FLAGS (HCR_RW | HCR_TGE | HCR_E2H)
 
+#define MPAMHCR_HOST_FLAGS	0
+
 /* TCR_EL2 Registers bits */
 #define TCR_EL2_RES1		((1U << 31) | (1 << 23))
 #define TCR_EL2_TBI		(1 << 20)

arch/arm64/include/asm/mpam.h
@@ -62,7 +62,7 @@ enum mpam_enable_type {
 };
 
 /* check whether all CPUs have MPAM support */
-static inline bool mpam_cpus_have_feature(void)
+static __always_inline bool mpam_cpus_have_feature(void)
 {
 	if (IS_ENABLED(CONFIG_ARM64_MPAM))
 		return cpus_have_const_cap(ARM64_MPAM);
@@ -70,7 +70,7 @@ static inline bool mpam_cpus_have_feature(void)
 }
 
 /* check whether all CPUs have MPAM virtualisation support */
-static inline bool mpam_cpus_have_mpam_hcr(void)
+static __always_inline bool mpam_cpus_have_mpam_hcr(void)
 {
 	if (IS_ENABLED(CONFIG_ARM64_MPAM))
 		return static_branch_unlikely(&arm64_mpam_has_hcr);

arch/arm64/kvm/sys_regs.c
@@ -360,6 +360,15 @@ static bool trap_loregion(struct kvm_vcpu *vcpu,
 	return trap_raz_wi(vcpu, p, r);
 }
 
+static bool trap_mpam(struct kvm_vcpu *vcpu,
+		      struct sys_reg_params *p,
+		      const struct sys_reg_desc *r)
+{
+	kvm_inject_undefined(vcpu);
+
+	return false;
+}
+
 static bool trap_oslsr_el1(struct kvm_vcpu *vcpu,
 			   struct sys_reg_params *p,
 			   const struct sys_reg_desc *r)
@@ -1582,7 +1591,11 @@ static const struct sys_reg_desc sys_reg_descs[] = {
 	{ SYS_DESC(SYS_LOREA_EL1), trap_loregion },
 	{ SYS_DESC(SYS_LORN_EL1), trap_loregion },
 	{ SYS_DESC(SYS_LORC_EL1), trap_loregion },
+	{ SYS_DESC(SYS_MPAMIDR_EL1), trap_mpam },
 	{ SYS_DESC(SYS_LORID_EL1), trap_loregion },
+
+	{ SYS_DESC(SYS_MPAM1_EL1), trap_mpam },
+	{ SYS_DESC(SYS_MPAM0_EL1), trap_mpam },
 
 	{ SYS_DESC(SYS_VBAR_EL1), NULL, reset_val, VBAR_EL1, 0 },
 	{ SYS_DESC(SYS_DISR_EL1), NULL, reset_val, DISR_EL1, 0 },

commit 008cfd0a77d3be1b8e525a0df3035baf7fcbf312

anolis: KVM: arm64: Only access MPAM registers when MPAM is enabled
ANBZ: #7198

The commmit dda408b60b40 ("KVM: arm64: Trap guest accesses to the MPAM
registers") will access MPAM registers like MPAM2_EL2 when
mpam_cpus_have_feature() returns true. However, mpam_cpus_have_feature()
only check the MPAM bits in ID_AA64PFR0_EL1. But in some BIOS, it is also
necessary to add a check for the existence of the MPAM ACPI table.
Otherwise, access to these registers will cause the system to crash.

To fix this error, add a condition on mpam_enabled. mpam_enabled is only
true if the MPAM ACPI table exists and the host kernel enables MPAM.

Fixes: dda408b60b40 ("KVM: arm64: Trap guest accesses to the MPAM registers")
Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>

arch/arm64/kernel/image-vars.h
@@ -82,6 +82,7 @@ KVM_NVHE_ALIAS(cpu_hwcap_keys);
 /* Additional static keys for cpufeatures */
 #ifdef CONFIG_ARM64_MPAM
 KVM_NVHE_ALIAS(arm64_mpam_has_hcr);
+KVM_NVHE_ALIAS(mpam_enabled);
 #endif
 
 /* Static keys which are set if a vGIC trap should be handled in hyp. */

arch/arm64/kvm/hyp/include/hyp/switch.h
@@ -86,7 +86,7 @@ static inline void  __activate_traps_mpam(struct kvm_vcpu *vcpu)
 {
 	u64 r = MPAM_SYSREG_TRAP_MPAM0_EL1 | MPAM_SYSREG_TRAP_MPAM1_EL1;
 
-	if (!mpam_cpus_have_feature())
+	if (!mpam_cpus_have_feature() || !static_branch_likely(&mpam_enabled))
 		return;
 
 	/* trap guest access to MPAMIDR_EL1 */
@@ -102,7 +102,7 @@ static inline void  __activate_traps_mpam(struct kvm_vcpu *vcpu)
 
 static inline void __deactivate_traps_mpam(void)
 {
-	if (!mpam_cpus_have_feature())
+	if (!mpam_cpus_have_feature() || !static_branch_likely(&mpam_enabled))
 		return;
 
 	write_sysreg_s(0, SYS_MPAM2_EL2);

arch/arm64/kvm/hyp/include/hyp/sysreg-sr.h
@@ -202,7 +202,8 @@ static inline void __sysreg32_restore_state(struct kvm_vcpu *vcpu)
  */
 static inline void __mpam_guest_load(void)
 {
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature())
+	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature() &&
+	    static_branch_likely(&mpam_enabled))
 		write_sysreg_el1(read_sysreg_s(SYS_MPAM0_EL1), SYS_MPAM1);
 }
 
@@ -216,7 +217,8 @@ static inline void __mpam_guest_put(void)
 	u64 val, mask = MPAM_SYSREG_PMG_D | MPAM_SYSREG_PMG_I |
 			MPAM_SYSREG_PARTID_D | MPAM_SYSREG_PARTID_I;
 
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature()) {
+	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature() &&
+	    static_branch_likely(&mpam_enabled)) {
 		val = FIELD_GET(mask, read_sysreg_s(SYS_MPAM2_EL2));
 		write_sysreg_el1(val, SYS_MPAM1);
 	}

arch/arm64/kvm/hyp/nvhe/switch.c
@@ -165,7 +165,8 @@ static void __pmu_switch_to_host(struct kvm_cpu_context *host_ctxt)
 /* Use the host thread's partid and pmg for world switch */
 static void __mpam_copy_el1_to_el2(void)
 {
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature())
+	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature() &&
+	    static_branch_likely(&mpam_enabled))
 		write_sysreg_s(read_sysreg_s(SYS_MPAM1_EL1), SYS_MPAM2_EL2);
 }
 