72412763b2c1bc9862408a15fd2fc6b5625bf1a4
Xin Jiang <jiangxin@hygon.cn>
2023-11-24 10:54:00 +0800

    anolis: crypto: ccp: Add CCP/PSP PCI interface on Hygon hardware
    ANBZ: #7628
    
    Add CCP/PSP PCI interface on Hygon hardware.
    
    Signed-off-by: Xin Jiang <jiangxin@hygon.cn>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2464

--- drivers/crypto/ccp/sp-pci.c
+++ drivers/crypto/ccp/sp-pci.c
@@ -412,6 +412,8 @@ static const struct pci_device_id sp_pci_table[] = {
 	{ PCI_VDEVICE(HYGON, 0x1456), (kernel_ulong_t)&dev_vdata[1] },
 	{ PCI_VDEVICE(HYGON, 0x1468), (kernel_ulong_t)&dev_vdata[2] },
 	{ PCI_VDEVICE(HYGON, 0x1486), (kernel_ulong_t)&dev_vdata[6] },
+	{ PCI_VDEVICE(HYGON, 0x14b8), (kernel_ulong_t)&dev_vdata[2] },
+	{ PCI_VDEVICE(HYGON, 0x14a6), (kernel_ulong_t)&dev_vdata[6] },
 	/* Last entry must be zero */
 	{ 0, }
 };
