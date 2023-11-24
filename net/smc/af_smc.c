jeevandeepj994/anolis_perf

commit 0cd87b278f06ab3e9afa86910d35df23eab85627

anolis: net/smc: force SMC_CLOSED after smc_listen_out_err
ANBZ: #7110

Keep state transitions the same as upstream, that is
make state awlays to be state SMC_CLOSED. It will prevert
userspace call sendmsg() in advance.

Fixes: f3b29e8ae1("anolis: net/smc: protect connection state transitions in listen work")
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>
Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2398

net/smc/af_smc.c
@@ -2233,10 +2233,9 @@ static void smc_listen_out_err(struct smc_sock *new_smc)
 	this_cpu_inc(net->smc.smc_stats->srv_hshake_err_cnt);
 
 	lock_sock(newsmcsk);
-	if (smc_sk_state(newsmcsk) == SMC_INIT) {
+	if (smc_sk_state(newsmcsk) != SMC_CLOSED)
 		sock_put(&new_smc->sk); /* passive closing */
-		smc_sk_set_state(newsmcsk, SMC_CLOSED);
-	}
+	smc_sk_set_state(newsmcsk, SMC_CLOSED);
 	release_sock(newsmcsk);
 
 	smc_listen_out(new_smc);

commit d39c792cfea9c3e81fd01780335ab120effae03d

anolis: net/smc: fix UAF due to SMC_PROCESSABORT
ANBZ: #7564

When passive connection receives abort from peer, a
potential UAF would occur:

		smc_close_passive_work
			smc_close_passive_abort_received
				SMC_INIT:
					sk->sk_state = SMC_PROCESSABORT;
					sock_put(sk);

smc_listen_out_err
if (sk->sk_state != SMC_CLOSED)
	sock_put(sk);

Hence, we add an extra condition check for SMC_PROCESSABORT,
which will prevent a double put for single hold.

Fixes: 0cd87b278f06 ("anolis: net/smc: force SMC_CLOSED after smc_listen_out_err")
Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>

net/smc/af_smc.c
@@ -2239,7 +2239,8 @@ static void smc_listen_out_err(struct smc_sock *new_smc)
 	this_cpu_inc(net->smc.smc_stats->srv_hshake_err_cnt);
 
 	lock_sock(newsmcsk);
-	if (smc_sk_state(newsmcsk) != SMC_CLOSED)
+	if (smc_sk_state(newsmcsk) != SMC_CLOSED &&
+	    smc_sk_state(newsmcsk) != SMC_PROCESSABORT)
 		sock_put(&new_smc->sk); /* passive closing */
 	smc_sk_set_state(newsmcsk, SMC_CLOSED);
 	release_sock(newsmcsk);