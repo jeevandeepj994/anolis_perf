jeevandeepj994/anolis_perf

commit 33c0fc4caf936a2107b0297eea92cb76a9e72f59

anolis: sched/core: introduce core to struct cfs_rq
ANBZ: #7227

Introduce core to struct cfs_rq, indicates the corresponding cfs_rq of
rq->core.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Yi Tao <escape@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2421

kernel/sched/fair.c
@@ -13095,9 +13095,21 @@ void account_ht_aware_quota(struct task_struct *p, u64 delta)
 		}
 	}
 }
+
+static inline void sched_core_init_cfs_rq(struct task_group *tg, struct cfs_rq *cfs_rq)
+{
+#ifdef CONFIG_FAIR_GROUP_SCHED
+	struct rq *rq = rq_of(cfs_rq);
+	int core_id = rq->core_id;
+
+	cfs_rq->core = tg->cfs_rq[core_id];
+#endif
+}
 #else
 static inline void task_tick_core(struct rq *rq, struct task_struct *curr) {}
+static inline void sched_core_init_cfs_rq(struct task_group *tg, struct cfs_rq *cfs_rq) {}
 #endif
+
 
 /*
  * scheduler tick hitting a task of our scheduling class.
@@ -13527,6 +13539,7 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 
 		init_cfs_rq(cfs_rq);
 		init_tg_cfs_entry(tg, cfs_rq, se, i, parent->se[i]);
+		sched_core_init_cfs_rq(tg, cfs_rq);
 		init_entity_runnable_average(se);
 	}
 

kernel/sched/core.c
@@ -5368,6 +5368,7 @@ static void sched_core_cpu_starting(unsigned int cpu)
 		if (t == cpu) {
 			rq->core = core_rq;
 			rq->core_id = core_id;
+			rq->cfs.core = &core_rq->cfs;
 		}
 
 		WARN_ON_ONCE(rq->core != core_rq);
@@ -5429,6 +5430,7 @@ static void sched_core_cpu_deactivate(unsigned int cpu)
 		rq = cpu_rq(t);
 		rq->core = core_rq;
 		rq->core_id = core_id;
+		rq->cfs.core = &core_rq->cfs;
 	}
 
 unlock:
@@ -5442,6 +5444,7 @@ static inline void sched_core_cpu_dying(unsigned int cpu)
 	if (rq->core != rq) {
 		rq->core = rq;
 		rq->core_id = cpu;
+		rq->cfs.core = &rq->cfs;
 	}
 }
 
@@ -8330,6 +8333,7 @@ void __init sched_init(void)
 		rq->core_sibidle_occupation = 0;
 		rq->core_sibidle_start = 0;
 		rq->core_id = i;
+		rq->cfs.core = &rq->cfs;
 
 		rq->core_cookie = 0UL;
 #endif

commit 8446862456b6840aaea5ecd47fa178682d61a89b

anolis: sched/fair: fix the problem that cfs_rq->core is NULL
ANBZ: #7639

If rq->core_id is the cpu id of ht1, when we init task group,
tg->cfs_rq[ht0]->core = tg->cfs_rq[ht1] will be NULL, as tg->cfs_rq[ht1]
haven't allocated yet.

To fix this problem, we init tg->cfs_rq[i]->core after all the cfs_rqs
allocate.

Fixes: 33c0fc4caf93 ("anolis: sched/core: introduce core to struct cfs_rq")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/fair.c
@@ -13637,8 +13637,12 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 
 		init_cfs_rq(cfs_rq);
 		init_tg_cfs_entry(tg, cfs_rq, se, i, parent->se[i]);
-		sched_core_init_cfs_rq(tg, cfs_rq);
 		init_entity_runnable_average(se);
+	}
+
+	for_each_possible_cpu(i) {
+		cfs_rq = tg->cfs_rq[i];
+		sched_core_init_cfs_rq(tg, cfs_rq);
 	}
 
 	return 1;