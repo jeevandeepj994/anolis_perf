cf6b276eb65e8b4fcf41b1f02652f0102db09c77
Qiao Ma <mqaio@linux.alibaba.com>
2023-12-04 19:32:15 +0800

    anolis: configs: add scripts for update kconfigs
    ANBZ: #7687
    
    This patch add 2 new command to refresh kconfigs:
    
    1. `make dist-configs-update`, it will refresh kconfigs by follow steps:
      a. merge all splited kconfig files into the full big kconfig files.
      b. run `make ARCH=${arch} \
              CROSS_COMPILE=scripts/dummy-tools/ \
              make olddefconfig`
         to refresh them.
      c. split them into different small files again.
    
    2. `make dist-configs-move`, it is used usually in 2 scenarios:
      a. adjust the level of a specific kconfig. For example:
         `make dist-configs-move OLD=L2 C=CONFIG_FOO L=L1` to adjust the
         level of CONFIG_FOO from level-2 to level-1
      b. determine the level of a unknown new kconfig. For example, after
         refreshing kconfigs, the CONFIG_FOO may appears in UNKNOWN/ dir,
         and the command `make dist-configs-move C=CONFIG_FOO L=L1` could
         adjust it to level-0.
    
    Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Acked-by: Dust Li <dust.li@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Acked-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Acked-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Acked-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2666

---
+++ anolis/configs/scripts/update_configs.sh
@@ -0,0 +1,149 @@
+#! /bin/bash
+# SPDX-License-Identifier: GPL-2.0
+#
+# To update kconfigs.
+#
+# Copyright (C) 2023 Qiao Ma <mqaio@linux.alibaba.com>
+
+set -e
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+BASE_CONFIG_DIR=$(realpath ${SCRIPT_DIR}/..)
+TMP_DIR=${DIST_OUTPUT}/configs
+OLD_CONFIG_DIR=${TMP_DIR}/old
+NEW_CONFIG_DIR=${TMP_DIR}/new
+BACKUP_CONFIG_DIR=${BASE_CONFIG_DIR}/configs.${DIST_CONFIG_KERNEL_NAME}.old
+
+if [ "${DIST_CONFIG_KERNEL_NAME}" != "ANCK" ]; then
+    OVERRIDE_CONFIG_DIR=${BASE_CONFIG_DIR}/OVERRIDE/${DIST_CONFIG_KERNEL_NAME}
+else
+    OVERRIDE_CONFIG_DIR=${BASE_CONFIG_DIR}
+fi
+
+function log() {
+    echo $@
+}
+
+function prepare_env() {
+    rm -rf ${TMP_DIR}
+    mkdir -p ${OLD_CONFIG_DIR}
+    mkdir -p ${NEW_CONFIG_DIR}
+}
+
+function prepare_old_configs() {
+    log "collect all old configs..."
+    # generate old config files
+    python3 ${SCRIPT_DIR}/anolis_kconfig.py generate \
+            --input_dir ${BASE_CONFIG_DIR} \
+            --output_dir ${OLD_CONFIG_DIR} \
+            ${DIST_CONFIG_KERNEL_ARCHS}
+
+    for level in ${DIST_LEVELS}
+    do
+        if [ -d ${OVERRIDE_CONFIG_DIR}/${level} ]; then
+            cp -r ${OVERRIDE_CONFIG_DIR}/${level} ${OLD_CONFIG_DIR}
+        fi
+    done
+}
+
+function import_old_configs() {
+    TMP=${OLD_CONFIG_DIR}
+
+    pushd ${DIST_SRCROOT} > /dev/null
+
+    mkdir -p ${TMP}
+
+    cat ${SCRIPT_DIR}/kconfig_locations | grep -v "^#" | while IFS= read -r line
+    do
+        if [ -z "$line" ]; then
+            continue
+        fi
+        local array=($line)
+        local arch=${array[0]}
+        local name=${array[1]}
+        local config=${array[2]}
+        cp ${config} .config
+        ARCH=${arch} CROSS_COMPILE=scripts/dummy-tools/ make olddefconfig > /dev/null 2>&1
+        cp .config ${TMP}/kernel-${DIST_KERNELVERSION}-${name}-${DIST_CONFIG_KERNEL_NAME}.config
+    done
+
+    popd > /dev/null
+}
+
+function refresh_old_configs() {
+    # refresh old config files into new config files
+    log "refresh all old configs with \`make olddefconfig\`..."
+    pushd ${DIST_SRCROOT} > /dev/null
+    for arch in ${DIST_CONFIG_KERNEL_ARCHS}
+    do
+        local file_name=kernel-${DIST_KERNELVERSION}-${arch}-${DIST_CONFIG_KERNEL_NAME}.config
+        local arch=$(echo ${arch} | sed -e 's/-debug//')
+        cp -f ${OLD_CONFIG_DIR}/${file_name} ${DIST_SRCROOT}/.config
+        ARCH=${arch} CROSS_COMPILE=scripts/dummy-tools/ make olddefconfig > /dev/null 2>&1
+        cp .config ${NEW_CONFIG_DIR}/${file_name}
+    done
+    popd > /dev/null
+}
+
+function split_new_configs() {
+    # split new config files
+    echo "split new configs..."
+    local new_config_files=$(find ${NEW_CONFIG_DIR} -type f)
+    python3 ${SCRIPT_DIR}/anolis_kconfig.py split \
+            --old_top_dir ${BASE_CONFIG_DIR} \
+            --output_top_dir ${NEW_CONFIG_DIR} \
+            ${new_config_files}
+}
+
+function replace_with_new_configs() {
+    log "replace old configs with new configs...."
+
+    rm -rf ${BACKUP_CONFIG_DIR}
+    mkdir -p ${BACKUP_CONFIG_DIR}
+    mkdir -p ${OVERRIDE_CONFIG_DIR}
+    for level in ${DIST_LEVELS};
+    do
+        if [ -d ${OVERRIDE_CONFIG_DIR}/${level} ]; then
+            mv ${OVERRIDE_CONFIG_DIR}/${level} ${BACKUP_CONFIG_DIR}
+        fi
+    done
+
+    for level in ${DIST_LEVELS}
+    do
+        if [ -d ${NEW_CONFIG_DIR}/${level} ]; then
+            mv ${NEW_CONFIG_DIR}/${level} ${OVERRIDE_CONFIG_DIR}
+        fi
+    done
+}
+
+function check_configs() {
+    # check unknown config files
+    echo ""
+    echo "******************************************************************************"
+    local unknown_dir=${OVERRIDE_CONFIG_DIR}/UNKNOWN
+    if [ -d ${unknown_dir} ] && [ -n "$(ls ${unknown_dir})" ]; then
+        echo "There are some UNKNOWN level's new configs."
+        echo ""
+        ls ${unknown_dir}
+        echo ""
+        echo "Need to classify above configs manually !!!"
+        echo "See: ${unknown_dir}"
+        echo "HINT: \`make dist-configs-move\` can help you."
+        echo "eg: make dist-configs-move C=CONFIG_CAN* L=L2"
+    else
+        echo ""
+        echo "Congratulations, all configs has a determined level."
+        echo "**DO NOT FORGET** to add changelogs if any config is changed"
+        rm -rf ${BACKUP_CONFIG_DIR}
+    fi
+    echo ""
+    echo "******************************************************************************"
+    echo ""
+}
+
+prepare_env
+prepare_old_configs
+refresh_old_configs
+split_new_configs
+replace_with_new_configs
+check_configs
