3e6bd704c9d7a6e5209f33e7161e9abe70cf605a
Qiao Ma <mqaio@linux.alibaba.com>
2023-12-04 19:21:59 +0800

    anolis: configs: add basic framework for kconfig baseline operations
    ANBZ: #7687
    
    This patch add kconfig baseline basic framework, and it only supports
    following commands:
    1. `make dist-defconfig` to generate anolis_defconfig file
       for current arch.
    2. `make dist-debug-defconfig` to generate anolis-debug_defconfig file
       for current arch.
    3. `make dist-configs` to generate {anolis,anolis-debug}_defconfig for
    both x86 and arm64 arch.
    
    Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Acked-by: Dust Li <dust.li@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Acked-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Acked-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Acked-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2666

---
+++ anolis/configs/README.zh.md
@@ -0,0 +1,87 @@
+# ANCK config说明
+# 为什么会有 ANCK kconfig 基线
+## 管理较乱
+原来的 ANCK kconfig 管理较乱，出现了许多问题。比如：
+
+1. 以为打开了，实际没打开的情况
+比如 [CONFIG_NUMA_AWARE_SPINLOCKS](https://gitee.com/anolis/cloud-kernel/pulls/535) 和 [CONFIG_CK_KABI_SIZE_ALIGN_CHECKS](https://gitee.com/anolis/cloud-kernel/pulls/1627)，虽然修改了 anolis_defconfig 文件，但是由于依赖不满足，实际上未成功开启。
+
+2. 在 Kconfig 中新增了 kconfig ，但是没有更新 anolis_defconfig 文件
+比如 [CONFIG_VTOA](https://gitee.com/anolis/cloud-kernel/pulls/1749) 和 [CONFIG_SCHED_ACPU](https://gitee.com/anolis/cloud-kernel/pulls/2260)
+
+3. kconfig依赖错误
+比如 [CONFIG_YITIAN_CPER_RAWDATA](https://gitee.com/anolis/cloud-kernel/pulls/2046)，仅与 arm64 arch 相关，但出现在了 x86 的 anolis_defconfig 中。
+
+4. 更新config配置时，未同时更新 anolis_defconfig 和 anolis_debug-defconfig
+比如：[CONFIG_KVM_INTEL_TDX](https://gitee.com/anolis/cloud-kernel/pulls/818) 和 [CONFIG_AMD_PTDMA](https://gitee.com/anolis/cloud-kernel/pulls/288)
+
+5. 重要config被错误修改，导致严重的性能问题
+比如 [CONFIG_ARM64_TLB_RANGE 和 CONFIG_ARM64_PTR_AUTH](https://gitee.com/anolis/cloud-kernel/pulls/1960)
+
+## 变更难以追溯
+之前许多 kconfig 的变更没有及时记录下来，导致在决定 kconfig 是否可以修改时需要仔细斟酌，没有可以参考的历史信息。
+通过 git 回溯信息也有困难，因为不断地刷新 anolis_defconfig 文件，导致很多 kconfig 的位置不断变化，导致需要 git blame 多次才能找到原始的 commit 。
+
+## 兼容性
+ANCK 需要将重要 kconfig 高亮出来，作为给 ANCK 下游衍生版本的参考，以保证下游衍生版本与 ANCK 的兼容性。
+
+# kconfig 目录结构说明
+
+kconfig 目录位于 $(srctree)/anolis/configs 中，共分为以下几类：
+- metadata/ ，用于存放 kconfig 的元数据信息，包括 描述信息 (description) 和 变更记录 (changelog)
+- scripts/ ，用于存放于 kconfig 有关的脚本文件
+- L*/ ，以分层方式存放 kconfig 的配置信息
+- OVERRIDE/ ，为 ANCK 衍生版提供的，用于存放覆盖 ANCK 的基础配置的目录
+
+## 分层说明
+ANCK 按照重要程度，将所有的 kconfig 划分为 4 个层级，以便标记重要的 config，为开发者修改 kconfig 时提供参考。
+### L0-MANDATORY
+最核心的 kconfig，这类 kconfig 赋予内核最基础的产品化能力，保证内核能作为一个基本的服务器操作系统进行使用。
+这类 kconfig 的变更需要十分谨慎，建议 ANCK 下游衍生版不要去 override 此类配置。
+
+入选条件：
+1. 有国家标准/行业标准背书。
+2. 对兼容性有着重要影响的 kconfig。具体而言，可分为以下几类：
+- 具有不证自明的基础能力支持，比如CONFIG_NET、CONFIG_PCI。
+- 具有广泛的通用使用场景的 kconfig。如CONFIG_NFS_FS，绝大多数服务器操作系统都支持了 nfs。
+- 被主流开源软件所广泛使用/依赖的kconfig。如CONFIG_USERFAULTFD，qemu 热迁移需要该特性。
+3. 有现实案例背书，或者有用户反馈错误配置会导致严重的功能/性能问题的 kconfig。
+
+### L1-RECOMMEND
+针对特定场景有着重要意义的 kconfig，此类 kconfig 的错误配置将导致该场景出现严重的产品化问题。
+Anolis 会站在云场景和服务器场景的角度配置 L1 层的 kconfig，下游衍生版按照可根据实际业务需求对其酌情 override。
+
+入选条件：
+1. 有重要的特定业务场景背书。注意是特定场景，如果是通用场景，请放L0。
+2. 在特定场景中因为错误配置，引发过一些事故的。
+
+### L2-OPTIONAL
+不被关注的 kconfig。
+
+此层级 kconfig 其中分为两类：
+1. 可以被手动修改的 kconfig。
+此类 kconfig 当前已配置，但是无法确定其对现有场景是否有重要意义，出于兼容性考虑，保持其不变，但将其放置到 L2。
+ANCK 认为它们的变更不会对现有使用场景造成严重影响，可以被任意打开或者关闭，比如 CONFIG_CAN、CONFIG_WIRELESS。
+下游衍生版如有需要，可随意覆盖。
+若后续发现该层级中某些 kconfig 对于某些场景非常重要，可提 PR 申请将其调整至 L1 或 L0。
+
+2. 无法被手动修改的 kconfig。
+某些 kconfig 无法被手动调整，只能通过调整其他 kconfig 时通过依赖关系自动 select。关注此类 kconfig 的意义不大，因此将其放置到 L2 中。
+典型的 kconfig，如：CONFIG_ARCH_WANT_XXX，CONFIG_HAVE_XXX
+
+入选条件：
+1. 当前已经配置了，但是说不清具体使用场景和使用价值的 kconfig
+2. 不能被手动配置，只能被自动 select 的 kconfig
+
+### OVERRIDE
+这一层级用于 ANCK 下游衍生版本自定义 kconfig 配置，常见的覆盖方式如 修改 config，新增/删除 config。这一目录下同样需要对 override 的 kconfig 定义 L0 到 L2 等级。
+
+# 子目录说明
+ANCK 目前支持 x86、arm64架构，每个架构分为标准配置和 debug 配置，其中 debug 配置用于测试，标准配置是正式上线时使用的。
+因此，每一层级中的 kconfig 的组织形式如下：
+- generic，当各个架构的标准配置都相同时，将 kconfig 存放至该目录
+- x86/x86-debug，对应 x86 架构的标准配置和 debug 配置。
+- arm64/arm64-debug，同上
+
+## 如何更新 kconfig
+请参考 How-To-Modify-Kconfig.zh.md
---
+++ anolis/configs/scripts/anolis_kconfig.py
@@ -0,0 +1,410 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+#
+# The core script for ANCK kconfig baseline
+# It is not recommended to call directly.
+#
+# Copyright (C) 2023 Qiao Ma <mqaio@linux.alibaba.com>
+
+import argparse, re, os, glob, shutil, copy
+from typing import List, Dict, Type
+import json
+
+def die(*args, **kwargs):
+    print(*args, **kwargs)
+    exit(1)
+
+class ConfigRule():
+    var_levels: List[str]
+    var_levels = None
+
+    @staticmethod
+    def __get_env(env_name: str):
+        value = os.getenv(env_name)
+        if value == None:
+            die(f"cannot find variable {env_name}")
+        return value
+
+    @staticmethod
+    def levels() -> List[str]:
+        if ConfigRule.var_levels == None:
+            ConfigRule.var_levels = ConfigRule.__get_env("DIST_LEVELS").split()
+        return ConfigRule.var_levels
+
+    @staticmethod
+    def lookup_order(arch: str) -> List[str]:
+        return {
+            "x86": ["x86", "default"],
+            "x86-debug": ["x86-debug", "x86", "default"],
+            "arm64": ["arm64", "default"],
+            "arm64-debug": ["arm64-debug", "arm64", "default"],
+        }[arch]
+
+    @staticmethod
+    def kernel_version() -> str:
+        return ConfigRule.__get_env("DIST_KERNELVERSION")
+
+    @staticmethod
+    def dist_dependencies(dist: str) -> List[str]:
+        return ConfigRule.__get_env(f"DIST_CONFIG_KERNEL_DEPENDENCIES_{dist}").split()
+
+    @staticmethod
+    def arch_list(dist: str) -> List[str]:
+        return ConfigRule.__get_env(f"DIST_CONFIG_KERNEL_ARCHS_{dist}").split()
+
+    @staticmethod
+    def default_dist() -> str:
+        return ConfigRule.__get_env("DIST_CONFIG_KERNEL_NAME")
+
+    @staticmethod
+    def is_override(dist: str) -> bool:
+        return dist != "ANCK"
+
+class ConfigValue():
+    """ store config values"""
+    def __init__(self, name: str, value: str) -> None:
+        self.name = name
+        self.value = value
+
+    @staticmethod
+    def from_text(line: str) -> Type["ConfigValue"] :
+        RE_CONFIG_SET = r'^(CONFIG_\w+)=(.*)$'
+        RE_CONFIG_NOT_SET = r'^# (CONFIG_\w+) is not set$'
+
+        if re.match(RE_CONFIG_SET, line):
+            obj = re.match(RE_CONFIG_SET, line)
+            return ConfigValue(obj.group(1), obj.group(2))
+        elif re.match(RE_CONFIG_NOT_SET, line):
+            obj = re.match(RE_CONFIG_NOT_SET, line)
+            return ConfigValue(obj.group(1), "n")
+        return None
+
+    def as_string(self) -> str:
+        if self.value == None or self.value == "n":
+            return f"# {self.name} is not set\n"
+        return f"{self.name}={self.value}\n"
+
+    def equal(self, another) -> bool:
+        return self.value == another.value
+
+    def is_empty(self) -> bool:
+        return self.value == None
+
+class ConfigValues():
+    values: List[ConfigValue]
+
+    def __init__(self) -> None:
+        self.values = []
+
+    def add_value(self, value: ConfigValue):
+        self.values.append(value)
+
+    @staticmethod
+    def from_config_file(path: str) -> Type["ConfigValues"]:
+        configs = ConfigValues()
+        with open(path) as f:
+            for line in f.readlines():
+                value = ConfigValue.from_text(line)
+                if value is None:
+                    continue
+                configs.add_value(value)
+        return configs
+
+class Config():
+    name: str
+    level: str
+    values: Dict[str, ConfigValue]
+
+    def __init__(self, name: str, level: str) -> None:
+        self.name = name
+        self.level = level
+        self.values = {}
+
+    def add_value(self, arch: str, value: str):
+        self.values[arch] = value
+
+    def get_value(self, arch: str):
+        for arch in ConfigRule.lookup_order(arch):
+            if arch in self.values:
+                return self.values[arch]
+        return None
+
+    def __collapse_value(self, full_arch_list: List[str], collapse_archs: List[str], arch_new: str = None):
+        base_value = None
+
+        # for downstream distributions, the arch like arm64 may not be supported, ignore it
+        final_archs = []
+        for arch in collapse_archs:
+            if arch in full_arch_list:
+                final_archs.append(arch)
+
+        if len(final_archs) == 0:
+            return
+
+        for arch in final_archs:
+            if arch not in self.values:
+                return
+            if base_value == None:
+                base_value = self.values[arch]
+            elif not base_value.equal(self.values[arch]):
+                return
+
+        if arch_new == None:
+            arch_new = final_archs[0]
+
+        for arch in final_archs:
+            del self.values[arch]
+
+        self.values[arch_new] = base_value
+
+    def __remove_empty_values(self, arch_list: List[str]):
+        for arch in arch_list:
+            if arch in self.values and f"{arch}-debug" not in self.values:
+                if self.values[arch].is_empty():
+                    del self.values[arch]
+
+    def collapse_values(self, all_archs: List[str]):
+        self.__collapse_value(all_archs, ["x86", "x86-debug"])
+        self.__collapse_value(all_archs, ["arm64", "arm64-debug"])
+        self.__collapse_value(all_archs, ["x86", "arm64"], "default")
+        if "default" not in self.values:
+            # Optimization for such case:
+            # CONFIG_ARM_GIC_V3 only appears in arch arm64,
+            # it is unnecessary to place related file in x86 arch,
+            # so just remove it
+            self.__remove_empty_values(["x86", "arm64"])
+        elif self.values["default"].is_empty():
+            # Optimization for such case:
+            # CONFIG_FAILSLAB does not appear in x86, arm64,
+            # it only appears in x86-debug, arm64-debug configs,
+            # which finally causes a default file exists, but has its value like:
+            # > # CONFIG_FAILSLAB is not appear
+            # which looks ugly.
+            del self.values["default"]
+
+    def expand_values(self, all_archs: List[str]):
+        new_values: Dict[str, ConfigValue] = {}
+        for arch in all_archs:
+            value = self.get_value(arch)
+            if value != None:
+                new_values[arch] = copy.deepcopy(value)
+            else:
+                new_values[arch] = ConfigValue(self.name, None)
+        self.values = new_values
+
+    def set_default_values(self, all_archs: List[str]):
+        for arch in all_archs:
+            if arch not in self.values:
+                self.values[arch] = ConfigValue(self.name, None)
+
+    def write_split_files(self, top_dir: str):
+        for arch, value in self.values.items():
+            if self.level != "UNKNOWN":
+                filename = os.path.join(top_dir, self.level, arch, self.name)
+            else:
+                filename = os.path.join(top_dir, self.level, self.name, arch, self.name)
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, "w") as f:
+                f.write(value.as_string())
+
+    def is_all_empty(self, archs: List[str]) -> bool:
+        for arch in archs:
+            if arch not in self.values:
+                continue
+            if self.values[arch].value == None:
+                continue
+            return False
+        return True
+
+    def diff_to_base(self, base: Type["Config"]) -> bool:
+        for arch,value in self.values.items():
+            if arch not in base.values:
+                continue
+            if not value.equal(base.values[arch]):
+                return True
+        return False
+
+    def as_json(self):
+        data = {
+            "name": self.name,
+            "desc": None,
+            "level": self.level
+        }
+        for arch, value in self.values.items():
+            data[arch] = value.value
+        return data
+
+    @staticmethod
+    def empty_instance(name: str, level: str):
+        conf = Config(name, level)
+        conf.add_value("default", ConfigValue(name, None))
+        return conf
+
+    def dump(self):
+        print(f"{self.name}  {self.level}")
+
+class Configs():
+    configs: Dict[str, Config]
+    dist: str
+    archs: set
+
+    def __init__(self, dist: str):
+        self.configs = {}
+        self.dist = dist
+
+    def add_value(self, value: ConfigValue, level: str, arch: str):
+        name = value.name
+        if name not in self.configs:
+            self.configs[name] = Config(name, level)
+        self.configs[name].add_value(arch, value)
+
+    def add_values(self, values: ConfigValues, level: str, arch: str):
+        for value in values.values:
+            self.add_value(value, level, arch)
+
+    def expand_values(self):
+        for config in self.configs.values():
+            config.expand_values(ConfigRule.arch_list(self.dist))
+
+    def collapse_values(self):
+        for config in self.configs.values():
+            config.collapse_values(ConfigRule.arch_list(self.dist))
+
+    def set_default_values(self):
+        for config in self.configs.values():
+            config.set_default_values(ConfigRule.arch_list(self.dist))
+
+    def level_of(self, conf_name: str) -> str:
+        if conf_name in self.configs:
+            return self.configs[conf_name].level
+        return "UNKNOWN"
+
+    def write_split_files(self, top_dir: str):
+        for config in self.configs.values():
+            config.write_split_files(top_dir)
+
+    def dump(self):
+        for config in self.configs.values():
+            config.dump()
+
+    def merge_with_override(self, override: Type["Configs"]):
+        for config in override.configs.values():
+            self.configs[config.name] = copy.deepcopy(config)
+        self.dist = override.dist
+
+    def diff_to_base(self, base_configs: Type["Configs"]):
+        same_configs: List[Config] = []
+
+        # diff from override to base
+        for config in self.configs.values():
+            if config.name not in base_configs.configs:
+                continue
+            if not config.diff_to_base(base_configs.configs[config.name]):
+                same_configs.append(config)
+
+        # diff from base to override
+        for config in base_configs.configs.values():
+            if config.name in self.configs:
+                continue
+            # avoid write files for follow case:
+            # ANCK base: x86 x86-debug are empty, but arm64 arm64-debug has values
+            # downstream: only support x86 x86-debug, and they are empty, too
+            if config.is_all_empty(ConfigRule.arch_list(self.dist)):
+                continue
+            self.configs[config.name] = Config.empty_instance(config.name, config.level)
+
+        for config in same_configs:
+            del self.configs[config.name]
+
+    def as_json(self):
+        data_list = []
+        for config in self.configs.values():
+            data_list.append(config.as_json())
+        return data_list
+
+class Merger():
+    """merge all splited files"""
+    @staticmethod
+    def __merge_from_arch_dirs(top_dir: str, level: str, configs: Configs):
+        for arch in os.listdir(top_dir):
+            arch_dir = os.path.join(top_dir, arch)
+            for conf_name in os.listdir(arch_dir):
+                conf_path = os.path.join(arch_dir, conf_name)
+                values = ConfigValues.from_config_file(conf_path)
+                configs.add_values(values, level, arch)
+
+    @staticmethod
+    def __load_configs(path: str, dist: str) -> Configs:
+        configs = Configs(dist)
+        for level in ConfigRule.levels():
+            if ConfigRule.is_override(dist):
+                level_dir = os.path.join(path, "OVERRIDE", dist, level)
+            else:
+                level_dir = os.path.join(path, level)
+            if not os.path.exists(level_dir):
+                continue
+            if level != "UNKNOWN":
+                Merger.__merge_from_arch_dirs(level_dir, level, configs)
+            else:
+                for conf in os.listdir(level_dir):
+                    Merger.__merge_from_arch_dirs(os.path.join(level_dir, conf), level, configs)
+        return configs
+
+    @staticmethod
+    def from_path(path: str, dist: str) -> Configs:
+        dist_list = ConfigRule.dist_dependencies(dist)
+        dist_list.append(dist)
+
+        configs = None
+        for dist in dist_list:
+            dist_configs = Merger.__load_configs(path, dist)
+            if configs == None:
+                configs = dist_configs
+            else:
+                configs.merge_with_override(dist_configs)
+
+        configs.expand_values()
+        return configs
+
+class Generator():
+    """generate all config files to build kernel"""
+    @staticmethod
+    def generate(configs: Configs, top_dir: str, dist: str, arch_list: List[str]):
+        kernel_version = ConfigRule.kernel_version()
+        for arch in arch_list:
+            file_name = f"kernel-{kernel_version}-{arch}-{dist}.config"
+            with open(os.path.join(top_dir, file_name), "w") as f:
+                for conf_name in sorted(configs.configs):
+                    config = configs.configs[conf_name]
+                    value = config.get_value(arch)
+                    if value is None:
+                        continue
+                    f.write(value.as_string())
+            print(f"* {file_name} generated in {top_dir}")
+
+    @staticmethod
+    def do_generate(args):
+        dist = ConfigRule.default_dist()
+        input_dir = args.input_dir
+        output_dir = args.output_dir
+        arch_list = args.archs
+
+        configs = Merger.from_path(input_dir, dist)
+        Generator.generate(configs, output_dir, dist, arch_list)
+
+def default_args_func(args):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='process configs')
+    parser.set_defaults(func=default_args_func)
+    subparsers = parser.add_subparsers()
+
+    generator = subparsers.add_parser('generate', description="generate all files")
+    generator.add_argument("--input_dir", required=True, help="the top dir of splited configs")
+    generator.add_argument("--output_dir", required=True, help="the output dir to store config files")
+    generator.add_argument("archs", nargs="+", help="the archs, eg: x86/x86-debug/arm64/arm64-debug")
+    generator.set_defaults(func=Generator.do_generate)
+
+    args = parser.parse_args()
+    args.func(args)
