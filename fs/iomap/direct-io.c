51dbaf57ba3f21c1dbaeab62af9fd59c937a806a
Christoph Hellwig <hch@lst.de>
2023-12-07 16:46:38 +0800

    iomap: rename the flags variable in __iomap_dio_rw
    ANBZ: #7740
    
    commit 5724be5de88f5f6863d44c859f42f70d5cc667ed upstream.
    
    Rename flags to iomap_flags to make the usage a little more clear.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    [ Gao Xiang: adapt ANCK 5.10. ]
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2519

--- fs/iomap/direct-io.c
+++ fs/iomap/direct-io.c
@@ -427,7 +427,7 @@ __iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 	size_t count = iov_iter_count(iter);
 	loff_t pos = iocb->ki_pos;
 	loff_t end = iocb->ki_pos + count - 1, ret = 0;
-	unsigned int flags = IOMAP_DIRECT;
+	unsigned int iomap_flags = IOMAP_DIRECT;
 	struct blk_plug plug;
 	struct iomap_dio *dio;
 
@@ -463,13 +463,13 @@ __iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 				ret = -EAGAIN;
 				goto out_free_dio;
 			}
-			flags |= IOMAP_NOWAIT;
+			iomap_flags |= IOMAP_NOWAIT;
 		}
 
 		if (iter_is_iovec(iter))
 			dio->flags |= IOMAP_DIO_DIRTY;
 	} else {
-		flags |= IOMAP_WRITE;
+		iomap_flags |= IOMAP_WRITE;
 		dio->flags |= IOMAP_DIO_WRITE;
 
 		if (iocb->ki_flags & IOCB_NOWAIT) {
@@ -477,7 +477,7 @@ __iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 				ret = -EAGAIN;
 				goto out_free_dio;
 			}
-			flags |= IOMAP_NOWAIT;
+			iomap_flags |= IOMAP_NOWAIT;
 		}
 
 		/* for data sync or sync, we need sync completion processing */
@@ -522,7 +522,7 @@ __iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 
 	blk_start_plug(&plug);
 	do {
-		ret = iomap_apply(inode, pos, count, flags, ops, dio,
+		ret = iomap_apply(inode, pos, count, iomap_flags, ops, dio,
 				iomap_dio_actor);
 		if (ret <= 0) {
 			/* magic error code to fall back to buffered I/O */
