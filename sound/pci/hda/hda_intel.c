fc6cb5c0882261548395b4e2aed00a43c077eecd
Pu Wen <puwen@hygon.cn>
2023-12-21 20:42:12 +0800

    anolis: ALSA: hda: Fix single byte write failure issue for Hygon
    ANBZ: #5455
    
    On Hygon controller, some registers such as GCTL, SD_CTL and SD_CTL_3B
    should be accessed in dword.
    
    Signed-off-by: Pu Wen <puwen@hygon.cn>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1904,6 +1904,10 @@ static int azx_first_init(struct azx *chip)
 	}
 #endif
 
+	if (chip->driver_type == AZX_DRIVER_HYGON &&
+	    chip->pci->device == PCI_DEVICE_ID_HYGON_18H_M05H_HDA)
+		bus->hygon_dword_access = 1;
+
 	err = pci_request_regions(pci, "ICH HD audio");
 	if (err < 0)
 		return err;
