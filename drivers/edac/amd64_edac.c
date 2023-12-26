jeevandeepj994/anolis_perf

commit fb926ef18387238403b5bd318f02b7663283a9f7

anolis: EDAC/amd64: Add support for Hygon family 18h model 6h
ANBZ: #5455

Add F0/F6 device IDs for Hygon family 18h model 6h processor.

Signed-off-by: Pu Wen <puwen@hygon.cn>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1717

drivers/edac/amd64_edac.c
@@ -2446,6 +2446,16 @@ static struct amd64_family_type family_types[] = {
 			.dbam_to_cs		= f17_addr_mask_to_cs_size,
 		}
 	},
+	[F18_M06H_CPUS] = {
+		.ctl_name = "F18h_M06h",
+		.f0_id = PCI_DEVICE_ID_HYGON_18H_M06H_DF_F0,
+		.f6_id = PCI_DEVICE_ID_HYGON_18H_M06H_DF_F6,
+		.max_mcs = 2,
+		.ops = {
+			.early_channel_count	= f17_early_channel_count,
+			.dbam_to_cs		= f17_addr_mask_to_cs_size,
+		}
+	},
 	[F19_CPUS] = {
 		.ctl_name = "F19h",
 		.f0_id = PCI_DEVICE_ID_AMD_19H_DF_F0,
@@ -3544,6 +3554,10 @@ static struct amd64_family_type *per_family_init(struct amd64_pvt *pvt)
 			pvt->ops = &family_types[F17_M30H_CPUS].ops;
 			family_types[F17_M30H_CPUS].max_mcs = 1;
 			family_types[F17_M30H_CPUS].ctl_name = "F18h_M05h";
+			break;
+		} else if (pvt->model == 0x6) {
+			fam_type = &family_types[F18_M06H_CPUS];
+			pvt->ops = &family_types[F18_M06H_CPUS].ops;
 			break;
 		}
 		fam_type	= &family_types[F17_CPUS];

drivers/edac/amd64_edac.h
@@ -129,6 +129,9 @@
 #define PCI_DEVICE_ID_AMD_19H_M10H_DF_F0 0x14ad
 #define PCI_DEVICE_ID_AMD_19H_M10H_DF_F6 0x14b3
 
+#define PCI_DEVICE_ID_HYGON_18H_M06H_DF_F0 0x14b0
+#define PCI_DEVICE_ID_HYGON_18H_M06H_DF_F6 0x14b6
+
 /*
  * Function 1 - Address Map
  */
@@ -302,6 +305,7 @@ enum amd_families {
 	F17_M30H_CPUS,
 	F17_M60H_CPUS,
 	F17_M70H_CPUS,
+	F18_M06H_CPUS,
 	F19_CPUS,
 	F19_M10H_CPUS,
 	NUM_FAMILIES,

commit 2c9d919127c47b3272e1704ed764a0236d67c8a6

anolis: EDAC/amd64: Adjust UMC channel for Hygon family 18h model 6h
ANBZ: #5455

Hygon family 18h model 6h has 2 cs mapped to 1 umc, so adjust for it.

Fixes: fb926ef18387 ("anolis: EDAC/amd64: Add support for Hygon family 18h model 6h")
Signed-off-by: Pu Wen <puwen@hygon.cn>

drivers/edac/amd64_edac.c
@@ -2750,6 +2750,7 @@ static void decode_umc_error(int node_id, struct mce *m)
 	struct amd64_pvt *pvt;
 	struct err_info err;
 	u64 sys_addr;
+	u8 umc;
 
 	mci = edac_mc_find(node_id);
 	if (!mci)
@@ -2780,7 +2781,12 @@ static void decode_umc_error(int node_id, struct mce *m)
 
 	err.csrow = m->synd & 0x7;
 
-	if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, err.channel, &sys_addr)) {
+	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6)
+		umc = err.channel << 1;
+	else
+		umc = err.channel;
+
+	if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc, &sys_addr)) {
 		err.err_code = ERR_NORM_ADDR;
 		goto log_error;
 	}