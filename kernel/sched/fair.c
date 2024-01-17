4facd10d2c7f8dfcfae213326b7b66165551d0f9
xianglai li <lixianglai@loongson.cn>
2024-01-17 17:01:31 +0800

    anolis: kernel/sched: Fix compile error on loongarch
    ANBZ: #7928
    
    kernel errors occur when compiling loongarch due to the combination of
    the following patch.
    commit b14670127570 ("anolis: configs:
    enable CONFIG_IRQ_TIME_ACCOUNTING for arm64")
    commit 877afe6642de ("anolis: sched:
    fix conflicts between group identity and core scheduling")
    This problem is remedied through reasonable planning of macro definition.
    
    Signed-off-by: xianglai li <lixianglai@loongson.cn>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2627
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Juxin Gao <gaojuxin@loongson.cn>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1373,6 +1373,7 @@ void group_identity_put(void)
 	atomic_dec(&group_identity_count);
 }
 
+#if defined(CONFIG_SCHED_CORE)
 bool sched_check_group_identity_lock(void)
 {
 	mutex_lock(&sched_core_gi_conflict_mutex);
@@ -1385,6 +1386,7 @@ void sched_check_group_identity_unlock(void)
 {
 	mutex_unlock(&sched_core_gi_conflict_mutex);
 }
+#endif
 
 int sched_group_identity_enable_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *lenp,
