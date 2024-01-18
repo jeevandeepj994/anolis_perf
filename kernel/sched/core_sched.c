jeevandeepj994/anolis_perf

commit 877afe6642ded81e4fffa282d22cbfeb212a60f8

anolis: sched: fix conflicts between group identity and core scheduling
ANBZ: #7882

If core scheduling and group identity are used at the same time,
pick_task_fair may get an empty sched_entity beacause task is in
underclass's cfs_rq, which result in kernel panic.

Add check when enabling group identity and core scheduling to
avoid use them together.

Signed-off-by: Yi Tao <escape@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2585

kernel/sched/core_sched.c
@@ -268,6 +268,20 @@ void clear_all_cookie(void)
 	flush_workqueue(system_wq);
 }
 
+bool sched_check_sched_core_lock(void)
+{
+	mutex_lock(&sched_core_gi_conflict_mutex);
+	if (!static_branch_unlikely(&__sysctl_sched_core_enabled))
+		return true;
+	mutex_unlock(&sched_core_gi_conflict_mutex);
+	return false;
+}
+
+void sched_check_sched_core_unlock(void)
+{
+	mutex_unlock(&sched_core_gi_conflict_mutex);
+}
+
 int sysctl_sched_core_handler(struct ctl_table *table, int write,
 				void __user *buffer, size_t *lenp, loff_t *ppos)
 {
@@ -286,7 +300,12 @@ int sysctl_sched_core_handler(struct ctl_table *table, int write,
 		return err;
 
 	if (new) {
+		if (!sched_check_group_identity_lock()) {
+			sysctl_sched_core = old;
+			return -EBUSY;
+		}
 		static_branch_enable(&__sysctl_sched_core_enabled);
+		sched_check_group_identity_unlock();
 	} else {
 		/*
 		 * When turn off sched_core, we need to clear cookie of all the tasks, to

kernel/sched/fair.c
@@ -1373,6 +1373,19 @@ void group_identity_put(void)
 	atomic_dec(&group_identity_count);
 }
 
+bool sched_check_group_identity_lock(void)
+{
+	mutex_lock(&sched_core_gi_conflict_mutex);
+	if (group_identity_disabled())
+		return true;
+	mutex_unlock(&sched_core_gi_conflict_mutex);
+	return false;
+}
+void sched_check_group_identity_unlock(void)
+{
+	mutex_unlock(&sched_core_gi_conflict_mutex);
+}
+
 int sched_group_identity_enable_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *lenp,
 		loff_t *ppos)
@@ -1388,17 +1401,24 @@ int sched_group_identity_enable_handler(struct ctl_table *table, int write,
 	}
 
 	if (atomic_read(&group_identity_count)) {
-		mutex_unlock(&identity_mutex);
-		return -EBUSY;
+		ret = -EBUSY;
+		goto out;
 	}
 	old = sysctl_sched_group_indentity_enabled;
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 	new = sysctl_sched_group_indentity_enabled;
 	if (!ret && write && (old != new)) {
-		if (new)
+		if (new) {
+			if (!sched_check_sched_core_lock()) {
+				sysctl_sched_group_indentity_enabled = old;
+				ret = -EBUSY;
+				goto out;
+			}
 			__group_identity_enable();
-		else
+			sched_check_sched_core_unlock();
+		} else {
 			__group_identity_disable();
+		}
 	}
 out:
 	mutex_unlock(&identity_mutex);

include/linux/sched.h
@@ -2351,4 +2351,17 @@ static inline void sched_core_fork(struct task_struct *p) { }
 static inline int sched_core_idle_cpu(int cpu) { return idle_cpu(cpu); }
 #endif
 
+#if defined(CONFIG_SCHED_CORE) && defined(CONFIG_GROUP_IDENTITY)
+extern struct mutex sched_core_gi_conflict_mutex;
+extern bool sched_check_group_identity_lock(void);
+extern void sched_check_group_identity_unlock(void);
+extern bool sched_check_sched_core_lock(void);
+extern void sched_check_sched_core_unlock(void);
+#else
+static inline bool sched_check_group_identity_lock(void) { return true; }
+static inline void sched_check_group_identity_unlock(void) { }
+static inline bool sched_check_sched_core_lock(void) { return true; }
+static inline void sched_check_sched_core_unlock(void) { }
+#endif
+
 #endif

kernel/sched/core.c
@@ -399,6 +399,10 @@ sched_core_dequeue(struct rq *rq, struct task_struct *p, int flags) { }
 
 #endif /* CONFIG_SCHED_CORE */
 
+#if defined(CONFIG_SCHED_CORE) && defined(CONFIG_GROUP_IDENTITY)
+DEFINE_MUTEX(sched_core_gi_conflict_mutex);
+#endif
+
 /*
  * part of the period that we allow rt tasks to run in us.
  * default: 0.95s

commit 5352b875c13b5ab00b93611106b0f7f7f967297e

anolis: sched: fix compile error if only set GI or core sched
ANBZ: #7935

fix compile error when:

CONFIG_GROUP_IDENTITY=y
CONFIG_SCHED_CORE is not set

or:
CONFIG_GROUP_IDENTITY is not set
CONFIG_SCHED_CORE=y

Fixes: 877afe6642de ("anolis: sched: fix conflicts between group identity and core")
Signed-off-by: Yi Tao <escape@linux.alibaba.com>

kernel/sched/core_sched.c
@@ -268,6 +268,7 @@ void clear_all_cookie(void)
 	flush_workqueue(system_wq);
 }
 
+#ifdef CONFIG_GROUP_IDENTITY
 bool sched_check_sched_core_lock(void)
 {
 	mutex_lock(&sched_core_gi_conflict_mutex);
@@ -281,6 +282,7 @@ void sched_check_sched_core_unlock(void)
 {
 	mutex_unlock(&sched_core_gi_conflict_mutex);
 }
+#endif
 
 int sysctl_sched_core_handler(struct ctl_table *table, int write,
 				void __user *buffer, size_t *lenp, loff_t *ppos)

kernel/sched/fair.c
@@ -1373,6 +1373,7 @@ void group_identity_put(void)
 	atomic_dec(&group_identity_count);
 }
 
+#ifdef CONFIG_SCHED_CORE
 bool sched_check_group_identity_lock(void)
 {
 	mutex_lock(&sched_core_gi_conflict_mutex);
@@ -1385,6 +1386,7 @@ void sched_check_group_identity_unlock(void)
 {
 	mutex_unlock(&sched_core_gi_conflict_mutex);
 }
+#endif
 
 int sched_group_identity_enable_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *lenp,