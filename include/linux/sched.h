jeevandeepj994/anolis_perf

commit b37e67a6c64803976efb16b23cf591264ad92c1d

ck: sched: introduce per-cgroup identity
to #34609774

In collocation environment, we deploy latency sensitive (LS) workloads
and best effort (BE) workloads on the same machine, and borrowed vritual
time(BVT) is the legacy feature to help ensure the schedule priority
for LS.

This patch is just porting the idea, however, in order to reduce the
overhead, we introduced group identity feature which based on the idea
of multiple rb-tree.

There are now two rb-tree for each cfs_rq, and tasks with special
identity will be queued into the corresponding tree, this help reduce
the complexity of implementation and obviously reduce the overhead
of following smt expeller(Noise Clean) feature.

By write particular identity into the new cpu-cgroup entry named
'group_identity', tasks of this cgroup will perform the special
schedule behavior, including:

  * ID_NORMAL
    Normal CFS tasks
  * ID_HIGHCLASS
    Tasks preempt normal & underclass tasks on wakeup,
    also consider underclass only cpu as idle.
  * ID_UNDERCLASS
    Tasks take penalty on wakeup.

And the legacy entry 'bvt_warp_ns' was reserved for compatibility, the
relationship with identity are:

  * 2 == ID_HIGHCLASS
  * 1 == ID_HIGHCLASS
  * 0 == ID_NORMAL
  * -1 == ID_UNDERCLASS
  * -2 == ID_UNDERCLASS

Signed-off-by: Michael Wang <yun.wang@linux.alibaba.com>
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Acked-by: Michael Wang <yun.wang@linux.alibaba.com>

include/linux/sched.h
@@ -449,6 +449,9 @@ struct sched_statistics {
 	u64				nr_migrations_cold;
 	u64				nr_failed_migrations_affine;
 	u64				nr_failed_migrations_running;
+#ifdef CONFIG_GROUP_IDENTITY
+	u64				nr_failed_migrations_id;
+#endif
 	u64				nr_failed_migrations_hot;
 	u64				nr_forced_migrations;
 
@@ -506,6 +509,10 @@ struct sched_entity {
 	struct cfs_rq			*my_q;
 	/* cached value of my_q->h_nr_running */
 	unsigned long			runnable_weight;
+#endif
+
+#ifdef CONFIG_GROUP_IDENTITY
+	int				id_flags;
 #endif
 
 #ifdef CONFIG_SMP

init/Kconfig
@@ -959,6 +959,22 @@ config FAIR_GROUP_SCHED
 	depends on CGROUP_SCHED
 	default CGROUP_SCHED
 
+config GROUP_IDENTITY
+	bool "Group identity for SCHED_OTHER"
+	depends on (CGROUP_SCHED && 64BIT && SMP)
+	default CGROUP_SCHED
+	help
+	  This feature introduced identity for cgroup, including:
+	    ID_NORMAL
+		Normal CFS tasks.
+	    ID_HIGHCLASS
+		Tasks preempt normal & underclass tasks on wakeup,
+		also consider underclass only cpu as idle.
+	    ID_UNDERCLASS
+		Tasks take penalty on wakeup.
+	  These identity allow the tasks of cgroup to perform special
+	  schedule behaviour.
+
 config CFS_BANDWIDTH
 	bool "CPU bandwidth provisioning for FAIR_GROUP_SCHED"
 	depends on FAIR_GROUP_SCHED

kernel/sched/core.c
@@ -3110,6 +3110,10 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.cfs_rq			= NULL;
 #endif
 
+#ifdef CONFIG_GROUP_IDENTITY
+	p->se.id_flags			= 0;
+#endif
+
 #ifdef CONFIG_SCHEDSTATS
 	/* Even if schedstat is disabled, there should not be garbage */
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
@@ -8351,6 +8355,40 @@ static u64 cpu_rt_period_read_uint(struct cgroup_subsys_state *css,
 }
 #endif /* CONFIG_RT_GROUP_SCHED */
 
+#ifdef CONFIG_GROUP_IDENTITY
+static int cpu_bvt_warp_ns_write_s64(struct cgroup_subsys_state *css,
+				struct cftype *cftype, s64 val)
+{
+	struct task_group *tg = css_tg(css);
+
+	return update_bvt_warp_ns(tg, val);
+}
+
+static s64 cpu_bvt_warp_ns_read_s64(struct cgroup_subsys_state *css,
+			       struct cftype *cft)
+{
+	struct task_group *tg = css_tg(css);
+
+	return tg->bvt_warp_ns;
+}
+
+static int cpu_identity_write_s64(struct cgroup_subsys_state *css,
+				struct cftype *cftype, s64 val)
+{
+	struct task_group *tg = css_tg(css);
+
+	return update_identity(tg, val);
+}
+
+static s64 cpu_identity_read_s64(struct cgroup_subsys_state *css,
+			       struct cftype *cft)
+{
+	struct task_group *tg = css_tg(css);
+
+	return tg->id_flags;
+}
+#endif
+
 static struct cftype cpu_legacy_files[] = {
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	{
@@ -8409,6 +8447,19 @@ static struct cftype cpu_legacy_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.seq_show = cpu_uclamp_max_show,
 		.write = cpu_uclamp_max_write,
+	},
+#endif
+#ifdef CONFIG_GROUP_IDENTITY
+	/* legacy bvt interface */
+	{
+		.name = "bvt_warp_ns",
+		.read_s64 = cpu_bvt_warp_ns_read_s64,
+		.write_s64 = cpu_bvt_warp_ns_write_s64,
+	},
+	{
+		.name = "identity",
+		.read_s64 = cpu_identity_read_s64,
+		.write_s64 = cpu_identity_write_s64,
 	},
 #endif
 	{ }	/* Terminate */

kernel/sched/sched.h
@@ -426,6 +426,11 @@ struct task_group {
 #endif
 #endif
 
+#ifdef CONFIG_GROUP_IDENTITY
+	int			bvt_warp_ns;
+	int			id_flags;
+#endif
+
 #ifdef CONFIG_RT_GROUP_SCHED
 	struct sched_rt_entity	**rt_se;
 	struct rt_rq		**rt_rq;
@@ -541,6 +546,11 @@ struct cfs_bandwidth { };
 
 #endif	/* CONFIG_CGROUP_SCHED */
 
+#ifdef CONFIG_GROUP_IDENTITY
+extern int update_identity(struct task_group *tg, s64 val);
+extern int update_bvt_warp_ns(struct task_group *tg, s64 val);
+#endif
+
 /* CFS-related fields in a runqueue */
 struct cfs_rq {
 	struct load_weight	load;
@@ -555,6 +565,12 @@ struct cfs_rq {
 #endif
 
 	struct rb_root_cached	tasks_timeline;
+
+#ifdef CONFIG_GROUP_IDENTITY
+	unsigned int		nr_tasks;
+	u64			min_under_vruntime;
+	struct rb_root_cached	under_timeline;
+#endif
 
 	/*
 	 * 'curr' points to currently running entity on this cfs_rq.
@@ -972,6 +988,11 @@ struct rq {
 	struct list_head	leaf_cfs_rq_list;
 	struct list_head	*tmp_alone_branch;
 #endif /* CONFIG_FAIR_GROUP_SCHED */
+
+#ifdef CONFIG_GROUP_IDENTITY
+	unsigned int		nr_high_running;
+	unsigned int		nr_under_running;
+#endif
 
 	/*
 	 * This is part of a global counter where only the total sum

include/linux/sched/sysctl.h
@@ -76,6 +76,10 @@ extern unsigned int sysctl_sched_cfs_bw_burst_onset_percent;
 extern unsigned int sysctl_sched_cfs_bw_burst_enabled;
 #endif
 
+#ifdef CONFIG_GROUP_IDENTITY
+extern unsigned int sysctl_sched_bvt_place_epsilon;
+#endif
+
 #ifdef CONFIG_SCHED_AUTOGROUP
 extern unsigned int sysctl_sched_autogroup_enabled;
 #endif

kernel/sched/debug.c
@@ -595,6 +595,10 @@ void print_cfs_rq(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq)
 	SEQ_printf(m, "  .%-30s: %d\n", "nr_spread_over",
 			cfs_rq->nr_spread_over);
 	SEQ_printf(m, "  .%-30s: %d\n", "nr_running", cfs_rq->nr_running);
+#ifdef CONFIG_GROUP_IDENTITY
+	SEQ_printf(m, "  .%-30s: %lld.%06lu\n", "min_under_vruntime",
+			SPLIT_NS(cfs_rq->min_under_vruntime));
+#endif
 	SEQ_printf(m, "  .%-30s: %ld\n", "load", cfs_rq->load.weight);
 #ifdef CONFIG_SMP
 	SEQ_printf(m, "  .%-30s: %lu\n", "load_avg",
@@ -710,6 +714,10 @@ do {									\
 	SEQ_printf(m, "  .%-30s: %Ld.%06ld\n", #x, SPLIT_NS(rq->x))
 
 	P(nr_running);
+#ifdef CONFIG_GROUP_IDENTITY
+	P(nr_high_running);
+	P(nr_under_running);
+#endif
 	P(nr_switches);
 	P(nr_uninterruptible);
 	PN(next_balance);
@@ -962,6 +970,9 @@ void proc_sched_show_task(struct task_struct *p, struct seq_file *m)
 		P_SCHEDSTAT(se.statistics.nr_migrations_cold);
 		P_SCHEDSTAT(se.statistics.nr_failed_migrations_affine);
 		P_SCHEDSTAT(se.statistics.nr_failed_migrations_running);
+#ifdef CONFIG_GROUP_IDENTITY
+		P_SCHEDSTAT(se.statistics.nr_failed_migrations_id);
+#endif
 		P_SCHEDSTAT(se.statistics.nr_failed_migrations_hot);
 		P_SCHEDSTAT(se.statistics.nr_forced_migrations);
 		P_SCHEDSTAT(se.statistics.nr_wakeups);

kernel/sysctl.c
@@ -1977,6 +1977,15 @@ static struct ctl_table kern_table[] = {
 		.extra2		= SYSCTL_ONE,
 	},
 #endif
+#ifdef CONFIG_GROUP_IDENTITY
+	{
+		.procname	= "sched_bvt_place_epsilon",
+		.data		= &sysctl_sched_bvt_place_epsilon,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+#endif
 #ifdef CONFIG_PROVE_LOCKING
 	{
 		.procname	= "prove_locking",

commit cd99fd963b52d865bd5c6987b07647458ed2a6e4

anolis: fix build error when CONFIG_FAIR_GROUP_SCHED is not defined
ANBZ: #7997

CONFIG_GROUP_IDENTITY should depend on CONFIG_FAIR_GROUP_SCHED.
Fix ht aware quota with wrapper of CONFIG_CFS_BANDWIDTH.

Fixes: b37e67a6c648 ("ck: sched: introduce per-cgroup identity")
Fixes: 4031fe7bdb25 ("anolis: sched/core: introduce ht-aware-quota")
Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

include/linux/sched.h
@@ -579,7 +579,9 @@ struct sched_entity {
 
 #ifdef CONFIG_SCHED_CORE
 	u64				core_vruntime;
+#ifdef CONFIG_CFS_BANDWIDTH
 	unsigned int			ht_ratio;
+#endif
 #endif
 
 	CK_KABI_USE(1, long priority)

init/Kconfig
@@ -961,8 +961,8 @@ config FAIR_GROUP_SCHED
 
 config GROUP_IDENTITY
 	bool "Group identity for SCHED_OTHER"
-	depends on (CGROUP_SCHED && 64BIT && SMP)
-	default CGROUP_SCHED
+	depends on FAIR_GROUP_SCHED && 64BIT && SMP
+	default FAIR_GROUP_SCHED
 	help
 	  This feature introduced identity for cgroup, including:
 	    ID_NORMAL

kernel/sched/core.c
@@ -8618,7 +8618,7 @@ struct task_group *sched_create_group(struct task_group *parent)
 
 	alloc_uclamp_sched_group(tg, parent);
 
-#ifdef CONFIG_SCHED_CORE
+#if defined(CONFIG_SCHED_CORE) && defined(CONFIG_CFS_BANDWIDTH)
 	tg->ht_ratio = 100;
 #endif
 	return tg;
@@ -9563,7 +9563,7 @@ static s64 cpu_identity_read_s64(struct cgroup_subsys_state *css,
 }
 #endif
 
-#ifdef CONFIG_SCHED_CORE
+#if defined(CONFIG_SCHED_CORE) && defined(CONFIG_CFS_BANDWIDTH)
 static int cpu_ht_ratio_write(struct cgroup_subsys_state *css,
 			      struct cftype *cftype, u64 ht_ratio)
 {
@@ -9708,7 +9708,7 @@ static struct cftype cpu_legacy_files[] = {
 		.write_u64 = cpu_ht_stable_write_u64,
 	},
 #endif
-#ifdef CONFIG_SCHED_CORE
+#if defined(CONFIG_SCHED_CORE) && defined(CONFIG_CFS_BANDWIDTH)
 	{
 		.name = "ht_ratio",
 		.read_u64 = cpu_ht_ratio_read,
@@ -10288,7 +10288,7 @@ static struct cftype cpu_files[] = {
 		.seq_show = sched_lat_stat_show
 	},
 #endif
-#ifdef CONFIG_SCHED_CORE
+#if defined(CONFIG_SCHED_CORE) && defined(CONFIG_CFS_BANDWIDTH)
 	{
 		.name = "ht_ratio",
 		.read_u64 = cpu_ht_ratio_read,

kernel/sched/core_sched.c
@@ -375,7 +375,7 @@ void __sched_core_account_sibidle(struct rq *rq)
 		account_ht_aware_quota(p, delta);
 	}
 
-out:
+out:;
 #ifdef CONFIG_SCHED_ACPU
 	for_each_cpu(i, smt_mask) {
 		rq_i = cpu_rq(i);

kernel/sched/fair.c
@@ -13330,6 +13330,7 @@ static int task_is_throttled_fair(struct task_struct *p, int cpu)
 	return throttled_hierarchy(cfs_rq);
 }
 
+#ifdef CONFIG_CFS_BANDWIDTH
 void account_ht_aware_quota(struct task_struct *p, u64 delta)
 {
 	struct sched_entity *se;
@@ -13366,6 +13367,7 @@ void account_ht_aware_quota(struct task_struct *p, u64 delta)
 		}
 	}
 }
+#endif
 
 static inline void sched_core_init_cfs_rq(struct task_group *tg, struct cfs_rq *cfs_rq)
 {

kernel/sched/features.h
@@ -109,6 +109,8 @@ SCHED_FEAT(ID_ABSOLUTE_EXPEL, false)
 #endif
 
 #ifdef CONFIG_SCHED_CORE
+#ifdef CONFIG_CFS_BANDWIDTH
 SCHED_FEAT(SCHED_CORE_HT_AWARE_QUOTA, false)
+#endif
 SCHED_FEAT(SCHED_CORE_VRUNTIME, false)
 #endif

kernel/sched/sched.h
@@ -545,7 +545,7 @@ struct task_group {
 	struct sched_cgroup_lat_stat_cpu __percpu *lat_stat_cpu;
 #endif
 
-#ifdef CONFIG_SCHED_CORE
+#if defined(CONFIG_SCHED_CORE) && defined(CONFIG_CFS_BANDWIDTH)
 	unsigned int		ht_ratio;
 #endif
 	CK_KABI_USE(1, long priority)
@@ -1524,7 +1524,11 @@ extern void sched_core_dequeue(struct rq *rq, struct task_struct *p, int flags);
 extern void sched_core_get(void);
 extern void sched_core_put(void);
 
+#ifdef CONFIG_CFS_BANDWIDTH
 extern void account_ht_aware_quota(struct task_struct *p, u64 delta);
+#else
+static inline void account_ht_aware_quota(struct task_struct *p, u64 delta) { }
+#endif
 #else /* !CONFIG_SCHED_CORE */
 
 static inline bool sched_core_enabled(struct rq *rq)