jeevandeepj994/anolis_perf

commit f42dd06c15b8d0ad6db161c4efeea285f07b5ea4

anolis: ALSA: hda: Add Hygon HD-Audio support
ANBZ: #5455

Add the new PCI ID 0x1d94 0x14a9 Hygon HDA controller.

Signed-off-by: Pu Wen <puwen@hygon.cn>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2546

sound/pci/hda/hda_intel.c
@@ -285,6 +285,7 @@ enum {
 	AZX_DRIVER_CMEDIA,
 	AZX_DRIVER_ZHAOXIN,
 	AZX_DRIVER_ZXHDMI,
+	AZX_DRIVER_HYGON,
 	AZX_DRIVER_GENERIC,
 	AZX_NUM_DRIVERS, /* keep this as last entry */
 };
@@ -407,6 +408,7 @@ static char *driver_short_names[] = {
 	[AZX_DRIVER_CMEDIA] = "HDA C-Media",
 	[AZX_DRIVER_ZHAOXIN] = "HDA Zhaoxin",
 	[AZX_DRIVER_ZXHDMI] = "HDA Zhaoxin GFX",
+	[AZX_DRIVER_HYGON] = "HDA Hygon",
 	[AZX_DRIVER_GENERIC] = "HD-Audio Generic",
 };
 
@@ -2856,6 +2858,9 @@ static const struct pci_device_id azx_ids[] = {
 	  .driver_data = AZX_DRIVER_ZXHDMI | AZX_DCAPS_POSFIX_LPIB | AZX_DCAPS_NO_MSI | AZX_DCAPS_RIRB_PRE_DELAY },
 	{ PCI_DEVICE(0x1d17, 0x9144),
 	  .driver_data = AZX_DRIVER_ZXHDMI | AZX_DCAPS_POSFIX_LPIB | AZX_DCAPS_NO_MSI | AZX_DCAPS_RIRB_PRE_DELAY },
+	/* Hygon HDAudio */
+	{ PCI_DEVICE(0x1d94, 0x14a9),
+	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB },
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, azx_ids);

commit a74c6fc0414fae0459512a3fd16d2b76c38c5c3f

anolis: ALSA: hda: Not use MSI for Hygon family 18h model 5h HD-Audio
ANBZ: #5455

Using MSI for Hygon model 5h HDA controller may lose interrupts in
some heavy loading scenario, so use INTx instead.

Fixes: f42dd06c15b8 ("anolis: ALSA: hda: Add Hygon HD-Audio support")
Signed-off-by: Pu Wen <puwen@hygon.cn>

sound/pci/hda/hda_intel.c
@@ -2864,7 +2864,7 @@ static const struct pci_device_id azx_ids[] = {
 	  .driver_data = AZX_DRIVER_ZXHDMI | AZX_DCAPS_POSFIX_LPIB | AZX_DCAPS_NO_MSI | AZX_DCAPS_RIRB_PRE_DELAY },
 	/* Hygon HDAudio */
 	{ PCI_DEVICE(0x1d94, 0x14a9),
-	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB },
+	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB | AZX_DCAPS_NO_MSI },
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, azx_ids);