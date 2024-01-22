jeevandeepj994/anolis_perf

commit b02e88d2989d3a30541e962bedec5246a3fcff39

anolis: ALSA: hda: Add Hygon HD-Audio support
ANBZ: #5455

Add the new PCI ID 0x1d94 0x14a9 Hygon HDA controller.

Signed-off-by: Pu Wen <puwen@hygon.cn>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2537

sound/pci/hda/hda_intel.c
@@ -273,6 +273,7 @@ enum {
 	AZX_DRIVER_CTHDA,
 	AZX_DRIVER_CMEDIA,
 	AZX_DRIVER_ZHAOXIN,
+	AZX_DRIVER_HYGON,
 	AZX_DRIVER_GENERIC,
 	AZX_NUM_DRIVERS, /* keep this as last entry */
 };
@@ -394,6 +395,7 @@ static const char * const driver_short_names[] = {
 	[AZX_DRIVER_CTHDA] = "HDA Creative",
 	[AZX_DRIVER_CMEDIA] = "HDA C-Media",
 	[AZX_DRIVER_ZHAOXIN] = "HDA Zhaoxin",
+	[AZX_DRIVER_HYGON] = "HDA Hygon",
 	[AZX_DRIVER_GENERIC] = "HD-Audio Generic",
 };
 
@@ -2810,6 +2812,9 @@ static const struct pci_device_id azx_ids[] = {
 	  .driver_data = AZX_DRIVER_GENERIC | AZX_DCAPS_PRESET_ATI_HDMI },
 	/* Zhaoxin */
 	{ PCI_DEVICE(0x1d17, 0x3288), .driver_data = AZX_DRIVER_ZHAOXIN },
+	/* Hygon HDAudio */
+	{ PCI_DEVICE(0x1d94, 0x14a9),
+	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB },
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, azx_ids);

commit 212b05e58592c419032e22a85bd5a3246e847e12

anolis: ALSA: hda: Not use MSI for Hygon family 18h model 5h HD-Audio
ANBZ: #5455

Using MSI for Hygon model 5h HDA controller may lose interrupts in
some heavy loading scenario, so use INTx instead.

Fixes: b02e88d2989d ("anolis: ALSA: hda: Add Hygon HD-Audio support")
Signed-off-by: Pu Wen <puwen@hygon.cn>

sound/pci/hda/hda_intel.c
@@ -2818,7 +2818,7 @@ static const struct pci_device_id azx_ids[] = {
 	{ PCI_DEVICE(0x1d17, 0x3288), .driver_data = AZX_DRIVER_ZHAOXIN },
 	/* Hygon HDAudio */
 	{ PCI_DEVICE(0x1d94, 0x14a9),
-	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB },
+	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB | AZX_DCAPS_NO_MSI },
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, azx_ids);