jeevandeepj994/anolis_perf

commit 951fb1358cc5939ca57b80a6e331213f51793a68

anolis: x86/head/64: Flush caches for .bss..decrypted section after CR3 switches to early_top_pgt
ANBZ: #7830

The memory region of .bss..decrypted section maybe mapped with encryption
before early boot stage of Linux. If the correspond stale caches lives in
earlier stage were not flushed before we access that memory region in
later stages, then Linux will crash because the stale caches will pollute
the memory.

Fix this issue by flush the caches with encrypted mapping before we
access .bss..decrypted section.

Signed-off-by: hanliyang <hanliyang@hygon.cn>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2569

arch/x86/kernel/head64.c
@@ -326,6 +326,54 @@ unsigned long __startup_secondary_64(void)
 	return sme_get_me_mask();
 }
 
+#ifdef CONFIG_AMD_MEM_ENCRYPT
+extern bool bsp_flush_bss_decrypted_section_done;
+
+void early_clflush_bss_decrypted_section(void)
+{
+	/* Only allow bsp flush these caches and the bsp must at early boot stage */
+	if (bsp_flush_bss_decrypted_section_done)
+		return;
+
+	if (read_cr3_pa() != __pa_nodebug(early_top_pgt))
+		return;
+
+	if (sme_get_me_mask()) {
+		unsigned long vaddr, vaddr_end;
+		char *cl, *start, *end;
+
+		/*
+		 * The memory region of .bss..decrypted section maybe mapped
+		 * with encryption in earlier stage. If the correspond stale
+		 * caches lives in earlier stage were not flushed before we
+		 * access that memory region, then Linux will crash later
+		 * because the stale caches will pollute the memory. So we
+		 * need flush the caches with encrypted mapping before we
+		 * access .bss..decrypted section.
+		 *
+		 * The function __startup_64() have already filled the
+		 * encrypted mapping for .bss..decrypted section, use that
+		 * mapping here.
+		 */
+		vaddr = (unsigned long)__start_bss_decrypted -
+					__START_KERNEL_map + phys_base;
+		vaddr_end = (unsigned long)__end_bss_decrypted -
+					__START_KERNEL_map + phys_base;
+
+		/* Hardcode cl-size to 64 at this stage. */
+		start = (char *)(vaddr & ~63);
+		end   = (char *)((vaddr_end + 63) & ~63);
+
+		asm volatile("mfence" : : : "memory");
+		for (cl = start; cl != end; cl += 64)
+			clflush(cl);
+		asm volatile("mfence" : : : "memory");
+	}
+
+	bsp_flush_bss_decrypted_section_done = true;
+}
+#endif
+
 /* Wipe all early page tables except for the kernel symbol map */
 static void __init reset_early_page_tables(void)
 {

arch/x86/kernel/head_64.S
@@ -239,6 +239,16 @@ SYM_INNER_LABEL(secondary_startup_64_no_verify, SYM_L_GLOBAL)
 	 */
 	movq initial_stack(%rip), %rsp
 
+#ifdef CONFIG_AMD_MEM_ENCRYPT
+	/*
+	 * Ensure .bss.decrypted memory's stale caches which lived in earlier
+	 * stage to be flushed.
+	 */
+	pushq	%rsi
+	call	early_clflush_bss_decrypted_section
+	popq	%rsi
+#endif
+
 	/* Setup and Load IDT */
 	pushq	%rsi
 	call	early_setup_idt
@@ -375,6 +385,8 @@ SYM_DATA(initial_code,	.quad x86_64_start_kernel)
 SYM_DATA(initial_gs,	.quad INIT_PER_CPU_VAR(fixed_percpu_data))
 #ifdef CONFIG_AMD_MEM_ENCRYPT
 SYM_DATA(initial_vc_handler,	.quad handle_vc_boot_ghcb)
+SYM_DATA(bsp_flush_bss_decrypted_section_done,	.byte 0x0)
+	.balign	8
 #endif
 
 /*

commit 009d119df226f88c77899353b034aad70c73aaff

anolis: Move early_clflush_bss_decrypted_section() to .init.text section
ANBZ: #8050

If CONFIG_AMD_MEM_ENCRYPT is enabled, a build warning is printed when
building kernel:
  LD [M]  drivers/gpu/drm/nouveau/nouveau.o
  LD [M]  drivers/gpu/drm/i915/i915.o
  AR      drivers/gpu/built-in.a
  AR      drivers/built-in.a
  GEN     .version
  CHK     include/generated/compile.h
  UPD     include/generated/compile.h
  CC      init/version.o
  AR      init/built-in.a
  LD      vmlinux.o
  MODPOST vmlinux.symvers
WARNING: modpost: vmlinux.o(.text+0x10a0): Section mismatch in
reference from the function early_clflush_bss_decrypted_section()
to the variable .init.data:early_top_pgt
The function early_clflush_bss_decrypted_section() references
the variable __initdata early_top_pgt.
This is often because early_clflush_bss_decrypted_section lacks a __initdata
annotation or the annotation of early_top_pgt is wrong.

  MODINFO modules.builtin.modinfo
  GEN     modules.builtin
  LD      .tmp_vmlinux.btf

This warning indicates that the function
early_clflush_bss_decrypted_section() is referencing the variable
early_top_pgt, which is in the .init.data section. If
early_clflush_bss_decrypted_section() is not itself in an initialization
section (like .init.text), then this reference is problematic because
early_top_pgt may be discarded after initialization,
while early_clflush_bss_decrypted_section() may still be present and
try to access it, leading to potential memory corruption or crashes.

However early_clflush_bss_decrypted_section() is just used at initial
stage, add "__init" to move it to .init.text.

Fixes: 951fb1358cc59 ("anolis: x86/head/64: Flush caches for .bss..decrypted section after CR3 switches to early_top_pgt")
Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2668

arch/x86/kernel/head64.c
@@ -329,7 +329,7 @@ unsigned long __startup_secondary_64(void)
 #ifdef CONFIG_AMD_MEM_ENCRYPT
 extern bool bsp_flush_bss_decrypted_section_done;
 
-void early_clflush_bss_decrypted_section(void)
+void __init early_clflush_bss_decrypted_section(void)
 {
 	/* Only allow bsp flush these caches and the bsp must at early boot stage */
 	if (bsp_flush_bss_decrypted_section_done)