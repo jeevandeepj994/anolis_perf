5a6f19204fad1a40f84732b243450e6a72763a70
DuanqiangWen <duanqiangwen@net-swift.com>
2024-01-29 22:43:34 +0800

    anolis: net: txgbevf: add hardware initialization
    ANBZ: #8072
    
    initialize hardware, including vf mac layer,
    mailbox interface.
    
    Signed-off-by: DuanqiangWen <duanqiangwen@net-swift.com>
    Reviewed-by: Duanqiang Wen <duanqiangwen@net-swift.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3121

---
+++ drivers/net/ethernet/wangxun/txgbevf/txgbe_vf.c
@@ -0,0 +1,529 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2015 - 2024 Beijing WangXun Technology Co., Ltd. */
+#include "txgbe_vf.h"
+#include "txgbe_mbx.h"
+
+s32 txgbe_start_hw_vf(struct txgbe_hw *hw)
+{
+	/* Clear adapter stopped flag */
+	hw->adapter_stopped = false;
+
+	return 0;
+}
+
+s32 txgbe_get_mac_addr_vf(struct txgbe_hw *hw, u8 *mac_addr)
+{
+	int i;
+
+	for (i = 0; i < 6; i++)
+		mac_addr[i] = hw->mac.perm_addr[i];
+
+	return 0;
+}
+
+s32 txgbe_init_hw_vf(struct txgbe_hw *hw)
+{
+	s32 status = hw->mac.ops.start_hw(hw);
+
+	hw->mac.ops.get_mac_addr(hw, hw->mac.addr);
+
+	return status;
+}
+
+/* txgbe_virt_clr_reg - Set register to default (power on) state.
+ *  @hw: pointer to hardware structure
+ */
+static void txgbe_virt_clr_reg(struct txgbe_hw *hw)
+{
+	int i;
+	u32 vfsrrctl;
+
+	/* VRSRRCTL default values (BSIZEPACKET = 2048, BSIZEHEADER = 256) */
+	vfsrrctl = TXGBE_VXRXDCTL_HDRSZ(txgbe_hdr_sz(TXGBE_RX_HDR_SIZE));
+	vfsrrctl |= TXGBE_VXRXDCTL_BUFSZ(txgbe_buf_sz(TXGBE_RX_BUF_SIZE));
+
+	for (i = 0; i < 7; i++) {
+		wr32m(hw, TXGBE_VXRXDCTL(i),
+		      (TXGBE_VXRXDCTL_HDRSZ(~0) | TXGBE_VXRXDCTL_BUFSZ(~0)),
+			vfsrrctl);
+	}
+
+	txgbe_flush(hw);
+}
+
+s32 txgbe_reset_hw_vf(struct txgbe_hw *hw)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 timeout = TXGBE_VF_INIT_TIMEOUT;
+	s32 err;
+	u32 msgbuf[TXGBE_VF_PERMADDR_MSG_LEN];
+	u8 *addr = (u8 *)(&msgbuf[1]);
+	u32 i;
+
+	/* reset the api version */
+	hw->api_version = txgbe_mbox_api_10;
+
+	txgbevf_dbg(hw, "Issuing a function reset to MAC\n");
+
+	/* backup msix vectors */
+	for (i = 0; i < 16; i++)
+		hw->b4_buf[i] = txgbe_rd32(hw->b4_addr, i * 4);
+
+	wr32m(hw, TXGBE_VXCTRL, TXGBE_VXCTRL_RST, TXGBE_VXCTRL_RST);
+	txgbe_flush(hw);
+
+	msleep(50);
+
+	/* we cannot reset while the RSTI / RSTD bits are asserted */
+	while (!mbx->ops.check_for_rst(hw, 0) && timeout) {
+		timeout--;
+		udelay(5);
+	}
+
+	/* restore msix vectors */
+	for (i = 0; i < 16; i++)
+		txgbe_wr32(hw->b4_addr, i * 4, hw->b4_buf[i]);
+
+	if (!timeout)
+		return TXGBE_ERR_RESET_FAILED;
+
+	/* Reset VF registers to initial values */
+	txgbe_virt_clr_reg(hw);
+
+	/* mailbox timeout can now become active */
+	mbx->timeout = TXGBE_VF_MBX_INIT_TIMEOUT;
+
+	msgbuf[0] = TXGBE_VF_RESET;
+	err = mbx->ops.write_posted(hw, msgbuf, 1, 0);
+	if (err)
+		return err;
+
+	usleep_range(10000, 20000);
+
+	/* set our "perm_addr" based on info provided by PF
+	 * also set up the mc_filter_type which is piggy backed
+	 * on the mac address in word 3
+	 */
+	err = mbx->ops.read_posted(hw, msgbuf,
+			TXGBE_VF_PERMADDR_MSG_LEN, 0);
+	if (err)
+		return err;
+
+	if (msgbuf[0] != (TXGBE_VF_RESET | TXGBE_VT_MSGTYPE_ACK) &&
+	    msgbuf[0] != (TXGBE_VF_RESET | TXGBE_VT_MSGTYPE_NACK))
+		return TXGBE_ERR_INVALID_MAC_ADDR;
+
+	if (msgbuf[0] == (TXGBE_VF_RESET | TXGBE_VT_MSGTYPE_ACK))
+		memcpy(hw->mac.perm_addr, addr, 6);
+
+	hw->mac.mc_filter_type = msgbuf[TXGBE_VF_MC_TYPE_WORD];
+
+	return 0;
+}
+
+s32 txgbe_stop_adapter_vf(struct txgbe_hw *hw)
+{
+	u32 reg_val;
+	u16 i;
+
+	/* Set the adapter_stopped flag so other driver functions stop touching
+	 * the hardware
+	 */
+	hw->adapter_stopped = true;
+
+	/* Clear interrupt mask to stop from interrupts being generated */
+	wr32(hw, TXGBE_VXIMS, TXGBE_VF_IRQ_CLEAR_MASK);
+
+	/* Clear any pending interrupts, flush previous writes */
+	wr32(hw, TXGBE_VXICR, ~0);
+
+	/* Disable the transmit unit.  Each queue must be disabled. */
+	for (i = 0; i < hw->mac.max_tx_queues; i++)
+		wr32(hw, TXGBE_VXTXDCTL(i), TXGBE_VXTXDCTL_FLUSH);
+
+	/* Disable the receive unit by stopping each queue */
+	for (i = 0; i < hw->mac.max_rx_queues; i++) {
+		reg_val = rd32(hw, TXGBE_VXRXDCTL(i));
+		reg_val &= ~TXGBE_VXRXDCTL_ENABLE;
+		wr32(hw, TXGBE_VXRXDCTL(i), reg_val);
+	}
+	/* Clear packet split and pool config */
+	wr32(hw, TXGBE_VXMRQC, 0);
+
+	/* flush all queues disables */
+	txgbe_flush(hw);
+	usleep_range(10000, 20000);
+
+	return 0;
+}
+
+s32 txgbe_get_fw_version(struct txgbe_hw *hw)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 msgbuf[2];
+	s32 err;
+
+	msgbuf[0] = TXGBE_VF_GET_FW_VERSION;
+	msgbuf[1] = 0x0;
+
+	err = mbx->ops.write_posted(hw, msgbuf, 2, 0);
+	if (err)
+		return err;
+
+	err = mbx->ops.read_posted(hw, msgbuf, 2, 0);
+	if (err)
+		return err;
+
+	if (err || (msgbuf[0] & TXGBE_VT_MSGTYPE_NACK)) {
+		err = TXGBE_ERR_MBX;
+	} else {
+		snprintf(txgbe_firmware_version, TXGBE_FW_VER_SIZE, "0x%08x", msgbuf[1]);
+		err = 0;
+	}
+
+	return err;
+}
+
+s32 txgbe_check_mac_link_vf(struct txgbe_hw *hw, txgbe_link_speed *speed,
+			    bool *link_up, bool __always_unused autoneg_wait_to_complete)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	struct txgbe_mac_info *mac = &hw->mac;
+	s32 err = 0;
+	u32 links_reg;
+	u32 in_msg = 0;
+	u8 i = 0;
+
+	/* If we were hit with a reset drop the link */
+	if (!mbx->ops.check_for_rst(hw, 0) || !mbx->timeout)
+		mac->get_link_status = true;
+
+	mac->get_link_status = true;
+
+	if (!mac->get_link_status)
+		goto out;
+
+	/* if link status is down no point in checking to see if pf is up */
+	links_reg = rd32(hw, TXGBE_VXSTATUS);
+	if (!(links_reg & TXGBE_VXSTATUS_UP))
+		goto out;
+
+	/* for SFP+ modules and DA cables, it can take up to 500usecs
+	 * before the link status is correct
+	 */
+	if (!po32m(hw, TXGBE_VXSTATUS, TXGBE_VXSTATUS_UP, 0, 100, 5))
+		goto out;
+
+	for (i = 0; i < 5; i++) {
+		usleep_range(100, 200);
+		links_reg = rd32(hw, TXGBE_VXSTATUS);
+
+		if (!(links_reg & TXGBE_VXSTATUS_UP))
+			goto out;
+	}
+
+	switch (TXGBE_VXSTATUS_SPEED(links_reg)) {
+	case TXGBE_VXSTATUS_SPEED_10G:
+		*speed = TXGBE_LINK_SPEED_10GB_FULL;
+		break;
+	case TXGBE_VXSTATUS_SPEED_1G:
+		*speed = TXGBE_LINK_SPEED_1GB_FULL;
+		break;
+	case TXGBE_VXSTATUS_SPEED_100M:
+		*speed = TXGBE_LINK_SPEED_100_FULL;
+		break;
+	}
+
+	/* if the read failed it could just be a mailbox collision, best wait
+	 * until we are called again and don't report an error
+	 */
+	if (mbx->ops.read(hw, &in_msg, 1, 0))
+		goto out;
+
+	if (!(in_msg & TXGBE_VT_MSGTYPE_CTS)) {
+		/* msg is not CTS and is NACK we must have lost CTS status */
+		if (in_msg & TXGBE_VT_MSGTYPE_NACK)
+			err = -1;
+		goto out;
+	}
+
+	/* the pf is talking, if we timed out in the past we reinit */
+	if (!mbx->timeout) {
+		err = -1;
+		goto out;
+	}
+
+	/* if we passed all the tests above then the link is up and we no
+	 * longer need to check for link
+	 */
+	mac->get_link_status = false;
+out:
+	*link_up = !mac->get_link_status;
+	return err;
+}
+
+s32 txgbe_set_rar_vf(struct txgbe_hw *hw, u32 __always_unused index, u8 *addr,
+		     u32 __always_unused vmdq, u32 __always_unused enable_addr)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 msgbuf[3];
+	u8 *msg_addr = (u8 *)(&msgbuf[1]);
+	s32 err;
+
+	memset(msgbuf, 0, 12);
+	msgbuf[0] = TXGBE_VF_SET_MAC_ADDR;
+	memcpy(msg_addr, addr, 6);
+	err = mbx->ops.write_posted(hw, msgbuf, 3, 0);
+
+	if (!err)
+		err = mbx->ops.read_posted(hw, msgbuf, 3, 0);
+
+	msgbuf[0] &= ~TXGBE_VT_MSGTYPE_CTS;
+
+	/* if nacked the address was rejected, use "perm_addr" */
+	if (!err &&
+	    (msgbuf[0] == (TXGBE_VF_SET_MAC_ADDR | TXGBE_VT_MSGTYPE_NACK))) {
+		txgbe_get_mac_addr_vf(hw, hw->mac.addr);
+		return TXGBE_ERR_MBX;
+	}
+
+	return err;
+}
+
+s32 txgbe_set_uc_addr_vf(struct txgbe_hw *hw, u32 index, u8 *addr)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 msgbuf[3];
+	u8 *msg_addr = (u8 *)(&msgbuf[1]);
+	s32 err;
+
+	memset(msgbuf, 0, sizeof(msgbuf));
+	/* If index is one then this is the start of a new list and needs
+	 * indication to the PF so it can do it's own list management.
+	 * If it is zero then that tells the PF to just clear all of
+	 * this VF's macvlans and there is no new list.
+	 */
+	msgbuf[0] |= index << TXGBE_VT_MSGINFO_SHIFT;
+	msgbuf[0] |= TXGBE_VF_SET_MACVLAN;
+	if (addr)
+		memcpy(msg_addr, addr, 6);
+	err = mbx->ops.write_posted(hw, msgbuf, 3, 0);
+
+	if (!err)
+		err = mbx->ops.read_posted(hw, msgbuf, 3, 0);
+
+	msgbuf[0] &= ~TXGBE_VT_MSGTYPE_CTS;
+
+	if (!err)
+		if (msgbuf[0] == (TXGBE_VF_SET_MACVLAN | TXGBE_VT_MSGTYPE_NACK))
+			err = TXGBE_ERR_OUT_OF_MEM;
+
+	return err;
+}
+
+static s32 txgbe_mta_vector(struct txgbe_hw *hw, u8 *mc_addr)
+{
+	u32 vector = 0;
+
+	switch (hw->mac.mc_filter_type) {
+	case 0:   /* use bits [47:36] of the address */
+		vector = ((mc_addr[4] >> 4) | (((u16)mc_addr[5]) << 4));
+		break;
+	case 1:   /* use bits [46:35] of the address */
+		vector = ((mc_addr[4] >> 3) | (((u16)mc_addr[5]) << 5));
+		break;
+	case 2:   /* use bits [45:34] of the address */
+		vector = ((mc_addr[4] >> 2) | (((u16)mc_addr[5]) << 6));
+		break;
+	case 3:   /* use bits [43:32] of the address */
+		vector = ((mc_addr[4]) | (((u16)mc_addr[5]) << 8));
+		break;
+	default:  /* Invalid mc_filter_type */
+		txgbevf_dbg(hw, "MC filter type param set incorrectly\n");
+		break;
+	}
+
+	/* vector can only be 12-bits or boundary will be exceeded */
+	vector &= 0xFFF;
+
+	return vector;
+}
+
+s32 txgbe_update_mc_addr_list_vf(struct txgbe_hw *hw, u8 *mc_addr_list,
+				 u32 mc_addr_count, txgbe_mc_addr_itr next,
+				 bool __always_unused clear)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 msgbuf[TXGBE_VXMAILBOX_SIZE];
+	u16 *vector_list = (u16 *)&msgbuf[1];
+	u32 vector;
+	u32 cnt, i;
+	u32 vmdq;
+
+	/* Each entry in the list uses 1 16 bit word.  We have 30
+	 * 16 bit words available in our HW msg buffer (minus 1 for the
+	 * msg type).  That's 30 hash values if we pack 'em right.  If
+	 * there are more than 30 MC addresses to add then punt the
+	 * extras for now and then add code to handle more than 30 later.
+	 * It would be unusual for a server to request that many multi-cast
+	 * addresses except for in large enterprise network environments.
+	 */
+
+	txgbevf_dbg(hw, "MC Addr Count = %d\n", mc_addr_count);
+
+	cnt = (mc_addr_count > 30) ? 30 : mc_addr_count;
+	msgbuf[0] = TXGBE_VF_SET_MULTICAST;
+	msgbuf[0] |= cnt << TXGBE_VT_MSGINFO_SHIFT;
+
+	for (i = 0; i < cnt; i++) {
+		vector = txgbe_mta_vector(hw, next(hw, &mc_addr_list, &vmdq));
+		txgbevf_dbg(hw, "Hash value = 0x%03X\n", vector);
+		vector_list[i] = (u16)vector;
+	}
+
+	return mbx->ops.write_posted(hw, msgbuf, TXGBE_VXMAILBOX_SIZE, 0);
+}
+
+s32 txgbe_update_xcast_mode(struct txgbe_hw *hw, int xcast_mode)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 msgbuf[2];
+	s32 err;
+
+	switch (hw->api_version) {
+	case txgbe_mbox_api_12:
+		if (xcast_mode > TXGBE_XCAST_MODE_ALLMULTI)
+			return TXGBE_ERR_FEATURE_NOT_SUPPORTED;
+	case txgbe_mbox_api_13:
+	//case txgbe_mbox_api_15:
+		break;
+	default:
+		return TXGBE_ERR_FEATURE_NOT_SUPPORTED;
+	}
+
+	msgbuf[0] = TXGBE_VF_UPDATE_XCAST_MODE;
+	msgbuf[1] = xcast_mode;
+
+	err = mbx->ops.write_posted(hw, msgbuf, 2, 0);
+	if (err)
+		return err;
+
+	err = mbx->ops.read_posted(hw, msgbuf, 2, 0);
+	if (err)
+		return err;
+
+	msgbuf[0] &= ~TXGBE_VT_MSGTYPE_CTS;
+	if (msgbuf[0] == (TXGBE_VF_UPDATE_XCAST_MODE | TXGBE_VT_MSGTYPE_NACK))
+		return TXGBE_ERR_FEATURE_NOT_SUPPORTED;
+	return 0;
+}
+
+s32 txgbe_get_link_state_vf(struct txgbe_hw *hw, bool *link_state)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 msgbuf[2];
+	s32 err;
+	s32 ret_val;
+
+	msgbuf[0] = TXGBE_VF_GET_LINK_STATE;
+	msgbuf[1] = 0x0;
+
+	err = mbx->ops.write_posted(hw, msgbuf, 2, 0);
+	if (err)
+		return err;
+
+	err = mbx->ops.read_posted(hw, msgbuf, 2, 0);
+	if (err)
+		return err;
+
+	if (err || (msgbuf[0] & TXGBE_VT_MSGTYPE_NACK)) {
+		ret_val = TXGBE_ERR_MBX;
+	} else {
+		ret_val = 0;
+		*link_state = msgbuf[1];
+	}
+
+	return ret_val;
+}
+
+s32 txgbe_set_vfta_vf(struct txgbe_hw *hw, u32 vlan, u32 vind,
+		      bool __always_unused vlan_on, bool __always_unused vlvf_bypass)
+{
+	struct txgbe_mbx_info *mbx = &hw->mbx;
+	u32 msgbuf[2];
+	s32 err;
+
+	msgbuf[0] = TXGBE_VF_SET_VLAN;
+	msgbuf[1] = vlan;
+	/* Setting the 8 bit field MSG INFO to TRUE indicates "add" */
+	msgbuf[0] |= vlan_on << TXGBE_VT_MSGINFO_SHIFT;
+
+	err = mbx->ops.write_posted(hw, msgbuf, 2, 0);
+	if (!err)
+		err = mbx->ops.read_posted(hw, msgbuf, 1, 0);
+
+	if (!err && (msgbuf[0] & TXGBE_VT_MSGTYPE_ACK))
+		return 0;
+
+	return err | (msgbuf[0] & TXGBE_VT_MSGTYPE_NACK);
+}
+
+/**
+ *  txgbe_negotiate_api_version - Negotiate supported API version
+ *  @hw: pointer to the HW structure
+ *  @api: integer containing requested API version
+ **/
+int txgbe_negotiate_api_version(struct txgbe_hw *hw, int api)
+{
+	int err;
+	u32 msg[3];
+
+	/* Negotiate the mailbox API version */
+	msg[0] = TXGBE_VF_API_NEGOTIATE;
+	msg[1] = api;
+	msg[2] = 0;
+	err = hw->mbx.ops.write_posted(hw, msg, 3, 0);
+
+	if (!err)
+		err = hw->mbx.ops.read_posted(hw, msg, 3, 0);
+
+	if (!err) {
+		msg[0] &= ~TXGBE_VT_MSGTYPE_CTS;
+
+		/* Store value and return 0 on success */
+		if (msg[0] == (TXGBE_VF_API_NEGOTIATE | TXGBE_VT_MSGTYPE_ACK)) {
+			hw->api_version = api;
+			return 0;
+		}
+
+		err = TXGBE_ERR_INVALID_ARGUMENT;
+	}
+	return err;
+}
+
+void txgbe_init_ops_vf(struct txgbe_hw *hw)
+{
+	/* MAC */
+	hw->mac.ops.init_hw = txgbe_init_hw_vf;
+	hw->mac.ops.reset_hw = txgbe_reset_hw_vf;
+	hw->mac.ops.start_hw = txgbe_start_hw_vf;
+	/* Cannot clear stats on VF */
+	hw->mac.ops.get_mac_addr = txgbe_get_mac_addr_vf;
+	hw->mac.ops.get_fw_version = txgbe_get_fw_version;
+
+	/* Link */
+	hw->mac.ops.check_link = txgbe_check_mac_link_vf;
+
+	/* RAR, Multicast, VLAN */
+	hw->mac.ops.set_rar = txgbe_set_rar_vf;
+	hw->mac.ops.set_uc_addr = txgbe_set_uc_addr_vf;
+	hw->mac.ops.update_mc_addr_list = txgbe_update_mc_addr_list_vf;
+	hw->mac.ops.update_xcast_mode = txgbe_update_xcast_mode;
+	hw->mac.ops.get_link_state = txgbe_get_link_state_vf;
+	hw->mac.ops.set_vfta = txgbe_set_vfta_vf;
+
+	hw->mac.max_tx_queues = 1;
+	hw->mac.max_rx_queues = 1;
+
+	hw->mbx.ops.init_params = txgbe_init_mbx_params_vf;
+}
--- drivers/net/ethernet/wangxun/txgbevf/txgbevf_main.c
+++ drivers/net/ethernet/wangxun/txgbevf/txgbevf_main.c
@@ -8,8 +8,11 @@
 #include <linux/string.h>
 #include <linux/aer.h>
 #include <linux/etherdevice.h>
+#include <linux/atomic.h>
+#
 
 #include "txgbe_vf.h"
+#include "txgbe_mbx.h"
 
 char txgbe_driver_name[] = "txgbevf";
 
@@ -18,6 +21,8 @@ static const char txgbe_driver_string[] =
 
 const char txgbe_driver_version[] = __stringify(1.3.1);
 
+char txgbe_firmware_version[TXGBE_FW_VER_SIZE] = "N/A";
+
 /* txgbe_pci_tbl - PCI Device ID Table */
 static struct pci_device_id txgbe_pci_tbl[] = {
 	{ PCI_VDEVICE(WANGXUN, TXGBE_DEV_ID_SP1000_VF), 0},
@@ -25,6 +30,198 @@ static struct pci_device_id txgbe_pci_tbl[] = {
 	{ .device = 0 } /* required last entry */
 };
 
+static struct txgbe_info txgbe_sp_vf_info = {
+	.mac    = txgbe_mac_sp_vf,
+	.flags  = 0,
+};
+
+static const struct txgbe_info *txgbe_info_tbl[] = {
+	[board_sp_vf] = &txgbe_sp_vf_info,
+};
+
+static const struct net_device_ops txgbe_netdev_ops = {
+	.ndo_open               = txgbe_open,
+	.ndo_stop               = txgbe_close,
+};
+
+static inline int txgbevf_init_rss_key(struct txgbe_adapter *adapter)
+{
+	u32 *rss_key;
+
+	if (!adapter->rss_key) {
+		rss_key = kzalloc(40, GFP_KERNEL);
+		if (unlikely(!rss_key))
+			return -ENOMEM;
+		netdev_rss_key_fill(rss_key, 40);
+		adapter->rss_key = rss_key;
+	}
+	return 0;
+}
+
+int txgbe_open(struct net_device *netdev)
+{
+	return 0;
+}
+
+int txgbe_close(struct net_device *netdev)
+{
+	return 0;
+}
+
+void txgbe_assign_netdev_ops(struct net_device *dev)
+{
+	dev->netdev_ops = &txgbe_netdev_ops;
+}
+
+void txgbe_negotiate_api(struct txgbe_adapter *adapter)
+{
+	struct txgbe_hw *hw = &adapter->hw;
+	int api[] = {
+		     txgbe_mbox_api_13,
+		     txgbe_mbox_api_12,
+		     txgbe_mbox_api_11,
+		     txgbe_mbox_api_10,
+		     txgbe_mbox_api_unknown};
+	int err = 0, idx = 0;
+
+	spin_lock_bh(&adapter->mbx_lock);
+
+	while (api[idx] != txgbe_mbox_api_unknown) {
+		err = txgbe_negotiate_api_version(hw, api[idx]);
+		if (!err)
+			break;
+		idx++;
+	}
+
+	spin_unlock_bh(&adapter->mbx_lock);
+}
+
+/**
+ * txgbe_sw_init - Initialize general software structures (struct txgbe_adapter)
+ * @adapter: board private structure to initialize
+ **/
+static int txgbe_sw_init(struct txgbe_adapter *adapter)
+{
+	struct txgbe_hw *hw = &adapter->hw;
+	struct pci_dev *pdev = adapter->pdev;
+	struct net_device *netdev = adapter->netdev;
+	int err = 0;
+
+	/* PCI config space info */
+	hw->vendor_id = pdev->vendor;
+	hw->device_id = pdev->device;
+	pci_read_config_byte(pdev, PCI_REVISION_ID, &hw->revision_id);
+	hw->subsystem_vendor_id = pdev->subsystem_vendor;
+	hw->subsystem_device_id = pdev->subsystem_device;
+
+	txgbe_init_ops_vf(hw);
+	hw->mbx.ops.init_params(hw);
+	err = txgbevf_init_rss_key(adapter);
+	if (err)
+		return err;
+
+	/* assume legacy case in which PF would only give VF 2 queues */
+	hw->mac.max_tx_queues = 4;
+	hw->mac.max_rx_queues = 4;
+
+	/* lock to protect mailbox accesses */
+	spin_lock_init(&adapter->mbx_lock);
+	spin_lock_init(&adapter->pf_count_lock);
+
+	/*make sure PF is up*/
+	if (adapter->bd_number == 0)
+		msleep(1500);
+
+	err = hw->mac.ops.reset_hw(hw);
+	if (err) {
+		dev_info(&pdev->dev,
+			 "PF still in reset state.  Is the PF interface up?\n");
+	} else {
+		err = hw->mac.ops.init_hw(hw);
+		if (err) {
+			dev_err(&pdev->dev,
+				"init_shared_code failed: %d\n", err);
+			return err;
+		}
+		txgbe_negotiate_api(adapter);
+		err = hw->mac.ops.get_mac_addr(hw, hw->mac.addr);
+		if (err)
+			dev_info(&pdev->dev, "Error reading MAC address\n");
+		else if (is_zero_ether_addr(adapter->hw.mac.addr))
+			dev_info(&pdev->dev,
+				 "MAC address not assigned by administrator.\n");
+		ether_addr_copy(netdev->dev_addr, hw->mac.addr);
+	}
+
+	if (!is_valid_ether_addr(netdev->dev_addr)) {
+		dev_info(&pdev->dev, "Assigning random MAC address\n");
+		eth_hw_addr_random(netdev);
+		ether_addr_copy(hw->mac.addr, netdev->dev_addr);
+		ether_addr_copy(hw->mac.perm_addr, netdev->dev_addr);
+	}
+
+	/* Enable dynamic interrupt throttling rates */
+	adapter->rx_itr_setting = 1;
+	adapter->tx_itr_setting = 1;
+
+	/* set default ring sizes */
+	adapter->tx_ring_count = TXGBE_DEFAULT_TXD;
+	adapter->rx_ring_count = TXGBE_DEFAULT_RXD;
+
+	/* enable rx csum by default */
+	adapter->flagsd |= TXGBE_F_CAP_RX_CSUM;
+
+	adapter->link_state = true;
+
+	set_bit(__TXGBE_DOWN, &adapter->state);
+
+	return 0;
+}
+
+void txgbe_init_last_counter_stats(struct txgbe_adapter *adapter)
+{
+	struct txgbe_hw *hw = &adapter->hw;
+	int i = 0;
+
+	adapter->last_stats.gprc = 0;
+	adapter->last_stats.gorc = 0;
+	adapter->last_stats.gptc = 0;
+	adapter->last_stats.gotc = 0;
+	adapter->last_stats.mprc = 0;
+
+	for (i = 0; i < MAX_RX_QUEUES; i++) {
+		adapter->last_reg_stats[i].gprc = rd32(hw, TXGBE_VXGPRC(i));
+		adapter->last_stats.gprc += adapter->last_reg_stats[i].gprc;
+
+		adapter->last_reg_stats[i].gorc = rd32(hw, TXGBE_VXGORC_LSB(i));
+		adapter->last_reg_stats[i].gorc = adapter->last_reg_stats[i].gorc |
+			((u64)(rd32(hw, TXGBE_VXGORC_MSB(i))) << 32);
+		adapter->last_stats.gorc += adapter->last_reg_stats[i].gorc;
+
+		adapter->last_reg_stats[i].gptc = rd32(hw, TXGBE_VXGPTC(i));
+		adapter->last_stats.gptc += adapter->last_reg_stats[i].gptc;
+
+		adapter->last_reg_stats[i].gotc = rd32(hw, TXGBE_VXGOTC_LSB(i));
+		adapter->last_reg_stats[i].gotc = adapter->last_reg_stats[i].gotc |
+			((u64)(rd32(hw, TXGBE_VXGOTC_MSB(i))) << 32);
+		adapter->last_stats.gotc += adapter->last_reg_stats[i].gotc;
+
+		adapter->last_reg_stats[i].mprc = rd32(hw, TXGBE_VXMPRC(i));
+		adapter->last_stats.mprc += adapter->last_reg_stats[i].mprc;
+
+		adapter->reg_stats[i].gprc = 0;
+		adapter->reg_stats[i].gorc = 0;
+		adapter->reg_stats[i].gptc = 0;
+		adapter->reg_stats[i].gotc = 0;
+		adapter->reg_stats[i].mprc = 0;
+	}
+
+	adapter->base_stats.gprc = adapter->last_stats.gprc;
+	adapter->base_stats.gorc = adapter->last_stats.gorc;
+	adapter->base_stats.gptc = adapter->last_stats.gptc;
+	adapter->base_stats.gotc = adapter->last_stats.gotc;
+	adapter->base_stats.mprc = adapter->last_stats.mprc;
+}
 /**
  * txgbe_probe - Device Initialization Routine
  * @pdev: PCI device information struct
@@ -41,6 +238,10 @@ static int txgbe_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	struct txgbe_adapter *adapter = NULL;
 	struct net_device *netdev;
 	int err;
+	struct txgbe_hw *hw = NULL;
+	const struct txgbe_info *ei = txgbe_info_tbl[ent->driver_data];
+	static int cards_found;
+	unsigned int min_mtu, max_mtu;
 
 	err = pci_enable_device_mem(pdev);
 	if (err)
@@ -79,6 +280,16 @@ static int txgbe_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	adapter = netdev_priv(netdev);
 	adapter->netdev = netdev;
 	adapter->pdev = pdev;
+	hw = &adapter->hw;
+
+	hw->back = adapter;
+	hw->msg_enable = &adapter->msg_enable;
+	hw->pdev = adapter->pdev;
+	hw->mac.type = ei->mac;
+	adapter->flagsd = ei->flags;
+	adapter->msg_enable = DEFAULT_DEBUG_LEVEL;
+
+	pci_save_state(pdev);
 
 	adapter->io_addr = devm_ioremap(&pdev->dev,
 					pci_resource_start(pdev, 0),
@@ -96,8 +307,54 @@ static int txgbe_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		goto err_pci_release_regions;
 	}
 
+	txgbe_assign_netdev_ops(netdev);
+	adapter->bd_number = cards_found;
+
+		/* setup the private structure */
+	err = txgbe_sw_init(adapter);
+	if (err)
+		goto err_sw_init;
+
+	min_mtu = ETH_MIN_MTU;
+	switch (adapter->hw.api_version) {
+	case txgbe_mbox_api_11:
+	case txgbe_mbox_api_12:
+	case txgbe_mbox_api_13:
+		max_mtu = TXGBE_MAX_JUMBO_FRAME_SIZE -
+			  (ETH_HLEN + ETH_FCS_LEN);
+		break;
+	default:
+		if (adapter->hw.mac.type != txgbe_mac_sp_vf)
+			max_mtu = TXGBE_MAX_JUMBO_FRAME_SIZE -
+				  (ETH_HLEN + ETH_FCS_LEN);
+		else
+			max_mtu = ETH_DATA_LEN + ETH_FCS_LEN;
+		break;
+	}
+
+	netdev->min_mtu = min_mtu;
+	netdev->max_mtu = max_mtu;
+
+	hw->mac.ops.get_fw_version(hw);
+
+	strcpy(netdev->name, "eth%d");
+
+	err = register_netdev(netdev);
+	if (err)
+		goto err_sw_init;
+
+	pci_set_drvdata(pdev, netdev);
+	netif_carrier_off(netdev);
+
+	netif_tx_stop_all_queues(netdev);
+	txgbe_init_last_counter_stats(adapter);
+
+	cards_found++;
+
 	return 0;
 
+err_sw_init:
+	iounmap(adapter->io_addr);
 err_pci_release_regions:
 	pci_release_selected_regions(pdev,
 				     pci_select_bars(pdev, IORESOURCE_MEM));
