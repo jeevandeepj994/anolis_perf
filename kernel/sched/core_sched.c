jeevandeepj994/anolis_perf

commit 575317e961ef2d0187fd1c9786cf0eb3cbc7416c

anolis: fix the bug that turn off kernel.sched_core doesn't work
ANBZ: #7066

When turn off kernel.sched_core, static branch __sysctl_sched_core_enabled
is disabled first, resulting that clear_all_cookie() will do nothing.
The cookies of the cookie'd tasks are still cookie'd, and sched_core is
still enabled, which conflicts with our intention.

To fix this, we clear the cookies first, anzhe disable static branch
__sysctl_sched_core_enabled.

Fixes: c02f4ea40824 ("anolis: sched/core: introduce sysctl_sched_core")
Reported-by: Yi Tao <escape@linux.alibaba.com>
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2385

kernel/sched/core_sched.c
@@ -288,12 +288,12 @@ int sysctl_sched_core_handler(struct ctl_table *table, int write,
 	if (new) {
 		static_branch_enable(&__sysctl_sched_core_enabled);
 	} else {
-		static_branch_disable(&__sysctl_sched_core_enabled);
 		/*
 		 * When turn off sched_core, we need to clear cookie of all the tasks, to
 		 * make __sched_core_enabled off.
 		 */
 		clear_all_cookie();
+		static_branch_disable(&__sysctl_sched_core_enabled);
 	}
 
 	return err;

commit 83ea395d5e4bb0dae3e96601cba69dff3023038e

anolis: sched/core: fix the race condition when disable core sched
ANBZ: #8441

There is a race condition between disable __sysctl_sched_core_enabled
and clear_all_cookie in function sysctl_sched_core_handler(). If we
echo 0 > /proc/sys/kernel/sched_core, and we call prctl to set cookie
at the same time, there will be some cookies not cleared.

To fix this problem, we disable __sysctl_sched_core_enabled first
to prevent new cookies created, and we allow clear cookie when
__sysctl_sched_core_enabled is disabled.

Fixes: commit 575317e961ef ("anolis: fix the bug that turn off kernel.sched_core doesn't work")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/core_sched.c
@@ -144,7 +144,7 @@ int sched_core_share_pid(unsigned int cmd, pid_t pid, enum pid_type type,
 		return -ENODEV;
 
 	if (!static_branch_likely(&__sysctl_sched_core_enabled) &&
-	    cmd != PR_SCHED_CORE_GET)
+	    cmd != PR_SCHED_CORE_GET && cmd != PR_SCHED_CORE_CLEAR)
 		return -EPERM;
 
 	BUILD_BUG_ON(PR_SCHED_CORE_SCOPE_THREAD != PIDTYPE_PID);
@@ -309,12 +309,12 @@ int sysctl_sched_core_handler(struct ctl_table *table, int write,
 		static_branch_enable(&__sysctl_sched_core_enabled);
 		sched_check_group_identity_unlock();
 	} else {
+		static_branch_disable(&__sysctl_sched_core_enabled);
 		/*
 		 * When turn off sched_core, we need to clear cookie of all the tasks, to
 		 * make __sched_core_enabled off.
 		 */
 		clear_all_cookie();
-		static_branch_disable(&__sysctl_sched_core_enabled);
 	}
 
 	return err;