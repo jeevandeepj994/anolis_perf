e61ac77f426ad64944f99202e1ef54aa4afe5ae7
Tianchen Ding <dtcccc@linux.alibaba.com>
2024-03-08 14:50:42 +0800

    anolis: kfence: enhance kfence for 6.6
    ANBZ: #8499
    
    Port anolis own kfence features from ANCK5.10. Enhance the ability of
    memory debugging about slub objects.
    
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2874

--- mm/slub.c
+++ mm/slub.c
@@ -3461,7 +3461,7 @@ static __fastpath_inline void *slab_alloc_node(struct kmem_cache *s, struct list
 	if (!s)
 		return NULL;
 
-	object = kfence_alloc(s, orig_size, gfpflags);
+	object = kfence_alloc_node(s, orig_size, gfpflags, node);
 	if (unlikely(object))
 		goto out;
 
