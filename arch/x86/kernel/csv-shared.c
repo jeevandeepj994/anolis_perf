5024716f9ecf64175565f7f86a217189e48aa57b
Xin Jiang <jiangxin@hygon.cn>
2024-03-11 15:27:59 +0800

    anolis: x86/boot/compressed/64: Add CSV3 guest detection
    ANBZ: #8683
    
    Check if CSV3 guest is active at boot compressed stage. It
    checks HYGON hardware with CPUID 0x00000000 and bit30 of MSR
    0xc0010131.
    
    Signed-off-by: Xin Jiang <jiangxin@hygon.cn>
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2996

---
+++ arch/x86/kernel/csv-shared.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Hygon CSV support
+ *
+ * This file is shared between decompression boot code and running
+ * linux kernel.
+ *
+ * Copyright (C) Hygon Info Technologies Ltd.
+ */
+
+#define CPUID_VENDOR_HygonGenuine_ebx	0x6f677948
+#define CPUID_VENDOR_HygonGenuine_ecx	0x656e6975
+#define CPUID_VENDOR_HygonGenuine_edx	0x6e65476e
+
+#define MSR_CSV3_ENABLED_BIT		30
+#define MSR_CSV3_ENABLED		BIT_ULL(MSR_CSV3_ENABLED_BIT)
