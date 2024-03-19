085dae3c929b261c64762d33b79aabd37f98d6b2
Yuanhe Shu <xiangzao@linux.alibaba.com>
2024-03-19 15:51:08 +0800

    anolis: selftests/bpf: introduce samples/bpf testcases
    ANBZ: #5530
    
    Introduce samples/bpf testcases into tools/testing/selftests/bpf
    then test framework can take advantage of to perform more tests.
    
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2908

---
+++ tools/testing/selftests/bpf/samples_test.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+
+yum install -y iperf3 netperf iproute-tc nmap-ncat readline
+
+cd ../../../../samples/bpf
+make -j8
+
+ulimit -l unlimited
+
+./map_perf_test
+./sampleip
+
+./sockex1
+./sockex2
+./sockex3
+
+./sock_example
+./syscall_tp
+./task_fd_query
+./tc_l2_redirect.sh
+./test_cgrp2_sock2.sh
+./test_cgrp2_sock.sh
+./test_cgrp2_tc.sh
+./test_cls_bpf.sh
+./test_current_task_under_cgroup
+./test_overhead
+
+timeout 10 ./tracex1
+./tracex2
+timeout 10 ./tracex3
+timeout 10 ./tracex4
+timeout 10 ./tracex5
+./tracex6
+./tracex7
+
+ip link add veth0t type veth peer name veth1t
+ip link set veth0t up
+ip link set veth1t up
+timeout 10 ./xdp_redirect_cpu --dev veth0t --prog 2 --cpu 0 --cpu 1 --cpu 4
+timeout 10 ./xdp_redirect_cpu --dev veth1t --prog 4 --cpu 2 --cpu 3 --cpu 4
+timeout 10 ./xdp_redirect_map `cat /sys/class/net/veth0t/ifindex` \
+`cat /sys/class/net/veth1t/ifindex`
+ip link delete veth0t
+
+./fds_example -F /sys/fs/bpf/m -G -m -k 1
+./fds_example -F /sys/fs/bpf/m -G -m -k 1 -v 24
+./fds_example -F /sys/fs/bpf/m -G -m -k 1
+./fds_example -F /sys/fs/bpf/m2 -G -m -k 1
+./fds_example -F /sys/fs/bpf/m2 -G -m
+./fds_example -F /sys/fs/bpf/p -G -p
+./fds_example -F /sys/fs/bpf/p2 -G -p
+
+./do_hbm_test.sh -l -d=1 -D --stats
+./do_hbm_test.sh -l -d=1 -D --stats dctcp
+./do_hbm_test.sh -l -d=1 -D --stats -f=2
+./do_hbm_test.sh -l -d=1 -D --stats -f=4 -P
+./do_hbm_test.sh -l -d=1 -D --stats -f=4 -N
+
+./test_ipip.sh
+./trace_output
+
+cd ../../tools/testing/selftest/bpf/
