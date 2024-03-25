0day-ci/linux

erofs: dax support for non-tailpacking regular file
DAX is quite useful for some VM use cases in order to save guest
memory extremely with minimal lightweight EROFS.

In order to prepare for such use cases, add preliminary dax support
for non-tailpacking regular files for now.

Tested with the DRAM-emulated PMEM and the EROFS image generated by
"mkfs.erofs -Enoinline_data enwik9.fsdax.img enwik9"

Cc: nvdimm@lists.linux.dev
Cc: linux-fsdevel@vger.kernel.org
Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

fs/erofs/super.c
@@ -11,6 +11,7 @@
 #include <linux/crc32c.h>
 #include <linux/fs_context.h>
 #include <linux/fs_parser.h>
+#include <linux/dax.h>
 #include "xattr.h"
 
 #define CREATE_TRACE_POINTS
@@ -355,6 +356,8 @@ enum {
 	Opt_user_xattr,
 	Opt_acl,
 	Opt_cache_strategy,
+	Opt_dax,
+	Opt_dax_enum,
 	Opt_err
 };
 
@@ -365,13 +368,46 @@ static const struct constant_table erofs_param_cache_strategy[] = {
 	{}
 };
 
+static const struct constant_table erofs_dax_param_enums[] = {
+	{"always",	EROFS_MOUNT_DAX_ALWAYS},
+	{"never",	EROFS_MOUNT_DAX_NEVER},
+	{}
+};
+
 static const struct fs_parameter_spec erofs_fs_parameters[] = {
 	fsparam_flag_no("user_xattr",	Opt_user_xattr),
 	fsparam_flag_no("acl",		Opt_acl),
 	fsparam_enum("cache_strategy",	Opt_cache_strategy,
 		     erofs_param_cache_strategy),
+	fsparam_flag("dax",             Opt_dax),
+	fsparam_enum("dax",		Opt_dax_enum, erofs_dax_param_enums),
 	{}
 };
+
+static bool erofs_fc_set_dax_mode(struct fs_context *fc, unsigned int mode)
+{
+#ifdef CONFIG_FS_DAX
+	struct erofs_fs_context *ctx = fc->fs_private;
+
+	switch (mode) {
+	case EROFS_MOUNT_DAX_ALWAYS:
+		warnfc(fc, "DAX enabled. Warning: EXPERIMENTAL, use at your own risk");
+		set_opt(ctx, DAX_ALWAYS);
+		clear_opt(ctx, DAX_NEVER);
+		return true;
+	case EROFS_MOUNT_DAX_NEVER:
+		set_opt(ctx, DAX_NEVER);
+		clear_opt(ctx, DAX_ALWAYS);
+		return true;
+	default:
+		DBG_BUGON(1);
+		return false;
+	}
+#else
+	errorfc(fc, "dax options not supported");
+	return false;
+#endif
+}
 
 static int erofs_fc_parse_param(struct fs_context *fc,
 				struct fs_parameter *param)
@@ -411,6 +447,14 @@ static int erofs_fc_parse_param(struct fs_context *fc,
 #else
 		errorfc(fc, "compression not supported, cache_strategy ignored");
 #endif
+		break;
+	case Opt_dax:
+		if (!erofs_fc_set_dax_mode(fc, EROFS_MOUNT_DAX_ALWAYS))
+			return -EINVAL;
+		break;
+	case Opt_dax_enum:
+		if (!erofs_fc_set_dax_mode(fc, result.uint_32))
+			return -EINVAL;
 		break;
 	default:
 		return -ENOPARAM;
@@ -496,10 +540,16 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 		return -ENOMEM;
 
 	sb->s_fs_info = sbi;
+	sbi->dax_dev = fs_dax_get_by_bdev(sb->s_bdev);
 	err = erofs_read_superblock(sb);
 	if (err)
 		return err;
 
+	if (test_opt(ctx, DAX_ALWAYS) &&
+	    !bdev_dax_supported(sb->s_bdev, EROFS_BLKSIZ)) {
+		errorfc(fc, "DAX unsupported by block device. Turning off DAX.");
+		clear_opt(ctx, DAX_ALWAYS);
+	}
 	sb->s_flags |= SB_RDONLY | SB_NOATIME;
 	sb->s_maxbytes = MAX_LFS_FILESIZE;
 	sb->s_time_gran = 1;
@@ -609,6 +659,7 @@ static void erofs_kill_sb(struct super_block *sb)
 	sbi = EROFS_SB(sb);
 	if (!sbi)
 		return;
+	fs_put_dax(sbi->dax_dev);
 	kfree(sbi);
 	sb->s_fs_info = NULL;
 }
@@ -711,8 +762,8 @@ static int erofs_statfs(struct dentry *dentry, struct kstatfs *buf)
 
 static int erofs_show_options(struct seq_file *seq, struct dentry *root)
 {
-	struct erofs_sb_info *sbi __maybe_unused = EROFS_SB(root->d_sb);
-	struct erofs_fs_context *ctx __maybe_unused = &sbi->ctx;
+	struct erofs_sb_info *sbi = EROFS_SB(root->d_sb);
+	struct erofs_fs_context *ctx = &sbi->ctx;
 
 #ifdef CONFIG_EROFS_FS_XATTR
 	if (test_opt(ctx, XATTR_USER))
@@ -734,6 +785,10 @@ static int erofs_show_options(struct seq_file *seq, struct dentry *root)
 	else if (ctx->cache_strategy == EROFS_ZIP_CACHE_READAROUND)
 		seq_puts(seq, ",cache_strategy=readaround");
 #endif
+	if (test_opt(ctx, DAX_ALWAYS))
+		seq_puts(seq, ",dax=always");
+	if (test_opt(ctx, DAX_NEVER))
+		seq_puts(seq, ",dax=never");
 	return 0;
 }
 

jeevandeepj994/anolis_perf

ANBZ: #8559
commit 06252e9ce05b94b587e522667b85848a30197b15 upstream.
Link: https://lore.kernel.org/r/20210805003601.183063-3-hsiangkao@linux.alibaba.com
Reviewed-by: Chao Yu <chao@kernel.org>
Conflicts:
	fs/erofs/data.c
	fs/erofs/internal.h
	fs/erofs/super.c
[ Gao Xiang: multiple device support was landed before. ]
Acked-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2941

fs/erofs/super.c
@@ -12,6 +12,7 @@
 #include <linux/crc32c.h>
 #include <linux/backing-dev-defs.h>
 #include <linux/exportfs.h>
+#include <linux/dax.h>
 #include "xattr.h"
 
 #define CREATE_TRACE_POINTS
@@ -276,6 +277,7 @@ static int erofs_init_device(struct erofs_buf *buf, struct super_block *sb,
 		if (IS_ERR(bdev))
 			return PTR_ERR(bdev);
 		dif->bdev = bdev;
+		dif->dax_dev = fs_dax_get_by_bdev(bdev);
 	}
 
 	dif->blocks = le32_to_cpu(dis->blocks);
@@ -497,6 +499,7 @@ enum {
 	Opt_domain_id,
 	Opt_bootstrap_path,
 	Opt_blob_dir_path,
+	Opt_dax, Opt_dax_always, Opt_dax_never,
 	Opt_err
 };
 
@@ -511,6 +514,9 @@ static match_table_t erofs_tokens = {
 	{Opt_domain_id, "domain_id=%s"},
 	{Opt_bootstrap_path, "bootstrap_path=%s"},
 	{Opt_blob_dir_path, "blob_dir_path=%s"},
+	{Opt_dax_always, "dax=always"},
+	{Opt_dax_never, "dax=never"},
+	{Opt_dax, "dax"},
 	{Opt_err, NULL}
 };
 
@@ -625,10 +631,28 @@ static int erofs_parse_options(struct super_block *sb, char *options)
 				return -ENOMEM;
 			erofs_dbg("RAFS bootstrap_path %s", sbi->bootstrap_path);
 			break;
+#ifdef CONFIG_FS_DAX
+		case Opt_dax:
+		case Opt_dax_always:
+			set_opt(sbi, DAX_ALWAYS);
+			clear_opt(sbi, DAX_NEVER);
+			break;
+		case Opt_dax_never:
+			set_opt(sbi, DAX_NEVER);
+			clear_opt(sbi, DAX_ALWAYS);
+			break;
+#else
+		case Opt_dax:
+		case Opt_dax_always:
+		case Opt_dax_never:
+			erofs_err(sb, "dax options not supported");
+			break;
+#endif
 		default:
 			erofs_err(sb, "Unrecognized mount option \"%s\" or missing value", p);
 			return -EINVAL;
 		}
+		break;
 	}
 
 	if (sbi->blob_dir_path && !sbi->bootstrap_path) {
@@ -837,6 +861,7 @@ static int erofs_fill_super(struct super_block *sb, void *data, int silent)
 	if (err)
 		return err;
 
+	sbi->dax_dev = fs_dax_get_by_bdev(sb->s_bdev);
 	err = erofs_read_superblock(sb);
 	if (err)
 		return err;
@@ -854,6 +879,12 @@ static int erofs_fill_super(struct super_block *sb, void *data, int silent)
 			erofs_err(sb, "failed to set erofs blksize");
 			return -EINVAL;
 		}
+	}
+
+	if (test_opt(sbi, DAX_ALWAYS) &&
+	    !bdev_dax_supported(sb->s_bdev, sb->s_blocksize)) {
+		erofs_err(sb, "DAX unsupported by block device. Turning off DAX.");
+		clear_opt(sbi, DAX_ALWAYS);
 	}
 
 	if (test_opt(sbi, POSIX_ACL))
@@ -906,6 +937,7 @@ static int erofs_release_device_info(int id, void *ptr, void *data)
 {
 	struct erofs_device_info *dif = ptr;
 
+	fs_put_dax(dif->dax_dev);
 	if (dif->bdev)
 		blkdev_put(dif->bdev, FMODE_READ | FMODE_EXCL);
 	if (dif->blobfile)
@@ -998,6 +1030,7 @@ static void erofs_kill_sb(struct super_block *sb)
 	if (!sbi)
 		return;
 	erofs_free_dev_context(sbi->devs);
+	fs_put_dax(sbi->dax_dev);
 	if (sbi->bootstrap)
 		filp_close(sbi->bootstrap, NULL);
 	if (sbi->blob_dir_path)
@@ -1141,7 +1174,7 @@ static int erofs_statfs(struct dentry *dentry, struct kstatfs *buf)
 
 static int erofs_show_options(struct seq_file *seq, struct dentry *root)
 {
-	struct erofs_sb_info *sbi __maybe_unused = EROFS_SB(root->d_sb);
+	struct erofs_sb_info *sbi = EROFS_SB(root->d_sb);
 
 #ifdef CONFIG_EROFS_FS_XATTR
 	if (test_opt(sbi, XATTR_USER))
@@ -1170,6 +1203,10 @@ static int erofs_show_options(struct seq_file *seq, struct dentry *root)
 	if (sbi->domain_id)
 		seq_printf(seq, ",domain_id=%s", sbi->domain_id);
 #endif
+	if (test_opt(sbi, DAX_ALWAYS))
+		seq_puts(seq, ",dax=always");
+	if (test_opt(sbi, DAX_NEVER))
+		seq_puts(seq, ",dax=never");
 	return 0;
 }
 