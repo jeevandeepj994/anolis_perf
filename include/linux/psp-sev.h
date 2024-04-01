b0945c1daf573f87ad74a3ee23c024ef3382ba61
chench00 <chench@hygon.cn>
2024-04-01 11:02:27 +0800

    anolis: crypto: command co-processor: Add another mailbox interrupt support for PSP sending command to X86
    ANBZ: #8670
    
    The existing kernel supports only interrupt for the mailbox interface for X86
    sending commands to PSP and PSP to ack, e.g. the SEV commands. However, some
    PSP-based security modules in Hygon CPU, such as TPCM and TDM(Trusted Dynamic
    Measuring), needs sending commands/notifications proactively to X86 core via
    interrupt and a 2nd mailbox interface. Similar to the existing one, the 2nd
    mailbox consists of a 32-bits command register and two 32-bits data registers.
    
    The PSP interrupt handling needs to add this interrupt support; besides, in
    order to support user defined command handler, a callback registration function
    is also provided. Up to 16 command callbacks is supported, which are indexed by
    command IDs. Currently, command ID 0 is assigned to TPCM and 1 to TDM, while
    others are reserved.
    
    Signed-off-by: chench00 <chench@hygon.cn>

--- include/linux/psp-sev.h
+++ include/linux/psp-sev.h
@@ -631,6 +631,16 @@ struct csv_data_ring_buffer {
 	u16 int_on_empty;		/* In */
 } __packed;
 
+#ifdef CONFIG_HYGON_PSP2CPU_CMD
+
+typedef int (*p2c_notifier_t)(uint32_t id, uint64_t data);
+
+int psp_register_cmd_notifier(uint32_t cmd_id, int (*notifier)(uint32_t id, uint64_t data));
+
+int psp_unregister_cmd_notifier(uint32_t cmd_id, int (*notifier)(uint32_t id, uint64_t data));
+
+#endif
+
 #ifdef CONFIG_CRYPTO_DEV_SP_PSP
 
 int psp_do_cmd(int cmd, void *data, int *psp_ret);
