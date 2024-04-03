jeevandeepj994/anolis_perf

commit aa1ad269896c12677c90afe8544a582de1673dff

anolis: sched: introduce ACPU accounting
ANBZ: #8547

When SMT is on, tasks will be disturbed by the tasks on it's SMT
sibling, which will make the tasks running sometimes fast and sometimes
slowly. So far, there isn't any way to assess how much disturbance the
task has received.

To assess the SMT disturbance, we introduce ACPU(assess CPU), which will
account how long the task is running with SMT sibling idle. The
statistical data is shown in /proc/<pid>/sched, row se.core_sibidletime.
Only when kernel.sched_schedstats is on, the data will be counted and
shown.

Co-developed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2893

kernel/sched/core.c
@@ -155,6 +155,10 @@ __read_mostly int scheduler_running;
 
 DEFINE_STATIC_KEY_FALSE(__sched_core_enabled);
 
+#ifdef CONFIG_SCHED_ACPU
+DEFINE_STATIC_KEY_FALSE(acpu_enabled);
+#endif
+
 /* kernel prio, less is more */
 static inline int __task_prio(const struct task_struct *p)
 {
@@ -4989,6 +4993,83 @@ fire_sched_out_preempt_notifiers(struct task_struct *curr,
 
 #endif /* CONFIG_PREEMPT_NOTIFIERS */
 
+#ifdef CONFIG_SCHED_ACPU
+void acpu_enable(void)
+{
+	int i;
+
+	for_each_possible_cpu(i) {
+		struct rq *rq = cpu_rq(i);
+
+		/* It may be not that accurate, but useful enough. */
+		rq->last_acpu_update_time = rq->clock;
+	}
+	static_branch_enable(&acpu_enabled);
+}
+
+static void update_acpu(struct rq *rq, struct task_struct *prev, struct task_struct *next)
+{
+	const int cpu = cpu_of(rq);
+	const struct cpumask *smt_mask = cpu_smt_mask(cpu);
+	u64 now = rq_clock(rq);
+	u64 sibidle_sum, last_update_time;
+	s64 delta, last;
+	int i;
+
+	if (!static_branch_likely(&acpu_enabled) || !schedstat_enabled())
+		return;
+
+	/* Update idle sum and busy sum for current rq. */
+	delta = now - rq->last_acpu_update_time;
+	if (prev == rq->idle)
+		rq->acpu_idle_sum += delta;
+
+	/*
+	 * Be carefule, smt_mask maybe NULL.
+	 * We only consider the case where there are two SMT at this stage.
+	 */
+	if (unlikely(!smt_mask) || unlikely(cpumask_weight(smt_mask) != 2))
+		goto out;
+
+	for_each_cpu(i, smt_mask) {
+		if (i != cpu) {
+			struct rq *rq_i = cpu_rq(i);
+			struct task_struct *curr_i = rq_i->curr;
+
+			last = (s64)(rq->last_acpu_update_time -
+				     rq_i->last_acpu_update_time);
+			last_update_time = last >= 0 ? rq->last_acpu_update_time :
+						       rq_i->last_acpu_update_time;
+			/*
+			 * Sibling may update acpu at the same time, and it's
+			 * timestamp may be newer than this rq.
+			 */
+			delta = now - last_update_time;
+			delta = delta > 0 ? delta : 0;
+
+			/* Add the delta to improve accuracy. */
+			sibidle_sum = last >= 0 ? rq->sibidle_sum : rq_i->acpu_idle_sum;
+			if (curr_i == rq_i->idle)
+				sibidle_sum += delta;
+		}
+	}
+
+	if (prev != rq->idle) {
+		delta = sibidle_sum - rq->sibidle_sum;
+		delta = delta > 0 ? delta : 0;
+		__account_sibidle_time(prev, delta);
+	}
+
+	rq->sibidle_sum = sibidle_sum;
+out:
+	rq->last_acpu_update_time = now;
+}
+#else
+static inline void update_acpu(struct rq *rq, struct task_struct *prev, struct task_struct *next)
+{
+}
+#endif /* CONFIG_SCHED_ACPU */
+
 static inline void prepare_task(struct task_struct *next)
 {
 #ifdef CONFIG_SMP
@@ -5197,6 +5278,7 @@ prepare_task_switch(struct rq *rq, struct task_struct *prev,
 {
 	kcov_prepare_switch(prev);
 	sched_info_switch(rq, prev, next);
+	update_acpu(rq, prev, next);
 	perf_event_task_sched_out(prev, next);
 	rseq_preempt(prev);
 	fire_sched_out_preempt_notifiers(prev, next);
@@ -5669,6 +5751,7 @@ void scheduler_tick(void)
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
 	update_thermal_load_avg(rq_clock_thermal(rq), rq, thermal_pressure);
 	curr->sched_class->task_tick(rq, curr, 0);
+	update_acpu(rq, curr, curr);
 	if (sched_feat(LATENCY_WARN))
 		resched_latency = cpu_resched_latency(rq);
 	calc_global_load_tick(rq);
@@ -10054,6 +10137,12 @@ void __init sched_init(void)
 		rcuwait_init(&rq->hotplug_wait);
 #endif
 #endif /* CONFIG_SMP */
+
+#ifdef CONFIG_SCHED_ACPU
+		rq->acpu_idle_sum = 0;
+		rq->sibidle_sum = 0;
+		rq->last_acpu_update_time = rq->clock;
+#endif
 		hrtick_rq_init(rq);
 		atomic_set(&rq->nr_iowait, 0);
 

include/linux/kernel_stat.h
@@ -31,6 +31,9 @@ enum cpu_usage_stat {
 #ifdef CONFIG_SCHED_CORE
 	CPUTIME_FORCEIDLE,
 #endif
+#ifdef CONFIG_SCHED_ACPU
+	CPUTIME_SIBIDLE,
+#endif
 	NR_STATS,
 };
 
@@ -132,6 +135,9 @@ extern void account_idle_ticks(unsigned long ticks);
 
 #ifdef CONFIG_SCHED_CORE
 extern void __account_forceidle_time(struct task_struct *tsk, u64 delta);
+#endif
+#ifdef CONFIG_SCHED_ACPU
+extern void __account_sibidle_time(struct task_struct *tsk, u64 delta);
 #endif
 
 #endif /* _LINUX_KERNEL_STAT_H */

include/linux/sched.h
@@ -542,6 +542,10 @@ struct sched_statistics {
 #ifdef CONFIG_SCHED_CORE
 	u64				core_forceidle_sum;
 #endif
+#ifdef CONFIG_SCHED_ACPU
+	u64				core_sibidle_sum;
+#endif
+
 #endif /* CONFIG_SCHEDSTATS */
 } ____cacheline_aligned;
 
@@ -2494,4 +2498,10 @@ static inline int sched_core_idle_cpu(int cpu) { return idle_cpu(cpu); }
 
 extern void sched_set_stop_task(int cpu, struct task_struct *stop);
 
+#endif
+
+#ifdef CONFIG_SCHED_ACPU
+extern void acpu_enable(void);
+#else
+static inline void acpu_enable(void) { }
 #endif

kernel/sched/cputime.c
@@ -244,6 +244,14 @@ void __account_forceidle_time(struct task_struct *p, u64 delta)
 	task_group_account_field(p, CPUTIME_FORCEIDLE, delta);
 }
 #endif
+#ifdef CONFIG_SCHED_ACPU
+void __account_sibidle_time(struct task_struct *p, u64 delta)
+{
+	__schedstat_add(p->stats.core_sibidle_sum, delta);
+
+	task_group_account_field(p, CPUTIME_SIBIDLE, delta);
+}
+#endif
 
 /*
  * When a guest is interrupted for a longer amount of time, missed clock

kernel/sched/debug.c
@@ -1060,6 +1060,9 @@ void proc_sched_show_task(struct task_struct *p, struct pid_namespace *ns,
 #ifdef CONFIG_SCHED_CORE
 		PN_SCHEDSTAT(core_forceidle_sum);
 #endif
+#ifdef CONFIG_SCHED_ACPU
+		PN_SCHEDSTAT(core_sibidle_sum);
+#endif
 	}
 
 	__P(nr_switches);

kernel/sched/sched.h
@@ -1166,6 +1166,12 @@ struct rq {
 	call_single_data_t	cfsb_csd;
 	struct list_head	cfsb_csd_list;
 #endif
+
+#ifdef CONFIG_SCHED_ACPU
+	u64 acpu_idle_sum;
+	u64 sibidle_sum;
+	u64 last_acpu_update_time;
+#endif
 };
 
 #ifdef CONFIG_FAIR_GROUP_SCHED

kernel/smpboot.c
@@ -73,6 +73,7 @@ void __init idle_threads_init(void)
 		if (cpu != boot_cpu)
 			idle_init(cpu);
 	}
+	acpu_enable();
 }
 #endif
 

lib/Kconfig.debug
@@ -1255,6 +1255,13 @@ config SCHEDSTATS
 	  application, you can say N to avoid the very slight overhead
 	  this adds.
 
+config SCHED_ACPU
+	bool "ACPU info: account idle time of smt to task"
+	depends on DEBUG_KERNEL && PROC_FS && SMP && SCHED_SMT
+	default y
+	help
+	  Add ACPU info in /proc/<pid>/sched.
+
 endmenu
 
 config DEBUG_TIMEKEEPING

commit e6086aee5cf80ff6b45e70d1c68bcd21f703fddc

anolis: sched: fix compile error when CONFIG_SCHED_CORE is disabled
ANBZ: #8698

When CONFIG_SCHED_CORE is disabled, acpu_enabled and
sysctl_sched_cfs_bw_burst_onset_percent cannot be found because they
are defined under CONFIG_SCHED_CORE. This patch is to fix this problem.

Fixes: aa1ad269896c("anolis: sched: introduce ACPU accounting")
Fixes: 55a1c37487b1("anolis: sched/fair: Make CFS bandwidth controller burstable")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/core.c
@@ -151,14 +151,22 @@ const_debug unsigned int sysctl_sched_nr_migrate = SCHED_NR_MIGRATE_BREAK;
 
 __read_mostly int scheduler_running;
 
-#ifdef CONFIG_SCHED_CORE
-
-DEFINE_STATIC_KEY_FALSE(__sched_core_enabled);
-
 #ifdef CONFIG_SCHED_ACPU
 DEFINE_STATIC_KEY_FALSE(acpu_enabled);
 unsigned int sysctl_sched_acpu_enabled;
 #endif
+
+#ifdef CONFIG_CFS_BANDWIDTH
+/*
+ * Percent of burst assigned to cfs_b->runtime on tg_set_cfs_bandwidth,
+ * 0 by default.
+ */
+unsigned int sysctl_sched_cfs_bw_burst_onset_percent;
+#endif
+
+#ifdef CONFIG_SCHED_CORE
+
+DEFINE_STATIC_KEY_FALSE(__sched_core_enabled);
 
 /* kernel prio, less is more */
 static inline int __task_prio(const struct task_struct *p)
@@ -174,14 +182,6 @@ static inline int __task_prio(const struct task_struct *p)
 
 	return MAX_RT_PRIO + MAX_NICE; /* 120, squash fair */
 }
-
-#ifdef CONFIG_CFS_BANDWIDTH
-/*
- * Percent of burst assigned to cfs_b->runtime on tg_set_cfs_bandwidth,
- * 0 by default.
- */
-unsigned int sysctl_sched_cfs_bw_burst_onset_percent;
-#endif
 
 /*
  * l(a,b)