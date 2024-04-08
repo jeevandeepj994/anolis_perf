4cecd612e98e7f67662bb3468916854062b2a845
leoliu-oc <leoliu-oc@zhaoxin.com>
2024-04-08 17:25:52 +0800

    anolis: KVM: x86: Introduce support for Zhaoxin ZXPAUSE instruction
    ANBZ: #7809
    
    This patch introduces support for the ZXPAUSE instruction, a new addition
    akin to Intel's TPAUSE. Two primary distinctions set apart ZXPAUSE from
    TPAUSE:
    
    1. ZXPAUSE utilizes a delta tsc, determined from the lesser value between
       (MSR_ZX_PAUSE_CONTROL[31:2] << 2) and the EDX:EAX input to the ZXPAUSE
       instruction, subtracted from the current tsc value.
    
       In contrast, TPAUSE employs a target tsc, computed from the lesser
       value between (MSR_IA32_UMWAIT_CONTROL[31:2] << 2) and the EDX:EAX
       input to the TPAUSE instruction.
    
    2. As of now, ZXPAUSE exclusively supports the C0.1 optimization state,
       whereas TPAUSE potentially extends support to both C0.1 and C0.2.
    
    Successful integration of this patch hinges on QEMU's backing for ZXPAUSE,
    a contribution we're currently forwarding to QEMU. It also requires the
    preceding patch in this patchset, which offers Linux kernel support for
    ZXPAUSE.
    
    The choice of the name "vmx->msr_ia32_umwait_control" is deliberate. In
    patches for other Linux versions (e.g., 5.5), a
    "vmx->msr_ia32_umwait_control" already exists. By sharing this variable
    name with Intel, it ensures compatibility. The difference is merely
    software-based and poses no real-world conflicts.
    
    Currently, if the Guest writes to the ZXPAUSE/TPAUSE CONTROL MSR, we
    simply bypass the WRMSR instruction. If the Guest attempts to use
    ZXPAUSE/TPAUSE to transition the vCPU into an optimized state, it will
    succeed, with the duration of the optimized state being the value passed
    in EDX:EAX.
    
    Of course, this state can be interrupted by external interrupts and other
    events specified in the specification.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1462,8 +1462,8 @@ static const u32 msrs_to_save_base[] = {
 	MSR_IA32_RTIT_ADDR2_A, MSR_IA32_RTIT_ADDR2_B,
 	MSR_IA32_RTIT_ADDR3_A, MSR_IA32_RTIT_ADDR3_B,
 	MSR_IA32_UMWAIT_CONTROL,
-
 	MSR_IA32_XFD, MSR_IA32_XFD_ERR,
+	MSR_ZX_PAUSE_CONTROL,
 };
 
 static const u32 msrs_to_save_pmu[] = {
@@ -7165,6 +7165,10 @@ static void kvm_probe_msr_to_save(u32 msr_index)
 		break;
 	case MSR_IA32_UMWAIT_CONTROL:
 		if (!kvm_cpu_cap_has(X86_FEATURE_WAITPKG))
+			return;
+		break;
+	case MSR_ZX_PAUSE_CONTROL:
+		if (!kvm_cpu_cap_has(X86_FEATURE_ZXPAUSE))
 			return;
 		break;
 	case MSR_IA32_RTIT_CTL:
