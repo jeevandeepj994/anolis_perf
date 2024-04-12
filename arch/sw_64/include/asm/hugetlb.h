22e59f044fe6741bd8cb966bc44c899c4e3dd728
Gu Zitao <guzitao@wxiat.com>
2024-04-12 09:04:21 +0800

    anolis: sw64: fix compile errors for huge_ptep_clear_flush()
    ANBZ: #4688
    
    These compile errors are caused by commit ae07562909f3 ("mm: change
    huge_ptep_clear_flush() to return the original pte").  Now fix them
    according to upstream.
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    Reviewed-by: WU Hao <fuyuan@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3076

--- arch/sw_64/include/asm/hugetlb.h
+++ arch/sw_64/include/asm/hugetlb.h
@@ -18,7 +18,7 @@ extern pte_t huge_ptep_get_and_clear(struct mm_struct *mm,
 					unsigned long addr, pte_t *ptep);
 
 #define __HAVE_ARCH_HUGE_PTEP_CLEAR_FLUSH
-extern void huge_ptep_clear_flush(struct vm_area_struct *vma, unsigned long addr,
+extern pte_t huge_ptep_clear_flush(struct vm_area_struct *vma, unsigned long addr,
 					pte_t *ptep);
 
 #define __HAVE_ARCH_HUGE_PTEP_SET_WRPROTECT
