jeevandeepj994/anolis_perf

commit 01a6cd39d511c3128e5784a75638ad672471d585

anolis: KVM: arm64: Only access MPAM registers when MPAM is enabled
ANBZ: #7198

The commmit dda408b60b40 ("KVM: arm64: Trap guest accesses to the MPAM
registers") will access MPAM registers like MPAM2_EL2 when
mpam_cpus_have_feature() returns true. However, mpam_cpus_have_feature()
only check the MPAM bits in ID_AA64PFR0_EL1. But in some BIOS, it is also
necessary to add a check for the existence of the MPAM ACPI table.
Otherwise, access to these registers will cause the system to crash.

To fix this error, add a condition on mpam_enabled. mpam_enabled is only
true if the MPAM ACPI table exists and the host kernel enables MPAM.

Fixes: dda408b60b40 ("KVM: arm64: Trap guest accesses to the MPAM registers")
Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2437

arch/arm64/kvm/hyp/include/hyp/switch.h
@@ -86,7 +86,7 @@ static inline void  __activate_traps_mpam(struct kvm_vcpu *vcpu)
 {
 	u64 r = MPAM_SYSREG_TRAP_MPAM0_EL1 | MPAM_SYSREG_TRAP_MPAM1_EL1;
 
-	if (!mpam_cpus_have_feature())
+	if (!mpam_cpus_have_feature() || !static_branch_likely(&mpam_enabled))
 		return;
 
 	/* trap guest access to MPAMIDR_EL1 */
@@ -102,7 +102,7 @@ static inline void  __activate_traps_mpam(struct kvm_vcpu *vcpu)
 
 static inline void __deactivate_traps_mpam(void)
 {
-	if (!mpam_cpus_have_feature())
+	if (!mpam_cpus_have_feature() || !static_branch_likely(&mpam_enabled))
 		return;
 
 	write_sysreg_s(0, SYS_MPAM2_EL2);

arch/arm64/kernel/image-vars.h
@@ -82,6 +82,7 @@ KVM_NVHE_ALIAS(cpu_hwcap_keys);
 /* Additional static keys for cpufeatures */
 #ifdef CONFIG_ARM64_MPAM
 KVM_NVHE_ALIAS(arm64_mpam_has_hcr);
+KVM_NVHE_ALIAS(mpam_enabled);
 #endif
 
 /* Static keys which are set if a vGIC trap should be handled in hyp. */

arch/arm64/kvm/hyp/include/hyp/sysreg-sr.h
@@ -202,7 +202,8 @@ static inline void __sysreg32_restore_state(struct kvm_vcpu *vcpu)
  */
 static inline void __mpam_guest_load(void)
 {
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature())
+	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature() &&
+	    static_branch_likely(&mpam_enabled))
 		write_sysreg_el1(read_sysreg_s(SYS_MPAM0_EL1), SYS_MPAM1);
 }
 
@@ -216,7 +217,8 @@ static inline void __mpam_guest_put(void)
 	u64 val, mask = MPAM_SYSREG_PMG_D | MPAM_SYSREG_PMG_I |
 			MPAM_SYSREG_PARTID_D | MPAM_SYSREG_PARTID_I;
 
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature()) {
+	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature() &&
+	    static_branch_likely(&mpam_enabled)) {
 		val = FIELD_GET(mask, read_sysreg_s(SYS_MPAM2_EL2));
 		write_sysreg_el1(val, SYS_MPAM1);
 	}

arch/arm64/kvm/hyp/nvhe/switch.c
@@ -165,7 +165,8 @@ static void __pmu_switch_to_host(struct kvm_cpu_context *host_ctxt)
 /* Use the host thread's partid and pmg for world switch */
 static void __mpam_copy_el1_to_el2(void)
 {
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature())
+	if (IS_ENABLED(CONFIG_ARM64_MPAM) && mpam_cpus_have_feature() &&
+	    static_branch_likely(&mpam_enabled))
 		write_sysreg_s(read_sysreg_s(SYS_MPAM1_EL1), SYS_MPAM2_EL2);
 }
 

commit 1546d8d328b6e1597cc23c60fc1e630e612a803c

anolis: KVM: arm64: Fix mpam_enabled undefined error when KVM is built as a module
ANBZ: #8781

When KVM is built as a module, it cannot find the defination of
mpam_enabled since this symbol is not exported.

Besides, when MPAM is not enabled, functions for MPAM in KVM should not
check the mpam_enabled.

Fixes: 01a6cd39d511 ("anolis: KVM: arm64: Only access MPAM registers when MPAM is enabled")
Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>

arch/arm64/kvm/hyp/include/hyp/switch.h
@@ -86,7 +86,8 @@ static inline void  __activate_traps_mpam(struct kvm_vcpu *vcpu)
 {
 	u64 r = MPAM_SYSREG_TRAP_MPAM0_EL1 | MPAM_SYSREG_TRAP_MPAM1_EL1;
 
-	if (!mpam_cpus_have_feature() || !static_branch_likely(&mpam_enabled))
+	if (!IS_ENABLED(CONFIG_ARM64_MPAM) || !mpam_cpus_have_feature() ||
+	    !static_branch_likely(&mpam_enabled))
 		return;
 
 	/* trap guest access to MPAMIDR_EL1 */
@@ -102,7 +103,8 @@ static inline void  __activate_traps_mpam(struct kvm_vcpu *vcpu)
 
 static inline void __deactivate_traps_mpam(void)
 {
-	if (!mpam_cpus_have_feature() || !static_branch_likely(&mpam_enabled))
+	if (!IS_ENABLED(CONFIG_ARM64_MPAM) || !mpam_cpus_have_feature() ||
+	    !static_branch_likely(&mpam_enabled))
 		return;
 
 	write_sysreg_s(0, SYS_MPAM2_EL2);

drivers/platform/mpam/mpam_devices.c
@@ -36,6 +36,7 @@
 extern int ddr_cpufreq;
 
 DEFINE_STATIC_KEY_FALSE(mpam_enabled);
+EXPORT_SYMBOL(mpam_enabled);
 
 /*
  * mpam_list_lock protects the RCU lists when writing. Once the