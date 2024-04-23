jeevandeepj994/anolis_perf

commit 6cac472dcc7e92831411f0ce109f6c59e1d9cb58

anolis: sw64: introduce new entry framework for C4
ANBZ: #4688

The pt_regs used to be partly handled by firmware, which limits
the extension of os because of compatibility.

This patch introduces new entry framework for Core4. Firmware is
responsible for preparing the necessary parameters when exception
or interrupt occurs and saving them to the specified CSRs. struct
pt_regs is accessed by the kernel only and ksp is saved to struct
pcb_struct instead of struct vcpucb.

For guest os, since SAVE_ALL may be interrupted by host IRQ, we
have to reserve enough space in vcpucb to save these CSRs.

Due to a flaw of hardware, when there are consecutive write and
read on a CSR in a recent period, the read may be issued before
write in some cases, leading to a wrong result. It has to read
the CSR twice to avoid this bug.

Signed-off-by: Wu Liliu <wuliliu@wxiat.com>
Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>
Reviewed-by: WU Hao <fuyuan@linux.alibaba.com>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3076

arch/sw_64/include/asm/hmcall.h
@@ -155,9 +155,7 @@ __CALL_HMC_VOID(sleepen);
 __CALL_HMC_VOID(mtinten);
 
 __CALL_HMC_VOID(rdktp);
-#define restore_ktp()	rdktp()
 __CALL_HMC_VOID(wrktp);
-#define save_ktp()	wrktp()
 
 __CALL_HMC_R0(rdps, unsigned long);
 __CALL_HMC_R0(rvpcr, unsigned long);

arch/sw_64/kernel/early_init.c
@@ -6,6 +6,5 @@
 asmlinkage __visible void __init sw64_start_kernel(void)
 {
 	fixup_hmcall();
-	save_ktp();
 	start_kernel();
 }

arch/sw_64/kernel/head.S
@@ -10,6 +10,15 @@
 #include <asm/asm-offsets.h>
 #include <asm/hmcall.h>
 #include <asm/setup.h>
+#include <asm/csr.h>
+
+	.macro SAVE_KTP
+#ifdef CONFIG_SUBARCH_C3B
+	sys_call HMC_wrktp
+#else
+	csrw	$8, CSR_KTP
+#endif
+	.endm
 
 __HEAD
 	.globl _stext
@@ -23,6 +32,7 @@ __start:
 1:	ldgp	$29, 0($27)
 	/* We need to get current_task_info loaded up...  */
 	ldi	$8, init_task
+	SAVE_KTP
 	ldl	$30, TASK_STACK($8)
 	/* ... and find our stack ... */
 	ldi	$30, ASM_THREAD_SIZE($30)
@@ -88,7 +98,7 @@ __smp_callin:
 	ldi	$2, idle_task_pointer
 	s8addl	$0, $2, $2
 	ldl	$8, 0($2)	# Get ksp of idle thread
-	sys_call HMC_wrktp
+	SAVE_KTP
 
 	ldl	$30, TASK_STACK($8)
 	ldi	$30, ASM_THREAD_SIZE($30)

arch/sw_64/include/asm/csr.h
@@ -46,6 +46,15 @@
 #define CSR_DV_MASK		0x57
 #define CSR_IDA_MATCH		0xc5
 #define CSR_IDA_MASK		0xc6
+#define CSR_BASE_KREGS		0xe0
+#define CSR_PS			0xe8
+#define CSR_PC			0xe9
+#define CSR_EARG0		0xea
+#define CSR_EARG1		0xeb
+#define CSR_EARG2		0xec
+#define CSR_SCRATCH		0xed
+#define CSR_SP			0xee
+#define CSR_KTP			0xef
 
 #define DA_MATCH_EN_S		4
 #define DV_MATCH_EN_S		6

arch/sw_64/include/asm/kvm.h
@@ -1,0 +1,142 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_KVM_H
+#define _ASM_SW64_KVM_H
+#include <uapi/asm/kvm.h>
+
+/*
+ * KVM SW specific structures and definitions.
+ */
+#define SWVM_IRQS 256
+#define IRQ_PENDING_INTX_SHIFT		16
+#define IRQ_PENDING_MSI_VECTORS_SHIFT	17
+
+#define SWVM_NUM_NUMA_MEMBANKS	1
+
+/*
+ * for KVM_GET_REGS and KVM_SET_REGS
+ */
+#ifdef CONFIG_SUBARCH_C3B
+struct kvm_regs {
+	unsigned long r0;
+	unsigned long r1;
+	unsigned long r2;
+	unsigned long r3;
+
+	unsigned long r4;
+	unsigned long r5;
+	unsigned long r6;
+	unsigned long r7;
+
+	unsigned long r8;
+	unsigned long r9;
+	unsigned long r10;
+	unsigned long r11;
+
+	unsigned long r12;
+	unsigned long r13;
+	unsigned long r14;
+	unsigned long r15;
+
+	unsigned long r19;
+	unsigned long r20;
+	unsigned long r21;
+	unsigned long r22;
+
+	unsigned long r23;
+	unsigned long r24;
+	unsigned long r25;
+	unsigned long r26;
+
+	unsigned long r27;
+	unsigned long r28;
+	unsigned long __padding0;
+	unsigned long fpcr;
+
+	unsigned long fp[124];
+	/* These are saved by HMcode: */
+	unsigned long ps;
+	unsigned long pc;
+	unsigned long gp;
+	unsigned long r16;
+	unsigned long r17;
+	unsigned long r18;
+};
+
+#elif CONFIG_SUBARCH_C4
+struct kvm_regs {
+	unsigned long r0;
+	unsigned long r1;
+	unsigned long r2;
+	unsigned long r3;
+
+	unsigned long r4;
+	unsigned long r5;
+	unsigned long r6;
+	unsigned long r7;
+
+	unsigned long r8;
+	unsigned long r9;
+	unsigned long r10;
+	unsigned long r11;
+
+	unsigned long r12;
+	unsigned long r13;
+	unsigned long r14;
+	unsigned long r15;
+
+	unsigned long r16;
+	unsigned long r17;
+	unsigned long r18;
+	unsigned long r19;
+
+	unsigned long r20;
+	unsigned long r21;
+	unsigned long r22;
+	unsigned long r23;
+
+	unsigned long r24;
+	unsigned long r25;
+	unsigned long r26;
+	unsigned long r27;
+
+	unsigned long r28;
+	unsigned long gp;
+	unsigned long sp;
+	unsigned long fpcr;
+
+	unsigned long fp[124];
+	unsigned long ps;
+	unsigned long pc;
+};
+#endif
+
+/*
+ * return stack for __sw64_vcpu_run
+ */
+struct vcpu_run_ret_stack {
+	unsigned long ra;
+	unsigned long r0;
+};
+
+struct host_int_args {
+	unsigned long r18;
+	unsigned long r17;
+	unsigned long r16;
+};
+
+struct hcall_args {
+	unsigned long arg0, arg1, arg2;
+};
+
+struct swvm_mem_bank {
+	unsigned long guest_phys_addr;
+	unsigned long host_phys_addr;
+	unsigned long host_addr;
+	unsigned long size;
+};
+
+struct swvm_mem {
+	struct swvm_mem_bank membank[SWVM_NUM_NUMA_MEMBANKS];
+};
+
+#endif /* _ASM_SW64_KVM_H */

arch/sw_64/include/asm/ptrace.h
@@ -31,9 +31,9 @@ struct pt_regs {
 	unsigned long hm_ps;
 	unsigned long hm_pc;
 	unsigned long hm_gp;
-	unsigned long hm_r16;
-	unsigned long hm_r17;
-	unsigned long hm_r18;
+	unsigned long earg0;
+	unsigned long earg1;
+	unsigned long earg2;
 };
 
 #define arch_has_single_step()		(1)

arch/sw_64/include/asm/thread_info.h
@@ -14,6 +14,7 @@ typedef struct {
 
 
 struct pcb_struct {
+	unsigned long ksp;
 	unsigned long tp;
 	unsigned long da_match, da_mask;
 	unsigned long dv_match, dv_mask;

arch/sw_64/include/asm/vcpu.h
@@ -55,7 +55,7 @@ struct vcpucb {
 	unsigned long reserved[3];
 };
 
-#else
+#elif CONFIG_SUBARCH_C4
 
 struct vcpucb {
 	unsigned long ktp;
@@ -100,7 +100,14 @@ struct vcpucb {
 	unsigned long shtclock_offset;
 	unsigned long migration_mark;
 	unsigned long shtclock;
-	unsigned long reserved[6];
+	unsigned long csr_pc;
+	unsigned long csr_ps;
+	unsigned long csr_sp;
+	unsigned long csr_ktp;
+	unsigned long csr_earg0;
+	unsigned long csr_earg1;
+	unsigned long csr_earg2;
+	unsigned long csr_scratch;
 };
 #endif
 

commit 3cc019ef57678a919614046e0d33aaf000d2a740

anolis: sw64: fix save_ktp compatibility for C3
ANBZ: #4688

Commit 6cac472dcc7e ("anolis: sw64: introduce new entry framework for C4") moves
save_ktp() to head.S, and breaks compatibility with old firmware because
hmcall_wrktp was not implemented.

To fix it, save_ktp() should come after fixup_hmcall().

Fixes: 6cac472dcc7e ("anolis: sw64: introduce new entry framework for C4")
Signed-off-by: Wu Liliu <wuliliu@wxiat.com>
Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>

arch/sw_64/include/asm/hmcall.h
@@ -230,13 +230,18 @@ __CALL_HMC_W1(wrtp, unsigned long);
 /* Invalidate all user TLB with current UPN and VPN */
 #define tbiu()		__tbi(4, /* no second argument */)
 
-#ifndef CONFIG_SUBARCH_C3B
+#if defined(CONFIG_SUBARCH_C4)
 __CALL_HMC_W2(wrap_asid, unsigned long, unsigned long);
-#else
+static inline void save_ktp(void)
+{
+	__asm__ __volatile__("csrw $8, 0xef");
+}
+#elif defined(CONFIG_SUBARCH_C3B)
 static inline void wrap_asid(unsigned long asid, unsigned long ptbr)
 {
 	tbivp();
 }
+#define save_ktp()     wrktp()
 #endif
 
 #endif /* !__ASSEMBLY__ */

arch/sw_64/kernel/early_init.c
@@ -6,5 +6,6 @@
 asmlinkage __visible void __init sw64_start_kernel(void)
 {
 	fixup_hmcall();
+	save_ktp();
 	start_kernel();
 }

arch/sw_64/kernel/head.S
@@ -32,7 +32,6 @@ __start:
 1:	ldgp	$29, 0($27)
 	/* We need to get current_task_info loaded up...  */
 	ldi	$8, init_task
-	SAVE_KTP
 	ldl	$30, TASK_STACK($8)
 	/* ... and find our stack ... */
 	ldi	$30, ASM_THREAD_SIZE($30)