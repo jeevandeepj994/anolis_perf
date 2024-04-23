jeevandeepj994/anolis_perf

commit 52249b01d37b78a3347a72d4aaafcf4605bf9a02

anolis: sched/fair: introduce sched_feat ID_ABSOLUTE_EXPEL
ANBZ: #7705

Introduce ID_ABSOLUTE_EXPEL to expel underclass absolutely, that is,
if there is highclass or normal sched_entity in cfs_rq, underclass
will never be picked.

To prevent priority inversion once ID_ABSOLUTE_EXPEL is turned off,
we record expel start when we pick a highclass or normal sched
entity, to promise min_under_vruntime will not be ahead of
min_vruntime because of ID_ABSOLUTE_EXPEL.

The default value of ID_ABSOLUTE_EXPEL is false, and it is recommended
to turn it on at the same time as ID_RESCUE_EXPELLEE to prevent
underclass starvation.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2497

kernel/sched/fair.c
@@ -1734,16 +1734,25 @@ id_rb_first_cached(struct cfs_rq *cfs_rq)
 	 */
 	update_expel_spread(cfs_rq);
 
-	if (cfs_rq->min_under_vruntime + get_expel_spread(cfs_rq) <
-	    cfs_rq->min_vruntime) {
-		roots[0] = &cfs_rq->under_timeline;
-		roots[1] = &cfs_rq->tasks_timeline;
+	if (!sched_feat(ID_ABSOLUTE_EXPEL)) {
+		if (cfs_rq->min_under_vruntime + get_expel_spread(cfs_rq) <
+		    cfs_rq->min_vruntime) {
+			roots[0] = &cfs_rq->under_timeline;
+			roots[1] = &cfs_rq->tasks_timeline;
+		}
 	}
 
 	for (i = 0; i < 2; i++) {
 		left = rb_first_cached(roots[i]);
-		if (left)
+		if (left) {
+			/* To prevent priority inversion once ID_ABSOLUTE_EXPEL
+			 * is turned off.
+			 */
+			if (sched_feat(ID_ABSOLUTE_EXPEL) &&
+			    !is_underclass(rb_entry(left, struct sched_entity, run_node)))
+				update_expel_start(cfs_rq, NULL);
 			return left;
+		}
 	}
 
 	return NULL;

kernel/sched/features.h
@@ -105,6 +105,7 @@ SCHED_FEAT(ID_EXPELLEE_NEVER_HOT, false)
 SCHED_FEAT(ID_LAST_HIGHCLASS_STAY, false)
 SCHED_FEAT(ID_LOOSE_EXPEL, false)
 SCHED_FEAT(ID_EXPELLER_SHARE_CORE, true)
+SCHED_FEAT(ID_ABSOLUTE_EXPEL, false)
 #endif
 
 #ifdef CONFIG_SCHED_CORE

commit 30fa7f0fbff8dd702970e89ea708a8dec73a2009

anolis: sched/fair: fix invalid ID_ABSOLUTE_EXPEL without CONFIG_SCHED_SMT
ANBZ: #8835

If CONFIG_SCHED_SMT is turned off, ID_ABSOLUTE_EXPELL will be invalid,
because update_expel_start() is a NULL function, resulting that there's no
chance to adjust vruntime to make underclass lag, and underclass tasks
will get a chance to run, unexpectedly.

To fix this problem, we just change the logic of id_vruntime_before(),
letting the vruntime of highclass and normal sched_entity always be
before underclass sched_entity.

Fixes: 52249b01d37b("anolis: sched/fair: introduce sched_feat ID_ABSOLUTE_EXPEL")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/fair.c
@@ -1271,17 +1271,10 @@ hierarchy_update_nr_expel_immune(struct sched_entity *se, long delta)
 	}
 }
 
-#ifdef CONFIG_SCHED_SMT
 static inline u64 get_expel_spread(struct cfs_rq *cfs_rq)
 {
 	return cfs_rq->expel_spread;
 }
-#else
-static inline u64 get_expel_spread(struct cfs_rq *cfs_rq)
-{
-	return 0;
-}
-#endif
 
 static void __enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se);
 static void __dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se);
@@ -1657,7 +1650,6 @@ id_rb_root(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	return &cfs_rq->tasks_timeline;
 }
 
-#ifdef CONFIG_SCHED_SMT
 static inline void
 update_expel_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
@@ -1700,6 +1692,7 @@ static inline void update_expel_spread(struct cfs_rq *cfs_rq)
 	cfs_rq->expel_start = 0;
 }
 
+#ifdef CONFIG_SCHED_SMT
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial);
 
@@ -1737,15 +1730,6 @@ static inline struct rb_node *skip_expellee_se(struct cfs_rq *cfs_rq)
 	return left;
 }
 #else
-static inline void
-update_expel_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
-{
-}
-
-static inline void update_expel_spread(struct cfs_rq *cfs_rq)
-{
-}
-
 static inline void check_expellee_se(struct cfs_rq *cfs_rq)
 {
 }
@@ -1851,8 +1835,14 @@ static inline u64 id_vruntime(struct sched_entity *se)
 static inline bool
 id_entity_before(struct sched_entity *a, struct sched_entity *b)
 {
+	bool a_is_underclass = __is_underclass(a);
+	bool b_is_underclass = __is_underclass(b);
+
 	if (group_identity_disabled())
 		return entity_before(a, b);
+
+	if (sched_feat(ID_ABSOLUTE_EXPEL) && a_is_underclass != b_is_underclass)
+		return b_is_underclass;
 
 	return (s64)(__id_vruntime(a) - __id_vruntime(b)) < 0;
 }

kernel/sched/sched.h
@@ -785,11 +785,13 @@ struct cfs_rq {
 #endif
 #if defined(CONFIG_GROUP_IDENTITY) && !defined(CONFIG_SCHED_SMT)
 	CK_KABI_USE(3, unsigned int h_nr_expel_immune)
+	CK_KABI_USE(4, u64 expel_spread)
+	CK_KABI_USE(5, u64 expel_start)
 #else
 	CK_KABI_RESERVE(3)
-#endif
 	CK_KABI_RESERVE(4)
 	CK_KABI_RESERVE(5)
+#endif
 	CK_KABI_RESERVE(6)
 	CK_KABI_RESERVE(7)
 	CK_KABI_RESERVE(8)