jeevandeepj994/anolis_perf

commit 52249b01d37b78a3347a72d4aaafcf4605bf9a02

anolis: sched/fair: introduce sched_feat ID_ABSOLUTE_EXPEL
ANBZ: #7705

Introduce ID_ABSOLUTE_EXPEL to expel underclass absolutely, that is,
if there is highclass or normal sched_entity in cfs_rq, underclass
will never be picked.

To prevent priority inversion once ID_ABSOLUTE_EXPEL is turned off,
we record expel start when we pick a highclass or normal sched
entity, to promise min_under_vruntime will not be ahead of
min_vruntime because of ID_ABSOLUTE_EXPEL.

The default value of ID_ABSOLUTE_EXPEL is false, and it is recommended
to turn it on at the same time as ID_RESCUE_EXPELLEE to prevent
underclass starvation.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2497

kernel/sched/fair.c
@@ -1734,16 +1734,25 @@ id_rb_first_cached(struct cfs_rq *cfs_rq)
 	 */
 	update_expel_spread(cfs_rq);
 
-	if (cfs_rq->min_under_vruntime + get_expel_spread(cfs_rq) <
-	    cfs_rq->min_vruntime) {
-		roots[0] = &cfs_rq->under_timeline;
-		roots[1] = &cfs_rq->tasks_timeline;
+	if (!sched_feat(ID_ABSOLUTE_EXPEL)) {
+		if (cfs_rq->min_under_vruntime + get_expel_spread(cfs_rq) <
+		    cfs_rq->min_vruntime) {
+			roots[0] = &cfs_rq->under_timeline;
+			roots[1] = &cfs_rq->tasks_timeline;
+		}
 	}
 
 	for (i = 0; i < 2; i++) {
 		left = rb_first_cached(roots[i]);
-		if (left)
+		if (left) {
+			/* To prevent priority inversion once ID_ABSOLUTE_EXPEL
+			 * is turned off.
+			 */
+			if (sched_feat(ID_ABSOLUTE_EXPEL) &&
+			    !is_underclass(rb_entry(left, struct sched_entity, run_node)))
+				update_expel_start(cfs_rq, NULL);
 			return left;
+		}
 	}
 
 	return NULL;

kernel/sched/features.h
@@ -105,6 +105,7 @@ SCHED_FEAT(ID_EXPELLEE_NEVER_HOT, false)
 SCHED_FEAT(ID_LAST_HIGHCLASS_STAY, false)
 SCHED_FEAT(ID_LOOSE_EXPEL, false)
 SCHED_FEAT(ID_EXPELLER_SHARE_CORE, true)
+SCHED_FEAT(ID_ABSOLUTE_EXPEL, false)
 #endif
 
 #ifdef CONFIG_SCHED_CORE

commit 781264d6ea93bcd2f4822ee955dd5192333c1e83

anolis: sched/fair: fix underclass unscheduled after ID_ABSOLUTE_EXPEL turned off
ANBZ: #8835

In function sync_min_vruntime(), expel_start and expel_spread will not
be cleared if CONFIG_SCHED_SMT is off, which results that the priority
of underclass will be much lower than other once ID_ABSOLUTE_EXPEL is
turned off, because min_vruntime << min_under_vruntime + expel_spread
after sync_min_vruntime().

To fix this problem, we clear expel_start and expel_spread in
sync_min_vruntime() regardless of whether CONFIG_SCHED_SMT is on.

Fixes: 52249b01d37b("anolis: sched/fair: introduce sched_feat ID_ABSOLUTE_EXPEL")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/fair.c
@@ -1953,7 +1953,6 @@ static inline void sync_min_vruntime(struct cfs_rq *cfs_rq)
 		sync_up = true;
 	}
 
-#ifdef CONFIG_SCHED_SMT
 	/*
 	 * Once min vruntime sync up, the expel penalty will be
 	 * included, just clear it.
@@ -1962,7 +1961,6 @@ static inline void sync_min_vruntime(struct cfs_rq *cfs_rq)
 		cfs_rq->expel_start = 0;
 		cfs_rq->expel_spread = 0;
 	}
-#endif
 }
 
 static inline void update_under_min_vruntime(struct cfs_rq *cfs_rq)