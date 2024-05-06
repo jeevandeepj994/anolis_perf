jeevandeepj994/anolis_perf

commit 1bb09f9140045bdd00d62db405b4656648addd57

anolis: kfence: support dynamically enabling after system startup
ANBZ: #28

Based on previous commits, KFENCE dynamically enabling is introduced. We
keep trying to alloc free 1GiB continuous pages until the request
requirements are met or no space. Writing a positive number to
/sys/module/kfence/parameters/num_objects_pernode to set max number of
objects KFENCE can hold and then writing a positive number to
/sys/module/kfence/parameters/sample_interval to enable KFENCE.
NOTE: this needs KFENCE not been inited since booting.
To keep sync with rb_tree searching, stop_machine() is called when
updating the tree.

Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>

mm/kfence/core.c
@@ -26,6 +26,7 @@
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/string.h>
+#include <linux/stop_machine.h>
 
 #include <asm/kfence.h>
 
@@ -76,11 +77,16 @@ static int param_set_sample_interval(const char *val, const struct kernel_param 
 #ifdef CONFIG_KFENCE_STATIC_KEYS
 		static_branch_disable(&kfence_allocation_key);
 #endif
-	} else if (!READ_ONCE(kfence_enabled) && system_state != SYSTEM_BOOTING) {
+	} else if (!READ_ONCE(kfence_enabled) && system_state != SYSTEM_BOOTING &&
+		   static_branch_unlikely(&kfence_once_inited)) {
 		return -EINVAL; /* Cannot (re-)enable KFENCE on-the-fly. */
 	}
 
 	*((unsigned long *)kp->arg) = num;
+
+	if (system_state != SYSTEM_BOOTING)
+		kfence_init_late();
+
 	return 0;
 }
 
@@ -106,7 +112,8 @@ static int param_set_num_objects(const char *val, const struct kernel_param *kp)
 	if (ret < 0)
 		return ret;
 
-	if (system_state != SYSTEM_BOOTING)
+	if ((system_state != SYSTEM_BOOTING &&
+	     static_branch_unlikely(&kfence_once_inited)) || !num)
 		return -EINVAL;
 
 	*((unsigned long *)kp->arg) = num;
@@ -115,9 +122,6 @@ static int param_set_num_objects(const char *val, const struct kernel_param *kp)
 
 static int param_get_num_objects(char *buffer, const struct kernel_param *kp)
 {
-	if (!READ_ONCE(kfence_enabled))
-		return sprintf(buffer, "0\n");
-
 	return param_get_ulong(buffer, kp);
 }
 
@@ -955,6 +959,68 @@ static bool __init kfence_init_pool(void)
 	return success_once;
 }
 
+static void kfence_alloc_pool_late_node(int node, struct list_head *ready)
+{
+	unsigned long nr_need, nr_request;
+
+	nr_need = roundup(kfence_num_objects, KFENCE_MAX_OBJECTS_PER_AREA);
+	nr_request = KFENCE_MAX_OBJECTS_PER_AREA;
+
+	while (nr_need) {
+		struct page *page;
+		struct kfence_pool_area *kpa;
+		unsigned long nr_pages = (nr_request + 1) * 2;
+
+		page = alloc_contig_pages(nr_pages, GFP_KERNEL | __GFP_ZERO | __GFP_THISNODE,
+					  node, NULL);
+		if (!page) {
+			pr_err("kfence alloc metadata on node %d failed\n", node);
+			return;
+		}
+		kpa = kzalloc_node(sizeof(struct kfence_pool_area), GFP_KERNEL, node);
+		if (!kpa)
+			goto fail;
+		kpa->meta = vzalloc_node(sizeof(struct kfence_metadata) * nr_request, node);
+		if (!kpa->meta)
+			goto fail;
+		kpa->addr = page_to_virt(page);
+		kpa->pool_size = nr_pages * PAGE_SIZE;
+		kpa->nr_objects = nr_request;
+		kpa->node = node;
+
+		if (!__kfence_init_pool_area(kpa))
+			goto fail;
+
+		list_add(&kpa->list, ready);
+		nr_need -= nr_request;
+		nr_request = min(nr_request, nr_need);
+
+		continue;
+
+fail:
+		free_contig_range(page_to_pfn(page), nr_pages);
+		if (kpa) {
+			vfree(kpa->meta);
+			kfree(kpa);
+		}
+		return;
+	}
+}
+
+static int kfence_update_pool_root(void *info)
+{
+	struct list_head *ready_list = info;
+	struct kfence_pool_area *kpa;
+
+	while (!list_empty(ready_list)) {
+		kpa = list_first_entry(ready_list, struct kfence_pool_area, list);
+		rb_add(&kpa->rb_node, &kfence_pool_root, kfence_rb_less);
+		list_del(&kpa->list);
+	}
+
+	return 0;
+}
+
 /* === DebugFS Interface ==================================================== */
 
 static inline void print_pool_size(struct seq_file *seq, unsigned long byte)
@@ -1166,7 +1232,7 @@ void __init kfence_alloc_pool(void)
 
 	/* Setting kfence_sample_interval to 0 on boot disables KFENCE. */
 	if (!READ_ONCE(kfence_sample_interval))
-		return;
+		goto fail;
 
 	/*
 	 * If num less than this value, use the upstream mode;
@@ -1179,14 +1245,16 @@ void __init kfence_alloc_pool(void)
 
 	__kfence_pool_area = memblock_alloc(sizeof(char *) * nr_node_ids * nr_area_pernode,
 					    PAGE_SIZE);
-
-	if (!__kfence_pool_area) {
-		WRITE_ONCE(kfence_sample_interval, 0);
-		return;
-	}
+	if (!__kfence_pool_area)
+		goto fail;
 
 	for_each_node(node)
 		kfence_alloc_pool_node(node);
+
+	return;
+
+fail:
+	WRITE_ONCE(kfence_sample_interval, 0);
 }
 
 void __init kfence_init(void)
@@ -1226,6 +1294,45 @@ out:
 	memblock_free_late(__pa(__kfence_pool_area), sizeof(char *) * nr_node_ids *
 			   nr_area_pernode);
 	__kfence_pool_area = NULL;
+}
+
+void kfence_init_late(void)
+{
+	int node;
+	LIST_HEAD(ready_list);
+
+	if (!READ_ONCE(kfence_sample_interval) || static_branch_unlikely(&kfence_once_inited))
+		return;
+
+	/*
+	 * We enable kfence_once_inited at here, and not care whether kfence
+	 * finally inited successfully or not. Because we have tried to enable
+	 * kfence, even if we failed. If failed, there may be problems,
+	 * do not try again. (This is a temp action, trying again will be
+	 * allowed in the next patch.)
+	 * Another goal of kfence_once_inited is to improve performance while
+	 * disabling kfence. So whenevner we try to enable it, meaning we do
+	 * not care so much about performance. This will be fine.
+	 */
+	static_branch_enable(&kfence_once_inited);
+
+	if (!__init_freelist())
+		goto fail;
+
+	for_each_node(node)
+		kfence_alloc_pool_late_node(node, &ready_list);
+
+	if (list_empty(&ready_list))
+		goto fail;
+
+	stop_machine(kfence_update_pool_root, &ready_list, NULL);
+
+	__start_kfence();
+	return;
+
+fail:
+	__free_freelist();
+	WRITE_ONCE(kfence_sample_interval, 0);
 }
 
 static void kfence_shutdown_cache_area(struct kmem_cache *s, struct kfence_pool_area *kpa)

include/linux/kfence.h
@@ -20,6 +20,7 @@ struct kfence_pool_area {
 	unsigned long pool_size; /* size of kfence pool of this area */
 	unsigned long nr_objects; /* max object number of this area */
 	int node; /* the numa node this area belongs to */
+	struct list_head list; /* ready to be added to kfence_pool_root */
 };
 
 #ifdef CONFIG_KFENCE

mm/kfence/kfence.h
@@ -113,6 +113,7 @@ void kfence_report_error(unsigned long address, bool is_write, struct pt_regs *r
 			 const struct kfence_metadata *meta, enum kfence_error_type type);
 
 void kfence_print_object(struct seq_file *seq, const struct kfence_metadata *meta);
+void kfence_init_late(void);
 
 extern struct rb_root kfence_pool_root;
 #define kfence_rbentry(cur) rb_entry((cur), struct kfence_pool_area, rb_node)

commit a84325049b895e49a511888b17b8ae4e83f50c10

anolis: kfence: Fix build errors when CONTIG_ALLOC and KFENCE undefined
ANBZ: #8917

Wrap all specific codes in #ifdef to avoid build errors.

Fixes: 1bb09f914004 ("anolis: kfence: support dynamically enabling after system startup")
Fixes: c955f88dc924 ("anolis: kfence: automatically free unused areas after disabled")
Fixes: ad2d7b18e15a ("anolis: mm/kidled: do not monitor kfence memory")
Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

mm/kfence/core.c
@@ -1183,8 +1183,12 @@ static void kfence_alloc_pool_late_node(int node, struct list_head *ready, bool 
 		struct page *page;
 		struct kfence_pool_area *kpa;
 		unsigned long nr_pages = (nr_request + 1) * 2;
-
+#ifdef CONFIG_CONTIG_ALLOC
 		page = alloc_contig_pages(nr_pages, gfp_mask, node, NULL);
+#else
+		pr_warn("anolis kfence only supports enabled later with CONFIG_CONTIG_ALLOC\n");
+		page = NULL;
+#endif
 		if (!page) {
 			pr_err("kfence alloc metadata on node %d failed\n", node);
 			return;
@@ -1204,7 +1208,9 @@ static void kfence_alloc_pool_late_node(int node, struct list_head *ready, bool 
 		continue;
 
 fail:
+#ifdef CONFIG_CONTIG_ALLOC
 		free_contig_range(page_to_pfn(page), nr_pages);
+#endif
 		kfree(kpa);
 
 		return;
@@ -1224,7 +1230,9 @@ static inline void kfence_free_pool_area(struct kfence_pool_area *kpa)
 
 static inline void kfence_free_pool_late_area(struct kfence_pool_area *kpa)
 {
+#ifdef CONFIG_CONTIG_ALLOC
 	free_contig_range(page_to_pfn(virt_to_page(kpa->addr)), kpa->pool_size / PAGE_SIZE);
+#endif
 }
 
 static void get_kpa(struct kfence_pool_area *kpa)

mm/page_alloc.c
@@ -1285,9 +1285,11 @@ static inline int check_free_page(struct page *page)
 	if (likely(page_expected_state(page, PAGE_FLAGS_CHECK_AT_FREE)))
 		return 0;
 
+#ifdef CONFIG_KFENCE
 	/* It's not performance sensitive when reaching here */
-	if (is_kfence_address(page_to_virt(page)))
+	if (PageKfence(page))
 		return 0;
+#endif
 
 	/* Something has gone sideways, find it */
 	check_free_page_bad(page);

mm/slab.h
@@ -211,9 +211,11 @@ static inline int cache_vmstat_idx(struct kmem_cache *s)
 #ifdef CONFIG_KIDLED
 static inline bool kidled_available_slab(struct page *page, struct kmem_cache *s)
 {
+#ifdef CONFIG_KFENCE
 	/* Do not monitor kfence memory. */
 	if (unlikely(PageKfence(page)))
 		return false;
+#endif
 
 	if (!strcmp(s->name, "inode_cache") ||
 		!strcmp(s->name, "ext4_inode_cache") ||