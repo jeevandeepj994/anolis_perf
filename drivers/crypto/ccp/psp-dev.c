jeevandeepj994/anolis_perf

commit 39e18cb04c1f6a694e12aa83e90b3064f267ef30

anolis: crypto: ccp: Fixup the capability of Hygon PSP during initialization
ANBZ: #8560

The meaning of the data read from feature register of Hygon PSP is not
exactly the same as AMD ASP. The bit 1 in feature register is used to
indicates TEE in AMD ASP, but not in Hygon PSP, which will cause host
to crash during module initialization, as shown below.

[   27.898723] BUG: kernel NULL pointer dereference, address: 0000000000000014
[   27.906503] #PF: supervisor read access in kernel mode
[   27.912242] #PF: error_code(0x0000) - not-present page
[   27.917981] PGD 0 P4D 0
[   27.920810] Oops: 0000 [#1] PREEMPT SMP NOPTI
[   27.925676] CPU: 67 PID: 1668 Comm: systemd-udevd Not tainted 6.6.7-for-gerrit #3
[   27.934033] Hardware name: HYGON Hygon65N32/65N32, BIOS A0173036 02/01/2023
[   27.941807] RIP: 0010:psp_firmware_is_visible+0x3c/0x70 [ccp]
[   27.948240] Code: 00 00 48 85 c0 74 12 48 81 fe e0 54 53 c1 74 2f 48 81 fe c0 54 53 c1 74 03 31 c0 c3 f6 40 70 02 74 f7 48 8b 50 10 48 8b 52 08 <8b> 52 14 85 d2 74 e8 48 03 50 38 48 89 d7 e8 51 71 0a d7 eb 14 48
[   27.969204] RSP: 0018:ffffc9000b80fa70 EFLAGS: 00010202
[   27.975039] RAX: ffff888113c2d9a8 RBX: ffffffffc1535460 RCX: 0000000000000124
[   27.983008] RDX: 0000000000000000 RSI: ffffffffc15354c0 RDI: ffff8888830dc0c0
[   27.993320] RBP: ffff888883060980 R08: 0000000000000001 R09: 00000006c8df7639
[   28.005756] R10: ffff888100258278 R11: 0000000000000100 R12: ffff8888830dc0c0
[   28.019695] R13: 0000000000000001 R14: 0000000000000000 R15: ffffffffc1535490
[   28.032285] FS:  00007f7c9ba2b880(0000) GS:ffff88885fcc0000(0000) knlGS:0000000000000000
[   28.044626] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
[   28.054928] CR2: 0000000000000014 CR3: 0000800106e50000 CR4: 00000000003506e0
[   28.065028] Call Trace:
[   28.067751]  <TASK>
[   28.070095]  ? __die_body+0x1f/0x60
[   28.073995]  ? page_fault_oops+0x15d/0x460
[   28.078573]  ? exc_page_fault+0x78/0x170
[   28.082956]  ? asm_exc_page_fault+0x26/0x30

drivers/crypto/ccp/psp-dev.c
@@ -56,6 +56,13 @@ static irqreturn_t psp_irq_handler(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
+static void hygon_fixup_psp_caps(struct psp_device *psp)
+{
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+		psp->capability &= ~(PSP_CAPABILITY_TEE |
+				     PSP_CAPABILITY_PSP_SECURITY_REPORTING);
+}
+
 static unsigned int psp_get_capability(struct psp_device *psp)
 {
 	unsigned int val = ioread32(psp->io_regs + psp->vdata->feature_reg);
@@ -72,6 +79,12 @@ static unsigned int psp_get_capability(struct psp_device *psp)
 		return -ENODEV;
 	}
 	psp->capability = val;
+
+	/*
+	 * Fix capability of Hygon psp, the meaning of Hygon psp feature
+	 * register is not exactly the same as AMD.
+	 */
+	hygon_fixup_psp_caps(psp);
 
 	/* Detect if TSME and SME are both enabled */
 	if (psp->capability & PSP_CAPABILITY_PSP_SECURITY_REPORTING &&

commit 2adc8ce0f625db1c89a2273865816e8de74032a8

anolis: crypto: ccp: Return -ENODEV if Hygon PSP is not configured with CSV capability
ANBZ: #9023

The rules to determine psp_master is not exactly the same between
AMD ASP and Hygon PSP. If a Hygon PSP is not configured with CSV
capability, it should not be the psp_master.

Fixes: 39e18cb04c1f ("anolis: crypto: ccp: Fixup the capability of Hygon PSP during initialization")
Signed-off-by: hanliyang <hanliyang@hygon.cn>

drivers/crypto/ccp/psp-dev.c
@@ -236,11 +236,15 @@ static irqreturn_t psp_irq_handler_hygon(int irq, void *data)
 }
 #endif
 
-static void hygon_fixup_psp_caps(struct psp_device *psp)
+static int hygon_fixup_psp_caps(struct psp_device *psp)
 {
-	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
-		psp->capability &= ~(PSP_CAPABILITY_TEE |
-				     PSP_CAPABILITY_PSP_SECURITY_REPORTING);
+	/* the hygon psp is unavailable if bit0 cleared in feature reg */
+	if (!(psp->capability & PSP_CAPABILITY_SEV))
+		return -ENODEV;
+
+	psp->capability &= ~(PSP_CAPABILITY_TEE |
+			     PSP_CAPABILITY_PSP_SECURITY_REPORTING);
+	return 0;
 }
 
 static unsigned int psp_get_capability(struct psp_device *psp)
@@ -263,8 +267,13 @@ static unsigned int psp_get_capability(struct psp_device *psp)
 	/*
 	 * Fix capability of Hygon psp, the meaning of Hygon psp feature
 	 * register is not exactly the same as AMD.
+	 * Return -ENODEV directly if hygon psp not configured with CSV
+	 * capability.
 	 */
-	hygon_fixup_psp_caps(psp);
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) {
+		if (hygon_fixup_psp_caps(psp))
+			return -ENODEV;
+	}
 
 	/* Detect if TSME and SME are both enabled */
 	if (psp->capability & PSP_CAPABILITY_PSP_SECURITY_REPORTING &&