jeevandeepj994/anolis_perf

commit 8658623023f763cc0a6bd58c024ddc8196dbde04

net/smc: Fix setsockopt and sysctl to specify same buffer size again
ANBZ: #8971

commit 833bac7ec392bf75053c8a4fa4c36d4148dac77d upstream

Commit 0227f058aa29 ("net/smc: Unbind r/w buffer size from clcsock
and make them tunable") introduced the net.smc.rmem and net.smc.wmem
sysctls to specify the size of buffers to be used for SMC type
connections. This created a regression for users that specified the
buffer size via setsockopt() as the effective buffer size was now
doubled.

Re-introduce the division by 2 in the SMC buffer create code and level
this out by duplicating the net.smc.[rw]mem values used for initializing
sk_rcvbuf/sk_sndbuf at socket creation time. This gives users of both
methods (setsockopt or sysctl) the effective buffer size that they
expect.

Initialize net.smc.[rw]mem from its own constant of 64kB, respectively.
Internal performance tests show that this value is a good compromise
between throughput/latency and memory consumption. Also, this decouples
it from any tuning that was done to net.ipv4.tcp_[rw]mem[1] before the
module for SMC protocol was loaded. Check that no more than INT_MAX / 2
is assigned to net.smc.[rw]mem, in order to avoid any overflow condition
when that is doubled for use in sk_sndbuf or sk_rcvbuf.

While at it, drop the confusing sk_buf_size variable from
__smc_buf_create and name "compressed" buffer size variables more
consistently.

Background:

Before the commit mentioned above, SMC's buffer allocator in
__smc_buf_create() always used half of the sockets' sk_rcvbuf/sk_sndbuf
value as initial value to search for appropriate buffers. If the search
resorted to using a bigger buffer when all buffers of the specified
size were busy, the duplicate of the used effective buffer size is
stored back to sk_rcvbuf/sk_sndbuf.

When available, buffers of exactly the size that a user had specified as
input to setsockopt() were used, despite setsockopt()'s documentation in
"man 7 socket" talking of a mandatory duplication:

[...]

net/smc/af_smc.c
@@ -482,7 +482,7 @@ static void smc_sock_init_passive(struct sock *par, struct sock *sk)
 	smc_sock_init_common(sk);
 	smc_sk(sk)->listen_smc = parent;
 	/* restore the smc_sk_sndbuf before handshake */
-	smc_sk(sk)->smc_sk_sndbuf = READ_ONCE(sock_net(sk)->smc.sysctl_wmem);
+	smc_sk(sk)->smc_sk_sndbuf = 2 * READ_ONCE(sock_net(sk)->smc.sysctl_wmem);
 
 	smc_sock_clone_negotiator_ops(par, sk);
 
@@ -505,8 +505,8 @@ static void smc_sock_init(struct sock *sk, struct net *net)
 	INIT_WORK(&smc->tcp_listen_work, smc_tcp_listen_work);
 	INIT_LIST_HEAD(&smc->accept_q);
 	spin_lock_init(&smc->accept_q_lock);
-	WRITE_ONCE(sk->sk_sndbuf, READ_ONCE(net->smc.sysctl_wmem));
-	WRITE_ONCE(sk->sk_rcvbuf, READ_ONCE(net->smc.sysctl_rmem));
+	WRITE_ONCE(sk->sk_sndbuf, 2 * READ_ONCE(net->smc.sysctl_wmem));
+	WRITE_ONCE(sk->sk_rcvbuf, 2 * READ_ONCE(net->smc.sysctl_rmem));
 	smc->limit_smc_hs = net->smc.limit_smc_hs;
 	smc_sock_assign_negotiator_ops(smc, "anolis");
 

net/smc/smc_core.c
@@ -2481,31 +2481,31 @@ static int __smc_buf_create(struct smc_sock *smc, bool is_smcd, bool is_rmb)
 	struct smc_connection *conn = &smc->conn;
 	struct smc_link_group *lgr = conn->lgr;
 	struct list_head *buf_list;
-	int bufsize, bufsize_short;
+	int bufsize, bufsize_comp;
 	struct rw_semaphore *lock;	/* lock buffer list */
 	bool is_dgraded = false;
-	int sk_buf_size;
 
 	if (is_rmb)
 		/* use socket recv buffer size (w/o overhead) as start value */
-		sk_buf_size = smc->sk.sk_rcvbuf;
+		bufsize = smc->sk.sk_rcvbuf / 2;
 	else
 		/* only inet sock will set smc_sk_sndbuf, for smc sock it is zero */
-		sk_buf_size = smc->smc_sk_sndbuf ?: smc->sk.sk_sndbuf;
+		bufsize = smc->smc_sk_sndbuf ?
+				smc->smc_sk_sndbuf / 2 : smc->sk.sk_sndbuf / 2;
 
-	for (bufsize_short = smc_compress_bufsize(sk_buf_size, is_smcd, is_rmb);
-	     bufsize_short >= 0; bufsize_short--) {
+	for (bufsize_comp = smc_compress_bufsize(bufsize, is_smcd, is_rmb);
+	     bufsize_comp >= 0; bufsize_comp--) {
 		if (is_rmb) {
 			lock = &lgr->rmbs_lock;
-			buf_list = &lgr->rmbs[bufsize_short];
+			buf_list = &lgr->rmbs[bufsize_comp];
 		} else {
 			lock = &lgr->sndbufs_lock;
-			buf_list = &lgr->sndbufs[bufsize_short];
+			buf_list = &lgr->sndbufs[bufsize_comp];
 		}
-		bufsize = smc_uncompress_bufsize(bufsize_short);
+		bufsize = smc_uncompress_bufsize(bufsize_comp);
 
 		/* check for reusable slot in the link group */
-		buf_desc = smc_buf_get_slot(bufsize_short, lock, buf_list);
+		buf_desc = smc_buf_get_slot(bufsize_comp, lock, buf_list);
 		if (buf_desc) {
 			buf_desc->is_dma_need_sync = 0;
 			SMC_STAT_RMB_SIZE(smc, is_smcd, is_rmb, bufsize);
@@ -2549,8 +2549,8 @@ static int __smc_buf_create(struct smc_sock *smc, bool is_smcd, bool is_rmb)
 
 	if (is_rmb) {
 		conn->rmb_desc = buf_desc;
-		conn->rmbe_size_short = bufsize_short;
-		smc->sk.sk_rcvbuf = bufsize;
+		conn->rmbe_size_comp = bufsize_comp;
+		smc->sk.sk_rcvbuf = bufsize * 2;
 		atomic_set(&conn->bytes_to_rcv, 0);
 		conn->rmbe_update_limit =
 			smc_rmb_wnd_update_limit(buf_desc->len);
@@ -2558,7 +2558,7 @@ static int __smc_buf_create(struct smc_sock *smc, bool is_smcd, bool is_rmb)
 			smc_ism_set_conn(conn); /* map RMB/smcd_dev to conn */
 	} else {
 		conn->sndbuf_desc = buf_desc;
-		smc->sk.sk_sndbuf = bufsize;
+		smc->sk.sk_sndbuf = bufsize * 2;
 		atomic_set(&conn->sndbuf_space, bufsize);
 	}
 	return 0;

include/net/smc.h
@@ -209,7 +209,7 @@ struct smc_connection {
 
 	struct smc_buf_desc	*sndbuf_desc;	/* send buffer descriptor */
 	struct smc_buf_desc	*rmb_desc;	/* RMBE descriptor */
-	int			rmbe_size_short;/* compressed notation */
+	int                     rmbe_size_comp; /* compressed notation */
 	int			rmbe_update_limit;
 						/* lower limit for consumer
 						 * cursor update

net/smc/smc_clc.c
@@ -1074,7 +1074,7 @@ static int smc_clc_send_confirm_accept(struct smc_sock *smc,
 		clc->d0.gid =
 			conn->lgr->smcd->ops->get_local_gid(conn->lgr->smcd);
 		clc->d0.token = conn->rmb_desc->token;
-		clc->d0.dmbe_size = conn->rmbe_size_short;
+		clc->d0.dmbe_size = conn->rmbe_size_comp;
 		clc->d0.dmbe_idx = 0;
 		memcpy(&clc->d0.linkid, conn->lgr->id, SMC_LGR_ID_SIZE);
 		if (version == SMC_V1) {
@@ -1123,7 +1123,7 @@ static int smc_clc_send_confirm_accept(struct smc_sock *smc,
 				clc->r0.init_credits = (u8)link->wr_rx_cnt;
 			break;
 		}
-		clc->r0.rmbe_size = conn->rmbe_size_short;
+		clc->r0.rmbe_size = conn->rmbe_size_comp;
 		clc->r0.rmb_dma_addr = conn->rmb_desc->is_vm ?
 			cpu_to_be64((uintptr_t)conn->rmb_desc->cpu_addr) :
 			cpu_to_be64((u64)sg_dma_address

net/smc/smc_sysctl.c
@@ -22,6 +22,10 @@
 static int two = 2;
 static int min_sndbuf = SMC_BUF_MIN_SIZE;
 static int min_rcvbuf = SMC_BUF_MIN_SIZE;
+static int max_sndbuf = INT_MAX / 2;
+static int max_rcvbuf = INT_MAX / 2;
+static const int net_smc_wmem_init = (256 * 1024); /* ANCK: 256K, upstream: 64K */
+static const int net_smc_rmem_init = (256 * 1024);
 static int links_per_lgr_min = SMC_LINKS_ADD_LNK_MIN;
 static int links_per_lgr_max = SMC_LINKS_ADD_LNK_MAX;
 static int conns_per_lgr_min = SMC_CONN_PER_LGR_MIN;
@@ -58,6 +62,7 @@ static struct ctl_table smc_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &min_sndbuf,
+		.extra2		= &max_sndbuf,
 	},
 	{
 		.procname	= "rmem",
@@ -66,6 +71,7 @@ static struct ctl_table smc_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &min_rcvbuf,
+		.extra2		= &max_rcvbuf,
 	},
 	{
 		.procname	= "tcp2smc",
@@ -126,8 +132,8 @@ int __net_init smc_sysctl_net_init(struct net *net)
 	net->smc.sysctl_smcr_buf_type = SMCR_MIXED_BUFS;
 	net->smc.sysctl_vendor_exp_options = ~0U;
 	net->smc.sysctl_smcr_testlink_time = SMC_LLC_TESTLINK_DEFAULT_TIME;
-	net->smc.sysctl_wmem = 262144; /* 256 KiB */
-	net->smc.sysctl_rmem = 262144; /* 256 KiB */
+	WRITE_ONCE(net->smc.sysctl_wmem, net_smc_wmem_init);
+	WRITE_ONCE(net->smc.sysctl_rmem, net_smc_rmem_init);
 	net->smc.sysctl_tcp2smc = 0;
 	/* enable handshake limitation by default */
 	net->smc.limit_smc_hs = 1;

commit 0c3ceb9c40ae3b4ece5c5640d78b9dac4f99a9db

anolis: net/smc: restore the correspondence between sk_{snd|rcv}buf and sndbuf/RMB
ANBZ: #9167

The patch[1], which was merged from upstream, changes the correspondence
between sk_sndbuf/sk_rcvbuf and sndbuf/RMB.

- the sk_{snd|rcv}buf value is equal to 2 * sysctl net.smc.{w|r}mem, and
  is equal to 2 * val that set by setsockopt(SO_{SND|RCV}BUF);
- the sndbuf/RMB size is equal to sk_sndbuf/sk_rcvbuf value / 2;

Therefore, the size of sndbuf/RMB will be the value of sysctl net.smc.{w|rmem}
or the value set by setsockopt(SO_{SND|RCV}BUF)

However, this is inconsistent with the behavior of TCP, and SMC is mainly
used to transparently upgrade TCP. Therefore, the relationship between
sk_{snd|rcv}buf and sndbuf/RMB in SMC is restored to that before the
introduction of [1], that is:

- the sk_{snd|rcv}buf value is equal to sysctl net.smc.{w|r}mem, and is
  equal to 2 * val that set by setsockopt(SO_{SND|RCV}BUF);
- the sndbuf/RMB size is equal to sk_sndbuf/sk_rcvbuf value;

Then, the size of sndbuf/RMB will be the value of sysctl net.smc.{w|rmem}
or *2 times* the value set by setsockopt(SO_{SND|RCV}BUF).

[1] 8658623023f7 ("net/smc: Fix setsockopt and sysctl to specify same buffer size again")

Fixes: 8658623023f7 ("net/smc: Fix setsockopt and sysctl to specify same buffer size again")
Fixes: 9eedcb7a19cd ("net/smc: Use correct buffer sizes when switching between TCP and SMC")
Signed-off-by: Wen Gu <guwen@linux.alibaba.com>

net/smc/af_smc.c
@@ -480,7 +480,7 @@ static void smc_sock_init_passive(struct sock *par, struct sock *sk)
 	smc_sock_init_common(sk);
 	smc_sk(sk)->listen_smc = parent;
 	/* restore the smc_sk_sndbuf before handshake */
-	smc_sk(sk)->smc_sk_sndbuf = 2 * READ_ONCE(sock_net(sk)->smc.sysctl_wmem);
+	smc_sk(sk)->smc_sk_sndbuf = READ_ONCE(sock_net(sk)->smc.sysctl_wmem);
 
 	smc_sock_clone_negotiator_ops(par, sk);
 
@@ -503,8 +503,8 @@ static void smc_sock_init(struct sock *sk, struct net *net)
 	INIT_WORK(&smc->tcp_listen_work, smc_tcp_listen_work);
 	INIT_LIST_HEAD(&smc->accept_q);
 	spin_lock_init(&smc->accept_q_lock);
-	WRITE_ONCE(sk->sk_sndbuf, 2 * READ_ONCE(net->smc.sysctl_wmem));
-	WRITE_ONCE(sk->sk_rcvbuf, 2 * READ_ONCE(net->smc.sysctl_rmem));
+	WRITE_ONCE(sk->sk_sndbuf, READ_ONCE(net->smc.sysctl_wmem));
+	WRITE_ONCE(sk->sk_rcvbuf, READ_ONCE(net->smc.sysctl_rmem));
 	smc->limit_smc_hs = net->smc.limit_smc_hs;
 	smc_sock_assign_negotiator_ops(smc, "anolis");
 
@@ -610,7 +610,7 @@ static void smc_adjust_sock_bufsizes(struct sock *nsk, struct sock *osk,
 				   READ_ONCE(nnet->ipv4.sysctl_tcp_wmem[1]));
 		else
 			WRITE_ONCE(nsk->sk_sndbuf,
-				   2 * READ_ONCE(nnet->smc.sysctl_wmem));
+				   READ_ONCE(nnet->smc.sysctl_wmem));
 	}
 	if (osk->sk_userlocks & SOCK_RCVBUF_LOCK) {
 		nsk->sk_rcvbuf = osk->sk_rcvbuf;
@@ -620,7 +620,7 @@ static void smc_adjust_sock_bufsizes(struct sock *nsk, struct sock *osk,
 				   READ_ONCE(nnet->ipv4.sysctl_tcp_rmem[1]));
 		else
 			WRITE_ONCE(nsk->sk_rcvbuf,
-				   2 * READ_ONCE(nnet->smc.sysctl_rmem));
+				   READ_ONCE(nnet->smc.sysctl_rmem));
 	}
 }
 

net/smc/smc_core.c
@@ -2487,11 +2487,10 @@ static int __smc_buf_create(struct smc_sock *smc, bool is_smcd, bool is_rmb)
 
 	if (is_rmb)
 		/* use socket recv buffer size (w/o overhead) as start value */
-		bufsize = smc->sk.sk_rcvbuf / 2;
+		bufsize = smc->sk.sk_rcvbuf;
 	else
 		/* only inet sock will set smc_sk_sndbuf, for smc sock it is zero */
-		bufsize = smc->smc_sk_sndbuf ?
-				smc->smc_sk_sndbuf / 2 : smc->sk.sk_sndbuf / 2;
+		bufsize = smc->smc_sk_sndbuf ?: smc->sk.sk_sndbuf;
 
 	for (bufsize_comp = smc_compress_bufsize(bufsize, is_smcd, is_rmb);
 	     bufsize_comp >= 0; bufsize_comp--) {
@@ -2550,7 +2549,7 @@ static int __smc_buf_create(struct smc_sock *smc, bool is_smcd, bool is_rmb)
 	if (is_rmb) {
 		conn->rmb_desc = buf_desc;
 		conn->rmbe_size_comp = bufsize_comp;
-		smc->sk.sk_rcvbuf = bufsize * 2;
+		smc->sk.sk_rcvbuf = bufsize;
 		atomic_set(&conn->bytes_to_rcv, 0);
 		conn->rmbe_update_limit =
 			smc_rmb_wnd_update_limit(buf_desc->len);
@@ -2558,7 +2557,7 @@ static int __smc_buf_create(struct smc_sock *smc, bool is_smcd, bool is_rmb)
 			smc_ism_set_conn(conn); /* map RMB/smcd_dev to conn */
 	} else {
 		conn->sndbuf_desc = buf_desc;
-		smc->sk.sk_sndbuf = bufsize * 2;
+		smc->sk.sk_sndbuf = bufsize;
 		atomic_set(&conn->sndbuf_space, bufsize);
 	}
 	return 0;