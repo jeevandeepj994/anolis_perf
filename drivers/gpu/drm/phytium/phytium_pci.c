jeevandeepj994/anolis_perf

commit 8aa6dbac73b688d63bb803cc01e0c3ef60b41eb2

anolis: DRM: Phytium Add Phytium Display Engine support.
ANBZ: #6670

This is Phytium Display Engine support,DC/DP driver patch.

Signed-off-by: Yang Xun <yangxun@phytium.com.cn>
Signed-off-by: Chen Baozi <chenbaozi@phytium.com.cn>
Signed-off-by: Li Shuo <lishuo@phytium.com.cn>
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2261

drivers/gpu/drm/phytium/phytium_pci.c
@@ -1,0 +1,388 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Phytium display drm driver
+ *
+ * Copyright (C) 2021-2023, Phytium Technology Co., Ltd.
+ */
+
+#include <linux/pci.h>
+#include <drm/drm_drv.h>
+#include <linux/dmaengine.h>
+#include "phytium_display_drv.h"
+#include "phytium_pci.h"
+#include "phytium_dp.h"
+#include "phytium_gem.h"
+#include "px210_dc.h"
+#include "px210_dp.h"
+#include "pe220x_dc.h"
+#include "pe220x_dp.h"
+
+int dc_msi_enable;
+module_param(dc_msi_enable, int, 0644);
+MODULE_PARM_DESC(dc_msi_enable, "Enable DC msi interrupt (0-disabled; 1-enabled; default-0)");
+
+void phytium_pci_vram_hw_init(struct phytium_display_private *priv)
+{
+	struct phytium_pci_private *pci_priv = to_pci_priv(priv);
+
+	pci_priv->dc_hw_vram_init(priv, priv->pool_phys_addr, priv->pool_size);
+}
+
+int phytium_pci_vram_init(struct pci_dev *pdev, struct phytium_display_private *priv)
+{
+	int ret = 0;
+
+	priv->pool_phys_addr = pci_resource_start(pdev, 2);
+	priv->pool_size = pci_resource_len(pdev, 2);
+	if ((priv->pool_phys_addr != 0) && (priv->pool_size != 0)) {
+		priv->pool_virt_addr = devm_ioremap_wc(&pdev->dev, priv->pool_phys_addr,
+						       priv->pool_size);
+		if (priv->pool_virt_addr == NULL) {
+			DRM_ERROR("pci vram ioremap fail, addr:0x%llx, size:0x%llx\n",
+				   priv->pool_phys_addr, priv->pool_size);
+			ret = -EINVAL;
+			goto failed_ioremap;
+		}
+		ret = phytium_memory_pool_init(&pdev->dev, priv);
+		if (ret)
+			goto failed_init_memory_pool;
+
+		priv->mem_state[PHYTIUM_MEM_VRAM_TOTAL] = priv->pool_size;
+		priv->support_memory_type = MEMORY_TYPE_VRAM;
+		priv->vram_hw_init = phytium_pci_vram_hw_init;
+	} else {
+		DRM_DEBUG_KMS("not support vram\n");
+		priv->pool_virt_addr = NULL;
+		priv->mem_state[PHYTIUM_MEM_VRAM_TOTAL] = 0;
+		priv->support_memory_type = MEMORY_TYPE_SYSTEM_UNIFIED;
+		priv->vram_hw_init = NULL;
+	}
+
+	return 0;
+
+failed_init_memory_pool:
+	devm_iounmap(&pdev->dev, priv->pool_virt_addr);
+failed_ioremap:
+	return ret;
+}
+
+void phytium_pci_vram_fini(struct pci_dev *pdev, struct phytium_display_private *priv)
+{
+	if (priv->support_memory_type == MEMORY_TYPE_VRAM) {
+		phytium_memory_pool_fini(&pdev->dev, priv);
+		devm_iounmap(&pdev->dev, priv->pool_virt_addr);
+	}
+}
+
+static bool phytium_pci_dma_chan_filter(struct dma_chan *chan, void *param)
+{
+	struct phytium_dma_slave *s = param;
+
+	if (s->dma_dev != chan->device->dev)
+		return false;
+
+	if (s->chan_id ==  chan->chan_id)
+		return true;
+	else
+		return false;
+}
+
+int phytium_pci_dma_init(struct phytium_display_private *priv)
+{
+	struct pci_dev *dma_dev, *gpu_dev;
+	struct drm_device *drm_dev =  priv->dev;
+	dma_cap_mask_t mask;
+	struct phytium_dma_slave s;
+	int ret = 0;
+	u16 cmd;
+
+	/* check px210 gpu enable */
+	gpu_dev = pci_get_device(PCI_VENDOR_ID_PHYTIUM, 0xdc20, NULL);
+	if (!gpu_dev) {
+		DRM_INFO("failed to get gpu_dev\n");
+		ret = -ENODEV;
+		goto failed;
+	}
+
+	pci_read_config_word(gpu_dev, PCI_COMMAND, &cmd);
+	if (!(cmd & PCI_COMMAND_MASTER)) {
+		DRM_INFO("gpu_dev master is disabled\n");
+		ret = -ENODEV;
+		goto failed;
+	}
+
+	dma_dev = pci_get_device(PCI_VENDOR_ID_PHYTIUM, 0xdc3c, NULL);
+	if (!dma_dev) {
+		DRM_INFO("failed to get dma_dev\n");
+		ret = -ENODEV;
+		goto failed;
+	}
+
+	dma_cap_zero(mask);
+	dma_cap_set(DMA_SLAVE, mask);
+
+	s.dma_dev = &dma_dev->dev;
+	s.chan_id = 2;
+	priv->dma_chan = dma_request_channel(mask, phytium_pci_dma_chan_filter, &s);
+	if (!priv->dma_chan) {
+		DRM_DEV_ERROR(drm_dev->dev, "failed to request dma chan\n");
+		ret = -EBUSY;
+		goto failed;
+	}
+	priv->dma_inited = 1;
+
+failed:
+	return ret;
+}
+
+void phytium_pci_dma_fini(struct phytium_display_private *priv)
+{
+	if (priv->dma_inited)
+		dma_release_channel(priv->dma_chan);
+	priv->dma_inited = 0;
+	priv->dma_chan =  NULL;
+}
+
+static struct phytium_display_private*
+phytium_pci_private_init(struct pci_dev *pdev, const struct pci_device_id *ent)
+{
+	struct drm_device *dev = pci_get_drvdata(pdev);
+	struct phytium_display_private *priv = NULL;
+	struct phytium_pci_private *pci_priv = NULL;
+	struct phytium_device_info *phytium_info = (struct phytium_device_info *)ent->driver_data;
+	int i = 0;
+	resource_size_t io_addr, io_size;
+
+	pci_priv = devm_kzalloc(&pdev->dev, sizeof(*pci_priv), GFP_KERNEL);
+	if (!pci_priv) {
+		DRM_ERROR("no memory to allocate for drm_display_private\n");
+		goto failed_malloc_priv;
+	}
+
+	memset(pci_priv, 0, sizeof(*pci_priv));
+	priv = &pci_priv->base;
+	phytium_display_private_init(priv, dev);
+
+	memcpy(&(priv->info), phytium_info, sizeof(struct phytium_device_info));
+	DRM_DEBUG_KMS("priv->info.num_pipes :%d\n", priv->info.num_pipes);
+	priv->info.pipe_mask = ((pdev->subsystem_device >> PIPE_MASK_SHIFT) & PIPE_MASK_MASK);
+	priv->info.edp_mask = ((pdev->subsystem_device >> EDP_MASK_SHIFT) & EDP_MASK_MASK);
+	priv->info.num_pipes = 0;
+	for_each_pipe_masked(priv, i)
+		priv->info.num_pipes++;
+	if (priv->info.num_pipes == 0) {
+		DRM_ERROR("num_pipes is zero, so exit init\n");
+		goto failed_init_numpipe;
+	}
+
+	io_addr = pci_resource_start(pdev, 0);
+	io_size = pci_resource_len(pdev, 0);
+	priv->regs = ioremap(io_addr, io_size);
+	if (priv->regs == NULL) {
+		DRM_ERROR("pci bar0 ioremap fail, addr:0x%llx, size:0x%llx\n", io_addr, io_size);
+		goto failed_ioremap;
+	}
+
+	priv->irq = pdev->irq;
+	if (IS_PX210(priv)) {
+		pci_priv->dc_hw_vram_init = px210_dc_hw_vram_init;
+		priv->dc_hw_clear_msi_irq = px210_dc_hw_clear_msi_irq;
+		priv->dc_hw_fb_format_check = px210_dc_hw_fb_format_check;
+	} else if (IS_PE220X(priv)) {
+		pci_priv->dc_hw_vram_init = pe220x_dc_hw_vram_init;
+		priv->dc_hw_clear_msi_irq = NULL;
+		priv->dc_hw_fb_format_check = pe220x_dc_hw_fb_format_check;
+	}
+
+	return priv;
+
+failed_ioremap:
+failed_init_numpipe:
+	devm_kfree(&pdev->dev, pci_priv);
+failed_malloc_priv:
+	return NULL;
+}
+
+static void
+phytium_pci_private_fini(struct pci_dev *pdev, struct phytium_display_private *priv)
+{
+	struct phytium_pci_private *pci_priv = to_pci_priv(priv);
+
+	if (priv->regs)
+		iounmap(priv->regs);
+
+	devm_kfree(&pdev->dev, pci_priv);
+}
+
+static int phytium_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
+{
+	struct phytium_display_private *priv = NULL;
+	struct drm_device *dev = NULL;
+	int ret = 0;
+
+	dev = drm_dev_alloc(&phytium_display_drm_driver, &pdev->dev);
+	if (IS_ERR(dev)) {
+		DRM_ERROR("failed to allocate drm_device\n");
+		return PTR_ERR(dev);
+	}
+	dev->pdev = pdev;
+	pci_set_drvdata(pdev, dev);
+	pci_set_master(pdev);
+	ret = pci_enable_device(pdev);
+	if (ret) {
+		DRM_ERROR("pci enable device fail\n");
+		goto failed_enable_device;
+	}
+
+	if (dc_msi_enable) {
+		ret = pci_enable_msi(pdev);
+		if (ret)
+			DRM_ERROR("pci enable msi fail\n");
+	}
+
+	dma_set_mask(&pdev->dev, DMA_BIT_MASK(40));
+
+	priv = phytium_pci_private_init(pdev, ent);
+	if (priv)
+		dev->dev_private = priv;
+	else
+		goto failed_pci_private_init;
+
+	ret = phytium_pci_vram_init(pdev, priv);
+	if (ret) {
+		DRM_ERROR("failed to init pci vram\n");
+		goto failed_pci_vram_init;
+	}
+
+	ret = drm_dev_register(dev, 0);
+	if (ret) {
+		DRM_ERROR("failed to register drm dev\n");
+		goto failed_register_drm;
+	}
+
+	phytium_dp_hpd_irq_setup(dev, true);
+
+	return 0;
+
+failed_register_drm:
+	phytium_pci_vram_fini(pdev, priv);
+failed_pci_vram_init:
+	phytium_pci_private_fini(pdev, priv);
+failed_pci_private_init:
+	if (pdev->msi_enabled)
+		pci_disable_msi(pdev);
+	pci_disable_device(pdev);
+failed_enable_device:
+	pci_set_drvdata(pdev, NULL);
+	drm_dev_put(dev);
+
+	return -1;
+}
+
+static void phytium_pci_remove(struct pci_dev *pdev)
+{
+	struct drm_device *dev = pci_get_drvdata(pdev);
+	struct phytium_display_private *priv = dev->dev_private;
+
+	phytium_dp_hpd_irq_setup(dev, false);
+	cancel_work_sync(&priv->hotplug_work);
+	drm_dev_unregister(dev);
+	phytium_pci_vram_fini(pdev, priv);
+	phytium_pci_private_fini(pdev, priv);
+	if (pdev->msi_enabled)
+		pci_disable_msi(pdev);
+	pci_disable_device(pdev);
+	pci_set_drvdata(pdev, NULL);
+	drm_dev_put(dev);
+}
+
+static void phytium_pci_shutdown(struct pci_dev *pdev)
+{
+	struct drm_device *dev = pci_get_drvdata(pdev);
+	struct phytium_display_private *priv = dev->dev_private;
+
+	priv->display_shutdown(dev);
+}
+
+static int phytium_pci_pm_suspend(struct device *dev)
+{
+	struct pci_dev *pdev = to_pci_dev(dev);
+	struct drm_device *drm_dev = pci_get_drvdata(pdev);
+	struct phytium_display_private *priv = drm_dev->dev_private;
+	int ret = 0;
+
+	if (IS_PX210(priv))
+		phytium_pci_dma_init(priv);
+
+	ret = priv->display_pm_suspend(drm_dev);
+	if (ret < 0)
+		goto out;
+
+	pci_save_state(pdev);
+	pci_disable_device(pdev);
+	pci_set_power_state(pdev, PCI_D3hot);
+	udelay(200);
+
+out:
+	return ret;
+}
+
+static int phytium_pci_pm_resume(struct device *dev)
+{
+	struct pci_dev *pdev = to_pci_dev(dev);
+	struct drm_device *drm_dev = pci_get_drvdata(pdev);
+	struct phytium_display_private *priv = drm_dev->dev_private;
+	int ret = 0;
+
+	pci_set_power_state(pdev, PCI_D0);
+	pci_restore_state(pdev);
+	ret = pci_enable_device(pdev);
+	if (ret)
+		return ret;
+	pci_set_master(pdev);
+
+	ret = priv->display_pm_resume(drm_dev);
+	if (IS_PX210(priv))
+		phytium_pci_dma_fini(priv);
+
+	return ret;
+}
+
+static const struct dev_pm_ops phytium_pci_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(phytium_pci_pm_suspend, phytium_pci_pm_resume)
+};
+
+static const struct phytium_device_info px210_info = {
+	.platform_mask = BIT(PHYTIUM_PLATFORM_PX210),
+	.total_pipes = 3,
+	.crtc_clock_max = PX210_DC_PIX_CLOCK_MAX,
+	.hdisplay_max = PX210_DC_HDISPLAY_MAX,
+	.vdisplay_max = PX210_DC_VDISPLAY_MAX,
+	.address_mask = PX210_DC_ADDRESS_MASK,
+	.backlight_max = PX210_DP_BACKLIGHT_MAX,
+};
+
+static const struct phytium_device_info pe220x_info = {
+	.platform_mask = BIT(PHYTIUM_PLATFORM_PE220X),
+	.total_pipes = 2,
+	.crtc_clock_max = PE220X_DC_PIX_CLOCK_MAX,
+	.hdisplay_max = PE220X_DC_HDISPLAY_MAX,
+	.vdisplay_max = PE220X_DC_VDISPLAY_MAX,
+	.address_mask = PE220X_DC_ADDRESS_MASK,
+	.backlight_max = PE220X_DP_BACKLIGHT_MAX,
+};
+
+static const struct pci_device_id phytium_display_pci_ids[] = {
+	{ PCI_VDEVICE(PHYTIUM, 0xdc22), (kernel_ulong_t)&px210_info },
+	{ PCI_VDEVICE(PHYTIUM, 0xdc3e), (kernel_ulong_t)&pe220x_info },
+	{ /* End: all zeroes */ }
+};
+MODULE_DEVICE_TABLE(pci, phytium_display_pci_ids);
+
+struct pci_driver phytium_pci_driver = {
+	.name = "phytium_display_pci",
+	.id_table = phytium_display_pci_ids,
+	.probe = phytium_pci_probe,
+	.remove = phytium_pci_remove,
+	.shutdown = phytium_pci_shutdown,
+	.driver.pm = &phytium_pci_pm_ops,
+};

Documentation/devicetree/bindings/gpu/phytium,dc.yaml
@@ -1,0 +1,49 @@
+# SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+%YAML 1.2
+---
+$id: http://devicetree.org/schemas/dc/snps,dc.yaml#
+$schema: http://devicetree.org/meta-schemas/core.yaml#
+
+title: Phytium Display Controller
+
+maintainers:
+  - Chen Baozi <chenbaozi@phytium.com.cn>
+
+allOf:
+  - $ref: /schemas/dc/display-controller.yaml#
+
+properties:
+  compatible:
+    const: phytium,dc
+
+  reg:
+    minItems: 1
+    items:
+      - description: Offset and length of the memory mapped registers
+
+  interrupts:
+    maxItems: 1
+
+  clocks:
+    minItems: 1
+    items:
+      - description:Display controller reference clock source
+
+unevaluatedProperties: false
+
+required:
+  - compatible
+  - reg
+  - interrupts
+
+Example:
+       /memreserve/ 0xf4000000 0x4000000; // (optional)
+
+       dc0@32000000 {
+               compatible = "phytium,dc";
+               reg = <0x0 0x32000000 0x0 0x8000>,
+                     <0x0 0xf4000000 0x0 0x4000000>; // (optional)
+               interrupts = <GIC_SPI 44 IRQ_TYPE_LEVEL_HIGH>;
+               pipe_mask = 0x3
+               edp_mask = 0x0;
+       };

drivers/gpu/drm/Kconfig
@@ -398,6 +398,8 @@ source "drivers/gpu/drm/verisilicon/Kconfig"
 
 source "drivers/gpu/drm/inspur/Kconfig"
 
+source "drivers/gpu/drm/phytium/Kconfig"
+
 # Keep legacy drivers last
 
 menuconfig DRM_LEGACY

drivers/gpu/drm/Makefile
@@ -125,5 +125,6 @@ obj-$(CONFIG_DRM_ASPEED_GFX) += aspeed/
 obj-$(CONFIG_DRM_MCDE) += mcde/
 obj-$(CONFIG_DRM_TIDSS) += tidss/
 obj-y			+= xlnx/
+obj-$(CONFIG_DRM_PHYTIUM) += phytium/
 obj-$(CONFIG_DRM_VERISILICON) += verisilicon/
 obj-$(CONFIG_DRM_INSPUR)  += inspur/

drivers/gpu/drm/phytium/Kconfig
@@ -1,0 +1,7 @@
+config DRM_PHYTIUM
+	tristate "DRM Support for Phytium Graphics Card"
+	depends on DRM && ARCH_PHYTIUM
+	select DRM_KMS_HELPER
+	help
+	  Choose this option if you have a phytium graphics card.
+	  This driver provides kernel mode setting and buffer management to userspace.

drivers/gpu/drm/phytium/Makefile
@@ -1,0 +1,18 @@
+phytium-dc-drm-y := phytium_display_drv.o \
+		    phytium_plane.o \
+		    phytium_crtc.o \
+		    phytium_dp.o \
+		    phytium_fb.o \
+		    phytium_gem.o \
+		    phytium_fbdev.o \
+		    phytium_debugfs.o \
+		    px210_dp.o \
+		    phytium_panel.o \
+		    px210_dc.o \
+		    phytium_pci.o \
+		    pe220x_dp.o \
+		    pe220x_dc.o \
+		    phytium_platform.o
+
+obj-$(CONFIG_DRM_PHYTIUM) += phytium-dc-drm.o
+CFLAGS_REMOVE_phytium_crtc.o += -mgeneral-regs-only

drivers/gpu/drm/phytium/pe220x_dc.c
@@ -1,0 +1,255 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Phytium Pe220x display controller DRM driver
+ *
+ * Copyright (C) 2021-2023, Phytium Technology Co., Ltd.
+ */
+
+#include <drm/drm_atomic_helper.h>
+#include <drm/drm_atomic.h>
+#include <asm/neon.h>
+#include <linux/delay.h>
+#include "phytium_display_drv.h"
+#include "pe220x_reg.h"
+#include "phytium_crtc.h"
+#include "phytium_plane.h"
+#include "phytium_fb.h"
+#include "phytium_gem.h"
+
+void pe220x_dc_hw_disable(struct drm_crtc *crtc);
+
+static const unsigned int pe220x_primary_formats[] = {
+	DRM_FORMAT_ARGB2101010,
+	DRM_FORMAT_ABGR2101010,
+	DRM_FORMAT_RGBA1010102,
+	DRM_FORMAT_BGRA1010102,
+	DRM_FORMAT_ARGB8888,
+	DRM_FORMAT_ABGR8888,
+	DRM_FORMAT_RGBA8888,
+	DRM_FORMAT_BGRA8888,
+	DRM_FORMAT_XRGB8888,
+	DRM_FORMAT_XBGR8888,
+	DRM_FORMAT_RGBX8888,
+	DRM_FORMAT_BGRX8888,
+	DRM_FORMAT_ARGB4444,
+	DRM_FORMAT_ABGR4444,
+	DRM_FORMAT_RGBA4444,
+	DRM_FORMAT_BGRA4444,
+	DRM_FORMAT_XRGB4444,
+	DRM_FORMAT_XBGR4444,
+	DRM_FORMAT_RGBX4444,
+	DRM_FORMAT_BGRX4444,
+	DRM_FORMAT_ARGB1555,
+	DRM_FORMAT_ABGR1555,
+	DRM_FORMAT_RGBA5551,
+	DRM_FORMAT_BGRA5551,
+	DRM_FORMAT_XRGB1555,
+	DRM_FORMAT_XBGR1555,
+	DRM_FORMAT_RGBX5551,
+	DRM_FORMAT_BGRX5551,
+	DRM_FORMAT_RGB565,
+	DRM_FORMAT_BGR565,
+	DRM_FORMAT_YUYV,
+	DRM_FORMAT_UYVY,
+	DRM_FORMAT_NV16,
+	DRM_FORMAT_NV12,
+	DRM_FORMAT_NV21,
+};
+
+static uint64_t pe220x_primary_formats_modifiers[] = {
+	DRM_FORMAT_MOD_LINEAR,
+	DRM_FORMAT_MOD_INVALID
+};
+
+static uint64_t pe220x_cursor_formats_modifiers[] = {
+	DRM_FORMAT_MOD_LINEAR,
+	DRM_FORMAT_MOD_INVALID
+};
+
+static const unsigned int pe220x_cursor_formats[] = {
+	DRM_FORMAT_ARGB8888,
+};
+
+void pe220x_dc_hw_vram_init(struct phytium_display_private *priv, resource_size_t vram_addr,
+			   resource_size_t vram_size)
+{
+	uint32_t config;
+	uint32_t group_offset = priv->address_transform_base;
+
+	phytium_writel_reg(priv, (vram_addr & SRC_ADDR_MASK) >> SRC_ADDR_OFFSET,
+			   group_offset, PE220X_DC_ADDRESS_TRANSFORM_SRC_ADDR);
+	phytium_writel_reg(priv, (vram_size >> SIZE_OFFSET) | ADDRESS_TRANSFORM_ENABLE,
+			   group_offset, PE220X_DC_ADDRESS_TRANSFORM_SIZE);
+	config = phytium_readl_reg(priv, group_offset, PE220X_DC_ADDRESS_TRANSFORM_DST_ADDR);
+	phytium_writel_reg(priv, config, group_offset, PE220X_DC_ADDRESS_TRANSFORM_DST_ADDR);
+}
+
+void pe220x_dc_hw_config_pix_clock(struct drm_crtc *crtc, int clock)
+{
+	struct drm_device *dev = crtc->dev;
+	struct phytium_display_private *priv = dev->dev_private;
+	struct phytium_crtc *phytium_crtc = to_phytium_crtc(crtc);
+	int phys_pipe = phytium_crtc->phys_pipe;
+	int ret = 0;
+
+	/* config pix clock */
+	phytium_writel_reg(priv, FLAG_REQUEST | CMD_PIXEL_CLOCK | (clock & PIXEL_CLOCK_MASK),
+			   0, PE220X_DC_CMD_REGISTER(phys_pipe));
+	ret = phytium_wait_cmd_done(priv, PE220X_DC_CMD_REGISTER(phys_pipe),
+				    FLAG_REQUEST, FLAG_REPLY);
+	if (ret < 0)
+		DRM_ERROR("%s: failed to set pixel clock\n", __func__);
+}
+
+void pe220x_dc_hw_reset(struct drm_crtc *crtc)
+{
+	struct drm_device *dev = crtc->dev;
+	struct phytium_display_private *priv = dev->dev_private;
+	struct phytium_crtc *phytium_crtc = to_phytium_crtc(crtc);
+	int config = 0;
+	int phys_pipe = phytium_crtc->phys_pipe;
+
+	/* disable pixel clock for bmc mode */
+	if (phys_pipe == 0)
+		pe220x_dc_hw_disable(crtc);
+
+	config = phytium_readl_reg(priv, 0, PE220X_DC_CLOCK_CONTROL);
+	config &= (~(DC0_CORE_RESET | DC1_CORE_RESET | AXI_RESET | AHB_RESET));
+
+	if (phys_pipe == 0) {
+		phytium_writel_reg(priv, config | DC0_CORE_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC0_CORE_RESET | AXI_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC0_CORE_RESET | AXI_RESET | AHB_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC0_CORE_RESET | AXI_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC0_CORE_RESET,
+				      0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config, 0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+	} else {
+		phytium_writel_reg(priv, config | DC1_CORE_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC1_CORE_RESET | AXI_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC1_CORE_RESET | AXI_RESET | AHB_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC1_CORE_RESET | AXI_RESET,
+				   0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config | DC1_CORE_RESET,
+				      0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config, 0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+	}
+}
+
+void pe220x_dc_hw_disable(struct drm_crtc *crtc)
+{
+	struct drm_device *dev = crtc->dev;
+	struct phytium_display_private *priv = dev->dev_private;
+	struct phytium_crtc *phytium_crtc = to_phytium_crtc(crtc);
+	int config = 0;
+	int phys_pipe = phytium_crtc->phys_pipe;
+
+	/* clear framebuffer */
+	phytium_writel_reg(priv, CLEAR_VALUE_BLACK, priv->dc_reg_base[phys_pipe],
+			   PHYTIUM_DC_FRAMEBUFFER_CLEARVALUE);
+	config = phytium_readl_reg(priv, priv->dc_reg_base[phys_pipe],
+				   PHYTIUM_DC_FRAMEBUFFER_CONFIG);
+	config |= FRAMEBUFFER_CLEAR;
+	phytium_writel_reg(priv, config, priv->dc_reg_base[phys_pipe],
+			   PHYTIUM_DC_FRAMEBUFFER_CONFIG);
+
+	/* disable cursor */
+	config = phytium_readl_reg(priv, priv->dc_reg_base[phys_pipe], PHYTIUM_DC_CURSOR_CONFIG);
+	config = ((config & (~CURSOR_FORMAT_MASK)) | CURSOR_FORMAT_DISABLED);
+	phytium_writel_reg(priv, config, priv->dc_reg_base[phys_pipe], PHYTIUM_DC_CURSOR_CONFIG);
+	mdelay(20);
+
+	/* reset pix clock */
+	pe220x_dc_hw_config_pix_clock(crtc, 0);
+
+	if (phys_pipe == 0) {
+		config = phytium_readl_reg(priv, 0, PE220X_DC_CLOCK_CONTROL);
+		phytium_writel_reg(priv, config | DC0_CORE_RESET, 0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config & (~DC0_CORE_RESET), 0, PE220X_DC_CLOCK_CONTROL);
+	} else {
+		config = phytium_readl_reg(priv, 0, PE220X_DC_CLOCK_CONTROL);
+		phytium_writel_reg(priv, config | DC1_CORE_RESET, 0, PE220X_DC_CLOCK_CONTROL);
+		udelay(20);
+		phytium_writel_reg(priv, config & (~DC1_CORE_RESET), 0, PE220X_DC_CLOCK_CONTROL);
+	}
+	udelay(20);
+}
+
+int pe220x_dc_hw_fb_format_check(const struct drm_mode_fb_cmd2 *mode_cmd, int count)
+{
+	int ret = 0;
+
+	if (mode_cmd->modifier[count] != DRM_FORMAT_MOD_LINEAR) {
+		DRM_ERROR("unsupported fb modifier 0x%llx\n", mode_cmd->modifier[count]);
+		ret = -EINVAL;
+	}
+
+	return ret;
+}
+
+void pe220x_dc_hw_plane_get_primary_format(const uint64_t **format_modifiers,
+					  const uint32_t **formats,
+					  uint32_t *format_count)
+{
+	*format_modifiers = pe220x_primary_formats_modifiers;
+	*formats = pe220x_primary_formats;
+	*format_count = ARRAY_SIZE(pe220x_primary_formats);
+}
+
+void pe220x_dc_hw_plane_get_cursor_format(const uint64_t **format_modifiers,
+					 const uint32_t **formats,
+					 uint32_t *format_count)
+{
+	*format_modifiers = pe220x_cursor_formats_modifiers;
+	*formats = pe220x_cursor_formats;
+	*format_count = ARRAY_SIZE(pe220x_cursor_formats);
+}
+
+void pe220x_dc_hw_update_primary_hi_addr(struct drm_plane *plane)
+{
+	struct drm_device *dev = plane->dev;
+	struct phytium_display_private *priv = dev->dev_private;
+	struct phytium_plane *phytium_plane = to_phytium_plane(plane);
+	int phys_pipe = phytium_plane->phys_pipe;
+
+	phytium_writel_reg(priv, (phytium_plane->iova[0] >> PREFIX_SHIFT) & PREFIX_MASK,
+			   priv->dc_reg_base[phys_pipe], PE220X_DC_FRAMEBUFFER_Y_HI_ADDRESS);
+
+	phytium_writel_reg(priv, (phytium_plane->iova[1] >> U_PREFIX_SHIFT) & U_PREFIX_MASK,
+			   priv->dc_reg_base[phys_pipe], PE220X_DC_FRAMEBUFFER_U_HI_ADDRESS);
+
+	phytium_writel_reg(priv, (phytium_plane->iova[2] >> V_PREFIX_SHIFT) & V_PREFIX_MASK,
+			   priv->dc_reg_base[phys_pipe], PE220X_DC_FRAMEBUFFER_V_HI_ADDRESS);
+}
+
+void pe220x_dc_hw_update_cursor_hi_addr(struct drm_plane *plane, uint64_t iova)
+{
+	struct drm_device *dev = plane->dev;
+	struct phytium_display_private *priv = dev->dev_private;
+	struct phytium_plane *phytium_plane = to_phytium_plane(plane);
+	int phys_pipe = phytium_plane->phys_pipe;
+	int config;
+
+	config = ((iova >> CURSOR_PREFIX_SHIFT) & CURSOR_PREFIX_MASK);
+	phytium_writel_reg(priv, config, priv->dc_reg_base[phys_pipe], PE220X_DC_CURSOR_HI_ADDRESS);
+}

drivers/gpu/drm/phytium/pe220x_dc.h
@@ -1,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Phytium Pe220x display controller DRM driver
+ *
+ * Copyright (C) 2021-2023, Phytium Technology Co., Ltd.
+ */
+
+#ifndef __PE220X_DC_H__
+#define __PE220X_DC_H__
+
+#define PE220X_DC_PIX_CLOCK_MAX				(594000)
+#define PE220X_DC_HDISPLAY_MAX				3840
+#define PE220X_DC_VDISPLAY_MAX				2160
+#define PE220X_DC_ADDRESS_MASK				0x7f
+
+extern void pe220x_dc_hw_vram_init(struct phytium_display_private *priv,
+				  resource_size_t vram_addr,
+				  resource_size_t vram_size);
+extern void pe220x_dc_hw_config_pix_clock(struct drm_crtc *crtc, int clock);
+extern void pe220x_dc_hw_disable(struct drm_crtc *crtc);
+extern int pe220x_dc_hw_fb_format_check(const struct drm_mode_fb_cmd2 *mode_cmd, int count);
+extern void pe220x_dc_hw_plane_get_primary_format(const uint64_t **format_modifiers,
+						 const uint32_t **formats,
+						 uint32_t *format_count);
+extern void pe220x_dc_hw_plane_get_cursor_format(const uint64_t **format_modifiers,
+						const uint32_t **formats,
+						uint32_t *format_count);
+extern void pe220x_dc_hw_update_primary_hi_addr(struct drm_plane *plane);
+extern void pe220x_dc_hw_update_cursor_hi_addr(struct drm_plane *plane, uint64_t iova);
+void pe220x_dc_hw_reset(struct drm_crtc *crtc);
+#endif /* __PE220X_DC_H__ */

commit f75dfedd090da15bfe1b45b5f27a16e91f1bf648

anolis: drm/phytium: Replace default efi fb0 with dc fb
ANBZ: #9177

phytium inclusion
category: bugfix
CVE: NA

--------------------------------

When enable efi fb, replace the original efi fb0 when loading the
dc driver.

Fixes: 8aa6dbac73b6 (anolis: DRM: Phytium Add Phytium Display Engine support.)
Signed-off-by: WangHao <wanghao1851@phytium.com.cn>
Signed-off-by: Yang Xun <yangxun@phytium.com.cn>
Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
Signed-off-by: Jiakun Shuai <shuaijiakun1288@phytium.com.cn>

drivers/gpu/drm/phytium/phytium_pci.c
@@ -6,6 +6,7 @@
 
 #include <linux/pci.h>
 #include <drm/drm_drv.h>
+#include <drm/drm_fb_helper.h>
 #include <linux/dmaengine.h>
 #include "phytium_display_drv.h"
 #include "phytium_pci.h"
@@ -238,11 +239,33 @@ phytium_pci_private_fini(struct pci_dev *pdev, struct phytium_display_private *p
 	devm_kfree(&pdev->dev, pci_priv);
 }
 
+static int phytium_kick_out_firmware_fb(struct pci_dev *pdev)
+{
+	struct apertures_struct *ap;
+
+	ap = alloc_apertures(1);
+	if (!ap)
+		return -ENOMEM;
+
+	ap->ranges[0].base = pci_resource_start(pdev, 2);
+	ap->ranges[0].size = pci_resource_len(pdev, 2);
+
+	drm_fb_helper_remove_conflicting_framebuffers(ap, "phytiumdrmfb",
+			false);
+	kfree(ap);
+
+	return 0;
+}
+
 static int phytium_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
 	struct phytium_display_private *priv = NULL;
 	struct drm_device *dev = NULL;
 	int ret = 0;
+
+	ret = phytium_kick_out_firmware_fb(pdev);
+	if (ret)
+		DRM_ERROR("failed to remove conflicting framebuffers\n");
 
 	dev = drm_dev_alloc(&phytium_display_drm_driver, &pdev->dev);
 	if (IS_ERR(dev)) {