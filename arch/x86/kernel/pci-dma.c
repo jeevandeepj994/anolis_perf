jeevandeepj994/anolis_perf

commit fb5016b847cfe77174f5b54195c27342f92fdd99

anolis: Fix streaming DMA writes disorder issue for Zhaoxin's KH-40000 platform
ANBZ: #4718

The Zhaoxin KH-40000 platform can't keep the PCIE transaction order for
DMA writes, whose target addresses are located in different NUMA nodes,
from the same device. Patch this issue by flushing the target DMA write
with a subsequent PCIE configuration space read operation.

For streaming dma ops callback .unmap_page/sg and .sync_sg/single_for_cpu,
add PCIE configuration space read operation to flush the target DMA write.

Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

arch/x86/kernel/pci-dma.c
@@ -136,6 +136,142 @@ static int __init pci_iommu_init(void)
 rootfs_initcall(pci_iommu_init);
 
 #ifdef CONFIG_PCI
+#include <linux/intel-iommu.h>
+/***
+ * usage:
+ *  set "zhaoxin_patch_bitmask=0|1" in cmdline
+ * value description:
+ *  bit 0: enable(1) node check or not(0). default 1
+ */
+unsigned long zhaoxin_patch_code = ZHAOXIN_PATCH_CODE_DEFAULT;
+static int __init zhaoxin_patch_code_setup(char *str)
+{
+	int err = kstrtoul(str, 0, &zhaoxin_patch_code);
+
+	if (err || zhaoxin_patch_code > ZHAOXIN_PATCH_CODE_MAX) {
+		pr_err("cmdline 'zhaoxin_patch_bitmask=%s' inappropriate\n", str);
+		zhaoxin_patch_code = ZHAOXIN_PATCH_CODE_DEFAULT;
+		return err;
+	}
+
+	if (ZHAOXIN_P2CW_NODE_CHECK | zhaoxin_patch_code)
+		pr_info("zhaoxin p2cw patch node check is enabled\n");
+
+	return 0;
+}
+__setup("zhaoxin_patch_bitmask=", zhaoxin_patch_code_setup);
+
+bool zhaoxin_kh40000;
+
+bool is_zhaoxin_kh40000(void)
+{
+	return zhaoxin_kh40000;
+}
+
+static void quirk_zhaoxin_p2cw_patch(struct pci_dev *pci)
+{
+	if (pci->revision == 0x10) {
+		zhaoxin_kh40000 = true;
+		pr_debug("zhaoxin p2cw patch is enabled\n");
+	}
+}
+
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_ZHAOXIN, 0x1001, quirk_zhaoxin_p2cw_patch);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_ZHAOXIN, 0x345B, quirk_zhaoxin_p2cw_patch);
+
+static struct pci_dev *get_pci_dev(struct device *dev)
+{
+	if (dev_is_pci(dev))
+		return to_pci_dev(dev);
+
+	if (dev->parent)
+		return get_pci_dev(dev->parent);
+
+	return NULL;
+}
+
+static int patch_do_basic_check(struct device *dev,
+				enum dma_data_direction dir)
+{
+	u64 dma_mask = *dev->dma_mask;
+
+	if (dir != DMA_FROM_DEVICE || dir != DMA_BIDIRECTIONAL)
+		return false;
+
+	if (dma_mask <= DMA_BIT_MASK(32))
+		return false;
+
+	return true;
+}
+
+static int patch_check_paddr(struct device *dev, phys_addr_t paddr,
+			     bool is_iommu)
+{
+	unsigned long pfn;
+	const struct iommu_ops *ops;
+	struct dmar_domain *domain;
+
+	if ((zhaoxin_patch_code & ZHAOXIN_P2CW_NODE_CHECK) == 0)
+		return true;
+
+	if (is_iommu) {
+#ifdef CONFIG_INTEL_IOMMU
+		ops = pci_bus_type.iommu_ops;
+		if (ops && ops->iova_to_phys) {
+			domain = find_domain(dev);
+			paddr = ops->iova_to_phys(&domain->domain, paddr);
+		}
+#endif
+	}
+
+	pfn = PFN_DOWN(paddr);
+	if (pfn_to_nid(pfn) != dev_to_node(dev))
+		return true;
+
+	return false;
+}
+
+static void patch_pci_posted_request_order(struct device *dev)
+{
+	u8 vid;
+	struct pci_dev *pci;
+
+	pci = get_pci_dev(dev);
+	if (!pci)
+		return;
+
+	pci_read_config_byte(pci, PCI_VENDOR_ID, &vid);
+}
+
+void patch_p2cw_single_map(struct device *dev, dma_addr_t paddr,
+			   enum dma_data_direction dir,
+			   const struct dma_map_ops *ops)
+{
+	bool is_iommu = ops ? 1 : 0;
+
+	if (patch_do_basic_check(dev, dir))
+		if (patch_check_paddr(dev, paddr, is_iommu))
+			patch_pci_posted_request_order(dev);
+}
+
+void patch_p2cw_sg_map(struct device *dev, struct scatterlist *sglist,
+		       int nelems, enum dma_data_direction dir,
+		       const struct dma_map_ops *ops)
+{
+	struct scatterlist *sg;
+	int i;
+	bool is_iommu = ops ? 1 : 0;
+
+	if (patch_do_basic_check(dev, dir)) {
+		for_each_sg(sglist, sg, nelems, i) {
+			if (patch_check_paddr(dev, sg_dma_address(sg), is_iommu)) {
+				patch_pci_posted_request_order(dev);
+				break;
+			}
+		}
+	}
+}
+
 /* Many VIA bridges seem to corrupt data for DAC. Disable it here */
 
 static int via_no_dac_cb(struct pci_dev *pdev, void *data)

include/linux/dma-map-ops.h
@@ -349,4 +349,34 @@ static inline void debug_dma_dump_mappings(struct device *dev)
 
 extern const struct dma_map_ops dma_dummy_ops;
 
+#if defined CONFIG_PCI && defined CONFIG_X86_64
+#define ZHAOXIN_P2CW_NODE_CHECK		BIT(0)
+#define ZHAOXIN_PATCH_CODE_DEFAULT	ZHAOXIN_P2CW_NODE_CHECK
+#define ZHAOXIN_PATCH_CODE_MAX		ZHAOXIN_P2CW_NODE_CHECK
+extern unsigned long zhaoxin_patch_code;
+bool is_zhaoxin_kh40000(void);
+
+void patch_p2cw_single_map(struct device *dev, dma_addr_t paddr,
+			   enum dma_data_direction dir, const struct dma_map_ops *ops);
+void patch_p2cw_sg_map(struct device *dev, struct scatterlist *sglist,
+		       int nelems, enum dma_data_direction dir, const struct dma_map_ops *ops);
+#else
+static inline bool is_zhaoxin_kh40000(void)
+{
+	return false;
+}
+
+static inline void patch_p2cw_single_map(struct device *dev, dma_addr_t paddr,
+					 enum dma_data_direction dir, const struct dma_map_ops *ops)
+{
+}
+
+static inline void patch_p2cw_sg_map(struct device *dev, struct scatterlist *sglist,
+				     int nelems, enum dma_data_direction dir,
+				     const struct dma_map_ops *ops)
+{
+}
+
+#endif
+
 #endif /* _LINUX_DMA_MAP_OPS_H */

kernel/dma/mapping.c
@@ -165,6 +165,10 @@ void dma_unmap_page_attrs(struct device *dev, dma_addr_t addr, size_t size,
 	const struct dma_map_ops *ops = get_dma_ops(dev);
 
 	BUG_ON(!valid_dma_direction(dir));
+
+	if (is_zhaoxin_kh40000())
+		patch_p2cw_single_map(dev, addr, dir, ops);
+
 	if (dma_map_direct(dev, ops))
 		dma_direct_unmap_page(dev, addr, size, dir, attrs);
 	else if (ops->unmap_page)
@@ -207,6 +211,10 @@ void dma_unmap_sg_attrs(struct device *dev, struct scatterlist *sg,
 
 	BUG_ON(!valid_dma_direction(dir));
 	debug_dma_unmap_sg(dev, sg, nents, dir);
+
+	if (is_zhaoxin_kh40000())
+		patch_p2cw_sg_map(dev, sg, nents, dir, ops);
+
 	if (dma_map_direct(dev, ops))
 		dma_direct_unmap_sg(dev, sg, nents, dir, attrs);
 	else if (ops->unmap_sg)
@@ -257,6 +265,10 @@ void dma_sync_single_for_cpu(struct device *dev, dma_addr_t addr, size_t size,
 	const struct dma_map_ops *ops = get_dma_ops(dev);
 
 	BUG_ON(!valid_dma_direction(dir));
+
+	if (is_zhaoxin_kh40000())
+		patch_p2cw_single_map(dev, addr, dir, ops);
+
 	if (dma_map_direct(dev, ops))
 		dma_direct_sync_single_for_cpu(dev, addr, size, dir);
 	else if (ops->sync_single_for_cpu)
@@ -285,6 +297,10 @@ void dma_sync_sg_for_cpu(struct device *dev, struct scatterlist *sg,
 	const struct dma_map_ops *ops = get_dma_ops(dev);
 
 	BUG_ON(!valid_dma_direction(dir));
+
+	if (is_zhaoxin_kh40000())
+		patch_p2cw_sg_map(dev, sg, nelems, dir, ops);
+
 	if (dma_map_direct(dev, ops))
 		dma_direct_sync_sg_for_cpu(dev, sg, nelems, dir);
 	else if (ops->sync_sg_for_cpu)

commit 283501dfaf627b5dc0e5509eeb9b29dc54403ca7

anolis: x86/pci-dma: Fix logical error in patch_do_basic_check()
ANBZ: #9155

The purpose of the if statement in function patch_do_basic_check()
regarding dir is to return false only when dir is not equal to
DMA_FROM_DEVICE or DMA_BIDIRECTIONAL. Therefore, the '||' (logical OR)
should be replaced with '&&' (logical AND).

Fixes: fb5016b847cf ("anolis: Fix streaming DMA writes disorder issue for
Zhaoxin's KH-40000 platform")
Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

arch/x86/kernel/pci-dma.c
@@ -195,7 +195,7 @@ static int patch_do_basic_check(struct device *dev,
 {
 	u64 dma_mask = *dev->dma_mask;
 
-	if (dir != DMA_FROM_DEVICE || dir != DMA_BIDIRECTIONAL)
+	if (dir != DMA_FROM_DEVICE && dir != DMA_BIDIRECTIONAL)
 		return false;
 
 	if (dma_mask <= DMA_BIT_MASK(32))