jeevandeepj994/anolis_perf

commit 8df0979486c2ec89dc6a169446e67326053bafbd

EDAC/amd64: Use new AMD Address Translation Library
ANBZ: #8932

commit 6c9058f49084569d1d816e87185e0a4f9ab1a321 upstream.

Remove old address translation code and use the new AMD Address
Translation Library.

Use "imply" in Kconfig so that the "AMD_ATL" config option takes the
value of "EDAC_AMD64" as its default.

Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>
Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
Link: https://lore.kernel.org/r/20240123041401.79812-3-yazen.ghannam@amd.com
[Ruidong: remain hygon address translation]
Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3233

drivers/edac/amd64_edac.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
+#include <linux/ras.h>
 #include "amd64_edac.h"
 #include <asm/amd_nb.h>
 
@@ -2765,9 +2766,11 @@ static void decode_umc_error(int node_id, struct mce *m)
 	u8 ecc_type = (m->status >> 45) & 0x3;
 	struct mem_ctl_info *mci;
 	struct amd64_pvt *pvt;
+	struct atl_err a_err;
 	struct err_info err;
 	u64 sys_addr;
 	u8 umc;
+
 
 	mci = edac_mc_find(node_id);
 	if (!mci)
@@ -2796,16 +2799,23 @@ static void decode_umc_error(int node_id, struct mce *m)
 
 	pvt->ops->get_err_info(m, &err);
 
-	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6)
+	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6) {
 		umc = err.channel << 1;
-	else
-		umc = err.channel;
+		if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc, &sys_addr)) {
+			err.err_code = ERR_NORM_ADDR;
+			goto log_error;
+		}
+	} else {
+		a_err.addr = m->addr;
+		a_err.ipid = m->ipid;
+		a_err.cpu  = m->extcpu;
 
-	if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc, &sys_addr)) {
-		err.err_code = ERR_NORM_ADDR;
-		goto log_error;
+		sys_addr = (u64)amd_convert_umc_mca_addr_to_sys_addr(&a_err);
+		if (IS_ERR_VALUE(sys_addr)) {
+			err.err_code = ERR_NORM_ADDR;
+			goto log_error;
+		}
 	}
-
 	error_address_to_page_and_offset(sys_addr, &err);
 
 log_error:

drivers/edac/Kconfig
@@ -78,6 +78,7 @@ config EDAC_GHES
 config EDAC_AMD64
 	tristate "AMD64 (Opteron, Athlon64)"
 	depends on AMD_NB && EDAC_DECODE_MCE
+	imply AMD_ATL
 	help
 	  Support for error detection and correction of DRAM ECC errors on
 	  the AMD64 families (>= K8) of memory controllers.

commit 6e79157d30a0d9f5dd03cbc7ae53985f4a03a91a

anolis: EDAC/amd64: fix Hygon EDAC to support family 18h model 4h and 5h
ANBZ: #8932

The decoding method for DDR channel in family 18h model 6h differs from
that in model 4h and 5h.

Fixes: 8df0979486c2 ("EDAC/amd64: Use new AMD Address Translation Library")
Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>

drivers/edac/amd64_edac.c
@@ -2799,8 +2799,12 @@ static void decode_umc_error(int node_id, struct mce *m)
 
 	pvt->ops->get_err_info(m, &err);
 
-	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6) {
-		umc = err.channel << 1;
+	if (hygon_f18h_m4h()) {
+		if (boot_cpu_data.x86_model == 0x6)
+			umc = err.channel << 1;
+		else
+			umc = err.channel;
+
 		if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc, &sys_addr)) {
 			err.err_code = ERR_NORM_ADDR;
 			goto log_error;