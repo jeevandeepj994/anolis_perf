ecc41c1295e1ffa2abbf417fbfcff8d0d1bdc1db
Guangguan Wang <guangguan.wang@linux.alibaba.com>
2024-06-03 11:00:19 +0800

    net/smc: change SMCR_RMBE_SIZES from 5 to 15
    ANBZ: #9297
    
    commit 2f4b101c542e102c680d136fc6dca081e3fbda4a upstream
    
    SMCR_RMBE_SIZES is the upper boundary of SMC-R's snd_buf and rcv_buf.
    The maximum bytes of snd_buf and rcv_buf can be calculated by 2^SMCR_
    RMBE_SIZES * 16KB. SMCR_RMBE_SIZES = 5 means the upper boundary is 512KB.
    TCP's snd_buf and rcv_buf max size is configured by net.ipv4.tcp_w/rmem[2]
    whose default value is 4MB or 6MB, is much larger than SMC-R's upper
    boundary.
    
    In some scenarios, such as Recommendation System, the communication
    pattern is mainly large size send/recv, where the size of snd_buf and
    rcv_buf greatly affects performance. Due to the upper boundary
    disadvantage, SMC-R performs poor than TCP in those scenarios. So it
    is time to enlarge the upper boundary size of SMC-R's snd_buf and rcv_buf,
    so that the SMC-R's snd_buf and rcv_buf can be configured to larger size
    for performance gain in such scenarios.
    
    The SMC-R rcv_buf's size will be transferred to peer by the field
    rmbe_size in clc accept and confirm message. The length of the field
    rmbe_size is four bits, which means the maximum value of SMCR_RMBE_SIZES
    is 15. In case of frequently adjusting the value of SMCR_RMBE_SIZES
    in different scenarios, set the value of SMCR_RMBE_SIZES to the maximum
    value 15, which means the upper boundary of SMC-R's snd_buf and rcv_buf
    is 512MB. As the real memory usage is determined by the value of
    net.smc.w/rmem, not by the upper boundary, set the value of SMCR_RMBE_SIZES
    to the maximum value has no side affects.
    
    Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
    Co-developed-by: Wen Gu <guwen@linux.alibaba.com>
    Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3326

--- net/smc/smc_core.c
+++ net/smc/smc_core.c
@@ -2124,7 +2124,7 @@ locked:
 }
 
 #define SMCD_DMBE_SIZES		6 /* 0 -> 16KB, 1 -> 32KB, .. 6 -> 1MB */
-#define SMCR_RMBE_SIZES		5 /* 0 -> 16KB, 1 -> 32KB, .. 5 -> 512KB */
+#define SMCR_RMBE_SIZES		15 /* 0 -> 16KB, 1 -> 32KB, .. 15 -> 512MB */
 
 /* convert the RMB size into the compressed notation (minimum 16K, see
  * SMCD/R_DMBE_SIZES.
