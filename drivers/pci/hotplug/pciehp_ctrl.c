jeevandeepj994/anolis_perf

commit c58940f9cdb18b2ab386ef953f73b0345b2c3845

anolis: pci: Change setting indicators before handling device
ANBZ: #3911

Change setting power indicator on and attention indicator off before
configuring device in board_added(), and change setting power indicator
off before unconfiguring device in remove_board().

Signed-off-by: Guixin Liu <kanie@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Reviewed-by: Liu Song <liusong@linux.alibaba.com>
Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/1127

drivers/pci/hotplug/pciehp_ctrl.c
@@ -83,17 +83,24 @@ static int board_added(struct controller *ctrl)
 		goto err_exit;
 	}
 
+	/*
+	 * Some device drivers will change indicators, so move setting
+	 * indicators before configuring device.
+	 */
+	pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_ON,
+			      PCI_EXP_SLTCTL_ATTN_IND_OFF);
+
 	retval = pciehp_configure_device(ctrl);
 	if (retval) {
 		if (retval != -EEXIST) {
 			ctrl_err(ctrl, "Cannot add device at %04x:%02x:00\n",
 				 pci_domain_nr(parent), parent->number);
+			pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
+			      PCI_EXP_SLTCTL_ATTN_IND_OFF);
 			goto err_exit;
 		}
 	}
 
-	pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_ON,
-			      PCI_EXP_SLTCTL_ATTN_IND_OFF);
 	return 0;
 
 err_exit:
@@ -108,8 +115,14 @@ err_exit:
  */
 static void remove_board(struct controller *ctrl, bool safe_removal)
 {
-	pciehp_unconfigure_device(ctrl, safe_removal);
+	/*
+	 * Some device drivers will change indicators, so move setting
+	 * indicators before unconfiguring device.
+	 */
+	pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
+			      INDICATOR_NOOP);
 
+	pciehp_unconfigure_device(ctrl, safe_removal);
 	if (POWER_CTRL(ctrl)) {
 		pciehp_power_off_slot(ctrl);
 
@@ -124,9 +137,6 @@ static void remove_board(struct controller *ctrl, bool safe_removal)
 		atomic_and(~(PCI_EXP_SLTSTA_DLLSC | PCI_EXP_SLTSTA_PDC),
 			   &ctrl->pending_events);
 	}
-
-	pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
-			      INDICATOR_NOOP);
 }
 
 static int pciehp_enable_slot(struct controller *ctrl);

commit ef2d8c30a824d6aa031ce74f57fd6bd9554f5f11

anolis: pci: fix driver hang when hotplug
ANBZ: #8740

The power indicator is off after pressing the hotplug button,
if remove the board immediately, may cause driver hang, because
the driver may not clean up yet. So do the inserting of board.

Fixes: c58940f9cdb1 ("anolis: pci: Change setting indicators before handling device")
Signed-off-by: Guixin Liu <kanie@linux.alibaba.com>

drivers/pci/hotplug/pciehp_ctrl.c
@@ -87,7 +87,8 @@ static int board_added(struct controller *ctrl)
 	 * Some device drivers will change indicators, so move setting
 	 * indicators before configuring device.
 	 */
-	pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_ON,
+	if (parent->self->enable_vpp)
+		pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_ON,
 			      PCI_EXP_SLTCTL_ATTN_IND_OFF);
 
 	retval = pciehp_configure_device(ctrl);
@@ -95,12 +96,16 @@ static int board_added(struct controller *ctrl)
 		if (retval != -EEXIST) {
 			ctrl_err(ctrl, "Cannot add device at %04x:%02x:00\n",
 				 pci_domain_nr(parent), parent->number);
-			pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
-			      PCI_EXP_SLTCTL_ATTN_IND_OFF);
+			if (parent->self->enable_vpp)
+				pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
+					PCI_EXP_SLTCTL_ATTN_IND_OFF);
 			goto err_exit;
 		}
 	}
 
+	if (!parent->self->enable_vpp)
+		pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_ON,
+			      PCI_EXP_SLTCTL_ATTN_IND_OFF);
 	return 0;
 
 err_exit:
@@ -115,12 +120,15 @@ err_exit:
  */
 static void remove_board(struct controller *ctrl, bool safe_removal)
 {
+	struct pci_bus *parent = ctrl->pcie->port->subordinate;
+
 	/*
 	 * Some device drivers will change indicators, so move setting
 	 * indicators before unconfiguring device.
 	 */
-	pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
-			      INDICATOR_NOOP);
+	if (parent->self->enable_vpp)
+		pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
+			INDICATOR_NOOP);
 
 	pciehp_unconfigure_device(ctrl, safe_removal);
 	if (POWER_CTRL(ctrl)) {
@@ -137,6 +145,10 @@ static void remove_board(struct controller *ctrl, bool safe_removal)
 		atomic_and(~(PCI_EXP_SLTSTA_DLLSC | PCI_EXP_SLTSTA_PDC),
 			   &ctrl->pending_events);
 	}
+
+	if (!parent->self->enable_vpp)
+		pciehp_set_indicators(ctrl, PCI_EXP_SLTCTL_PWR_IND_OFF,
+			INDICATOR_NOOP);
 }
 
 static int pciehp_enable_slot(struct controller *ctrl);

drivers/pci/quirks.c
@@ -31,6 +31,9 @@
 #include <linux/switchtec.h>
 #include <asm/dma.h>	/* isa_dma_bridge_buggy */
 #include "pci.h"
+#ifdef CONFIG_ARM64
+#include <linux/arm-smccc.h>
+#endif
 
 static ktime_t fixup_debug_start(struct pci_dev *dev,
 				 void (*fn)(struct pci_dev *dev))
@@ -5795,6 +5798,14 @@ DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_NVIDIA, 0x0ab8, nvidia_ion_ahci_fixup);
  */
 static void quirk_save_yitian710_regs(struct pci_dev *dev)
 {
+#ifdef CONFIG_ARM64
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(ARM_SMCCC_VENDOR_ENABLE_VPP_FUNC_ID,
+			0, 0, 0, 0, 0, 0, 0, &res);
+
+	dev->enable_vpp = res.a0 != ARM_SMCCC_VENDOR_ENABLE_VPP_FUNC_ID ? 0 : 1;
+#endif
 	dev->broken_bus_reset = 1;
 }
 DECLARE_PCI_FIXUP_CLASS_EARLY(PCI_VENDOR_ID_ALIBABA, 0x8000,

include/linux/pci.h
@@ -466,6 +466,7 @@ struct pci_dev {
 	unsigned int	no_vf_scan:1;		/* Don't scan for VFs after IOV enablement */
 	unsigned int	no_command_memory:1;	/* No PCI_COMMAND_MEMORY */
 	unsigned int	broken_bus_reset:1;	/* Abnormal bus reset */
+	unsigned int	enable_vpp:1;		/* Change the sequence of operation lights */
 	pci_dev_flags_t dev_flags;
 	atomic_t	enable_cnt;	/* pci_enable_device has been called */
 