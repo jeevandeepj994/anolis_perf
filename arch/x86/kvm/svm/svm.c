jeevandeepj994/anolis_perf

commit aa99e6f66d1914e3972d7d4626fd9870555e4875

anolis: KVM: SVM: Export MSR_AMD64_SEV_ES_GHCB to userspace for CSV2 guest
ANBZ: #5950

VMCB.control.ghcb_gpa contains necessary info to support runtime CSV2
guest. At present, it includes the following points:

    1. For GHCB MSR protocol, ghcb_gpa stores the negotiation result
    2. For GHCB page protocol, ghcb_gpa stores the GPA of GHCB page

In addition, AP VCPU's SIPI state and GHCB page mapping state are
temporarily stored in KVM.

When CSV2 guest was migrated to the recipient, KVM needs to restore
VMCB.control.ghcb_gpa, VCPU's SIPI state and GHCB page mapping state on
the source side.

This patch is to support export MSR_AMD64_SEV_ES_GHCB to userspace. KVM
can collect all the infos dictated above and return to userspace if
userspace request to get MSR_AMD64_SEV_ES_GHCB, and KVM can restore all
the infos dictated above if userspace request to set
MSR_AMD64_SEV_ES_GHCB.

Signed-off-by: hanliyang <hanliyang@hygon.cn>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2038

arch/x86/kvm/svm/svm.c
@@ -2756,6 +2756,31 @@ static int svm_get_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	case MSR_F10H_DECFG:
 		msr_info->data = svm->msr_decfg;
 		break;
+	case MSR_AMD64_SEV_ES_GHCB:
+		if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) {
+			/*
+			 * Only support userspace get/set from/to
+			 * vmcb.control.ghcb_gpa
+			 */
+			if (!msr_info->host_initiated ||
+			    !sev_es_guest(svm->vcpu.kvm))
+				return 1;
+
+			msr_info->data = svm->vmcb->control.ghcb_gpa;
+
+			/* Only set status bits when using GHCB page protocol */
+			if (msr_info->data &&
+			    !is_ghcb_msr_protocol(msr_info->data)) {
+				if (svm->ghcb)
+					msr_info->data |= GHCB_MSR_MAPPED_MASK;
+
+				if (svm->received_first_sipi)
+					msr_info->data |=
+						GHCB_MSR_RECEIVED_FIRST_SIPI_MASK;
+			}
+			break;
+		}
+		return 1;
 	default:
 		return kvm_get_msr_common(vcpu, msr_info);
 	}
@@ -2965,6 +2990,47 @@ static int svm_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr)
 		svm->msr_decfg = data;
 		break;
 	}
+	case MSR_AMD64_SEV_ES_GHCB:
+		if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) {
+			/*
+			 * Only support userspace get/set from/to
+			 * vmcb.control.ghcb_gpa
+			 */
+			if (!msr->host_initiated ||
+			    !sev_es_guest(svm->vcpu.kvm))
+				return 1;
+
+			/*
+			 * Value 0 means uninitialized userspace MSR data,
+			 * userspace need get the initial MSR data afterwards.
+			 */
+			if (!data)
+				return 0;
+
+			/* Extract status info when using GHCB page protocol */
+			if (!is_ghcb_msr_protocol(data)) {
+				if (!svm->ghcb &&
+				    (data & GHCB_MSR_MAPPED_MASK)) {
+					/*
+					 * This happened on recipient of migration,
+					 * should return error if cannot map the
+					 * ghcb page.
+					 */
+					if (sev_es_ghcb_map(to_svm(vcpu),
+						data & ~GHCB_MSR_KVM_STATUS_MASK))
+						return 1;
+				}
+
+				if (data & GHCB_MSR_RECEIVED_FIRST_SIPI_MASK)
+					svm->received_first_sipi = true;
+
+				data &= ~GHCB_MSR_KVM_STATUS_MASK;
+			}
+
+			svm->vmcb->control.ghcb_gpa = data;
+			break;
+		}
+		return 1;
 	case MSR_IA32_APICBASE:
 		if (kvm_vcpu_apicv_active(vcpu))
 			avic_update_vapic_bar(to_svm(vcpu), data);
@@ -3798,6 +3864,19 @@ static __no_kcsan fastpath_t svm_vcpu_run(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_svm *svm = to_svm(vcpu);
 
+	/*
+	 * For receipient side of CSV2 guest, fake the exit code as
+	 * SVM_EXIT_ERR and return directly if failed to mapping
+	 * the necessary GHCB page. When handling the exit code
+	 * afterwards, it can exit to userspace and stop the guest.
+	 */
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON &&
+	    sev_es_guest(vcpu->kvm) &&
+	    svm->receiver_ghcb_map_fail) {
+		svm->vmcb->control.exit_code = SVM_EXIT_ERR;
+		return EXIT_FASTPATH_NONE;
+	}
+
 	svm->vmcb->save.rax = vcpu->arch.regs[VCPU_REGS_RAX];
 	svm->vmcb->save.rsp = vcpu->arch.regs[VCPU_REGS_RSP];
 	svm->vmcb->save.rip = vcpu->arch.regs[VCPU_REGS_RIP];
@@ -3992,6 +4071,15 @@ static bool svm_has_emulated_msr(struct kvm *kvm, u32 index)
 	case MSR_IA32_SMBASE:
 		/* SEV-ES guests do not support SMM, so report false */
 		if (kvm && sev_es_guest(kvm))
+			return false;
+		break;
+	case MSR_AMD64_SEV_ES_GHCB:
+		/*
+		 * Only CSV2 guests support to export this MSR, this should
+		 * be determined after KVM_CREATE_VM.
+		 */
+		if (boot_cpu_data.x86_vendor != X86_VENDOR_HYGON ||
+		    (kvm && !sev_es_guest(kvm)))
 			return false;
 		break;
 	default:

arch/x86/kvm/svm/sev.c
@@ -3518,3 +3518,22 @@ void sev_vcpu_deliver_sipi_vector(struct kvm_vcpu *vcpu, u8 vector)
 
 	ghcb_set_sw_exit_info_2(svm->ghcb, 1);
 }
+
+int sev_es_ghcb_map(struct vcpu_svm *svm, u64 ghcb_gpa)
+{
+	if (kvm_vcpu_map(&svm->vcpu, ghcb_gpa >> PAGE_SHIFT, &svm->ghcb_map)) {
+		/* Unable to map GHCB from guest */
+		vcpu_unimpl(&svm->vcpu, "Missing GHCB [%#llx] from guest\n",
+			    ghcb_gpa);
+
+		svm->receiver_ghcb_map_fail = true;
+		return -EINVAL;
+	}
+
+	svm->ghcb = svm->ghcb_map.hva;
+	svm->receiver_ghcb_map_fail = false;
+
+	pr_info("Mapping GHCB [%#llx] from guest at recipient\n", ghcb_gpa);
+
+	return 0;
+}

arch/x86/kvm/svm/svm.h
@@ -192,6 +192,9 @@ struct vcpu_svm {
 	u32 ghcb_sa_len;
 	bool ghcb_sa_sync;
 	bool ghcb_sa_free;
+
+	/* CSV2 migrated ghcb mapping state support */
+	bool receiver_ghcb_map_fail;
 };
 
 struct svm_cpu_data {
@@ -534,12 +537,50 @@ void svm_vcpu_unblocking(struct kvm_vcpu *vcpu);
 #define GHCB_VERSION_MAX	1ULL
 #define GHCB_VERSION_MIN	1ULL
 
+/*
+ * CSV2 live migration support:
+ *     If MSR_AMD64_SEV_ES_GHCB in migration didn't apply GHCB MSR protocol,
+ *     reuse bits [52-63] to indicate vcpu status. The following status are
+ *     currently included:
+ *         * ghcb_map: indicate whether GHCB page was mapped. The mapped GHCB
+ *                     page may be filled with GPRs before VMRUN, so we must
+ *                     remap GHCB page on the recipient's side.
+ *         * received_first_sipi: indicate AP's INIT-SIPI-SIPI stage. Reuse
+ *                     these bits for received_first_sipi is acceptable cause
+ *                     runtime stage of guest's linux only applies GHCB page
+ *                     protocol.
+ *                     It's unlikely that the migration encounter other stages
+ *                     of guest's linux. Once encountered, AP bringup may fail
+ *                     which will not impact user payload.
+ *     Otherbits keep their's original meaning. (See GHCB Spec 2.3.1 for detail)
+ */
+#define GHCB_MSR_KVM_STATUS_POS		52
+#define GHCB_MSR_KVM_STATUS_BITS	12
+#define GHCB_MSR_KVM_STATUS_MASK			\
+	((BIT_ULL(GHCB_MSR_KVM_STATUS_BITS) - 1)	\
+			<< GHCB_MSR_KVM_STATUS_POS)
+#define GHCB_MSR_MAPPED_POS		63
+#define GHCB_MSR_MAPPED_BITS		1
+#define GHCB_MSR_MAPPED_MASK				\
+	((BIT_ULL(GHCB_MSR_MAPPED_BITS) - 1)		\
+			 << GHCB_MSR_MAPPED_POS)
+#define GHCB_MSR_RECEIVED_FIRST_SIPI_POS	62
+#define GHCB_MSR_RECEIVED_FIRST_SIPI_BITS	1
+#define GHCB_MSR_RECEIVED_FIRST_SIPI_MASK			\
+	((BIT_ULL(GHCB_MSR_RECEIVED_FIRST_SIPI_BITS) - 1)	\
+			 << GHCB_MSR_RECEIVED_FIRST_SIPI_POS)
+
 
 extern unsigned int max_sev_asid;
 
 static inline bool svm_sev_enabled(void)
 {
 	return IS_ENABLED(CONFIG_KVM_AMD_SEV) ? max_sev_asid : 0;
+}
+
+static inline bool is_ghcb_msr_protocol(u64 ghcb_val)
+{
+	return ghcb_val & GHCB_MSR_INFO_MASK;
 }
 
 void sev_vm_destroy(struct kvm *kvm);
@@ -563,6 +604,7 @@ void sev_es_vcpu_load(struct vcpu_svm *svm, int cpu);
 void sev_es_vcpu_put(struct vcpu_svm *svm);
 void sev_vcpu_deliver_sipi_vector(struct kvm_vcpu *vcpu, u8 vector);
 void sev_es_unmap_ghcb(struct vcpu_svm *svm);
+int sev_es_ghcb_map(struct vcpu_svm *svm, u64 ghcb_gpa);
 
 /* vmenter.S */
 

arch/x86/kvm/vmx/vmx.c
@@ -6903,6 +6903,7 @@ static bool vmx_has_emulated_msr(struct kvm *kvm, u32 index)
 	case MSR_IA32_VMX_BASIC ... MSR_IA32_VMX_VMFUNC:
 		return nested;
 	case MSR_AMD64_VIRT_SPEC_CTRL:
+	case MSR_AMD64_SEV_ES_GHCB:
 		/* This is AMD only.  */
 		return false;
 	default:

arch/x86/kvm/x86.c
@@ -1377,6 +1377,8 @@ static const u32 emulated_msrs_all[] = {
 
 	MSR_K7_HWCR,
 	MSR_KVM_POLL_CONTROL,
+
+	MSR_AMD64_SEV_ES_GHCB,
 };
 
 static u32 emulated_msrs[ARRAY_SIZE(emulated_msrs_all)];
@@ -3986,6 +3988,17 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		break;
 	case KVM_CAP_X86_NOTIFY_VMEXIT:
 		r = kvm_has_notify_vmexit;
+		break;
+	case KVM_CAP_SEV_ES_GHCB:
+		r = 0;
+
+		/* Both CSV2 and SEV-ES guests support MSR_AMD64_SEV_ES_GHCB,
+		 * but only CSV2 guest support export to emulate
+		 * MSR_AMD64_SEV_ES_GHCB.
+		 */
+		if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+			r = kvm_x86_ops.has_emulated_msr(kvm,
+						MSR_AMD64_SEV_ES_GHCB);
 		break;
 	default:
 		break;

include/uapi/linux/kvm.h
@@ -1095,6 +1095,8 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_X86_TRIPLE_FAULT_EVENT 218
 #define KVM_CAP_X86_NOTIFY_VMEXIT 219
 
+#define KVM_CAP_SEV_ES_GHCB 500
+
 #define KVM_CAP_LOONGARCH_FPU 800
 #define KVM_CAP_LOONGARCH_LSX 801
 #define KVM_CAP_LOONGARCH_VZ 802

commit 38fe09f0c5d3009002c1b93aee1d3e8597b61a83

anolis: KVM: SVM: Return 0 if Non-CSV2 virtual machine try to rd/wr MSR_AMD64_SEV_ES_GHCB from host-side
ANBZ: #9277

state_test/smm_test selftests are failing on HYGON with:
"Unexpected result from KVM_GET_MSRS, r: 84 (failed MSR was 0xc0010130)"

In order to support live migration for Hygon CSV2 guest, the commit
aa99e6f66d19 ("anolis: KVM: SVM: Export MSR_AMD64_SEV_ES_GHCB to
userspace for CSV2 guest") support to emulate KVM_AMD64_SEV_ES_GHCB. But
it doesn't handling the return code well if the virtual machine is
not a CSV2 guest.

Fixes: aa99e6f66d19 ("anolis: KVM: SVM: Export MSR_AMD64_SEV_ES_GHCB to userspace for CSV2 guest")
Signed-off-by: hanliyang <hanliyang@hygon.cn>

arch/x86/kvm/svm/svm.c
@@ -2775,9 +2775,14 @@ static int svm_get_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 			 * Only support userspace get/set from/to
 			 * vmcb.control.ghcb_gpa
 			 */
-			if (!msr_info->host_initiated ||
-			    !sev_es_guest(svm->vcpu.kvm))
+			if (!msr_info->host_initiated)
 				return 1;
+
+			/* Filling the data as 0 if it's not a Hygon CSV2 guest */
+			if (!sev_es_guest(svm->vcpu.kvm)) {
+				msr_info->data = 0;
+				return 0;
+			}
 
 			msr_info->data = svm->vmcb->control.ghcb_gpa;
 
@@ -3009,9 +3014,15 @@ static int svm_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr)
 			 * Only support userspace get/set from/to
 			 * vmcb.control.ghcb_gpa
 			 */
-			if (!msr->host_initiated ||
-			    !sev_es_guest(svm->vcpu.kvm))
+			if (!msr->host_initiated)
 				return 1;
+
+			/*
+			 * Ignore write to this MSR if it's not a Hygon CSV2
+			 * guest.
+			 */
+			if (!sev_es_guest(svm->vcpu.kvm))
+				return 0;
 
 			/*
 			 * Value 0 means uninitialized userspace MSR data,