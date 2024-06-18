960dd0857dc794d28df33389634e63cd122ef216
Yuanhe Shu <xiangzao@linux.alibaba.com>
2024-06-18 19:48:09 +0800

    anolis: selftests/bpf: fix samples/bpf testcases
    ANBZ: #5530
    
    Remove some testcases which could not pass due to
    certain setup.
    
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3359

--- tools/testing/selftests/bpf/samples_test.sh
+++ tools/testing/selftests/bpf/samples_test.sh
@@ -8,20 +8,6 @@ make -j8
 
 ulimit -l unlimited
 
-./fds_example -F /sys/fs/bpf/m -G -m -k 1
-./fds_example -F /sys/fs/bpf/m -G -m -k 1 -v 24
-./fds_example -F /sys/fs/bpf/m -G -m -k 1
-./fds_example -F /sys/fs/bpf/m2 -G -m -k 1
-./fds_example -F /sys/fs/bpf/m2 -G -m
-./fds_example -F /sys/fs/bpf/p -G -p
-./fds_example -F /sys/fs/bpf/p2 -G -p
-
-./do_hbm_test.sh -l -d=1 -D --stats
-./do_hbm_test.sh -l -d=1 -D --stats dctcp
-./do_hbm_test.sh -l -d=1 -D --stats -f=2
-./do_hbm_test.sh -l -d=1 -D --stats -f=4 -P
-./do_hbm_test.sh -l -d=1 -D --stats -f=4 -N
-
 ./sampleip
 
 ./sockex1
@@ -30,13 +16,10 @@ ulimit -l unlimited
 ./sock_example
 ./spintest
 ./syscall_tp
-./task_fd_query
 ./tc_l2_redirect.sh
-./test_cgrp2_sock2.sh
 ./test_cgrp2_sock.sh
 ./test_cgrp2_tc.sh
 ./test_cls_bpf.sh
-./test_current_task_under_cgroup
 ./test_map_in_map
 ./test_overhead
 timeout 10 ./tracex1
@@ -45,7 +28,6 @@ timeout 10 ./tracex3
 timeout 10 ./tracex4
 timeout 10 ./tracex5
 ./tracex6
-./tracex7
 
 ip link add veth0t type veth peer name veth1t
 ip link set veth0t up
@@ -58,7 +40,6 @@ timeout 10 ./xdp_redirect_map `cat /sys/class/net/veth0t/ifindex` \
 `cat /sys/class/net/veth1t/ifindex`
 ip link delete veth0t
 
-./test_ipip.sh
 ./trace_output
 
 cd ../../tools/testing/selftest/bpf/
