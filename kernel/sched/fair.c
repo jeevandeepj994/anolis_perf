jeevandeepj994/anolis_perf

commit 87dd20ef17dc369e7b70089f43d457d9bbee0043

anolis: bpf: add whitelist for fmod_ret functions to prepare for user-space scheduling
ANBZ: #8975

Since sched_ext is still pending and not merged into upstream kernel, we
designed a light-weight user-space scheduling thanks to bpf fmod_ret. It
can modify the return value of almost any function. For safety, upstream
kernel limits the scope of functions allowed to be modified, and let's
enlarge it.

Also add a sysctl "bpf_customized_fmodret" to control this feature.

Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3146

kernel/sched/fair.c
@@ -6440,7 +6440,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 /*
  * Preempt the current task with a newly woken task if needed:
  */
-static void
+static noinline void
 check_preempt_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 {
 	unsigned long ideal_runtime, delta_exec;
@@ -6532,7 +6532,7 @@ wakeup_preempt_entity(struct sched_entity *curr, struct sched_entity *se);
  * 3) pick the "last" process, for cache locality
  * 4) do not run the "skip" process, if something else is available
  */
-static struct sched_entity *
+static noinline struct sched_entity *
 pick_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 {
 	struct sched_entity *left = __pick_first_entity(cfs_rq);
@@ -8513,7 +8513,7 @@ static inline int select_idle_smt(struct task_struct *p, struct sched_domain *sd
  * comparing the average scan cost (tracked in sd->avg_scan_cost) against the
  * average idle time for this rq (as found in rq->avg_idle).
  */
-static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int target)
+static noinline int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int target)
 {
 	struct cpumask *cpus = this_cpu_cpumask_var_ptr(select_idle_mask);
 	struct sched_domain *this_sd;
@@ -9362,7 +9362,7 @@ static void set_skip_buddy(struct sched_entity *se)
 /*
  * Preempt the current task with a newly woken task if needed:
  */
-static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
+static noinline void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
 {
 	struct task_struct *curr = rq->curr;
 	struct sched_entity *se = &curr->se, *pse = &p->se;

include/linux/bpf.h
@@ -1420,6 +1420,7 @@ struct bpf_map *bpf_map_get_curr_or_next(u32 *id);
 struct bpf_prog *bpf_prog_get_curr_or_next(u32 *id);
 
 extern int sysctl_unprivileged_bpf_disabled;
+extern int sysctl_bpf_customized_fmodret;
 
 static inline bool bpf_allow_ptr_leaks(void)
 {

kernel/bpf/verifier.c
@@ -12861,10 +12861,29 @@ static int check_struct_ops_btf_id(struct bpf_verifier_env *env)
 }
 #define SECURITY_PREFIX "security_"
 
-static int check_attach_modify_return(unsigned long addr, const char *func_name)
+/*
+ * List of functions that allowed to be modified by anolis cloud-kernel.
+ * WARNING: Applying fmod_ret on these functions is risky and may cause
+ * kernel crash. Users should have full knowledge about bpf and the
+ * corresponding subsystem (e.g., scheduler).
+ */
+BTF_SET_START(btf_customized_allow_fmodret)
+/* The functions below can be used for user-space scheduling. */
+BTF_ID(func, pick_next_entity)
+BTF_ID(func, check_preempt_tick)
+BTF_ID(func, check_preempt_wakeup)
+BTF_ID(func, select_idle_cpu)
+BTF_SET_END(btf_customized_allow_fmodret)
+
+int sysctl_bpf_customized_fmodret __read_mostly;
+static int check_attach_modify_return(unsigned long addr, const char *func_name, u32 btf_id)
 {
 	if (within_error_injection_list(addr) ||
 	    !strncmp(SECURITY_PREFIX, func_name, sizeof(SECURITY_PREFIX) - 1))
+		return 0;
+
+	if (sysctl_bpf_customized_fmodret &&
+	    btf_id_set_contains(&btf_customized_allow_fmodret, btf_id))
 		return 0;
 
 	return -EINVAL;
@@ -13106,7 +13125,7 @@ int bpf_check_attach_target(struct bpf_verifier_log *log,
 				bpf_log(log, "can't modify return codes of BPF programs\n");
 				return -EINVAL;
 			}
-			ret = check_attach_modify_return(addr, tname);
+			ret = check_attach_modify_return(addr, tname, btf_id);
 			if (ret) {
 				bpf_log(log, "%s() is not modifiable\n", tname);
 				return ret;

kernel/sysctl.c
@@ -2899,6 +2899,15 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= bpf_stats_handler,
 	},
+	{
+		.procname	= "bpf_customized_fmodret",
+		.data		= &sysctl_bpf_customized_fmodret,
+		.maxlen		= sizeof(sysctl_bpf_customized_fmodret),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
 #endif
 #if defined(CONFIG_TREE_RCU)
 	{

commit 71f4485a0f7d938893c3b496dd0cd56fa84ea2e5

anolis: sched: fix compile error with gcc9
ANBZ: #9490

If we compile kernel with gcc9, there will be error:

FAILED unresolved symbol select_idle_cpu
make[1]: *** [vmlinux] Error 255
make: *** [__sub-make] Error 2

This is because we add some functions to bpf whitelist, which still
retains the static attribute. To fix this problem, we remove the static
attribute of the following functions:

  - select_idle_cpu
  - pick_next_entity
  - check_preempt_tick
  - check_preempt_wakeup

Fixes: 87dd20ef17dc("anolis: bpf: add whitelist for fmod_ret functions to prepare for user-space scheduling")
Reported-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/fair.c
@@ -6440,7 +6440,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 /*
  * Preempt the current task with a newly woken task if needed:
  */
-static noinline void
+noinline void
 check_preempt_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 {
 	unsigned long ideal_runtime, delta_exec;
@@ -6532,7 +6532,7 @@ wakeup_preempt_entity(struct sched_entity *curr, struct sched_entity *se);
  * 3) pick the "last" process, for cache locality
  * 4) do not run the "skip" process, if something else is available
  */
-static noinline struct sched_entity *
+noinline struct sched_entity *
 pick_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 {
 	struct sched_entity *left = __pick_first_entity(cfs_rq);
@@ -8514,7 +8514,7 @@ static inline int select_idle_smt(struct task_struct *p, struct sched_domain *sd
  * comparing the average scan cost (tracked in sd->avg_scan_cost) against the
  * average idle time for this rq (as found in rq->avg_idle).
  */
-static noinline int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int target)
+noinline int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int target)
 {
 	struct cpumask *cpus = this_cpu_cpumask_var_ptr(select_idle_mask);
 	struct sched_domain *this_sd;
@@ -9363,7 +9363,7 @@ static void set_skip_buddy(struct sched_entity *se)
 /*
  * Preempt the current task with a newly woken task if needed:
  */
-static noinline void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
+noinline void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
 {
 	struct task_struct *curr = rq->curr;
 	struct sched_entity *se = &curr->se, *pse = &p->se;