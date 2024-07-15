jeevandeepj994/anolis_perf

commit 823433ba94d2f7cc19d93c591d9e4783b2883811

anolis: sched: calculate specs from hierarchical_quota and add cgroup interface
ANBZ: #8765

As the bug of hierarchical_quota has been fixed, we calculate specs
from hierarchical_quota everytime hierarchical_quota updates:
  specs_ratio = hierarchical_quota / ((1 << BW_SHIFT) / 100)

We also add cgroup interface cpu.specs_ratio to show the specs_ratio.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3323

kernel/sched/core.c
@@ -136,43 +136,6 @@ int sched_group_balancer_enable_handler(struct ctl_table *table, int write,
 
 	return ret;
 }
-
-static int tg_set_specs_percent_down(struct task_group *tg, void *data)
-{
-	int *sp = data;
-
-	if (tg->group_balancer &&
-	    tg->cfs_bandwidth.quota == RUNTIME_INF)
-		tg->specs_percent = *sp;
-
-	return 0;
-}
-
-static void tg_set_specs_percent(struct task_group *tg, u64 period, u64 quota)
-{
-	int sp;
-
-	if (!group_balancer_enabled())
-		return;
-	read_lock(&group_balancer_lock);
-	if (quota < 0) {
-		if (tg->group_balancer)
-			tg->specs_percent = -1;
-	} else if ((u64)quota <= U64_MAX / NSEC_PER_USEC) {
-		sp = quota * 100 / period;
-		/* Considering the limited accuracy. */
-		if (unlikely(!sp))
-			sp = 1;
-		if (tg->group_balancer)
-			tg->specs_percent = sp;
-		else
-			walk_tg_tree_from(tg, tg_set_specs_percent_down,
-					  tg_nop, &sp);
-	}
-	read_unlock(&group_balancer_lock);
-}
-#else
-static inline void tg_set_specs_percent(struct task_group *tg, u64 period, u64 quota) { }
 #endif
 
 #ifdef CONFIG_SCHED_CORE
@@ -8547,7 +8510,7 @@ void __init sched_init(void)
 #endif /* CONFIG_RT_GROUP_SCHED */
 	}
 #ifdef CONFIG_GROUP_BALANCER
-	root_task_group.specs_percent = -1;
+	root_task_group.specs_ratio = -1;
 	root_task_group.group_balancer = 0;
 	root_task_group.soft_cpus_version = 0;
 #endif
@@ -8962,7 +8925,6 @@ struct task_group *sched_create_group(struct task_group *parent)
 #endif
 #ifdef CONFIG_GROUP_BALANCER
 	cpumask_copy(&tg->soft_cpus_allowed, &parent->soft_cpus_allowed);
-	tg->specs_percent = -1;
 	tg->group_balancer = 0;
 	tg->soft_cpus_version = 0;
 #endif
@@ -9545,11 +9507,6 @@ static int tg_set_cfs_bandwidth(struct task_group *tg, u64 period, u64 quota,
 	}
 	if (runtime_was_enabled && !runtime_enabled)
 		cfs_bandwidth_usage_dec();
-	/*
-	 * It's ok if we don't hold cfs_b->lock here, because if quota/period
-	 * changed, tg_set_specs_percent() will be called again.
-	 */
-	tg_set_specs_percent(tg, period, quota);
 out_unlock:
 	mutex_unlock(&cfs_constraints_mutex);
 	put_online_cpus();
@@ -9783,6 +9740,7 @@ static int tg_cfs_schedulable_down(struct task_group *tg, void *data)
 		}
 	}
 	cfs_b->hierarchical_quota = quota;
+	tg_set_specs_ratio(tg, quota);
 
 	return 0;
 }
@@ -10063,38 +10021,22 @@ static int cpu_group_balancer_write_u64(struct cgroup_subsys_state *css,
 		goto out;
 
 	if (new) {
-		struct task_group *trail_tg = tg;
-		struct cfs_bandwidth *cfs_b;
-		int specs;
-
 		retval = validate_group_balancer(tg);
 		if (retval)
 			goto out;
-		/*
-		 * If one of his ancestor(or himself) has limited quota, take
-		 * the specs of the ancstor(or himself) as his.
-		 */
-		while (trail_tg != &root_task_group) {
-			cfs_b = &trail_tg->cfs_bandwidth;
-			raw_spin_lock_irq(&cfs_b->lock);
-			if (cfs_b->quota != RUNTIME_INF) {
-				specs = cfs_b->quota * 100 / cfs_b->period;
-				if (unlikely(!specs))
-					specs = 1;
-				tg->specs_percent = specs;
-				raw_spin_unlock_irq(&cfs_b->lock);
-				break;
-			}
-			raw_spin_unlock_irq(&cfs_b->lock);
-			trail_tg = trail_tg->parent;
-		}
-	} else {
-		tg->specs_percent = -1;
 	}
 	tg->group_balancer = new;
 out:
 	write_unlock(&group_balancer_lock);
 	return retval;
+}
+
+static s64 cpu_specs_ratio_read_s64(struct cgroup_subsys_state *css,
+				    struct cftype *cft)
+{
+	struct task_group *tg = css_tg(css);
+
+	return tg->specs_ratio;
 }
 #endif
 
@@ -10231,6 +10173,11 @@ static struct cftype cpu_legacy_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_group_balancer_read_u64,
 		.write_u64 = cpu_group_balancer_write_u64,
+	},
+	{
+		.name = "specs_ratio",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_s64 = cpu_specs_ratio_read_s64,
 	},
 #endif
 	{ }	/* Terminate */
@@ -10826,6 +10773,11 @@ static struct cftype cpu_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_group_balancer_read_u64,
 		.write_u64 = cpu_group_balancer_write_u64,
+	},
+	{
+		.name = "specs_ratio",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_s64 = cpu_specs_ratio_read_s64,
 	},
 #endif
 	{ }	/* terminate */

kernel/sched/fair.c
@@ -13918,6 +13918,7 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 #endif
 
 	init_cfs_bandwidth(tg_cfs_bandwidth(tg), tg_cfs_bandwidth(parent));
+	tg_set_specs_ratio(tg, tg_cfs_bandwidth(tg)->hierarchical_quota);
 
 	for_each_possible_cpu(i) {
 		cfs_rq = kzalloc_node(sizeof(struct cfs_rq),

kernel/sched/sched.h
@@ -551,7 +551,7 @@ struct task_group {
 #ifdef CONFIG_GROUP_BALANCER
 	cpumask_t		soft_cpus_allowed;
 	int			soft_cpus_version;
-	int			specs_percent;
+	int			specs_ratio;
 	bool			group_balancer;
 #endif
 
@@ -3409,9 +3409,29 @@ static inline const struct cpumask *task_allowed_cpu(struct task_struct *p)
 	}
 	return p->cpus_ptr;
 }
+
+static inline void tg_set_specs_ratio(struct task_group *tg, u64 quota)
+{
+	u64 specs_ratio;
+
+	if (quota == RUNTIME_INF) {
+		tg->specs_ratio = -1;
+		return;
+	}
+
+	specs_ratio = quota / ((1 << BW_SHIFT) / 100);
+
+	/* If specs_ratio is bigger than INT_MAX, set specs_ratio -1. */
+	tg->specs_ratio = specs_ratio > INT_MAX ? -1 : specs_ratio;
+}
+
+extern int sched_init_group_balancer_sched_domains(const struct cpumask *cpu_map);
+extern void sched_clear_group_balancer_sched_domains(void);
+extern void sched_init_group_balancer(void);
 #else
 static inline const struct cpumask *task_allowed_cpu(struct task_struct *p)
 {
 	return p->cpus_ptr;
 }
+static void tg_set_specs_ratio(struct task_group *tg, u64 quota) { }
 #endif

commit 81e0e8be59292033a755dea636fceac80926ecea

anolis: sched: fix compile error of tg_set_specs_ratio()
ANBZ: #9510

If CONFIG_CFS_BANDWIDTH is off, there will be compile error of
tg_set_specs_ratio(), which references hierarchical_quota directly.

To fix this error, we only pass one parameter, struct task_group *tg,
and we get hierarchical_quota from tg in function tg_set_specs_ratio(),
as we have already updated hierarchical_quota before we call
tg_set_specs_ratio().

Fixes: 823433ba94d2 ("anolis: sched: calculate specs from hierarchical_quota and add cgroup interface")
Reported-by: Zelin Deng <zelin.deng@linux.alibaba.com>
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/core.c
@@ -9686,7 +9686,7 @@ static int tg_cfs_schedulable_down(struct task_group *tg, void *data)
 		}
 	}
 	cfs_b->hierarchical_quota = quota;
-	tg_set_specs_ratio(tg, quota);
+	tg_set_specs_ratio(tg);
 
 	return 0;
 }

kernel/sched/fair.c
@@ -13898,6 +13898,24 @@ void free_fair_sched_group(struct task_group *tg)
 	kfree(tg->se);
 }
 
+#ifdef CONFIG_GROUP_BALANCER
+void tg_set_specs_ratio(struct task_group *tg)
+{
+	u64 quota = tg_cfs_bandwidth(tg)->hierarchical_quota;
+	u64 specs_ratio;
+
+	if (quota == RUNTIME_INF) {
+		tg->specs_ratio = -1;
+		return;
+	}
+
+	specs_ratio = quota / ((1 << BW_SHIFT) / 100);
+
+	/* If specs_ratio is bigger than INT_MAX, set specs_ratio -1. */
+	tg->specs_ratio = specs_ratio > INT_MAX ? -1 : specs_ratio;
+}
+#endif
+
 int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 {
 	struct sched_entity *se;
@@ -13924,7 +13942,7 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 #endif
 
 	init_cfs_bandwidth(tg_cfs_bandwidth(tg), tg_cfs_bandwidth(parent));
-	tg_set_specs_ratio(tg, tg_cfs_bandwidth(tg)->hierarchical_quota);
+	tg_set_specs_ratio(tg);
 
 	for_each_possible_cpu(i) {
 		cfs_rq = kzalloc_node(sizeof(struct cfs_rq),

kernel/sched/sched.h
@@ -3394,28 +3394,14 @@ static inline const struct cpumask *task_allowed_cpu(struct task_struct *p)
 	return p->cpus_ptr;
 }
 
-static inline void tg_set_specs_ratio(struct task_group *tg, u64 quota)
-{
-	u64 specs_ratio;
-
-	if (quota == RUNTIME_INF) {
-		tg->specs_ratio = -1;
-		return;
-	}
-
-	specs_ratio = quota / ((1 << BW_SHIFT) / 100);
-
-	/* If specs_ratio is bigger than INT_MAX, set specs_ratio -1. */
-	tg->specs_ratio = specs_ratio > INT_MAX ? -1 : specs_ratio;
-}
-
 extern int sched_init_group_balancer_sched_domains(const struct cpumask *cpu_map);
 extern void sched_clear_group_balancer_sched_domains(void);
 extern void sched_init_group_balancer(void);
+extern void tg_set_specs_ratio(struct task_group *tg);
 #else
 static inline const struct cpumask *task_allowed_cpu(struct task_struct *p)
 {
 	return p->cpus_ptr;
 }
-static inline void tg_set_specs_ratio(struct task_group *tg, u64 quota) { }
+static inline void tg_set_specs_ratio(struct task_group *tg) { }
 #endif