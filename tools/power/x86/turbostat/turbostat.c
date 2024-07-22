jeevandeepj994/anolis_perf

commit bdc99cd192ede050be49462160c5e7a82ebc5fb6

tools/power/turbostat: Add initial support for ArrowLake
ANBZ: #9561

commit 7b57e7b683e3872b02117f46bd7dc7ad765888a8 upstream.

Add initial support for ArrowLake platform.

It shares the same features with CannonLake.

Signed-off-by: Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3521

tools/power/x86/turbostat/turbostat.c
@@ -870,6 +870,7 @@ static const struct platform_data turbostat_pdata[] = {
 	{ INTEL_FAM6_RAPTORLAKE_S, &adl_features },
 	{ INTEL_FAM6_METEORLAKE, &cnl_features },
 	{ INTEL_FAM6_METEORLAKE_L, &cnl_features },
+	{ INTEL_FAM6_ARROWLAKE, &cnl_features },
 	{ INTEL_FAM6_ATOM_SILVERMONT, &slv_features },
 	{ INTEL_FAM6_ATOM_SILVERMONT_D, &slvd_features },
 	{ INTEL_FAM6_ATOM_AIRMONT, &amt_features },

commit e537c794ac64bd0eba55896deb7c8d527b6e3d11

anolis: tools: turbostat: fix turbostat compile issue
ANBZ: #9565

Fix turbostat compile failed issue. In current code base Intel CPU
famlies INTEL_FAM6_RAPTORLAKE_S, INTEL_FAM6_ARROWLAKE and INTEL_FAM6_LUNARLAKE_M
are not decleared, so there will be compile error while building
turbostat.

turbostat.c:870:4: error: ‘INTEL_FAM6_RAPTORLAKE_S’ undeclared here (not in a function); did you mean ‘INTEL_FAM6_RAPTORLAKE_P’?
  870 |  { INTEL_FAM6_RAPTORLAKE_S, &adl_features },
      |    ^~~~~~~~~~~~~~~~~~~~~~~
      |    INTEL_FAM6_RAPTORLAKE_P
turbostat.c:873:4: error: ‘INTEL_FAM6_ARROWLAKE’ undeclared here (not in a function); did you mean ‘INTEL_FAM6_RAPTORLAKE’?
  873 |  { INTEL_FAM6_ARROWLAKE, &cnl_features },
      |    ^~~~~~~~~~~~~~~~~~~~
      |    INTEL_FAM6_RAPTORLAKE
turbostat.c:874:4: error: ‘INTEL_FAM6_LUNARLAKE_M’ undeclared here (not in a function); did you mean ‘INTEL_FAM6_TIGERLAKE_L’?
  874 |  { INTEL_FAM6_LUNARLAKE_M, &cnl_features },
      |    ^~~~~~~~~~~~~~~~~~~~~~
      |    INTEL_FAM6_TIGERLAKE_L
make: *** [Makefile:20: turbostat] Error 1

So, just delete them for now. Later, this patch should be reversed when
the code base actually support these CPU famlies.

Fixes: bdc99cd192ed ("tools/power/turbostat: Add initial support for ArrowLake")
Fixes: 8f1897a108e9 ("tools/power/turbostat: Add initial support for LunarLake")
Fixes: a37f135d4632 ("tools/power/turbostat: Add skeleton support for table driven feature enumeration")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

tools/power/x86/turbostat/turbostat.c
@@ -867,11 +867,8 @@ static const struct platform_data turbostat_pdata[] = {
 	{ INTEL_FAM6_ALDERLAKE_L, &adl_features },
 	{ INTEL_FAM6_RAPTORLAKE, &adl_features },
 	{ INTEL_FAM6_RAPTORLAKE_P, &adl_features },
-	{ INTEL_FAM6_RAPTORLAKE_S, &adl_features },
 	{ INTEL_FAM6_METEORLAKE, &cnl_features },
 	{ INTEL_FAM6_METEORLAKE_L, &cnl_features },
-	{ INTEL_FAM6_ARROWLAKE, &cnl_features },
-	{ INTEL_FAM6_LUNARLAKE_M, &cnl_features },
 	{ INTEL_FAM6_ATOM_SILVERMONT, &slv_features },
 	{ INTEL_FAM6_ATOM_SILVERMONT_D, &slvd_features },
 	{ INTEL_FAM6_ATOM_AIRMONT, &amt_features },