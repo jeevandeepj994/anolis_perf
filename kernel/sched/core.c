jeevandeepj994/anolis_perf

commit b8c642c811549941513cfdc5bd0edd8b1734583d

anolis: sched: introduce ACPU accounting
ANBZ: #6729

When SMT is on, tasks will be disturbed by the tasks on it's SMT
sibling, which will make the tasks running sometimes fast and sometimes
slowly. So far, there isn't any way to assess how much disturbance the
task has received.

To assess the SMT disturbance, we introduce ACPU(assess CPU), which will
account how long the task is running with SMT sibling idle. The
statistical data is shown in /proc/<pid>/sched, row se.core_sibidletime.
Only when kernel.sched_schedstats is on, the data will be counted and
shown.

Co-developed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2260

kernel/sched/core.c
@@ -77,6 +77,10 @@ unsigned int sysctl_sched_cfs_bw_burst_onset_percent;
 unsigned int sysctl_sched_cfs_bw_burst_enabled = 1;
 #endif
 
+#ifdef CONFIG_SCHED_ACPU
+DEFINE_STATIC_KEY_FALSE(acpu_enabled);
+#endif
+
 /*
  * period over which we measure -rt task CPU usage in us.
  * default: 1s
@@ -3913,6 +3917,84 @@ fire_sched_out_preempt_notifiers(struct task_struct *curr,
 
 #endif /* CONFIG_PREEMPT_NOTIFIERS */
 
+#ifdef CONFIG_SCHED_ACPU
+void acpu_enable(void)
+{
+	int i;
+
+	for_each_possible_cpu(i) {
+		struct rq *rq = cpu_rq(i);
+
+		/* It may be not that accurate, but useful enough. */
+		rq->last_acpu_update_time = rq->clock;
+	}
+	static_branch_enable(&acpu_enabled);
+}
+
+static void update_acpu(struct rq *rq, struct task_struct *prev, struct task_struct *next)
+{
+	const int cpu = cpu_of(rq);
+	const struct cpumask *smt_mask = cpu_smt_mask(cpu);
+	u64 now = rq_clock(rq);
+	u64 sibidle_sum, last_update_time;
+	s64 delta, last;
+	int i;
+
+	if (!static_branch_likely(&acpu_enabled) || !schedstat_enabled())
+		return;
+
+	/* Update idle sum and busy sum for current rq. */
+	delta = now - rq->last_acpu_update_time;
+	if (prev == rq->idle)
+		rq->acpu_idle_sum += delta;
+
+	/*
+	 * Be carefule, smt_mask maybe NULL.
+	 * We only consider the case where there are two SMT at this stage.
+	 */
+	if (unlikely(!smt_mask) || unlikely(cpumask_weight(smt_mask) != 2))
+		goto out;
+
+	for_each_cpu(i, smt_mask) {
+		if (i != cpu) {
+			struct rq *rq_i = cpu_rq(i);
+			struct task_struct *curr_i = rq_i->curr;
+
+			last = (s64)(rq->last_acpu_update_time -
+				     rq_i->last_acpu_update_time);
+			last_update_time = last >= 0 ? rq->last_acpu_update_time :
+						       rq_i->last_acpu_update_time;
+			/*
+			 * Sibling may update acpu at the same time, and it's
+			 * timestamp may be newer than this rq.
+			 */
+			delta = now - last_update_time;
+			delta = delta > 0 ? delta : 0;
+
+			/* Add the delta to improve accuracy. */
+			sibidle_sum = last >= 0 ? rq->sibidle_sum : rq_i->acpu_idle_sum;
+			if (curr_i == rq_i->idle)
+				sibidle_sum += delta;
+		}
+	}
+
+	if (prev != rq->idle) {
+		delta = sibidle_sum - rq->sibidle_sum;
+		delta = delta > 0 ? delta : 0;
+		__account_sibidle_time(prev, delta);
+	}
+
+	if (next != rq->idle)
+		rq->sibidle_sum = sibidle_sum;
+out:
+	rq->last_acpu_update_time = now;
+}
+#else
+static inline void update_acpu(struct rq *rq, struct task_struct *prev, struct task_struct *next)
+{
+}
+#endif /* CONFIG_SCHED_ACPU */
+
 static inline void prepare_task(struct task_struct *next)
 {
 #ifdef CONFIG_SMP
@@ -4004,6 +4086,7 @@ prepare_task_switch(struct rq *rq, struct task_struct *prev,
 {
 	kcov_prepare_switch(prev);
 	sched_info_switch(rq, prev, next);
+	update_acpu(rq, prev, next);
 	perf_event_task_sched_out(prev, next);
 	rseq_preempt(prev);
 	fire_sched_out_preempt_notifiers(prev, next);
@@ -4456,6 +4539,7 @@ void scheduler_tick(void)
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
 	update_thermal_load_avg(rq_clock_thermal(rq), rq, thermal_pressure);
 	curr->sched_class->task_tick(rq, curr, 0);
+	update_acpu(rq, curr, curr);
 	calc_global_load_tick(rq);
 	psi_task_tick(rq);
 	sched_core_tick(rq);
@@ -8171,6 +8255,12 @@ void __init sched_init(void)
 		rq_csd_init(rq, &rq->nohz_csd, nohz_csd_func);
 #endif
 #endif /* CONFIG_SMP */
+
+#ifdef CONFIG_SCHED_ACPU
+		rq->acpu_idle_sum = 0;
+		rq->sibidle_sum = 0;
+		rq->last_acpu_update_time = rq->clock;
+#endif
 		hrtick_rq_init(rq);
 		atomic_set(&rq->nr_iowait, 0);
 #if defined(CONFIG_GROUP_IDENTITY) && defined(CONFIG_SCHED_SMT)

kernel/sched/sched.h
@@ -1320,6 +1320,12 @@ struct rq {
 	struct task_struct	*force_idled_core_pick;
 #endif
 
+#ifdef CONFIG_SCHED_ACPU
+	u64 acpu_idle_sum;
+	u64 sibidle_sum;
+	u64 last_acpu_update_time;
+#endif
+
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)

include/linux/sched.h
@@ -500,6 +500,10 @@ struct sched_statistics {
 	u64				core_forceidle_sum;
 #endif
 
+#ifdef CONFIG_SCHED_ACPU
+	u64				core_sibidle_sum;
+#endif
+
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)
@@ -2345,6 +2349,12 @@ extern int sched_core_idle_cpu(int cpu);
 static inline void sched_core_free(struct task_struct *tsk) { }
 static inline void sched_core_fork(struct task_struct *p) { }
 static inline int sched_core_idle_cpu(int cpu) { return idle_cpu(cpu); }
+#endif
+
+#ifdef CONFIG_SCHED_ACPU
+extern void acpu_enable(void);
+#else
+static inline void acpu_enable(void) { }
 #endif
 
 #endif

include/linux/kernel_stat.h
@@ -31,6 +31,9 @@ enum cpu_usage_stat {
 #ifdef CONFIG_SCHED_CORE
 	CPUTIME_FORCEIDLE,
 #endif
+#ifdef CONFIG_SCHED_ACPU
+	CPUTIME_SIBIDLE,
+#endif
 	NR_STATS,
 };
 
@@ -121,6 +124,9 @@ extern void account_idle_ticks(unsigned long ticks);
 
 #ifdef CONFIG_SCHED_CORE
 extern void __account_forceidle_time(struct task_struct *tsk, u64 delta);
+#endif
+#ifdef CONFIG_SCHED_ACPU
+extern void __account_sibidle_time(struct task_struct *tsk, u64 delta);
 #endif
 
 #endif /* _LINUX_KERNEL_STAT_H */

kernel/sched/cputime.c
@@ -238,6 +238,14 @@ void __account_forceidle_time(struct task_struct *p, u64 delta)
 	task_group_account_field(p, CPUTIME_FORCEIDLE, delta);
 }
 #endif
+#ifdef CONFIG_SCHED_ACPU
+void __account_sibidle_time(struct task_struct *p, u64 delta)
+{
+	__schedstat_add(p->se.statistics.core_sibidle_sum, delta);
+
+	task_group_account_field(p, CPUTIME_SIBIDLE, delta);
+}
+#endif
 
 /*
  * When a guest is interrupted for a longer amount of time, missed clock

kernel/sched/debug.c
@@ -1029,6 +1029,9 @@ void proc_sched_show_task(struct task_struct *p, struct seq_file *m)
 #ifdef CONFIG_SCHED_CORE
 		PN_SCHEDSTAT(se.statistics.core_forceidle_sum);
 #endif
+#ifdef CONFIG_SCHED_ACPU
+		PN_SCHEDSTAT(se.statistics.core_sibidle_sum);
+#endif
 	}
 
 	__P(nr_switches);

kernel/smpboot.c
@@ -73,6 +73,7 @@ void __init idle_threads_init(void)
 		if (cpu != boot_cpu)
 			idle_init(cpu);
 	}
+	acpu_enable();
 }
 #endif
 

lib/Kconfig.debug
@@ -1132,6 +1132,13 @@ config SCHEDSTATS_HOST
 	  this config. If Y here, the default value of it is 1, and if N,
 	  the value is 0.
 
+config SCHED_ACPU
+	bool "ACPU info: account idle time of smt to task"
+	depends on DEBUG_KERNEL && PROC_FS && SMP && SCHED_SMT
+	default y
+	help
+	  Add ACPU info in /proc/<pid>/sched.
+
 endmenu
 
 config DEBUG_TIMEKEEPING

commit 236ce4b82e282bea0f0b228906a7fbade515c56d

anolis: sched: remove sibidle time of idle task from rq->sibidle_sum
ANBZ: #9671

In function update_acpu(), we account the sibidle time of the idle
ask into rq->sibidle_sum, which conflicts with the defination of
sibidle.

To fix this problem, we only account the sibidle time into
rq->sibidle_sum if prev is not idle, and it's the same to
rq->acpu_idle_sum, the snapshot of the sibling's sibidle_sum.

Fixes: b8c642c81154 ("anolis: sched: introduce ACPU accounting")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

kernel/sched/core.c
@@ -4042,11 +4042,6 @@ static void update_acpu(struct rq *rq, struct task_struct *prev, struct task_str
 		goto out;
 #endif
 
-	/* Update idle sum and busy sum for current rq. */
-	delta = now - rq->last_acpu_update_time;
-	if (prev == rq->idle)
-		rq->acpu_idle_sum += delta;
-
 	/*
 	 * Be carefule, smt_mask maybe NULL.
 	 * We only consider the case where there are two SMT at this stage.
@@ -4059,6 +4054,7 @@ static void update_acpu(struct rq *rq, struct task_struct *prev, struct task_str
 			struct rq *rq_i = cpu_rq(i);
 			struct task_struct *curr_i = rq_i->curr;
 
+			/* If last >= 0, this cpu is the latest one, else the sibling is. */
 			last = (s64)(rq->last_acpu_update_time -
 				     rq_i->last_acpu_update_time);
 			last_update_time = last >= 0 ? rq->last_acpu_update_time :
@@ -4077,13 +4073,23 @@ static void update_acpu(struct rq *rq, struct task_struct *prev, struct task_str
 			delta_task = now_task - last_update_time_task;
 			delta_task = delta_task > 0 ? delta_task : 0;
 
+			if (prev == rq->idle && rq_i->curr != rq_i->idle) {
+				rq->acpu_idle_sum = last >= 0 ? rq->acpu_idle_sum + delta :
+								rq_i->sibidle_sum + delta;
+				rq->acpu_idle_task_sum = last >= 0 ?
+							 rq->acpu_idle_task_sum + delta_task :
+							 rq_i->sibidle_task_sum + delta_task;
+			}
+
 			/* Add the delta to improve accuracy. */
 			sibidle_sum = last >= 0 ? rq->sibidle_sum : rq_i->acpu_idle_sum;
 			sibidle_task_sum = last_task >= 0 ? rq->sibidle_task_sum :
-							    rq_i->acpu_idle_sum;
-			if (curr_i == rq_i->idle) {
+							    rq_i->acpu_idle_task_sum;
+			if (prev != rq->idle && curr_i == rq_i->idle) {
 				sibidle_sum += delta;
 				sibidle_task_sum += delta_task;
+				rq_i->acpu_idle_sum = sibidle_sum;
+				rq_i->acpu_idle_task_sum = sibidle_task_sum;
 			}
 		}
 	}
@@ -8569,7 +8575,9 @@ void __init sched_init(void)
 
 #ifdef CONFIG_SCHED_ACPU
 		rq->acpu_idle_sum = 0;
+		rq->acpu_idle_task_sum = 0;
 		rq->sibidle_sum = 0;
+		rq->sibidle_task_sum = 0;
 		rq->last_acpu_update_time = rq->clock;
 #endif
 		hrtick_rq_init(rq);

kernel/sched/sched.h
@@ -1337,10 +1337,26 @@ struct rq {
 #endif
 
 #ifdef CONFIG_SCHED_ACPU
+	/* acpu_idle_sum is the snapshot of sibling's sibidle_sum. */
 	u64			acpu_idle_sum;
+	/* sibidle_sum is the time that this rq is busy while its sibling is idle. */
 	u64			sibidle_sum;
+	/*
+	 * last_acpu_update_time is the timestamp of rq_clock() that update_acpu() is called
+	 * last time.
+	 */
 	u64			last_acpu_update_time;
+	/* acpu_idle_task_sum is the snapshot of sibling's sibidle_task_sum. */
+	u64			acpu_idle_task_sum;
+	/*
+	 * sibidle_task_sum is the time that this rq is busy while its sibiling is busy,
+	 * excluding irq time.
+	 */
 	u64			sibidle_task_sum;
+	/*
+	 * last_acpu_update_time_task is the timestamp of rq_clock_task() that update_acpu()
+	 * is called last time.
+	 */
 	u64			last_acpu_update_time_task;
 #endif
 