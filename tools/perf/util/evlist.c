jeevandeepj994/anolis_perf

commit 3796cc24d1f5c3125941766148cfc73ad3b8d274

perf mem: Disable 'mem-loads-aux' group before reporting
ANBZ: #9586

commit d5a8bd0fcd069819aa48f5e38548e07d5eb3e651 upstream.

For some platforms, such as Alderlake, the 'mem-loads' event is required
to use together with 'mem-loads-aux' within a group and 'mem-loads-aux'
must be the group leader. Now we disable this group before reporting
because 'mem-loads-aux' is just an auxiliary event. It doesn't carry
any valid memory load result. If we show the 'mem-loads-aux' +
'mem-loads' as a group in report, it needs many of changes but they
are totally unnecessary.

  [kun: conflicts solved. ]

Signed-off-by: Jin Yao <yao.jin@linux.intel.com>
Acked-by: Jiri Olsa <jolsa@redhat.com>
Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
Cc: Andi Kleen <ak@linux.intel.com>
Cc: Ingo Molnar <mingo@redhat.com>
Cc: Kan Liang <kan.liang@intel.com>
Cc: Peter Zijlstra <peterz@infradead.org>
Link: https://lore.kernel.org/r/20210527001610.10553-8-yao.jin@linux.intel.com
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3551

tools/perf/util/evlist.c
@@ -1982,3 +1982,28 @@ struct evsel *evlist__find_evsel(struct evlist *evlist, int idx)
 	}
 	return NULL;
 }
+
+void evlist__check_mem_load_aux(struct evlist *evlist)
+{
+	struct evsel *leader, *evsel, *pos;
+
+	/*
+	 * For some platforms, the 'mem-loads' event is required to use
+	 * together with 'mem-loads-aux' within a group and 'mem-loads-aux'
+	 * must be the group leader. Now we disable this group before reporting
+	 * because 'mem-loads-aux' is just an auxiliary event. It doesn't carry
+	 * any valid memory load information.
+	 */
+	evlist__for_each_entry(evlist, evsel) {
+		leader = evsel->leader;
+		if (leader == evsel)
+			continue;
+
+		if (leader->name && strstr(leader->name, "mem-loads-aux")) {
+			for_each_group_evsel(pos, leader) {
+				pos->leader = pos;
+				pos->core.nr_members = 0;
+			}
+		}
+	}
+}

tools/perf/builtin-report.c
@@ -930,6 +930,8 @@ static int __cmd_report(struct report *rep)
 		return ret;
 	}
 
+	evlist__check_mem_load_aux(session->evlist);
+
 	if (rep->stats_mode)
 		return stats_print(rep);
 

tools/perf/util/evlist.h
@@ -373,4 +373,6 @@ int evlist__ctlfd_ack(struct evlist *evlist);
 #define EVLIST_DISABLED_MSG "Events disabled\n"
 
 struct evsel *evlist__find_evsel(struct evlist *evlist, int idx);
+
+void evlist__check_mem_load_aux(struct evlist *evlist);
 #endif /* __PERF_EVLIST_H */

commit acda67250e3d6191a64fbb73d7f19ea0b1228dd2

anolis: perf mem: fix build error in evlist__check_mem_load_aux()
ANBZ: #9661

evlist__check_mem_load_aux() is introduced, but we missed that a previous patch
8117e613a1cb ("libperf: Move 'leader' from tools/perf to
perf_evsel::leader") already remove 'leader' in struct evsel. So cause a
compiler error.

tools/perf/util/evlist.c:1998:17: error: ‘struct evsel’ has no member named ‘leader’
 1998 |   leader = evsel->leader;
      |                 ^~
tools/perf/util/evlist.c:2004:8: error: ‘struct evsel’ has no member named ‘leader’
 2004 |     pos->leader = pos;
      |        ^~
error: command 'gcc' failed with exit status 1

So, we fix this based on 8117e613a1cb ("libperf: Move 'leader' from tools/perf to
perf_evsel::leader").

Fixes: 3796cc24d1f5 ("perf mem: Disable 'mem-loads-aux' group before reporting")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

tools/perf/util/evlist.c
@@ -1995,13 +1995,13 @@ void evlist__check_mem_load_aux(struct evlist *evlist)
 	 * any valid memory load information.
 	 */
 	evlist__for_each_entry(evlist, evsel) {
-		leader = evsel->leader;
+		leader = evsel__leader(evsel);
 		if (leader == evsel)
 			continue;
 
 		if (leader->name && strstr(leader->name, "mem-loads-aux")) {
 			for_each_group_evsel(pos, leader) {
-				pos->leader = pos;
+				evsel__set_leader(pos, pos);
 				pos->core.nr_members = 0;
 			}
 		}