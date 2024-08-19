jeevandeepj994/anolis_perf

commit 117474ee29fdf6872c88c7b71ab890c38b03c81e

ck: sched: Introduce per-cgroup steal accounting
to #34559224

From the previous patch. We know there are 4 possible states.
Since steal state's transition is complex. We choose to account
its supplement.

        steal = elapse - idle - sum_exec_raw - ineffective

Where elapse is the time since the cgroup is created. sum_exec_raw is
the running time including IRQ time. ineffective is the total time that
the cpuacct-binded cpuset doesn't allow this cpu for the cgroup.

Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>
Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>
Acked-by: Michael Wang <yun.wang@linux.alibaba.com>
Signed-off-by: Erwei Deng <erwei@linux.alibaba.com>
Reviewed-by: Yihao Wu <wuyihao@linux.alibaba.com>
Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>

include/linux/sched.h
@@ -477,8 +477,14 @@ struct sched_entity {
 	u64				vruntime;
 	u64				prev_sum_exec_runtime;
 
+	/* irq time is included */
+	u64				exec_start_raw;
+	u64				sum_exec_raw;
 	u64				cg_idle_start;
 	u64				cg_idle_sum;
+	u64				cg_init_time;
+	u64				cg_ineffective_sum;
+	u64				cg_ineffective_start;
 	seqlock_t			idle_seqlock;
 
 	u64				nr_migrations;

kernel/sched/cpuacct.c
@@ -471,6 +471,10 @@ void cpuacct_cpuset_changed(struct cgroup *cgrp, struct cpumask *deleted,
 		for_each_cpu(cpu, added) {
 			se = tg->se[cpu];
 			cgroup_idle_start(se);
+			__schedstat_add(se->cg_ineffective_sum,
+				__rq_clock_broken(cpu_rq(cpu)) -
+					se->cg_ineffective_start);
+			__schedstat_set(se->cg_ineffective_start, 0);
 		}
 	}
 
@@ -479,6 +483,9 @@ void cpuacct_cpuset_changed(struct cgroup *cgrp, struct cpumask *deleted,
 		for_each_cpu(cpu, deleted) {
 			se = tg->se[cpu];
 			cgroup_idle_end(se);
+			/* Use __rq_clock_broken to avoid warning */
+			__schedstat_set(se->cg_ineffective_start,
+				__rq_clock_broken(cpu_rq(cpu)));
 		}
 	}
 
@@ -526,8 +533,8 @@ static void __cpuacct_get_usage_result(struct cpuacct *ca, int cpu,
 	res->softirq = kcpustat->cpustat[CPUTIME_SOFTIRQ];
 	if (se && schedstat_enabled()) {
 		unsigned int seq;
-		u64 idle_start;
-		u64 clock = cpu_clock(cpu);
+		u64 idle_start, ineff, ineff_start, elapse, complement;
+		u64 clock;
 
 		do {
 			seq = read_seqbegin(&se->idle_seqlock);
@@ -538,7 +545,18 @@ static void __cpuacct_get_usage_result(struct cpuacct *ca, int cpu,
 				res->idle += clock - idle_start;
 		} while (read_seqretry(&se->idle_seqlock, seq));
 
+		ineff = schedstat_val(se->cg_ineffective_sum);
+		ineff_start = schedstat_val(se->cg_ineffective_start);
+		if (ineff_start)
+			__schedstat_add(ineff, clock - ineff_start);
+
 		res->steal = 0;
+
+		elapse = clock - schedstat_val(se->cg_init_time);
+		complement = res->idle + se->sum_exec_raw + ineff;
+		if (elapse > complement)
+			res->steal = elapse - complement;
+
 	} else {
 		res->idle = res->iowait = res->steal = 0;
 	}

kernel/sched/fair.c
@@ -828,6 +828,15 @@ static void update_tg_load_avg(struct cfs_rq *cfs_rq)
 }
 #endif /* CONFIG_SMP */
 
+static inline void
+update_exec_raw(struct cfs_rq *cfs_rq, struct sched_entity *curr)
+{
+	u64 now = rq_clock(rq_of(cfs_rq));
+
+	curr->sum_exec_raw += now - curr->exec_start_raw;
+	curr->exec_start_raw = now;
+}
+
 /*
  * Update the current task's runtime statistics.
  */
@@ -864,6 +873,7 @@ static void update_curr(struct cfs_rq *cfs_rq)
 	}
 
 	account_cfs_rq_runtime(cfs_rq, delta_exec);
+	update_exec_raw(cfs_rq, curr);
 }
 
 static void update_curr_fair(struct rq *rq)
@@ -1045,6 +1055,7 @@ update_stats_curr_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	 * We are starting a new run period:
 	 */
 	se->exec_start = rq_clock_task(rq_of(cfs_rq));
+	se->exec_start_raw = rq_clock(rq_of(cfs_rq));
 }
 
 /**************************************************
@@ -11213,7 +11224,7 @@ void init_tg_cfs_entry(struct task_group *tg, struct cfs_rq *cfs_rq,
 	update_load_set(&se->load, NICE_0_LOAD);
 	se->parent = parent;
 	seqlock_init(&se->idle_seqlock);
-	se->cg_idle_start = cpu_clock(cpu);
+	se->cg_idle_start = se->cg_init_time = cpu_clock(cpu);
 }
 
 static DEFINE_MUTEX(shares_mutex);

commit b7b2e9514c5e8cde246c3f0da06262dfd69b2092

anolis: sched: fix per-cgroup steal accounting when vm get preempted
ANBZ: #9789

sum_exec_raw includes vm's steal accounting, so container's steal will
miss this part, use sum_exec_runtime to let container's steal includes
vm's steal. sum_exec_raw also includes vm's hi/si, which result in
container's steal including vm's hi/si, per-cgroup hi/si accouting makes
no sense, so putting it to steal is tolerable.

Fixes: 117474ee29fd ("ck: sched: Introduce per-cgroup steal accounting")
Signed-off-by: Yi Tao <escape@linux.alibaba.com>

include/linux/sched.h
@@ -531,9 +531,6 @@ struct sched_entity {
 	u64				vruntime;
 	u64				prev_sum_exec_runtime;
 
-	/* irq time is included */
-	u64				exec_start_raw;
-	u64				sum_exec_raw;
 	u64				cg_idle_start;
 	u64				cg_idle_sum;
 	u64				cg_init_time;

kernel/sched/cpuacct.c
@@ -889,7 +889,7 @@ static void __cpuacct_get_usage_result(struct cpuacct *ca, int cpu,
 		res->steal = 0;
 
 		elapse = clock - schedstat_val(se->cg_init_time);
-		complement = res->idle + se->sum_exec_raw + ineff;
+		complement = res->idle + se->sum_exec_runtime + ineff;
 		if (elapse > complement)
 			res->steal = elapse - complement;
 

kernel/sched/fair.c
@@ -2685,15 +2685,6 @@ static void update_tg_load_avg(struct cfs_rq *cfs_rq)
 }
 #endif /* CONFIG_SMP */
 
-static inline void
-update_exec_raw(struct cfs_rq *cfs_rq, struct sched_entity *curr)
-{
-	u64 now = rq_clock(rq_of(cfs_rq));
-
-	curr->sum_exec_raw += now - curr->exec_start_raw;
-	curr->exec_start_raw = now;
-}
-
 /*
  * Update the current task's runtime statistics.
  */
@@ -2735,7 +2726,6 @@ static void update_curr(struct cfs_rq *cfs_rq)
 	}
 
 	account_cfs_rq_runtime(cfs_rq, delta_exec);
-	update_exec_raw(cfs_rq, curr);
 }
 
 static void update_curr_fair(struct rq *rq)
@@ -2970,7 +2960,6 @@ update_stats_curr_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	 * We are starting a new run period:
 	 */
 	se->exec_start = rq_clock_task(rq_of(cfs_rq));
-	se->exec_start_raw = rq_clock(rq_of(cfs_rq));
 }
 
 /**************************************************