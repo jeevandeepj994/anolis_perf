kernelorg-mirror/linux_kernel_git_morse_linux

x86/resctrl: Introduce interface to display number of monitoring counters
The ABMC feature provides an option to the user to assign a hardware
counter to an RMID and monitor the bandwidth as long as the counter is
assigned. Number of assignments depend on number of monitoring counters
available.

Provide the interface to display the number of monitoring counters
supported.

Signed-off-by: Babu Moger <babu.moger@amd.com>

arch/x86/kernel/cpu/resctrl/monitor.c
@@ -1242,6 +1242,8 @@ int __init rdt_get_mon_l3_config(struct rdt_resource *r)
 			r->mon.num_mbm_cntrs = (ebx & 0xFFFF) + 1;
 			if (WARN_ON(r->mon.num_mbm_cntrs > 64))
 				r->mon.num_mbm_cntrs = 64;
+
+			resctrl_file_fflags_init("num_mbm_cntrs", RFTYPE_MON_INFO);
 		}
 	}
 

jeevandeepj994/anolis_perf

ANBZ: #9790
cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

fs/resctrl/monitor.c
@@ -748,6 +748,9 @@ static void l3_mon_evt_init(struct rdt_resource *r)
 					 RF_MON_INFO | RFTYPE_RES_CACHE);
 	}
 
+	if (resctrl_arch_get_abmc_enabled())
+		resctrl_file_fflags_init("num_mbm_cntrs", RF_MON_INFO);
+
 	if (resctrl_arch_is_llc_occupancy_enabled())
 		list_add_tail(&llc_occupancy_event.list, &r->mon.evt_list);
 /*