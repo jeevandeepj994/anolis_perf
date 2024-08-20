kernelorg-mirror/linux_kernel_git_morse_linux

x86/resctrl: Introduce resctrl_file_fflags_init() to initialize fflags
thread_throttle_mode_init() and mbm_config_rftype_init() both initialize
fflags for resctrl files.

Adding new files will involve adding another function to initialize
the fflags. This can be simplified by adding a new function
resctrl_file_fflags_init() and passing the file name and flags
to be initialized.

Consolidate fflags initialization into resctrl_file_fflags_init() and
remove thread_throttle_mode_init() and mbm_config_rftype_init().

Signed-off-by: Babu Moger <babu.moger@amd.com>
Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>

arch/x86/kernel/cpu/resctrl/monitor.c
@@ -1223,11 +1223,13 @@ int __init rdt_get_mon_l3_config(struct rdt_resource *r)
 
 		if (rdt_cpu_has(X86_FEATURE_CQM_MBM_TOTAL)) {
 			mbm_total_event.configurable = true;
-			mbm_config_rftype_init("mbm_total_bytes_config");
+			resctrl_file_fflags_init("mbm_total_bytes_config",
+						 RFTYPE_MON_INFO | RFTYPE_RES_CACHE);
 		}
 		if (rdt_cpu_has(X86_FEATURE_CQM_MBM_LOCAL)) {
 			mbm_local_event.configurable = true;
-			mbm_config_rftype_init("mbm_local_bytes_config");
+			resctrl_file_fflags_init("mbm_local_bytes_config",
+						 RFTYPE_MON_INFO | RFTYPE_RES_CACHE);
 		}
 
 		if (rdt_cpu_has(X86_FEATURE_ABMC)) {

jeevandeepj994/anolis_perf

ANBZ: #9790
cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/
  [kun: we keep thread_throttle_mode_init() to maintain mpam logic.]
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

fs/resctrl/monitor.c
@@ -739,11 +739,13 @@ static void l3_mon_evt_init(struct rdt_resource *r)
 
 	if (resctrl_arch_is_mbm_total_enabled()) {
 		mbm_total_event.configurable = true;
-		mbm_config_rftype_init("mbm_total_bytes_config");
+		resctrl_file_fflags_init("mbm_total_bytes_config",
+					 RF_MON_INFO | RFTYPE_RES_CACHE);
 	}
 	if (resctrl_arch_is_mbm_local_enabled()) {
 		mbm_local_event.configurable = true;
-		mbm_config_rftype_init("mbm_local_bytes_config");
+		resctrl_file_fflags_init("mbm_local_bytes_config",
+					 RF_MON_INFO | RFTYPE_RES_CACHE);
 	}
 
 	if (resctrl_arch_is_llc_occupancy_enabled())