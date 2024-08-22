jeevandeepj994/anolis_perf

commit 36b24ee62f5487870d42bf468e2c1ffac97e548e

anolis: nvme: support new activate namespace
ANBZ: #9752

In new scheme, the nvme driver should activate namespace before
creating io cq/sq, now the activation info should be put into
nvme controller instead of nvme namespace.

Signed-off-by: Guixin Liu <kanie@linux.alibaba.com>
Reviewed-by: Liu Song <liusong@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3704

drivers/nvme/host/core.c
@@ -19,7 +19,6 @@
 #include <linux/ptrace.h>
 #include <linux/nvme_ioctl.h>
 #include <linux/pm_qos.h>
-#include <linux/crc32c.h>
 #include <asm/unaligned.h>
 
 #include "nvme.h"
@@ -3120,44 +3119,12 @@ static ssize_t nsid_show(struct device *dev, struct device_attribute *attr,
 }
 static DEVICE_ATTR_RO(nsid);
 
-static ssize_t activation_info_store(struct device *dev,
-			struct device_attribute *attr, const char *buf,
-			size_t count)
-{
-	struct nvme_ns_head *head = dev_to_ns_head(dev);
-	struct nvme_ctrl *ctrl = nvme_get_ns_from_dev(dev)->ctrl;
-
-	if (ctrl->quirks & NVME_QUIRK_ACTIVATE_NS &&
-	    count == sizeof(struct nvme_activation_info)) {
-		memcpy(&head->activation_info, buf, count);
-		head->activation_check_crc32 = crc32c(0,
-						&head->activation_info, count);
-		return count;
-	}
-	dev_warn(dev, "activation set fail, count:%lu, activate ns quirk:%lu\n",
-		 count, ctrl->quirks & NVME_QUIRK_ACTIVATE_NS);
-	return -EINVAL;
-}
-static DEVICE_ATTR_WO(activation_info);
-
-static ssize_t activation_status_show(struct device *dev,
-			struct device_attribute *attr, char *buf)
-{
-	struct nvme_ns_head *head = dev_to_ns_head(dev);
-
-	return sysfs_emit(buf, "activation_result:0x%.4x\nactivation_count:%llu\n",
-			  head->activation_result, head->activation_count);
-}
-static DEVICE_ATTR_RO(activation_status);
-
 static struct attribute *nvme_ns_id_attrs[] = {
 	&dev_attr_wwid.attr,
 	&dev_attr_uuid.attr,
 	&dev_attr_nguid.attr,
 	&dev_attr_eui.attr,
 	&dev_attr_nsid.attr,
-	&dev_attr_activation_info.attr,
-	&dev_attr_activation_status.attr,
 #ifdef CONFIG_NVME_MULTIPATH
 	&dev_attr_ana_grpid.attr,
 	&dev_attr_ana_state.attr,
@@ -3185,14 +3152,7 @@ static umode_t nvme_ns_id_attrs_are_visible(struct kobject *kobj,
 		if (!memchr_inv(ids->eui64, 0, sizeof(ids->eui64)))
 			return 0;
 	}
-	if (a == &dev_attr_activation_info.attr ||
-	    a == &dev_attr_activation_status.attr) {
-		if (dev_to_disk(dev)->fops != &nvme_bdev_ops)
-			return 0;
-		ctrl = nvme_get_ns_from_dev(dev)->ctrl;
-		if (!(ctrl->quirks & NVME_QUIRK_ACTIVATE_NS))
-			return 0;
-	}
+
 #ifdef CONFIG_NVME_MULTIPATH
 	if (a == &dev_attr_ana_grpid.attr || a == &dev_attr_ana_state.attr) {
 		if (dev_to_disk(dev)->fops != &nvme_bdev_ops) /* per-path attr */
@@ -3984,47 +3944,6 @@ static void nvme_scan_ns_sequential(struct nvme_ctrl *ctrl)
 	nvme_remove_invalid_namespaces(ctrl, nn);
 }
 
-static int nvme_activate_ns(struct nvme_ctrl *ctrl, struct nvme_ns_head *head)
-{
-	struct nvme_command c;
-
-	memset(&c, 0, sizeof(struct nvme_command));
-	c.features.opcode = nvme_admin_set_features;
-	c.features.fid = cpu_to_le32(NVME_FEAT_VENDOR_ACTIVATE_NS);
-	c.features.nsid = cpu_to_le32(head->ns_id);
-	c.features.dword11 = cpu_to_le32(NVME_ACTIVATION_CHECK_MODE_CRC32);
-	c.features.dword12 = cpu_to_le32(head->activation_check_crc32);
-
-	return __nvme_submit_sync_cmd(ctrl->admin_q, &c, NULL,
-			&head->activation_info, sizeof(struct nvme_activation_info),
-			0, NVME_QID_ANY, 0, 0, false);
-}
-
-static void nvme_activate_all_ns(struct nvme_ctrl *ctrl)
-{
-	struct nvme_ns *ns;
-	struct nvme_ns_head *head;
-	int ret;
-
-	if (!(ctrl->quirks & NVME_QUIRK_ACTIVATE_NS))
-		return;
-
-	down_read(&ctrl->namespaces_rwsem);
-	list_for_each_entry(ns, &ctrl->namespaces, list) {
-		head = ns->head;
-		if (head->activation_check_crc32) {
-			dev_info(ctrl->device, "activate ns:%d, start\n",
-				 head->ns_id);
-			ret = nvme_activate_ns(ctrl, head);
-			head->activation_result = ret;
-			head->activation_count++;
-			dev_info(ctrl->device, "activate ns:%d, result:0x%.4x\n",
-				 head->ns_id, ret);
-		}
-	}
-	up_read(&ctrl->namespaces_rwsem);
-}
-
 static void nvme_clear_changed_ns_log(struct nvme_ctrl *ctrl)
 {
 	size_t log_size = NVME_MAX_CHANGED_NAMESPACES * sizeof(__le32);
@@ -4336,7 +4255,6 @@ void nvme_start_ctrl(struct nvme_ctrl *ctrl)
 
 	if (ctrl->queue_count > 1) {
 		nvme_queue_scan(ctrl);
-		nvme_activate_all_ns(ctrl);
 		nvme_start_queues(ctrl);
 		nvme_mpath_update(ctrl);
 	}

drivers/nvme/host/nvme.h
@@ -407,19 +407,6 @@ struct nvme_ns_ids {
 	u8	csi;
 };
 
-enum nvme_activation_check_mode {
-	NVME_ACTIVATION_CHECK_MODE_CRC32 = 1 << 0,
-};
-
-struct nvme_activation_info {
-	u8	user_id[64];
-	u8	cluster_id[64];
-	u8	pod_id[64];
-	u8	disk_id[64];
-	u8	token[128];
-	u8	rsvd[3712];
-};
-
 /*
  * Anchor structure for namespaces.  There is one for each namespace in a
  * NVMe subsystem that any of our controllers can see, and the namespace
@@ -443,10 +430,6 @@ struct nvme_ns_head {
 	struct device		cdev_device;
 
 	struct gendisk		*disk;
-	struct nvme_activation_info activation_info;
-	u32			activation_check_crc32;
-	int			activation_result;
-	u64			activation_count;
 #ifdef CONFIG_NVME_MULTIPATH
 	struct bio_list		requeue_list;
 	spinlock_t		requeue_lock;

drivers/nvme/host/pci.c
@@ -30,6 +30,7 @@
 #ifdef CONFIG_ARM64
 #include <linux/arm-smccc.h>
 #endif
+#include <linux/crc32c.h>
 
 #include "trace.h"
 #include "nvme.h"
@@ -112,6 +113,18 @@ struct nvme_queue;
 static void nvme_dev_disable(struct nvme_dev *dev, bool shutdown);
 static bool __nvme_disable_io_queues(struct nvme_dev *dev, u8 opcode);
 
+enum nvme_activation_check_mode {
+	NVME_ACTIVATION_CHECK_MODE_CRC32 = 1 << 0,
+};
+
+struct nvme_activation_info {
+	/*
+	 * The kernel does not care the details of activation,
+	 * only care its size.
+	 */
+	u8	rsvd[4096];
+};
+
 /*
  * Represents an NVM Express device.  Each nvme_dev is a PCI function.
  */
@@ -162,6 +175,11 @@ struct nvme_dev {
 	unsigned int nr_poll_queues;
 
 	bool attrs_added;
+
+	struct nvme_activation_info activation_info;
+	u32 activation_check_crc32;
+	int activation_result;
+	u64 activation_count;
 };
 
 static int io_queue_depth_set(const char *val, const struct kernel_param *kp)
@@ -2100,6 +2118,36 @@ static ssize_t cmb_show(struct device *dev, struct device_attribute *attr,
 }
 static DEVICE_ATTR_RO(cmb);
 
+static ssize_t activation_info_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t count)
+{
+	struct nvme_dev *ndev = to_nvme_dev(dev_get_drvdata(dev));
+	struct nvme_ctrl *ctrl = &ndev->ctrl;
+
+	if (ctrl->quirks & NVME_QUIRK_ACTIVATE_NS &&
+	    count == sizeof(struct nvme_activation_info)) {
+		memcpy(&ndev->activation_info, buf, count);
+		ndev->activation_check_crc32 = crc32c(0,
+				&ndev->activation_info, count);
+		return count;
+	}
+	dev_warn(dev, "activation set fail, count:%lu, activate ns quirk:%lu\n",
+		 count, ctrl->quirks & NVME_QUIRK_ACTIVATE_NS);
+	return -EINVAL;
+}
+static DEVICE_ATTR_WO(activation_info);
+
+static ssize_t activation_status_show(struct device *dev,
+				      struct device_attribute *attr, char *buf)
+{
+	struct nvme_dev *ndev = to_nvme_dev(dev_get_drvdata(dev));
+
+	return sysfs_emit(buf, "activation_result:0x%.4x\nactivation_count:%llu\n",
+			  ndev->activation_result, ndev->activation_count);
+}
+static DEVICE_ATTR_RO(activation_status);
+
 static umode_t nvme_pci_attrs_are_visible(struct kobject *kobj,
 		struct attribute *a, int n)
 {
@@ -2112,12 +2160,21 @@ static umode_t nvme_pci_attrs_are_visible(struct kobject *kobj,
 
 	if (a == &dev_attr_hmb.attr && !ctrl->hmpre)
 		return 0;
+
+	if (a == &dev_attr_activation_info.attr ||
+	    a == &dev_attr_activation_status.attr) {
+		if (!(ctrl->quirks & NVME_QUIRK_ACTIVATE_NS))
+			return 0;
+	}
+
 	return a->mode;
 }
 
 static struct attribute *nvme_pci_attrs[] = {
 	&dev_attr_cmb.attr,
 	&dev_attr_hmb.attr,
+	&dev_attr_activation_info.attr,
+	&dev_attr_activation_status.attr,
 	NULL,
 };
 
@@ -2674,6 +2731,33 @@ static void nvme_remove_dead_ctrl(struct nvme_dev *dev)
 		nvme_put_ctrl(&dev->ctrl);
 }
 
+static void nvme_activate_ns(struct nvme_dev *ndev)
+{
+	struct nvme_ctrl *ctrl = &ndev->ctrl;
+	struct nvme_command c = {0};
+	int ret;
+
+	if (!(ctrl->quirks & NVME_QUIRK_ACTIVATE_NS))
+		return;
+
+	if (!ndev->activation_check_crc32)
+		return;
+
+	dev_info(ctrl->device, "activate ns start\n");
+	c.features.opcode = nvme_admin_set_features;
+	c.features.fid = cpu_to_le32(NVME_FEAT_VENDOR_ACTIVATE_NS);
+	c.features.dword11 = cpu_to_le32(NVME_ACTIVATION_CHECK_MODE_CRC32);
+	c.features.dword12 = cpu_to_le32(ndev->activation_check_crc32);
+
+	ret = __nvme_submit_sync_cmd(ndev->ctrl.admin_q, &c, NULL,
+			&ndev->activation_info, sizeof(struct nvme_activation_info),
+			0, NVME_QID_ANY, 0, 0, false);
+
+	ndev->activation_result = ret;
+	ndev->activation_count++;
+	dev_info(ctrl->device, "activate ns result:0x%.4x\n", ret);
+}
+
 static void nvme_reset_work(struct work_struct *work)
 {
 	struct nvme_dev *dev =
@@ -2770,6 +2854,8 @@ static void nvme_reset_work(struct work_struct *work)
 		if (result < 0)
 			goto out;
 	}
+
+	nvme_activate_ns(dev);
 
 	result = nvme_setup_io_queues(dev);
 	if (result)

commit d826c779b09b65456c8453f8eeb80a25bb89a5e5

anolis: x86: fix compiler warning in nvme_ns_id_attrs_are_visible
ANBZ: #9799

Warning while compiling in x86 platform:
drivers/nvme/host/core.c: In function ‘nvme_ns_id_attrs_are_visible’:
drivers/nvme/host/core.c:3140:20: warning: unused variable ‘ctrl’ [-Wunused-variable]
 3140 |  struct nvme_ctrl *ctrl;
      |                    ^~~~
So fix it.

Fixes: 36b24ee62f54 ("anolis: nvme: support new activate namespace")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

drivers/nvme/host/core.c
@@ -3137,7 +3137,6 @@ static umode_t nvme_ns_id_attrs_are_visible(struct kobject *kobj,
 {
 	struct device *dev = container_of(kobj, struct device, kobj);
 	struct nvme_ns_ids *ids = &dev_to_ns_head(dev)->ids;
-	struct nvme_ctrl *ctrl;
 
 	if (a == &dev_attr_uuid.attr) {
 		if (uuid_is_null(&ids->uuid) &&