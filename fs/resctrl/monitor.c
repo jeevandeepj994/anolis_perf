jeevandeepj994/anolis_perf

commit dcfedc900dc47faf6b4d48ca555f54df75ab3940

anolis: fs/resctrl: Add a new resctrl monitoring event to get MB in Bps
ANBZ: #8044

Some platforms like Yitian710 can get the memory bandwidth of a specific
PARTID in Bps directly, while current resctrl file system only support
mbm_{local,total}_bytes as counters in bytes. Add a new resctrl monitoring
event mbm_Bps to support this feature.

To avoid introducing a new interface, remains the name "mbm_local_bytes"
instead of "mbm_Bps" as before.

Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2661

fs/resctrl/monitor.c
@@ -307,6 +307,9 @@ static int __mon_event_count(u32 closid, u32 rmid, struct rmid_read *rr)
 	case QOS_L3_OCCUP_EVENT_ID:
 		rr->val += tval;
 		return 0;
+	case QOS_MC_MBM_BPS_EVENT_ID:
+		rr->val += tval;
+		return 0;
 	case QOS_L3_MBM_TOTAL_EVENT_ID:
 		m = &rr->d->mbm_total[idx];
 		break;
@@ -718,6 +721,11 @@ static struct mon_evt mbm_local_event = {
 	.evtid		= QOS_L3_MBM_LOCAL_EVENT_ID,
 };
 
+static struct mon_evt mbm_bps_event = {
+	.name		= "mbm_local_bytes",
+	.evtid		= QOS_MC_MBM_BPS_EVENT_ID,
+};
+
 /*
  * Initialize the event list for the resource.
  *
@@ -754,6 +762,8 @@ static void mba_mon_evt_init(struct rdt_resource *r)
 		list_add_tail(&mbm_total_event.list, &r->evt_list);
 	if (resctrl_arch_is_mbm_local_enabled())
 		list_add_tail(&mbm_local_event.list, &r->evt_list);
+	if (resctrl_arch_is_mbm_bps_enabled())
+		list_add_tail(&mbm_bps_event.list, &r->evt_list);
 }
 #endif
 

arch/x86/include/asm/resctrl.h
@@ -213,6 +213,11 @@ static inline bool resctrl_arch_is_mbm_local_enabled(void)
 	return (rdt_mon_features & (1 << QOS_L3_MBM_LOCAL_EVENT_ID));
 }
 
+static inline bool resctrl_arch_is_mbm_bps_enabled(void)
+{
+	return false;
+}
+
 u64 resctrl_arch_get_prefetch_disable_bits(void);
 int resctrl_arch_pseudo_lock_fn(void *_plr);
 int resctrl_arch_measure_cycles_lat_fn(void *_plr);

drivers/platform/mpam/mpam_resctrl.c
@@ -29,7 +29,7 @@ static struct mpam_resctrl_res mpam_resctrl_exports[RDT_NUM_RESOURCES];
 
 static bool exposed_alloc_capable;
 static bool exposed_mon_capable;
-static struct mpam_class *mbm_local_class;
+static struct mpam_class *mbm_local_class, *mbm_bps_class;
 
 /*
  * MPAM emulates CDP by setting different PARTID in the I/D fields of MPAM1_EL1.
@@ -65,6 +65,11 @@ bool resctrl_arch_mon_capable(void)
 bool resctrl_arch_is_mbm_local_enabled(void)
 {
 	return mbm_local_class;
+}
+
+bool resctrl_arch_is_mbm_bps_enabled(void)
+{
+	return mbm_bps_class;
 }
 
 bool resctrl_arch_get_cdp_enabled(enum resctrl_res_level rid)
@@ -237,7 +242,10 @@ int resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r, int evtid)
 		res = container_of(r, struct mpam_resctrl_res, resctrl_res);
 
 		return mpam_alloc_mbwu_mon(res->class);
-	case QOS_L3_MBM_TOTAL_EVENT_ID:
+	case QOS_MC_MBM_BPS_EVENT_ID:
+		if (mpam_current_machine == MPAM_YITIAN710)
+			return USE_RMID_IDX;
+		return -EOPNOTSUPP;
 	default:
 		return -EOPNOTSUPP;
 	}
@@ -287,7 +295,11 @@ int resctrl_arch_rmid_read(struct rdt_resource	*r, struct rdt_domain *d,
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
 		type = mpam_feat_msmon_mbwu;
 		break;
-	case QOS_L3_MBM_TOTAL_EVENT_ID:
+	case QOS_MC_MBM_BPS_EVENT_ID:
+		if (mpam_current_machine == MPAM_YITIAN710)
+			type = mpam_feat_impl_msmon_mbwu;
+		break;
+	default:
 		return -EOPNOTSUPP;
 	}
 
@@ -421,6 +433,18 @@ static bool class_has_usable_mbwu(struct mpam_class *class)
 	else
 		mpam_monitors_free_runing = true;
 
+	return true;
+}
+
+static bool class_has_usable_impl_mbwu(struct mpam_class *class)
+{
+	struct mpam_props *cprops = &class->props;
+
+	if (!mpam_has_feature(mpam_feat_impl_msmon_mbwu, cprops))
+		return false;
+
+	/* Some vendor-specific judgements may be needed. */
+	mpam_monitors_free_runing = true;
 	return true;
 }
 
@@ -731,6 +755,11 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 			 */
 			r->num_rmid = 1;
 			mbm_local_class = class;
+		} else if (class_has_usable_impl_mbwu(class)) {
+			r->mon_capable = true;
+			exposed_mon_capable = true;
+			if (mpam_current_machine == MPAM_YITIAN710)
+				mbm_bps_class = class;
 		}
 	}
 

fs/resctrl/rdtgroup.c
@@ -92,13 +92,14 @@ void rdt_last_cmd_printf(const char *fmt, ...)
 static bool resctrl_is_mbm_enabled(void)
 {
 	return (resctrl_arch_is_mbm_total_enabled() ||
-		resctrl_arch_is_mbm_local_enabled());
+		resctrl_arch_is_mbm_local_enabled() ||
+		resctrl_arch_is_mbm_bps_enabled());
 }
 
 static bool resctrl_is_mbm_event(int e)
 {
 	return (e >= QOS_L3_MBM_TOTAL_EVENT_ID &&
-		e <= QOS_L3_MBM_LOCAL_EVENT_ID);
+		e <= QOS_MC_MBM_BPS_EVENT_ID);
 }
 
 /*

include/linux/arm_mpam.h
@@ -63,12 +63,11 @@ static inline bool resctrl_arch_event_is_free_running(enum resctrl_event_id evt)
 		return true;
 	case QOS_L3_MBM_TOTAL_EVENT_ID:
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
+	case QOS_MC_MBM_BPS_EVENT_ID:
 		return mpam_monitors_free_runing;
+	default:
+		return false;
 	}
-
-	unreachable();
-
-	return false;
 }
 
 static inline unsigned int resctrl_arch_round_mon_val(unsigned int val)
@@ -80,6 +79,7 @@ bool resctrl_arch_alloc_capable(void);
 bool resctrl_arch_mon_capable(void);
 bool resctrl_arch_is_llc_occupancy_enabled(void);
 bool resctrl_arch_is_mbm_local_enabled(void);
+bool resctrl_arch_is_mbm_bps_enabled(void);
 
 static inline bool resctrl_arch_is_mbm_total_enabled(void)
 {

include/linux/resctrl_types.h
@@ -67,6 +67,9 @@ enum resctrl_event_id {
 	QOS_L3_OCCUP_EVENT_ID		= 0x01,
 	QOS_L3_MBM_TOTAL_EVENT_ID	= 0x02,
 	QOS_L3_MBM_LOCAL_EVENT_ID	= 0x03,
+	QOS_MC_MBM_BPS_EVENT_ID		= 0x04,
 };
+
+#define RESCTRL_MAX_EVENT_NUM		4
 
 #endif /* __LINUX_RESCTRL_TYPES_H */

commit 681fb48ffc3becaaa54de1f49274c50050834068

anolis: x86: fix compiler warning in fs/resctrl/monitor.c
ANBZ: #9799

Warning while compiling in x86 platform:
fs/resctrl/monitor.c:724:23: warning: ‘mbm_bps_event’ defined but not used [-Wunused-variable]
  724 | static struct mon_evt mbm_bps_event = {
      |                       ^~~~~~~~~~~~~
So, fix it.

Fixes: dcfedc900dc4 ("anolis: fs/resctrl: Add a new resctrl monitoring event to get MB in Bps")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

fs/resctrl/monitor.c
@@ -720,11 +720,12 @@ static struct mon_evt mbm_local_event = {
 	.name		= "mbm_local_bytes",
 	.evtid		= QOS_L3_MBM_LOCAL_EVENT_ID,
 };
-
+#ifdef CONFIG_ARM64
 static struct mon_evt mbm_bps_event = {
 	.name		= "mbm_local_bytes",
 	.evtid		= QOS_MC_MBM_BPS_EVENT_ID,
 };
+#endif
 
 /*
  * Initialize the event list for the resource.