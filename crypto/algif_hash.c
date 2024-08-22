jeevandeepj994/anolis_perf

commit b591738dfbc4e1485db840d5b1114623fe8e39cb

anolis: newfeature: crypto: ccp: Process multiple VQ commands once for SM3 ccp.
ANBZ: #6244

optimize sm3 processing performance, the physical page of each sg list
corresponds to a CCP cmd, all cmd prepared, then start ccp.

Signed-off-by: Yabin Li <liyabin@hygon.cn>
Signed-off-by: yangdepei <yangdepei@hygon.cn>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2095

crypto/algif_hash.c
@@ -19,8 +19,17 @@
 #define HASH_COPY_IOVEC_LEN     1
 #define HASH_UPDATE_IOVEC_LEN   2
 
+struct hash_alg_sgl {
+	struct af_alg_sgl sgl;
+	struct list_head list;
+};
+
 struct hash_ctx {
 	struct af_alg_sgl sgl;
+
+	struct hash_alg_sgl first_sgl;
+	struct hash_alg_sgl *last_sgl;
+	struct list_head tsgl_list;
 
 	u8 *result;
 
@@ -63,6 +72,51 @@ static void hash_free_result(struct sock *sk, struct hash_ctx *ctx)
 	ctx->result = NULL;
 }
 
+static int hash_alg_get_tsgl(struct sock *sk, struct hash_ctx *ctx,
+			     struct msghdr *msg, size_t size)
+{
+	struct hash_alg_sgl *tsgl;
+	int err;
+
+	while (size > 0) {
+		if (list_empty(&ctx->tsgl_list)) {
+			tsgl = &ctx->first_sgl;
+		} else {
+			tsgl = sock_kmalloc(sk, sizeof(*tsgl), GFP_KERNEL);
+			if (!tsgl)
+				return -ENOMEM;
+		}
+
+		list_add_tail(&tsgl->list, &ctx->tsgl_list);
+
+		err = af_alg_make_sg(&tsgl->sgl, &msg->msg_iter, size);
+		if (err < 0)
+			return err;
+
+		if (ctx->last_sgl)
+			af_alg_sgl_link(&ctx->last_sgl->sgl, &tsgl->sgl);
+
+		ctx->last_sgl = tsgl;
+		size -= err;
+		iov_iter_advance(&msg->msg_iter, err);
+	}
+
+	return 0;
+}
+
+static void hash_alg_put_tsgl(struct sock *sk, struct hash_ctx *ctx)
+{
+	struct hash_alg_sgl *tsgl, *tmp;
+
+	list_for_each_entry_safe(tsgl, tmp, &ctx->tsgl_list, list) {
+		af_alg_free_sg(&tsgl->sgl);
+		list_del(&tsgl->list);
+		if (tsgl != &ctx->first_sgl)
+			sock_kfree_s(sk, tsgl, sizeof(*tsgl));
+	}
+	ctx->last_sgl = NULL;
+}
+
 #ifdef CONFIG_X86
 static int hash_alg_cmsg_send(struct hash_ctx *ctx, struct msghdr *msg, int *set)
 {
@@ -86,7 +140,7 @@ static int hash_alg_cmsg_send(struct hash_ctx *ctx, struct msghdr *msg, int *set
 
 static int hash_sendmsg_ex(struct socket *sock, struct msghdr *msg)
 {
-	int limit = ALG_MAX_PAGES * PAGE_SIZE;
+	int limit = ALG_MAX_PAGES * ALG_MAX_PAGES * PAGE_SIZE;
 	struct sock *sk = sock->sk;
 	struct alg_sock *ask = alg_sk(sk);
 	struct hash_ctx *ctx = ask->private;
@@ -97,9 +151,6 @@ static int hash_sendmsg_ex(struct socket *sock, struct msghdr *msg)
 	int setiv = 0;
 	int remain = 0;
 	int err;
-
-	if (limit > sk->sk_sndbuf)
-		limit = sk->sk_sndbuf;
 
 	lock_sock(sk);
 	if (!ctx->more) {
@@ -143,22 +194,24 @@ static int hash_sendmsg_ex(struct socket *sock, struct msghdr *msg)
 		if (len > limit)
 			len = limit;
 
-		len = af_alg_make_sg(&ctx->sgl, &msg->msg_iter, len);
-		if (len < 0) {
-			err = copied ? 0 : len;
+		err = hash_alg_get_tsgl(sk, ctx, msg, len);
+		if (err < 0) {
+			err = copied ? 0 : err;
+			hash_alg_put_tsgl(sk, ctx);
 			goto unlock;
 		}
 
-		ahash_request_set_crypt(&ctx->req, ctx->sgl.sg, NULL, len);
+		ahash_request_set_crypt(&ctx->req, ctx->first_sgl.sgl.sg, NULL, len);
 
 		err = crypto_wait_req(crypto_ahash_update(&ctx->req),
 				      &ctx->wait);
-		af_alg_free_sg(&ctx->sgl);
-		if (err)
+		if (err) {
+			hash_alg_put_tsgl(sk, ctx);
 			goto unlock;
+		}
 
+		hash_alg_put_tsgl(sk, ctx);
 		copied += len;
-		iov_iter_advance(&msg->msg_iter, len);
 	}
 
 	if (remain) {
@@ -564,6 +617,8 @@ static int hash_accept_parent_nokey(void *private, struct sock *sk)
 	ctx->len = len;
 	ctx->more = false;
 	crypto_init_wait(&ctx->wait);
+	ctx->last_sgl = NULL;
+	INIT_LIST_HEAD(&ctx->tsgl_list);
 
 	ask->private = ctx;
 

crypto/af_alg.c
@@ -448,6 +448,13 @@ static void af_alg_link_sg(struct af_alg_sgl *sgl_prev,
 	sg_chain(sgl_prev->sg, sgl_prev->npages + 1, sgl_new->sg);
 }
 
+void af_alg_sgl_link(struct af_alg_sgl *sgl_prev,
+		struct af_alg_sgl *sgl_new)
+{
+	af_alg_link_sg(sgl_prev, sgl_new);
+}
+EXPORT_SYMBOL_GPL(af_alg_sgl_link);
+
 void af_alg_free_sg(struct af_alg_sgl *sgl)
 {
 	int i;

drivers/crypto/ccp/ccp-dev-v5.c
@@ -263,6 +263,76 @@ static unsigned int ccp5_get_free_slots(struct ccp_cmd_queue *cmd_q)
 	return n % command_per_q; /* Always one unused spot */
 }
 
+static int ccp5_do_multi_cmds(struct ccp5_desc *desc,
+			struct ccp_cmd_queue *cmd_q)
+{
+	u32 *mP;
+	__le32 *dP;
+	int     i;
+	u32 command_per_q;
+
+	command_per_q = command_per_queue();
+
+	cmd_q->total_ops++;
+
+	if (CCP5_CMD_SOC(desc)) {
+		CCP5_CMD_IOC(desc) = 1;
+		CCP5_CMD_SOC(desc) = 0;
+	}
+
+	mutex_lock(&cmd_q->q_mutex);
+
+	mP = (u32 *) &cmd_q->qbase[cmd_q->qidx];
+	dP = (__le32 *) desc;
+	for (i = 0; i < 8; i++)
+		mP[i] = cpu_to_le32(dP[i]); /* handle endianness */
+
+	cmd_q->qidx = (cmd_q->qidx + 1) % command_per_q;
+
+	mutex_unlock(&cmd_q->q_mutex);
+
+	return 0;
+}
+
+static int ccp5_do_run_cmd(struct ccp_op *op)
+{
+	struct ccp_cmd_queue *cmd_q = op->cmd_q;
+	u32 tail;
+	int ret = 0;
+
+	mutex_lock(&cmd_q->q_mutex);
+
+	/* The data used by this command must be flushed to memory */
+	wmb();
+
+	/* Write the new tail address back to the queue register */
+	tail = low_address(cmd_q->qdma_tail + cmd_q->qidx * Q_DESC_SIZE);
+	iowrite32(tail, cmd_q->reg_tail_lo);
+
+	/* Turn the queue back on using our cached control register */
+	iowrite32(cmd_q->qcontrol | CMD5_Q_RUN, cmd_q->reg_control);
+	mutex_unlock(&cmd_q->q_mutex);
+
+	if (op->ioc) {
+		/* Wait for the job to complete */
+		ret = wait_event_interruptible(cmd_q->int_queue,
+					       cmd_q->int_rcvd);
+		if (ret || cmd_q->cmd_error) {
+			/* Log the error and flush the queue by
+			 * moving the head pointer
+			 */
+			if (cmd_q->cmd_error)
+				ccp_log_error(cmd_q->ccp, cmd_q->cmd_error);
+			iowrite32(tail, cmd_q->reg_head_lo);
+			if (!ret)
+				ret = -EIO;
+		}
+		cmd_q->int_rcvd = 0;
+	}
+
+	return ret;
+}
+
 static int ccp5_do_cmd(struct ccp5_desc *desc,
 		       struct ccp_cmd_queue *cmd_q)
 {
@@ -700,7 +770,7 @@ static int ccp5_perform_sm3(struct ccp_op *op)
 		CCP5_CMD_SM3_HI(&desc) = upper_32_bits(op->u.sm3.msg_bits);
 	}
 
-	return ccp5_do_cmd(&desc, op->cmd_q);
+	return ccp5_do_multi_cmds(&desc, op->cmd_q);
 }
 
 static int ccp5_perform_sm4(struct ccp_op *op)
@@ -1336,6 +1406,7 @@ static const struct ccp_actions ccp5_actions = {
 	.sm3 = ccp5_perform_sm3,
 	.sm4 = ccp5_perform_sm4,
 	.sm4_ctr = ccp5_perform_sm4_ctr,
+	.run_cmd = ccp5_do_run_cmd,
 	.sballoc = ccp_lsb_alloc,
 	.sbfree = ccp_lsb_free,
 	.init = ccp5_init,

drivers/crypto/ccp/ccp-dev.h
@@ -696,6 +696,7 @@ struct ccp_actions {
 	int (*sm3)(struct ccp_op *op);
 	int (*sm4)(struct ccp_op *op);
 	int (*sm4_ctr)(struct ccp_op *op);
+	int (*run_cmd)(struct ccp_op *op);
 	u32 (*sballoc)(struct ccp_cmd_queue *, unsigned int);
 	void (*sbfree)(struct ccp_cmd_queue *, unsigned int, unsigned int);
 	unsigned int (*get_free_slots)(struct ccp_cmd_queue *);

drivers/crypto/ccp/ccp-ops.c
@@ -2622,10 +2622,23 @@ static int ccp_run_sm3_cmd(struct ccp_cmd_queue *cmd_q, struct ccp_cmd *cmd)
 			if (!src.sg_wa.bytes_left && sm3->final)
 				op.eom = 1;
 
+			if (!src.sg_wa.bytes_left || op.soc)
+				op.ioc = 1;
+			else
+				op.ioc = 0;
+
 			ret = cmd_q->ccp->vdata->perform->sm3(&op);
 			if (ret) {
 				cmd->engine_error = cmd_q->cmd_error;
 				goto e_data;
+			}
+
+			if (!src.sg_wa.bytes_left || op.soc) {
+				ret = cmd_q->ccp->vdata->perform->run_cmd(&op);
+				if (ret) {
+					cmd->engine_error = cmd_q->cmd_error;
+					goto e_data;
+				}
 			}
 
 			ccp_process_data(&src, NULL, &op);
@@ -2643,6 +2656,12 @@ static int ccp_run_sm3_cmd(struct ccp_cmd_queue *cmd_q, struct ccp_cmd *cmd)
 		op.src.u.dma.length = SM3_BLOCK_SIZE;
 
 		ret = cmd_q->ccp->vdata->perform->sm3(&op);
+		if (ret) {
+			cmd->engine_error = cmd_q->cmd_error;
+			goto e_data;
+		}
+
+		ret = cmd_q->ccp->vdata->perform->run_cmd(&op);
 		if (ret) {
 			cmd->engine_error = cmd_q->cmd_error;
 			goto e_data;

include/crypto/if_alg.h
@@ -172,6 +172,7 @@ int af_alg_make_sg(struct af_alg_sgl *sgl, struct iov_iter *iter, int len);
 void af_alg_free_sg(struct af_alg_sgl *sgl);
 
 int af_alg_ctrl_cmsg_send(struct msghdr *msg, struct af_alg_control *con);
+void af_alg_sgl_link(struct af_alg_sgl *sgl_prev, struct af_alg_sgl *sgl_new);
 
 static inline struct alg_sock *alg_sk(struct sock *sk)
 {

commit 80e9fe3d922b41e732cc1d017d5d181d262f38e1

anolis: arm64: fix compiler warning in crypto/algif_hash.c
ANBZ: #9799

Warning while compiling in arm64 platform:
crypto/algif_hash.c:107:13: warning: ‘hash_alg_put_tsgl’ defined but not used [-Wunused-function]
  107 | static void hash_alg_put_tsgl(struct sock *sk, struct hash_ctx *ctx, int sg_err)
      |             ^~~~~~~~~~~~~~~~~
crypto/algif_hash.c:75:12: warning: ‘hash_alg_get_tsgl’ defined but not used [-Wunused-function]
   75 | static int hash_alg_get_tsgl(struct sock *sk, struct hash_ctx *ctx,
So fix it.

Fixes: b591738dfbc4 ("anolis: newfeature: crypto: ccp: Process multiple VQ commands once for SM3 ccp.")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

crypto/algif_hash.c
@@ -72,6 +72,7 @@ static void hash_free_result(struct sock *sk, struct hash_ctx *ctx)
 	ctx->result = NULL;
 }
 
+#ifdef CONFIG_X86
 static int hash_alg_get_tsgl(struct sock *sk, struct hash_ctx *ctx,
 			     struct msghdr *msg, size_t size)
 {
@@ -119,7 +120,6 @@ static void hash_alg_put_tsgl(struct sock *sk, struct hash_ctx *ctx, int sg_err)
 	ctx->last_sgl = NULL;
 }
 
-#ifdef CONFIG_X86
 static int hash_alg_cmsg_send(struct hash_ctx *ctx, struct msghdr *msg, int *set)
 {
 	struct af_alg_control con = {};