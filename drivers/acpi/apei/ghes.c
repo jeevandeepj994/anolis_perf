jeevandeepj994/anolis_perf

commit b517a90bf817c8f2674ddc219610e98ffedbcdae

anolis: arm64: avoid speculative access after memory failure
ANBZ: #6171

Memory failure would pass an interrupt when the hardware detects
a memory page corrupted, then the kernel needs to unmap uncorrectable
pages to avoid access and allocation. However, in the implementation
of arm64,  the uncorrectable page is unmapped  only in the page table
of user-space process, the page table of the linear mapping range is
not considered. It incurs the cpu will continue to accept errors
through interrupts because of speculative access.

For that, the uncorrectable page needs to be set to invalid to avoid
speculative access. If the linear mapping is block mapping, we should
firstly split it to pte level.

Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2057

drivers/acpi/apei/ghes.c
@@ -41,6 +41,7 @@
 #include <linux/uuid.h>
 #include <linux/ras.h>
 #include <linux/task_work.h>
+#include <linux/set_memory.h>
 
 #include <acpi/actbl1.h>
 #include <acpi/ghes.h>
@@ -476,12 +477,24 @@ static void memory_failure_cb(struct callback_head *twork)
 	int rc;
 	struct mce_task_work *twcb =
 		container_of(twork, struct mce_task_work, twork);
+	unsigned long pfn = twcb->pfn;
 
 	rc = memory_failure(twcb->pfn, twcb->flags);
 	kfree(twcb);
 
 	if (!rc)
 		return;
+
+	/*
+	 * If the pfn reported by ghes can not be recovered, set
+	 * the corresponding page table of linear mapping range
+	 * to be non-present, which avoids the speculative
+	 * access of corrupted memory.
+	 */
+#ifdef CONFIG_ARM64
+	set_memory_np((unsigned long)page_to_virt(pfn_to_page(pfn)), 1);
+#endif
+
 	/*
 	 * -EHWPOISON from memory_failure() means that it already sent SIGBUS
 	 * to the current process with the proper error info, so no need to

commit 00e0c478df97a49a13f6995ee5131cf96ae11d09

anolis: x86: fix compiler warning in memory_failure_cb
ANBZ: #9799

Warning while compiling in x86 platform:
drivers/acpi/apei/ghes.c: In function ‘memory_failure_cb’:
drivers/acpi/apei/ghes.c:480:16: warning: unused variable ‘pfn’ [-Wunused-variable]
  480 |  unsigned long pfn = twcb->pfn;
      |                ^~~
So, fix it.

Fixes: b517a90bf817 ("anolis: arm64: avoid speculative access after memory failure")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

drivers/acpi/apei/ghes.c
@@ -479,7 +479,7 @@ static void memory_failure_cb(struct callback_head *twork)
 		container_of(twork, struct mce_task_work, twork);
 	unsigned long pfn = twcb->pfn;
 
-	rc = memory_failure(twcb->pfn, twcb->flags);
+	rc = memory_failure(pfn, twcb->flags);
 	kfree(twcb);
 
 	if (!rc)