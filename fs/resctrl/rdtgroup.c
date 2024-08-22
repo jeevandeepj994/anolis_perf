jeevandeepj994/anolis_perf

commit e8c5929cf3ad9e83aac5f915de2afc8edcfb3323

x86/resctrl: Introduce resctrl_file_fflags_init() to initialize fflags
ANBZ: #9790

cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/

thread_throttle_mode_init() and mbm_config_rftype_init() both initialize
fflags for resctrl files.

Adding new files will involve adding another function to initialize
the fflags. This can be simplified by adding a new function
resctrl_file_fflags_init() and passing the file name and flags
to be initialized.

Consolidate fflags initialization into resctrl_file_fflags_init() and
remove thread_throttle_mode_init() and mbm_config_rftype_init().

  [kun: we keep thread_throttle_mode_init() to maintain mpam logic.]

Signed-off-by: Babu Moger <babu.moger@amd.com>
Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3731

fs/resctrl/rdtgroup.c
@@ -1950,20 +1950,18 @@ static void thread_throttle_mode_init(void)
 	    r->membw.throttle_mode == THREAD_THROTTLE_UNDEFINED)
 		return;
 
-	rft = rdtgroup_get_rftype_by_name("thread_throttle_mode");
-	if (!rft)
-		return;
-
-	rft->fflags = RF_CTRL_INFO | RFTYPE_RES_MB;
+	resctrl_file_fflags_init("thread_throttle_mode",
+				 RF_CTRL_INFO | RFTYPE_RES_MB);
 }
 
-void mbm_config_rftype_init(const char *config)
+void resctrl_file_fflags_init(const char *config,
+			      unsigned long fflags)
 {
 	struct rftype *rft;
 
 	rft = rdtgroup_get_rftype_by_name(config);
 	if (rft)
-		rft->fflags = RF_MON_INFO | RFTYPE_RES_CACHE;
+		rft->fflags = fflags;
 }
 
 /**

fs/resctrl/internal.h
@@ -272,6 +272,7 @@ void cqm_setup_limbo_handler(struct rdt_domain *dom, unsigned long delay_ms,
 void cqm_handle_limbo(struct work_struct *work);
 bool has_busy_rmid(struct rdt_resource *r, struct rdt_domain *d);
 void __check_limbo(struct rdt_domain *d, bool force_free);
-void mbm_config_rftype_init(const char *config);
+void resctrl_file_fflags_init(const char *config,
+			      unsigned long fflags);
 
 #endif /* _FS_RESCTRL_INTERNAL_H */

fs/resctrl/monitor.c
@@ -739,11 +739,13 @@ static void l3_mon_evt_init(struct rdt_resource *r)
 
 	if (resctrl_arch_is_mbm_total_enabled()) {
 		mbm_total_event.configurable = true;
-		mbm_config_rftype_init("mbm_total_bytes_config");
+		resctrl_file_fflags_init("mbm_total_bytes_config",
+					 RF_MON_INFO | RFTYPE_RES_CACHE);
 	}
 	if (resctrl_arch_is_mbm_local_enabled()) {
 		mbm_local_event.configurable = true;
-		mbm_config_rftype_init("mbm_local_bytes_config");
+		resctrl_file_fflags_init("mbm_local_bytes_config",
+					 RF_MON_INFO | RFTYPE_RES_CACHE);
 	}
 
 	if (resctrl_arch_is_llc_occupancy_enabled())

commit 416cff69595aae30dc93ef691ac31b3a0e71ab36

anolis: x86: fix compiler warning in thread_throttle_mode_init
ANBZ: #9799

Warnings while compiling:
fs/resctrl/rdtgroup.c: In function ‘thread_throttle_mode_init’:
fs/resctrl/rdtgroup.c:2573:17: warning: unused variable ‘rft’ [-Wunused-variable]
 2573 |  struct rftype *rft;
      |                 ^~~
So fix it.

Fixes: e8c5929cf3ad ("x86/resctrl: Introduce resctrl_file_fflags_init() to initialize fflags")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

fs/resctrl/rdtgroup.c
@@ -2570,7 +2570,6 @@ static struct rftype *rdtgroup_get_rftype_by_name(const char *name)
 static void thread_throttle_mode_init(void)
 {
 	struct rdt_resource *r = resctrl_arch_get_resource(RDT_RESOURCE_MBA);
-	struct rftype *rft;
 
 	if (!r->alloc_capable ||
 	    r->membw.throttle_mode == THREAD_THROTTLE_UNDEFINED)