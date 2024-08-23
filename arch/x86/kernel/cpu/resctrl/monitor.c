jeevandeepj994/anolis_perf

commit 9683299508cf047e53a2d143c0c81e238234c7ec

x86/resctrl: Introduce mbm_total_cfg and mbm_local_cfg
ANBZ: #9790

cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/

If the BMEC (Bandwidth Monitoring Event Configuration) feature is
supported, the bandwidth events can be configured to track specific
events. The event configuration is domain specific. ABMC (Assignable
Bandwidth Monitoring Counters) feature needs event configuration
information to assign hardware counter to an RMID. Event configurations
are not stored in resctrl but instead always read from or written to
hardware directly when prompted by user space.

Read the event configuration from the hardware during the domain
initialization. Save the configuration information in rdt_hw_mon_domain,
so it can be used for counter assignment.

  [kun: change mbm_{local,total}_event.configurable into
   resctrl_arch_is_mbm_{local,total}_enabled(), cause they are the same
   logic.]

Signed-off-by: Babu Moger <babu.moger@amd.com>
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3731

arch/x86/kernel/cpu/resctrl/monitor.c
@@ -321,6 +321,32 @@ int __init rdt_get_mon_l3_config(struct rdt_resource *r)
 	return 0;
 }
 
+void resctrl_mbm_evt_config_init(struct rdt_hw_domain *hw_dom)
+{
+	unsigned int index;
+	u64 msrval;
+
+	/*
+	 * Read the configuration registers QOS_EVT_CFG_n, where <n> is
+	 * the BMEC event number (EvtID).
+	 */
+	if (resctrl_arch_is_mbm_total_enabled()) {
+		index = mon_event_config_index_get(QOS_L3_MBM_TOTAL_EVENT_ID);
+		rdmsrl(MSR_IA32_EVT_CFG_BASE + index, msrval);
+		hw_dom->mbm_total_cfg = msrval & MAX_EVT_CONFIG_BITS;
+	} else {
+		hw_dom->mbm_total_cfg = INVALID_CONFIG_VALUE;
+	}
+
+	if (resctrl_arch_is_mbm_local_enabled()) {
+		index = mon_event_config_index_get(QOS_L3_MBM_LOCAL_EVENT_ID);
+		rdmsrl(MSR_IA32_EVT_CFG_BASE + index, msrval);
+		hw_dom->mbm_local_cfg = msrval & MAX_EVT_CONFIG_BITS;
+	} else {
+		hw_dom->mbm_total_cfg = INVALID_CONFIG_VALUE;
+	}
+}
+
 void __init intel_rdt_mbm_apply_quirk(void)
 {
 	int cf_index;

arch/x86/kernel/cpu/resctrl/core.c
@@ -529,6 +529,8 @@ static void domain_add_cpu(int cpu, struct rdt_resource *r)
 		return;
 	}
 
+	resctrl_mbm_evt_config_init(hw_dom);
+
 	if (r->mon_capable && arch_domain_mbm_alloc(r->mon.num_rmid, hw_dom)) {
 		domain_free(hw_dom);
 		return;

arch/x86/kernel/cpu/resctrl/internal.h
@@ -85,6 +85,8 @@ struct arch_mbm_state {
  * @ctrl_val:	array of cache or mem ctrl values (indexed by CLOSID)
  * @arch_mbm_total:	arch private state for MBM total bandwidth
  * @arch_mbm_local:	arch private state for MBM local bandwidth
+ * @mbm_total_cfg:	MBM total bandwidth configuration
+ * @mbm_local_cfg:	MBM local bandwidth configuration
  *
  * Members of this structure are accessed via helpers that provide abstraction.
  */
@@ -93,6 +95,8 @@ struct rdt_hw_domain {
 	u32				*ctrl_val;
 	struct arch_mbm_state		*arch_mbm_total;
 	struct arch_mbm_state		*arch_mbm_local;
+	u32				mbm_total_cfg;
+	u32				mbm_local_cfg;
 };
 
 static inline struct rdt_hw_domain *resctrl_to_arch_dom(struct rdt_domain *r)
@@ -306,5 +310,6 @@ int rdt_get_mon_l3_config(struct rdt_resource *r);
 bool __init rdt_cpu_has(int flag);
 void __init intel_rdt_mbm_apply_quirk(void);
 void rdt_domain_reconfigure_cdp(struct rdt_resource *r);
+void resctrl_mbm_evt_config_init(struct rdt_hw_domain *hw_dom);
 
 #endif /* _ASM_X86_RESCTRL_INTERNAL_H */

fs/resctrl/internal.h
@@ -277,4 +277,5 @@ void resctrl_file_fflags_init(const char *config,
 
 int mbm_cntr_alloc(struct rdt_resource *r);
 void mbm_cntr_free(u32 cntr_id);
+unsigned int mon_event_config_index_get(u32 evtid);
 #endif /* _FS_RESCTRL_INTERNAL_H */

include/linux/resctrl_types.h
@@ -73,4 +73,5 @@ enum resctrl_event_id {
 
 #define RESCTRL_MAX_EVENT_NUM		4
 
+#define INVALID_CONFIG_VALUE		U32_MAX
 #endif /* __LINUX_RESCTRL_TYPES_H */

commit 558c99c63e9ce264029f42dc6f23064534460cd2

anolis: resctrl: fix unintended RDMSR in resctrl_mbm_evt_config_init
ANBZ: #9810

resctrl_mbm_evt_config_init will execute RDMSR while
mbm_{total,local}_event.configurable = true. But unfortunately, in
9683299508cf ("x86/resctrl: Introduce mbm_total_cfg and mbm_local_cfg")
just naively replace configurable logic to resctrl_arch_is_mbm{local,total}_enabled().
That make it a calltrace in platform that unsupport ABMC.

Fixes: 9683299508cf ("x86/resctrl: Introduce mbm_total_cfg and mbm_local_cfg")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

arch/x86/kernel/cpu/resctrl/monitor.c
@@ -331,7 +331,7 @@ void resctrl_mbm_evt_config_init(struct rdt_hw_domain *hw_dom)
 	 * Read the configuration registers QOS_EVT_CFG_n, where <n> is
 	 * the BMEC event number (EvtID).
 	 */
-	if (resctrl_arch_is_mbm_total_enabled()) {
+	if (resctrl_arch_is_mbm_total_configurable()) {
 		index = mon_event_config_index_get(QOS_L3_MBM_TOTAL_EVENT_ID);
 		rdmsrl(MSR_IA32_EVT_CFG_BASE + index, msrval);
 		hw_dom->mbm_total_cfg = msrval & MAX_EVT_CONFIG_BITS;
@@ -339,7 +339,7 @@ void resctrl_mbm_evt_config_init(struct rdt_hw_domain *hw_dom)
 		hw_dom->mbm_total_cfg = INVALID_CONFIG_VALUE;
 	}
 
-	if (resctrl_arch_is_mbm_local_enabled()) {
+	if (resctrl_arch_is_mbm_local_configurable()) {
 		index = mon_event_config_index_get(QOS_L3_MBM_LOCAL_EVENT_ID);
 		rdmsrl(MSR_IA32_EVT_CFG_BASE + index, msrval);
 		hw_dom->mbm_local_cfg = msrval & MAX_EVT_CONFIG_BITS;