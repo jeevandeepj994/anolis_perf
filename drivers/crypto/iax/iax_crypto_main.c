jeevandeepj994/anolis_perf

commit fcf11975e6d8e16105cdde753ccf63cb9876f20b

crypto: iax - Add async interface support
ANBZ: #2046

commit 631a333ca12110d17c59005eccb3b26cf06bd3d1 intel-github.

Add support for the crypto async interface, so that the IAX crypto
hardware can be used with facilities that require it, such as zswap.

Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/689
Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

drivers/crypto/iax/iax_crypto_main.c
@@ -10,6 +10,7 @@
 #include <linux/uacce.h>
 #include <uapi/linux/idxd.h>
 #include <linux/highmem.h>
+#include <crypto/internal/acompress.h>
 
 #include "registers.h"
 #include "idxd.h"
@@ -659,6 +660,65 @@ static struct crypto_alg iax_comp_deflate = {
 	}
 };
 
+static int iax_comp_acompress(struct acomp_req *req)
+{
+	struct crypto_tfm *tfm = req->base.tfm;
+	void *src, *dst;
+	int ret;
+
+	src = kmap_atomic(sg_page(req->src)) + req->src->offset;
+	dst = kmap_atomic(sg_page(req->dst)) + req->dst->offset;
+
+	pr_debug("%s: src %p (offset %d), slen %d, dst %p (offset %d), dlen %u\n",
+		 __func__, src, req->src->offset, req->slen,
+		 dst, req->dst->offset, req->dlen);
+
+	ret = iax_compress(tfm, (const u8 *)src, req->slen, (u8 *)dst, &req->dlen);
+
+	kunmap_atomic(src);
+	kunmap_atomic(dst);
+
+	if (ret != 0)
+		pr_warn("asynchronous compress failed ret=%d\n", ret);
+
+	return ret;
+}
+
+static int iax_comp_adecompress(struct acomp_req *req)
+{
+	struct crypto_tfm *tfm = req->base.tfm;
+	void *src, *dst;
+	int ret;
+
+	src = kmap_atomic(sg_page(req->src)) + req->src->offset;
+	dst = kmap_atomic(sg_page(req->dst)) + req->dst->offset;
+
+	pr_debug("%s: src %p (offset %d), slen %d, dst %p (offset %d), dlen %u\n",
+		 __func__, src, req->src->offset, req->slen,
+		 dst, req->dst->offset, req->dlen);
+
+	ret = iax_decompress(tfm, (const u8 *)src, req->slen, (u8 *)dst, &req->dlen);
+
+	kunmap_atomic(src);
+	kunmap_atomic(dst);
+
+	if (ret != 0)
+		pr_warn("asynchronous decompress failed ret=%d\n", ret);
+
+	return ret;
+}
+
+static struct acomp_alg iax_acomp_deflate = {
+	.compress		= iax_comp_acompress,
+	.decompress		= iax_comp_adecompress,
+	.base			= {
+		.cra_name		= "deflate",
+		.cra_driver_name	= "iax_crypto",
+		.cra_module		= THIS_MODULE,
+		.cra_priority           = IAX_ALG_PRIORITY,
+	}
+};
+
 static int iax_register_compression_device(void)
 {
 	int ret;
@@ -669,12 +729,24 @@ static int iax_register_compression_device(void)
 		return ret;
 	}
 
+	ret = crypto_register_acomp(&iax_acomp_deflate);
+	if (ret) {
+		pr_err("deflate algorithm acomp registration failed (%d)\n", ret);
+		goto err_unregister_alg_deflate;
+	}
+
+	return ret;
+
+err_unregister_alg_deflate:
+	crypto_unregister_alg(&iax_comp_deflate);
+
 	return ret;
 }
 
 static void iax_unregister_compression_device(void)
 {
 	crypto_unregister_alg(&iax_comp_deflate);
+	crypto_unregister_acomp(&iax_acomp_deflate);
 }
 
 static void iax_set_aecs(void)

commit 9d22f66d2aa59b205423d55ef43980e1e47a85f6

anolis: crypto: iax: fix deflate acomp registration failure
ANBZ: #9813

In fcf11975e6d8 ("crypto: iax - Add async interface support") introduced
acomp interface to iax_crypto, and reuse .cra_driver_name in its struct.
Though, it worked fine at that time, recently patch from upstream
84192e3639c7 ("crypto: api - Disallow identical driver names") disallow
that. After that patch being picked, .cra_driver_name reuse may cause an
error when crypto module init. So we reconfig acomp registration to a
different .cra_driver_name.

Fixes: fcf11975e6d8 ("crypto: iax - Add async interface support")
Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

drivers/crypto/iax/iax_crypto_main.c
@@ -1041,7 +1041,7 @@ static struct acomp_alg iax_acomp_deflate = {
 	.decompress		= iax_comp_adecompress,
 	.base			= {
 		.cra_name		= "deflate",
-		.cra_driver_name	= "iax_crypto",
+		.cra_driver_name	= "iax_crypto_acomp",
 		.cra_module		= THIS_MODULE,
 		.cra_ctxsize		= sizeof(struct deflate_generic_ctx),
 		.cra_init		= iax_deflate_generic_init,